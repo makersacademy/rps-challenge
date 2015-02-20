c2a5f2879c0004c078ee0657b4619d4d4a719da7
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-20 13:16:01 +0000

    player class passing specs

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,11 @@
 class Player
 
-  attr_accessor :name, :choice
+  attr_accessor :name, :choice, :score
 
   def initialize(name)
     @name = name
     @choice = choice
+    @score = 0
   end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,8 +8,22 @@ describe Player do
     expect(player.name).to eq "Kevin"
   end
 
-  it 'should start game without making a choice' do
-    expect(player.choice).to eq nil
+  it 'should start game having made no choice yet' do
+    expect(player.choice).to eq(nil)
+  end
+
+  it 'should make a choice' do
+    player.choice = "Rock"
+    expect(player.choice).to eq "Rock"
+  end
+
+  it 'should start with no score' do
+    expect(player.score).to eq(0)
+  end
+
+  it 'should calculate a score' do
+    player.score += 1
+    expect(player.score).to eq(1)
   end
 
 end
