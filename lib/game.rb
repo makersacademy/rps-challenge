3649054cbcc1a511c5ec2df7d288f35c7488badf
Kevin Lanzon <kevinlanzon@gmail.com>
2015-02-20 14:57:04 +0000

    all tests passing

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,30 @@
 class Game
 
   attr_accessor :player, :name
-  attr_reader :computer_choice
+  attr_reader :computer_choice, :winner
 
   def initialize
     @player = nil
     @name = name
     @computer_choice = nil
+    @winner = nil
+  end
+
+  RULES = {
+    "rock" => ["scissors"],
+    "scissors" => ["paper"],
+    "paper" => ["rock"]
+  }
+
+  def choose_randomly
+    @computer_choice = ["rock", "paper", "scissors"].sample
+  end
+
+  def result
+    choose_randomly
+    @winner = 'Draw' if @player.choice == @computer_choice
+    @winner = @player.name if RULES[@player.choice].include? @computer_choice
+    @winner = 'Computer' if RULES[@computer_choice].include? @player.choice
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,13 +9,27 @@ describe Game do
     expect(game.player).to eq(nil)
   end
 
-  it 'should let players join' do
+  it 'should let a player join' do
     game.player = player
     expect(game.player.name).to eq("Kevin")
   end
 
-  it 'opponent should start with no choice' do
+  it 'computer should start with no choice' do
     expect(game.computer_choice).to eq(nil)
   end
 
+  it 'should randomly choose the computer\'s choice' do
+    expect(game.choose_randomly).to satisfy{|m| ["rock", "paper", "scissors"].include?(m)}
+  end
+
+  it 'should start with no winner' do
+    expect(game.winner).to eq(nil)
+  end
+
+  it 'should know who wins the game' do
+    game.player = player
+    game.player.choice = "rock"
+    game.result
+    expect(game.winner).not_to eq nil
+  end
 end
