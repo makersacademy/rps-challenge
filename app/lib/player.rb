d45b9acd2d67c811c7d1eabf4c4be41113d9f45f
Erik Griffin <griffin.erik@gmail.com>
2015-02-20 12:12:21 +0000

    Armed players

--- app/lib/player.rb
+++ app/lib/player.rb
@@ -1,13 +1,18 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :weapon
 
   def initialize(name = 'Unknown Player')
     @name = name
+    @weapon = :no_weapon
   end
 
   def set_name(name)
     @name = name
+  end
+
+  def choose(weapon)
+    @weapon = weapon
   end
 
 
--- app/spec/player_spec.rb
+++ app/spec/player_spec.rb
@@ -9,6 +9,14 @@ describe 'Player' do
     expect(player.name).to eq 'Kirk'
   end
 
+  it 'has a weapon choice' do
+    expect(player.weapon).to eq :no_weapon
+  end
+
+  it 'can choose a weapon' do
+    player.choose(:ROCK)
+    expect(player.weapon).to eq :ROCK
+  end
 
 
 end
