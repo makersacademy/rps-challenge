b7750472d94dfd96c35028ef06e65edb2f991ae0
SebastienPires <pires.sebastien@gmail.com>
2015-02-20 13:30:54 +0000

    Added Winning, losing and draw games in specs, all tests green

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,8 @@ require 'game.rb'
 describe Game do
   let(:game){Game.new}
 
+  context 'Adding players' do
+
   it 'should be able to add a player' do
     expect(game.add_player1("Sebastien")).to eq ["Sebastien"]
   end
@@ -10,9 +12,50 @@ describe Game do
   it 'should be able to add a second player' do
     expect(game.add_player2("Opponent")).to eq ["Opponent"]
   end
+  end
+
+  context 'Winning game' do
 
   it 'should return win when player1 selects rock and player2 selects scissors' do
       expect(game.result(:rock, :scissors)).to eq 'Win!'
+  end
+
+  it 'should return win when player1 selects paper and player2 selects rock' do
+      expect(game.result(:paper, :rock)).to eq 'Win!'
+  end
+
+  it 'should return win when player1 selects scissors and player2 selects paper' do
+      expect(game.result(:scissors, :paper)).to eq 'Win!'
+  end
+  end
+
+  context 'Losing game' do
+
+  it 'should return lost when player1 selects scissors and player2 selects rock' do
+      expect(game.result(:scissors, :rock)).to eq 'Lost!'
+  end
+
+  it 'should return lost when player1 selects rock and player2 selects paper' do
+      expect(game.result(:rock, :paper)).to eq 'Lost!'
+  end
+
+  it 'should return lost when player1 selects paper and player2 selects scissors' do
+      expect(game.result(:paper, :scissors)).to eq 'Lost!'
+  end
+  end
+
+  context 'Draw game' do
+  it 'should return draw when player1 and player2 both choose rock' do
+      expect(game.result(:rock, :rock)).to eq 'Draw'
+  end
+
+  it 'should return draw when player1 and player2 both choose paper' do
+      expect(game.result(:paper, :paper)).to eq 'Draw'
+  end
+
+  it 'should return draw when player1 and player2 both choose scissors' do
+      expect(game.result(:scissors, :scissors)).to eq 'Draw'
+  end
   end
 
 end
