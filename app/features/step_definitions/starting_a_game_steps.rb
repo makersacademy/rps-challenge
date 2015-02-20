e987a1229a23dbc98af7f809af9ab9689e257001
tommasobratto <tbratto22@gmail.com>
2015-02-20 16:30:50 +0000

    added steps file

---
+++ app/features/step_definitions/starting_a_game_steps.rb
@@ -0,0 +1,27 @@
+Given(/^I am on the homepage$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+And(/^I fill the "(.*?)" form$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press the "(.*?)" button$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should receive a "(.*?)" with my chosen name$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on the player page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select the "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should receive a "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
