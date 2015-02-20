5b2570d3a97a952a7b2a5694938be1b8418a13b5
Chris Ward <chriswarddev@gmail.com>
2015-02-20 16:41:13 +0000

    cucumber tests pass

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -18,7 +18,7 @@ Given(/^I do not enter a name into the form "(.*?)"$/) do |player_name|
   fill_in(player_name, :with => "")
 end
 
-Then(/^I should stay on the home page and see "(.*?)"$/) do |text|
+Then(/^I should stay on the home page and see "(.*?)"MAM$/) do |text|
   expect(page).to have_content text
 end
 
