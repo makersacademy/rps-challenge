9b3d257444db1d64d3427c213d577770caa266d7
Tom Coakes <tomcoakes@gmail.com>
2015-02-21 12:01:52 +0000

    All outcomes working. Not happy with the implementation of Game#winner

--- lib/game.rb
+++ lib/game.rb
@@ -13,6 +13,14 @@ class Game
       return @players[1]
     elsif @players[0].weapon == :Paper && @players[1].weapon == :Rock
       return @players[0]
+    elsif @players[0].weapon == :Scissors && @players[1].weapon == :Paper
+      return @players[0]
+    elsif @players[0].weapon == :Paper && @players[1].weapon == :Scissors
+      return @players[1]
+    elsif @players[0].weapon == :Rock && @players[1].weapon == :Paper
+      return @players[1]
+    elsif @players[0].weapon == :Rock && @players[1].weapon == :Scissors
+      return @players[0]
     elsif @players[0].weapon == @players[1].weapon
       return nil
     end
