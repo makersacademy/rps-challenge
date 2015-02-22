2f478a1a7e9b1569e38f17280dc91a8b3272add3
Yannick Brunner <yannickbrunner@gmail.com>
2015-02-22 11:38:01 +0000

    cucumber-sinatra set up to get working

--- lib/my_app.rb
+++ lib/my_app.rb
@@ -1,8 +1,8 @@
 require 'sinatra/base'
 
-class Rps < Sinatra::Base
+class rps < Sinatra::Base
   get '/' do
-    'Hello Rps!'
+    'Hello rps!'
   end
 
   # start the server if ruby file executed directly
