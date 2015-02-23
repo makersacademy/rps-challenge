58cc84ee96d884588f9e5f073c270c86ca12a8e2
tommasobratto <tbratto22@gmail.com>
2015-02-23 10:19:04 +0000

    fixed indentation in player_spec.rb

--- app/spec/player_spec.rb
+++ app/spec/player_spec.rb
@@ -6,19 +6,19 @@ describe Player do
   let(:rock)     { double :element, name: "rock", loses_to: "paper"        }
   let(:scissors) { double :element, name: "scissors", loses_to: "rock" }
 
-    it "should have a name" do
-      expect(player.name).to eq("Mario")
-    end
+  it "should have a name" do
+    expect(player.name).to eq("Mario")
+  end
 
-    it "should be able to select an element" do
-      player.select_element(rock)
-      expect(player.element_selected).to eq(rock)
-    end
+  it "should be able to select an element" do
+    player.select_element(rock)
+    expect(player.element_selected).to eq(rock)
+  end
 
-    it "should be able to win" do
-      player.select_element(rock)
-      allow(rock).to receive(:confront).with(scissors)
-      allow(rock).to receive(:win).and_return(true)
-      expect(player.win?).to eq(true)
-    end
+  it "should be able to win" do
+    player.select_element(rock)
+    allow(rock).to receive(:confront).with(scissors)
+    allow(rock).to receive(:win).and_return(true)
+    expect(player.win?).to eq(true)
+  end
 end
