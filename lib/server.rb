25b39bebbdfaa18d441f227bc12479bb0192fd5d
Edward Brown <edward.brown.london@gmail.com>
2015-02-23 11:55:22 +0000

    two players on the same browser is working

--- lib/server.rb
+++ lib/server.rb
@@ -5,40 +5,50 @@ require '~/Projects/rps-challenge/lib/rps.rb'
 
 class RpsChallenge < Sinatra::Base
 
-	set :views, Proc.new { File.join(root, "..", "views") }
 
-	game = Game.new
-	ed = Player.new
-	diego = Player.new
-	game.add(ed)
-	game.add(diego)
-	rps_Ed = Rps.new
-	rps_Diego = Rps.new
-	ed.add(rps_Ed)
-	diego.add(rps_Diego)
-	p1choice = 'dawg'
-	p2choice = 'dawg'
+	enable :sessions
+
+	set :views, Proc.new { File.join(root, "..", "views") }
 
   get '/' do
     erb :index
   end
 
   get '/player1choose' do
-    erb :new_game
+    erb :p1choose
   end
 
-  post '/player1choose' do
-  		ed.choose_rock  if p1choice == 'Rock'	
-  		ed.choose_scissors if p1choice == 'Scissors'
-  		ed.choose_paper if p1choice == 'Paper'
-  		redirect ('/player2choose')
-  		erb :new_game
+  post '/player2choose' do
+		session[:p1] = params[:weapon]
+  	erb :p2choose
 	end
 
-	get '/player2choose' do
-    erb :new_game2
-  end
-  
+	post '/gameoutcome' do
+		session[:p2] = params[:weapon]
+
+		game = Game.new
+		player1 = Player.new
+		player2 = Player.new
+		game.add(player1)
+		game.add(player2)
+		rps_1 = Rps.new
+		rps_2 = Rps.new
+		player1.add(rps_1)
+		player2.add(rps_2)
+		
+
+		player1.choose_scissors if session[:p1] == 'Scissors'
+		player1.choose_paper if session[:p1] == 'Paper'
+		player1.choose_rock if session[:p1] == 'Rock'
+
+		player2.choose_scissors if session[:p2] == 'Scissors'
+		player2.choose_paper if session[:p2] == 'Paper'
+		player2.choose_rock if session[:p2] == 'Rock'
+
+		@output = game.winner
+
+		erb :game_winner
+	end
 
 
 
