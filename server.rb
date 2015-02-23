ba65680d2790f47f24f72a1ebcb95ff10f0a6803
gabe <GabeMaker@users.noreply.github.com>
2015-02-23 14:22:50 +0000

    can restart games

--- server.rb
+++ server.rb
@@ -7,31 +7,32 @@ class RPS < Sinatra::Base
   get '/' do
     GAME = Game.new
     computer  = Player.new(name: "computer player")
-    
-    computer.pick [:rock, :paper, :scissors].sample
+
+    # computer.pick [:rock, :paper, :scissors].sample
     GAME.add computer
-    
+
     erb :name
   end
 
   post '/play' do
-    
+    GAME.player_one.pick [:rock, :paper, :scissors].sample
+    GAME.players.pop if GAME.players.count == 2
     @name     = params[:name]
     user      = Player.new(name: @name)
     GAME.add user
-    
+
     erb :play
   end
 
   post '/result' do
-    
+
     @user_shape     = GAME.player_two.pick params.values.first.to_sym
     @computer_shape = GAME.player_one.shape
     @name = GAME.player_two.name
-    
+
     if GAME.winner == GAME.player_one
       @winner = :computer
-    elsif GAME.winner == GAME.player_two  
+    elsif GAME.winner == GAME.player_two
       @winner = :user
     end
 
