54d039b82d7f5fba380156008a8680863a56a1d0
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-15 13:28:22 +0000

    removed game constant

--- app/controllers/one_player.rb
+++ app/controllers/one_player.rb
@@ -1,4 +1,4 @@
-GAME = Game.new
+game = Game.new
 
 get '/one_player' do
 
@@ -11,8 +11,8 @@ end
 post '/one_player' do
     
   computer = Computer.new
-  GAME.player1 = get_player
-  GAME.player2 = computer  
+  game.player1 = get_player
+  game.player2 = computer  
   get_player.name = params[:name]
 
   erb :one_player
@@ -20,9 +20,9 @@ end
 
 post '/one_player_game' do 
 
-  p get_player.selection = params[:choice].to_sym
-  p @computer_choice = GAME.player2.get_selection
-  p @result = GAME.determine_winner
+  get_player.selection = params[:choice].to_sym
+  @computer_choice = game.player2.get_selection
+  @result = game.determine_winner
 
   erb :one_player
 end  
--- app/controllers/two_player.rb
+++ app/controllers/two_player.rb
@@ -1,10 +1,19 @@
-get '/two_player' do
+game = Game.new
 
+get '/two_player' do
+  player = Player.new 
+  session[:player_id] = player.object_id
   erb :two_player
 end  
 
 post '/two_player' do
-  
-
+  get_player.name = params[:name]
+  game.player1 == nil ? game.player1 = get_player : game.player2 = get_player
   erb :two_player
 end 
+
+post '/two_player_game' do 
+  get_player.selection = params[:choice].to_sym
+  @result = game.determine_winner
+  erb :two_player
+end  
