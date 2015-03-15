34d407616e6a6e96fd69ad5f08fc23587af54942
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-15 20:09:10 +0000

    pretty sure this should be done with databases to store user details, and sessions for the moves

--- app/controllers/two_player.rb
+++ app/controllers/two_player.rb
@@ -2,36 +2,35 @@ GAME = Game.new
 @ready = true
 
 get '/two_player' do
-
   player = Player.new 
   session[:player_id] = player.object_id
-
   erb :two_player
 end  
 
 post '/two_player' do
-
   get_player.name = params[:name]    
   GAME.player1 ? GAME.player2 = get_player : GAME.player1 = get_player    
-
   erb :two_player
 end 
 
 get '/two_player_game' do 
-
   get_opponent.selection ? @ready = true : @ready = false
-  @opponent_choice = get_opponent.selection 
   @result = GAME.determine_winner
-
   erb :two_player
 end  
 
 post '/two_player_game' do 
-
   get_player.selection = params[:choice].to_sym  
   get_opponent.selection ? @ready = true : @ready = false
-  @opponent_choice = get_opponent.selection 
   @result = GAME.determine_winner
+  erb :two_player
+end  
+
+get '/go_again' do 
+  @result = nil
+  get_player.selection = nil
+  get_opponent.selection = nil
+  @ready = true
 
   erb :two_player
 end  
