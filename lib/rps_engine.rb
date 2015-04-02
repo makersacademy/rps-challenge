ee5577340a6222b0aab6299a8ee9f1c2c6890274
Richard Lynch <rjlynch86@googlemail.com>
2015-04-02 17:50:01 +0100

    adds rps engine and some tests

---
+++ lib/rps_engine.rb
@@ -0,0 +1,27 @@
+class RpsGame
+  attr_reader :p1_move, :winner
+
+  MOVES = [:rock, :paper, :scissors]
+
+  def p1_go move
+    fail 'not a valid move' unless MOVES.include? move
+    @p1_move = move
+  end
+
+  def compare player1_move, player2_move
+    @winner = :draw if player1_move == player2_move
+
+    case player1_move
+    when :rock
+      @winner = player1_move if player2_move == :scissors
+      @winner = player2_move if player2_move == :paper
+    when :paper
+      @winner = player1_move if player2_move == :rock
+      @winner = player2_move if player2_move == :scissors
+    when :scissors
+      @winner = player1_move if player2_move == :paper
+      @winner = player2_move if player2_move == :rock
+    end
+    @winner
+  end
+end
---
+++ spec/rps_engine_spec.rb
@@ -0,0 +1,41 @@
+require 'rps_engine'
+
+describe RpsGame do
+  let(:game) {described_class.new} 
+  let(:moves) {[:rock, :paper, :scissors]}
+
+  context 'taking input' do
+
+    it 'can take a move as input' do
+      moves.each do |move|
+        game.p1_go move
+        expect(game.p1_move).to eq move
+      end
+    end
+
+    it 'raises an error if input move is incorrect' do
+      expect{game.p1_go :not_a_move}.to raise_error 'not a valid move'
+    end
+
+  end
+
+  context 'given two moves' do
+
+    it 'can find the winner' do
+      #USE BETTER LOGIC CHECK, this one is trivial
+      # game.p1_go :rock
+      # allow(game).to receice(:computer_go).and_return(:paper)
+      game.compare :rock, :paper
+      expect(game.winner).to be :paper
+    end
+
+    it 'can declare a draw' do
+      game.compare :rock, :rock
+      expect(game.winner).to be :draw
+    end
+  
+  end
+
+
+
+end
