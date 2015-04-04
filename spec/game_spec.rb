1576493ff6628beaaa8972ecf09f380439fb429a
Jade King <dev@jadeking.co.uk>
2015-04-04 21:28:05 +0100

    ficx numbers to start from 0

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,9 @@
 require 'game'
 describe Game do
   it 'picks a random number between' do
-    expect(subject.choose_move).to be_between(1, 3)
+    expect(subject.choose_move).to be_between(0, 2)
   end
   it 'translates a number to rock, paper or scissors' do
-    expect(subject.tranlate_move(3)).to eq "Scissors"
+    expect(subject.tranlate_move(2)).to eq "Scissors"
   end
 end
