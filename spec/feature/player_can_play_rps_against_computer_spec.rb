cd9525d8db9878667104a98c4d2c26a71e76f016
Jade King <dev@jadeking.co.uk>
2015-04-04 23:56:55 +0100

    change scenario title

--- spec/feature/player_can_play_rps_against_computer_spec.rb
+++ spec/feature/player_can_play_rps_against_computer_spec.rb
@@ -1,8 +1,9 @@
 feature 'player can play Rock, paper, scissors against the computer' do
-  scenario 'player plays against the computer in the command line' do
+  scenario 'player plays against the computer' do
     player = Player.new("Jade")
     round = Game.new
-    allow(round).to receive(:computer_move).and_return("scissors")
-    expect(round.play("rock", round.computer_move)).to eq "Jade wins!"
+    allow(round).to receive(:computer_move).and_return("Scissors")
+    expect(round.play(player.name, "Rock", round.computer_move)
+      ).to eq "Jade wins!"
   end
 end
