24ecafa15f3d083af5152013b353451f36a49563
Jade King <dev@jadeking.co.uk>
2015-04-04 22:14:23 +0100

    test accept player move

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,4 +6,11 @@ describe Game do
   it 'translates a number to rock, paper or scissors' do
     expect(subject.translate_move(2)).to eq "Scissors"
   end
+  it 'returns the computers move' do
+    allow(subject).to receive(:computer_move).and_return('Scissors')
+    expect(subject.computer_move).to eq 'Scissors'
+  end
+  it 'accepts a players move' do
+    expect(subject.play('Rock')).to eq 'Rock'
+  end
 end
