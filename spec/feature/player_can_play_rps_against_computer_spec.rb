1c7c9f2ac0cc4587d89224e72dafcde0e66f41d0
Jade King <dev@jadeking.co.uk>
2015-04-04 21:38:04 +0100

    correct method name

--- spec/feature/player_can_play_rps_against_computer_spec.rb
+++ spec/feature/player_can_play_rps_against_computer_spec.rb
@@ -2,7 +2,7 @@ feature 'player can play Rock, paper, scissors against the computer' do
   scenario 'player plays against the computer in the command line' do
     player = Player.new("Jade")
     round = Game.new
-    allow(round).to receive(:computer_turn).and_return("scissors")
+    allow(round).to receive(:computer_move).and_return("scissors")
     round.computer_move
     expect(round.play("rock")).to eq "Jade wins!"
   end
