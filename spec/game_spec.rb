39c1e3dbbda753c421736585e0afffa1b8f1cbec
Jade King <dev@jadeking.co.uk>
2015-04-04 21:15:29 +0100

    Game translate nu,ber to string move test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,5 +3,7 @@ describe Game do
   it 'picks a random number between' do
     expect(subject.choose_move).to be_between(1, 3)
   end
-  xit 'translates a number to rock, paper or scissors'
+  it 'translates a number to rock, paper or scissors' do
+    expect(subject.tranlates_move(3)).to eq "Scissors"
+  end
 end
