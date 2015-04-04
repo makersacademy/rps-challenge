59f336793a5558c72af57b7af2fba532badf5331
Jade King <dev@jadeking.co.uk>
2015-04-04 20:57:16 +0100

    fix syntax error

--- spec/feature/player_can_play_rps_against_computer_spec.rb
+++ spec/feature/player_can_play_rps_against_computer_spec.rb
@@ -2,7 +2,7 @@ feature 'player can play Rock, paper, scissors against the computer' do
   scenario 'player plays against the computer in the command line' do
     player = Player.new("Jade")
     round = Game.new
-    allow(round).to_receive(:computer_turn).and_return("scissors")
+    allow(round).to receive(:computer_turn).and_return("scissors")
     round.computer_turn
     expect(round.play("rock")).to eq "Jade wins!"
   end
