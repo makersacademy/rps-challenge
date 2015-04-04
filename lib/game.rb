e7d3051efefdf9b700f8bbbb97b935a1f483254e
Jade King <dev@jadeking.co.uk>
2015-04-04 22:03:29 +0100

    method computer move passing test

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,14 @@
 class Game
   def choose_move
-    rand(2)
+    rand(3)
   end
 
   def translate_move(number)
     moves = %w(Rock Paper Scissors)
     moves[number]
+  end
+
+  def computer_move
+    translate_move(choose_move)
   end
 end
