a6b9c9d9c921f24a824de02563cbafbf84bbe01b
Jade King <dev@jadeking.co.uk>
2015-04-04 20:34:18 +0100

    fix parenteses

--- spec/feature/player_can_play_rps_against_computer_spec.rb
+++ spec/feature/player_can_play_rps_against_computer_spec.rb
@@ -1,7 +1,7 @@
 feature 'player can play Rock, paper, scissors against the computer' do
   scenario 'player plays against the computer in the command line' do
     player = Player.new("Jade")
-    round = Game.new()
+    round = Game.new
     allow(round).to_receive(:computer_turn).and_return("scissors")
     computer_choice = round.computer_turn
     expect(round.play("rock")).to eq "Jade wins!"
