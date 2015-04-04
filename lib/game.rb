62ab057405c7e6ad0ef54eb2714648ebf85ef786
Jade King <dev@jadeking.co.uk>
2015-04-04 23:55:13 +0100

    play method working

--- lib/game.rb
+++ lib/game.rb
@@ -12,11 +12,12 @@ class Game
     translate_move(choose_move)
   end
 
-  def play(move, computer_move)
+  def play(name, move, computer_move)
     winning_pairs = [%w(Rock Scissors), %w(Scissors Paper), %w(Paper Rock)]
     winner = winning_pairs.any? do |player_move, comp_move|
       player_move == move && comp_move == computer_move
     end
-    winner == true ? 'Player' : 'Computer'
+    result = winner == true ? name : 'Computer'
+    "#{result} wins!"
   end
 end
