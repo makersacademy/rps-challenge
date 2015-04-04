69a84cf320d9d33d9a4f79c4ae88ecd4976b3e6f
Jade King <dev@jadeking.co.uk>
2015-04-04 23:25:39 +0100

    update test return winner string

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,6 +12,6 @@ describe Game do
   end
   it 'compares players move against the computers move and returns winner' do
     allow(subject).to receive(:computer_move).and_return('Scissors')
-    expect(subject.play('Rock', subject.computer_move)).to eq 'Player'
+    expect(subject.play('Rock', subject.computer_move)).to eq 'Jade wins!'
   end
 end
