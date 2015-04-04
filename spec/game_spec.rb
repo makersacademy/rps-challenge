ea5436669e9e235d5029178f4e476a80054daa3f
Jade King <dev@jadeking.co.uk>
2015-04-04 20:50:25 +0100

    add Game pick random number test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,6 @@
 describe Game do
-  it
+  it 'picks a random number between' do
+    expect(subject.choose_move).to be_between(1, 3)
+  end
+  xit 'translates a number to rock, paper or scissors'
 end
