3e12573be1decf619c81709ebda6ef16916f6bfe
Jade King <dev@jadeking.co.uk>
2015-04-05 03:57:00 +0100

    refactor play method

--- lib/game.rb
+++ lib/game.rb
@@ -3,11 +3,14 @@ class Game
   include RpsDroid
   def play(name, move)
     computer_mv = make_move
+    result = winner?(move, computer_mv) == true ? name : 'Computer'
+    "#{name} chose: #{move} Computer chose: #{computer_mv}, #{result} wins!"
+  end
+
+  def winner?(user_move, computer_move)
     winning_pairs = [%w(Rock Scissors), %w(Scissors Paper), %w(Paper Rock)]
     winner = winning_pairs.any? do |player_move, comp_move|
-      player_move == move && comp_move == computer_mv
+      player_move == user_move && comp_move == computer_move
     end
-    result = winner == true ? name : 'Computer'
-    "#{name} chose: #{move} Computer chose: #{computer_mv}, #{result} wins!"
   end
 end
