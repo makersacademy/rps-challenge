cc580bd93bddc4e40eddf8bbe1ea61dbbf6b0eda
Jade King <dev@jadeking.co.uk>
2015-04-05 04:48:16 +0100

    validate move input method

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,18 @@
 require_relative 'rps_droid'
 class Game
   include RpsDroid
+  attr_reader :valid_moves
+  def initialize
+    @valid_moves = %w(Rock Paper Scissors)
+  end
+
   def play(name, move)
+    fail "I don't recognise that move, try again" unless valid_move?(move)
     computer_mv = make_move
-    result = winner?(move, computer_mv) == true ? name : 'Computer'
-    "#{name} chose: #{move} Computer chose: #{computer_mv}, #{result} wins!"
+    moves = "#{name} chose: #{move} Computer chose: #{computer_mv}, "
+    return moves + "Its a draw!" if draw?(move, computer_mv)
+    result = winner?(move, computer_mv) ? name : 'Computer'
+    moves + "#{result} wins!"
   end
 
   def winner?(user_move, computer_move)
@@ -12,5 +20,13 @@ class Game
     winner = winning_pairs.any? do |player_move, comp_move|
       player_move == user_move && comp_move == computer_move
     end
+  end
+
+  def draw?(user_move, computer_move)
+    user_move == computer_move
+  end
+
+  def valid_move?(player_move)
+    @valid_moves.any? { |move| move == player_move }
   end
 end
