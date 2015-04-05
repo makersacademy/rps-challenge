f26bf808ccd5d4590366ac3e0c1270c03ad26e38
Jade King <dev@jadeking.co.uk>
2015-04-05 04:34:08 +0100

    add error check scenario

--- spec/feature/player_can_play_rps_against_computer_spec.rb
+++ spec/feature/player_can_play_rps_against_computer_spec.rb
@@ -1,9 +1,20 @@
 feature 'player can play Rock, paper, scissors against the computer' do
-  scenario 'player plays against the computer' do
-    player = Player.new("Jade")
-    round = Game.new
+  let(:player) { Player.new("Jade") }
+  let(:round) { Game.new }
+  before(:each) do
     allow(round).to receive(:make_move).and_return("Scissors")
+  end
+  scenario 'player plays against the computer with winning hand' do
     expect(round.play(player.name, "Rock")
       ).to eq 'Jade chose: Rock Computer chose: Scissors, Jade wins!'
+  end
+  scenario 'player and computer make same choice' do
+    expect(round.play(player.name, "Scissors")
+      ).to eq 'Jade chose: Scissors Computer chose: Scissors, Its a draw!'
+  end
+  scenario 'player inputs incorrect value' do
+    expect do
+      round.play(player.name, "blah")
+    end.to raise_error "I don't recognise that move, try again"
   end
 end
