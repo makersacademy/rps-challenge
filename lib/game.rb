b40d2070b6352ea04a74d24d3958f1c9b3de28df
Kanishk Walia <kanishk.walia@gmail.com>
2015-04-05 18:35:27 +0100

    Added code for potential winning combinations

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,20 @@
 class Game
 
+	attr_reader :potential_winning_combinations
+
 	def initialize
 		@new_player = nil
+		@potential_winning_combinations = { "paper" => "rock", "rock" => "scissors", "scissors" => "paper" }
 	end
 
-	def outcome (rock, scissors)
+	def outcome(player1_selection, opponent_selection)
+		if opponent_selection == potential_winning_combinations[player1_selection]
+		message = "Congratulations, you have won!"
+		end
+		puts message
 	end
 
-	def add_player(Player)
-		@new_player = Player
+	def add_player(player1)
+		@new_player = player1
 	end
-
 end
