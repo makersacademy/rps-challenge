a6df2093201413557ba795ca8f423f5569d5ce0a
Jade King <dev@jadeking.co.uk>
2015-04-05 02:20:09 +0100

    seperate out droid tests from game class

--- spec/rps_droid_spec.rb
+++ spec/rps_droid_spec.rb
@@ -3,4 +3,11 @@ describe RpsDroid do
   it 'picks a random number between' do
     expect(subject.make_move).to be_between(0, 2)
   end
+  it 'translates a number to rock, paper or scissors' do
+    expect(subject.translate_move(2)).to eq "Scissors"
+  end
+  it 'returns the computers move' do
+    allow(subject).to receive(:computer_move).and_return('Scissors')
+    expect(subject.computer_move).to eq 'Scissors'
+  end
 end
