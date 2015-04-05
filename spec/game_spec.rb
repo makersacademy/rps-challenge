48b274955055eebf9f94a647a6dd5f013803287d
Jade King <dev@jadeking.co.uk>
2015-04-05 02:22:00 +0100

    moved game droid tests to own class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,5 @@
 require 'game'
 describe Game do
-  it 'picks a random number between' do
-    expect(subject.choose_move).to be_between(0, 2)
-  end
-  it 'translates a number to rock, paper or scissors' do
-    expect(subject.translate_move(2)).to eq "Scissors"
-  end
-  it 'returns the computers move' do
-    allow(subject).to receive(:computer_move).and_return('Scissors')
-    expect(subject.computer_move).to eq 'Scissors'
-  end
   it 'compares players move against the computers move and returns winner' do
     allow(subject).to receive(:computer_move).and_return('Scissors')
     expect(subject.play('Jade', 'Rock', subject.computer_move)
