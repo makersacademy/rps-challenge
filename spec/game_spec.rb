108489176b177bee58a9eb631395f242a42e9896
Jade King <dev@jadeking.co.uk>
2015-04-05 04:34:30 +0100

    add error check unit test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,19 @@
 require 'game'
 describe Game do
-  it 'compares players move against the computers move and returns winner' do
+  before(:each) do
     allow(subject).to receive(:make_move).and_return('Scissors')
+  end
+  it 'compares players move against the computers move and returns winner' do
     expect(subject.play('Jade', 'Rock')
       ).to eq 'Jade chose: Rock Computer chose: Scissors, Jade wins!'
+  end
+  it 'returns draw if both play the same move' do
+    expect(subject.play('Jade', 'Scissors')
+      ).to eq 'Jade chose: Scissors Computer chose: Scissors, Its a draw!'
+  end
+  it 'raises an error if player inputs incorrect value' do
+    expect do
+      subject.play('Jade', "blah")
+    end.to raise_error "I don't recognise that move, try again"
   end
 end
