65a25104ca2264ce4d45cc5882a4cce2a8efa1df
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-05 20:20:41 +0100

    Game can compare 2 hands and gives the winner s name or returns Tie

--- lib/game.rb
+++ lib/game.rb
@@ -9,12 +9,48 @@ class Game
     @hand_value = %w(Rock Paper Scissors)[rand(3)]
   end
 
-  def add_player(name)
+  def add_player(player)
     if @number_of_p < 2
       @number_of_p += 1
-      @players << name
+      @players << player
     else
-      fail 'Sorry ! 2 players only!'
+      fail 'Sorry! 2 players only!'
+    end
+  end
+
+  def winner
+    matching_hands_with_players
+    comparing_hands
+  end
+
+  def matching_hands_with_players
+    @player1 = @players[0]
+    if @players.length == 1
+      @player2 = 'The game'
+      @phv2 = chose_hand.downcase
+    else
+      @player2 = @players[1]
+      @phv2 = @player2.hand_value.downcase
+    end
+    @phv1 = @player1.hand_value.downcase
+  end
+
+  def comparing_hands
+    # I know it's ugly. Will need major refactoring! But it works so far!
+    if @phv1 == @phv2
+      fail 'Tie!'
+    elsif @phv1 == 'rock' && @phv2 == 'scissors'
+      @player1
+    elsif @phv1 == 'rock' && @phv2 == 'paper'
+      @player2
+    elsif @phv1 == 'paper' && @phv2 == 'rock'
+      @player1
+    elsif @phv1 == 'paper' && @phv2 == 'scissors'
+      @player2
+    elsif @phv1 == 'scissors' && @phv2 == 'rock'
+      @player2
+    elsif @phv1 == 'scissors' && @phv2 == 'paper'
+      @player1
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,31 +3,31 @@ require 'game'
 describe Game do
   let(:game) { described_class.new }
   let(:player1) { double :player, hand_value: 'Rock' }
+  let(:player2) { double :player, hand_value: 'Scissors' }
+  let(:player2b) { double :player, hand_value: 'Rock' }
+  before { game.add_player(player1) }
+
   it 'can choose a random Rock, Paper or Scissors hand' do
     game.chose_hand
     expect(game.hand_value).to satisfy { 'Rock' || 'Paper' || 'Scissors' }
   end
 
   it 'knows the players that are in the game' do
-    game.add_player(player1)
     expect(game.players).to eq [player1]
   end
 
   it 'denies access to more than 2 players' do
-    player2 = double :player
-    player3 = double :player
-    game.add_player(player1)
     game.add_player(player2)
-    expect { game.add_player(player3) }.to raise_error 'Sorry ! 2 players only!'
+    expect { game.add_player(player2b) }.to raise_error 'Sorry! 2 players only!'
   end
 
-  xit 'can compare its hand with the player hand' do
+  it 'can compare 2 hands and gives the winner\'s name' do
+    game.add_player(player2)
+    expect(game.winner).to eq player1
   end
 
-  xit 'gives a winner if there is one' do
+  it 'returns Tie! if it\'s a tie' do
+    game.add_player(player2b)
+    expect { game.winner } .to raise_error 'Tie!'
   end
-
-  xit 'tells that it\'s a tie if it\'s the same hand as player' do
-  end
-
 end
