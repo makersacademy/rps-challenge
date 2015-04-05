872cd8a34d8f08b82e1ee0d6acba7ebde1e08101
Jade King <dev@jadeking.co.uk>
2015-04-05 06:28:30 +0100

    correct class camelcasing

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,10 +1,10 @@
 require 'sinatra/base'
 
-class Rock_paper_scissors < Sinatra::Base
+class RockPaperScissors < Sinatra::Base
   get '/' do
     'Hello Rock_paper_scissors!'
   end
 
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
 end
