975f00a32ee7520bc755cfde7bad2f67629e3fbb
Jade King <dev@jadeking.co.uk>
2015-04-05 02:22:18 +0100

    moved droid methods to seperate class

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,4 @@
 class Game
-  def choose_move
-    rand(3)
-  end
-
-  def translate_move(number)
-    moves = %w(Rock Paper Scissors)
-    moves[number]
-  end
-
-  def computer_move
-    translate_move(choose_move)
-  end
-
   def play(name, move, computer_mv)
     winning_pairs = [%w(Rock Scissors), %w(Scissors Paper), %w(Paper Rock)]
     winner = winning_pairs.any? do |player_move, comp_move|
