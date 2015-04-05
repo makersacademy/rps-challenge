12bd2f0484d7402f31d4c5d9e197144743678833
Jade King <dev@jadeking.co.uk>
2015-04-05 02:16:29 +0100

    update feature to include RpsDroid class

--- spec/feature/player_can_play_rps_against_computer_spec.rb
+++ spec/feature/player_can_play_rps_against_computer_spec.rb
@@ -1,9 +1,10 @@
 feature 'player can play Rock, paper, scissors against the computer' do
   scenario 'player plays against the computer' do
     player = Player.new("Jade")
+    computer = RpsDroid.new
     round = Game.new
-    allow(round).to receive(:computer_move).and_return("Scissors")
-    expect(round.play(player.name, "Rock", round.computer_move)
+    allow(computer).to receive(:make_move).and_return("Scissors")
+    expect(round.play(player.name, "Rock", computer.make_move)
       ).to eq "Jade wins!"
   end
 end
