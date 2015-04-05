7bc9d81f4ac30eef4b654b0f44f515a907592bfc
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-05 16:41:29 +0100

    Player has a name

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,6 @@
 class Player
+  attr_reader :name
+  def initialize(name = 'Player1')
+    @name = name
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,9 @@
 require 'player'
 
 describe Player do
-  xit 'has a name' do
+  player = described_class.new('Guillaume')
+  it 'has a name' do
+    expect(player.name). to eq 'Guillaume'
   end
 
   xit 'can choose a hand' do
