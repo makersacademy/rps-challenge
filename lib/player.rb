ecd93a6ef1c07eead3f59bc3106a3f6af375f13a
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-05 16:55:43 +0100

    player can choose a hand

--- lib/player.rb
+++ lib/player.rb
@@ -3,4 +3,10 @@ class Player
   def initialize(name = 'Player1')
     @name = name
   end
+
+  def hand_value(choice)
+    # attr_writer :hand_value didn't work at first (before if-statement)
+    # need to ask teachers why
+    @hand_value = choice
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,7 +6,8 @@ describe Player do
     expect(player.name). to eq 'Guillaume'
   end
 
-  xit 'can choose a hand' do
+  it 'can choose a hand' do
+    expect(player.hand_value('Rock')). to eq 'Rock'
   end
 
   xit 'can only choose a hand that is Rock paper or Scissors' do
