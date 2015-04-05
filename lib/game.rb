e658b1c8796d48766b52bfc318432ad13361045a
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-05 17:23:58 +0100

    game can choose a random Rock, Paper or Scissors hand

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,6 @@
 class Game
+  attr_reader :hand_value
+  def chose_hand
+    @hand_value = %w(Rock Paper Scissors)[rand(3)]
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,10 @@
 require 'game'
 
 describe Game do
-  xit 'can choose a random Rock, Paper or Scissors hand' do
+  game = described_class.new
+  it 'can choose a random Rock, Paper or Scissors hand' do
+    game.chose_hand
+    expect(game.hand_value).to satisfy { 'Rock' || 'Paper' || 'Scissors' }
   end
 
   xit 'knows that there is a player online' do
