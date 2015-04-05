d85156d8056c24b778ea22664e5f7e1c261d18e6
Kanishk Walia <kanishk.walia@gmail.com>
2015-04-05 18:13:36 +0100

    Added tests for winning context and player registration

---
+++ lib/game.rb
@@ -0,0 +1,14 @@
+class Game
+
+	def initialize
+		@new_player = nil
+	end
+
+	def outcome (rock, scissors)
+	end
+
+	def add_player(Player)
+		@new_player = Player
+	end
+
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,28 @@
+require 'game'
+
+describe Game do
+	let(:game) {Game.new}
+
+	context "when registering a player" do
+ 
+		it "can add a player to the game" do
+			expect(game.add_player("Player1")).to eq "Player1"
+		end
+	end
+
+	context "When Player1 is the winner" do
+
+		it "should allow Player1 to win when Player1 selects paper and the opponent selects rock" do
+			expect(game.outcome(:paper, :rock)).to eq "Congratulations, you have won!"
+		end
+
+		it "should allow Player1 to win when Player1 selects rock and the opponent selects scissors" do
+			expect(game.outcome(:rock, :scissors)).to eq "Congratulations, you have won!"
+		end
+
+		it "should allow Player1 to win when Player1 selects scissors and the opponent selects paper" do
+			expect(game.outcome(:scissors, :paper)).to eq "Congratulations, you have won!"
+		end
+	end
+
+end
