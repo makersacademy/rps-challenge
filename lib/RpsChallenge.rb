88dc76be673535cbf988d25779b0f3c08a4ec1c9
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-06 19:02:19 +0100

    first erb files created. Not linked to game yet

--- lib/RpsChallenge.rb
+++ lib/RpsChallenge.rb
@@ -1,8 +1,28 @@
 require 'sinatra/base'
+require_relative 'game'
+require_relative 'player'
 
 class RpsChallenge < Sinatra::Base
+  set :public, Proc.new { File.join(root,"..", "public")}
+  set :views, Proc.new { File.join(root,"..", "views")}
+  enable :sessions
+
   get '/' do
-    'Hello RpsChallenge!'
+    erb :index
+  end
+
+  get '/start' do
+    erb :name_page
+  end
+
+  get '/new_game' do
+    @num = params[:number]
+    erb :enter_names
+    # session[:number] = @num
+  end
+
+  get '/choose_hands' do
+    erb :enter_names
   end
 
   # start the server if ruby file executed directly
