52fc4da631608156e9519a8c1b0cc5279596d706
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-06 23:36:34 +0100

    trying to link the ruby files with the web version

--- lib/RpsChallenge.rb
+++ lib/RpsChallenge.rb
@@ -17,18 +17,33 @@ class RpsChallenge < Sinatra::Base
 
   get '/new_game' do
     @num = params[:number]
+    game = Game.new
     erb :enter_names
   end
 
   get '/choose_hands' do
     @name1 = params[:name1]
     @name2 = params[:name2]
+    @player1 = session[:player1] || Player.new(@name1)
+    @player2 = session[:player2] || Player.new(@name2)
+    session[:player1] = @player1
+    session[:player2] = @player2
     erb :hands_choices
   end
 
   get '/results' do
-    @name1 = params[:name1]
-    @name2 = params[:name2]
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+    @choice1 = params[:choice1]
+    @choice2 = params[:choice2]
+    # not working! I struggle in passing my rps game along. It works
+    # in irb.
+    # @player1.chose_hand(@choice1)
+    # @player2.chose_hand(@choice2)
+    # game.add_player(@player1)
+    # game.add_player(@player2)
+    # game.winner
+
     erb :results
   end
 
