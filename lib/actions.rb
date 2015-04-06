4439aee8108470e47f64fd7551f896839a8efb90
Mark Pokorny <iridium193@gmail.com>
2015-04-06 20:18:26 +0100

    Attempted to clean up ‘Actions’ module to keep RuboCop/Hound happy. Not very successful!

--- lib/actions.rb
+++ lib/actions.rb
@@ -2,18 +2,17 @@ module Actions
   ALLOWED_ACTIONS = [:rock, :paper, :scissors]
 
   def winning_action action1, action2
-    return nil if action1 == action2
+    winning_action = nil
     if action1 == :rock
-      return :rock if action2 == :scissors
-      return :paper if action2 == :paper
+      winning_action = :rock if action2 == :scissors
+      winning_action = :paper if action2 == :paper
     elsif action1 == :paper
-      return :scissors if action2 == :scissors
-      return :paper if action2 == :rock
+      winning_action = :scissors if action2 == :scissors
+      winning_action = :paper if action2 == :rock
     elsif action1 == :scissors
-      return :scissors if action2 == :paper
-      return :rock if action2 == :rock
-    else
-      return nil
+      winning_action = :scissors if action2 == :paper
+      winning_action = :rock if action2 == :rock
     end
+    winning_action
   end
 end
