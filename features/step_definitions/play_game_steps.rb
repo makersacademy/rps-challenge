d22a650764c7fbe936653988ce687d3e59703745
Kanishk Walia <kanishk.walia@gmail.com>
2015-04-06 18:30:09 +0100

    Added latest version but two tests still pending and web interface not complete

--- features/step_definitions/play_game_steps.rb
+++ features/step_definitions/play_game_steps.rb
@@ -2,8 +2,8 @@ Given (/^I'm on the game page$/) do
   visit '/game'
 end
 
-Then (/^I should see Hey Kanishk! Welcome to the Rock Paper Scissors game! Pick your strategy!\?$/) do
-  body.should match "Hey Kanishk! Welcome to the Rock Paper Scissors game! Pick your strategy!"
+Then (/^I should see Hey, Kanishk! Welcome to the Rock Paper Scissors game! Pick your strategy!\?$/) do
+  body.should match "Hey, Kanishk! Welcome to the Rock Paper Scissors game! Pick your strategy!"
 end
 
 When (/^I follow "(.*?)"$/) do |button|
