2672ece7231692b3579da4df0b26263872b3e690
Mark Pokorny <iridium193@gmail.com>
2015-04-06 16:45:36 +0100

    Added complete suite of result tests to ‘round_spec.rb’.

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -7,4 +7,60 @@ describe Round do
     round = described_class.new player_hash, opponent_hash
     expect(round.winning_action).to equal :rock
   end
+
+  it 'knows ‘scissors’ is beaten by ‘rock’' do
+    player_hash = { name: 'Player', action: :scissors }
+    opponent_hash = { name: 'Opponent', action: :rock }
+    round = described_class.new player_hash, opponent_hash
+    expect(round.winning_action).to equal :rock
+  end
+
+  it 'knows ‘scissors’ beats ‘paper’' do
+    player_hash = { name: 'Player', action: :scissors }
+    opponent_hash = { name: 'Opponent', action: :paper }
+    round = described_class.new player_hash, opponent_hash
+    expect(round.winning_action).to equal :scissors
+  end
+
+  it 'knows ‘paper’ is beaten by ‘scissors’' do
+    player_hash = { name: 'Player', action: :paper }
+    opponent_hash = { name: 'Opponent', action: :scissors }
+    round = described_class.new player_hash, opponent_hash
+    expect(round.winning_action).to equal :scissors
+  end
+
+  it 'knows ‘paper’ beats ‘rock’' do
+    player_hash = { name: 'Player', action: :paper }
+    opponent_hash = { name: 'Opponent', action: :rock }
+    round = described_class.new player_hash, opponent_hash
+    expect(round.winning_action).to equal :paper
+  end
+
+  it 'knows ‘rock’ is beaten by ‘paper’' do
+    player_hash = { name: 'Player', action: :rock }
+    opponent_hash = { name: 'Opponent', action: :paper }
+    round = described_class.new player_hash, opponent_hash
+    expect(round.winning_action).to equal :paper
+  end
+
+  it 'knows ‘rock’ draws with ‘rock’' do
+    player_hash = { name: 'Player', action: :rock }
+    opponent_hash = { name: 'Opponent', action: :rock }
+    round = described_class.new player_hash, opponent_hash
+    expect(round.winning_action).to be_nil
+  end
+
+  it 'knows ‘paper’ draws with ‘paper’' do
+    player_hash = { name: 'Player', action: :paper }
+    opponent_hash = { name: 'Opponent', action: :paper }
+    round = described_class.new player_hash, opponent_hash
+    expect(round.winning_action).to be_nil
+  end
+
+  it 'knows ‘scissors’ draws with ‘scissors’' do
+    player_hash = { name: 'Player', action: :scissors }
+    opponent_hash = { name: 'Opponent', action: :scissors }
+    round = described_class.new player_hash, opponent_hash
+    expect(round.winning_action).to be_nil
+  end
 end
