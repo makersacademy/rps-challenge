40a52e1b78e05ee0cba8af8ccea3f83065b46803
Jade King <dev@jadeking.co.uk>
2015-04-07 06:17:52 +0100

    update player name test

--- spec/feature/player_can_play_rps_against_computer_spec.rb
+++ spec/feature/player_can_play_rps_against_computer_spec.rb
@@ -1,7 +1,8 @@
 feature 'player can play Rock, paper, scissors against the computer' do
-  let(:player) { Player.new("Jade") }
+  let(:player) { Player.new }
   let(:round) { Game.new }
   before(:each) do
+    player.name = "Jade"
     allow(round).to receive(:make_move).and_return("Scissors")
   end
   scenario 'player plays against the computer with winning hand' do
