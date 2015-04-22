bdd88e2a51d76b449535de83119f2b892773ac25
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-22 11:28:04 +0100

    can pick any of the three available options

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,7 +7,15 @@ describe Player do
     expect(player.name).to eq "Player1"
   end
 
-  it 'can pick an option' do
+  it 'can pick rock option' do
     expect(player.choose('rock')).to eq :rock
+  end
+
+  it 'can pick paper option' do
+    expect(player.choose('paper')).to eq :paper
+  end
+
+  it 'can pick scissors option' do
+    expect(player.choose('scissors')).to eq :scissors
   end
 end
