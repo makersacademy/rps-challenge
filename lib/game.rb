08f44d22af9056eb51766afad3932c06980f6345
Joanne <joannezanetton@gmail.com>
2015-05-15 18:54:29 +0100

    code completed

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,21 @@
 class Game
 
   def computer_choice
-    computer_choice = ["Rock", "Paper","Scissors"].sample
+    computer_choice = ['Rock', 'Paper','Scissors'].sample
   end
 
   def player_choice
-    player_choice = ["Rock", "Paper","Scissors"]
+    player_choice = ['Rock', 'Paper','Scissors']
   end
 
   def result (p1,p2)
-  if
-  p1 == p2
-  'Draw'
-
-  else
-    'Computer wins'
-end
-end
+    if
+      p1 == p2
+      'Draw'
+    elsif (p1 == 'Rock' && p2 == 'Scissors') || (p1 == 'Scissors' && p2 == 'Paper') || (p1 == 'Paper' && p2 == 'Rock')
+      'Player wins'
+    else
+      'Computer wins'
+    end
+  end
 end
