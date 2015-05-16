94980ab696694c308fa52564b81357b4fb319c7d
Daniel Johnston <macademy@danieljohnston.co.uk>
2015-05-16 16:28:12 +0100

    created backend classes and initial code for them

--- lib/rps_web.rb
+++ lib/rps_web.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require_relative 'game'
+require_relative 'player'
 
 class RPSWeb < Sinatra::Base
   # Some basic setup stuff to get Sinatra working
@@ -8,20 +10,42 @@ class RPSWeb < Sinatra::Base
 
   # Welcome screen and name entry
   get '/' do
+    # player id - basic one player version
+    unless session.has_key?('player_id')
+      session[:player_id] = :player_1
+    end
+
     erb :index
   end
 
   post '/play' do
-    # Name entry checks go first
+    # Create game if it doesn't exist yet
+    # Unimplemented: choose winning score, type of game, type of opponent
+    @@game ||= Game.new Player
+
+    # Name entry checks
     if params.has_key?('name')
       redirect '/' unless params[:name].length > 0
-      @@name = params[:name]
+      @@name ||= {}
+      @@name[session[:player_id]] = params[:name]
     elsif not(defined?(@@name))
       redirect '/'
     end
 
+    # Check if it's my turn, hold if not
+    # Waiting on setup of two player mode
+
+    # Check if a move's been submitted
+    if params.has_key?('Rock')
+      @@game.send(session[:player_id]).play :Rock
+    elsif params.has_key?('Paper')
+      @@game.send(session[:player_id]).play :Paper
+    elsif params.has_key?('Scissors')
+      @@game.send(session[:player_id]).play :Scissors
+    end
+
     # Make variables accessible from the ERB file
-    @name = @@name
+    @name = @@name[session[:player_id]]
     erb :play
 
   end
