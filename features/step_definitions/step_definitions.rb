4ce6d06c4681b914d22a23731da3797ad35da04d
Dan Bolger <bowedcymbal@gmail.com>
2015-05-17 18:25:38 +0100

    adds (incorrectly) passing tests for register_name feature

--- features/step_definitions/step_definitions.rb
+++ features/step_definitions/step_definitions.rb
@@ -9,3 +9,11 @@ end
 When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
   fill_in('name', :with => 'Dan')
 end
+
+When(/^I press "([^"]*)"$/) do |arg1|
+  click_button('play')
+end
+
+Then(/^I should be on the game page$/) do
+  visit '/game'
+end
