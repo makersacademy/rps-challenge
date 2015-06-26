6d4a294fcf49093739d622dc69f764cc73fb25b3
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-26 18:38:57 +0100

    Making sure the game cannot return a result if a player has not made a choice

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require_relative 'player'
+
 class RockPaperScissors
 
   attr_reader :player_1, :player_2
@@ -15,5 +17,47 @@ class RockPaperScissors
     player = playerClass.new
     player
   end
+
+  def won_lost_or_tied player
+    fail 'You have to choose between rock, paper or scissors' unless player.choice
+    fail 'Erm, you have no opponent :/' unless player.opponent
+    fail 'Still waiting for your opponent to decide' unless player.opponent.choice
+
+    if tied?
+      "You've tied"
+    elsif winner?
+      "You won! :)"
+    else
+      "You lost :("
+    end
+  end
+
+  private
+
+  def winner?
+    rock_paper_scissors
+  end
+
+  def tied? player
+    player.choice == player.opponent.choice
+  end
+
+  def choice_to_number player
+    choice = player.choice
+    rps_hash = { "rock" => 0, "scissors" => 2, "paper" => 1 }
+    rps_hash[choice]
+  end
+
+  def rock_paper_scissors player
+
+    opponent_choice = player.opponent.choice
+
+    your_number = choice_to_number(player)
+    opponents_number = choice_to_number(opponent_choice)
+
+    (your_number - opponents_number) % 3 == 1
+
+  end
+
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,19 @@
 require './lib/game'
 
+
 describe RockPaperScissors do
 
+  # let(:player1) { double :player , new: nil, opponent: player2}
+  # let(:player2) { double :player , new: nil, opponent: player1}
+  subject { RockPaperScissors.new Player }
+
   it { is_expected.to respond_to(:initialize_player).with(1).argument }
+
+
+  it 'cannot return the result of the game if a player has not made a choice' do
+    player = double :player
+    allow(player).to receive(:choice).and_return(nil)
+    expect{ subject.won_lost_or_tied player }.to raise_error 'You have to choose between rock, paper or scissors'
+  end
 
 end
