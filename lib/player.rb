e8810246b441cd3305de8fdce4c79a4288d50ff5
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-26 16:26:25 +0100

    test + pass for player able to select

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,7 @@
 class Player
   attr_accessor :name
+
+  def select(arg)
+
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,8 +4,8 @@ describe Player do
 
     it { is_expected.to respond_to :name }
 
-    # it 'can select rock, paper or scissors' do
-    #   expect(subject).to respond_to(:select).with(1).argument
-    # end
+    it 'can select rock, paper or scissors' do
+      expect(subject).to respond_to(:select).with(1).argument
+    end
 
 end
