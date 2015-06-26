96fbff81adb987133afeb57e79013a442aeb3d9c
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-26 16:49:00 +0100

    added validiation method and a fail statement

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,13 @@
 class Player
 
   def choose(choice)
+    fail 'Pick Rock Paper or Scissors' unless correct_input?(choice)
     @choice = choice
+  end
+
+  private
+  def correct_input?(input)
+    input == :Rock || input == :Paper || input == :Scissors
   end
 
 end
