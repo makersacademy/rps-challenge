57cf3ac7479983260fb7d70db9d27e0cdd7271d9
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-26 15:52:12 +0100

    added 2 tests for paper and scissors and passed them

--- lib/choices.rb
+++ lib/choices.rb
@@ -2,4 +2,12 @@ class Choices
   def self.rock
     :Rock
   end
+
+  def self.paper
+    :Paper
+  end
+
+  def self.scissors
+    :Scissors
+  end
 end
--- spec/choices_spec.rb
+++ spec/choices_spec.rb
@@ -7,4 +7,16 @@ describe Choices do
     end
   end
 
+  describe "Scissors" do
+    it "is a class method that returns :Scissors " do
+      expect(Choices.scissors).to eq :Scissors
+    end
+  end
+
+  describe "Paper" do
+    it "is a class method that returns :Paper " do
+      expect(Choices.paper).to eq :Paper
+    end
+  end
+
 end
