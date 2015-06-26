3846550345ecc3edeb6a863bf0fc6b00f5b42895
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-06-26 17:06:16 +0100

    test + pass for game having a player 2

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_accessor :player_1
+  attr_accessor :player_1, :player_2
 
   def initialize
     @player_1 = Player.new
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,6 +6,10 @@ describe Game do
     expect(subject).to respond_to(:player_1)
   end
 
+  it 'has a player 2' do
+    expect(subject).to respond_to(:player_2)
+  end
+
   it 'creates a player' do
     expect(subject.player_1).to be player
   end
