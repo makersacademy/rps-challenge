fe14eeb26779decf42377cbdbcd6fc3afb501356
Gerard Morera <grdmorera@gmail.com>
2015-06-27 22:27:31 +0100

    jesus christ

--- lib/comparator.rb
+++ lib/comparator.rb
@@ -1,17 +1,33 @@
 module Comparator
-
-  EXPECTATIONS = 
                 
 def self.compair comparable_player_1, comparable_player_2
+  metrics comparable_player_1, comparable_player_2
+end
 
+def self.metrics
+
+  if    comparable_player_1[0] is_a? Scissor && comparable_player_2[1] is_a? Rock
+    comparable_player_2[1]
+  elsif comparable_player_1[0] is_a? Rock    && comparable_player_2[1] is_a? Scissor
+  comparable_player_1[1]
+
+  elsif comparable_player_1[0] is_a? Scissor && comparable_player_2[1] is_a? Paper
+    comparable_player_1[1]
+  elsif comparable_player_1[0] is_a? Paper   && comparable_player_2[1] is_a? Scissor
+  comparable_player_2[1]
+
+  elsif comparable_player_1[0] is_a? Rock    && comparable_player_2[1] is_a? Paper
+    comparable_player_2[1]
+  elsif comparable_player_1[0] is_a? Paper   && comparable_player_2[1] is_a? Rock
+    comparable_player_1[1]
+
+  elsif comparable_player_1[0] is_a? Rock    && comparable_player_2[1] is_a? Rock
+  elsif comparable_player_1[0] is_a? Paper   && comparable_player_2[1] is_a? Paper
+  elsif comparable_player_1[0] is_a? Scissor && comparable_player_2[1] is_a? Scissor
+  end
 
 end
 
-
-comparable_player_1 = move.to_sym, self
-comparable_player_2 = move.to_sym, self
-
-comparable_player_1[0], comparable_player_2[0]
 
 
 
--- lib/rock.rb
+++ lib/rock.rb
@@ -1,3 +1,8 @@
 class Rock
 
+  def > another_object
+    true   if another_object is_a? Scissors 
+    false  if another_object is_a? Paper
+    :equal if another_object is_a? Rock
+
 end
