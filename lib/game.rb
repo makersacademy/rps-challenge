ef2291de9394abf5d5fd6648682de531ba87bc12
Gerard Morera <grdmorera@gmail.com>
2015-06-27 15:32:23 +0100

    class game

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,12 @@
 class Game
+
+  def initialize playerClass, comparator
+    @player_1 = initialize_player playerClass, comparator
+    @player_2 = initialize_player playerClass, comparator
+  end
+
+  def initialize_player player_class, comparator
+    playerClass.new comparator
+  end
+  
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,13 @@
+require "game"
+
 describe Game do
 
   let(:player) { double :player}
-  let(:game) subject {Game.new players: players}
+  let(:game) { subject Game.new players: players}
+  let(:playerClass) { double :playerClass, :new => :player}
 
   before do
     allow(player).to receive(:comparator)
-  end
-
   end
 
 end
