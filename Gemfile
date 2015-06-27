31f716ea386ed7ba4caf4964d3d32e44981984af
Elin <elinnet@gmail.com>
2015-06-27 21:33:39 +0100

    add of controller, views pages (index/name) and intial spec files

--- Gemfile
+++ Gemfile
@@ -6,13 +6,12 @@ gem 'sinatra'
 
 group :test do
   gem 'rspec'
-  gem 'rspec-sinatra’ 
+  gem 'rspec-sinatra'
   gem 'rubocop-rspec'
   gem 'cucumber'
   gem 'rubocop'
   gem 'coveralls', require: false
   gem 'capybara'
   gem 'byebug'
-  gem 'rack', '~> 1.6.4'
-  gem ‘launchy’
+  gem 'launchy'
 end
---
+++ lib/rps.rb
@@ -0,0 +1,23 @@
+require 'sinatra/base'
+
+
+
+class RPSchallenge < Sinatra::Base
+  set :views, proc { File.join(root, '..', 'views') }
+
+  get '/' do
+  
+    erb :index
+  
+  end
+
+  get '/name' do
+
+    erb :name
+  
+  end
+
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
