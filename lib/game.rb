af3b7a8a52fde1e862ae305c5f1a989b95ca5f7c
Elin <elinnet@gmail.com>
2015-06-27 14:25:45 +0100

    add of logic hash for paper,scissors,rock to game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -6,11 +6,47 @@ class Game
   attr_reader :comp_pick
 
   def initialize
-    @comp_pick = ""
+    # @comp_pick = ""
   end
 
   def comp_choice 
-    @comp_pick = ['rock','paper','scissors'].sample
+    # @comp_pick = 
+    ['rock','paper','scissors'].sample
   end
+
+  def play player_choice
+
+    comp = comp_choice
+
+    print "Player choice: " + "#{player_choice}\n" 
+    print "Computer choice: " + comp
+
+    comp = comp.to_sym
+    
+    if player_choice == 'paper'
+      a = {paper: "tie", rock: "wins", scissors: "lose"}
+      a[comp]
+    elsif player_choice == 'rock'
+      a = {paper: "lose", rock: "tie", scissors: "wins"}
+      b = (comp_choice).to_sym
+      a[comp]
+    else player_choice == 'scissors'
+      a = {paper: "win", rock: "lose", scissors: "tie"}
+      b = (comp_choice).to_sym
+      a[comp]
+    end
+
+  end
+
+  def logic
+
+
+
+  end
+
+
+
+
+
 
 end
--- spec/unit_tests/game_spec.rb
+++ spec/unit_tests/game_spec.rb
@@ -8,14 +8,32 @@ describe Game do
     expect(subject.comp_choice).not_to eq(nil)
   end
 
-  it 'can make a choice for the computer' do
-    expect(subject.comp_choice).not_to eq(nil)
+  it 'can recieve a player and computer choice' do
+    expect(subject).to respond_to(:play).with(1).argument
   end
 
-  # it 'stores the computer selection' do
-  #   allow(subject).to receive(:comp_choice).and_return(:paper)
-  #   expect(subject.comp_pick).to eq 'paper'
-  # end
+  it 'can run logic of game' do
+    expect(subject).to respond_to(:logic)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+  xit 'stores the computer selection' do
+    allow(subject).to receive(:comp_choice).and_return(:paper)
+    expect(subject.comp_pick).to eq("paper")
+  #why for this test, is the production code giving ""
+  end
+  
+
 end
 
 
