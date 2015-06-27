c7ca25cad4d6360623f5c98a18af45b90cd61c28
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-27 17:33:28 +0100

    Adding feature test to get users name

--- spec/features/starting_game_spec.rb
+++ spec/features/starting_game_spec.rb
@@ -1,1 +1,12 @@
 require 'spec_helper'
+
+feature 'starting a game' do
+
+  scenario 'the welcome page allows a player to set their name' do
+    visit '/'
+    fill_in('name', with: 'Nicola')
+    click_button('Begin Game')
+    expect(page).to have_content 'Nicola'
+  end
+
+end
