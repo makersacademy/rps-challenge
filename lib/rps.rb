1285788f0ef81778568e400297ca745cd2fa39ae
Elin <elinnet@gmail.com>
2015-06-27 21:52:49 +0100

    created game.erb, tests pass

--- lib/rps.rb
+++ lib/rps.rb
@@ -6,17 +6,17 @@ class RPSchallenge < Sinatra::Base
   set :views, proc { File.join(root, '..', 'views') }
 
   get '/' do
-  
     erb :index
-  
   end
 
   get '/name' do
-
     erb :name
-  
   end
 
+  get '/game' do
+    erb :game
+  end
+ 
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -7,4 +7,11 @@ feature 'Starting a new game' do
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
+
+  scenario 'It can receive a name' do
+    visit '/name'
+    fill_in 'name', with: 'Elin'
+    click_button('Start Game')
+    expect(page).to have_content 'Choose your weapon'
+  end
 end
