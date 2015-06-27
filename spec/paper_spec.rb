3f737ccd3d8c03d1d56792595053afc811b15dce
TheoLeanse <theoleanse@gmail.com>
2015-06-27 16:01:46 +0100

    adds unit test specs for Paper class

---
+++ spec/paper_spec.rb
@@ -0,0 +1,25 @@
+require 'paper'
+
+describe Paper do
+  it 'loses against scissors' do
+    expect(subject.result_against_scissors).to eq "scissors wins"
+  end
+
+  it 'wins against rock' do
+    expect(subject.result_against_rock).to eq "rock loses"
+  end
+
+  it 'draws against paper' do
+    expect(subject.result_against_paper).to eq "paper draws"
+  end
+
+  describe '#versus' do
+    let(:rock) { double :rock, result_against_paper: 'paper loses' }
+    it 'compares itself with opponent' do
+      expect(rock).to receive :result_against_paper
+      subject.versus rock
+    end
+  end
+
+
+end
