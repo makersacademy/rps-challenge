0c3ddc9572bf2c43dd3d162fb714ea6225005512
TheoLeanse <theoleanse@gmail.com>
2015-06-27 16:42:00 +0100

    implements HandGameHandler class

---
+++ lib/rps.rb
@@ -0,0 +1,13 @@
+class HandGameHandler # change to SignGame. Rock paper scissors can inherit from it?
+  def initialize *choices
+    @choices = choices
+  end
+
+  def choose player_choice#, random_choice
+    player_choice.versus random_choice
+  end
+
+  def random_choice
+    @choices.sample
+  end
+end
