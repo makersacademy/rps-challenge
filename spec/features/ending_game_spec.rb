49a8dffc89016d43e3419d5e160d1c6986fde0cc
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-28 12:24:16 +0100

    DRYing out tests

--- spec/features/ending_game_spec.rb
+++ spec/features/ending_game_spec.rb
@@ -3,34 +3,31 @@ require 'spec_helper'
 feature 'ending the game' do
 
   scenario 'the user knows if she has tied' do
-    visit '/start'
-    fill_in('name', with: 'Nicola')
-    click_button('Submit')
-    choose('rock')
+    nicola_selects_rock
     allow($game.player_2).to receive(:choice).and_return('rock')
     click_button('Choose')
     expect(page).to have_content "You've tied"
   end
 
   scenario 'the user knows if she has won' do
-    visit '/start'
-    fill_in('name', with: 'Nicola')
-    click_button('Submit')
-    choose('rock')
+    nicola_selects_rock
     allow($game.player_2).to receive(:choice).and_return('scissors')
     click_button('Choose')
     expect(page).to have_content "You won! :)"
   end
 
   scenario 'the user knows if she has lost' do
-    visit '/start'
-    fill_in('name', with: 'Nicola')
-    click_button('Submit')
-    choose('rock')
+    nicola_selects_rock
     allow($game.player_2).to receive(:choice).and_return('paper')
     click_button('Choose')
     expect(page).to have_content "You lost :("
   end
 
+  def nicola_selects_rock
+    visit '/start'
+    fill_in('name', with: 'Nicola')
+    click_button('Submit')
+    choose('rock')
+  end
 
 end
--- spec/features/making_choice_spec.rb
+++ spec/features/making_choice_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 feature 'making a choice' do
 
   scenario 'after making a choice you see your choice' do
-    visit '/start'
-    fill_in('name', with: 'Nicola')
-    click_button('Submit')
-    choose('rock')
-    click_button('Choose')
+    nicola_chooses_rock
     expect(page).to have_content 'Nicola, you chose Rock'
   end
 
   scenario 'after making a choice you see your opponent\'s name and choice' do
+    nicola_chooses_rock
+    expect(page).to have_content 'Computer, chose'
+  end
+
+  def nicola_chooses_rock
     visit '/start'
     fill_in('name', with: 'Nicola')
     click_button('Submit')
     choose('rock')
     click_button('Choose')
-    expect(page).to have_content 'Computer, chose'
   end
 
 end
