529e02a6cc7ad2bc3c7a391b068a4ee9b95d93f5
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-28 14:28:52 +0100

    adding a Rock button

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/game'
+require './lib/player'
 
 
 class RPSchallenge < Sinatra::Base
@@ -15,21 +16,25 @@ class RPSchallenge < Sinatra::Base
   end
 
   get '/challenge' do
-    $player = params[:name]
+
+    $player1 = params[:name]
+
     erb :challenge
+
   end
 
+
+  post '/challenge' do
+
+    @choice = params[:choice].to_s
+    player = Player.new
+    game = Game.new player
+    @played = game.challenge(player.play(@choice))
+
+    erb :challenge
+
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
-
-
-
-
-  # player = Player.new
-    # game = Game.new player
-    # @giocata = game.challenge(player.play('Rock'))
-
-
-     # <!--  <%= @giocata %> -->
