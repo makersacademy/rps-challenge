a9f0c7d2383b472d01ebc89c1022b90a37a9888e
Salman <salman.karim@hotmail.com>
2015-06-28 15:43:11 +0100

    stubs computer weapon

--- spec/features/1pgame_spec.rb
+++ spec/features/1pgame_spec.rb
@@ -2,30 +2,47 @@ require 'capybara/rspec'
 
 feature 'Game' do
 
-  scenario 'greets the user' do
-    visit '/'
-    expect(page).to have_content "Hello, please enter your name to start:"
+  context 'Setting up game' do
+
+    scenario 'greets the user' do
+      visit '/'
+      expect(page).to have_content "Hello, please enter your name to start:"
+    end
+
+    scenario 'allows user to enter name' do
+      visit '/'
+      expect(page).to have_css("input")
+    end
+
+    scenario 'allows user to enter name' do
+      visit '/'
+      expect(page).to have_button("Submit")
+    end
+
   end
 
-  scenario 'allows user to enter name' do
-    visit '/'
-    expect(page).to have_css("input")
+  context 'Playing game' do
+
+    before(:each) do
+      visit '/'
+      fill_in 'name', with: 'Salman'
+      click_button 'Submit'
+    end
+
+    scenario 'welcomes user and presents with weapon options' do
+      expect(page).to have_content("Welcome, Salman!")
+      expect(page).to have_content("Please select your weapon")
+      expect(page).to have_select 'weapon', with_options: ['Rock','Paper','Scissors']
+      expect(page).to have_button("Play!")
+    end
+
+    scenario 'runs game' do
+      allow($game.computer).to receive(:random_weapon).and_return(:scissors)
+      select 'Rock', :from => 'weapon'
+      click_button 'Play!'
+      expect(page).to have_content("You win!")
+    end
+
   end
-
-  scenario 'allows user to enter name' do
-    visit '/'
-    expect(page).to have_button("Submit")
-  end
-
-  scenario 'welcomes the user' do
-    visit '/'
-    fill_in 'name', with: 'Salman'
-    click_button 'Submit'
-    expect(page).to have_content("Welcome, Salman!")
-    expect(page).to have_content("Please select your weapon")
-    expect(page).to have_select 'weapon', with_options: ['Rock','Paper','Scissors']
-  end
-
-
 
 end
