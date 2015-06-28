5bd2c456409603fca7f57f1f92c60f76170de00e
Makers Laptop 86 <faisal.choura@gmail.com>
2015-06-28 22:45:14 +0100

    player can now play vs ai

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,28 @@
 class Game
 
-  def initialize
+  attr_reader :player_1
+
+  def initialize(player,ai)
     @power_hash = {Rock: :Paper,
                   Paper: :Scissors,
                   Scissors: :Rock}
     @won = false
+
+    @player_1 = player.new
+    @ai = ai.new
   end
 
   def has_winner?
     @won
   end
 
-  def play(selection_1,selection_2)
+  def play(selection_1)
+    selection_2 = @ai.choose
     if @power_hash[selection_1] == selection_2
+      @player_1.wins
       @won = true
     elsif @power_hash[selection_2] == selection_1
+      @ai.wins
       @won = true
     else
       @won = 'Tie'
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,28 @@
 require './lib/game'
 describe Game do
+    let(:player) { double :player, wins: true }
+    let(:playerClass) { double :Player, new: player }
+    let(:ai) { double :ai, choose: :Paper }
+
 
   it 'does not have winner on init' do
-    expect(subject.has_winner?).to eq false
+    aiClass = double :aiClass, new: ai
+    game = Game.new(playerClass,aiClass)
+    expect(game.has_winner?).to eq false
   end
 
   it 'can be won' do
-    subject.play(:Rock,:Paper)
-    expect(subject.has_winner?).to eq true
+    aiClass = double :aiClass, new: ai
+    game = Game.new(playerClass,aiClass)
+    game.play(:Rock)
+    expect(game.has_winner?).to eq true
+  end
+
+  it 'can be a tie' do
+    aiClass = double :aiClass, new: ai
+    game = Game.new(playerClass,aiClass)
+    game.play(:Paper)
+    expect(game.has_winner?).to eq 'Tie'
   end
 
 end
