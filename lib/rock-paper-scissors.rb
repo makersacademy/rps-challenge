874bed5fcd7e9c6f3d1a9db89f0b612cc1c542d3
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-28 10:25:36 +0100

    Allowing user to enter name and greeting them

--- lib/rock-paper-scissors.rb
+++ lib/rock-paper-scissors.rb
@@ -1,9 +1,25 @@
 require 'sinatra/base'
+require './lib/game'
+require './lib/player'
 
-
-class RockPaperScissors
+class RockPaperScissors < Sinatra::Base
 
   set :views, proc { File.join(root, '..', 'views') }
+
+  get '/' do
+    erb :index
+  end
+
+  get '/start' do
+    @user = params[:name]
+    if @user and @user.strip != ''
+      $game = Game.new Player
+      $game.player_1.name = @user
+      erb :choice
+    else
+      erb :start
+    end
+  end
 
   run! if app_file == $0
 
--- spec/features/starting_game_spec.rb
+++ spec/features/starting_game_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 feature 'starting a game' do
 
-  scenario 'the welcome page allows a player to set their name' do
+  scenario 'the welcome page allows a player to statrt a new game' do
     visit '/'
+    click_button('Start Game')
+    expect(page).to have_content 'Enter your name'
+  end
+
+  scenario 'the users enters their name before picking an option' do
+    visit '/start'
     fill_in('name', with: 'Nicola')
-    click_button('Begin Game')
+    click_button('Submit')
     expect(page).to have_content 'Nicola'
   end
 
