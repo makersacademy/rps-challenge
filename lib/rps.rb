3ccf006e827e70f2b2949257b4c3297fa5f47d91
MassimilianoMura <massimilianomura1983@gmail.com>
2015-06-28 11:39:55 +0100

    adding a test to check the input form can save the user name and give him a welcome in the next page

--- lib/rps.rb
+++ lib/rps.rb
@@ -7,15 +7,25 @@ class RPSchallenge < Sinatra::Base
   set :views, proc { File.join(root, '..', 'views') }
 
   get '/' do
-    player = Player.new
-    game = Game.new player
-    @giocata = game.challenge(player.play('Rock'))
-
-
-    erb :index
+    $player = params[:name]
+    if $player == ''
+      erb :challenge
+    else
+      erb :index
+    end
   end
 
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+
+
+
+  # player = Player.new
+    # game = Game.new player
+    # @giocata = game.challenge(player.play('Rock'))
+
+
+     # <!--  <%= @giocata %> -->
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 feature 'Starting a new game' do
-  scenario '' do
+
+  scenario "asking for the user's name" do
+  visit '/'
+  expect(page).to have_content "What's your name?"
+end
+
+  scenario "user can login with his name" do
+  visit '/'
+  fill_in 'name', with: 'Massi'
+  click_button ('Submit')
+  expect(page).to have_content "Welcome Massi, have a look to the rules and let's star playing!"
 end
 
 
