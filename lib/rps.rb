13ef9006206f8464dd0d9929f41317c92568eef0
Robert Morgan <robbie.morgan@gmail.com>
2015-06-28 17:14:03 +0100

    passing first feature test

--- lib/rps.rb
+++ lib/rps.rb
@@ -8,4 +8,10 @@ class RPS < Sinatra::Base
   # start the server if ruby file executed directly
   run! if app_file == $0
 
+
+get '/' do 
+	erb :index
+
+end
+
 end
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 feature 'Starting a new game' do
   scenario 'I am asked to enter my name' do
     visit '/'
-    click_link 'New Game'
-    expect(page).to have_content "What is your name?"
+    expect(page).to have_content "Please enter your name:"
   end
 
 end
