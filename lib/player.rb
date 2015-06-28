89b2d34b1042ce58025f625fcf177ae12e3ca15d
Zeina Dajani <dajani.zeina@gmail.com>
2015-06-28 16:25:39 +0100

    added player class and spec test

---
+++ lib/player.rb
@@ -0,0 +1,5 @@
+class Player
+
+  def choose rockpaperscissors
+  end
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,8 @@
+require 'player'
+
+describe Player do
+
+  it {is_expected.to respond_to(:choose).with(1).argument }
+
+
+end
