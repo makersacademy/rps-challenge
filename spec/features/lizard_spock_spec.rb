a5b6f0453bd9ec0d0d3f9cc4838f8cc863695767
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-28 21:52:27 +0100

    Testing new advanced mode has the correct logic

--- spec/features/lizard_spock_spec.rb
+++ spec/features/lizard_spock_spec.rb
@@ -9,10 +9,34 @@ feature 'extending the game to Rock Paper Scissors Lizard Spock' do
 
   scenario 'Spock loses to Lizard' do
     nicola_in_single_player_advanced_mode
-    choose('lizard')
+    choose('lizard', :match => :first)
     allow($game.player_2).to receive(:choice).and_return('spock')
     click_button('Choose')
     expect(page).to have_content 'Lizard poisons Spock'
+  end
+
+  scenario 'Spock beats Scissors' do
+    nicola_in_single_player_advanced_mode
+    choose('spock', :match => :first)
+    allow($game.player_2).to receive(:choice).and_return('scissors')
+    click_button('Choose')
+    expect(page).to have_content 'Spock smashes Scissors'
+  end
+
+  scenario 'Spock loses to Rock' do
+    nicola_in_single_player_advanced_mode
+    choose('spock', :match => :first)
+    allow($game.player_2).to receive(:choice).and_return('rock')
+    click_button('Choose')
+    expect(page).to have_content 'Spock vapourizes Rock'
+  end
+
+  scenario 'Lizard beats Paper' do
+    nicola_in_single_player_advanced_mode
+    choose('lizard', :match => :first)
+    allow($game.player_2).to receive(:choice).and_return('paper')
+    click_button('Choose')
+    expect(page).to have_content 'You won'
   end
 
   def nicola_in_single_player_advanced_mode
