6a17504c1910a6bbb695b53ec3eb5fc17898b869
Makers Laptop 88 <makerslaptop88@Makerss-MacBook-Pro.local>
2015-06-28 15:07:55 +0100

    Player can enter name

--- Gemfile
+++ Gemfile
@@ -7,9 +7,11 @@ gem 'sinatra'
 group :test do
   gem 'rspec'
   gem 'rubocop-rspec'
-  gem 'cucumber'
   gem 'rubocop'
   gem 'coveralls', require: false
   gem 'capybara'
   gem 'byebug'
+  gem 'rspec-sinatra'
+  gem 'shotgun'
+  gem 'selenium-webdriver'
 end
---
+++ spec/features/play_game_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'Starting a new game' do
+  scenario 'user should be able to enter their name' do
+    visit '/'
+    expect(page).to have_content "Please enter your name"
+  end
+
+  scenario 'user can choose between rock, paper or scissors' do
+    visit '/playing'
+    expect(page).to have_content "Make you're choice!"
+  end
+end
