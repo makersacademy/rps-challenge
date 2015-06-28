554bbb26a16c40f2c7203d6a83f3d57800178566
Nicola Hughes <nicolarhughes@gmail.com>
2015-06-28 10:35:57 +0100

    Refactoring to use a post request for getting the username

--- lib/rock-paper-scissors.rb
+++ lib/rock-paper-scissors.rb
@@ -11,13 +11,18 @@ class RockPaperScissors < Sinatra::Base
   end
 
   get '/start' do
+    erb :start
+  end
+
+  post '/start' do
     @user = params[:name]
     if @user and @user.strip != ''
       $game = Game.new Player
       $game.player_1.name = @user
+      @options = Game::OPTIONS
       erb :choice
     else
-      erb :start
+      redirect '/start'
     end
   end
 
