bf6dfd81ff79b07e15b4909c95e47a621b161a56
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-02 19:09:18 +0100

    Adding comment to logic which decides on teh winner

--- lib/game.rb
+++ lib/game.rb
@@ -74,8 +74,14 @@ class Game
 
     your_number = choice_to_number(player)
     opponents_number = choice_to_number(player.opponent)
-    # Add comment
-    (your_number - opponents_number) % @@options.count > 0 and (your_number - opponents_number) % @@options.count <= @@options.count / 2
+    # If all options are ordered such that, when wrapped circularly,
+    # each option beats half the remaining elements counter clockwise and loses
+    # to the other half clockwise then the logic states that
+    # (your choice - opponents choice) modulus the number of choices is greater
+    # than 0 and less than or equal to the number of choices divided by two.
+    # Thus, if the last line of the method rock_paper_scissors is true then you have won.
+    (your_number - opponents_number) % @@options.count > 0 &&
+    (your_number - opponents_number) % @@options.count <= @@options.count / 2
 
   end
 
