ddec95933f5ef359265f11ff8d46fd928a671f74
zlahham <zlahham@gmail.com>
2015-08-07 18:51:30 +0100

    Setting up the project folder

---
+++ lib/rock_paper_scissors_web.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+
+class RockPaperScissors < Sinatra::Base
+  get '/' do
+    'Hello RockPaperScissors!'
+  end
+
+
+
+  
+
+  set :views, Proc.new { File.join(root, "..", "views") }
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
