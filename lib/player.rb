44e97bcc2b046bc28f29df3be2418ed710fc59ed
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-08-07 21:06:59 +0100

    Added Game class and gesture to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,15 @@
 class Player
+
+  DEFAULT_GESTURE_OPTIONS = [:rock, :paper, :scissors]
+
+  attr_reader :gesture
+
+  def initialize gesture_options = DEFAULT_GESTURE_OPTIONS
+    @gesture_options = gesture_options
+  end
+
+  def throw_gesture *choice
+    choice[0] ? @gesture = choice[0] : @gesture = @gesture_options.sample
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,4 +2,13 @@ require 'player'
 
 describe Player do
 
+  it "should respond to #throw_gesture" do
+    expect(subject).to respond_to(:throw_gesture)
+  end
+
+  it "should have a @gesture after #throw_gesture" do
+    subject.throw_gesture
+    expect(subject).to respond_to(:gesture)
+  end
+
 end
