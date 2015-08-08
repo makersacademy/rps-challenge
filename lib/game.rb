31a890c75f89cfd75770ecef1460df8b3f1d14e0
Sivan Patel <sivan.patel@googlemail.com>
2015-08-08 17:41:37 +0100

    Game working with tests

--- lib/game.rb
+++ lib/game.rb
@@ -10,8 +10,16 @@ class Game
   end
 
   def check_win
-    raise "Only one player has made their choice" if (player_1.choice == nil || player_2.choice == nil)
-    raise "Draw!" if (player_1.choice == player_2.choice)
+    raise "Only one player has made their selection" if (player_1.selection == nil || player_2.selection == nil)
+    raise "Draw!" if (player_1.selection == player_2.selection)
+    case player_1.selection
+    when :rock
+      player_2.selection == :paper ? player_2 : player_1
+    when :paper
+      player_2.selection == :scissors ? player_2 : player_1
+    when :scissors
+      player_2.selection == :rock ? player_2 : player_1
+    end
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,10 +2,10 @@ require 'game'
 
 describe Game do
 
-  let(:player) { double (:player), choice: nil}
-  let(:rock_player) { double (:player), choice: :rock }
-  let(:paper_player) { double (:player), choice: :paper }
-  let(:scissors_player) { double (:player), choice: :scissors }
+  let(:player) { double (:player), selection: nil}
+  let(:rock_player) { double (:player), selection: :rock }
+  let(:paper_player) { double (:player), selection: :paper }
+  let(:scissors_player) { double (:player), selection: :scissors }
 
   it 'can check to see if there is a winner' do
     subject = Game.new player, player
@@ -14,12 +14,27 @@ describe Game do
 
   it 'raises an error if only one player has made a choice' do
     subject = Game.new player, rock_player
-    expect{ subject.check_win }.to raise_error("Only one player has made their choice")
+    expect{ subject.check_win }.to raise_error("Only one player has made their selection")
   end
 
   it 'lets you know when there has been a draw' do
     subject = Game.new rock_player, rock_player
-    expect{ subject.check_win}.to raise_error("Draw!")
+    expect{ subject.check_win }.to raise_error("Draw!")
+  end
+
+  it 'paper beats rock' do
+  subject = Game.new rock_player, paper_player
+  expect(subject.check_win).to be(paper_player)
+  end
+
+  it 'rock beats scissors' do
+    subject = Game.new rock_player, scissors_player
+    expect(subject.check_win).to be(rock_player)
+  end
+
+  it 'scissors beats paper' do
+    subject = Game.new paper_player, scissors_player
+    expect(subject.check_win).to be(scissors_player)
   end
 
 end
