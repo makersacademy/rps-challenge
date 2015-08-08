ad21a17cc5ee9fe6d38ec933063f68984c5deb78
UpstatePedro <piers.m.holden@gmail.com>
2015-08-08 14:10:29 +0100

    Tests & Code for Game & Player classes. Game in irb now possible

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
-require 'player'
+require_relative './player'
 
-class Game do
+class Game
+
+  attr_reader :player1, :player2
 
   def initialize (player1, player2)
     @player1 = player1
@@ -8,7 +10,16 @@ class Game do
   end
 
   def check_winner
-
-
+    raise "Not all players have made their selection!" if (player1.choice == nil || player2.choice == nil)
+    return "It's a draw!!" if player1.choice == player2.choice
+    case player1.choice
+    when :Rock
+      player2.choice == :Paper ? player2 : player1
+    when :Paper
+      player2.choice == :Scissors ? player2 : player1
+    when :Scissors
+      player2.choice == :Rock ? player2 : player1
+    end
   end
+
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,18 @@
+class Player
 
-class Player do
-  attr_accessor :name, :choice
+  VALID_CHOICES = [:Rock, :Paper, :Scissors]
+
+  attr_reader :choice
+  attr_accessor :name
 
   def initialize(name)
     @name = name
     @choice = nil
+  end
+
+  def set_choice selection
+    selection_sym = selection.capitalize.to_sym
+    return 'That is not a valid choice' if not VALID_CHOICES.any? {|vc| vc == selection_sym}
+    @choice = selection_sym
   end
 end
