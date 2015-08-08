b129d06bde54e494c8a2c00e745bbeab0b09a11c
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-08 12:55:32 +0100

    refactored working war mechanism

--- lib/war.rb
+++ lib/war.rb
@@ -1,5 +1,53 @@
 class War
 
-  def choose_weapon(weapon)
+  def initialize
+    @weapon_values = {
+      :rock => 1,
+      :paper => 2,
+      :scissors => 3,
+      :spock => 4,
+      :lizard => 5
+      }
+  end
+
+  def weapon_to_number(weapon)
+    @weapon_values[weapon]
+  end
+
+  def weapons_available(number)
+    @weapons_available = number
+  end
+
+  def difference(player_1, player_2)
+    weapon_to_number(player_1) - weapon_to_number(player_2)
+  end
+
+  def decide_winner(player_1, player_2)
+    difference(player_1, player_2) == 0 ? :draw : non_draw(player_1, player_2)
+  end
+
+  def non_draw(player_1, player_2)
+    weapon_modulo(player_1, player_2) % 2 == 0 ? :player_2_win : :player_1_win
+  end
+
+  def weapon_modulo(player_1, player_2)
+    difference(player_1, player_2) % @weapons_available
   end
 end
+
+
+
+
+
+=begin
+
+The majority of such proposed generalizations are isomorphic to a simple game of
+modulo arithmetic, where half the differences are wins for player one. For
+instance, rock-paper-scissors-Spock-lizard (note the different order of the last
+two moves) may be modeled as a game in which each player picks a number from one
+to five. Subtract the number chosen by player two from the number chosen by
+player one, and then take the remainder modulo 5 of the result. Player one is
+the victor if the difference is one or three, and player two is the victor if
+the difference is two or four. If the difference is zero, the game is a tie.
+
+=end
--- spec/war_spec.rb
+++ spec/war_spec.rb
@@ -1,7 +1,26 @@
 require "war"
 
 describe War do
-  it "can choose a weapon" do
-    expect(subject).to respond_to(:choose_weapon).with(1).argument
+
+  it "can decide a winner" do
+    expect(subject).to respond_to(:decide_winner).with(2).arguments
+  end
+
+  it "chooses :paper as a winner over :rock" do
+    subject.weapons_available(5)
+    expect(subject.decide_winner(:rock, :paper)).to eq :player_2_win
+  end
+
+  it "shows a draw when drawn" do
+    subject.weapons_available(3)
+    expect(subject.decide_winner(:paper,:paper)).to eq :draw
+  end
+
+  it "converts weapons to numbers" do
+    expect(subject).to respond_to(:weapon_to_number).with(1).argument
+  end
+
+  it "converts :rock to 1" do
+    expect(subject.weapon_to_number(:rock)).to eq 1
   end
 end
