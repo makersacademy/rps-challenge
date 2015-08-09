713d28bdfe928e336524e498d36f231e2774d754
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-09 23:51:29 +0100

    refactor

--- spec/features/game_page_spec.rb
+++ spec/features/game_page_spec.rb
@@ -13,7 +13,6 @@ feature 'Game Page' do
 
   scenario "Weapons options match AVAILABLE_WEAPONS constant's contents" do
     # Couldn't work out how to stub the constant
-
     RandomWeaponGenerator::AVAILABLE_WEAPONS.each do |weapon|
       expect(page).to have_field("weapon", with: weapon.to_s)
     end
@@ -28,31 +27,32 @@ feature 'Game Page' do
   end
 
   context "if user has selected a weapon and clicked 'GO!'" do
-    scenario "user is informed of opponent's choice" do
-      allow_any_instance_of(Game).to receive(:random_weapon).and_return :paper
+    def choose_rock_and_go
       choose 'rock'
       click_on 'GO!'
+    end
+
+    scenario "user is informed of opponent's choice" do
+      allow_any_instance_of(Game).to receive(:random_weapon).and_return :paper
+      choose_rock_and_go
       expect(page).to have_content "Opponent chose paper..."
     end
 
     scenario 'user is told they have won if they win the game' do
       allow_any_instance_of(Game).to receive(:play).with(:rock).and_return :win
-      choose 'rock'
-      click_on 'GO!'
+      choose_rock_and_go
       expect(page).to have_content "YOU WON!"
     end
 
     scenario 'user is told they have lost if they lose the game' do
       allow_any_instance_of(Game).to receive(:play).with(:rock).and_return :loss
-      choose 'rock'
-      click_on 'GO!'
+      choose_rock_and_go
       expect(page).to have_content "YOU LOST!"
     end
 
     scenario 'user is told they have drawn if they draw the game' do
       allow_any_instance_of(Game).to receive(:play).with(:rock).and_return :draw
-      choose 'rock'
-      click_on 'GO!'
+      choose_rock_and_go
       expect(page).to have_content "Draw! Try again!"
     end
   end
