503adac2b846db2e3e9175787277a638b4853dfb
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-09 15:36:53 +0100

    create a one player game of rock/paper/scissors playable in irb

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,34 @@
+require_relative 'random_weapon_generator'
+
 class Game
   attr_reader :name
 
   def initialize(name="Player 1")
     @name = name
+  end
+
+  include RandomWeaponGenerator
+
+  def play weapon
+    opponent_weapon = random_weapon
+
+    game_outcome(weapon, opponent_weapon)
+  end
+
+
+  private
+
+  def game_outcome player_weapon, opponent_weapon
+    return :win   if player_weapon == :rock and opponent_weapon == :scissors
+    return :loss  if player_weapon == :rock and opponent_weapon == :paper
+    return :draw  if player_weapon == :rock and opponent_weapon == :rock
+
+    return :win   if player_weapon == :paper and opponent_weapon == :rock
+    return :loss  if player_weapon == :paper and opponent_weapon == :scissors
+    return :draw  if player_weapon == :paper and opponent_weapon == :paper
+
+    return :win   if player_weapon == :scissors and opponent_weapon == :paper
+    return :loss  if player_weapon == :scissors and opponent_weapon == :rock
+    return :draw  if player_weapon == :scissors and opponent_weapon == :scissors
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,4 +5,63 @@ describe Game do
     expect(subject.name).to eq "Player 1"
   end
 
+  describe '#play' do
+    it 'returns :win or :loss' do
+      possible_outcomes = [:win, :loss, :draw]
+      expect(possible_outcomes).to include subject.play :rock
+    end
+
+    context 'when player chooses :rock' do
+      it 'returns :win when computer chooses :scissors' do
+        allow(subject).to receive(:random_weapon).and_return :scissors
+        expect(subject.play :rock).to eq :win
+      end
+
+      it 'returns :loss when computer chooses :paper' do
+        allow(subject).to receive(:random_weapon).and_return :paper
+        expect(subject.play :rock).to eq :loss
+      end
+
+      it 'returns :draw when computer chooses :rock' do
+        allow(subject).to receive(:random_weapon).and_return :rock
+        expect(subject.play :rock).to eq :draw
+      end
+    end
+
+    context 'when player chooses :paper' do
+      it 'returns :win when computer chooses :rock' do
+        allow(subject).to receive(:random_weapon).and_return :rock
+        expect(subject.play :paper).to eq :win
+      end
+
+      it 'returns :loss when computer chooses :scissors' do
+        allow(subject).to receive(:random_weapon).and_return :scissors
+        expect(subject.play :paper).to eq :loss
+      end
+      
+      it 'returns :draw when computer chooses :paper' do
+        allow(subject).to receive(:random_weapon).and_return :paper
+        expect(subject.play :paper).to eq :draw
+      end
+    end
+
+    context 'when player chooses :scissors' do
+      it 'returns :win when computer chooses :paper' do
+        allow(subject).to receive(:random_weapon).and_return :paper
+        expect(subject.play :scissors).to eq :win
+      end
+
+      it 'returns :loss when computer chooses :rock' do
+        allow(subject).to receive(:random_weapon).and_return :rock
+        expect(subject.play :scissors).to eq :loss
+      end
+
+      it 'returns :draw when computer chooses :scissors' do
+        allow(subject).to receive(:random_weapon).and_return :scissors
+        expect(subject.play :scissors).to eq :draw
+      end
+    end
+    
+  end
+
 end
