4c92e14291c6f111b5fb50172c81ecc0f677f7b0
Balint Nemes <squarebe03@gmail.com>
2015-08-09 09:28:54 +0100

    player able to enter its name

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+
+	set :views, proc {File.join(root, '..', 'views') }
+
   get '/' do
-    'Hello RockPaperScissors!'
+    erb :index
+  end
+
+  get '/name_set' do
+  	erb :enter_name
+  end
+
+  get '/play' do
+  	@name = params[:name]
+  	erb :game
   end
 
   # start the server if ruby file executed directly
--- spec/features/playing_the_game_spec.rb
+++ spec/features/playing_the_game_spec.rb
@@ -5,4 +5,23 @@ feature 'Starting the game' do
 		visit '/'
 		expect(page).to have_content 'Hello'
 	end
+
+	scenario 'finds link to the game' do
+		visit '/'
+		expect(page).to have_content 'Wanna play?'
+	end
+
+	scenario 'i need to enter my name' do
+		visit '/'
+		click_link 'Wanna play?'
+		expect(page).to have_content 'Please enter your name:'
+	end
+
+	scenario 'accepting a players name' do
+		visit '/name_set'
+		dummy_name = 'Joe'
+		fill_in "name", with: dummy_name
+		click_button 'Submit'
+		expect(page).to have_content "Hello #{dummy_name}"
+	end
 end
