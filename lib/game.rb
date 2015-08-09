86941e9b72748052a3a0ec13ae163a611e37193f
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-09 16:12:29 +0100

    change 'add' to '&&' because Hound told me to

--- lib/game.rb
+++ lib/game.rb
@@ -13,20 +13,19 @@ class Game
     game_outcome(weapon, random_weapon)
   end
 
-
   private
 
   def game_outcome player_weapon, opponent_weapon
-    return :win   if player_weapon == :rock and opponent_weapon == :scissors
-    return :loss  if player_weapon == :rock and opponent_weapon == :paper
-    return :draw  if player_weapon == :rock and opponent_weapon == :rock
+    return :win   if player_weapon == :rock && opponent_weapon == :scissors
+    return :loss  if player_weapon == :rock && opponent_weapon == :paper
+    return :draw  if player_weapon == :rock && opponent_weapon == :rock
 
-    return :win   if player_weapon == :paper and opponent_weapon == :rock
-    return :loss  if player_weapon == :paper and opponent_weapon == :scissors
-    return :draw  if player_weapon == :paper and opponent_weapon == :paper
+    return :win   if player_weapon == :paper && opponent_weapon == :rock
+    return :loss  if player_weapon == :paper && opponent_weapon == :scissors
+    return :draw  if player_weapon == :paper && opponent_weapon == :paper
 
-    return :win   if player_weapon == :scissors and opponent_weapon == :paper
-    return :loss  if player_weapon == :scissors and opponent_weapon == :rock
-    return :draw  if player_weapon == :scissors and opponent_weapon == :scissors
+    return :win   if player_weapon == :scissors && opponent_weapon == :paper
+    return :loss  if player_weapon == :scissors && opponent_weapon == :rock
+    return :draw  if player_weapon == :scissors && opponent_weapon == :scissors
   end
 end
