e2468883a0f3ff5c2a8ec129affd262c5464939b
UpstatePedro <piers.m.holden@gmail.com>
2015-08-09 10:21:32 +0100

    Added update-game route

--- app/server.rb
+++ app/server.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
+require_relative '../lib/game'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
+  # tell sinatra where we're storing the views & public files
+  set :views, proc { File.join(root, '..', 'views') }
+  set :public_dir, proc { File.join(root, '..', 'public_dir') }
+
   get '/' do
-    'Hello RockPaperScissors!'
+    erb :index
+  end
+
+  post '/update-game' do
+    
   end
 
   # start the server if ruby file executed directly
