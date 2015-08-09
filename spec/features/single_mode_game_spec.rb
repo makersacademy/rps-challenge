492b9d8ca535fdbbb0fc3c6155ee16d07ab5c0de
giuseppe de santis <giusdesan@gmail.com>
2015-08-09 11:29:07 +0100

    added feature spec files for single and multiplayer modes

--- spec/features/single_mode_game_spec.rb
+++ spec/features/single_mode_game_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Starting a new game' do
-  scenario 'The user is asked to enter a name' do
+  scenario 'The user asked to enter a name' do
     visit '/'
     click_link "New Game"
     expect(page).to have_content "Hello awesome marketeer, what's your name?"
