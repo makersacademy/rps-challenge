b3cf7c2a64b0f8295955f4f3af8be15c6757f967
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-09 16:16:16 +0100

    refactor

--- lib/game.rb
+++ lib/game.rb
@@ -18,14 +18,13 @@ class Game
   def game_outcome player_weapon, opponent_weapon
     return :win   if player_weapon == :rock && opponent_weapon == :scissors
     return :loss  if player_weapon == :rock && opponent_weapon == :paper
-    return :draw  if player_weapon == :rock && opponent_weapon == :rock
 
     return :win   if player_weapon == :paper && opponent_weapon == :rock
     return :loss  if player_weapon == :paper && opponent_weapon == :scissors
-    return :draw  if player_weapon == :paper && opponent_weapon == :paper
 
     return :win   if player_weapon == :scissors && opponent_weapon == :paper
     return :loss  if player_weapon == :scissors && opponent_weapon == :rock
-    return :draw  if player_weapon == :scissors && opponent_weapon == :scissors
+
+    :draw if player_weapon == opponent_weapon
   end
 end
