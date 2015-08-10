c4c2c7d7ef71bf14ea5a93155b75d8773ff40512
Antonio Voce <antonio.v0c3@gmail.com>
2015-08-10 21:49:06 +0100

    recommit to pass CI with Master

--- lib/rps.rb
+++ lib/rps.rb
@@ -21,34 +21,9 @@ class RockPaperScissors < Sinatra::Base
     @my_move = params[:option]
     @computer_move = Computer.new.move
     @result = Game.new.result(@my_move, @computer_move)
-    #session[:option] = @my_move
     p params
     erb :game
   end
-
-  # get '/multi' do
-  #   if $player1
-  #     $player2 = params[:playername]
-  #     session[:player2] = $player2
-  #     session[:player1] = $player1
-  #     puts "you're player 2"
-  #   else
-  #     $player1 = params[:playername]
-  #     session[:player1] = $player1
-  #     puts "you're player 1"
-  #   end
-  #   p session
-  #   erb :multi
-  # end
-  #
-  # post '/multi' do
-  #   $player_1_move = params[:option]
-  #   session[:player_1_move] = $player_1_move  #need to make sure I understand this, might not be correct
-  #   $player_2_move = params[:option]
-  #   session[:player_2_move] = $player_2_move
-  #   $result = Game.new.result($player_1_move, $player_2_move)
-  # end
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
