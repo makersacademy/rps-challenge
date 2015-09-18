72872920ea253fe4bb507eb5adb2386703492278
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-18 23:22:56 +0100

    enabled sessions, added enter name feature

--- rps_controller.rb
+++ rps_controller.rb
@@ -2,10 +2,23 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
+  post '/' do
+    session[:name] = params[:player_name]
+    redirect '/play_rps'
+  end
+
+  get '/play_rps' do
+    @name = session[:name]
+    erb :play_rps
+  end
+
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
+
 end
--- spec/features/starting_the_game_spec.rb
+++ spec/features/starting_the_game_spec.rb
@@ -1,10 +1,17 @@
-  require 'spec_helper'
+require 'spec_helper'
 
-  feature 'Starting the game.' do
+feature 'Starting the game.' do
 
-    scenario 'displays welcome message' do
-      visit '/'
-      expect(page).to have_content 'Welcome, brave marketeer.'
-    end
-
+  scenario 'displays welcome message' do
+    visit '/'
+    expect(page).to have_content 'Welcome, brave marketeer.'
   end
+
+  scenario 'lets user enter name and remembers it' do
+    visit '/'
+    fill_in 'player_name', :with => 'Steerpike'
+    click_button 'GO'
+    expect(page).to have_content 'Choose your weapon, Steerpike:'
+  end
+
+end
