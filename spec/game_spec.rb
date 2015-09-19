a1e1e93c45c2d2b5782e05ff617acead13f24895
chn.challenger <chn.challenger@gmail.com>
2015-09-19 10:08:32 +0100

    1st test for game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+require 'game'
+
+describe Game do
+
+  describe "play" do
+    it 'Player wins when player choose rock and computer choose scissors' do
+      game = Game.new
+      computer_player = double(:fake_computer_player, choice:'scissors'.to_sym)
+      # allow(computer_player).to receive(:choice).and_return(:rock)
+      computer_choice = computer_player.choice
+      player_choice = :rock
+      expect(game.play(player_choice,computer_choice)).to eq('Player wins!')
+    end
+  end
+
+end
