d58cc0a10ac8664c483966dffe100da0bf2f21de
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-19 21:56:42 +0100

    welcome page and name input form added

--- lib/rps-web.rb
+++ lib/rps-web.rb
@@ -5,6 +5,8 @@ require_relative 'comp_player'
 
 class RPSWeb < Sinatra::Base
 
+  enable :sessions
+
   set :views, proc { File.join(root, '..', 'views')}
 
   get '/' do
@@ -12,9 +14,16 @@ class RPSWeb < Sinatra::Base
   end
 
   get '/name' do
+    @name = params[:name]
+    session[:name] = params[:name]
     erb :new_game
   end
 
+  get '/choose_shape' do
+    @name = session[:name]
+    erb :choose_shape
+  end
+
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
 end
--- spec/features/starting_rps_spec.rb
+++ spec/features/starting_rps_spec.rb
@@ -8,4 +8,29 @@ feature 'Staring a new rps game' do
     expect(page).to have_content "What's your name?"
   end
 
+  scenario 'Recieves name' do
+    visit '/'
+    click_link 'New Game'
+    fill_in('name', with: 'Harry')
+    click_button 'Submit'
+    expect(page).to have_content "Please press 'start' to begin Rock, Paper, Scissors:"
+  end
+
+  scenario 'Does not recieve name' do
+    visit '/'
+    click_link 'New Game'
+    fill_in('name', with: '')
+    click_button 'Submit'
+    expect(page).to have_content 'Please enter your name'
+  end
+
+  scenario 'Goes to choose shape when start button is pressed' do
+    visit '/'
+    click_link 'New Game'
+    fill_in('name', :with => 'Harry')
+    click_button 'Submit'
+    click_button 'Start'
+    expect(page).to have_content "Choose your shape Harry"
+  end
+
 end
