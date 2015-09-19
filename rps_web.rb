30d6c4b73935fb5604056f9c908680473bcd9439
Proskurina <yproskurina@yahoo.com>
2015-09-19 12:21:43 +0100

    Can register name, can see a rps choice

--- rps_web.rb
+++ rps_web.rb
@@ -2,8 +2,24 @@ require 'sinatra/base'
 
 class Rps_challenge < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
+  end
+
+  post '/' do
+    session[:name] = params[:name]
+    redirect '/new_game'
+  end
+
+  get '/new_game' do
+    @name = session[:name]
+    erb :new_game
+  end
+
+  post '/result' do
+
   end
 
   # start the server if ruby file executed directly
--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -10,29 +10,20 @@ feature 'Play the game' do
 
   scenario 'I am asked to enter my name' do
     visit '/'
-    expect(page).to have_content "Please enter your name"
+    expect(page).to have_content "Please enter your name:"
   end
 
   scenario 'after I entered my name' do
     visit '/'
     fill_in('name', :with => 'John')
     click_button('submit')
-    expect(page).to have_content "Hello, John! Make your choice!"
+    expect(page).to have_content "Hello, John! Please, make your choice!"
   end
 
-  # scenario 'takes you to a page where you can see your board' do
-  #   visit '/new_player'
-  #   click_button('See your board')
-  #   expect(page).to have_content "This is your board"
-  # end
-
-  # scenario 'shows the board' do
-  #   visit '/place_ships'
-  #   expect(page).to have_xpath(".//tr", :count => 10)
-  # end
-
-
-
+  scenario 'I can make my rps choise' do
+    visit '/new_game'
+    expect(page).to have_select('rps')
+  end
 
 
 end
