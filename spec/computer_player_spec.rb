7a49f023b4e11d0fb33d5eaa1deff52dd257e3f3
chn.challenger <chn.challenger@gmail.com>
2015-09-19 09:44:04 +0100

    Written tests for computer player class

---
+++ spec/computer_player_spec.rb
@@ -0,0 +1,19 @@
+require '../lib/computer_player'
+
+describe ComputerPlayer do
+
+  describe '#choice' do
+    it 'sometimes plays rock' do
+
+    end
+
+    it 'sometimes plays scissors' do
+
+    end
+
+    it 'sometimes plays paper' do
+
+    end
+  end
+
+end
