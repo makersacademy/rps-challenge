36a5ffb3891119a55b7927f285d1c5d014083a5d
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-19 17:37:54 +0100

    Game logic fine. Now tests failing

--- lib/game.rb
+++ lib/game.rb
@@ -1,31 +1,43 @@
 class Game
 
-  attr_accessor :choice
-
-
   def user_choice(choice)
     @choice = choice
   end
 
   def comp_choice
     @comp_choice = ["rock", "paper","scissors"].sample
+    @comp_decision = @comp_choice
+    result
   end
 
   def draw?
-    return true if comp_choice == @choice
+    return true if @comp_decision == @choice
   end
 
   def winner?
-   return true if @choice == "scissors" && comp_choice == "paper"
-   return true if @choice == "paper" && comp_choice == "rock"
-   return true if  @choice == "rock" && comp_choice == "scissors"
+   return true if @choice == "scissors" && @comp_decision == "paper"
+   return true if @choice == "paper" && @comp_decision == "rock"
+   return true if  @choice == "rock" && @comp_decision == "scissors"
   end
 
   def lose?
-   return true if @choice == "scissors" && comp_choice == "rock"
-   return true if @choice == "paper" && comp_choice == "scissors"
-   return true if  @choice == "rock" && comp_choice == "paper"
+   return true if @choice == "scissors" && @comp_decision == "rock"
+   return true if @choice == "paper" && @comp_decision == "scissors"
+   return true if  @choice == "rock" && @comp_decision == "paper"
   end
+
+  def result
+   if winner? == true
+     "CONGRATULATIONS! YOU BEAT A MACHINE"
+   elsif draw? == true
+    "You drew"
+   elsif lose? == true
+    "YOU LOST. THIS DUMB, ILLITERATE MACHINE BEAT YOU!"
+   end
+  end
+
+
+
 
 
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,10 @@ require 'game'
 
 describe Game do
 
+  before do
+    allow(subject).to receive(:comp_choice).and_return("paper")
+  end
+
   it 'should be able to choose one of three items' do
     expect(subject).to respond_to(:user_choice).with(1).arguments
   end
@@ -9,18 +13,17 @@ describe Game do
   # xit 'should return "scissors" when comp_choice is run' do
   #   expect(Game.new).to receive(:comp_choice).and_return(:scissors)
   # end
+
   describe 'draw?' do
     it 'should draw if comp_choice and user_choice are equal' do
       subject.user_choice("paper")
-      allow(subject).to receive(:comp_choice).and_return("paper")
       expect(subject.draw?).to eql(true)
     end
   end
 
   describe 'winner?' do
-    it 'should win if user_choice is rock and comp_choice is scissors' do
-      subject.user_choice("rock")
-      allow(subject).to receive(:comp_choice).and_return("scissors")
+    it 'should win if user_choice is scissors and comp_choice is paper' do
+      subject.user_choice("scissors")
       expect(subject.winner?).to eql(true)
     end
   end
@@ -28,11 +31,13 @@ describe Game do
   describe 'lose?' do
     it 'should lose if player chooses rock and comp chooses paper' do
       subject.user_choice("rock")
-      allow(subject).to receive(:comp_choice).and_return("paper")
       expect(subject.lose?).to eql(true)
     end
-
   end
 
-
+  describe 'result' do
+    it 'should display the result of the game' do
+    expect(subject).to respond_to(:result)
+    end
+  end 
 end
