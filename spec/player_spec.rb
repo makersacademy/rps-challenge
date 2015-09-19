93a19d2257a2f6e8d7cb68bbdcca2183f128aaeb
chweeks <chrisharryweeks@gmail.com>
2015-09-19 13:22:39 +0100

    added player class tests

---
+++ spec/player_spec.rb
@@ -0,0 +1,21 @@
+require 'player'
+
+describe Player do
+
+  it 'Should initialize with a choice of nil' do
+
+  end
+
+  context '#choice' do
+
+    it { is_expected.to respond_to :choice }
+
+    to 'should change players choice' do
+
+    end
+
+    to 'should raise error if choice is invalid' do
+
+    end
+  end
+end
