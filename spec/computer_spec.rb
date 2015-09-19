107f873d2e4772ed7e300e317cfd57eef4c1d9b6
Tim Hyson <timhyson@users.noreply.github.com>
2015-09-20 00:39:16 +0100

    Update computer_spec.rb

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,20 +1,18 @@
 require "computer"
 
 describe Computer do
+  it "can select rock at random" do
+    allow(subject).to receive(:select).and_return(:rock)
+    expect(subject.select).to eq :rock
+  end
 
-    it "can select rock at random" do
-      allow(subject).to receive(:select).and_return(:rock)
-      expect(subject.select).to eq :rock
-    end
-
-    it "can select paper at random" do
-      allow(subject).to receive(:select).and_return(:paper)
-      expect(subject.select).to eq :paper
-    end
-
-    it "can select scissors at random" do
-      allow(subject).to receive(:select).and_return(:scissors)
-      expect(subject.select).to eq :scissors
-    end
-
+  it "can select paper at random" do
+    allow(subject).to receive(:select).and_return(:paper)
+    expect(subject.select).to eq :paper
+  end
+  
+  it "can select scissors at random" do
+    allow(subject).to receive(:select).and_return(:scissors)
+    expect(subject.select).to eq :scissors
+  end
 end
