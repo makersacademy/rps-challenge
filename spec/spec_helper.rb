6595651923f6f7da3b7e6a05885c9d8cfcc6f172
Usman Jamil <usman.jamil@live.co.uk>
2015-09-20 15:32:30 +0100

    added commented text back to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+# ensure the following is AT THE TOP of your spec_helper.rb to get test coverage stats
 require 'coveralls'
 require 'simplecov'
 
@@ -5,7 +6,9 @@ SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
+# replace following line with SimpleCov.start to get coverage stats locally
 Coveralls.wear!
+# run `open coverage/index.html` from the command line to view details
 
 require 'byebug'
 require 'capybara/rspec'
