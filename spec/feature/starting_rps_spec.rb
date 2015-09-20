78ed06adaa37e1e2a60d17570220867b5343d024
Usman Jamil <usman.jamil@live.co.uk>
2015-09-20 15:26:14 +0100

    edit feature tests

--- spec/feature/starting_rps_spec.rb
+++ spec/feature/starting_rps_spec.rb
@@ -6,14 +6,14 @@ feature 'Starting a new game' do
     expect(page).to have_content "What's your name?"
   end
 
-  scenario 'Must enter name to start game' do
+  scenario 'Must ask player to choose an object' do
     visit '/'
     fill_in('name', with: "Usman")
     click_button 'Start Game'
     expect(page).to have_content "Usman, choose your object"
   end
 
-  scenario 'Must enter name to start game' do
+  scenario 'Must provide a result of the game' do
     visit '/start_game'
     click_button 'Submit'
     expect(page).to have_content "Game result:"
