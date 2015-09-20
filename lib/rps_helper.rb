28827ffc22f76f7cc2a228e460b9ef4332a2ba36
Harry <harryandrew.dix@gmail.com>
2015-09-20 19:36:47 +0100

    changed image hrefs

--- lib/rps_helper.rb
+++ lib/rps_helper.rb
@@ -3,7 +3,7 @@ $game = Game.new
 $cpu = Cpu.new
 
 def cpu_move_image
-  return "<img src='http://i59.tinypic.com/2v15doy.png'>" if $cpu.last_move == :rock
-  return "<img src='http://i62.tinypic.com/wtu5p1.png'>" if $cpu.last_move == :paper
-  return "<img src='http://i58.tinypic.com/f2tpqq.png'>" if $cpu.last_move == :scissors
+  return "<img src='http://oi58.tinypic.com/28hel93.jpg'>" if $cpu.last_move == :rock
+  return "<img src='http://oi61.tinypic.com/2w3x3za.jpg'>" if $cpu.last_move == :paper
+  return "<img src='http://oi57.tinypic.com/34xnz9v.jpg'>" if $cpu.last_move == :scissors
 end
