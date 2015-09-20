c9b518da1cb3a6bcab5181abff0097da918627f9
Aaron <aarongkendall@gmail.com>
2015-09-20 10:01:09 +0100

    web spec updated

--- spec/features/rps_spec.rb
+++ spec/features/rps_spec.rb
@@ -11,18 +11,21 @@ feature 'Playing a game of Rock, Paper, Scissors' do
 
   scenario 'There is a rock button that allows player to choose rock' do
     visit '/'
+    fill_in 'name', with: 'Aaron'
     click_button 'rock'
     expect(page).to have_content 'Rock'
   end
 
   scenario 'There is a paper button that allows player to choose rock' do
     visit '/'
+    fill_in 'name', with: 'Aaron'
     click_button 'paper'
     expect(page).to have_content 'Paper'
   end
 
   scenario 'There is a scissors button that allows player to choose rock' do
     visit '/'
+    fill_in 'name', with: 'Aaron'
     click_button 'scissors'
     expect(page).to have_content 'Scissors'
   end
