ab0a38bc3f14acba0ffa91b051e92c661dd931a1
Tim Harrison <mail@trwh.co.uk>
2015-09-20 16:43:19 +0100

    Tests passing ready to make a game UI.

--- rps-web.rb
+++ rps-web.rb
@@ -6,7 +6,15 @@ class RPSWeb < Sinatra::Base
     erb :index
   end
 
+  post '/new_game' do
+    session[:user_name] = params[:user_name]
+    @user_name = session[:user_name]
+
+    erb :new_game
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
+
   set :views, proc { File.join(root, 'views') }
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -7,5 +7,19 @@ feature 'Starting a new game,' do
     expect(page).to have_content "Please enter your name."
   end
 
+  scenario 'I hit submit with an empty name field.' do
+    visit '/'
+    fill_in('user_name', :with => '')
+    click_button 'Submit'
+    expect(page).to have_content 'Sorry, you must enter a name.'
+  end
+
+  scenario 'I want to see my name in lights because I\'m a marketeer.' do
+    visit '/'
+    fill_in('user_name', :with => 'Dobby')
+    click_button 'Submit'
+    expect(page).to have_content 'Welcome to Rock, Paper, Scissors, Dobby!'
+  end
+
 end
 
