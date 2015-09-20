3862cdce9e2ff21e3ee02089b1bcbe857c8de2aa
ana <adscarlos@gmail.com>
2015-09-20 15:46:09 +0100

    updates game logic - wins, loses or draw, and rspec tests

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,18 @@ class Game
     moves.sample
   end
 
+  def outcome player_move, computer_move
+    
+    return "It's a draw!" if player_move == computer_move
+    case player_move
+    when :scissors
+      declare_winner = computer_move == :paper ? "You win!" : "You lose!"
+    when :paper
+      declare_winner = computer_move == :rock ? "You win!" : "You lose!"
+    when :rock
+      declare_winner = computer_move == :scissors ? "You win!" : "You lose!"
+    end
+    declare_winner
+  end
+
 end
---
+++ spec/game_spec.rb
@@ -0,0 +1,83 @@
+require 'game'
+
+  describe Game do
+
+    xit "Raises error if player's move isn't valid" do
+      computer_move = subject.generate_move
+      player_move = :banana
+      expect{subject.outcome(player_move, computer_move)}.to raise_error "That's not a valid move"
+    end
+
+    context "When the computer's move is rock" do
+      before(:each) do
+        allow(subject).to receive(:generate_move){:rock}
+      end
+
+      it "Player wins when their move is paper" do
+        computer_move = subject.generate_move
+        player_move = :paper
+        expect(subject.outcome(player_move, computer_move)).to eq "You win!"
+      end
+
+      it "Player loses when their move is scissors" do
+        computer_move = subject.generate_move
+        player_move = :scissors
+        expect(subject.outcome(player_move, computer_move)).to eq "You lose!"
+      end
+
+      it "It's a draw when the player's move is rock" do
+       computer_move = subject.generate_move
+       player_move = :rock
+       expect(subject.outcome(player_move, computer_move)).to eq "It's a draw!"
+      end
+    end
+
+    context "When the computer's move is paper" do
+      before(:each) do
+        allow(subject).to receive(:generate_move){:paper}
+      end
+
+      it "Player wins when their move is scissors" do
+        computer_move = subject.generate_move
+        player_move = :scissors
+        expect(subject.outcome(player_move, computer_move)).to eq "You win!"
+      end
+
+      it "Player loses when their move is rock" do
+        computer_move = subject.generate_move
+        player_move = :rock
+        expect(subject.outcome(player_move, computer_move)).to eq "You lose!"
+      end
+
+      it "It's a draw when the player's move is paper" do
+       computer_move = subject.generate_move
+       player_move = :paper
+       expect(subject.outcome(player_move, computer_move)).to eq "It's a draw!"
+      end
+    end
+
+    context "When the computer's move is scissors" do
+      before(:each) do
+        allow(subject).to receive(:generate_move){:scissors}
+      end
+
+      it "Player wins when their move is rock" do
+        computer_move = subject.generate_move
+        player_move = :rock
+        expect(subject.outcome(player_move, computer_move)).to eq "You win!"
+      end
+
+      it "Player loses when their move is paper" do
+        computer_move = subject.generate_move
+        player_move = :paper
+        expect(subject.outcome(player_move, computer_move)).to eq "You lose!"
+      end
+
+      it "It's a draw when the player's move is scissors" do
+       computer_move = subject.generate_move
+       player_move = :scissors
+       expect(subject.outcome(player_move, computer_move)).to eq "It's a draw!"
+      end
+    end
+
+  end
