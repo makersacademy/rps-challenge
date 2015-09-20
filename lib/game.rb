4806eb2c3d6ca673abd97767180b6c6c5121e229
zsid <zhivko.siderov@gmail.com>
2015-09-20 15:19:55 +0100

    Some fixes

--- lib/game.rb
+++ lib/game.rb
@@ -14,19 +14,23 @@ class Game
   def play(player_choice)
     player_choice = player.choose(player_choice.to_sym)
     computer_choice = computer.choose
-    return "It's a draw!" if is_a_draw?(player_choice, computer_choice)
+    return "It's a draw!" if a_draw?(player_choice, computer_choice)
     return "#{player.name} wins!" if player_wins?(player_choice, computer_choice)
-    return "You lose, #{player.name}!"
+    return "You lose, #{player.name}!" if comp_wins?(player_choice, computer_choice)
   end
 
   private
 
-  def is_a_draw?(player_choice, computer_choice)
+  def a_draw?(player_choice, computer_choice)
     player_choice == computer_choice
   end
 
   def player_wins?(player_choice, computer_choice)
     computer_choice == defeat[player_choice]
+  end
+
+  def comp_wins?(player_choice, computer_choice)
+    player_choice == defeat[computer_choice]
   end
 
 end
--- lib/multiplayer.rb
+++ lib/multiplayer.rb
@@ -13,19 +13,23 @@ class Multiplayer
   def play(player1, player2)
     p1_choice = p1.choose(player1.to_sym)
     p2_choice = p2.choose(player2.to_sym)
-    return "It's a draw!" if is_a_draw?(p1_choice, p2_choice)
+    return "It's a draw!" if a_draw?(p1_choice, p2_choice)
     return "#{p1.name} wins!" if p1_wins?(p1_choice, p2_choice)
-    return "#{p2.name} wins!"
+    return "#{p2.name} wins!" if p2_wins?(p1_choice, p2_choice)
   end
 
   private
 
-  def is_a_draw?(p1_choice, p2_choice)
+  def a_draw?(p1_choice, p2_choice)
     p1_choice == p2_choice
   end
 
   def p1_wins?(p1_choice, p2_choice)
     p2_choice == defeat[p1_choice]
+  end
+
+  def p2_wins?(p1_choice, p2_choice)
+    p1_choice == defeat[p2_choice]
   end
 
 end
