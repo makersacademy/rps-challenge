7cbb4c2259ab7b4e85faf1b895f62858484032f0
adellanno <anthony@dellanno.co.uk>
2015-09-20 22:55:50 +0100

    minor hound changes

--- spec/features/rps_spec.rb
+++ spec/features/rps_spec.rb
@@ -16,14 +16,14 @@ feature 'Creates a game of Rock, Paper, Scissors' do
 
   it 'clicking submit will take you to the results page' do
     visit '/game'
-    fill_in 'choice', :with => 'rock'
+    fill_in 'choice', with: 'rock'
     click_button('Submit')
     expect(page).to have_content "The result is:"
   end
 
   it 'you will be prompted to enter rock, paper, scissors if anything other is entered' do
     visit '/game'
-    fill_in 'choice', :with => 'pirates'
+    fill_in 'choice', with: 'pirates'
     click_button('Submit')
     expect(page).to have_content "Woops...Please enter either rock, paper or scissors"
   end
