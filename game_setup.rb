f39c88db436dec3c35f29c4f4c451337229dd1cb
Matt Paul <mattpaul001@icloud.com>
2015-09-20 13:04:04 +0100

    Corrected a couple of hound violations caused by Seeing is Believing

--- game_setup.rb
+++ game_setup.rb
@@ -1,12 +1,5 @@
-require './lib/game'    # ~> LoadError: cannot load such file -- ./lib/game
+require './lib/game'    
 require './lib/player'
 
 
 game = Game.new(Player.new("Matt"))
-
-# ~> LoadError
-# ~> cannot load such file -- ./lib/game
-# ~>
-# ~> /Users/matt/.rvm/rubies/ruby-2.1.5/lib/ruby/site_ruby/2.1.0/rubygems/core_ext/kernel_require.rb:54:in `require'
-# ~> /Users/matt/.rvm/rubies/ruby-2.1.5/lib/ruby/site_ruby/2.1.0/rubygems/core_ext/kernel_require.rb:54:in `require'
-# ~> /Users/matt/Dropbox/Projects/w3/rps-challenge/game_setup.rb:1:in `<main>'
