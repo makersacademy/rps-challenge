4f46694d64acf172e1bd18dfa38f6c670743bffb
Michael Roger <bodgeuk12@gmail.com>
2015-09-20 12:35:03 +0100

    Game complete (or at least functions well enough for repeated games of RPS)

--- lib/game.rb
+++ lib/game.rb
@@ -9,8 +9,9 @@ class Game
 
   def challenge
     fail "Both players must choose their weapon" if not_ready_to_challenge?
-    return "Same choice, draw." if draw?
-    "#{winner.name} wins the game!"
+    string = outcome_message
+    reset_player_options
+    return string
   end
 
   private
@@ -20,12 +21,22 @@ class Game
   end
 
   def draw?
-    player1.current_selection == player2.current_selection
+    player1.current_selection.class == player2.current_selection.class
   end
 
   def winner
     return player1 if player1.current_selection.beats?(player2.current_selection)
     player2
+  end
+
+  def outcome_message
+    return "Same choice, draw." if draw?
+    "#{winner.name} wins the game!"
+  end
+
+  def reset_player_options
+    player1.current_selection = nil
+    player2.current_selection = nil
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,10 +2,14 @@ require 'game'
 
 describe Game do
 
-  let(:shape1){double(:shape1)}
-  let(:shape2){double(:shape2)}
-  let(:player1){double(:player1, current_selection: shape1, name: "Player1")}
-  let(:player2){double(:player2, current_selection: shape2, name: "Player2")}
+  let(:shape1){class Shape1; Shape1.new; end}
+  let(:shape2){class Shape2; Shape2.new; end}
+  let(:player1){double(:player1,  :current_selection => shape1,
+                                  :current_selection= => nil,
+                                  :name => "Player1")}
+  let(:player2){double(:player2,  :current_selection => shape2,
+                                  :current_selection= => nil,
+                                  :name => "Player2")}
   let(:game){ Game.new(player1, player2) }
   let(:player_no_selection){double(:player_no, current_selection: nil)}
 
@@ -35,7 +39,7 @@ describe Game do
       expect(game.challenge).to eq "Player2 wins the game!"
     end
 
-    it 'declares a draw when players have the same choice' do
+    it 'declares a draw when players have chosen the same type of object' do
       game = Game.new(player1, player1)
       allow(shape1).to receive(:beats?) {false}
       expect(game.challenge).to eq "Same choice, draw."
@@ -44,6 +48,12 @@ describe Game do
     it 'raises an error if a player has not made a selection' do
       game = Game.new(player1, player_no_selection)
       expect{ game.challenge }.to raise_error "Both players must choose their weapon"
+    end
+
+    it 'clears the players current selections after a successful challenge' do
+      allow(shape1).to receive(:beats?) {true}
+      expect(player1).to receive(:current_selection=).with(nil)
+      game.challenge
     end
 
   end
