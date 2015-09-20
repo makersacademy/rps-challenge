428ec712d4c85f983c1356db670c3ce875c54dc4
Michael Roger <bodgeuk12@gmail.com>
2015-09-20 17:23:51 +0100

    Updated application files to randomly select a players choice

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,14 @@
 class Player
 
   attr_accessor :current_selection, :name
+  attr_reader :weapons
+
+  def initialize
+    @weapons = [Rock, Paper, Scissors]
+  end
+
+  def random_selector
+    @current_selection = weapons[rand(2)].new
+  end
 
 end
--- spec/features/playing_the_game_spec.rb
+++ spec/features/playing_the_game_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
-feature 'Playing a new game' do
+feature 'Playing the game' do
+
+  before do
+    visit '/'
+    fill_in 'name', with: "Mick"
+    click_button 'Enter'
+    click_button 'Start new game!'
+  end
+
+  scenario 'expect to have an option to select Rock' do
+    find_button 'Rock'
+  end
+
+  scenario 'expect to have an option to select Paper' do
+    find_button 'Paper'
+  end
+
+  scenario 'expect to have an option to select Scissors' do
+    find_button 'Scissors'
+  end
+
+  feature 'playing against the computer (randomly selecting its option)' do
+
+    scenario 'after selecting my option I see who won' do
+      click_button 'Rock'
+      expect(page).to have_content "wins the game!"
+    end
+
+  end
 
 end
