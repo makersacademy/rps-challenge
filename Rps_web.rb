3a360f56922e9315da88253b3c9af6aef0e6bf2b
chweeks <chrisharryweeks@gmail.com>
2015-09-20 19:45:13 +0100

    added name input and tests

--- Rps_web.rb
+++ Rps_web.rb
@@ -13,5 +13,21 @@ class Rps_web < Sinatra::Base
     erb :index
   end
 
+  get '/name' do
+    @name = session[:name]
+    erb :name
+  end
+
+  post '/name' do
+    p session[:name] = params[:name]
+    redirect ('/name') if params[:name].empty?
+    redirect ('/game')
+  end
+
+  get '/game' do
+    @name = session[:name]
+    erb :game
+  end
+
   run! if app_file == $0
 end
--- spec/features/rps_spec.rb
+++ spec/features/rps_spec.rb
@@ -5,4 +5,17 @@ feature 'Starting a new game' do
     visit '/'
     expect(page).to have_link "New Game"
   end
+
+  scenario 'Loop until name given' do
+    visit '/name'
+    click_button 'Start'
+    expect(page).to have_content "What's your name"
+  end
+
+  scenario 'Start game if name given' do
+    visit '/name'
+    fill_in 'name', with: 'Chris'
+    click_button 'Start'
+    expect(page).to have_content "Welcome Chris, GAME ON"
+  end
 end
