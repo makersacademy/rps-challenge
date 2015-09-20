3c6749e4446a5aa917c2750ba67772059383a739
danielstpaul <danielstpaul@hotmail.com>
2015-09-20 14:12:21 +0100

    refactoring

--- spec/features/start_rpsls_spec.rb
+++ spec/features/start_rpsls_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 feature 'Register name' do
   scenario 'Demands player name' do
     visit '/'
-    click_button 'Proceed'
+    click_button('Proceed')
     expect(page).to have_content("What's your name!")
   end
 
   scenario 'A name has to be registered' do
     visit '/name'
-    fill_in 'name', with: ''
-    click_button 'Next'
+    fill_in('name', with: '')
+    click_button('Next')
     expect(page).to have_content("What's your name!")
   end
 end
@@ -19,7 +19,7 @@ feature 'Instructions' do
   scenario 'Once player has registered, instructions are given' do
     visit '/name'
     fill_in('name', with: 'Dan')
-    click_button 'Next'
+    click_button('Next')
     expect(page).to have_content("Hello, Dan! Welcome to"\
     " Rock-Paper-Scissors-Lizard-Spock")
   end
@@ -33,16 +33,16 @@ feature 'Rock-Paper-Scissors-Lizard-Spock Game' do
 
   scenario 'Player is given five shapes to throw' do
     visit '/throw'
-    expect(page).to have_select 'shape'
+    expect(page).to have_select('shape')
   end
 
   scenario 'Player can throw a shape' do
     visit '/name'
     fill_in 'name', with: 'Dan'
-    click_button 'Next'
-    click_button "Let's Rock"
-    select 'Spock', from: 'shape'
-    click_button 'Throw'
-    expect(page).to have_content 'You threw Spock!'
+    click_button('Next')
+    click_button("Let's Rock")
+    select('Spock', from: 'shape')
+    click_button('Throw')
+    expect(page).to have_content('You threw Spock!')
   end
 end
