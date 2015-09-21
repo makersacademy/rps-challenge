2ebd073704d0fbe442ccccea8d40a5bc26d7bcf3
Usman Jamil <usman.jamil@live.co.uk>
2015-09-21 11:10:31 +0100

    hound changes

--- lib/game.rb
+++ lib/game.rb
@@ -4,11 +4,16 @@ require_relative 'computer'
 class Game
 
   RULES = {
-   rock:     {rock: :draw, paper: :computer_wins, scissors: :player_wins, spock: :computer_wins, lizard: :player_wins},
-   paper:    {rock: :player_wins, paper: :draw,  scissors: :computer_wins, spock: :player_wins, lizard: :computer_wins},
-   scissors: {rock: :computer_wins, paper: :player_wins, scissors: :draw, spock: :computer_wins, lizard: :player_wins},
-   spock:    {rock: :player_wins, paper: :computer_wins, scissors: :player_wins, spock: :draw, lizard: :computer_wins},
-   lizard:   {rock: :computer_wins, paper: :player_wins, scissors: :computer_wins, spock: :player_wins, lizard: :draw}
+   rock:     {rock: :draw, paper: :computer_wins, scissors: :player_wins,
+     spock: :computer_wins, lizard: :player_wins},
+   paper:    {rock: :player_wins, paper: :draw,  scissors: :computer_wins,
+     spock: :player_wins, lizard: :computer_wins},
+   scissors: {rock: :computer_wins, paper: :player_wins, scissors: :draw,
+     spock: :computer_wins, lizard: :player_wins},
+   spock:    {rock: :player_wins, paper: :computer_wins, scissors: :player_wins,
+     spock: :draw, lizard: :computer_wins},
+   lizard:   {rock: :computer_wins, paper: :player_wins, scissors: :computer_wins,
+     spock: :player_wins, lizard: :draw}
   }
 
   def play(player, computer)
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,12 +2,15 @@ require 'computer'
 
 describe Computer do
 
+
+
   it 'shoud select an item from the array' do
     allow(subject).to receive(:auto).and_return(:rock)
     expect(subject.auto).to eq :rock
   end
 
   it 'should only select one out of rock, paper, scissors, spock or lizard' do
-    expect(subject.auto).to eq(:rock).or eq(:paper).or eq(:scissors).or eq(:spock).or eq(:lizard)
+    expect(subject.auto).to eq(:rock).or eq(
+      :paper).or eq(:scissors).or eq(:spock).or eq(:lizard)
   end
 end
