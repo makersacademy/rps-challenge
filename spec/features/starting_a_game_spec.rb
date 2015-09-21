b835af99fccbf2f0caa4598e17da9169d763e8c5
TJQKAs <a26052013@gmail.com>
2015-09-21 02:05:16 +0100

    update all

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -1,22 +1,36 @@
 require 'spec_helper'
 
-feature 'Starting new game' do
-  scenario 'I see a greeting when I start the game' do
-    visit '/'
-    expect(page).to have_content "Welcome! Are you ready for new game"
-  end
+     feature 'Starting new game' do
+
+     scenario 'I see a greeting when I start the game' do
+     visit '/'
+     expect(page).to have_content "Welcome! Are you ready for new game"
   end
 
-feature 'Starting new game' do
-  scenario 'I am asked to enter my name' do
+
+    scenario 'I am asked to enter my name' do
     visit '/request'
     expect(page).to have_content "What's your name?"
   end
-  end
 
-   feature 'We should check that Player input his name' do
-   scenario "What's your name?"  do
-   redirect '/request?name='if @name == ""
-   expect(page).to have_content "You didn't input your name, please try again."
+
+  scenario 'After player wrote his name, he can see his name and prompt' do
+     visit '/request'
+     fill_in('name', with: 'John')
+     click_button('submit')
+     expect(page).to have_content "John! press button to start the game"
    end
+
+scenario 'After player submitted his name, he can make a choice' do
+   visit '/request'
+   fill_in('name', with: 'John')
+   click_button('submit')
+   click_button('Start')
+   visit '/game'
+   expect(page).to have_content "Make your choice John"
+end
+
+
+
+
  end
