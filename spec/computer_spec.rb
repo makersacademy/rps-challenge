4055adf76bb8ebefe35db29c67e48d97c26ace4e
adellanno <anthony@dellanno.co.uk>
2015-09-21 12:34:07 +0100

    rspec coverage mostly complete

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,1 +1,9 @@
-require 'computer'
+# require 'computer'
+
+# describe Computer do
+
+#   it "expect computer to contain either rock, paper or scissors" do
+#     expect(subject.computer_weapon).should include("rock", "paper", "scissors")
+#   end
+
+# end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,24 @@
 require 'player'
+
+describe Player do
+
+  it '# expect player to respond to choose_weapon' do
+    expect(subject).to respond_to(:choose_weapon)
+  end
+
+  it 'expect choose_weapon to store rock in player_weapon' do
+    subject.choose_weapon("rock")
+    expect(subject.player_weapon).to eq("rock")
+  end
+
+  it 'expect choose_weapon to store paper in player_weapon' do
+    subject.choose_weapon("paper")
+    expect(subject.player_weapon).to eq("paper")
+  end
+
+  it 'expect choose_weapon to store scissors in player_weapon' do
+    subject.choose_weapon("scissors")
+    expect(subject.player_weapon).to eq("scissors")
+  end
+
+end
