7128b6a323a1176d00bc572723cc3ef3985181cb
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-21 14:03:53 +0100

    Add additional capybara test for final results page

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -14,23 +14,14 @@ feature 'Entering the page' do
     expect(page).to have_content("Choose your weapon, Adrian!")
   end
 
-  # scenario 'I can see the result of the game' do
-  #   visit '/'
-  #   fill_in('name', with:'Adrian')
-  #   click_button("Start Game")
-  #   choose("rock")
-  #   click_button("PLAY!")
-  #   expect(page).to have_content("The winner is")
-  # end
+  scenario 'I can see the result of the game' do
+    visit '/'
+    fill_in('name', with:'Adrian')
+    click_button("Start Game")
+    choose("rock")
+    click_button("PLAY!")
+    expect(page).to have_content("You chose")
+  end
 
 
 end
-
-# feature 'displays whether you have won or lost' do
-#   scenario 'should go to results page when "PLAY" button clicked' do
-#   visit '/play-game'
-#   fill_in('weapon', with:'rock')
-#   click_button('submit')
-#   expect(page).to have_content("New Page")
-#   end
-# end
