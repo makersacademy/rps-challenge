c5412b569a67eb0555b8e05e14bfbf87d3717f64
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-21 01:43:46 +0100

    refactored to split up RpsGame class

--- lib/rps_game.rb
+++ lib/rps_game.rb
@@ -1,63 +1,48 @@
+require_relative 'rps_rules'
+
 class RpsGame
 
-  WEAPONS = ['Rock', 'Paper', 'Scissors']
-
-  RULES = { ['Rock', 'Scissors']  => 'blunts',
-            ['Scissors', 'Paper'] => 'cuts',
-            ['Paper', 'Rock']     => 'wraps' }
-
-  MORE_WEAPONS = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
-
-  MORE_RULES = { ['Rock', 'Scissors']   => 'blunts',
-                 ['Rock', 'Lizard']     => 'crushes',
-                 ['Scissors', 'Paper']  => 'cuts',
-                 ['Scissors', 'Lizard'] => 'decapitates',
-                 ['Paper', 'Rock']      => 'wraps',
-                 ['Paper', 'Spock']     => 'disproves',
-                 ['Lizard', 'Paper']    => 'eats',
-                 ['Lizard', 'Spock']    => 'poisons',
-                 ['Spock', 'Scissors']  => 'disassembles',
-                 ['Spock', 'Rock']      => 'vapourises'  }
-
-  @weapons = WEAPONS
-  @rules = RULES
-  @setup = { player: 'Player', scores: [0,0], extended: false,
-             player_weapon: 'Rock', computer_weapon: 'Rock' }
+  @rules = RpsRules
+  @setup = { player: 'Player', scores: [0,0], player_weapon: 'Rock',
+             computer_weapon: 'Rock', extended: false }
   @results = { winner: nil, report: 'Rock meets Rock' }
 
   class << self
-    attr_reader :weapons, :setup, :results
+    attr_reader :setup, :results, :rules
     private
-    attr_reader :rules
+    attr_writer :rules
   end
 
   def self.reset_scores
-    self.setup[:scores] = [0,0]
-  end
-
-  def self.enable_extended(flag)
-    self.setup[:extended] = flag
-    @weapons = flag ? MORE_WEAPONS : WEAPONS
-    @rules = flag ? MORE_RULES : RULES
+    setup[:scores] = [0,0]
   end
 
   def self.choose_player(name)
-    self.setup[:player] = name
+    setup[:player] = name
   end
 
   def self.choose_player_weapon(choice)
-    self.setup[:player_weapon] = choice
+    setup[:player_weapon] = choice
   end
 
   def self.choose_computer_weapon
-    self.setup[:computer_weapon] = weapons.sample
+    setup[:computer_weapon] = weapons.sample
+  end
+
+  def self.weapons
+    rules.weapons
+  end
+
+  def self.enable_extended(flag)
+    setup[:extended] = flag
+    rules.extend(flag)
   end
 
   def self.play
     weapon1 = setup[:player_weapon]
     weapon2 = setup[:computer_weapon]
-    message1 = rules[[weapon1, weapon2]]
-    message2 = rules[[weapon2, weapon1]]
+    message1 = rules.check(weapon1, weapon2)
+    message2 = rules.check(weapon2, weapon1)
     write_report(weapon1, weapon2, message1, message2)
     update_scores(results[:winner])
     results
@@ -81,8 +66,8 @@ class RpsGame
   end
 
   def self.update_scores(winner)
-    self.setup[:scores][0] += (winner == setup[:player] ? 1 : 0)
-    self.setup[:scores][1] += (winner == 'Computer' ? 1 : 0)
+    setup[:scores][0] += (winner == setup[:player] ? 1 : 0)
+    setup[:scores][1] += (winner == 'Computer' ? 1 : 0)
   end
 
   private_class_method :write_report, :report, :update_scores
