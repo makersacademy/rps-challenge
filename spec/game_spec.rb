478fb6813d44682cd65669a47b20e4ee38d6738a
Usman Jamil <usman.jamil@live.co.uk>
2015-09-21 14:07:34 +0100

    added tests for game.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,10 +3,17 @@ require 'game'
 describe Game do
 
   let(:player) {double :player, object: :scissors }
+  let(:player1) {double :player, object: :scissors }
+  let(:player2) {double :player, object: :rock }
+  let(:player3) {double :player, object: :paper }
   let(:computer) {double :computer}
 
   before :each do
     allow(player).to receive(:choose).with(:scissors).and_return :scissors
+    allow(player1).to receive(:choose).with(:scissors).and_return :scissors
+    allow(player2).to receive(:choose).with(:rock).and_return :rock
+    allow(player3).to receive(:choose).and_return :paper
+
     allow(computer).to receive(:auto).and_return(:scissors)
   end
 
@@ -25,6 +32,18 @@ describe Game do
     player.choose(:scissors)
     allow(computer).to receive(:auto).and_return(:paper)
     expect(subject.play(player, computer)).to eq("You are the winner!")
+  end
+
+  it 'should declare player 1 is the winner' do
+    player.choose(:scissors)
+    player3.choose(:paper)
+    expect(subject.play2(player, player3)).to eq("Player 1 wins!")
+  end
+
+  it 'should declare player 2 is the winner' do
+    player1.choose(:scissors)
+    player2.choose(:rock)
+    expect(subject.play2(player1, player2)).to eq("Player 2 wins!")
   end
 
 end
