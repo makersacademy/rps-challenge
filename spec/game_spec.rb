42760a910af5942820503f65468e40aba372f952
chweeks <chrisharryweeks@gmail.com>
2015-09-21 03:30:41 +0100

    added tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,10 +3,12 @@ require 'game'
 describe Game do
 
   let(:goku) {double :goku, hand: :rock}
+  let(:krillin) {double :krillin, hand: :spock}
 
   it 'should return true if human is winner' do
     vegeta = double :computer, hand: :scissors
     expect(subject.winner?(goku, vegeta)).to be true
+    expect(subject.winner?(krillin, vegeta)).to be true
   end
 
   it 'should return true if it\'s a draw' do
@@ -17,5 +19,6 @@ describe Game do
   it 'should return true if human loses' do
     vegeta = double :computer, hand: :paper
     expect(subject.loser?(goku, vegeta)).to be true
+    expect(subject.loser?(krillin, vegeta)).to be true
   end
 end
