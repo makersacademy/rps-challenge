8043996ca068e7ad47a4e59c270d990e39d42107
Usman Jamil <usman.jamil@live.co.uk>
2015-10-05 17:35:24 +0100

    refactoring controller

--- rpsweb.rb
+++ rpsweb.rb
@@ -14,96 +14,81 @@ class Rps_web < Sinatra::Base
 
   post '/' do
     session[:players] = params[:players]
-    redirect ('double_player') if params[:players] == '2 Player'
-    redirect ('/single_player')
+    redirect ('/players')
   end
 
-  get '/single_player' do
-    erb :player1
-  end
-
-  get '/double_player' do
-    erb :double_player
-  end
-
-  post '/double_player' do
-    session[:name1] = params[:name1]
-    session[:name2] = params[:name2]
-    redirect ('/start_game_double')
-  end
-
-  post '/single_player' do
-    session[:name] = params[:name]
-    redirect ('/single_player') if params[:name].empty?
-    redirect ('/start_game')
-  end
-
-  get '/start_game' do
-      @name = session[:name]
-      @game = Game.new
-      session[:game] = @game
-      @player = Player.new
-      session[:player] = @player
-      @computer = Computer.new
-      session[:computer] = @computer
-      erb :start_game
-  end
-
-  post '/game' do
-    session[:object] = params[:object].to_sym
-    redirect ('/game')
-  end
-
-  get '/game' do
-    session[:player].choose(session[:object])
-    session[:winner] = session[:game].play(session[:player], session[:computer])
-    erb :game
-  end
-
-  get '/start_game_double' do
-    @name1 = session[:name1]
-    @game = Game.new
-    session[:game] = @game
-    @player1 = Player.new
-    @player2 = Player.new
-    session[:player1] = @player1
-    session[:player2] = @player2
-    erb :start_game_double
-  end
-
-  post '/start_game_double' do
-    session[:object1] = params[:object1].to_sym
-    redirect ('/start_game_double2')
-  end
-
-  get '/start_game_double2' do
-    if @name2 == nil
-      @name2 = session[:name2]
-      session[:player2]
-      erb :start_game_double2
+  get '/players' do
+    if session[:players] == '2 Player'
+      erb :double_player
     else
-      session[:player1].choose(session[:object1])
-      session[:player2].choose(session[:object2])
-      session[:winner] = session[:game].play(session[:player], session[:computer])
-      erb :game
+      erb :player1
     end
   end
 
-  post '/start_game_double2' do
-    session[:object2] = params[:object2].to_sym
-    redirect ('/game_double')
+  post '/players' do
+    if session[:players] == '2 Player'
+      session[:name1] = params[:name1]
+      session[:name2] = params[:name2]
+      redirect ('/start_game')
+    else
+      session[:name] = params[:name]
+      redirect ('/players') if params[:name].empty?
+      redirect ('/start_game')
+    end
   end
 
-  post '/game_double' do
-    session[:object] = params[:object].to_sym
-    redirect ('/game_double')
+  get '/start_game' do
+      if session[:players] == '2 Player'
+        session[:object2] = nil
+        session[:object1] = nil
+        @name1 = session[:name1]
+        @game = Game.new
+        session[:game] = @game
+        @player1 = Player.new
+        @player2 = Player.new
+        session[:player1] = @player1
+        session[:player2] = @player2
+        erb :start_game_double
+      else
+        @name = session[:name]
+        @game = Game.new
+        session[:game] = @game
+        @player = Player.new
+        session[:player] = @player
+        @computer = Computer.new
+        session[:computer] = @computer
+        erb :start_game
+      end
   end
 
-  get '/game_double' do
-    session[:player1].choose(session[:object1])
-    session[:player2].choose(session[:object2])
-    session[:winner_double] = session[:game].play2(session[:player1], session[:player2])
-    erb :game_double
+  post '/game' do
+    if session[:players] == '2 Player' && session[:object1] == nil
+      session[:object1] = params[:object1].to_sym
+      redirect ('/game')
+    elsif session[:players] == '2 Player'
+      session[:object2] = params[:object2].to_sym
+      redirect ('/game')
+    else
+      session[:object] = params[:object].to_sym
+      redirect ('/game')
+    end
+  end
+
+  get '/game' do
+    if session[:players] == '2 Player' && session[:object2] == nil
+      @name2 = session[:name2]
+      session[:player2]
+      erb :start_game_double2
+    elsif session[:players] == '2 Player' && session[:object2] != nil
+      session[:player1].choose(session[:object1])
+      session[:player2].choose(session[:object2])
+      session[:winner_double] = session[:game].play2(session[:player1], session[:player2])
+      erb :game_double
+    else
+      session[:player].choose(session[:object])
+      session[:winner] = session[:game].play(session[:player], session[:computer])
+      erb :game
+    end
   end
 
   run! if app_file == Rps_web
