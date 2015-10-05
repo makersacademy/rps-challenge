8315bf718a8e7a8da887e465f15657bfb8c7daa4
Usman Jamil <usman.jamil@live.co.uk>
2015-10-05 18:27:40 +0100

    all tests fixed and passing after refactoring

--- spec/feature/starting_rps_spec.rb
+++ spec/feature/starting_rps_spec.rb
@@ -8,7 +8,7 @@ feature 'Starting a new game' do
   end
 
   scenario 'Must ask player to choose an object' do
-    visit ('/single_player')
+    visit ('/players')
     fill_in('name', with: "Usman")
     click_button 'Start Game'
     expect(page).to have_content "Usman, choose your object..."
@@ -37,20 +37,12 @@ feature 'Starting a new game' do
     expect(page).to have_content "Game result:"
   end
 
-  scenario 'asks for two players name' do
-    visit '/double_player'
-    expect(page).to have_content "Enter the name of player 1:" && "
-      Enter the name of player 2:"
-  end
-
-  scenario 'Must ask player 1 to choose an object' do
-    visit ('/start_game_double')
-    click_button 'rock'
-    expect(page).to have_content "choose your object..."
-  end
-
   scenario 'Must ask player 2 to choose an object' do
-    visit ('/start_game_double')
+    visit ('/')
+    click_button '2 Player'
+    fill_in('name1', with: "Usman")
+    fill_in('name2', with: "Jamil")
+    click_button 'Start Game'
     click_button 'rock'
     click_button 'rock'
     expect(page).to have_content "draw"
