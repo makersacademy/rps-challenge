cfb69395cc596d19585a1bfba1add6151ad56a4d
samover <samuel.overloop@gmail.com>
2015-10-30 16:02:46 +0000

    feature tests choosing RPS and confirming choice

---
+++ spec/features/choose_move_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature 'Choose move' do
+  scenario 'A player can choose Rock' do
+    sign_in_and_play
+    click_button('Rock')
+    expect(page).to have_content('You chose Rock') 
+  end
+  scenario 'A player can choose Paper' do
+    sign_in_and_play
+    click_button('Paper')
+    expect(page).to have_content('You chose Paper') 
+  end
+  scenario 'A player can choose Scissors' do
+    sign_in_and_play
+    click_button('Scissors')
+    expect(page).to have_content('You chose Scissors') 
+  end
+end
