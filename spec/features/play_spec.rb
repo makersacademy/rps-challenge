47017b52bb7aa75472c95adf04dec72306452262
“DovileSand” <dovile.sand@protonmail.ch>
2015-10-30 22:43:15 +0000

    Allow to enter choice and click on submit

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -3,11 +3,20 @@ require 'web_helpers'
 
 feature 'choosing keyword' do
 
-  scenario 'allows to choose rock, paper or scissors' do
+  scenario 'allows to see options' do
 
     register_and_play
 
     expect(page).to have_content('Please enter Rock, Paper or Scissors')
+
+  end
+
+  scenario 'allows to enter one of the options' do
+
+    register_and_play
+
+    click_on 'Shoot!'
+    expect(page).to have_content('You have chosen Rock')
 
   end
 
