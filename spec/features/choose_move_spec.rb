e7b96329dd52070221c0041c5873a67b7a424916
samover <samuel.overloop@gmail.com>
2015-10-30 17:41:33 +0000

    feature test for computer random move

--- spec/features/choose_move_spec.rb
+++ spec/features/choose_move_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
 feature 'Choose move' do
+
   scenario 'A player can choose Rock' do
     sign_in_and_play
     click_button('Rock')
     expect(page).to have_content('You chose Rock') 
   end
+
   scenario 'A player can choose Paper' do
     sign_in_and_play
     click_button('Paper')
     expect(page).to have_content('You chose Paper') 
   end
+
   scenario 'A player can choose Scissors' do
     sign_in_and_play
     click_button('Scissors')
     expect(page).to have_content('You chose Scissors') 
+  end
+
+  scenario 'Computer chooses random move' do
+    allow(game).to receive(:computer_move).and_return('Rock')
+    sign_in_and_play
+    click_button('Rock')
+    expect(page).to have_content('Computor chose Rock')
   end
 end
