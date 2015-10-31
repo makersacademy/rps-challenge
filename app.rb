6cea762cdaa8e08fa8e6421bfa99bde64add6a69
Fergus Lemon <fergus.lemon@gmail.com>
2015-10-31 10:23:47 +0000

    added lib, features and views folders

--- app.rb
+++ app.rb
@@ -1,1 +1,19 @@
 require 'sinatra'
+
+class RockPaperScissors < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'string'
+
+  get '/' do
+    'hello'
+  end
+
+
+
+
+
+
+
+
+  run! if app_file == $0
+end
