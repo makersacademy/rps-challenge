9744d1ec74a9bbc6edfd643aae6d2a613d864d38
jbhdeconinck <jbhdeconinck@gmail.com>
2015-10-31 11:46:53 +0000

    first user story feature test

---
+++ spec/features/player_name_spec.rb
@@ -0,0 +1,12 @@
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+
+feature "register player name" do
+  scenario "enter player name in box" do
+    visit ('/')
+    fill_in('name', with: 'Julien')
+    click_button('Register')
+    expect(page).to have_content('Player name: Julien')
+  end
+end
