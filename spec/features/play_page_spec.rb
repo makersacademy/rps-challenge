ea80bbdd75fb65ac8fb9faaca8d9df9eef2e48a9
dearshrewdwit <dearshrewdwit@gmail.com>
2015-10-31 11:29:06 +0000

    added rps options

--- spec/features/play_page_spec.rb
+++ spec/features/play_page_spec.rb
@@ -1,8 +1,27 @@
 feature "play page:" do
 
+  before { submit_names("Ed") }
+
   scenario "user sees name" do
-    submit_names("Ed")
     expect(page).to have_content "Ed's turn"
   end
-  
+
+  scenario "user sees three options" do
+    expect(page).to have_content "ROCK"
+    expect(page).to have_content "PAPER"
+    expect(page).to have_content "SCISSORS"
+  end
+
+  scenario "user can choose rock" do
+    within('div#options') { within('div#rock') { click_button 'Submit' } }
+  end
+
+  scenario "user can choose paper" do
+    within('div#options') { within('div#paper') { click_button 'Submit' } }
+  end
+
+  scenario "user can choose scissors" do
+    within('div#options') { within('div#scissors') { click_button 'Submit' } }
+  end
+
 end
