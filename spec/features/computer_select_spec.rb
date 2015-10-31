31e675137a82e3f3594da23d668a6f2cf557bf2a
Rebecca Collins <rebeccacollins@rebeccas-mbp-2.home>
2015-10-31 18:29:28 +0000

    working initial model

--- spec/features/computer_select_spec.rb
+++ spec/features/computer_select_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
-feature 'can click on each button action' do
-  let(:RockPaperScissors) {double('RockPaperScissors', computer_selection: 'Paper')}
-  scenario 'player1 can select - rock' do
+feature 'computer can randomly chose option' do
+   let(:RockPaperScissors) {double('RockPaperScissors', computer_selection: 'Paper')}
+
+  scenario 'computer selects option' do
+    # RockPaperScissors.any_instance.stub(:computer_selection).and_return('Paper')
     sign_in_and_play
     click_button 'Rock'
     expect(page).to have_content("Computer randomly choses attack method Paper")
