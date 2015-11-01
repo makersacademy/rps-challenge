7c909ac2f2006fe46cfa741e67617d8f7722023b
thisdotrob <thisdotrob@gmail.com>
2015-11-01 10:21:47 +0000

    Pass feature tests for drawing

--- lib/game.rb
+++ lib/game.rb
@@ -11,14 +11,14 @@ class Game
   end
 
   def set_player_choice(choice)
-    @player_choice = choice
+    @player_choice = choice.to_sym
   end
 
   def winner
     return nil if @player_choice.nil?
     set_computer_choice
     return 'Nobody' if draw?
-    index = @choices.index(@player_choice.to_sym)
+    index = @choices.index(@player_choice)
     (@choices[index-1] == @computer_choice) ? @player_name : 'Computer'
   end
 
