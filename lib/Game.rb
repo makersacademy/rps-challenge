cea8b4d44fbe855341201c008f2717bfa6a536ba
hasulica <raduasandei@gmail.com>
2015-11-01 22:47:14 +0000

    Added computer move method to model

--- lib/Game.rb
+++ lib/Game.rb
@@ -5,4 +5,8 @@ class Game
   def initialize(name)
     @player = name
   end
+
+  def computer_move
+    %w(rock paper scissors).sample
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,4 +7,10 @@ describe Game do
   it 'registers name of player' do
     expect(subject.player).to eq 'radu'
   end
+
+  it 'computer chooses a random number' do
+    srand(1)
+    expect(subject.computer_move).to eq 'paper'
+  end
+
 end
