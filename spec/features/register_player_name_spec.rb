dbb41118c99fedad9ba78b77743643f06741abd9
hasulica <raduasandei@gmail.com>
2015-11-01 20:03:32 +0000

    Initial commit

---
+++ spec/features/register_player_name_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'register player' do
+  scenario 'fill in form with name' do
+    visit('/')
+    fill_in(:player_one_name, with: 'Radu')
+    click_button('Submit')
+    expect(page).to have_content('Radu')
+  end
+end
