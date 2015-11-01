6452e5b3b64b752a710c2c505ebb6772c5b0af95
Hamza Sheikh <hsheikhm@gmail.com>
2015-11-01 19:29:34 +0000

    Completed styling for view: play

--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -1,6 +1,6 @@
 feature 'Register name' do
   scenario "registers name to play" do
     register_and_play
-    expect(page).to have_content('Welcome Hamza')
+    expect(page).to have_content('Hamza VS. Computer')
   end
 end
