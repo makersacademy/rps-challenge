ddbede296e2a48f403b068933792808557c1b3b3
hasulica <raduasandei@gmail.com>
2015-11-01 23:25:14 +0000

    Added tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -19,6 +19,30 @@ describe Game do
       expect(subject.winner('rock', 'scissors')).to eq 'Computer won!'
     end
 
+    it 'rock vs paper' do
+      subject.computer_move
+      expect(subject.winner('rock', 'paper')).to eq 'You won!'
+    end
+
+    it 'paper vs scissors' do
+      subject.computer_move
+      expect(subject.winner('paper', 'scissors')).to eq 'You won!'
+    end
+
+    it 'rock vs rock' do
+      subject.computer_move
+      expect(subject.winner('rock', 'rock')).to eq 'It\'s a draw!'
+    end
+
+    it 'scissors vs scissors' do
+      subject.computer_move
+      expect(subject.winner('scissors', 'scissors')).to eq 'It\'s a draw!'
+    end
+
+    it 'paper vs paper' do
+      subject.computer_move
+      expect(subject.winner('paper', 'paper')).to eq 'It\'s a draw!'
+    end
   end
 
 end
