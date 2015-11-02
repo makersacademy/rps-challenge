05bbc78e34f8ed735b619c40129219b58d9f26bb
Rebecca Collins <rebeccacollins@Rebeccas-MacBook-Pro-2.local>
2015-11-02 10:08:41 +0000

    last update

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
-feature 'result declares the winner' do
-  scenario 'compares and choses th correct winner' do
+feature 'win - loss - draw combinations' do
+  before do
     allow_any_instance_of(Array).to receive(:sample).and_return("Paper")
-    sign_in_and_play
-    click_button 'Rock'
-    expect(page).to have_content("Paper wraps Rock computer wins!!")
-
   end
+
+    scenario 'player loses' do
+      sign_in_and_play
+      click_button 'Rock'
+      expect(page).to have_content("Paper wraps Rock computer wins!!")
+    end
+
+    scenario 'player wins' do
+      visit '/'
+      fill_in 'Player1', with: 'Rebecca'
+      click_button 'Submit'
+      click_button 'Scissors'
+      expect(page).to have_content("Scissors cuts Paper Rebecca wins!!")
+    end
+
+    scenario 'player draws' do
+      visit '/'
+      fill_in 'Player1', with: 'Rebecca'
+      click_button 'Submit'
+      click_button 'Paper'
+      expect(page).to have_content("No body wins")
+    end
 end
