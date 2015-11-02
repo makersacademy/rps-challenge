8a13d3c6b9fac8a554f8eb481ad3fbce491a81fe
George Maddocks <gwpmad@Georges-MacBook-Pro.local>
2015-11-02 21:56:40 +0000

    Removed unnecessary /reset post request

--- app.rb
+++ app.rb
@@ -31,11 +31,6 @@ class Rps < Sinatra::Base
     erb(:result)
   end
 
-  post '/reset' do
-    @game = $game
-    redirect '/play'
-  end
-
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
