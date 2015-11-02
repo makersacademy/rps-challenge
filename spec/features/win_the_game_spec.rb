edc853de44a12caede94218289746c9e39f332f0
samover <samuel.overloop@gmail.com>
2015-11-02 11:18:06 +0000

    dry features specs

--- spec/features/win_the_game_spec.rb
+++ spec/features/win_the_game_spec.rb
@@ -1,27 +1,25 @@
 require 'spec_helper'
 
 feature 'Win the game' do
-  scenario 'Rock against Scissors' do
+  before do
     srand(67_809)
     sign_in_and_play
+  end
+
+  scenario 'Rock against Scissors' do
     click_button('rock')
     expect(page).to have_content('You win')
   end
   scenario 'Paper against Scissors' do
-    srand(67_809)
-    sign_in_and_play
     click_button('paper')
     expect(page).to have_content('Computer wins')
   end
   scenario 'Scissors against Scissors' do
-    srand(67_809)
-    sign_in_and_play
     click_button('scissors')
     expect(page).to have_content('It is a draw')
   end
   scenario 'Rock against Paper' do
     srand(67_808)
-    sign_in_and_play
     click_button('rock')
     expect(page).to have_content('Computer wins')
   end
