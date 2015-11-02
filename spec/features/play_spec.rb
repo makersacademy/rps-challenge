9a6987df18bb688bc71502c53e288866df21b887
thisdotrob <thisdotrob@gmail.com>
2015-11-02 11:43:48 +0000

    Remove unnecessary feature tests

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -24,18 +24,6 @@ feature '/play' do
       click_link('rock.jpg')
       expect(page).to have_content 'Computer wins!'
     end
-    scenario 'when player = paper & computer = scissors' do
-      allow(Kernel).to receive(:rand) { 0.999 }
-      enter_name_and_play
-      click_link('paper.jpg')
-      expect(page).to have_content 'Computer wins!'
-    end
-    scenario 'when player = scissors & computer = rock' do
-      allow(Kernel).to receive(:rand) { 0.332 }
-      enter_name_and_play
-      click_link('scissors.jpg')
-      expect(page).to have_content 'Computer wins!'
-    end
   end
 
   context 'player wins' do
@@ -45,18 +33,6 @@ feature '/play' do
       click_link('paper.jpg')
       expect(page).to have_content 'playerX wins!'
     end
-    scenario 'when computer = paper & player = scissors' do
-      allow(Kernel).to receive(:rand) { 0.665 }
-      enter_name_and_play
-      click_link('scissors.jpg')
-      expect(page).to have_content 'playerX wins!'
-    end
-    scenario 'when computer = scissors & player = rock' do
-      allow(Kernel).to receive(:rand) { 0.999 }
-      enter_name_and_play
-      click_link('rock.jpg')
-      expect(page).to have_content 'playerX wins!'
-    end
   end
 
   context 'Nobody wins' do
@@ -64,18 +40,6 @@ feature '/play' do
       allow(Kernel).to receive(:rand) { 0.332 }
       enter_name_and_play
       click_link('rock.jpg')
-      expect(page).to have_content 'Nobody wins!'
-    end
-    scenario 'when player = paper & computer = paper' do
-      allow(Kernel).to receive(:rand) { 0.665 }
-      enter_name_and_play
-      click_link('paper.jpg')
-      expect(page).to have_content 'Nobody wins!'
-    end
-    scenario 'when player = scissors & computer = scissors' do
-      allow(Kernel).to receive(:rand) { 0.999 }
-      enter_name_and_play
-      click_link('scissors.jpg')
       expect(page).to have_content 'Nobody wins!'
     end
   end
