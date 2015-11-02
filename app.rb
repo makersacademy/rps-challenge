89c657079f2f96cf23bc6d522fd2890e7db25dc7
Fergus Lemon <fergus.lemon@gmail.com>
2015-11-02 08:30:37 +0000

    refactor

--- app.rb
+++ app.rb
@@ -24,12 +24,13 @@ class RockPaperScissors < Sinatra::Base
 
   post '/selection' do
     $player1_choice = $game.player.choose_weapon(params[:weapon])
+    $computer_choice = $game.computer.weapon
     redirect 'result'
   end
 
   get '/result' do
     @player1_choice = $player1_choice
-    @computer_choice = $game.computer.weapon
+    @computer_choice = $computer_choice
     @game_result = $game.result
     erb :result
   end
