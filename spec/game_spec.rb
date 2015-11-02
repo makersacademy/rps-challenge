7f0849f4fa7e18dff59820815238bd5f0af44798
samover <samuel.overloop@gmail.com>
2015-11-02 09:23:37 +0000

    conform to ruby style guide

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,12 +2,8 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.new }
-  let(:case1) { {player_one: 'Rock', player_two: 'Paper'} }
-  let(:case2) { {player_one: 'Rock', player_two: 'Scissors'} }
-  let(:case3) { {player_one: 'Rock', player_two: 'Rock'} }
-  let(:case4) { {player_one: 'Scissors', player_two: 'Paper'} }
-  before :each do 
-    srand(67809)
+ before :each do 
+    srand(67_809)
   end
 
   context '#computer_move' do
