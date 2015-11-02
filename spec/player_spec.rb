29fc092b0383331ca41473f71e88c8ad6a948018
samover <samuel.overloop@gmail.com>
2015-11-02 11:50:02 +0000

    player #name unit testig green

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,10 @@
 require 'player'
 
 describe Player do
-
+  subject(:player) { described_class.new('Darth Vader') }
+  context '#name' do
+    it 'player has a name' do
+      expect(player.name).to eq 'Darth Vader'
+    end
+  end
 end
