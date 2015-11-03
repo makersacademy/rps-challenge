708a4a66d4148343be18d894724375bcb740b7aa
Deon Tan <deonjtan@gmail.com>
2015-11-03 23:56:49 +0000

    Opponent class refactored for implementation of FrequencyAnalyzer. Tests updated to reflect changes. Tests passing green.

--- lib/opponent.rb
+++ lib/opponent.rb
@@ -2,52 +2,38 @@ class Opponent
 
   HANDS = [:paper, :rock, :scissors]
   RELATIONSHIPS = {
-    paper: {beats: :rock, loses_to: :scissors},
-    rock: {beats: :scissors, loses_to: :paper},
-    scissors: {beats: :paper, loses_to: :rock}
+    paper: :rock,
+    rock: :scissors,
+    scissors: :paper
   }
 
-  def self.choose_hand(history = [])
-    Opponent.new(history).choose_hand
+  def self.choose_hand(history = [], frequency_analyzer_klass = FrequencyAnalyzer)
+    Opponent.new(history, frequency_analyzer_klass).choose_hand
   end
 
-  def initialize(history = [])
+  def initialize(history = [], frequency_analyzer_klass = FrequencyAnalyzer)
     @history = history
+    @frequency_analyzer_klass = frequency_analyzer_klass
   end
 
   def choose_hand
-    # pattern?
-    return recommend_hand(favoured_hand?.last) if favoured_hand?.first >= 0.5
-    random_hand
+    recommend_hand(run_frequency_analyzer) or random_hand
   end
+
+private
 
   def random_hand
     HANDS[rand(3)]
   end
 
-  def favoured_hand?
-    occurence_of_each_hand
-    hash_of_frequencies.max
-  end
-
-  def occurence_of_each_hand
-    @hand_count = []
-    HANDS.each {|hand| @hand_count << @history.count(hand)}
-    p @hand_count
-  end
-
-  def calculate_frequency
-    p @hand_count.map{|count| (count / @history.length.to_f).round(3) }
-  end
-
-  def hash_of_frequencies
-    (Array.new << calculate_frequency << HANDS).transpose.to_h
-  end
-
   def recommend_hand(hand)
-    RELATIONSHIPS[hand][:loses_to]
-    # RELATIONSHIPS.select{|key, lookups|
-    #   lookups == {beats: hand} }.to_a.flatten.first
+    return nil if hand.nil?
+    RELATIONSHIPS.find {|attack, wins_against| wins_against == hand}.first
   end
+
+  def run_frequency_analyzer
+    @frequency_analyzer_klass.run(@history)
+  end
+
 
 end
--- spec/opponent_spec.rb
+++ spec/opponent_spec.rb
@@ -1,27 +1,27 @@
 require 'opponent'
 
 describe Opponent do
-  subject(:opponent) {described_class.new}
-  xit "can choose a hand" do
-  allow(opponent).to receive(:rand).with(3).and_return(0)
-    expect(opponent.choose_hand).to eq :paper
+  subject(:opponent) {described_class}
+  let(:frequency_analyzer_klass) {double(:frequency_analyzer_klass)}
+
+  context "When choosing a hand the opponent:" do
+
+    it "can choose a random hand when there is no history" do
+      allow(opponent).to receive(:rand).with(3).and_return(0)
+      allow(frequency_analyzer_klass).to receive(:run).with(nil).and_return(nil)
+      expect(opponent.choose_hand(nil, frequency_analyzer_klass)).to eq :paper
+    end
+
+    it "passes history to FrequencyAnalyzer to improve its guess" do
+      expect(frequency_analyzer_klass).to receive(:run).with(:history)
+      opponent.choose_hand(:history, frequency_analyzer_klass)
+    end
+
+    it "guesses the statistically most favourable hand" do
+      allow(frequency_analyzer_klass).to receive(:run).with(:lots_of_papers).and_return(:paper)
+      expect(opponent.choose_hand(:lots_of_papers, frequency_analyzer_klass)).to eq :scissors
+    end
+
   end
-  # describe "#favoured_hand?" do
-  #   subject(:opponent) {described_class.new(favours_scissors)}
-  #   let(:favours_scissors) {[:scissors,:scissors,:scissors,:scissors,:scissors,:paper,:rock,:paper,:rock,:paper]}
-  #   it "inspects the history for a favoured hand" do
-  #     expect(subject.favoured_hand?).to include :scissors
-  #   end
-  #   it "counts the occurence of each hand in history" do
-  #     expect(subject.occurence_of_each_hand).to contain_exactly(3, 2, 5)
-  #   end
-  #   it "calculates the frequency of each hand" do
-  #     subject.occurence_of_each_hand
-  #     expect(subject.calculate_frequency).to contain_exactly(0.3, 0.2, 0.5)
-  #   end
-  #   it "recommends a hand if a hand appears 50% or more" do
-  #     expect(subject.choose_hand).to eq :rock
-  #   end
-  # end
 
 end
