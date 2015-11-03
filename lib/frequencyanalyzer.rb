d822bb689d0b5aa6bbd35712db78aefb6aba46ee
Deon Tan <deonjtan@gmail.com>
2015-11-03 23:08:05 +0000

    Frequency Analyzer green on tests

--- lib/frequencyanalyzer.rb
+++ lib/frequencyanalyzer.rb
@@ -1,17 +1,12 @@
 class FrequencyAnalyzer
   HANDS = [:paper, :rock, :scissors]
-  RELATIONSHIPS = {
-    paper: {beats: :rock, loses_to: :scissors},
-    rock: {beats: :scissors, loses_to: :paper},
-    scissors: {beats: :paper, loses_to: :rock}
-  }
 
   def self.run(history = [])
     FrequencyAnalyzer.new(history).run
   end
 
   def initialize(history = [])
-    @history.last = history
+    @history = history
   end
 
   def run
@@ -20,7 +15,6 @@ class FrequencyAnalyzer
   end
 
 private
-
 
   def mode_frequency(history)
     hash_of_frequencies(history).max.first / history.length.to_f
@@ -41,28 +35,3 @@ private
   end
 
 end
-
-
-history1 = [:paper, :paper, :paper, :paper, :paper, :paper, :rock, :scissors]
-history2 = [:paper, :paper, :paper, :rock, :rock, :rock, :scissors, :scissors, :scissors]
-history3 = [:]
-FrequencyAnalyzer.run(history)
-
-  # def favoured_hand?
-  #   occurence_of_each_hand
-  #   p hash_of_frequencies.max
-  # end
-
-  # def occurence_of_each_hand
-  #   @hand_count = []
-  #   HANDS.each {|hand| @hand_count << @history.count(hand)}
-  #   p @hand_count
-  # end
-
-  # def calculate_frequency
-  #   p @hand_count.map{|count| (count / @history.length.to_f).round(3) }
-  # end
-
-  # def hash_of_frequencies
-  #   (Array.new << calculate_frequency << HANDS).transpose.to_h
-  # end
--- spec/frequencyanalyzer_spec.rb
+++ spec/frequencyanalyzer_spec.rb
@@ -1,23 +1,26 @@
 require 'frequencyanalyzer'
 
 describe FrequencyAnalyzer do
-  subject(:frequency_analyzer) {described_class.new(favours_scissors)}
-    let(:favours_scissors) {[:scissors,:scissors,:scissors,:scissors,:scissors,:paper,:rock,:paper,:rock,:paper]}
-  context "when passed a history, the analyzer:" do
-      it "inspects the history for a favoured hand" do
-        expect(frequency_analyzer.favoured_hand?).to include :scissors
-      end
-      it "counts the occurence of each hand in history" do
-        expect(frequency_analyzer.occurence_of_each_hand).to contain_exactly(3, 2, 5)
-      end
-      it "calculates the frequency of each hand" do
-        subject.occurence_of_each_hand
-        expect(frequency_analyzer.calculate_frequency).to contain_exactly(0.3, 0.2, 0.5)
-      end
-      it "returns the most frequent hand if it appears over 50%" do
-        expect(frequency_analyzer.mode_hand).to eq :scissors
-      end
-    end
 
+  subject(:frequency_analyzer) {described_class}
+  let(:mode_paper) {[:paper, :paper, :paper, :paper, :paper, :paper, :rock, :scissors]}
+  let(:no_majority) {[:paper, :paper, :paper, :rock, :rock, :rock, :scissors, :scissors, :scissors]}
+  let(:empty_array) {[]}
+
+  context "when passed a history, the analyzer:" do
+
+      it "will calculate the mode" do
+        expect(frequency_analyzer.run(mode_paper)).to eq :paper
+      end
+
+      it "returns nil when there is no majority hand" do
+        expect(frequency_analyzer.run(no_majority)).to eq nil
+      end
+
+      it "returns nil when there is no history" do
+        expect(frequency_analyzer.run(empty_array)).to eq nil
+      end
+
+  end
 
 end
