6241219b41140360c90dd87add9d8c5a4fe4e680
Deon Tan <deonjtan@gmail.com>
2015-11-04 00:00:29 +0000

    All tests passing. Praise encapsulation.

--- spec/features/showdown_spec.rb
+++ spec/features/showdown_spec.rb
@@ -5,13 +5,13 @@ feature "reaching the 'Showdown' page" do
     click_button('Submit')
     click_button('Paper')
   end
-  xscenario "the page shows the player's choice" do
+  scenario "the page shows the player's choice" do
     expect(page).to have_content('You chose paper')
   end
-  xscenario "the page shows the opponent's choice" do
+  scenario "the page shows the opponent's choice" do
     expect(page).to have_content('Your opponent chose ')#stub
   end
-  xscenario "there is a 'play again' button" do
+  scenario "there is a 'play again' button" do
     find_button("play again").value
   end
 end
