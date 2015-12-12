899e589ac1f7d3f6e62ea9f4497f35757f343ab5
emmabeynon <emma.beynon@gmail.com>
2015-12-12 18:50:32 +0000

    Added a method to the Player class that allows the player to choose a weapon and assigns it to an instance variable.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,4 +2,16 @@ require 'player'
 
 describe Player do
   subject(:player_1) { described_class.new("Emma")}
+
+  describe 'default' do
+    it "returns the player's name" do
+      expect(player_1.name).to eq "Emma"
+    end
+  end
+
+  it "returns the player's weapon of choice" do
+    player_1.choose_weapon("Rock")
+    expect(player_1.weapon).to eq "Rock"
+  end
+
 end
