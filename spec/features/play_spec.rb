a13f71b3d13587e5244329f66faf74e6c6dfaf7a
emmabeynon <emma.beynon@gmail.com>
2015-12-12 19:29:27 +0000

    Created feature tests for various combinations of rock, paper and scissors.

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -24,4 +24,52 @@ describe 'User Stores - Play' do
       click_button "Let's find out..."
     end
   end
+
+  feature 'see the outcome of the game' do
+    scenario 'player chooses Rock, computer chooses Rock' do
+      sign_in_and_play_computer
+      choose_rock
+      expect(page).to have_content('Computer chose rock.')
+    end
+    scenario 'player chooses Rock, computer chooses Paper' do
+      sign_in_and_play_computer
+      choose_rock
+      expect(page).to have_content('Computer chose paper.')
+    end
+    scenario 'player chooses Rock, computer chooses Scissors' do
+      sign_in_and_play_computer
+      choose_rock
+      expect(page).to have_content('Computer chose scissors.')
+    end
+    scenario 'player chooses Paper, computer chooses Rock' do
+      sign_in_and_play_computer
+      choose_paper
+      expect(page).to have_content('Computer chose rock.')
+    end
+    scenario 'player chooses Paper, computer chooses Paper' do
+      sign_in_and_play_computer
+      choose_paper
+      expect(page).to have_content('Computer chose paper.')
+    end
+    scenario 'player chooses Paper, computer chooses Scissors' do
+      sign_in_and_play_computer
+      choose_paper
+      expect(page).to have_content('Computer chose scissors.')
+    end
+    scenario 'player chooses Scissors, computer chooses Rock' do
+      sign_in_and_play_computer
+      choose_scissors
+      expect(page).to have_content('Computer chose rock.')
+    end
+    scenario 'player chooses Scissors, computer chooses Paper' do
+      sign_in_and_play_computer
+      choose_scissors
+      expect(page).to have_content('Computer chose paper.')
+    end
+    scenario 'player chooses Scissors, computer chooses Scissors' do
+      sign_in_and_play_computer
+      choose_scissors
+      expect(page).to have_content('Computer chose scissors.')
+    end
+  end
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -3,3 +3,24 @@ def sign_in_and_play_computer
   fill_in :player_1_name, with: 'Emma'
   click_button 'Submit'
 end
+
+def choose_rock
+  visit('/play')
+  click_button 'Rock'
+  expect(page).to have_content('You chose rock. What will computer choose?')
+  click_button "Let's find out..."
+end
+
+def choose_paper
+  visit('/play')
+  click_button 'Paper'
+  expect(page).to have_content('You chose paper. What will computer choose?')
+  click_button "Let's find out..."
+end
+
+def choose_scissors
+  visit('/play')
+  click_button 'Scissors'
+  expect(page).to have_content('You chose scissors. What will computer choose?')
+  click_button "Let's find out..."
+end
