f56881bc88a9258585f36e7f4b9aef745ea89516
michaellennox <michaelctlennox@gmail.com>
2015-12-12 16:01:53 +0000

    Game passes unit tests for initializing with players

--- lib/game.rb
+++ lib/game.rb
@@ -2,4 +2,11 @@ class Game
 
   WEAPONS = [:rock, :paper, :scissors]
 
+  attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,19 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player, computer) }
   let(:player) { double :player }
   let(:computer) { double :computer }
 
-  
+  describe '#player_1' do
+    it 'should return the player' do
+      expect(game.player_1).to eq player
+    end
+  end
+
+  describe '#player_2' do
+    it 'should return the computer' do
+      expect(game.player_2).to eq computer
+    end
+  end
 end
