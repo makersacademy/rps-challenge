fe94ba1a6b7a16e52ab3fb971a2a7eb86f12ba8a
willibaur <williambautista@gmail.com>
2015-12-12 12:03:42 +0000

    User story 1
    As a marketeer
    So that I can see my name in lights
    I would like to register my name before playing an online game

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
   get '/' do
-    'Hello RPS!'
+    erb :index
   end
+
+  post '/names' do
+    $player_1 = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = $player_1
+    erb :play
+  end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -10,6 +10,8 @@ Capybara.default_driver= :selenium
 feature 'Enter name' do
   scenario 'submitting names' do
     visit('/')
-    fill_in :player
+    fill_in :player_1_name, with: 'David'
+    click_button 'Submit'
+    expect(page).to have_content 'David'
   end
 end
