fa6a3396e09c67e2055205000b43e472d4202290
michaellennox <michaelctlennox@gmail.com>
2015-12-12 15:57:58 +0000

    Extracts failure test for invalid weapon to private method

--- lib/player.rb
+++ lib/player.rb
@@ -8,8 +8,14 @@ class Player
   end
 
   def choose_weapon(weapon)
-    fail 'Not a valid weapon' unless Game::WEAPONS.include? weapon
+    fail 'Not a valid weapon' if invalid? weapon
     @weapon_choice = weapon
+  end
+
+  private
+
+  def invalid?(weapon)
+    !Game::WEAPONS.include? weapon
   end
 
 end
