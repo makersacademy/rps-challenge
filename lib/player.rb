755afc5b2ee719f2b1c16faaff0d96443028293e
michaellennox <michaelctlennox@gmail.com>
2015-12-12 15:49:42 +0000

    Passes new test cycle for player selecting weapon

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,14 @@
+require_relative 'game'
+
 class Player
   attr_reader :name
 
   def initialize(name)
     @name = name
+  end
+
+  def select_weapon(weapon)
+    fail 'Not a valid weapon' unless Game::WEAPONS.include? weapon
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,4 +9,10 @@ describe Player do
     end
   end
 
+  describe '#select_weapon' do
+    it 'should fail if weapon selected is not within the Game::WEAPONS array' do
+      expect{ player.select_weapon(:kitty) }.to raise_error 'Not a valid weapon'
+    end
+  end
+
 end
