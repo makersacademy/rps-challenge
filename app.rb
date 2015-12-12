6968baebc34d8eb5c95294d68ec9bf459438a119
Richard Moss <richard@englishtutorslive.com>
2015-12-12 19:06:38 +0000

    refactor for POST-redirect loop

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
 require 'sinatra/base'
 
 class Rps < Sinatra::Base
+	enable :sessions
+
   get '/' do
     erb :index
   end
 
  	post '/names' do 
- 		@name = params[:name]
+ 		session[:name] = params[:name]
+ 		redirect '/name'
+ 	end
+
+ 	get '/name' do
+ 		@name = session[:name]
  		erb :name
  	end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
