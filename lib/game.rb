8b3cbd3ed237175dea816ebee712bb5e78b2892d
emmabeynon <emma.beynon@gmail.com>
2015-12-12 22:06:41 +0000

    Added outcome method to calculate the winner of any possible combination of weapons. Created tests for this. Raises error if fewer than two weapons have been selected.

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,34 @@
 class Game
-  attr_reader :player_1, :player_2
+  attr_reader :player_1, :player_2, :winner, :weapons, :loser
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
+    @winner = nil
+    @loser = nil
+    @weapons = [player_1.weapon, player_2.weapon]
   end
 
+  def outcome
+    fail "Can't play: Two weapons need to be selected." if complete_weapons?
+    if weapons.include?("rock") && weapons.include?("paper")
+      @winner = "paper"
+      @loser = "rock"
+    elsif weapons.include?("rock") && weapons.include?("scissors")
+      @winner = "rock"
+      @loser = "scissors"
+    elsif weapons.include?("paper") && weapons.include?("scissors")
+      @winner = "scissors"
+      @loser = "paper"
+    else
+      @winner = nil
+      @loser = nil
+    end
+  end
+
+  private
+
+  def complete_weapons?
+    weapons.include?nil
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,33 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.new(player_1, player_2)}
-  let(:player_1) { double :player_1}
-  let(:player_2) { double :player_2}
+  let(:player_1) { double :player_1, weapon: "rock" }
+  let(:player_2) { double :player_2, weapon: "scissors" }
 
   describe 'default' do
     it 'accepts two players' do
       expect(game.player_1).not_to be_nil
       expect(game.player_2).not_to be_nil
+    end
+  end
+
+  describe '#weapons' do
+    it 'return the weapons used by both players' do
+      expect(game.weapons).to eq ["rock", "scissors"]
+    end
+  end
+
+  describe '#outcome' do
+    it 'returns the outcome of the two weapons' do
+      game.outcome
+      expect(game.winner).to eq "rock"
+    end
+
+    it 'raise an error if less than two weapons have been selected' do
+      allow(player_1).to receive(:weapon) {"rock"}
+      allow(player_2).to receive(:weapon) { nil }
+      message = "Can't play: Two weapons need to be selected."
+      expect{ game.outcome }.to raise_error message
     end
   end
 end
