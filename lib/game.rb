33204839e7088a97441974f3660e2a32b8537403
giamir <giamir.buoncristiani@gmail.com>
2015-12-12 13:08:25 +0000

    implement initialize method of Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,7 @@
 class Game
+  attr_reader :players
+
+  def initialize(player1, player2)
+    @players = [player1, player2]
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,21 @@
 require 'game'
 
 describe Game do
-  
+  let(:player1) { double :player1, is_computer?: false}
+  let(:player2) { double :player2, is_computer?: false}
+  let(:computer_player) { double :computer_player, is_computer?: true}
+  it 'stores the name of the players' do
+    game = described_class.new(player1, player2)
+    expect(game.players).to include player1, player2
+  end
+  context 'when playing by myself' do
+    before do
+      game = described_class.new(player1, computer_player)
+    end
+  end
+  context 'when playing with another player' do
+    before do
+      game = described_class.new(player1, player2)
+    end
+  end
 end
