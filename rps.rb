ac102c7394100b9e3dfc04e9cce4181975f47bf6
michaellennox <michaelctlennox@gmail.com>
2015-12-12 13:04:17 +0000

    Passes feature test for player login

--- rps.rb
+++ rps.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-    'Hello RPS!'
+    erb :login
+  end
+
+  post '/login' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb :play
   end
 
   # start the server if ruby file executed directly
