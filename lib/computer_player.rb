3736785f957c41cb72fb1fa3cd74cdd94e41844b
giamir <giamir.buoncristiani@gmail.com>
2015-12-12 12:52:02 +0000

    implement basic features of ComputerPlayer class

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,2 +1,6 @@
 class ComputerPlayer < Player
+  def initialize
+    @name = 'Computer'
+    @computer = true
+  end
 end
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -1,5 +1,14 @@
 require 'computer_player'
 
 describe ComputerPlayer do
-  
+  let(:computer_player) { described_class.new }
+  it 'has name Computer' do
+    expect(computer_player.name).to eq 'Computer'
+  end
+  describe '#is_computer?' do
+    it 'returns true' do
+      expect(computer_player.is_computer?).to be true
+    end
+  end
+
 end
