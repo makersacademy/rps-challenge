da0ef6ca0d2824e427e1f1e3c11d3718cce0cb58
michaellennox <michaelctlennox@gmail.com>
2015-12-12 15:43:23 +0000

    Test cycle for random selection of computer's weapon

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
 class Game
 
+  WEAPONS = [:rock, :paper, :scissors]
+
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,4 +2,11 @@ require 'computer'
 
 describe Computer do
   subject(:computer) { described_class.new }
+
+  describe '#choose_weapon' do
+    it 'should choose randomly from the Game::WEAPONS array' do
+      expect(Game::WEAPONS).to receive(:sample)
+      computer.choose_weapon
+    end
+  end
 end
