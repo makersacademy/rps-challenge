4428bdcf3da88dedfe3442dc1f651a2288109f83
giamir <giamir.buoncristiani@gmail.com>
2015-12-13 22:21:08 +0000

    define and implement beats? method of Element class\

--- lib/element.rb
+++ lib/element.rb
@@ -1,9 +1,14 @@
 class Element
 
+  RULES = {rock: :scissors,paper: :rock,scissors: :paper}
+
   attr_reader :type
 
   def initialize(type)
     @type = type.to_sym
   end
 
+  def beats?(other)
+    RULES[type] == other.type
+  end
 end
--- spec/element_spec.rb
+++ spec/element_spec.rb
@@ -2,7 +2,13 @@ require 'element'
 
 describe Element do
   let(:element) { described_class.new 'rock' }
+  let(:other) { described_class.new 'scissors' }
   it 'has been assigned a type at the initialization' do
     expect(element.type).to eq :rock
+  end
+  describe '#beats?' do
+    it 'returns true if the element type beats the one of the other element' do
+      expect(element.beats?(other)).to be true
+    end
   end
 end
