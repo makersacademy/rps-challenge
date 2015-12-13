7a9aa44a84b3e7106e31f04c393b84398358b5c6
Jamie Brown <jamie@Jamies-MacBook-Pro.local>
2015-12-13 22:47:15 +0000

    add sign in functionality

--- app.rb
+++ app.rb
@@ -1,10 +1,24 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
-  get '/' do
-    'Hello RPS!'
+  configure do
+    enable :sessions
   end
 
+
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/game'
+  end
+
+  get '/game' do
+    @name = session[:name]
+    erb :game
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
