807674036eb339bcaf26a01454805b01c5e4ddb8
giamir <giamir.buoncristiani@gmail.com>
2015-12-13 14:53:01 +0000

    implement play method of Game class

--- lib/game.rb
+++ lib/game.rb
@@ -4,11 +4,14 @@ class Game
   extend Forwardable
   def_delegator :current_turn, :name, :current_turn_name
 
-  attr_reader :players, :current_turn
+  DRAW_MSG = 'Draw, there is no winner'
+  WIN_MSG = ' wins the game'
+  attr_reader :players, :current_turn, :message
 
   def initialize(player1, player2)
     @players = [player1, player2]
     @current_turn = player1
+    @message = ''
   end
 
   def first_player_name
@@ -19,7 +22,38 @@ class Game
     players.last.name
   end
 
+  def play(element = nil)
+    @message << current_turn.assign_element(element) << '<br>'
+    return game_result if game_over?
+    switch
+    play if current_turn.is_computer?
+  end
+
+  private
+
+  def game_result
+    draw? ? @message << DRAW_MSG : @message << winner.name << WIN_MSG
+  end
+
+  def game_over?
+    !players.detect { |player| !player.element }
+  end
+
+  def draw?
+    players[0].element == players[1].element
+  end
+
   def switch
     @current_turn = players.detect { |player| player != current_turn }
+  end
+
+  def winner
+    if players[0].element == 'rock' && players[1].element == 'scissors' ||
+       players[0].element == 'scissors' && players[1].element == 'paper' ||
+       players[0].element == 'paper' && players[1].element == 'rock'
+      players[0]
+    else
+      players[1]
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,8 @@
 require 'game'
 
 describe Game do
-  let(:player1) { double :player1, is_computer?: false, name: 'name1'}
-  let(:player2) { double :player2, is_computer?: false, name: 'name2'}
-  let(:computer_player) { double :computer_player, is_computer?: true}
+  let(:player1) { double :player1, is_computer?: false, name: 'name1', element: nil}
+  let(:player2) { double :player2, is_computer?: false, name: 'name2', element: nil}
   let(:game) { described_class.new(player1, player2) }
   it 'stores the name of the players' do
     expect(game.players).to include player1, player2
@@ -26,16 +25,57 @@ describe Game do
       expect(game.current_turn_name).to eq 'name1'
     end
   end
-  describe '#switch' do
-    it 'switch the turn' do
-      game.switch
-      expect(game.current_turn).to eq player2
+  describe '#play' do
+    before do
+      allow(player1).to receive(:element).and_return('rock')
+      allow(player2).to receive(:element)
+      allow(player1).to receive(:assign_element).and_return 'name1 chose rock'
     end
-  end
-  context 'when playing by myself' do
-    let(:game) { described_class.new(player1, computer_player) }
-  end
-  context 'when playing with another player' do
-    let(:game) { described_class.new(player1, player2) }
+    it 'adds a string of last player assignment element to message' do
+      allow(player1).to receive(:assign_element).and_return 'name1 chose rock'
+      msg = 'name1 chose rock<br>'
+      game.play('rock')
+      expect(game.message).to eq msg
+    end
+    context 'when the game is over' do
+      context 'in draw' do
+        it 'adds a string that game ended in draw to message' do
+          msg = 'name1 chose rock<br>name2 chose rock<br>Draw, there is no winner'
+          game.play('rock')
+          allow(player2).to receive(:element).and_return('rock')
+          allow(player2).to receive(:assign_element).and_return 'name2 chose rock'
+          game.play('rock')
+          expect(game.message).to eq msg
+        end
+      end
+      context 'with a winner' do
+        it 'adds a string that game ended with a winner to message' do
+          msg = 'name1 chose rock<br>name2 chose scissors<br>name1 wins the game'
+          game.play('rock')
+          allow(player2).to receive(:element).and_return('scissors')
+          allow(player2).to receive(:assign_element).and_return 'name2 chose scissors'
+          game.play('scissors')
+          expect(game.message).to eq msg
+        end
+      end
+    end
+    context 'when the game is not over' do
+      it 'switch the turn' do
+        game.play('rock')
+        expect(game.current_turn).to eq player2
+      end
+      context 'and playing by myself' do
+        before do
+          allow(player2).to receive(:is_computer?).and_return true
+          allow(player2).to receive(:element)
+        end
+        describe '#play' do
+          it 'call itself automatically' do
+            expect(player2).to receive(:assign_element).and_return ''
+            game.play('rock')
+          end
+        end
+      end
+    end
   end
 end
