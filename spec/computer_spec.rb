10a21686aad4243e26c731c49ac7616d1639c7aa
Thomas Bradley <bradley.tom.r@gmail.com>
2015-12-14 00:31:46 +0000

    Adds result view and method but unable to calculate result correctly

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -6,6 +6,7 @@ describe Computer do
   describe '#weapon_choice' do
     it 'randomly selects a weapon' do
       allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
+      computer.choose_weapon
       expect(computer.weapon_choice).to eq :scissors
     end
   end
