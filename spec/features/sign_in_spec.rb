c26c3019dcd456938c3d9e8caa355a3d0cc3ad2f
edwardkerry <edwardkerry@gmail.com>
2015-12-14 18:36:49 +0000

    Basic CSS added

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,7 +2,7 @@ feature 'Sign in' do
   scenario 'sign in' do
     visit('/')
     fill_in :player_1, with: 'Ed'
-    expect(page).to have_content 'Welcome to RPS'
+    expect(page).to have_content 'Welcome'
   end
 
   scenario 'names on vs screen' do
