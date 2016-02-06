1cbefc535d62e744ce22b3d1090c2a4a3812be14
Iryna Howarth <irynahowarth@gmail.com>
2016-02-06 14:38:55 +0000

    add register player's name

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+
+  use Rack::Session::Pool, :expire_after => 2592000
+
   get '/' do
-    'Hello RPS!'
+    erb :index
   end
+
+  post '/names' do
+    session[:player1_name] = params[:player1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1_name = session[:player1_name]
+    erb :play
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
