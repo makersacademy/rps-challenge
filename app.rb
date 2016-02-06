126b30da9b32340e575d8a9e98e63e84d815d4f4
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-02-06 23:22:58 +0100

    Name can be entered and displayed#

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
+  end
+
+  post '/set_name' do
+    session[:player_name] = params[:player_name]
+    redirect '/match'
+  end
+
+  get '/match' do
+    @name = session[:player_name]
+    erb(:match)
   end
 
   # start the server if ruby file executed directly
