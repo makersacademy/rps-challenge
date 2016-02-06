1e600c92546ac25b30786751066a1ee627d23403
Rufus Raghunath <rufusraghunath@gmail.com>
2016-02-06 18:10:40 +0000

    added proper webhelpers to let build pass

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,5 +1,12 @@
 def sign_in_pvc
   visit('/')
+  find('#PvC').click
+  fill_in :player_name, with: 'Rufus'
+  click_button 'Enter'
+end
+
+def sign_in_pvp
+  visit('/')
   find('#PvP').click
   fill_in :player1, with: 'Tom'
   fill_in :player2, with: 'Jerry'
