d803737ee23cc0550f71439182495e3f79f7650c
Henry Gordon <hwgordon247@gmail.com>
2016-02-06 17:00:51 +0000

    meets minimum user requirements and is refactored

--- lib/game.rb
+++ lib/game.rb
@@ -5,24 +5,26 @@ class Game
 
   attr_reader :player, :computer
 
-
   def initialize(player, computer)
     @player = player
     @computer = computer
   end
 
-
   def evaluate_winner
+    return "Noone" if draw?
+    player_results ? player.name : "Computer"
+  end
 
+  private
+  def draw?
+    player.choice == computer.choice
+  end
+
+  def player_results
     result_combinations = {rock: {paper: false, scissors: true},
       paper: {scissors: false, rock: true},
       scissors: {rock: false, paper: true}
     }
-    return "Noone" if player.choice == computer.choice
-    player_results  = result_combinations[player.choice][computer.choice]
-    return player.name if player_results == true
-    return "Computer" if player_results == false
-
+    result_combinations[player.choice][computer.choice]
   end
-
 end
