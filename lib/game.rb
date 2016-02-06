522906521106efaa6e5e76b60eadf037643dfc5d
Alex Avlonitis <avlonitis@msn.com>
2016-02-06 15:42:09 +0000

    refactored the select the winner method

--- lib/game.rb
+++ lib/game.rb
@@ -20,13 +20,15 @@ class Game
   private
 
   def select_winner
-    return 0 if @player1.c_rps == :rock && @player2.c_rps == :rock
-    return 0 if @player1.c_rps == :paper && @player2.c_rps == :paper
-    return 0 if @player1.c_rps == :scissors && @player2.c_rps == :scissors
-    return @player1 if @player1.c_rps == :rock && @player2.c_rps == :scissors
-    return @player1 if @player1.c_rps == :paper && @player2.c_rps == :rock
-    return @player1 if @player1.c_rps == :scissors && @player2.c_rps == :paper
-    @player2
+    rock_to_scissors = @player1.c_rps == :rock && @player2.c_rps == :scissors
+    paper_to_rock = @player1.c_rps == :paper && @player2.c_rps == :rock
+    scissors_to_paper = @player1.c_rps == :scissors && @player2.c_rps == :paper
+    case
+      when @player1.c_rps == @player2.c_rps then 0
+      when rock_to_scissors || paper_to_rock || scissors_to_paper then @player1
+    else
+      @player2
+    end
   end
 
   def reset
