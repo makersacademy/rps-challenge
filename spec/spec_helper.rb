f504f73f64dd1caa76260c14616ea852f8cb5380
Heather <campbell.hf@gmail.com>
2016-02-06 14:52:17 +0000

    user able to register name and appear on page - feature test passing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # ensure the following is AT THE TOP of your spec_helper.rb to get test coverage stats
 require 'coveralls'
 require 'simplecov'
+require 'capybara/dsl'
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
@@ -8,9 +9,13 @@ SimpleCov.formatters = [
 ]
 # replace following line with SimpleCov.start to get coverage stats locally
 # SimpleCov.start
-Coveralls.wear! 
+Coveralls.wear!
 
 # run `open coverage/index.html` from the command line to view details
 
 require 'byebug'
 require 'capybara/rspec'
+
+ENV['RACK_ENV'] = 'test'
+require File.join(File.dirname(__FILE__), '..', '/app/app.rb')
+Capybara.app = RPS
