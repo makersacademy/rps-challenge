11ef66c8f741798a1a00cb9ba9ec2d3aac02d0d7
Rebecca Piper <piper.rebecca93@gmail.com>
2016-02-06 17:00:17 +0000

    Removed Capybara::DSL and selenium-webdriver that I had used for testing and viewing the web app on my computer

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,9 +1,4 @@
-require 'capybara/dsl'
-require 'selenium-webdriver'
-
 def start_game
-  include Capybara::DSL
-  Capybara.default_driver = :selenium
   visit('/')
   fill_in('player_name', with: 'Rebecca')
   click_button('Go!')
