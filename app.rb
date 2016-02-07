41a3d30aa616802a542932c90470b45aba79be24
David Parry <drjparry@gmail.com>
2016-02-07 23:14:43 +0100

    Added multiplayer functionality

--- app.rb
+++ app.rb
@@ -8,24 +8,64 @@ class RPS < Sinatra::Base
 
 
   get '/' do
-    erb :index
+    erb :player
   end
 
-  post '/name' do
+  get '/one_player' do
+    erb :one_player
+  end
+
+  get '/two_players' do
+    erb :two_players
+  end
+
+  post '/one_player_name' do
     session[:player_name] = params['player_name']
-    redirect '/play'
+    redirect '/play_one_player'
   end
 
-  get '/play' do
+  post '/two_player_names' do
+    session[:player_one_name] = params['player_one_name']
+    session[:player_two_name] = params['player_two_name']
+    @player_1 = Player.new(session[:player_one_name])
+    @player_2 = Player.new(session[:player_two_name])
+    erb :set_up
+  end
+
+
+  get '/play_one_player' do
     @player_1 = Player.new(session[:player_name])
     @player_2 = Computer.new
-    erb :play
+    erb :play_one
   end
 
-  post '/result' do
+  get '/play_two_players' do
+    @player_1 = Player.new(session[:player_one_name])
+    @player_2 = Player.new(session[:player_two_name])
+    erb :player_one_choice
+  end
+
+  post '/player_two_choice' do
+    session[:player_one_choice] = params['choice']
+    @player_2 = Player.new(session[:player_two_name])
+    erb :player_two_choice
+  end
+
+
+  post '/one_player_result' do
     @player_1 = Player.new(session[:player_name])
     @player_2 = Computer.new
     @player_1.choose(params['choice'])
+    $game = Game.new(@player_1, @player_2)
+    $game.play
+    erb :result
+  end
+
+  post '/two_player_result' do
+    @player_1 = Player.new(session[:player_one_name])
+    @player_2 = Player.new(session[:player_two_name])
+    @player_1.choose(params['choice'])
+    @player_2.choose(session[:player_one_choice])
     $game = Game.new(@player_1, @player_2)
     $game.play
     erb :result
--- spec/feature/enter_names_spec.rb
+++ spec/feature/enter_names_spec.rb
@@ -1,52 +1,69 @@
 require 'capybara/dsl'
 require 'web_helpers'
 
-feature "User story => RPS" do
+
+
+feature "User storys => RPS" do
 
 # As a marketeer
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
 
-  scenario "allow player to enter name" do
-    visit '/'
-    fill_in('player_name', with: 'Dave')
-    click_button('Submit')
-    expect(page).to have_content('Dave vs. The Computer')
-  end
-
 # As a marketeer
 # So that I can enjoy myself away from the daily grind
 # I would like to be able to play rock/paper/scissors
-  scenario "allow player to choose Rock" do
-    sign_in_and_play
-    click_button('rock')
-    expect(page).to have_content('Dave chose rock')
+
+  describe "Single Player Mode" do
+
+    scenario "allow player to enter name" do
+      sign_in_and_play
+      expect(page).to have_content('Dave vs. The Computer')
+    end
+
+      scenario "allow player to choose Rock" do
+      sign_in_and_play
+      click_button('rock')
+      expect(page).to have_content('Dave chose rock')
+    end
+
+    scenario "allow player to choose Paper" do
+      sign_in_and_play
+      click_button('paper')
+      expect(page).to have_content('Dave chose paper')
+    end
+
+    scenario "allow player to choose Scissors" do
+      sign_in_and_play
+      click_button('scissors')
+      expect(page).to have_content('Dave chose scissors')
+    end
+
+    scenario "allow computer to choose an option" do
+      allow_any_instance_of(Computer).to receive(:choice).and_return('rock')
+      sign_in_and_play
+      click_button('scissors')
+      expect(page).to have_content("The Computer chose rock")
+    end
+
+    scenario 'Declare a winner' do
+      allow_any_instance_of(Computer).to receive(:choice).and_return('rock')
+      sign_in_and_play
+      click_button('paper')
+      expect(page).to have_content('Dave wins!')
+    end
   end
 
-  scenario "allow player to choose Paper" do
-    sign_in_and_play
-    click_button('paper')
-    expect(page).to have_content('Dave chose paper')
+  describe 'Multiplayer' do
+
+      scenario "allow two players to enter names" do
+      sign_in_and_play_two
+      expect(page).to have_content('Dave vs. Mike')
   end
 
-  scenario "allow player to choose Scissors" do
-    sign_in_and_play
-    click_button('scissors')
-    expect(page).to have_content('Dave chose scissors')
-  end
-
-  scenario "allow computer to choose an option" do
-    allow_any_instance_of(Computer).to receive(:choice).and_return('rock')
-    sign_in_and_play
-    click_button('scissors')
-    expect(page).to have_content("The Computer chose rock")
-  end
-
-  scenario 'Declare a winner' do
-    allow_any_instance_of(Computer).to receive(:choice).and_return('rock')
-    sign_in_and_play
-    click_button('paper')
-    expect(page).to have_content('Dave wins!')
+    scenario "declare a winner in two player mode" do
+      full_set_up
+      expect(page).to have_content('Mike wins')
+    end
   end
 
 end
