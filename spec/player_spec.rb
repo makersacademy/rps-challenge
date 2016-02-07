c74553c91d0bfee6bf5c6091e9196280be3e7b65
Alex Avlonitis <avlonitis@msn.com>
2016-02-07 15:06:38 +0000

    added anedge case when a user selects a rps choice

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,13 +3,15 @@ require_relative '../lib/player'
 describe Player do
   subject(:player) {described_class.new}
 
-  let(:rock) {double :rock}
-  let(:paper) {double :paper}
-  let(:scissors) {double :scissors}
 
   describe '#select_rps' do
     it "sets an rps for the player" do
-      expect {player.select_rps(rock)}.to change(player, :c_rps).from(nil).to(rock)
+      expect {player.select_rps(:rock)}.to change(player, :c_rps).from(nil).to(:rock)
+    end
+
+    it "raises an error if you send wrong choice" do
+        error = "Wrong choice, select one of the following:#{Player::CHOICES}"
+        expect {player.select_rps(:banana)}.to raise_error error
     end
   end
 
