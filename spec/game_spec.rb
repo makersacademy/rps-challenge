bc6c5a58b5e60093f55175e2c2318b935c778813
Rufus Raghunath <rufusraghunath@gmail.com>
2016-02-07 19:57:21 +0000

    added some more unit testing, test coverage is bad on lib/game.rb because not every line of the case statement is tested (would probably be unneccessary

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,8 +4,9 @@ describe Game do
 
   # stub random choice, then check for outcomes
 
-  let(:player1) {double :player}
-  let(:player2) {double :player}
+  let(:player1) {double :player, choice: :paper}
+  let(:player2) {double :player, choice: :paper}
+  let(:player3) {double :player, choice: :rock}
 
   subject(:game) {described_class.new(player1, player2)}
 
@@ -13,4 +14,22 @@ describe Game do
     expect(game.player1).to eq player1
   end
 
+  describe '#calculate_winner' do
+    it 'returns draw when choices are equal' do
+      game.calculate_winner
+      expect(game.draw).to be true
+    end
+
+    let(:game2) {described_class.new(player1, player3)}
+
+    it 'returns winner when choices are not equal' do
+      game2.calculate_winner
+      expect(game2.winner).to eq player1
+    end
+
+    it 'returns appropriate outcome messages' do
+      game2.calculate_winner
+      expect(game2.outcome).to eq "Paper wraps rock."
+    end
+  end
 end
--- spec/pc_player_spec.rb
+++ spec/pc_player_spec.rb
@@ -2,7 +2,14 @@ require 'pc_player'
 
 describe PCPlayer do
 
-  it{is_expected.to respond_to(:generate_choice)}
+  subject(:pc_player) {described_class.new}
+
   it{is_expected.to respond_to(:choice)}
+
+  describe '#generate_choice' do
+    it 'generates a random RPS weapon' do
+      expect([:rock, :paper, :scissors, :lizard, :Spock]).to include pc_player.generate_choice
+    end
+  end
 
 end
