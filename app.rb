a93e793241d2b022c2f3b004a1e9346c094db3ff
David Parry <drjparry@gmail.com>
2016-02-07 12:42:03 +0100

    Player can now choose between Rock, Paper, scissors

--- app.rb
+++ app.rb
@@ -1,19 +1,29 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+ enable :sessions
+
+
   get '/' do
     erb :index
   end
 
   post '/name' do
-    $player = params['player_name']
+    session[:player_name] = params['player_name']
     redirect '/play'
   end
 
   get '/play' do
-    @player = $player
+    @player = session[:player_name]
     erb :play
   end
+
+  get '/result' do
+    @choice = params['choice']
+    erb :result
+  end
+
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/feature/enter_names_spec.rb
+++ spec/feature/enter_names_spec.rb
@@ -1,4 +1,5 @@
 require 'capybara/dsl'
+require 'web_helpers'
 
 feature "User story => RPS" do
 
@@ -12,5 +13,27 @@ feature "User story => RPS" do
     click_button('Submit')
     expect(page).to have_content('Dave vs. Computer')
   end
+
+# As a marketeer
+# So that I can enjoy myself away from the daily grind
+# I would like to be able to play rock/paper/scissors
+  scenario "allow player to choose Rock" do
+    sign_in_and_play
+    click_button('Rock')
+    expect(page).to have_content('You chose Rock')
+  end
+
+  scenario "allow player to choose Paper" do
+    sign_in_and_play
+    click_button('Paper')
+    expect(page).to have_content('You chose Paper')
+  end
+
+  scenario "allow player to choose Scissors" do
+    sign_in_and_play
+    click_button('Scissors')
+    expect(page).to have_content('You chose Scissors')
+  end
+
 end
 
