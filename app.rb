050e72aa28620102e2356b7b18faefeef97c3b21
valogopedina <valentina.monetta@tiscali.it>
2016-02-07 00:33:19 +0000

    Raw implementation, need some attention tomorrow

--- app.rb
+++ app.rb
@@ -1,8 +1,42 @@
 require 'sinatra/base'
 
 class Rps < Sinatra::Base
+  enable :sessions
+
+  #def initialize
+  #  @game = Game.new
+  #end
+
   get '/' do
-    'Hello Rps!'
+    erb :index
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player_name]
+    redirect '/game'
+  end
+
+  get '/game' do
+    if !params[:choice].nil?
+      #result = @game.play(params[:choice])
+      computer = ['rock', 'paper', 'scissors'].sample
+      choice = params[:choice]
+      if choice == 'rock'
+        result = 'tied' if computer == 'rock'
+        result = 'lost' if computer == 'paper'
+        result = 'won' if computer == 'scissors'
+      elsif choice == 'paper'
+        result = 'tied' if computer == 'paper'
+        result = 'lost' if computer == 'scissors'
+        result = 'won' if computer == 'rock'
+      else
+        result = 'tied' if computer == 'scissors'
+        result = 'lost' if computer == 'rock'
+        result = 'won' if computer == 'paper'
+      end
+      session[:result] = "The result is .... You #{result}"
+    end
+    erb :play
   end
 
   # start the server if ruby file executed directly
---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,79 @@
+
+require 'capybara/dsl'
+require 'selenium-webdriver'
+
+
+def browser_view
+  include Capybara::DSL
+  Capybara.default_driver = :selenium
+end
+
+def current_path_info
+  current_url.sub(%r{.*?://},'')[%r{[/\?\#].*}] || '/'
+end
+
+feature 'Enter name' do
+  scenario 'submitting names' do
+    #browser_view
+    visit('/')
+    fill_in :player_name, with: 'Valentina'
+    click_button 'Submit'
+    expect(page).to have_content 'Valentina, you are registered!'
+  end
+end
+
+feature 'Starting game' do
+  scenario 'player starts playing' do
+    visit('/')
+    fill_in :player_name, with: 'Valentina'
+    click_button 'Submit'
+    expect(page).to have_content 'Start playing!'
+  end
+end
+
+feature 'Being able to pick a choice' do
+  scenario 'player can make a choice' do
+    visit('/')
+    fill_in :player_name, with: 'Valentina'
+    click_button 'Submit'
+    expect(page).to have_content 'Pick your choice'
+    expect(page).to have_button('Rock')
+  end
+end
+
+feature 'Making a choice' do
+  scenario 'player makes a choice' do
+    visit('/')
+    fill_in :player_name, with: 'Valentina'
+    click_button 'Submit'
+    click_button 'Rock'
+    expect(current_path_info).to be == "/game?choice=rock"
+  end
+
+  scenario "When I submit 'Rock' I am told if I have won" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('scissors')
+    visit '/'
+    fill_in('player_name', with: 'Valentina')
+    click_button 'Submit'
+    click_button 'Rock'
+    expect(page).to have_content "The result is .... You won"
+  end
+
+  scenario "When I submit 'Rock' I am told if I have lost" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('paper')
+    visit '/'
+    fill_in('player_name', with: 'Valentina')
+    click_button 'Submit'
+    click_button 'Rock'
+    expect(page).to have_content "The result is .... You lost"
+  end
+
+  scenario "When I submit 'Rock' I am told if I have tied" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('rock')
+    visit '/'
+    fill_in('player_name', with: 'Valentina')
+    click_button 'Submit'
+    click_button 'Rock'
+    expect(page).to have_content "The result is .... You tied"
+  end
+end
