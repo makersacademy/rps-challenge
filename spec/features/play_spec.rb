24546f03a5dbaad5846911c78801340f6c835361
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-02-07 01:12:59 +0100

    Added play_spec and test 'options visible' passes

---
+++ spec/features/play_spec.rb
@@ -0,0 +1,19 @@
+# As a marketeer
+# So that I can enjoy myself away from the daily grind
+# I would like to be able to play rock/paper/scissors
+
+feature 'playing a game' do
+  before do
+    visit('/')
+    fill_in(:player_name, with: 'Marketeer')
+    click_button 'Submit'
+  end
+
+  scenario 'options visible' do
+    expect(page).to have_content 'Rock'
+    expect(page).to have_content 'Paper'
+    expect(page).to have_content 'Scissors'
+
+  end
+end
+  
