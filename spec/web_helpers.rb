83cb355081fd954b183ebf9d8663bec47b378e12
David Parry <drjparry@gmail.com>
2016-02-08 11:43:33 +0100

    Fixed a silly bug

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play_two
   visit '/'
-  click_button('Two Players')
+  click_button('Human')
   fill_in('player_one_name', with: 'Dave')
   fill_in('player_two_name', with: 'Mike')
   click_button('Submit')
@@ -8,14 +8,14 @@ end
 
 def sign_in_and_play
   visit '/'
-  click_button('One Player')
+  click_button('Computer')
   fill_in('player_name', with: 'Dave')
   click_button('Submit')
 end
 
 def full_set_up
   visit '/'
-  click_button('Two Players')
+  click_button('Human')
   fill_in('player_one_name', with: 'Dave')
   fill_in('player_two_name', with: 'Mike')
   click_button('Submit')
