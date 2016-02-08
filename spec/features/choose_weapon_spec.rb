558bf6c7cb83526c6e6e4ee6d1d878182deba8c2
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-08 09:07:25 +0000

    tried to make it a bit prettier

--- spec/features/choose_weapon_spec.rb
+++ spec/features/choose_weapon_spec.rb
@@ -9,7 +9,7 @@ feature 'Choose weapon' do
     sign_in_and_play
     choose('paper')
     click_button('Fight!')
-    expect(page).to have_content 'Arnold chose paper'
+    expect(page).to have_content 'Arnold chose Paper'
   end
 
   scenario 'selecting a weapon for player 2 in multiplayer' do
@@ -25,7 +25,7 @@ feature 'Choose weapon' do
     click_button('Fight!')
     choose('rock')
     click_button('Fight!')
-    expect(page).to have_content 'Arnold chose paper'
-    expect(page).to have_content 'Abram chose rock'
+    expect(page).to have_content 'Arnold chose Paper'
+    expect(page).to have_content 'Abram chose Rock'
   end
 end
