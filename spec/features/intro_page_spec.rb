df8150b7d9ffcf0f74b12ff7748da79a2bd96639
Henry Gordon <hwgordon247@gmail.com>
2016-02-08 10:55:14 +0000

    adding feature test for all outcomes

--- spec/features/intro_page_spec.rb
+++ spec/features/intro_page_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 feature 'introducing page' do
 
-  scenario 'there is the title of the game on the intro page' do
+  before(:example) do
     visit '/'
+  end
+
+  scenario 'there is the title of the game on the intro page' do
     expect(page).to have_content "Welcome to Rock Paper Scissors"
   end
 
   scenario 'asks player to input their name' do
-    visit '/'
     expect(page).to have_content "Please enter your name:"
   end
 end
--- spec/features/result_page_spec.rb
+++ spec/features/result_page_spec.rb
@@ -2,23 +2,32 @@ require 'spec_helper'
 require 'web_helper'
 
 feature 'result page' do
-  scenario 'displays the winner of the game' do
+
+  before(:example) do
     allow(Kernel).to receive(:rand).and_return(2)
     sign_in
+  end
+  scenario 'displays Harry as the winner of the game' do
     click_button 'Rock'
     expect(page).to have_content 'Harry wins!!!'
   end
 
+  scenario 'displays Computer as the winner of the game' do
+    click_button 'Paper'
+    expect(page).to have_content 'Computer wins!!!'
+  end
+
+  scenario 'displays a draw' do
+    click_button 'Scissors'
+    expect(page).to have_content 'Noone wins!!!'
+  end
+
   scenario 'displays the player\'s choice' do
-    allow(Kernel).to receive(:rand).and_return(2)
-    sign_in
     click_button 'Rock'
     expect(page).to have_content 'Harry chooses rock'
   end
 
   scenario 'displays the computer\'s choice' do
-    allow(Kernel).to receive(:rand).and_return(2)
-    sign_in
     click_button 'Rock'
     expect(page).to have_content 'Computer chooses scissors'
   end
