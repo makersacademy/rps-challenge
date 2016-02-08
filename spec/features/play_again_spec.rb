f8294c45ea41d671278e566af8e0219a3279a086
Tom Pickard <tcpickard94@gmail.com>
2016-02-08 19:22:18 +0000

    Update feature tests

--- spec/features/play_again_spec.rb
+++ spec/features/play_again_spec.rb
@@ -1,8 +1,8 @@
 feature 'can play again' do
   scenario 'user can navigate back to the play page after each game' do
     sign_in
-    click_link "Rock"
-    click_link "Play Again"
+    click_button "Rock"
+    click_button "Play Again"
     expect(page).to have_content "Dave vs. Computer"
   end
 end
--- spec/features/select_options_spec.rb
+++ spec/features/select_options_spec.rb
@@ -1,19 +1,19 @@
 feature 'can select options for play' do
   scenario 'rock' do
     sign_in
-    click_link 'Rock'
+    click_button 'Rock'
     expect(page).to have_content "You play: Rock"
   end
   
   scenario 'paper' do
     sign_in
-    click_link 'Paper'
+    click_button 'Paper'
     expect(page).to have_content "You play: Paper"
   end
   
   scenario 'scissors' do
     sign_in
-    click_link 'Scissors'
+    click_button 'Scissors'
     expect(page).to have_content "You play: Scissors"
   end
 end
