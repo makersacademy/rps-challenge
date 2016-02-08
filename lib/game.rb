067db95b7bedcfec38f7903e54a77d52aff6b288
Vicky Gray <vickygray26@hotmail.com>
2016-02-08 11:23:12 +0000

    refactor

--- lib/game.rb
+++ lib/game.rb
@@ -6,27 +6,27 @@ class Game
 
   WEAPONS = ['Rock', 'Paper', 'Scissors']
 
-  def initialize(player, computer = Computer.new)
-    @player = player
-    @computer = computer
+  def initialize(player_klass, computer_klass = Computer.new)
+    @player = player_klass
+    @computer = computer_klass
     @wins = [['Scissors', 'Paper'],
     ['Paper', 'Rock'],
     ['Rock', 'Scissors']]
   end
 
   def game_winner
-    check_winner(result)
+    check_winner(choices)
   end
 
   private
 
-  def result
-    @result = [@computer.weapon, @player.choice]
+  def choices
+    @choices = [@computer.weapon, @player.choice]
   end
 
-  def check_winner(result)
+  def check_winner(choices)
     return "It's a draw!" if @computer.weapon == @player.choice
-    return "Computer wins!" if @wins.include?(@result)
+    return "Computer wins!" if @wins.include?(@choices)
     return "You win!"
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,25 +13,27 @@ describe Game do
   end
 
   describe '#check_winner' do
+
+    before do
+      @new_game = Game.new(player)
+    end
+
     it 'confirms if player won the game' do
-      new_game = Game.new(player)
-      allow(new_game.computer).to receive(:weapon).and_return('Scissors')
-      allow(new_game.player).to receive(:choice).and_return('Rock')
-      expect(new_game.game_winner).to eq 'You win!'
+      allow(@new_game.computer).to receive(:weapon).and_return('Scissors')
+      allow(@new_game.player).to receive(:choice).and_return('Rock')
+      expect(@new_game.game_winner).to eq 'You win!'
     end
 
     it 'confirms if computer won the game' do
-      new_game = Game.new(player)
-      allow(new_game.computer).to receive(:weapon).and_return('Paper')
-      allow(new_game.player).to receive(:choice).and_return('Rock')
-      expect(new_game.game_winner).to eq 'Computer wins!'
+      allow(@new_game.computer).to receive(:weapon).and_return('Paper')
+      allow(@new_game.player).to receive(:choice).and_return('Rock')
+      expect(@new_game.game_winner).to eq 'Computer wins!'
     end
 
     it 'confirms if there is a draw' do
-      new_game = Game.new(player)
-      allow(new_game.computer).to receive(:weapon).and_return('Paper')
-      allow(new_game.player).to receive(:choice).and_return('Paper')
-      expect(new_game.game_winner).to eq "It's a draw!"
+      allow(@new_game.computer).to receive(:weapon).and_return('Paper')
+      allow(@new_game.player).to receive(:choice).and_return('Paper')
+      expect(@new_game.game_winner).to eq "It's a draw!"
     end
   end
 end
