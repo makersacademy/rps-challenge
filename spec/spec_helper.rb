8bc111db7f839a8e4eebc03c443858147fda43c8
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-03-18 20:24:04 +0000

    Corrected file renaming error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ Coveralls.wear!
 
 ENV['RACK_ENV'] = 'test'
 
-require File.join(File.dirname(__FILE__), '..', 'RPS.rb')
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
