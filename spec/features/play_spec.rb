289bd8b18f5bf7c5071833340b66553323e7bf3d
Misa Ogura <misa.ogura01@gmail.com>
2016-03-18 19:54:34 +0000

    Add ROCK button which sends param[:choice]=ROCK to controller which redirects to Result page

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,10 +1,7 @@
 feature 'Play page' do
 
   scenario '0.0. Page displays name of player' do
-    visit '/'
-    fill_in('p1_name', with: 'Misa')
-    click_button('Enter Game')
-    visit '/play'
+    sign_in_and_play
     expect(page).to have_content('Player 1: Misa')
   end
 end
