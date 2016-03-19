6b2e1707ede6098b3a17b4be1df215dc79bb533c
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-03-19 14:17:57 +0000

    Weapon choice now a button

--- spec/features/select_weapon_spec.rb
+++ spec/features/select_weapon_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 require 'capybara'
 
 feature 'Selecting weapon' do
-  scenario 'select weapon' do
+  scenario 'selecting weapon' do
     sign_in_and_play
-    fill_in :weapon, with: "Scissors"
-    click_button "Let's PLAY!"
+    click_button "Scissors"
     expect(page).to have_content 'You picked Scissors!'
   end
 end
