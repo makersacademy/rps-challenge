5aec7aaa6d4f53047dcea2105dede064d06f8c85
Misa Ogura <misa.ogura01@gmail.com>
2016-03-19 08:02:11 +0000

    Implement computerised opponent

--- app.rb
+++ app.rb
@@ -18,23 +18,28 @@ class RPS < Sinatra::Base
 
   post '/' do
     p1 = Player.new(params[:p1_name])
-    RPS.store_game(Game.new(p1))
+    p2 = Player.new('Computer')
+    RPS.store_game(Game.new(p1, p2))
     redirect '/play'
   end
 
   get '/play' do
     @p1_name = RPS.game.p1.name
+    @p2_name = RPS.game.p2.name
     erb(:play)
   end
 
   post '/play' do
     @p1_move = RPS.game.p1.choose(params[:move])
+    @p2_move = RPS.game.p2.choose_random
     redirect '/result'
   end
 
   get '/result' do
     @p1_name = RPS.game.p1.name
     @p1_move = RPS.game.p1.move
+    @p2_name = RPS.game.p2.name
+    @p2_move = RPS.game.p2.move
     erb(:result)
   end
 
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -3,19 +3,18 @@ require_relative '../app'
 describe RPS do
 
   subject(:rps) { described_class }
-  let(:new_game) { double :game }
-  let(:stored_game) { double :game }
+  let(:game) { double :game }
 
   describe '#self.store_game' do
     it '0.0. stores an instance of game' do
-      expect(rps.store_game(new_game)).to eq(new_game)
+      expect(rps.store_game(game)).to eq(game)
     end
   end
 
   describe '#self.game' do
     it '1.0. returns a stored game' do
-      rps.store_game(stored_game)
-      expect(rps.game).to eq(stored_game)
+      rps.store_game(game)
+      expect(rps.game).to eq(game)
     end
   end
 end
