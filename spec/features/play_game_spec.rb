71cf6c9c4eaa65e3606390b0a319efff60ad4292
Hanif Fakira <fakira.work@gmail.com>
2016-03-19 15:49:54 +0000

    US2 WIP added web helper

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,32 +1,24 @@
 feature 'view options' do
   scenario 'click rock link and see result' do
-    visit("/")
-    fill_in(:player_1, with: 'Han')
-    click_button 'PLAY'
+    register_and_play
     click_button 'rock'
     expect(page).to have_content "You chose Rock"
   end
 
   scenario 'click paper link and see result' do
-    visit("/")
-    fill_in(:player_1, with: 'Han')
-    click_button 'PLAY'
+    register_and_play
     click_button 'paper'
     expect(page).to have_content "You chose Paper"
   end
 
   scenario 'click scissors link and see result' do
-    visit("/")
-    fill_in(:player_1, with: 'Han')
-    click_button 'PLAY'
+    register_and_play
     click_button 'scissors'
     expect(page).to have_content "You chose Scissors"
   end
 
   scenario 'click link and see only one result' do
-    visit("/")
-    fill_in(:player_1, with: 'Han')
-    click_button 'PLAY'
+    register_and_play
     click_button 'paper'
     expect(page).to have_content "You chose Paper"
     expect(page).not_to have_content "You chose Rock"
--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter name' do
   scenario 'submit name to show name and  start playing game' do
-    visit('/')
-    fill_in(:player_1, with: 'Han')
-    click_button 'PLAY'
+    register_and_play
     expect(page).to have_content("Han choose wisely")
   end
 end
