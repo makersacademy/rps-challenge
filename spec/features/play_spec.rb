3f8f934f57b044d2bca9f269eba6090026a000df
Misa Ogura <misa.ogura01@gmail.com>
2016-03-19 08:01:27 +0000

    Play page display player2 name, Result page displays player2 random choice

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,7 +1,14 @@
 feature 'Play page' do
 
-  scenario '0.0. Page displays name of player' do
+  before(:each) do
     sign_in_and_play
+  end
+
+  scenario '0.0. Page displays name of player 1' do
     expect(page).to have_content('Player 1: Misa')
+  end
+
+  scenario '0.0. Page displays name of player 1' do
+    expect(page).to have_content('Player 2: Computer')
   end
 end
--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,10 +1,15 @@
 feature 'Result page' do
 
-  scenario '0.0. Displays player\'s choise' do
+  moves = Player::MOVES
+
+  before(:each) do
     sign_in_and_play
-    choice = ['ROCK','PAPER','SCISSORS'].sample
-    click_button("#{choice}")
+  end
+
+  scenario '0.0. Displays player\'s choise' do
+    choice_p1 = moves.sample
+    click_button(choice_p1)
     visit '/result'
-    expect(page).to have_content("Misa has chosen #{choice}!")
+    expect(page).to have_content("Misa has chosen... #{choice_p1}!")
   end
 end
