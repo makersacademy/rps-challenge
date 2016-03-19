91b32e4529c63622aae42364131bd3af321225aa
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-03-19 15:37:25 +0000

    first commit two-player

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 feature '/names' do
-  scenario 'player\'s name will appear after it has been entered.' do
+  scenario 'player1\'s name will appear after it has been entered.' do
     sign_in
-    expect(page).to have_content('Welcome to the game Charlie')
-
+    expect(page).to have_content('Welcome to the game Charlie and Roxanne')
   end
 end
