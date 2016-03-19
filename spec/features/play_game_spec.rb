d2cb8ad9b7235db1c59567cbc88a2a443c34343a
Hanif Fakira <fakira.work@gmail.com>
2016-03-19 19:30:44 +0000

    refactored computer#result, css, cleaned up specs

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,32 +1,30 @@
 feature 'play game' do
-  scenario 'see options' do
+  before do
     register_and_play
+  end
+
+  scenario 'see options' do
     expect(page).to have_button "rock"
     expect(page).to have_button "paper"
     expect(page).to have_button "scissors"
   end
 
-
   scenario 'click rock link and see result' do
-    register_and_play
     click_button 'rock'
     expect(page).to have_content "You chose Rock"
   end
 
   scenario 'click paper link and see result' do
-    register_and_play
     click_button 'paper'
     expect(page).to have_content "You chose Paper"
   end
 
   scenario 'click scissors link and see result' do
-    register_and_play
     click_button 'scissors'
     expect(page).to have_content "You chose Scissors"
   end
 
   scenario 'click link and see only one result' do
-    register_and_play
     click_button 'paper'
     expect(page).to have_content "You chose Paper"
     expect(page).not_to have_content "You chose Rock"
--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,36 +1,28 @@
 feature 'show results of the game' do
-  scenario 'you win' do
-    # play_rock
+  before do
     register_and_play
     srand(2)
+  end
+  scenario 'you win' do
     click_button 'paper'
     expect(page).to have_content('Han won')
   end
   scenario 'you lost' do
-    register_and_play
-    srand(2)
     click_button 'scissors'
     expect(page).to have_content('Han lost')
   end
   scenario 'you drew' do
-    register_and_play
-    srand(2)
     click_button 'rock'
     expect(page).to have_content('Han drew')
   end
 
   scenario 'can return to start of game' do
-    register_and_play
-    srand(2)
     click_button 'rock'
     expect(page).to have_button('New Game')
   end
 
   scenario 'can rematch' do
-    register_and_play
-    srand(2)
     click_button 'rock'
     expect(page).to have_button('Rematch')
   end
-
 end
