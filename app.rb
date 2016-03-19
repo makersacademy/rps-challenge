c555acd50a2202f072454b221b77e9e2f0f1b58c
Tobenna <tobennandu@gmail.com>
2016-03-19 02:17:50 +0000

    Displays user name

--- app.rb
+++ app.rb
@@ -5,6 +5,15 @@ class Jajanken < Sinatra::Base
     erb(:index)
   end
 
+  post '/setup' do
+    $username = params[:username]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @username = $username
+    erb(:play)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
 end
