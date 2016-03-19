671ea8151f9e12e0ffda9c5fcf6f84a6c496a99f
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-19 15:00:06 +0000

    implement welcome page

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,5 +5,14 @@ feature 'Welcome Screen and Login: ' do
     expect(page).to have_content 'Welcome to Rock Paper Scissor Lizard Spock'
   end
 
+  scenario 'Page has a form to insert the name' do
+    access_login_page
+    expect(page).to have_field 'name_field'
+  end
+
+  scenario 'Page as a Play! button' do
+    access_login_page
+    expect(page).to have_button 'play_button'
+  end
 
 end
