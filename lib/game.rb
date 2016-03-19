a58dc9f9b77e55636957aaa0e53c70782c7ef39e
Misa Ogura <misa.ogura01@gmail.com>
2016-03-19 13:24:56 +0000

    Implement return_winner method in Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,31 @@
 class Game
 
+  MOVES_VAL = { SCISSORS: 1,
+                PAPER:    2,
+                ROCK:     3,
+                LIZARD:   4,
+                SPOCK:    5
+                }
+
   attr_reader :p1, :p2
 
   def initialize(p1, p2)
     @p1 = p1
     @p2 = p2
+  end
+
+  def return_winner
+    move_to_val
+    return 'tie' if @p1_val == @p2_val
+    ( (@p1_val - @p2_val).abs % 5 == 1 || 3 ) ? @p1 : @p2
+  end
+
+  private
+
+  def move_to_val
+    @p1_sym = p1.move.to_sym
+    @p2_sym = p2.move.to_sym
+    @p1_val = MOVES_VAL[@p1_sym]
+    @p2_val = MOVES_VAL[@p2_sym]
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,16 +2,27 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new(p1, p2) }
-  let(:p1) {double :player}
-  let(:p2) {double :computer}
+  subject(:game){ described_class.new(p1_scissors, p2_paper) }
+  let(:p1_scissors){ double(:player, move: 'SCISSORS', move_to_sym: :SCISSORS) }
+  let(:p2_scissors){ double(:player, move: 'SCISSORS', move_to_sym: :SCISSORS) }
+  let(:p2_paper) { double(:computer, move: 'PAPER', move_to_sym: :PAPER) }
 
   describe '#initialize' do
     it '0.0. initializes with one player' do
-      expect(game.p1).to eq p1
+      expect(game.p1).to eq(p1_scissors)
     end
     it '0.1. initializes with one computer' do
-      expect(game.p2).to eq p2
+      expect(game.p2).to eq(p2_paper)
+    end
+  end
+
+  describe '#return_winner' do
+    it '1.0. returns the winner' do
+      expect(game.return_winner).to eq(p1_scissors)
+    end
+    it '1.1. annouces tie when p1 and p2 choose same move' do
+      tie_game = described_class.new(p1_scissors, p2_scissors)
+      expect(tie_game.return_winner).to eq('tie')
     end
   end
 end
