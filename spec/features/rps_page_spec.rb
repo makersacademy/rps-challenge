dbd752573ca444cc72af1e3c7b591f7ee45a3689
Anne <annemarie.k89@gmail.com>
2016-03-19 19:23:10 +0000

    Added feature specs for rules and moves

--- spec/features/rps_page_spec.rb
+++ spec/features/rps_page_spec.rb
@@ -7,4 +7,29 @@ feature 'Rock-Paper-Scissors page' do
 		text = "Hello Player!"
 		expect(page).to have_content(text)
 	end
+
+	scenario 'Show player the rules' do
+		text = "Here's a quick reminder of the rules:
+						Rock beats Scissors
+						Scissors beats Paper
+						Paper beats Rock"
+		expect(page).to have_content(text)
+	end
+
+	scenario 'Player choses their move' do
+		text = "You must now decide on a move"
+		expect(page).to have_content(text)
+	end
+
+	scenario 'Can chose Rock' do
+		find_button('Rock').click
+	end
+
+	scenario 'Can chose Paper' do
+		find_button('Paper').click
+	end
+
+	scenario 'Can chose Scissors' do
+		find_button('Scissors').click
+	end
 end
