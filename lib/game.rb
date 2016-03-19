e5c4cc6d3652152d379e6ce8ef4b76b86a26ab56
Misa Ogura <misa.ogura01@gmail.com>
2016-03-19 14:07:52 +0000

    Refactor return_winner method and tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,10 @@
 class Game
 
-  MOVES_VAL = { SCISSORS: 1,
+  MOVES_VAL = { ROCK:     0,
+                SPOCK:    1,
                 PAPER:    2,
-                ROCK:     3,
-                LIZARD:   4,
-                SPOCK:    5
+                LIZARD:   3,
+                SCISSORS: 4
                 }
 
   attr_reader :p1, :p2
@@ -17,7 +17,7 @@ class Game
   def return_winner
     move_to_val
     return 'tie' if @p1_val == @p2_val
-    ( (@p1_val - @p2_val).abs % 5 == 1 || 3 ) ? @p1 : @p2
+    p1_win? ? @p1 : @p2
   end
 
   private
@@ -27,5 +27,10 @@ class Game
     @p2_sym = p2.move.to_sym
     @p1_val = MOVES_VAL[@p1_sym]
     @p2_val = MOVES_VAL[@p2_sym]
+  end
+
+  def p1_win?
+    p diff = (@p1_val - @p2_val) % 5
+    diff == 1 || diff == 2
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,25 +2,30 @@ require 'game'
 
 describe Game do
 
-  subject(:game){ described_class.new(p1_scissors, p2_paper) }
+  let(:p1_game) { described_class.new(p1_scissors, p2_paper) }
   let(:p1_scissors){ double(:player, move: 'SCISSORS', move_to_sym: :SCISSORS) }
-  let(:p2_scissors){ double(:player, move: 'SCISSORS', move_to_sym: :SCISSORS) }
+  let(:p1_paper) { double(:player, move: 'PAPER', move_to_sym: :PAPER) }  
+  let(:p2_scissors){ double(:computer, move: 'SCISSORS', move_to_sym: :SCISSORS) }
   let(:p2_paper) { double(:computer, move: 'PAPER', move_to_sym: :PAPER) }
 
   describe '#initialize' do
     it '0.0. initializes with one player' do
-      expect(game.p1).to eq(p1_scissors)
+      expect(p1_game.p1).to eq(p1_scissors)
     end
     it '0.1. initializes with one computer' do
-      expect(game.p2).to eq(p2_paper)
+      expect(p1_game.p2).to eq(p2_paper)
     end
   end
 
   describe '#return_winner' do
-    it '1.0. returns the winner' do
-      expect(game.return_winner).to eq(p1_scissors)
+    it '1.0. returns the p1 when p1 wins' do
+      expect(p1_game.return_winner).to eq(p1_scissors)
     end
-    it '1.1. annouces tie when p1 and p2 choose same move' do
+    it '1.1. returns the p2 when p2 wins' do
+      p2_game = described_class.new(p1_paper, p2_scissors)
+      expect(p2_game.return_winner).to eq(p2_scissors)
+    end
+    it '1.2. annouces tie when p1 and p2 choose same move' do
       tie_game = described_class.new(p1_scissors, p2_scissors)
       expect(tie_game.return_winner).to eq('tie')
     end
