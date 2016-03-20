df5de177ae89f929f1f07c7e62575e1d62010bcc
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-20 12:58:13 +0000

    split result erb into 3 files. refactored

--- app.rb
+++ app.rb
@@ -11,31 +11,25 @@ class RPS < Sinatra::Base
 
   post "/names" do
     player_1 = Player.new(params[:player_1])
-    if params[:player_2] == ""
-      player_2 = Computer.new
-    else
-      player_2 = Player.new(params[:player_2])
-    end
+    name = params[:player_2]
+    name == "" ? player_2 = Computer.new : player_2 = Player.new(name)
     Game.start(Game.new(player_1, player_2))
     redirect "/play"
   end
 
   get "/play" do
-    @player_1_name = game.player_1.name
-    @player_2_name = game.player_2.name
     erb :play
   end
 
   post "/weapon" do
     store_weapon
     game.player_1.pick(@weapon)
-    redirect "/choose-again" unless game.player_2.name == "Computer"
+    redirect "/choose-again" unless computer?
     game.player_2.pick_weapon
     redirect "/result"
   end
 
   get "/choose-again" do
-    @player_2_name = game.player_2.name
     erb :choose_again
   end
 
@@ -47,11 +41,7 @@ class RPS < Sinatra::Base
 
   get "/result" do
     @result = game.result(game.player_1.weapon, game.player_2.weapon)
-    @player_1_name = game.player_1.name
-    @player_2_name = game.player_2.name
-    @player_1_choice = game.player_1.weapon
-    @player_2_choice = game.player_2.weapon
-    erb :result
+    erb @result
   end
 
   private
@@ -63,6 +53,10 @@ class RPS < Sinatra::Base
 
     def store_weapon
       Game::WEAPONS.each { |weapon| @weapon = weapon if params[weapon] }
+    end
+
+    def computer?
+      game.player_2.name == "Computer"
     end
   end
 
