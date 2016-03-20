bca1e32522c4ac8633be3161b28d402d8965a38c
adil ali <adiln1993@gmail.com>
2016-03-20 13:01:05 +0000

    created class variable to store player instances

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,11 @@
 class Player
   attr_reader :name
+  @players = []
+
+  def self.players
+    @players
+  end
+
   def initialize(name: name)
     @name = name
   end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,9 +4,17 @@ require 'spec_helper'
 describe Player do
   subject(:player){described_class.new(name: 'adil')}
 
+  it 'should have a empty array to store player instances' do
+    expect(Player.players).to eq []
+  end
+
   describe '#initialization' do
     it 'should accept a name as parameter and save it' do
       expect(player.name).to eq 'adil'
     end
   end
+
+
+
+
 end
