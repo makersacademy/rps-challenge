73ac33e4455782f30ab2288c6a11560e77403c32
eripheebs <erikapheby@gmail.com>
2016-03-20 13:14:33 +0000

    Feature tests passing for paper and scissors

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,4 +4,16 @@ feature '#attack' do
     click_button "Rock"
     expect(page).to have_content("You played Rock.")
   end
+
+  scenario '>should play turn as paper if paper button is clicked' do
+    sign_in_and_play
+    click_button "Paper"
+    expect(page).to have_content("You played Paper.")
+  end
+
+  scenario '>should play turn as scissors if scissor button is clicked' do
+    sign_in_and_play
+    click_button "Scissors"
+    expect(page).to have_content("You played Scissors.")
+  end
 end
