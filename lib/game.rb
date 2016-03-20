a7b6f296b8fbe091ac6c3b8c1c9236891ee2f35a
Gabrielle <gabivk@gmail.com>
2016-03-20 21:24:14 +0000

    IT WORKS BUT WHY

--- lib/game.rb
+++ lib/game.rb
@@ -22,11 +22,10 @@ class Game
   def initialize(player, computer)
     @player = player
     @computer = computer
-    # @weapon2 = computer.choice.to_sym
   end
 
-  def weapon1
-    @weapon1 = @player.weapon.to_sym
+  def weapon1 
+      @weapon1 = @player.weapon.to_sym
   end
 
   def weapon2
