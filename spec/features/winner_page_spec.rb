9368a2e90f8a0b0eac1389efeb063452e9c84030
Yasmin Green <yasmingreen@Yasmins-MBP.home>
2016-03-20 17:13:18 +0000

    Added a couple more feature tests

--- spec/features/winner_page_spec.rb
+++ spec/features/winner_page_spec.rb
@@ -1,4 +1,4 @@
-RSpec.feature 'winner_page: Shows winner or draw' do
+RSpec.feature 'winner_page: Shows winner' do
 
   before do
     singlemode
@@ -14,24 +14,31 @@ RSpec.feature 'winner_page: Shows winner or draw' do
       expect(page).to have_content("Computer\'s choice: Paper")
     end
 
-    scenario '3. singlemode: should have who wins' do
+    scenario '3. singlemode: should have computer wins' do
       expect(page).to have_content("Computer wins!")
     end
 
-    scenario '4. should have play again button' do
+    scenario '4. singlemode: should have player wins' do
+      singlemode
+      allow(Kernel).to receive(:rand).and_return(1)
+      click_button("Scissors")
+      expect(page).to have_content("player1 wins!")
+    end
+
+    scenario '5. should have play again button' do
       expect(page).to have_button("Play again?")
     end
 
-    scenario '5. play again button should go to play page' do
+    scenario '6. play again button should go to play page' do
       click_button("Play again?")
       expect(page).to have_content("Pick your weapon player1:")
     end
 
-    scenario '6. should have home button' do
+    scenario '7. should have home button' do
       expect(page).to have_button("Home")
     end
 
-    scenario '7. New player button should go to index page' do
+    scenario '8. New player button should go to index page' do
       click_button("Home")
       expect(page).to have_content("Lets play Rock, Paper, Scissors!")
     end
