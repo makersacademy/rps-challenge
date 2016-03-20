a46743807d1dd467ae29befd936e179e8d8c7be5
adil ali <adiln1993@gmail.com>
2016-03-20 13:14:32 +0000

    removed test and functionality because YAGNI

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,5 @@
 class Player
   attr_reader :name
-  @players = []
-
-  def self.players
-    @players
-  end
 
   def initialize(name: name)
     @name = name
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,9 +4,6 @@ require 'spec_helper'
 describe Player do
   subject(:player){described_class.new(name: 'adil')}
 
-  it 'should have a empty array to store player instances' do
-    expect(Player.players).to eq []
-  end
 
   describe '#initialization' do
     it 'should accept a name as parameter and save it' do
