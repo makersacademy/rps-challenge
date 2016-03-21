ebff6fb3f165daa0fa3ac7bca7ae40479e43ecb0
Sachin Karia <sachin.karia01@gmail.com>
2016-03-21 12:03:20 +0000

    updated readme

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -3,7 +3,7 @@ feature 'play game' do
   scenario 'selects options and plays game' do
     start_game
     play_rock
-    expect(page).to have_text("You played Rock!")
+    expect(page).to have_text("You ")
   end
 
 end
