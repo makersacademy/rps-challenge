bb95336cebc411743cb3a01d461a2df984f3b060
Tobenna <tobennandu@gmail.com>
2016-03-21 05:37:15 +0000

    Play again link

--- spec/features/display_winner_spec.rb
+++ spec/features/display_winner_spec.rb
@@ -2,25 +2,32 @@ feature 'Display Winner', type: :feature do
   let(:player1) { 'Gon Freecs' }
   let(:player2) { 'Darth Vadar' }
 
-  scenario "It's a tie" do
+  before do
     allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+  end
+
+  scenario "It's a tie" do
     sign_in_and_play(player1)
     click_button 'Rock'
     expect(page).to have_content "It's a tie"
   end
 
-  scenario "Player one Won" do
-    allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+  scenario 'Player one Won' do
     sign_in_and_play(player1)
     click_button 'Paper'
     expect(page).to have_content "#{player1} wins!"
     expect(page).to have_content "#{player1} wins!"
   end
 
-  scenario "Player two Won" do
-    allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+  scenario 'Player two Won' do
     sign_in_and_play(player1)
     click_button 'Scissors'
     expect(page).to have_content "#{player2} wins!"
+  end
+
+  scenario 'Allow user to player again' do
+    sign_in_and_play(player1)
+    click_button 'Scissors'
+    expect(page).to have_link 'Play Again?'
   end
 end
