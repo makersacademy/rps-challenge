eb63a4f3635ce9acaff169d601ba7d457008f0cc
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-23 18:49:36 +0000

    refactored

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,10 +3,6 @@ require 'computer'
 describe Computer do
 	subject(:computer) { described_class }
 
-	before do
-		srand 1
-	end
-
 	describe '#move' do
 		it 'returns a move' do
 			allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
