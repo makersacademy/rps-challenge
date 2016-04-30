3c0ebcb3489ce873252bf9f74541c72a96f13386
sitypop <sityemilyshah@gmail.com>
2016-04-30 14:39:03 +0100

    Added web helper for sign in and play

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter name' do
   scenario 'submitting name' do
-    visit('/')
-    fill_in :name, with: 'Sity'
-    click_button 'Push me'
+    sign_in_and_play
     expect(page).to have_content 'Sity vs. The Computer'
   end
 end
