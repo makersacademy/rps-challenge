01eebc3eaa6970f733662b0a4ebcf4fc328d7e45
Omar Alvarez <omajul2@hotmail.com>
2016-04-30 17:43:54 +0200

    Add layout.erb and play.erb

--- app.rb
+++ app.rb
@@ -1,8 +1,30 @@
 require 'sinatra/base'
+require './lib/player'
+require './lib/game'
 
 class RPS < Sinatra::Base
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
+  end
+
+  post '/start' do
+    player_1 = Player.new(params[:name])
+    player_2 = Player.new('Computer')
+    mode = params[:mode]
+    if mode == 'standard'
+    	@game = Game.create(player_1, player_2, true)
+    else
+    	@game = Game.create(player_1, player_2, false)
+    end
+    redirect '/play'
+  end
+
+  get '/play' do
+  	erb :play
   end
 
   # start the server if ruby file executed directly
