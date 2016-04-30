7e22ed7959af7a48d903ba594f5bd8233070a1a1
emilio <emsmnndz@gmail.com>
2016-04-30 20:19:46 +0100

    unregistered users have to register

--- app.rb
+++ app.rb
@@ -17,6 +17,21 @@ class RPS < Sinatra::Base
     @name = session[:name]
     erb :play
   end
+
+  post '/weapon' do
+    session[:weapon] = params[:weapon]
+    redirect '/weapon'
+  end
+
+  get '/weapon' do
+    @weapon = session[:weapon]
+    erb :weapon
+  end
+
+  get '/AI' do
+    erb :AI
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -1,17 +1,37 @@
 feature 'User' do
-  scenario 'can register' do
-    visit '/'
-    fill_in('name', with: 'Pepito')
-    click_button('Submit')
-    expect(page).to have_content('Pepito')
+
+  context 'when unregistered' do
+    scenario 'can register' do
+      visit '/'
+      fill_in('name', with: 'Pepito')
+      click_button('Submit')
+      expect(page).to have_content('Pepito')
+    end
+    it 'has to enter a name' do
+      visit '/'
+      click_button('Submit')
+      expect(page).to have_content('Please enter a name')
+    end
   end
 
-  scenario 'can see the choices' do
+  context 'when registered' do
+    scenario 'can see the choices' do
     visit '/'
     fill_in('name', with: 'Pepito')
     click_button('Submit')
     expect(page).to have_content('Rock')
     expect(page).to have_content('Paper')
     expect(page).to have_content('Scissors')
+    end
+
+
+    scenario 'can choose a weapon' do
+      visit '/'
+      fill_in('name', with: 'Pepito')
+      click_button('Submit')
+      click_button('ROCK')
+      expect(page).to have_content('You chose ROCK!')
+    end
   end
+
 end
