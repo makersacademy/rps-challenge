12d1c60c4ed7da62693321e24f6e3754bb79776b
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-04-30 14:48:02 +0100

    User can now enter their name, and it's displayed.

---
+++ spec/features/choose_move_spec.rb
@@ -0,0 +1,8 @@
+# feature 'Choose one of three moves' do
+#   scenario 'Chooses Rock' do
+#     visit '/'
+#     fill_in('player_name', with: 'Kojak')
+#     click_button('Submit')
+
+#   end
+# end
