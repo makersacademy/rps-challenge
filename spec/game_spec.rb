8eb43ea7d1d6a9eadb748da1a552b08b05f22283
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-04-30 10:08:14 +0100

    WIP start game class spec, thinking about real need of Player class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,11 @@
 describe 'Game' do
-  context '???????' do
-    it 'returns ' do
-
+  let(:game) {described_class.new(player1,player2,player_mode,game_mode)}
+  context 'on initialize' do
+    it 'receives parameters names and modes' do
+      # expect(game("Pepe"))
     end
   end
 
-  context 'mode two players' do
+  context 'game player mode' do
   end
 end
