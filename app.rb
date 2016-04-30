53947dbad5e92c82501df6e96a2a9441e5d96129
Omar Alvarez <omajul2@hotmail.com>
2016-04-30 21:44:48 +0200

    (WIP) - Play view

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require './lib/player'
 require './lib/game'
 
 class RPS < Sinatra::Base
+
+	enable :sessions
+
   before do
     @game = Game.instance
   end
@@ -12,15 +15,21 @@ class RPS < Sinatra::Base
   end
 
   post '/start' do
-    player_1 = Player.new(params[:name])
-    player_2 = Player.new('Computer')
-    mode = params[:mode]
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    mode = params[:game_mode]
     if mode == 'standard'
     	@game = Game.create(player_1, player_2, true)
     else
     	@game = Game.create(player_1, player_2, false)
     end
     redirect '/play'
+  end
+
+  get '/names' do
+  	@player_mode = params[:player_mode]
+  	@game_mode = params[:game_mode]
+  	erb :names
   end
 
   get '/play' do
