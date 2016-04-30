7002dba52a84e6e1d03e80976caeb1615901ee54
sitypop <sityemilyshah@gmail.com>
2016-04-30 14:33:35 +0100

    Post-redirect loop

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @name = params[:name]
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
     erb(:play)
   end
 
