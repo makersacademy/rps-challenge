5c8e2f0a1c0a19847defec3341310569d8725adc
Omar Alvarez <omajul2@hotmail.com>
2016-04-30 10:56:10 +0200

    Add method for finding the winner in Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,14 @@
 class Game
 
-  STANDARD = [rock: 1, paper: 2, scissors: 3]
-  EXTENDED = [rock: 1, paper: 2, scissors: 3, lizard: 4, spock: 5]
+  STANDARD = {rock: 1, paper: 2, scissors: 3}.freeze
+  EXTENDED = {rock: 1, paper: 2, scissors: 3, lizard: 4, spock: 5}.freeze
 
   attr_reader :player_1, :player_2
 
-  def initialize(player_1, player_2, extended = false)
+  def initialize(player_1, player_2, standard = true)
   	@player_1 = player_1
   	@player_2 = player_2
-    @extended = extended
+    @standard = standard
   end
 
   def self.create(player_1, player_2)
@@ -19,4 +19,22 @@ class Game
   	@game
   end
 
+  def start
+    result = find_winner
+    puts result == 'Tie' ? result : "The winner is #{result.name}"
+  end
+
+  private
+
+    def find_winner
+      if @standard
+        if STANDARD[player_1.choice] % 3 + 1 == STANDARD[player_2.choice]
+          player_2
+        elsif STANDARD[player_2.choice] % 3 + 1 == STANDARD[player_1.choice]
+          player_1
+        else
+          'Tie'
+        end
+      end     
+    end
 end
