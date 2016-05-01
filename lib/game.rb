2afc7424a13fa282600f74bee822288f45c2cf08
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-05-01 23:02:49 +0100

    game has a user and computer player

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,20 @@
+require_relative 'player'
+require_relative 'computer'
+
 class Game
 
 
-attr_reader :player
+attr_reader :player, :computer
 
-def initialize(name)
+def initialize(name, computer = Computer.new)
   @player = Player.new(name)
+  @computer = computer
   end
 
 
   def name
     @player.name
   end
+
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,22 @@
 require 'game'
 require 'player'
+require 'computer'
 
 describe Game do
-  subject(:game) {Game.new('User')}
+  let (:computer) {Computer.new}
+  subject(:game) {Game.new('User', computer)}
+
 
   describe "#initialize" do
     it "starts the game with a named player" do
       expect(game.player.name).to eq "User"
     end
+    it "starts the game wiht a computer opponent" do
+      expect(game.computer). to eq computer
+
+    end
+
+
   end
 
 end
