a53ffb497233c9b7ade763cda761216bfc4dd33f
Hanna <hannaeberhoefer@yahoo.com>
2016-05-01 21:03:35 +0100

    Added feature test

---
+++ spec/features/name_spec.rb
@@ -0,0 +1,12 @@
+
+feature 'Enter name' do
+  scenario 'submitting name' do
+    visit ('/')
+    fill_in :player_1_name, with: 'Luni'
+    click_button 'Submit'
+    expect(page).to have_content 'Luni vs. Computer'
+  end
+end
+
+
+
