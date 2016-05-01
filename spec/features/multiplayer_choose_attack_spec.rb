3514da1635d84fa0ea327dc279f51592f044732c
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-05-01 16:06:26 +0200

    minor indentation

--- spec/features/multiplayer_choose_attack_spec.rb
+++ spec/features/multiplayer_choose_attack_spec.rb
@@ -4,12 +4,10 @@ feature 'multiplayers choose attacks' do
     click_button 'Rock'
     expect(page).to have_content 'Emma made her choice'
   end
-
   scenario 'player 2 chooses an attack' do
     multi_player_with_names
     click_button 'Rock'
     click_button 'Paper'
     expect(page).to have_content 'The winner is: Ingrid'
   end
-
 end
