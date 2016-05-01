09c7e48ff25f85f1ba4e41cd1deced4c2e8fe8c1
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-01 13:15:12 +0100

    add play_2 make choice view, using GET

--- spec/features/play_2_spec.rb
+++ spec/features/play_2_spec.rb
@@ -3,6 +3,5 @@ feature 'Choices available', :type => :feature do
     mode_two_players_classic
     click_button("rock")
     expect(page).to have_content 'Paco'
-    expect(page).to_not have_content 'Sergio'
   end
 end
