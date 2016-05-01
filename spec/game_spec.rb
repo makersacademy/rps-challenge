1f38fac9932ac40e5cdaa483f49f0ee516fa2643
Omar Alvarez <omajul2@hotmail.com>
2016-05-01 17:28:30 +0200

    Update Game class tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,8 @@
 require 'game'
 
 describe Game do
-	subject(:game) { Game.new(player_1, player_2, true) }
-	subject(:game_extended) { Game.new(player_1, player_2, false) }
+	subject(:game) { Game.new(player_1, player_2, 2, true) }
+	subject(:game_extended) { Game.new(player_1, player_2, 2, false) }
 	let(:player_1) {  spy(:player, name: 'Omar', choice: :rock) }
 	let(:player_2) {  spy(:player, name: 'Laura', choice: :scissors) }
 
@@ -58,19 +58,19 @@ describe Game do
 
 	describe '.create' do
 		it 'creates a new game in standard mode' do
-			expect(Game.create(player_1, player_2, true)).to be_a Game
-			expect(Game.create(player_1, player_2, true).standard_mode).to be true
+			expect(Game.create(player_1, player_2, 2, true)).to be_a Game
+			expect(Game.create(player_1, player_2, 2, true).game_mode).to be true
 		end
 
 		it 'creates a new game in extended mode' do
-			expect(Game.create(player_1, player_2, false)).to be_a Game
-			expect(Game.create(player_1, player_2, false).standard_mode).to be false
+			expect(Game.create(player_1, player_2, 2, false)).to be_a Game
+			expect(Game.create(player_1, player_2, 2, false).game_mode).to be false
 		end		  	
 	end
 
 	describe '.instance' do
 		it 'returns the created game' do
-			instance = Game.create(player_1, player_2, true)
+			instance = Game.create(player_1, player_2, 2, true)
 			expect(Game.instance).to eq instance
 		end
 	end
