88f9c8d964490fcb9c0182ac832c2a660df4fb39
emilio <emsmnndz@gmail.com>
2016-05-01 20:40:39 +0100

    tests for end of game added and passed

---
+++ spec/features/end_game_spec.rb
@@ -0,0 +1,12 @@
+feature 'end of game' do
+  scenario 'allows to restart the game' do
+    visit '/'
+    fill_in('name', with: 'Pepito')
+    click_button('Submit')
+    click_button('ROCK')
+    click_button('continue')
+    click_button('continue')
+    click_button('Restart!')
+    expect(page).to have_content('Please enter your name')
+  end
+end
