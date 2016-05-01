3722519a537af04f1e2d6588d28d303488206e6a
Matthew Ward <matthewward@Matthews-MacBook-Pro-8.local>
2016-05-01 19:03:10 +0100

    Fixed option text bug

--- spec/features/choose_option_spec.rb
+++ spec/features/choose_option_spec.rb
@@ -2,9 +2,9 @@ feature 'Choose option' do
 
   scenario 'Rock, Paper and Scissors options exist' do
     sign_in
-    expect(page).to have_content('Choose Rock')
-    expect(page).to have_content('Choose Paper')
-    expect(page).to have_content('Choose Scissors')
+    expect(page).to have_content('Rock')
+    expect(page).to have_content('Paper')
+    expect(page).to have_content('Scissors')
   end
 
 end
