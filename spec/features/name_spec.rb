89683fbcc5faa2ef013e7f0828f991ddade12da3
Hanna <hannaeberhoefer@yahoo.com>
2016-05-02 11:40:18 +0100

    Corrected name_spec

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -2,7 +2,7 @@
 feature 'Enter name' do
   scenario 'submitting name' do
     visit ('/')
-    fill_in :player_1_name, with: 'Luni'
+    fill_in :player_name, with: 'Luni'
     click_button 'Submit'
     expect(page).to have_content 'Luni vs. Computer'
   end
