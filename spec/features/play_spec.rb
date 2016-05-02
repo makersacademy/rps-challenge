d50ab57e7c6c7e93b0cfa2ec1613f781ea2e5f03
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-02 16:48:08 +0100

    indentation checked

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -22,32 +22,32 @@ feature 'see options rock, paper or scissors' do
     expect(page).to have_content "Computer chose Scissors"
   end
 
-context 'ending game' do
-  before do
-       srand(SCISSORS)
-     end
+  context 'ending game' do
+    before do
+      srand(SCISSORS)
+    end
 
-  scenario 'it tells me when I win' do
-    sign_in_and_play
-    click_button 'Rock'
-    expect(page).to have_content 'YOU WIN'
+    scenario 'it tells me when I win' do
+      sign_in_and_play
+      click_button 'Rock'
+      expect(page).to have_content 'YOU WIN'
+    end
+
+    scenario 'it tells me when I lose' do
+      sign_in_and_play
+      click_button 'Paper'
+      expect(page).to have_content 'YOU LOSE'
+    end
+
+    scenario 'tie' do
+      sign_in_and_play
+      click_button 'Scissors'
+      expect(page).to have_content 'TIED GAME'
+    end
+
+    def possibe_messages
+      [:rock, :paper, :scissors].map { |shape| "Computer chose #{shape.to_s.capitalize}"}
+    end
+
   end
-
-  scenario 'it tells me when I lose' do
-    sign_in_and_play
-    click_button 'Paper'
-    expect(page).to have_content 'YOU LOSE'
-  end
-
-  scenario 'tie' do
-    sign_in_and_play
-    click_button 'Scissors'
-    expect(page).to have_content 'TIED GAME'
-  end
-
-  def possibe_messages
-    [:rock, :paper, :scissors].map { |shape| "Computer chose #{shape.to_s.capitalize}"}
-  end
-
-end
 end
