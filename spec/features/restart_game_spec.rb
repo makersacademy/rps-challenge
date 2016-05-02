fefc6f976ec89ae3033677b8ba671844d6a81e43
letianw <tmac4ever0824@gmail.com>
2016-05-03 03:25:41 +1000

    multiplayer implemented all tests passes

--- spec/features/restart_game_spec.rb
+++ spec/features/restart_game_spec.rb
@@ -7,12 +7,27 @@ feature 'can restart game' do
     expect(page).to have_content 'Rock Paper Scissors'
   end
 
+  scenario 'restart singleplayer with same player' do
+    sign_in_and_play
+    play_scissors
+    click_link 'Restart_Single'
+    expect(page).to have_content 'Hulk vs. Computer'
+  end
+
   scenario 'restart game from multiplayer' do
     multiplayer_start
     play_scissors
     play_scissors
     click_link 'Restart'
     expect(page).to have_content 'Rock Paper Scissors'
+  end
+
+  scenario 'restart multiplayer with same players' do
+    multiplayer_start
+    play_scissors
+    play_scissors
+    click_link 'Restart_Multi'
+    expect(page).to have_content 'Hulk vs. Ironman'
   end
 
 end
