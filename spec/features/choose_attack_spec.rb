b7eab4c759a64538a4e1d9d85dbe2a541d6865f6
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-05-02 21:20:51 +0200

    Minor style changes

--- spec/features/choose_attack_spec.rb
+++ spec/features/choose_attack_spec.rb
@@ -2,22 +2,22 @@ feature 'choose attack' do
   scenario 'chooses rock' do
     sign_in_with_name
     click_button 'Rock'
-    expect(page).to have_content 'Emma chosed Rock'
+    expect(page).to have_content 'Emma chose Rock'
   end
   scenario 'chooses paper' do
     sign_in_with_name
     click_button 'Paper'
-    expect(page).to have_content 'Emma chosed Paper'
+    expect(page).to have_content 'Emma chose Paper'
   end
   scenario 'chooses scissors' do
     sign_in_with_name
     click_button 'Scissors'
-    expect(page).to have_content 'Emma chosed Scissors'
+    expect(page).to have_content 'Emma chose Scissors'
   end
   scenario 'computer also chooses a random attack' do
     sign_in_with_name
     click_button 'Scissors'
-    expect(page).to have_content "AI chosed"
+    expect(page).to have_content "AI chose"
     expect(page).to have_content(/Rock|Paper|Scissors/)
   end
 end
