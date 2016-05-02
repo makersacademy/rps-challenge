3465f437df1c16cc7c4a0bdb46643a1ce494c96b
AdamSkuse <a.t.skuse@gmail.com>
2016-05-02 22:04:21 +0100

    Add full test coverage for all possible outcomes of Result.judge

--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -1,17 +1,39 @@
 require 'result'
 
 describe Result do
-  describe '#judge' do
-    it 'returns draw when the two players choose the same option' do
-      expect(Result.new("rock", "rock").judge).to eq :draw
-    end
 
-    it 'returns win for player when player beats computer' do
-      expect(Result.new("rock", "scissors").judge).to eq :win
+  describe 'returns wins when player beats computer' do
+    it 'rock v scissors' do
+      expect(Result.new('rock', 'scissors').judge).to eq :win
     end
-
-    it 'returns lose when computer beats player' do
-      expect(Result.new("scissors", "rock").judge).to eq :lose
+    it 'scissors v paper' do
+      expect(Result.new('scissors', 'paper').judge).to eq :win
+    end
+    it 'paper v rock' do
+      expect(Result.new('paper', 'rock').judge).to eq :win
     end
   end
-end
+
+  describe 'returns draw when player and comp choose the same option' do
+    it 'rock v rock' do
+      expect(Result.new('rock', 'rock').judge).to eq :draw
+    end
+    it 'paper v paper' do
+      expect(Result.new('paper', 'paper').judge).to eq :draw
+    end
+    it 'scissors v scissors' do
+      expect(Result.new('scissors', 'scissors').judge).to eq :draw
+    end
+  end
+
+  describe 'returns lose when computer beats player'
+    it 'scissors v rock'  do
+      expect(Result.new('scissors', 'rock').judge).to eq :lose
+    end
+    it 'rock v paper'  do
+      expect(Result.new('rock', 'paper').judge).to eq :lose
+    end
+    it 'paper v scissors'  do
+      expect(Result.new('paper', 'scissors').judge).to eq :lose
+    end
+  end
