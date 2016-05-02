ac7ae134739f9a46c2d79d94c33ac28f7540a49d
Macey Baker <maceybaker@Maceys-MacBook-Pro-2.local>
2016-05-02 13:20:10 +0100

    Updated tests for game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,9 +6,13 @@ describe Game do
   let(:computer){ Computer.new }
   subject(:game){ Game.new(computer, player) }
 
+  before (:each) do
+    allow(computer).to receive(:choose_at_random).and_return("rock")
+  end
+
   describe 'choices hash' do
     it 'adds a weapon to hash when selected' do
-      game.add_selection("rock", "scissors")
+      game.add_selection(computer.choose_at_random, player.choose("scissors"))
       expect(game.choices).to include("rock")
       expect(game.choices).to include("scissors")
     end
@@ -17,23 +21,25 @@ describe Game do
   context 'winning and losing' do
     describe 'when player wins' do
       it 'assigns player as winner' do
-
+        game.add_selection(computer.choose_at_random, player.choose("paper"))
+        expect(game.winner).to eq player
       end
     end
 
     describe 'when computer wins' do
       it 'assigns computer as winner' do
-
+        game.add_selection(computer.choose_at_random, player.choose("scissors"))
+        expect(game.winner).to eq computer
       end
     end
   end
 
   context 'draw' do
     it 'assigns neither player nor computer as winner' do
-
+      game.add_selection(computer.choose_at_random, player.choose("rock"))
+      expect(game.winner).not_to eq player
+      expect(game.winner).not_to eq computer
     end
   end
-
-
 
 end
