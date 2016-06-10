c3de3e66436ad789f462d3ecb56ad39dc8252c34
Stephen Dawes <sdawes@outlook.com>
2016-06-10 19:00:43 +0100

    amended app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
   get '/' do
-    'RockPaperScissors'
+    "Steve's RockPaperScissors Game"
   end
 
   # start the server if ruby file executed directly
