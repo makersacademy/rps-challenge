a5275e1edf8e562ae3f37d7135fac1485c7a92b0
LukaszGr <lukasz.grychowski@gmail.com>
2016-06-10 16:51:20 +0100

    post /name added

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+
   get '/' do
-    'Hello RPS!'
+    erb :index
+  end
+
+  post '/name' do
+    @player_name = params[:player_name]
+    erb :play
   end
 
   run! if app_file == $0
