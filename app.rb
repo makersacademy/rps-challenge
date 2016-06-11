a14898c47f480c7e1c171172f25d71baa7c7b637
Apoorva Saxena <apsaxena24@gmail.com>
2016-06-11 15:02:44 +0100

    Added play page

--- app.rb
+++ app.rb
@@ -6,8 +6,14 @@ class RPS < Sinatra::Base
   end
 
   post '/welcome_rules' do
-    @player_name = params[:player_name]
+    $player_name = params[:player_name]
+    @player_name = $player_name
     erb(:welcome_rules)
+  end
+
+  post '/play' do
+    @player_name = $player_name
+    erb(:play)
   end
 
   run! if app_file == $0
