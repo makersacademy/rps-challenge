5aaa63afde4ebfb748a6789abfd0f0c8695e823f
awye765 <awye765@googlemail.com>
2016-06-11 19:42:57 +0100

    Added name entry view, route and feature test

--- app.rb
+++ app.rb
@@ -10,5 +10,10 @@ class Rps < Sinatra::Base
     erb(:name)
   end
 
+  post('/name') do
+    @player_1 = params[:player_1]
+    erb(:play)
+  end
+
   run! if app_file == $0
 end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,8 +1,8 @@
-# feature "FEATURE: enter player name" do
-#   scenario "submitting names" do
-#     visit('/names')
-#     fill_in :player_name, with: "Alistair"
-#     click_button "Submit"
-#     expect(page).to have_content "Welcome Alistair"
-#   end
-# end
+feature "FEATURE: enter player name" do
+  scenario "submitting player name" do
+    visit('/name')
+    fill_in :player_1, with: "Alistair"
+    click_button "Submit"
+    expect(page).to have_content "Alistair vs. Ava"
+  end
+end
