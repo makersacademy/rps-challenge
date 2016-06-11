7ae045daa7669f4146d87251dd2441f169b25eae
Van Le <singularity@vannio.co.uk>
2016-06-11 21:41:06 +0100

    Some... css changes. Please be kind, Hound

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,16 @@
-document.body.onclick = function(){
-  
+var submissionForm = document.getElementById("form-continue");
+var submissionBtn = document.getElementById("form-continue-submit");
+var submissionImgs = document.getElementsByClassName("player-hand");
+
+submissionBtn.onclick = function(e){
+  e.preventDefault();
+
+  for (var i = 0; i < submissionImgs.length; i++) {
+    console.log(submissionImgs[i].classList);
+  	submissionImgs[i].classList.add("close");
+  }
+
+  setTimeout(function(){
+    submissionForm.submit();
+  }, 1000);
 }
