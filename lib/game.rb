c0580fc584e68f25ba63c95303628522a17aea69
joestephens <joe.stephens@live.co.uk>
2016-06-11 20:54:04 +0100

    Game class implemented and full RPSSL functionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,45 @@
+# understands winning
 class Game
+  attr_reader :players, :winner
+
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+    @winner = []
+  end
+
+  def attack
+    return player_1_wins if player_2_loses?
+    return player_2_wins if player_1_loses?
+    both_players_win
+  end
+
+  private
+
+  def player_1
+    players.first
+  end
+
+  def player_2
+    players.last
+  end
+
+  def player_1_wins
+    winner << player_1
+  end
+
+  def player_2_wins
+    winner << player_2
+  end
+
+  def both_players_win
+    @winner = players
+  end
+
+  def player_1_loses?
+    player_2.weapon.wins_against.include?(player_1.weapon)
+  end
+
+  def player_2_loses?
+    player_1.weapon.wins_against.include?(player_2.weapon)
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,48 @@
 require 'game'
 
 describe Game do
+  subject(:game_1) { described_class.new(player_1_wins, player_2_loses) }
+  let(:player_1_wins) { double(:player_1, weapon: scissors) }
+  let(:player_2_loses) { double(:player_2, weapon: paper) }
 
+  subject(:game_2) { described_class.new(player_1_loses, player_2_wins) }
+  let(:player_1_loses) { double(:player_1, weapon: lizard) }
+  let(:player_2_wins) { double(:player_2, weapon: rock) }
+
+  subject(:game_3) { described_class.new(player_1_ties, player_2_ties) }
+  let(:player_1_ties) { double(:player_1, weapon: paper) }
+  let(:player_2_ties) { double(:player_2, weapon: paper) }
+
+  let(:scissors) { double(:scissors, wins_against: [paper]) }
+  let(:paper) { double(:paper, wins_against: []) }
+  let(:rock) { double(:rock, wins_against: [lizard]) }
+  let(:lizard) { double(:lizard, wins_against: []) }
+
+  describe '#new' do
+    context 'when initialised' do
+      it 'has players' do
+        expect(game_1.players).to include player_1_wins
+        expect(game_1.players).to include player_2_loses
+      end
+    end
+  end
+
+  describe '#attack' do
+    context 'player attacks another player' do
+      it 'player 1 wins' do
+        game_1.attack
+        expect(game_1.winner).to eq [player_1_wins]
+      end
+
+      it 'player 2 wins' do
+        game_2.attack
+        expect(game_2.winner).to eq [player_2_wins]
+      end
+
+      it 'both players tie' do
+        game_3.attack
+        expect(game_3.winner).to eq [player_1_ties, player_2_ties]
+      end
+    end
+  end
 end
