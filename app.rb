3ea28bb9c35c942fed28503ada6a70435de11ec9
srMarquinho <sr.marquinho@gmail.com>
2016-06-11 13:17:25 +0100

    add player class

--- app.rb
+++ app.rb
@@ -8,6 +8,15 @@ class Battle < Sinatra::Base
     erb(:index)
   end
 
+  post '/names' do
+    player1 = Player.new(params[:player1])
+    player2 = Player.new(params[:player2])
+    redirect '/game'
+  end
+
+  post '/game' do
+end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features/start_spec.rb
@@ -0,0 +1,8 @@
+feature "Game starts" do
+
+  # scenario 'displays choices' do
+  #   sign_in_and_play
+  #   expect(page).to have_content "Player1, choose your hand!"
+  # end
+
+end
