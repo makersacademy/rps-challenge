f21089edef979c7fa5b36e6f0e3aca371d66fd12
Luke <lukeaveil@yahoo.com>
2016-06-11 12:43:58 +0100

    index and play view files created to enter player name

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 require 'sinatra/base'
 
 class Rps < Sinatra::Base
+
   get '/' do
-    'Hello Rps!'
+    erb :index
+  end
+
+  post '/name' do
+  	@player_name = params[:player_name]
+  	erb :play
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
-	
+
