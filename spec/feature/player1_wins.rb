56d872658c2398dc2007d7db517383c9fd954be3
srMarquinho <sr.marquinho@gmail.com>
2016-06-11 12:56:25 +0100

    fix setup

--- spec/feature/player1_wins.rb
+++
@@ -1,8 +0,0 @@
-feature "Player1 wins" do
-
-  scenario '' do
-    sign_in_and_play
-    expect(page).to have_content "Player1, choose your hand!"
-  end
-
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,12 +14,15 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 require 'features/web_helpers'
-
+require 'byebug'
 
 Capybara.app = Battle
-# replace following line with SimpleCov.start to get coverage stats locally
-SimpleCov.start
-# run `open coverage/index.html` from the command line to view details
-
-require 'byebug'
-require 'capybara/rspec'
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
