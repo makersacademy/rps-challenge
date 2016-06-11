15338217319a0816441219a685e16c8e67794066
joestephens <joe.stephens@live.co.uk>
2016-06-11 19:29:56 +0100

    Added Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,7 @@
 class Player
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,13 @@
 require 'player'
 
 describe Player do
-  
+  subject(:player) { described_class.new("Joe") }
+
+  describe '#new' do
+    context 'when initialised' do
+      it 'has a name' do
+        expect(player.name).to eq "Joe"
+      end
+    end
+  end
 end
