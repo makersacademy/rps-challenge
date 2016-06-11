bce318f74c630e8232606a1b955266c08568fa9f
Van Le <singularity@vannio.co.uk>
2016-06-11 21:41:26 +0100

    Added some gesture images

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,35 @@
-var submissionForm = document.getElementById("form-continue");
-var submissionBtn = document.getElementById("form-continue-submit");
-var submissionImgs = document.getElementsByClassName("player-hand");
+var playAgainBtn = document.getElementById("form-continue-submit");
+var submitBtn = document.getElementById("form-choice-submit");
+var handImages = document.getElementsByClassName("player-hand");
 
-submissionBtn.onclick = function(e){
-  e.preventDefault();
+if (playAgainBtn) {
+  playAgainBtn.onclick = function(e){
+    e.preventDefault();
 
-  for (var i = 0; i < submissionImgs.length; i++) {
-    console.log(submissionImgs[i].classList);
-  	submissionImgs[i].classList.add("close");
+    for (var i = 0; i < handImages.length; i++) {
+    	handImages[i].classList.add("close");
+    }
+
+    var form = document.getElementById("form-continue");
+    form.classList.add("submitted");
+
+    setTimeout(function(){
+      form.submit();
+    }, 2000);
   }
+}
 
-  setTimeout(function(){
-    submissionForm.submit();
-  }, 1000);
+if (submitBtn) {
+  submitBtn.onclick = function(e){
+    e.preventDefault();
+    
+    handImages[0].classList.add("countdown");
+
+    var form = document.getElementById("form-choice");
+    form.classList.add("submitted");
+
+    setTimeout(function(){
+      form.submit();
+    }, 4000);
+  }
 }
---
+++ public/scripts/preload.js
@@ -0,0 +1,21 @@
+var images = new Array();
+
+function preload() {
+	for (i = 0; i < preload.arguments.length; i++) {
+		images[i] = new Image()
+		images[i].src = preload.arguments[i]
+	}
+}
+preload (
+	"images/countdown.png",
+  "images/paper-open-1.png",
+  "images/paper-close-1.png",
+  "images/paper-open-2.png",
+  "images/paper-close-2.png",
+  "images/scissor-open-1.png",
+  "images/scissor-close-1.png",
+  "images/scissor-open-2.png",
+  "images/scissor-close-2.png",
+  "images/rock-1.png",
+  "images/rock-2.png"
+)
