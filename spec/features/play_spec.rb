2498d8029e703b8cee45440513cb2aa0cf9621b2
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-06-12 22:09:33 +0100

    Fixed a capybara test

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -5,10 +5,10 @@ feature 'Play' do
     expect(page).to have_content "Crescenzo, please select you option"
   end
 
-  scenario 'first move' do
+  scenario 'chose a move' do
     sign_in
     click_button 'Start Playing'
     click_button 'Paper'
-    expect(page).to have_content 'Crescenzo, you\'ve won this round'
+    expect(page).to have_content 'this round'
   end
 end
