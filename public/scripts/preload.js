037a689731a4b60111fcd2d1e53a7d5ad180265e
Van <vannio@users.noreply.github.com>
2016-06-12 23:42:51 +0100

    Tabs to spaces

--- public/scripts/preload.js
+++ public/scripts/preload.js
@@ -1,13 +1,14 @@
 var images = [];
 
 function preload() {
-	for (var i = 0; i < preload.arguments.length; i++) {
-		images[i] = new Image();
-		images[i].src = preload.arguments[i];
-	}
+  for (var i = 0; i < preload.arguments.length; i++) {
+    images[i] = new Image();
+    images[i].src = preload.arguments[i];
+  }
 }
+
 preload (
-	"images/countdown.png",
+  "images/countdown.png",
   "images/paper-open-1.png",
   "images/paper-close-1.png",
   "images/paper-open-2.png",
