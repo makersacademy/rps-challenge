4197a2b77c77306a5bb4676aa8858e883c1fdca7
joestephens <joe.stephens@live.co.uk>
2016-06-13 00:20:49 +0100

    Fixed error

--- app.rb
+++ app.rb
@@ -15,10 +15,10 @@ class RPSApp < Sinatra::Base
   end
 
   post '/enter-name' do
-    weapon = Startup::WEAPONS.get_weapon_by_name(params[:weapon])
+    weapon = Startup::get_weapon_by_name(params[:weapon])
     @player_1 = Player.new(params[:name], weapon)
     @game.add_player(@player_1)
-    @player_2 = Player.new("Computer", Startup::WEAPONS.random_weapon)
+    @player_2 = Player.new("Computer", Startup::random_weapon)
     @game.add_player(@player_2)
     redirect to '/play'
   end
