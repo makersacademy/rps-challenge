3242b799cdc2d511fa6c64ba05507564bb4739d6
Elena <elena.vilimaite@gmail.com>
2016-06-12 17:15:16 +0100

    playing - need tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,25 @@
-class Game 
-	# attr_reader :name
-	def initialize(playerNo, players, mode)
-		
+class Game
+	attr_reader :computer_choice
+	def initialize(playerChoice)
+		@computer_choice = get_computer_choice
+		@player_choice = playerChoice
 	end
 
-end 
+	def result
+		return :tie if @computer_choice == @player_choice
+		case @player_choice
+		when 'rock'
+			return @computer_choice == 'scissors' ? :victory : :defeat
+		when 'paper'
+			return @computer_choice == 'rock' ? :victory : :defeat
+		when 'scissors'
+			return @computer_choice == 'paper' ? :victory : :defeat
+		end
+	end 
+
+	private
+	def get_computer_choice
+		["rock", "paper", "scissors"].sample
+	end
+end
+ 
