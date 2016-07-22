62de3ea2ce37cf11ef69081ecc5fca6a36b3db73
nikklein <objcoding@gmail.com>
2016-07-22 16:50:59 +0100

    Add options

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
 
+
 class RPS < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    p params
-    @player = params[:player_name]#.capitalize
+    session[:player_name] = (params[:player_name]).capitalize
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
     erb :play
   end
   # start the server if ruby file executed directly
