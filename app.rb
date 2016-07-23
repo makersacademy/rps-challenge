d95ec8dba6f46662481475b25a668e65a484c312
Luke Cartledge <luke@lukecartledge.com>
2016-07-23 17:51:05 +0100

    starts game and completes first user story - takes name and responds

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    erb :play
   end
 
   # start the server if ruby file executed directly
