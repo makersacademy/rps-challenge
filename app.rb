d86727c0ae7432203c5bf5e43ce0a182d6650c11
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-23 19:45:47 +0100

    Saves player names in session

--- app.rb
+++ app.rb
@@ -1,20 +1,23 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb(:home)
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
-    #redirect '/play'
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
 
-  # post '/play' do
-  #   erb(:play)
-  # end
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
