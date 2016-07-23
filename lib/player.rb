14886bbd49ba47c11cea680bd7f8f8cd5e479353
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-23 21:40:45 +0100

    Adds weapon attr and choose weapon to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :weapon
 
   def initialize(name)
     @name = name
+    @weapon = nil
+  end
+
+  def choose(weapon)
+    @weapon = weapon
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,18 @@ require 'player'
 
 describe Player do
   subject(:player_1){Player.new("Jonny")}
-  it "displays player name" do
+
+  it "is initialized with a name" do
     expect(player_1.name).to eq "Jonny"
   end
+
+  it "is initialized with no weapon" do
+    expect(player_1.weapon). to be nil
+  end
+
+  it "can choose and store a weapon" do
+    player_1.choose("Paper")
+    expect(player_1.weapon). to eq "Paper"
+  end
+
 end
