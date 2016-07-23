49198ebfb0a19556c732a359d259da60de47a308
hannako <jessjones1@gmail.com>
2016-07-23 12:54:48 +0100

    Adds three weapon buttons to the play page. Adds a results page.

--- app/rps.rb
+++ app/rps.rb
@@ -4,9 +4,13 @@ class RPS < Sinatra::Base
   get '/' do
     erb(:index)
   end
-  
+
   post '/play' do
     erb(:play)
+  end
+
+  get '/result' do
+    erb(:result)
   end
 
   # start the server if ruby file executed directly
--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,10 +1,4 @@
 feature 'Playing the game' do
-  scenario 'The player is presented a choice of weapons' do
-    visit '/'
-    fill_in :name, with: 'Jess'
-    click_button 'Play'
-    expect(page).to have_content 'Rock Paper Scissors'
-  end
 
   scenario 'The player can click on a button to select a weapon' do
     visit '/'
