74d3e2056495f4265ed0b35e71fd4f7fb309a860
Jose <jcopoviking@outlook.com>
2016-07-23 23:23:01 +0100

    Fixes routing for play.rb and typos in tests

--- rps.rb
+++ rps.rb
@@ -1,16 +1,23 @@
 require 'sinatra/base'
+require_relative 'lib/player'
 
 class Rps < Sinatra::Base
-  set :sessions, true
+
+enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post '/names' do
-    human_player = Player.new(params[:human_player])
-    redirect 'play'
+  post '/player_name' do
+    @human_player = Player.new(params[:human_player])
+    erb(:play)
   end
+
+  # get '/play' do
+  #   erb(:play)
+  # end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/enter_player_spec.rb
+++ spec/features/enter_player_spec.rb
@@ -1,6 +1,6 @@
 feature "Enter player name" do
   scenario "Human player enters their name" do
     register_and_play
-    expect(page).to have_text("Ghengis")
+    expect(page).to have_text("Genghis")
   end
 end
