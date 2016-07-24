2bcf9b5bd537153f1270b8a999f5737f38f57ee7
sus111 <sgraham535@gmail.com>
2016-07-24 06:41:56 +0100

    add radio buttons

--- app.rb
+++ app.rb
@@ -3,14 +3,20 @@ require 'sinatra/base'
 # require_relative './lib/game'
 
 class RPS < Sinatra::Base
+  attr_reader :player_1, :weapon
 
   get '/' do
     erb(:index)
   end
-attr_reader :player_1
+
   post '/names' do
     @player_1 = params[:player_1_name]
     erb(:play)
+  end
+
+  post '/select' do
+    @weapon = params[:weapon]
+    erb(:attack)
   end
 
   run! if app_file == $0
--- spec/features/choose_weapon_spec.rb
+++ spec/features/choose_weapon_spec.rb
@@ -1,4 +1,4 @@
-feature 'Choose weapon' do
+feature 'Present weapon choices' do
   scenario 'choose rock weapon' do
     sign_in_and_play
     expect(page).to have_content "Rock"
@@ -12,5 +12,28 @@ feature 'Choose weapon' do
   scenario 'choose scissors weapon' do
     sign_in_and_play
     expect(page).to have_content "Scissors"
+  end
+end
+
+feature 'Choose weapon ' do
+  scenario 'choose rock weapon' do
+    sign_in_and_play
+    choose('Rock')
+    click_button('Choose')
+    expect(page).to have_content "You chose Rock"
+  end
+
+  scenario 'choose paper weapon' do
+    sign_in_and_play
+    choose('Paper')
+    click_button('Choose')
+    expect(page).to have_content "You chose Paper"
+  end
+
+  scenario 'choose rock weapon' do
+    sign_in_and_play
+    choose('Scissors')
+    click_button('Choose')
+    expect(page).to have_content "You chose Scissors"
   end
 end
