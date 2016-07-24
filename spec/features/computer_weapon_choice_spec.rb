ea6d5f270f253727ffd429e522112387c209206d
sus111 <sgraham535@gmail.com>
2016-07-24 20:38:28 +0100

    game test passing

--- spec/features/computer_weapon_choice_spec.rb
+++ spec/features/computer_weapon_choice_spec.rb
@@ -5,7 +5,7 @@ feature 'Ask computer to choose weapon' do
     expect(page).to have_content "Click 'View' to find out your opponent\'s weapon!"
   end
 
-  scenario 'Click view and see opponent\'s chose Rock' do
+  scenario 'Click view and see opponent chose Rock' do
     allow(Computer).to receive(:random_select).and_return("Rock")
     sign_in_and_play
     rock_choice
