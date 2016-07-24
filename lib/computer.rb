1a5abce655508e839414d1727dde5299fd581e4c
richo225 <richo225@gmail.com>
2016-07-24 17:22:17 +0100

    Computer picks a weapon at random

--- lib/computer.rb
+++ lib/computer.rb
@@ -4,4 +4,13 @@ class Computer
   def initialize
     @choice
   end
+
+  def choose_weapon
+    @choice = random_weapon
+  end
+
+  def random_weapon
+    ["Rock", "Paper", "Scissors"].sample
+  end
+
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -7,4 +7,17 @@ describe Computer do
       expect(computer.choice).to eq nil
     end
   end
+  describe "#random_weapon" do
+    it "returns a random weapon" do
+      allow(computer).to receive(:random_weapon).and_return("Scissors")
+      expect(computer.random_weapon).to eq "Scissors"
+    end
+  end
+  describe "#choose_weapon" do
+    it "assigns random weapon to choice" do
+      allow(computer).to receive(:random_weapon).and_return("Scissors")
+      computer.choose_weapon
+      expect(computer.choice).to eq "Scissors"
+    end
+  end
 end
