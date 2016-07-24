44fff3d88709236e05203b0512fae7ef1de92acf
JulieLev <jkgee1@yahoo.co.uk>
2016-07-24 17:53:17 +0100

    Radio buttons removed and replaced with form submit buttons. Feature tests edited for this change, all pass.

--- spec/features/show_result_spec.rb
+++ spec/features/show_result_spec.rb
@@ -6,29 +6,25 @@ RSpec.feature "Result is Displayed", :type => :feature do
   end
 
   scenario "Computer's choice of weapon is displayed" do
-    choose 'rock'
-    click_button "Submit"
+    click_button "Rock"
 
     expect(page).to have_content "I have selected Paper..."
   end
 
   scenario "User selects Rock" do
-    choose 'rock'
-    click_button "Submit"
+    click_button "Rock"
 
     expect(page).to have_content "Paper beats Rock... I win!"
   end
 
   scenario "User selects Paper" do
-    choose 'paper'
-    click_button "Submit"
+    click_button "Paper"
 
     expect(page).to have_content "We both chose Paper... It's a Draw!"
   end
 
   scenario "User selects Scissors" do
-    choose 'scissors'
-    click_button "Submit"
+    click_button "Scissors"
 
     expect(page).to have_content "Scissors beat Paper... You Win!"
   end
--- spec/features/user_weapon_select_spec.rb
+++ spec/features/user_weapon_select_spec.rb
@@ -4,22 +4,19 @@ RSpec.feature "User selects their weapon", :type => :feature do
   end
 
   scenario "User selects Rock" do
-    choose 'rock'
-    click_button "Submit"
+    click_button "Rock"
 
     expect(page).to have_content "Julie, you selected Rock."
   end
 
   scenario "User selects Paper" do
-    choose 'paper'
-    click_button "Submit"
+    click_button "Paper"
 
     expect(page).to have_content "Julie, you selected Paper."
   end
 
   scenario "User selects Scissors" do
-    choose 'scissors'
-    click_button "Submit"
+    click_button "Scissors"
 
     expect(page).to have_content "Julie, you selected Scissors."
   end
