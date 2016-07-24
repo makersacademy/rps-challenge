56375ae9138d7cdb1561f509c46d014c53870bf3
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-24 14:28:09 +0100

    Outcome screen displays win/lose/draw

--- spec/features/outcome_spec.rb
+++ spec/features/outcome_spec.rb
@@ -9,7 +9,18 @@ describe 'Outcome page' do
       choose("paper")
       click_button "Feeling pretty good about myself right now"
       click_button "..."
-      expect(page).to have_content "It seems you got lucky this time!"
+      expect(page).to have_content "It seems you got lucky this time! You WIN!"
+    end
+  end
+
+  context "You draw" do
+    scenario 'displays the outcome of the battle' do
+      sign_in_and_play
+      click_button "I was born ready"
+      choose("paper")
+      click_button "Feeling pretty good about myself right now"
+      click_button "..."
+      expect(page).to have_content "Great minds think alike. It's a draw!"
     end
   end
 
@@ -20,7 +31,7 @@ describe 'Outcome page' do
       choose("paper")
       click_button "Feeling pretty good about myself right now"
       click_button "..."
-      expect(page).to have_content "Oh oh, that was a bit embarrassing!"
+      expect(page).to have_content "Oh oh, that was a bit embarrassing! You LOSE!"
     end
   end
 
