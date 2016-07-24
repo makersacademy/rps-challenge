fc2ef756a03f510d82319267113eec721395746a
richo225 <richo225@gmail.com>
2016-07-25 00:04:13 +0100

    Update app with styling. Still need to rm global variables

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,6 +1,6 @@
 feature "Enter name" do
   scenario "user can enter and view their name" do
     sign_in_and_play
-    expect(page).to have_content "Welcome Richard!"
+    expect(page).to have_content "Welcome Richard"
   end
 end
