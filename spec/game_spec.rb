dc1dd81c42ed66c245f339545495d56180fb4ef2
Mali Michael <malimccalla@gmail.com>
2016-07-24 13:15:59 +0100

    Adds game specs

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,4 +2,28 @@ require 'game'
 
 describe Game do
 
+  let(:player) {double(:player)}
+  subject {described_class.new(player)}
+
+  describe '#player' do
+    it 'returns the player' do
+      expect(subject.player).to eq player
+    end
+  end
+
+  describe '#npc_score' do
+
+    it 'starts at zero' do
+      expect(subject.npc_score).to be_zero
+    end
+
+    it 'increments if player loses' do
+      allow(subject).to receive(:result).and_return "YOU LOSE!"
+      subject.calculate_score
+      expect(subject.npc_score).to eq 1
+    end
+
+  end
+
+
 end
