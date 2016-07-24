1514028a964df370f6728e387d8d014b8adf25ab
JulieLev <jkgee1@yahoo.co.uk>
2016-07-24 13:49:02 +0100

    Feature tests for Results added (not yet passed)

--- spec/features/show_result_spec.rb
+++ spec/features/show_result_spec.rb
@@ -10,14 +10,27 @@ RSpec.feature "Result is Displayed", :type => :feature do
     click_button "Submit"
 
     expect(page).to have_content "I have selected Paper..."
-
   end
-  # scenario "User selects Rock" do
-  #   choose 'rock'
-  #   click_button "Submit"
-  #
-  #   expect(page).to have_content ""
-  # end
 
+  scenario "User selects Rock" do
+    choose 'rock'
+    click_button "Submit"
+
+    expect(page).to have_content "Paper beats Rock... I win!"
+  end
+
+  scenario "User selects Paper" do
+    choose 'paper'
+    click_button "Submit"
+
+    expect(page).to have_content "We both chose Paper... It's a Draw!"
+  end
+
+  scenario "User selects Scissors" do
+    choose 'scissors'
+    click_button "Submit"
+
+    expect(page).to have_content "Rock beats Scissors... You Win!"
+  end
 
 end
