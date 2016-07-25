e8574799dfd8c0f8f91c7de80d7da4dbd16937bc
Harry Jaggs <harryjaggs92@gmail.com>
2016-07-25 08:13:17 +0100

    added test coverage

---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'sinatra/base'
+require './lib/player'
+require './lib/machine'
+
+class Rock_paper_scissors < Sinatra::Base
+
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    $player_1 = Player.new(params[:player_1_name])
+    @player_1_name = $player_1.name
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = $player_1.name
+    erb :play
+  end
+
+  post '/game' do
+    @player_1_name = $player_1.name
+    $selected_object = params[:rps]
+    @human_object = $selected_object
+    $machine = Machine.new
+    @machine = $machine
+    @machine_object = @machine.choose_object
+    erb :game
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
