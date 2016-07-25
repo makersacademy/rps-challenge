27a1d0f53de49d4ad8c32901c314f78be0d19bd5
Jack Oddy <jackoddy91@gmail.com>
2016-07-25 08:48:33 +0100

    removes unused double

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,7 +4,7 @@ describe Player do
   let(:weapons) {double(:weapons, new: weapons_chooser)}
   let(:weapons_chooser) {double(:weapons_chooser)}
   let(:subject) {described_class.new('Jack', weapons: weapons)}
-  let(:weps_choices) {double(:choices_const)}
+  
   describe '#name' do
     it 'knows it\'s name' do
       expect(subject.name).to eq 'Jack'
