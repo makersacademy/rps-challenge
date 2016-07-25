e8494d69ff1a87ad65f68d7cf10e64f95bce58c9
Mali Michael <malimccalla@gmail.com>
2016-07-25 10:01:35 +0100

    adds feature tests for choosing different weapons

--- spec/features/play_chooses_weapon_spec.rb
+++ spec/features/play_chooses_weapon_spec.rb
@@ -5,4 +5,16 @@ feature 'Choose weapon' do
     click_button("Let's go!")
     expect(page).to have_content 'You chose rock'
   end
+  scenario 'Player chooses paper' do
+    sign_in_and_play
+    choose('paper')
+    click_button("Let's go!")
+    expect(page).to have_content 'You chose paper'
+  end
+  scenario 'Player chooses scissors' do
+    sign_in_and_play
+    choose('scissors')
+    click_button("Let's go!")
+    expect(page).to have_content 'You chose scissors'
+  end
 end
