0866a2d293e84ece0d23b26694500ece46ca0769
lukew244 <luke.webster86@gmail.com>
2016-07-25 08:18:34 +0100

    Updates README and tests

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -12,29 +12,49 @@ describe 'features' do
     end
   end
 
-    feature 'play game' do
-      scenario 'Plays a game' do
+    feature 'singleplayer' do
+      scenario 'Plays a singleplayer game' do
         visit '/'
         fill_in :player1_name, with: 'Luke'
         click_button('submit')
-        click_button('rock')
+        click_button('rock', :match => :first)
+        click_button('fight')
         expect(page).to have_content 'Luke chose rock'
       end
     end
 
+    feature 'multiplayer' do
+      scenario 'Player 1 can win' do
+        visit '/'
+        fill_in :player1_name, with: 'Luke'
+        fill_in :player2_name, with: 'Sinatra'
+        click_button('submit')
+        click_button('rock', :match => :first)
+        click_button('p2 scissors')
+        click_button('fight')
+        expect(page).to have_content 'Luke wins!'
+      end
 
+      scenario 'Player 2 can win' do
+        visit '/'
+        fill_in :player1_name, with: 'Luke'
+        fill_in :player2_name, with: 'Sinatra'
+        click_button('submit')
+        click_button('paper', :match => :first)
+        click_button('p2 scissors')
+        click_button('fight')
+        expect(page).to have_content 'Sinatra wins'
+      end
 
-
-    # feature 'weapon' do
-    #   scenario 'Player can choose a weapon' do
-    #     visit '/'
-    #     fill_in :player1_name, with: 'Luke'
-    #     click_button('submit')
-    #     click_button('rock')
-    #     expect(page).to have_content 'The game is over!'
-    #   end
-    # end
-
-
-
+      scenario 'Players can draw' do
+        visit '/'
+        fill_in :player1_name, with: 'Luke'
+        fill_in :player2_name, with: 'Sinatra'
+        click_button('submit')
+        click_button('paper', :match => :first)
+        click_button('p2 paper')
+        click_button('fight')
+        expect(page).to have_content 'It\'s a draw!'
+      end
+    end
 end
