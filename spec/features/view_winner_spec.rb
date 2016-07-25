2ce9d9303bff5f8973aa335e7d1ec40cd0e1c732
sus111 <sgraham535@gmail.com>
2016-07-25 10:27:20 +0100

    formatting

--- spec/features/view_winner_spec.rb
+++ spec/features/view_winner_spec.rb
@@ -1,14 +1,14 @@
 feature 'View winner' do
 
   scenario "When I submit 'Rock' I am told I have won" do
-    allow(Computer).toreceive(:random_select).and_return(:Scissors)
+    allow(Computer).to receive(:random_select).and_return(:Scissors)
     rock_choice
     click_button('View')
     expect(page).to have_content "You win!!"
   end
 
   scenario "When I submit 'Rock' I am told I have lost" do
-    allow(Computer).toreceive(:random_select).and_return(:Paper)
+    allow(Computer).to receive(:random_select).and_return(:Paper)
     rock_choice
     click_button('View')
     expect(page).to have_content "You lose!!"
