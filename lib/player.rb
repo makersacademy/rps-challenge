7314b91363be55d93596295ada61ca32489954e1
samjbro <sjbroughton1@gmail.com>
2016-07-25 09:39:54 +0100

    removes unneccessary getter methods

--- lib/player.rb
+++ lib/player.rb
@@ -2,25 +2,15 @@ require_relative 'weapon'
 
 class Player
 
-	private
 	attr_reader :name, :weapon
-	public
 
 	def initialize name: nil
 		name ||= "Computer"
 		@name = name
 	end
 
-	def name
-		@name
-	end
-
 	def choose_weapon weapon: nil
 		@weapon = Weapon.new(type: weapon)
-	end
-
-	def weapon
-		@weapon
 	end
 
 end
--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,17 +1,11 @@
 class Weapon
 TYPES = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
 	
-	private
 	attr_reader :type
-	public
 
 	def initialize type: nil
 		type ||= TYPES[Kernel.rand(0..TYPES.length-1)]
 		@type = type
-	end
-
-	def type
-		@type
 	end
 
 	def score
