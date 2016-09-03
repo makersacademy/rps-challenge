a34511dae09c68c74f7f8856a8393b6aa806ba2a
Alfie <alfie_ab@hotmail.co.uk>
2016-09-03 15:30:41 +0100

    first user story done and rspec working

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
+require_relative '../../app'
 
 feature 'enter_name' do
   scenario 'player enters name' do
+    sign_in_and_play
     expect(page).to have_content("Welcome to rock, paper, scissors, Alfie!")
   end
 end
