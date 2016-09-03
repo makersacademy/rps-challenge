c405e275283428b931e33d3e38aa0e9e5ff631ab
mrmurtz <murtaza.s.abidi@gmail.com>
2016-09-03 12:51:52 +0100

    Redirects with player1's choice of rock, paper, scissors shown

--- app.rb
+++ app.rb
@@ -15,9 +15,14 @@ class Rps < Sinatra::Base
 
   get '/play' do
     @player1 = session[:player1]
+    @player1_choice = session[:player1_choice]
     erb(:play)
   end
 
+  post '/play' do
+    session[:player1_choice] = params[:player1_choice]
+    redirect '/play'
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,10 +1,18 @@
 feature "playing Rps" do
-  # presented the choices:
-  #(rock, paper and scissors)
+
   scenario 'see rock, paper, scissor choices on page' do
+    # presented the choices:
+    # (rock, paper and scissors)
     enter_name_and_play
     expect(page).to have_button 'Rock'
     expect(page).to have_button 'Paper'
     expect(page).to have_button 'Scissors'
+  end
+
+  scenario 'can see which they chose' do
+    # player1's selection is shown on play page
+    enter_name_and_play
+    click_button 'Rock'
+    expect(page).to have_content('You chose Rock')
   end
 end
