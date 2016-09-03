14e457bdcde3daf7bb017110f3576587d499378c
TomStuart92 <Thomascstuart@gmail.com>
2016-09-03 15:18:58 +0100

    Improve Visuals

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -4,7 +4,7 @@ require_relative './helpers.rb'
 feature 'In Single Player Game' do
   scenario 'User is presented with RPS choices' do
     single_player_sign_in_helper
-    expect(page).to have_content('Please choose your attack:')
+    expect(page).to have_content('Choose Your Attack')
   end
   scenario 'User chooses link' do
     single_player_sign_in_helper
@@ -16,11 +16,11 @@ end
 feature 'In Multiplayer Game' do
   scenario 'Player 1 is presented with RPS choices' do
     multiplayer_sign_in_helper
-    expect(page).to have_content('Please choose your attack Player 1')
+    expect(page).to have_content('Player 1 Choose Your Attack')
   end
   scenario 'Player 2 is presented with RPS choices' do
     multiplayer_sign_in_helper
     click_link('Rock')
-    expect(page).to have_content('Please choose your attack Player 2')
+    expect(page).to have_content('Player 2 Choose Your Attack')
   end
 end
--- spec/features/new_user_spec.rb
+++ spec/features/new_user_spec.rb
@@ -4,11 +4,11 @@ require_relative './helpers.rb'
 feature 'New User arrives at root page' do
   scenario 'Displays form on the home page' do
     visit('/')
-    expect(page).to have_content('Name')
+    expect(find_field('player_name').value).to eq 'Enter Your Name'
   end
 
   scenario 'Redirects to game page with welcome message' do
     single_player_sign_in_helper
-    expect(page).to have_content("Welcome to the Game, Reginald!")
+    expect(page).to have_content("Reginald")
   end
 end
