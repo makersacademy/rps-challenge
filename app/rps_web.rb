cf4791d05367f4037c39a61288d32c6bad694677
Tim3tang <Tim3tang@gmail.com>
2016-09-04 18:35:45 +0100

    failing build - unable to find field 'name' refactored to jafrog exemplar

--- app/rps_web.rb
+++ app/rps_web.rb
@@ -1,21 +1,28 @@
 require 'sinatra/base'
 
-enable :sessions
+class RPS < Sinatra::Base
+  enable :sessions
 
-class RPSWeb < Sinatra::Base
   get '/' do
     erb :index
   end
 
   post '/name' do
-    session[:name] = params[:name]
+    session[:player_name] = params[:name]
     redirect '/play'
   end
 
   get '/play' do
-    @name = session[:name]
+    @turn = Turn.new(session)
     erb :play
   end
 
+  post '/play' do
+    session[:player_shape] = params[:shape].downcase.to_sym
+    session[:opponent_shape] = Opponent.new.shape
+    redirect '/play'
+  end
+
+  # start the server if ruby file executed directly
   run! if app_file == $0
 end
