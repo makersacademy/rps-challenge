377c5ea7d99be5e6aaa2d22fa5c730fefde30814
ercekal <ercekalabalikoglu@gmail.com>
2016-09-04 23:32:14 +0100

    final correction for feature testing is done

--- spec/features/selection_spec.rb
+++ spec/features/selection_spec.rb
@@ -24,7 +24,7 @@ feature 'Select tool' do
       allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
       sign_in_and_play
       click_button "Rock"
-      expect(page).to have_content 'Computer has won'
+      expect(page).to have_content 'You lose!'
     end
   end
 end
