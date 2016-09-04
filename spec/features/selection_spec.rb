d7735a3734323f7509092eb8bfd5fb64a204b844
ercekal <ercekalabalikoglu@gmail.com>
2016-09-04 22:06:26 +0100

    feature test for the result is added

--- spec/features/selection_spec.rb
+++ spec/features/selection_spec.rb
@@ -18,4 +18,13 @@ feature 'Select tool' do
     click_button "Paper"
     expect(page).to have_content 'Erce selected Paper'
   end
+
+  feature 'Result of the game' do
+    scenario "When player submits 'Rock' and computer is 'Paper', it shows that computer won" do
+      allow_any_instance_of(Array).to receive(:sample).and_return('Paper')
+      sign_in_and_play
+      click_button "Rock"
+      expect(page).to have_content 'Computer has won'
+    end
+  end
 end
