5fc7378a5d9d253cb07dda0ae4032fc8a80cab25
Stéphan Gaud <stephgoeseast@gmail.com>
2016-09-04 22:55:42 +0100

    add test for class Game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,12 @@
 require 'game'
 
 describe Game do
-  subject(:game) {described_class.new(player)}
+  subject(:game) {described_class.new("stephan")}
   let (:robot) {double (:robot)}
 
   describe '#compare_rock' do
     it 'give a winner' do
+      allow(game).to receive(:compare_rock).and_return("draw")
       expect(game.compare_rock).to eq "draw"
     end
   end
