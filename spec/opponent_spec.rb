e1d897faf008f252ec7c3882eb2dc11aeba94041
Tim3tang <Tim3tang@gmail.com>
2016-09-04 19:56:08 +0100

    failing build - unable to find field 'name' refactored to jafrog exemplar

---
+++ spec/opponent_spec.rb
@@ -0,0 +1,11 @@
+require 'opponent'
+
+describe Opponent do
+  subject(:opponent) { described_class.new }
+
+  describe '#shape' do
+    it 'returns a random shape' do
+      expect(Opponent::SHAPES).to include opponent.shape
+    end
+  end
+end
