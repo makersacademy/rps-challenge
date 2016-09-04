ac69d3bafa922529f4278d1e12521b6c377d3310
lsewilson <laurawilson@hotmail.co.uk>
2016-09-04 20:50:23 +0100

    adds tests for all outcomes

---
+++ README.md
@@ -0,0 +1,24 @@
+# Rock, Paper, Scissors Challenge
+
+
+## Description
+
+This repository is a simple web application where a user can play rock, paper, scissors
+against a computer player.
+
+## Set Up Instructions
+```
+$ git clone https://github.com/lsewilson/rps-challenge.git
+$ cd rps-challenge
+$ bundle
+$ rackup
+```
+## Screenshots
+
+1. Navigate to localhost:9292 in your web browser.
+
+2. Name entry and game initiation:
+
+3. Option selection:
+
+4. Result:
--- spec/features/outcome_spec.rb
+++ spec/features/outcome_spec.rb
@@ -4,25 +4,69 @@ feature 'displays results' do
 
   let(:computer) { double :computer }
 
-  scenario 'draw between computer and player' do
+  before do
     sign_in_and_play
-    allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
-    click_button 'rock'
-    expect(page).to have_content("It's a DRAW!")
   end
 
-  scenario 'player wins' do
-    sign_in_and_play
-    allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
-    click_button 'rock'
-    expect(page).to have_content("You WON!")
+  feature 'player selects rock' do
+    scenario 'draw between computer and player' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+      click_button 'rock'
+      expect(page).to have_content("It's a DRAW!")
+    end
+
+    scenario 'player wins' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
+      click_button 'rock'
+      expect(page).to have_content("You WON!")
+    end
+
+    scenario 'computer wins' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
+      click_button 'rock'
+      expect(page).to have_content("You LOST!")
+    end
   end
 
-  scenario 'computer wins' do
-    sign_in_and_play
-    allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
-    click_button 'rock'
-    expect(page).to have_content("You LOST!")
+  feature 'player selects paper' do
+    scenario 'draw between computer and player' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
+      click_button 'paper'
+      expect(page).to have_content("It's a DRAW!")
+    end
+
+    scenario 'player wins' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+      click_button 'paper'
+      expect(page).to have_content("You WON!")
+    end
+
+    scenario 'computer wins' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
+      click_button 'paper'
+      expect(page).to have_content("You LOST!")
+    end
   end
+
+  feature 'player selects scissors' do
+    scenario 'draw between computer and player' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
+      click_button 'scissors'
+      expect(page).to have_content("It's a DRAW!")
+    end
+
+    scenario 'player wins' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
+      click_button 'scissors'
+      expect(page).to have_content("You WON!")
+    end
+
+    scenario 'computer wins' do
+      allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+      click_button 'scissors'
+      expect(page).to have_content("You LOST!")
+    end
+  end
+
 
 end
