aaad19f4805aa4c5b1528c7092ade88cd39a6a43
Dionysis <Dionysis.k22@gmail.com>
2016-10-15 13:13:51 +0100

    player has selection of options for game

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -8,4 +8,25 @@ RSpec.feature Rps, :type => :feature do
     expect(page).to have_text("It's Dionysis vs RPS-bot")
   end
 
+  scenario "list of choices for player to select - rock" do
+
+    sign_in_and_play
+
+    expect(page).to have_button("Rock")
+  end
+
+  scenario "list of choices for player to select - paper" do
+
+    sign_in_and_play
+
+    expect(page).to have_button("Paper")
+  end
+
+  scenario "list of choices for player to select - paper" do
+
+    sign_in_and_play
+
+    expect(page).to have_button("Paper")
+  end
+
 end
