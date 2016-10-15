7c9736dbbd9b4f0be729fc0cc9d2a769b2e026d6
andy jenkins <andygjenkins@gmail.com>
2016-10-15 19:17:55 +0100

    Add .move method

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,13 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :player_move
 
   def initialize(name)
     @name = name
   end
 
-end 
+  def move(move)
+    @player_move = move
+  end
+
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,19 @@ require 'player'
 
 describe Player do
   subject(:dave) { described_class.new('Dave') }
+  let(:move) { double :rock }
 
   describe 'initialize' do
     it 'has a name' do
       expect(dave.name).to eq 'Dave'
     end
-  end   
+  end
+
+  describe '#move' do
+    it 'contains the players choice' do
+      dave.move(move)
+      expect(dave.player_move).to eq move
+    end
+  end
+
 end
