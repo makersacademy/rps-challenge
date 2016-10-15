fbb27a03a80fb40ae1ddc674c0b2d4ae2515646b
alexanderwjrussell <awjrussell@live.co.uk>
2016-10-15 18:20:01 +0100

    Capybara working on features tests now.

--- spec/features/new_game_spec.rb
+++ spec/features/new_game_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "New Game" do
   scenario "Expects the new game button to reset the game back to the open page" do
     sign_in_and_play_a_few_games
-    click_button "Rock"
+    click_button "ROCK"
     click_button "New Game"
     expect(page).to have_content("Rock, Paper, Scissors!")
   end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,15 +1,15 @@
 def sign_in_and_play
   visit "/"
   fill_in "player_1_name", :with => "Ali"
-  click_button "Sumbit Name"
+  click_button "Submit Name"
 end
 
 def sign_in_and_play_a_few_games
   sign_in_and_play
-  click_button "Rock"
+  click_button "ROCK"
   click_button "OK"
-  click_button "Rock"
+  click_button "ROCK"
   click_button "OK"
-  click_button "Rock"
+  click_button "ROCK"
   click_button "OK"
 end
