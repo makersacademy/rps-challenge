f7dec7b23bc89162c97c24a654e76ffa8a8f1589
crispinandrews <crispin.andrews@gmail.com>
2016-10-15 16:00:36 +0100

    Player can choose attack option and see results. All tests green

--- app.rb
+++ app.rb
@@ -15,8 +15,20 @@ class RPS < Sinatra::Base
 
   get '/play' do
     @name = session[:name]
+    @choice = session[:choice]
     erb(:play)
   end
+
+post '/choice' do
+  session[:choice] = params[:choice]
+  redirect '/result'
+end
+
+get '/result' do
+  @name = session[:name]
+  @choice = session[:choice]
+  erb(:result)
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 feature 'Register name' do
   scenario 'Player can register and see name before playing' do
-    visit('/')
-    fill_in(:player_1, with: 'Batman')
-    click_button('Submit')
+    sign_in
     expect(page).to have_content "Hello Batman!"
   end
 end
