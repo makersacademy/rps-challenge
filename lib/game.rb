d3a00a995327979916576561bd70ccc5503f39bc
Frances Maxwell <fmaxwell@gmail.com>
2016-10-15 18:25:13 +0100

    Wrote and passed unit test for determining winner

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,10 @@
+require_relative 'player'
+
 class Game
 
   def initialize(player = Player.new("Mystery Player"))
     @player = player
+    @opponent = "The Computer"
   end
 
   def view_choices
@@ -16,4 +19,26 @@ class Game
     @choices.sample
   end
 
+  def result
+    case
+      when @player_selection == :Rock && computer_selection == :Rock
+        "You both chose the same. It's a draw!"
+      when @player_selection == :Rock && computer_selection == :Paper
+        "The winner is #{@opponent}"
+      when @player_selection == :Rock && computer_selection == :Scissors
+        "The winner is #{@player.capitalize}"
+      when @player_selection == :Paper && computer_selection == :Rock
+        "The winner is #{@player.capitalize}"
+      when @player_selection == :Paper && computer_selection == :Paper
+        "You both chose the same. It's a draw!"
+      when @player_selection == :Paper && computer_selection == :Scissors
+        "The winner is #{@opponent}"
+      when @player_selection == :Scissors && computer_selection == :Rock
+        "The winner is #{@opponent}"
+      when @player_selection == :Scissors && computer_selection == :Paper
+        "The winner is #{@player.capitalize}"
+      when @player_selection == :Scissors && computer_selection == :Scissors
+        "You both chose the same. It's a draw!"
+    end
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,22 +2,33 @@ require 'Game'
 
 describe Game do
 
+  let(:frances)   { double :player, name: "Frances" }
+  subject(:game)  { described_class.new(:frances)   }
+
   describe '#view_choices' do
     it 'lists the choices available' do
-      expect(subject.view_choices).to eq [:Rock, :Paper, :Scissors]
+      expect(game.view_choices).to eq [:Rock, :Paper, :Scissors]
     end
   end
 
   describe '#player_selection' do
     it "brings back the player's selection" do
-      expect(subject.player_selection(:Rock)).to eq :Rock
+      expect(game.player_selection(:Rock)).to eq :Rock
     end
   end
 
   describe '#computer_selection' do
     it "chooses a move randomly" do
-      allow(subject).to receive(:computer_selection).and_return(:Scissors)
-      expect(subject.computer_selection).to eq :Scissors
+      allow(game).to receive(:computer_selection).and_return(:Scissors)
+      expect(game.computer_selection).to eq :Scissors
+    end
+  end
+
+  describe '#result' do
+    it "selects a winner (or determines a draw) according to the rules" do
+      game.player_selection(:Rock)
+      allow(game).to receive(:computer_selection).and_return(:Scissors)
+      expect(game.result).to eq "The winner is Frances"
     end
   end
 
