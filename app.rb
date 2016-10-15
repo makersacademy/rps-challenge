a22b80845f036f9203792026ae8943e319a81aff
James <bakerje@mac.com>
2016-10-15 15:02:54 +0100

    Created views for index, play and result

--- app.rb
+++ app.rb
@@ -1,22 +1,32 @@
 require 'sinatra/base'
+require './lib/player'
+require './lib/game'
 
 class RockPaperScissors < Sinatra::Base
+
+  use Rack::Session::Cookie,:key => 'rack.session',
+                            :path => '/',
+                            :secret => 'your_secret'
+
   get '/' do
-    'Hello rps-challenge!'
+    erb :index
   end
 
+  post '/name' do
+    player_1 = Player.new(params[:player_1_name])
+    $game = Game.new(player_1)
+    redirect '/play'
+  end
 
+  get '/play' do
+    @game = $game
+    erb :play
+  end
 
-
-
-
-
-
-
-
-
-
-
+  post '/result' do
+    @game = $game
+    erb :result
+  end
 
   run! if app_file == $0
 end
