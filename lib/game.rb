cb7ab8ed37f15337b4767c3ebb8e92b897947656
Frances Maxwell <fmaxwell@gmail.com>
2016-10-15 22:21:58 +0100

    Refactor including private methods and many more unit tests

--- lib/game.rb
+++ lib/game.rb
@@ -2,13 +2,16 @@ require_relative 'player'
 
 class Game
 
-  def initialize(player = Player.new("Mystery Player"))
+  attr_reader :player, :opponent
+
+  def initialize(player = Player.new("Mystery Player"), opponent = Player.new("The Computer"))
     @player = player
-    @opponent = "The Computer"
+    @opponent = opponent
+    @choices = [:Rock, :Paper, :Scissors]
   end
 
   def view_choices
-    @choices = [:Rock, :Paper, :Scissors]
+    @choices.dup
   end
 
   def player_selection(selection)
@@ -19,26 +22,45 @@ class Game
     @choices.sample
   end
 
-  def result
+  def result(opponent_choice = computer_selection)
     case
-      when @player_selection == :Rock && computer_selection == :Rock
-        "You both chose the same. It's a draw!"
-      when @player_selection == :Rock && computer_selection == :Paper
-        "The winner is #{@opponent}"
-      when @player_selection == :Rock && computer_selection == :Scissors
-        "The winner is #{@player.capitalize}"
-      when @player_selection == :Paper && computer_selection == :Rock
-        "The winner is #{@player.capitalize}"
-      when @player_selection == :Paper && computer_selection == :Paper
-        "You both chose the same. It's a draw!"
-      when @player_selection == :Paper && computer_selection == :Scissors
-        "The winner is #{@opponent}"
-      when @player_selection == :Scissors && computer_selection == :Rock
-        "The winner is #{@opponent}"
-      when @player_selection == :Scissors && computer_selection == :Paper
-        "The winner is #{@player.capitalize}"
-      when @player_selection == :Scissors && computer_selection == :Scissors
-        "You both chose the same. It's a draw!"
+      when @player_selection == :Rock && opponent_choice == :Rock
+        draw
+      when @player_selection == :Rock && opponent_choice == :Paper
+        paper_beats_rock(@opponent)
+      when @player_selection == :Rock && opponent_choice == :Scissors
+        rock_beats_scissors(@player)
+      when @player_selection == :Paper && opponent_choice == :Rock
+        paper_beats_rock(@player)
+      when @player_selection == :Paper && opponent_choice == :Paper
+        draw
+      when @player_selection == :Paper && opponent_choice == :Scissors
+        scissors_beat_paper(@opponent)
+      when @player_selection == :Scissors && opponent_choice == :Rock
+        rock_beats_scissors(@opponent)
+      when @player_selection == :Scissors && opponent_choice == :Paper
+        scissors_beat_paper(@player)
+      when @player_selection == :Scissors && opponent_choice == :Scissors
+        draw
     end
   end
+
+  private
+
+  def draw
+    "You both chose the same. It's a draw!"
+  end
+
+  def rock_beats_scissors(winner)
+    "Rock beats Scissors. The winner is #{winner.name}"
+  end
+
+  def paper_beats_rock(winner)
+    "Paper beats Rock. The winner is #{winner.name}"
+  end
+
+  def scissors_beat_paper(winner)
+    "Scissors beat Paper. The winner is #{winner.name}"
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,16 @@
-require 'Game'
+require 'game'
 
 describe Game do
 
-  let(:frances)   { double :player, name: "Frances" }
-  subject(:game)  { described_class.new(:frances)   }
+  # how do I create a successful double for this?
+  player = Player.new("Frances")
+  subject(:game)  { described_class.new(player)   }
+
+  describe '#initialize' do
+    it 'instantiates a game with a Player object' do
+      expect(subject.player).to be_an_instance_of Player
+    end
+  end
 
   describe '#view_choices' do
     it 'lists the choices available' do
@@ -25,10 +32,50 @@ describe Game do
   end
 
   describe '#result' do
-    it "selects a winner (or determines a draw) according to the rules" do
+    it "shows a draw (both chose rock)" do
+      game.player_selection(:Rock)
+      allow(game).to receive(:computer_selection).and_return(:Rock)
+      expect(game.result).to eq "You both chose the same. It's a draw!"
+    end
+    it "selects the computer as winner (paper beats rock)" do
+      game.player_selection(:Rock)
+      allow(game).to receive(:computer_selection).and_return(:Paper)
+      expect(game.result).to eq "Paper beats Rock. The winner is The Computer"
+    end
+    it "selects the player as winner (rock beats scissors)" do
       game.player_selection(:Rock)
       allow(game).to receive(:computer_selection).and_return(:Scissors)
-      expect(game.result).to eq "The winner is Frances"
+      expect(game.result).to eq "Rock beats Scissors. The winner is Frances"
+    end
+    it "selects the player as winner (paper beats rock)" do
+      game.player_selection(:Paper)
+      allow(game).to receive(:computer_selection).and_return(:Rock)
+      expect(game.result).to eq "Paper beats Rock. The winner is Frances"
+    end
+    it "shows a draw (both chose paper)" do
+      game.player_selection(:Paper)
+      allow(game).to receive(:computer_selection).and_return(:Paper)
+      expect(game.result).to eq "You both chose the same. It's a draw!"
+    end
+    it "selects the computer as winner (paper, scissors)" do
+      game.player_selection(:Paper)
+      allow(game).to receive(:computer_selection).and_return(:Scissors)
+      expect(game.result).to eq "Scissors beat Paper. The winner is The Computer"
+    end
+    it "selects the computer as winner (rock beats scissors)" do
+      game.player_selection(:Scissors)
+      allow(game).to receive(:computer_selection).and_return(:Rock)
+      expect(game.result).to eq "Rock beats Scissors. The winner is The Computer"
+    end
+    it "selects the player as winner (scissors, paper)" do
+      game.player_selection(:Scissors)
+      allow(game).to receive(:computer_selection).and_return(:Paper)
+      expect(game.result).to eq "Scissors beat Paper. The winner is Frances"
+    end
+    it "shows a draw (both chose scissors)" do
+      game.player_selection(:Scissors)
+      allow(game).to receive(:computer_selection).and_return(:Scissors)
+      expect(game.result).to eq "You both chose the same. It's a draw!"
     end
   end
 
