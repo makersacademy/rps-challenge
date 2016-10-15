9b84cb900af16200c5102fbb0748846839e0c3cb
Laura Weston <laura.weston@outlook.com>
2016-10-15 11:36:10 +0100

    Game has rock, paper and scissor options for player to choose from

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,18 +1,43 @@
 class Game
-  attr_reader :player, :computer
+  attr_reader :player, :player_selection
 
   def initialize(player)
     @player = player
+  end
+
+  def rock
+    @player_selection = :rock
+  end
+
+  def paper
+    @player_selection = :paper
+  end
+
+  def scissors
+    @player_selection = :scissors
   end
 end
 
 describe Game do
   let(:player) { double :player }
-  let(:computer) { double :computer }
   subject(:game) { described_class.new(player)}
 
   it "has a player" do
     expect(game.player).to eq player
   end
+
+  it "allows player to choose rock" do
+    game.rock
+    expect(game.player_selection).to eq :rock
+  end
+  it "allows player to choose paper" do
+    game.paper
+    expect(game.player_selection).to eq :paper
+  end
+  it "allows player to choose scissors" do
+    game.scissors
+    expect(game.player_selection).to eq :scissors
+  end
+
 
 end
