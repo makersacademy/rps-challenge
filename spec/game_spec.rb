b51ca2b78e5145b5f41f84a7fc896539c13d0efe
Laura Weston <laura.weston@outlook.com>
2016-10-15 11:58:08 +0100

    Requires a player to make a selection

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,39 +5,21 @@ class Game
     @player = player
   end
 
-  def rock
-    @player_selection = :rock
-  end
-
-  def paper
-    @player_selection = :paper
-  end
-
-  def scissors
-    @player_selection = :scissors
+  def play
+    player.make_move
   end
 end
 
 describe Game do
-  let(:player) { double :player }
+  let(:player) { double :player, :make_move => nil }
   subject(:game) { described_class.new(player)}
 
   it "has a player" do
     expect(game.player).to eq player
   end
-
-  it "allows player to choose rock" do
-    game.rock
-    expect(game.player_selection).to eq :rock
+  it "asks player to make a selection" do
+    game.play
+    expect(player).to have_received(:make_move)
   end
-  it "allows player to choose paper" do
-    game.paper
-    expect(game.player_selection).to eq :paper
-  end
-  it "allows player to choose scissors" do
-    game.scissors
-    expect(game.player_selection).to eq :scissors
-  end
-
 
 end
