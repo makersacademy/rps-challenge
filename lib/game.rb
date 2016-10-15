864f329d6f9f63aa206b0c2eed397e938ec19cff
Frances Maxwell <fmaxwell@gmail.com>
2016-10-16 00:07:14 +0100

    Names and selections showing but case statement not working. Took out private methods. Still not working

--- lib/game.rb
+++ lib/game.rb
@@ -31,27 +31,36 @@ class Game
   end
 
   def result(opponent_choice = computer_selection)
-    "#{@player} choice is #{@player_selection} and #{@opponent} choice is #{opponent_choice}"
-    # case
-    #   when @player_selection == :Rock && opponent_choice == :Rock
-    #     draw
-    #   when @player_selection == :Paper && opponent_choice == :Paper
-    #     draw
-    #   when @player_selection == :Scissors && opponent_choice == :Scissors
-    #     draw
-    #   when @player_selection == :Paper && opponent_choice == :Rock
-    #     paper_beats_rock(@player)
-    #   when @player_selection == :Rock && opponent_choice == :Paper
-    #     paper_beats_rock(@opponent)
-    #   when @player_selection == :Rock && opponent_choice == :Scissors
-    #     rock_beats_scissors(@player)
-    #   when @player_selection == :Scissors && opponent_choice == :Rock
-    #     rock_beats_scissors(@opponent)
-    #   when @player_selection == :Scissors && opponent_choice == :Paper
-    #     scissors_beat_paper(@player)
-    #   when @player_selection == :Paper && opponent_choice == :Scissors
-    #     scissors_beat_paper(@opponent)
-    # end
+    "#{@player.name} choice is #{@player_selection} and #{@opponent.name} choice is #{opponent_choice}"
+    case
+      when @player_selection == "Rock" && opponent_choice == "Rock"
+        "You both chose the same. It's a draw!"
+        # draw
+      when @player_selection == "Paper" && opponent_choice == "Paper"
+        "You both chose the same. It's a draw!"
+        # draw
+      when @player_selection == "Scissors" && opponent_choice == "Scissors"
+        "You both chose the same. It's a draw!"
+        # draw
+      when @player_selection == "Paper" && opponent_choice == "Rock"
+        "Paper beats Rock. The winner is #{@player.name}"
+        # paper_beats_rock(@player)
+      when @player_selection == "Rock" && opponent_choice == "Paper"
+        "Paper beats Rock. The winner is #{@opponent.name}"
+        # paper_beats_rock(@opponent)
+      when @player_selection == "Rock" && opponent_choice == "Scissors"
+        "Rock beats Scissors. The winner is #{@player.name}"
+        # rock_beats_scissors(@player)
+      when @player_selection == "Scissors" && opponent_choice == "Rock"
+        "Rock beats Scissors. The winner is #{@opponent.name}"
+        # rock_beats_scissors(@opponent)
+      when @player_selection == "Scissors" && opponent_choice == "Paper"
+        "Scissors beat Paper. The winner is #{@player.name}"
+        # scissors_beat_paper(@player)
+      when @player_selection == "Paper" && opponent_choice == "Scissors"
+        "Scissors beat Paper. The winner is #{@opponent.name}"
+        # scissors_beat_paper(@opponent)
+    end
   end
 
   private
