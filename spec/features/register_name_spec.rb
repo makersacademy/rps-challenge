efe15c95310a1f1717efe38b4c91c8c3e4f14517
Chetan Sanghani <crsanghani@gmail.com>
2016-10-15 02:06:54 +0100

    basic setup and testing done

---
+++ spec/features/register_name_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'Register name' do
+ scenario 'name entered' do
+   visit('/')
+   fill_in :name, with: 'Frances'
+   click_button "Let's play"
+   expect(page).to have_content 'Hello Frances'
+ end
+
+end
