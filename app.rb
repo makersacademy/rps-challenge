20b16bb08f5248eeb87fdecb5761dcd92d98a433
Chetan Sanghani <crsanghani@gmail.com>
2016-10-15 12:39:20 +0100

    first test passing and some basic styling done

--- app.rb
+++ app.rb
@@ -3,10 +3,16 @@ require 'haml'
 require 'rspec'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
     haml :index
   end
 
+  post '/name' do
+    @player_1_name = params[:player_1_name]
+    haml :play
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 feature 'Register name' do
  scenario 'name entered' do
    visit('/')
-   fill_in :name, with: 'Frances'
-   click_button "Let's play"
-   expect(page).to have_content 'Hello Frances'
+   fill_in :player_1_name, with: 'Chet'
+   click_button "3 2 1 Draw!"
+   expect(page).to have_content "It's time Chet"
  end
-
 end
