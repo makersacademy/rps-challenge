7ecacabfc8f9777010b96375d01197be2a83430b
Laura Weston <laura.weston@outlook.com>
2016-10-16 10:02:27 +0100

    Player can make a move

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,8 +1,13 @@
 require 'player'
 
 describe Player do
+  subject(:player) { described_class.new("Laura") }
   it "has a name" do
-    player = Player.new("Laura")
     expect(player.name).to eq "Laura"
+  end
+
+  it "can make a move" do
+    player.make_move(:rock)
+    expect(player.move).to eq :rock
   end
 end
