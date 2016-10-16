d42d9aba38af1c5cca0542d9c4d4263d9527d525
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-10-16 14:10:20 +0100

    Added game outcome to model

--- lib/game.rb
+++ lib/game.rb
@@ -24,4 +24,24 @@ class Game
     @computer_choice = CHOICES.sample
   end
 
+  def outcome
+    winning_situations
+    losing_situations
+  end   
+
+  def win?
+    @win
+  end
+
+  def losing_situations
+    @win = false if (choice == "Rock" && computer_choice == "Paper") || 
+    (choice == "Scissors" && computer_choice == "Rock") ||
+    (choice == "Paper" && computer_choice == "Scissors")
+  end
+
+  def winning_situations
+    @win = true if (choice == "Rock" && computer_choice == "Scissors") ||
+    (choice == "Scissors" && computer_choice == "Paper") ||  
+    (choice == "Paper" && computer_choice == "Rock")
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -23,5 +23,90 @@ describe Game do
   	end
   end
 
+  context 'winning situations' do
+    it 'sets player as winner if player chooses rock and computer chooses scissors' do
+      allow(game).to receive(:computer_choice) { "Scissors" }
+      game.move("Rock")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_truthy
+    end
 
+    it 'sets player as winner if player chooses scissors and computer chooses paper' do
+      allow(game).to receive(:computer_choice) { "Paper" }
+      game.move("Scissors")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_truthy
+    end
+
+    it 'sets player as winner if player chooses paper and computer chooses rock' do
+      allow(game).to receive(:computer_choice) { "Rock" }
+      game.move("Paper")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_truthy
+    end
+  end
+  
+  context 'losing situations' do
+    it 'sets as loser if player chooses rock and computer chooses paper' do
+      allow(game).to receive(:computer_choice) { "Paper" }
+      game.move("Rock")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_falsey
+    end
+
+    it 'sets as loser if player chooses scissors and computer chooses rock' do
+      allow(game).to receive(:computer_choice) { "Rock" }
+      game.move("Scissors")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_falsey
+    end
+
+    it 'sets as loser if player chooses paper and computer chooses scissors' do
+      allow(game).to receive(:computer_choice) { "Scissors" }
+      game.move("Paper")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_falsey
+    end
+
+  end
+
+  context 'draw situations' do
+    it 'sets the game to a draw if player and computer both choose rock' do
+      allow(game).to receive(:computer_choice) { "Rock" }
+      game.move("Rock")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_nil
+    end
+
+    it 'sets the game to a draw if player and computer both choose scissors' do
+      allow(game).to receive(:computer_choice) { "Scissors" }
+      game.move("Scissors")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_nil
+    end
+
+    it 'sets the game to a draw if player and computer both choose paper' do
+      allow(game).to receive(:computer_choice) { "Paper" }
+      game.move("Paper")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to be_nil
+    end
+
+    it 'does not set the game to a draw if computer beats player' do
+      allow(game).to receive(:computer_choice) { "Scissors" }
+      game.move("Paper")
+      game.computer_move
+      game.outcome
+      expect(game.win?).to_not be_nil
+    end
+  end
 end
