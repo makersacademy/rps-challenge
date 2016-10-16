b97a38545973ae29a672b82e2f3e280109f5f127
Laurent Bouhnik <misterbl@hotmail.com>
2016-10-16 18:54:56 +0100

    more refactoring

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,7 @@
 require 'game'
 
 describe Game do
+  let(:players[0]) {double :players[0]}
   let(:player) {double :player}
   subject(:game) {described_class.new(player)}
 
@@ -17,5 +18,25 @@ describe Game do
     end
   end
 
+  describe "#add_compu_points" do
+    it "adds 1 point to the computer points" do
+      expect{game.add_compu_points}.to change{game.compu_points}.by 1
+    end
+  end
 
+  # describe "#check" do
+  #   it "checks if the player or computer has won" do
+  #     game = class_double("Game")
+  #     as_stubbed_const(:check => '/won_game')
+  #     expect(game.check).to eq '/won_game'
+  #   end
+  # end
+  #
+  # describe "#two_check" do
+  #   it "checks if one of the player has won" do
+  #       game = instance_double("Game")
+  #       allow(game).to receive(:two_check)
+  #     expect(game.check).to eq nil
+  #   end
+  # end
 end
