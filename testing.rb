5b7b41b69c7913707f9ec79598f034c5505aef92
your name <akram.rasikh@gmail.com>
2016-10-16 17:54:11 +0100

    testing template added

--- testing.rb
+++ testing.rb
@@ -1,9 +1,13 @@
-class test
+# rock!
+def game(input)
+  x = ["rock", "paper", "scissors"].sample
 
-
-
-myturn = "rock"
-
-x = ["rock", "paper", "scissors"].sample
-
+  if input.length == x.length
+    puts "Draw!"
+  elsif input.length < x.length
+    puts "You win!"
+  else
+    puts "you lose"
+  end
 end
+game("rock")
