c94b301cba43ebc03744bc5b9937696227ad977e
Laura Weston <laura.weston@outlook.com>
2016-10-16 18:25:59 +0100

    Computer instance has attr_reader name with value Computer

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,4 +1,9 @@
 class Computer
+  attr_reader :name
+  
+  def initialize
+    @name = "Computer"
+  end
   def move
     [:rock, :paper, :scissors].sample
   end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,6 +1,11 @@
 require 'computer'
 
 describe Computer do
+
+  it "has a name" do
+    expect(subject.name).to eq "Computer"
+  end
+
   it "can make a random move" do
     expect([:rock, :paper, :scissors]).to include subject.move
   end
