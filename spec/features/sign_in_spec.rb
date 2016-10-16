2e892dc66b1a81563cc2141cb4719777b7671de2
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-16 19:50:24 +0100

    Coverage increased to 100%

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
-feature 'enter name of player(s)' do
+feature 'enter name of players' do
   scenario 'player enters their name and is displayed on the next page' do
-    sign_in
+    sign_in_2p
     expect(page).to have_content "Welcome, Leeroy"
   end
   scenario 'player enters their name and is displayed on the next page' do
-    sign_in
+    sign_in_2p
     expect(page).to have_content "You are playing against Royston"
+  end
+end
+
+feature 'sign in for a signle plater game' do
+  scenario 'only one name is entered' do
+    sign_in_1p
+      expect(page).to have_content "You are playing against the_computer"
   end
 end
