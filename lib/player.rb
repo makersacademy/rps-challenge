fb8d6c1ac49a509cfc3f7f0412c2d3881ba2148d
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-16 12:15:11 +0100

    Create Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :player_choice
 
   def initialize(name)
     @name = name
---
+++ spec/player_spec.rb
@@ -0,0 +1,8 @@
+require 'player'
+require 'game'
+
+describe Player do
+  subject(:player) {described_class.new('Frodo')}
+
+
+end
