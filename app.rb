c9e50e60dba747ff342df56fcf2a59dafc8c8232
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-16 20:05:48 +0100

    Half working solution add all pages

--- app.rb
+++ app.rb
@@ -19,17 +19,55 @@ get '/playrock' do
   game = Game.new
   @computer_play = game.play
   erb(:playrock)
+  if @computer_play == "rock"
+    redirect '/draw'
+  elsif @computer_play == "paper"
+    redirect '/lose'
+  elsif @computer_play == "scissors"
+    redirect '/win'
+  end
 end
-
 
 get '/playpaper' do
   game = Game.new
   @computer_play = game.play
   erb(:playpaper)
+  if @computer_play == "paper"
+    redirect '/draw'
+  elsif @computer_play == "scissors"
+    redirect '/lose'
+  elsif @computer_play == "rock"
+    redirect '/win'
+  end
 end
 
 get '/playscissors' do
   game = Game.new
   @computer_play = game.play
   erb(:playscissors)
+  if @computer_play == "scissors"
+    redirect '/draw'
+  elsif @computer_play == "paper"
+    redirect '/win'
+  elsif @computer_play == "rock"
+    redirect '/lose'
+  end
+end
+
+get '/win' do
+  game = Game.new
+  @computer_play = game.play
+  erb(:win)
+end
+
+get '/lose' do
+  game = Game.new
+  @computer_play = game.play
+  erb(:lose)
+end
+
+get '/draw' do
+  game = Game.new
+  @computer_play = game.play
+  erb(:draw)
 end
