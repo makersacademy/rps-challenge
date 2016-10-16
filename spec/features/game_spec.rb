fed1b58416273631833ffbb0e34a8d82b4d63484
Michelle Epstein <epstein.michelle@gmail.com>
2016-10-16 14:32:38 +0100

    Player can select Rock, Paper or Scissors

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,6 +1,15 @@
+require "spec_helper"
+require_relative './web_helpers'
+
 feature "Player enters username and is ready to play" do
   scenario "Rock, Paper and Scissor options are shown" do
-    visit ('/game')
-    expect(page).to have_content(@options)
+    sign_in
+    expect(page).to have_content("Select your choice:")
+  end
+
+  scenario "The user clicks on their choice" do
+    sign_in
+    click_link('Rock')
+    expect(page).to have_content('Rock')
   end
 end
