8b6e9759319c480c5ac0bf30448c6dad8f129b81
Glenn <glynester@hotmail.com>
2016-10-16 09:43:35 +0000

    Refactored game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -3,8 +3,7 @@ require_relative 'player'
 
 class Game
 
-attr_reader :player_1, :player_2, :current_turn, :choice, :player_1_choice, :player_2_choice, :auto_choice, :winner
-#Can be removed from attr_reader later (:player_1_choice, :player_2_choice)
+attr_reader :player_1, :player_2, :current_turn, :player_1_choice, :player_2_choice, :auto_choice, :winner
 
 	def initialize(player1, player2)
 		@player_1 = player1
@@ -28,11 +27,12 @@ attr_reader :player_1, :player_2, :current_turn, :choice, :player_1_choice, :pla
 	
 	def selection(choice)
 		@choice = choice
+		record_player_choices				
 	end
 	
 	def record_player_choices
-		@player_1_choice = choice if @current_turn == @player_1
-		@player_2_choice = choice if @current_turn == @player_2
+		@player_1_choice = @choice if @current_turn == @player_1
+		@player_2_choice = @choice if @current_turn == @player_2
 	end
 	
 	def give_result
@@ -40,9 +40,9 @@ attr_reader :player_1, :player_2, :current_turn, :choice, :player_1_choice, :pla
 	end
 	
 	def reset_choices
-		@choice = nil												#Move to "reset" method
-		@player_1_choice = nil							#Move to "reset" method
-		@player_2_choice = nil							#Move to "reset" method
+		@choice = nil									
+		@player_1_choice = nil					
+		@player_2_choice = nil						
 	end
 	
 	def auto_choice
@@ -51,24 +51,32 @@ attr_reader :player_1, :player_2, :current_turn, :choice, :player_1_choice, :pla
 	end
 	
 	def result
-		if  @player_1_choice == @player_2_choice
-			@winner = "Draw"
-		elsif	(@player_1_choice == "rock" && @player_2_choice == "scissors") ||
-		(@player_1_choice == "paper" && @player_2_choice == "rock") ||
-		(@player_1_choice == "scissors" && @player_2_choice == "paper")
-			@winner = @player_1
-		else
-			@winner = @player_2
-		end
-		# @winner = @player_1 if @player_1_choice == "rock" && @player_2_choice == "scissors"
-		# @winner = @player_1 if @player_1_choice == "paper" && @player_2_choice == "rock"
-		# @winner = @player_1 if @player_1_choice == "scissors" && @player_2_choice == "paper"
-		# @winner = "Draw" if @player_1_choice == @player_2_choice
-		# @winner = @player_2
-		# puts "Something has gone wrong"																				#remove
+		if same_choice? then @winner = "Draw"
+		elsif	player_1_wins? then @winner = @player_1
+		else @winner = @player_2 end
 	end
 	
 	private
+	
+	def same_choice?
+		@player_1_choice == @player_2_choice
+	end
+
+	def player_1_wins?
+		rock_plays_scissors || paper_plays_rock || scissors_plays_paper
+	end
+	
+	def rock_plays_scissors
+		@player_1_choice == "rock" && @player_2_choice == "scissors"
+	end
+	
+	def paper_plays_rock
+		@player_1_choice == "paper" && @player_2_choice == "rock"
+	end
+	
+	def scissors_plays_paper
+		@player_1_choice == "scissors" && @player_2_choice == "paper"
+	end
 	
 	def opponent_of(current_player)
 		@players.select {|player| player != current_player}.first
