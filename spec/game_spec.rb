56f3b162b1501c93cd55a7b666d1c999c82683b4
Glenn <glynester@hotmail.com>
2016-10-16 13:14:19 +0000

    Added rspec tests for game method #give_result.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,8 @@
 describe Game do
   
   subject(:game) { described_class.new(player_one, computer) }
-  let(:player_one) { double :player1}
-  let(:computer) { double :player2}
-  let(:rock) { double :choice }
-  let(:paper) { double :choice }
-  let(:scissors) { double :choice }
+  let(:player_one) { double :player1, name: "Herbert"}
+  let(:computer) { double :player2, name: "Big Blue"}
   
   describe "initialize" do
     it "accepts player 1 to the game" do
@@ -30,15 +27,37 @@ describe Game do
   
   describe "#selection" do
     it "records the correct selection for player 1" do
-      game.selection(rock)
-      expect(game.player_1_choice).to eq rock
+      game.selection("rock")
+      expect(game.player_1_choice).to eq "rock"
     end
     it "records the correct selection for player 2" do
       game.switch_turns
-      game.selection(rock)
-      expect(game.player_2_choice).to eq rock
+      game.selection("rock")
+      expect(game.player_2_choice).to eq "rock"
+    end
+  end
+  
+  describe "#give_result" do
+    it "gives the correct result = p1 wins (p1 = 'paper', computer = 'rock')" do
+      game.selection("scissors")
+      game.switch_turns
+      game.selection("paper")
+      expect(game.give_result).to eq player_one.name      
+    end
+
+    it "gives the correct result = computer wins (p1 = 'rock', computer = 'paper')" do
+      game.selection("rock")
+      game.switch_turns
+      game.selection("paper")
+      expect(game.give_result).to eq computer.name     
     end
     
+    it "gives the correct result = draw (p1 = 'scissors', computer = 'scissors')" do
+      game.selection("scissors")
+      game.switch_turns
+      game.selection("scissors")
+      expect(game.give_result).to eq "Draw"        
+    end
   end
   
 end
