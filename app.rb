1bacc4356cbe7a5571a1cb6aef774e7a10eb8a1a
crispinandrews <crispin.andrews@gmail.com>
2016-10-16 11:58:37 +0100

    Game and player class updated, refactoring out sessions

--- app.rb
+++ app.rb
@@ -10,27 +10,30 @@ class RPS < Sinatra::Base
   end
 
   post '/name' do
-    session[:name] = params[:player_1_name]
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new('Opponent')
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
+  before do
+    @game = Game.instance
+  end
+
   get '/play' do
-    @player_1_name = session[:name]
-    @choice = session[:choice]
     erb(:play)
   end
 
-  post '/choice' do
-    session[:choice] = params[:choice]
-    redirect '/result'
-  end
+post '/choice' do
+  session[:choice] = params[:choice]
+  redirect '/result'
+end
 
-  get '/result' do
-    @name = session[:name]
-    @choice = session[:choice]
-    @opponent = Game.new.player_2_turn
-    erb(:result)
-  end
+get '/result' do
+  @choice = session[:choice]
+  @player_2_choice = @game.auto_turn
+  erb(:result)
+end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,34 @@
+require_relative 'player'
+
 class Game
 
-  def player_2_turn
+  attr_reader :player
+
+  def self.create(player_1, player_2)
+    @game = Game.new(player_1, player_2)
+  end
+
+  def self.instance
+    @game
+  end
+
+  def initialize(player_1, player_2)
+    @players = [player_1, player_2]
+  end
+
+  def player_1
+    @players.first
+  end
+
+  def player_2
+    @players.last
+  end
+
+  def auto_turn
     ['Rock', 'Paper', 'Scissors'].sample
   end
 
-  
+
+
 
 end
