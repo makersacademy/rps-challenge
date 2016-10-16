b93cd22cb9911ff13e387049568c324774f1bc92
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-16 16:09:16 +0100

    Implement method for the player to choose weapon

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,14 @@
+require_relative 'game'
+
 class Player
-  attr_reader :name, :player_choice
+  attr_reader :name, :weapon
 
   def initialize(name)
     @name = name
+  end
+
+  def weapon=(new_weapon)
+    fail 'Not a valid weapon' unless Game::WEAPONS.include? new_weapon
+    @weapon = new_weapon
   end
 end
