e41bc6a2f97adf0edbb67606f74fe9f50b35ed5f
Dionysis <Dionysis.k22@gmail.com>
2016-10-16 15:13:20 +0100

    player can restart the game after first game

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 RSpec.feature Rps, :type => :feature do
-  scenario "Player is ready to play against the rps bot" do
+  scenario "Player can restart the game" do
 
     sign_in_and_play
+    click_button "Rock"
 
-    expect(page).to have_text("Alright, let's play!")
+    expect(page).to have_text("Restart and play again")
   end
 end
