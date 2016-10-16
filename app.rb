75c195b168a22ead8fe0e5bf7f11044a1758244d
amidabrian51 <briankaty1@me.com>
2016-10-16 11:52:52 +0100

    Added input for names

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra/base'
+
+class RPS < Sinatra::Base
+
+
+  get '/' do
+      erb(:index)
+    end
+
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
+
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
