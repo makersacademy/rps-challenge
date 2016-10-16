a2c14506504fee58fedc7b81e672be661c288b32
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-16 19:09:05 +0100

    added sign in page

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require './lib/player'
+require './lib/game'
+
+class RPS < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  post '/name'
+    
+  end
+
+  run! if app_file == $0
+end
