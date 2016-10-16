096db23191e6608cf0a360e666227d4024a299bd
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-16 16:47:52 +0100

    Add Rock button

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ class RPS < Sinatra::Base
     erb :choose
   end
 
+  post'/selected' do
+    
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/feature/choose_spec.rb
@@ -0,0 +1,9 @@
+feature 'Choose' do
+    scenario 'Can click on a choice that takes you to another page with that image' do
+      visit "/"
+      fill_in :player1, with: 'Katy'
+      click_button "Play"
+      click_button "Rock"
+      expect(page).to have_xpath("//img[@src='http://weknowyourdreams.com/images/rock/rock-08.jpg']")
+  end
+end
