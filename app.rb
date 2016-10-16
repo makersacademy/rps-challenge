d3d5e4babca8fd774838daa643a313446bb63167
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-16 19:31:04 +0100

    Add all result pages

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-require_relative './lib/game'
+require_relative './lib/game.rb'
 
 get '/' do
     erb(:index)
@@ -13,4 +13,23 @@ end
 get '/print_name' do
   @player = $player
   erb(:print_name)
+end
+
+get '/playrock' do
+  game = Game.new
+  @computer_play = game.play
+  erb(:playrock)
+end
+
+
+get '/playpaper' do
+  game = Game.new
+  @computer_play = game.play
+  erb(:playpaper)
+end
+
+get '/playscissors' do
+  game = Game.new
+  @computer_play = game.play
+  erb(:playscissors)
 end
