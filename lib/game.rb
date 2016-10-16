5d7342a51981f826d640f2a9e2d8cfed455a6520
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-16 14:56:08 +0100

    added player and compputer selection

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,30 @@
+require_relative 'player'
+
 class Game
 
   attr_reader :player_1, :player_2
 
-  def initialize(player_1, player_2)
+  def initialize(player_1)
     @player_1 = player_1
-    @player_2 = player_2
+    @player_2 = Player.new(generate_name)
+    @selections = []
+  end
+
+  def player_select(selection)
+    @selections << selection
+  end
+
+  def computer_select
+    @selections << ['r','p','s'].sample
+  end
+
+  def choices
+    @selections.dup
+  end
+
+  private
+
+  def generate_name
+    ['Yoshimitsu','Frieza','Cersei','Robotnik','Farage'].sample
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,17 +2,39 @@ require 'game'
 
 describe Game do
 
-  let(:game) { described_class.new(player_1, player_2) }
+  let(:game) { described_class.new(player_1) }
   let(:player_1) { double(:player, name: 'Hilary') }
-  let(:player_2) { double(:player, name: 'Trump') }
 
   describe '#initialize' do
     it 'accepts player 1' do
       expect(game.player_1.name).to eq 'Hilary'
     end
 
-    it 'accepts player 2' do
+    it 'creates a computer player' do
+      allow(game.player_2).to receive(:name).and_return('Trump')
       expect(game.player_2.name).to eq 'Trump'
+    end
+  end
+
+  describe '#player_select' do
+    it 'adds r,p or s to the selections array' do
+      game.player_select('r')
+      expect(game.choices.empty?).to be false
+    end
+  end
+
+  describe '#computer_select' do
+    it 'adds r,p or s to the selections array' do
+      game.computer_select
+      expect(game.choices.empty?).to be false
+    end
+  end
+
+  describe '#choices' do
+    it 'contains the players selections' do
+      game.player_select('r')
+      game.computer_select
+      expect(game.choices.count).to eq 2
     end
   end
 end
