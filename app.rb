b83289994bc617b39ec3e866ddc934b6ec5c92e9
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-16 16:06:57 +0100

    First user story

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
   get '/' do
-    'Hello RPS!'
+    erb :index
+  end
+
+  post'/choose' do
+    @player1 = params[:player1]
+    erb :choose
   end
 
   # start the server if ruby file executed directly
