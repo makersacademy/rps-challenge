5b080c451d5bba86bc817ac4ceccfcd006d2d27a
amidabrian51 <briankaty1@me.com>
2016-10-16 13:16:15 +0100

    Added we_helper to the project

--- spec/features/choice_spec.rb
+++ spec/features/choice_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'View Rock Paper Scissors' do
   scenario 'the candidates will be presented the choices (rock, paper or scissors)' do
-    visit('/')
-    fill_in :player_1_name, with: 'Brian'
-    fill_in :player_2_name, with: 'Tom'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Rock, Paper or Scissors'
   end
 
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature 'enter names' do
     scenario 'Submitting names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Brian'
-      fill_in :player_2_name, with: 'Tom'
-      click_button 'Submit'
+      sign_in_and_play
       expect(page).to have_content 'Brian vs. Tom'
     end
 end
