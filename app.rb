beae15c3533fde9212f7a2f964f9d422ffdc1c10
James Gardiner <jamesgardiner075@gmail.com>
2016-10-16 18:00:29 +0100

    Add choose RPS option feature

--- app.rb
+++ app.rb
@@ -13,6 +13,16 @@ class RPS < Sinatra::Base
     erb (:play)
   end
 
+  post "/compare" do
+    session[:choice] = params[:choice]
+    redirect "/win"
+  end
+
+  get "/win" do
+    @user_choice = session[:choice]
+    erb (:win)
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -2,9 +2,7 @@ require "./app"
 
 feature "Enter name" do
   scenario "submitting a name" do
-    visit('/')
-    fill_in :name, with: 'John Doe'
-    click_button 'Play'
-    expect(page).to have_content 'John Doe'
+    sign_in_and_play
+    expect(page).to have_content "John Doe"
   end
 end
