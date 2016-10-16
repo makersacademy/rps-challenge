071966b11d3e55a1b6d61d76763a68c130e00518
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-16 12:12:04 +0100

    Deleted unused files

--- spec/features/name_forms_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-require '././rps.rb'
-
-RSpec.feature 'entering player name', :type => :feature do
-  scenario 'users enter their name' do
-    sign_in_and_play
-    expect(page).to have_text('What weapon do you choose, Frodo?')      
-  end
-end
