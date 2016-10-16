09d5099921182a92ac11c0564963e85d0d512243
Felix Harrison <fmlharrison@gmail.com>
2016-10-16 21:17:50 +0100

    css added to landing page

--- spec/feature_tests/starting_a_new_game_spec.rb
+++ spec/feature_tests/starting_a_new_game_spec.rb
@@ -5,6 +5,6 @@ feature 'Starting a new game' do
     sign_in_and_play
     click_button 'ROCK'
     click_button 'New Challenger'
-    expect(page).to have_content "Let's play Britain's most beloved school playground game: Rock, Paper, Sissors!"
+    expect(page).to have_content "Let's play a timeless classic:"
   end
 end
