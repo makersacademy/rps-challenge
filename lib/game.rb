278d8e5e2aaceb2e7460f75fee4fd4d4ec1593c5
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-16 11:11:38 +0100

    fixed game_spec and passed test

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,9 @@
 class Game
+
+  attr_reader :player_1, :player_2
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,16 +3,16 @@ require 'game'
 describe Game do
 
   let(:game) { described_class.new(player_1, player_2) }
-  let(:player_1) { double(:player, player_1: 'Hilary') }
-  let(:player_2) { double(:player, player_2: 'Trump') }
+  let(:player_1) { double(:player, name: 'Hilary') }
+  let(:player_2) { double(:player, name: 'Trump') }
 
   describe '#initialize' do
     it 'accepts player 1' do
-      expect(game.player_1).to eq 'Hilary'
+      expect(game.player_1.name).to eq 'Hilary'
     end
 
     it 'accepts player 2' do
-      expect(game.player_2).to eq 'Trump'
+      expect(game.player_2.name).to eq 'Trump'
     end
   end
 end
