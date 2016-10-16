55156993997c92bdd9c2e7c640330fffaf3c2ca2
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-16 10:42:36 +0100

    Fixed bug for when playing 2p mode

--- app/rps_app.rb
+++ app/rps_app.rb
@@ -23,20 +23,35 @@ class RPSApp < Sinatra::Base
     erb :choices
   end
 
+  post '/choices_2' do
+    $choice_1 = params[:choice_1]
+    @player_1 = $player_1
+    @player_2 = $player_2
+    erb :choices_2
+  end
+
   get '/evaluate' do
     @player_1 = $player_1
     @player_2 = $player_2
-    @choice = params[:choice]
+    @choice_2 = params[:choice_2]
     $weapon_1 = Weapon.new
-    if @choice == "ROCK"
+    if $choice_1 == "ROCK"
       $weapon_1.choose_rock
-    elsif @choice == "PAPER"
+    elsif $choice_1 == "PAPER"
       $weapon_1.choose_paper
     else
       $weapon_1.choose_scissors
     end
     $weapon_2 = Weapon.new
-    @computer_choice = $weapon_2.computer_choice
+    if @player_2.name == :the_computer
+      @choice_2 = $weapon_2.computer_choice
+    elsif @choice_2 == "ROCK"
+      $weapon_2.choose_rock
+    elsif @choice_2 == "PAPER"
+      $weapon_2.choose_paper
+    else
+      $weapon_2.choose_scissors
+    end
     $game = Game.new($weapon_1, $weapon_2)
     @player_1.store_choice($weapon_1.choice)
     @player_2.store_choice($weapon_2.choice)
