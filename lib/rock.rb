946a3bae37491dffc1a6590097348ba88fc5d370
Anna Beddow <anna.beddow@gmail.com>
2016-10-17 09:28:43 +0100

    game working

--- lib/rock.rb
+++ lib/rock.rb
@@ -3,8 +3,10 @@ require './views/game.rb'
 
 class RPS < Sinatra::Base
 
+
+
   get '/' do
-    @name = params[:name]
+
     erb (:enter_name)
   end
 
@@ -14,7 +16,7 @@ class RPS < Sinatra::Base
   end
 
   get '/play' do
-    @player = Player.new(params[:weapon],'name')
+    @player = Player.new(params[:weapon])
     @game = Game.new(@player.choice)
     @game.play
     erb (:play)
--- lib/views/player.rb
+++ lib/views/player.rb
@@ -3,10 +3,10 @@ class Player
 
   NEUTRAL_SCORE = 0
 
-  attr_reader :choice, :name
+  attr_reader :choice, :user_name
 
-  def initialize(choice, name)
+  def initialize(choice)
     @choice = choice
-    @name = name
   end
+
 end
