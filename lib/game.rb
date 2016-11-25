0a381fc6425c7b381f315881ea8e3945d6499359
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-25 21:50:30 +0000

    Added random generator for rps

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,13 @@
 class Game
 
+  attr_reader :choice
 
+  def initialize(choice)
+    @choice = choice
+  end
+
+  def random_rps
+    ["Rock", "Paper", "Scissors"].sample
+  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,22 @@ require './lib/game'
 
 describe Game do
 
+  subject(:game) { described_class.new(choice) }
+  let(:choice) { double(:choice) }
+
+  it "assigns user choice on instantiation" do
+    expect(game.choice).to eq choice
+  end
+
+  it "can generate a random rps choice" do
+    expect(game).to respond_to(:random_rps)
+  end
+
+  it "generates a random choice of rock, paper or scissors" do
+    expect(["Rock", "Paper", "Scissors"]).to include(game.random_rps)
+  end
+
+
 
 
 
