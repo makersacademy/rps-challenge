e0bba647dc93db140cb48c1ca4ae0a7b4d049b99
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-26 16:46:20 +0000

    Player can choose a weapon, but the weapon must be in the weapons list

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,17 @@
+require 'weapons'
+
 class Player
 
-  attr_reader :name
+  attr_reader :name, :weapon
 
   def initialize(name)
     raise "Player name must be a string" if is_not_a_string?(name)
     @name = name
+  end
+
+  def choose_weapon(weapon)
+    raise "Sorry, that weapon isn't available" if weapon_isnot_available?(weapon)
+    @weapon = weapon
   end
 
   private
@@ -12,5 +19,9 @@ class Player
     def is_not_a_string?(name)
       name.class != String
     end
-    
+
+    def weapon_isnot_available?(weapon)
+      !Weapons::LIST.include?(weapon)
+    end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,11 +12,37 @@ describe Player do
 
   end
 
+  describe '#choose_weapon' do
+
+    it 'responds to #choose_weapon' do
+      expect(player).to respond_to :choose_weapon
+    end
+
+    it 'stores a weapon' do
+      player.choose_weapon(:rock)
+      expect(player.weapon).to eq :rock
+    end
+
+  end
+
   describe 'error handling' do
 
-    it 'doesn\'t allow names to be anything except Strings' do
-      message = "Player name must be a string"
-      expect{ Player.new(23) }.to raise_error(RuntimeError, message)
+    describe 'names' do
+
+      it 'doesn\'t allow names to be anything except Strings' do
+        message = "Player name must be a string"
+        expect{ Player.new(23) }.to raise_error(RuntimeError, message)
+      end
+
+    end
+
+    describe 'choosing weapons' do
+
+      it 'requires the player to choose from the available weapons' do
+        message = "Sorry, that weapon isn't available"
+        expect{ player.choose_weapon("Banana") }.to raise_error(RuntimeError, message)
+      end
+
     end
 
   end
