da2a46ae56190922b7715c9e307839cde3e9f786
Russell <rusyeo@gmail.com>
2016-11-26 20:39:50 +0000

    player can start a new game

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
+
+  post '/name' do
+    @name = params[:player_name]
+    puts @name
+    erb(:play)
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/start_spec.rb
+++ spec/features/start_spec.rb
@@ -1,8 +1,15 @@
 RSpec.feature "Starting a game", :type => :feature do
 
-  scenario "Homepage has a title" do
+  scenario "Homepage" do
     visit('/')
     expect(page).to have_text("Rock Paper Scissors")
+  end
+
+  scenario "Player enters their name and starts game" do
+    visit '/'
+    fill_in "player_name", :with => "Russell"
+    click_button "Start"
+    expect(page).to have_text("Hello, Russell!")
   end
 
 
