2063de3ba4041888cef7b37c69d1017f202879d4
Louisa <louisaspicer@gmail.com>
2016-11-26 18:02:14 +0000

    Updated GET to accept Player instance

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/game.rb'
+require './lib/player.rb'
 
 class RPS < Sinatra::Base
   get '/' do
@@ -7,13 +8,13 @@ class RPS < Sinatra::Base
   end
 
   post '/player_name' do
-    Game.create(params["Player1"])
+    Game.create(Player.new(params["Player1"]))
     redirect '/play'
   end
 
   get '/play' do
     @game = Game.instance
-    "#{@game.player1} VS Computer"
+    "#{@game.player1.name} VS Computer"
   end
 
   run! if app_file == $0
