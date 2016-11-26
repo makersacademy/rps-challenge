2e7b12a956333956ce29c2b1ffae80986df99ef6
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-26 14:49:21 +0000

    game class can create an isntance of Game

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,10 @@ class Game
 
   attr_reader :player
 
+  def self.create(game_klass, player)
+    game_klass.new(player)
+  end
+
   def initialize(player)
     @player = player
   end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,18 @@ require 'game'
 describe Game do
 
   let(:player) { double :player }
+  let(:game_klass) { spy :game_klass }
 
   subject(:game) { described_class.new(player) }
+
+  describe '#self.create' do
+
+    it 'initializes a game object' do
+      Game.create(game_klass, player)
+      expect(game_klass).to have_received(:new).with(player)
+    end
+
+  end
 
   describe '#new' do
 
