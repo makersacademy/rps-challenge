e617851eee318b354181407fb22d8393b56da6db
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-11-26 12:23:12 +0000

    home page displays game title

--- spec/features/entering_name_spec.rb
+++ spec/features/entering_name_spec.rb
@@ -1,3 +1,10 @@
+feature "Home page" do
+  scenario "The home page displays the name of the game" do
+    visit('/')
+    expect(page).to have_content("Lets play Rock Paper Scissors!")
+  end
+end
+
 feature "entering player name" do
   scenario "player enters their name and is taken to the game screen" do
     visit('/')
