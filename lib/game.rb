fdc8427bb6e0760bbd1140461dad9bf4ce72b8bd
George Eaton <george_eaton@hotmail.co.uk>
2016-11-26 14:06:11 +0000

    game class takes a player

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,11 @@
-class Game
-  attr_reader :player_choice
+require_relative 'player'
 
-  def initialize
+class Game
+  attr_reader :player_choice, :player
+
+  def initialize(player)
+    @player = player
     @player_choice = nil
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,15 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(player) }
   let(:player) { double :player }
 
   it "should have an empty player choice" do
     expect(game.player_choice).to be nil
+  end
+
+  it "should take a player" do
+    expect(game.player).to eq player
   end
 
 end
