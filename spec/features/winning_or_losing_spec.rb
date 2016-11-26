19dfe0720d2e6ba046427ce016ce7e7687557cd7
Dan Pelensky <dan@pelensky.com>
2016-11-26 16:41:01 +0000

    player can lose

--- spec/features/winning_or_losing_spec.rb
+++ spec/features/winning_or_losing_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require_relative 'web_helper'
 
 feature "the computer wins" do
-  xscenario "the player selects rock and computer selects paper" do
+  scenario "the player selects rock and computer selects paper" do
     srand(1234)
     sign_in_and_select_rock
     expect(page).to have_text("Dan loses. AIbot Wins!")
