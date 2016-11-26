d7a5ed6d48ab3fe63b309691456466d91e2ea488
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-26 10:04:14 +0000

    Refactored logic to calculate results

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,14 @@
+require 'weapon'
+
 class Game
 
   attr_accessor :user_choice, :computer_choice
+
+  RULES = {
+    Rock: :Scissors,
+    Paper: :Rock,
+    Scissors: :Paper
+  }
 
   def self.instance
     @game
@@ -11,27 +19,25 @@ class Game
   end
 
   def initialize(user_choice)
-    @user_choice = user_choice
+    @user_choice = user_choice.to_sym
   end
 
   def random_rps
-    ["Rock", "Paper", "Scissors"].sample
+    [:Rock, :Paper, :Scissors].sample
   end
 
   def compare
     @computer_choice = random_rps
-
-    if @user_choice == @computer_choice
-      return :tie
-    elsif @user_choice == "Rock" && @computer_choice == "Scissors"
-      return :win
-    elsif @user_choice == "Scissors" && @computer_choice == "Paper"
-      return :win
-    elsif @user_choice == "Paper" && @computer_choice == "Rock"
-      return :win
-    else
-      return :lose
-    end
+    return :win if beat?
+    return :tie if @user_choice == @computer_choice
+    return :lose
   end
+
+  private
+  
+  def beat?
+    RULES[@user_choice] == @computer_choice
+  end
+
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,70 +6,79 @@ describe Game do
   subject(:game_paper) { described_class.new("Paper") }
   subject(:game_scissors) { described_class.new("Scissors") }
 
-  it "can create a new Game class using class method" do
-    expect(Game.create("Rock")).to be_an_instance_of(Game)
+  context "Class methods" do
+    it "can create a new Game class using class method" do
+      expect(Game.create("Rock")).to be_an_instance_of(Game)
+    end
+
+    it "can reference current instance of Game class using class method" do
+      expect(Game.instance).to be_an_instance_of(Game)
+    end
   end
 
-  it "can reference current instance of Game class using class method" do
-    expect(Game.instance).to be_an_instance_of(Game)
+  context "Initialization" do
+    it "assigns user choice on instantiation" do
+      expect(game_rock.user_choice).to eq :Rock
+    end
   end
 
-  it "assigns user choice on instantiation" do
-    expect(game_rock.user_choice).to eq "Rock"
+  context "Computer randomisation" do
+    it "generates a random choice of rock, paper or scissors" do
+      expect([:Rock, :Paper, :Scissors]).to include(game_rock.random_rps)
+    end
+
+    it "assigns a random choice of rock, paper or scissors to computer choice" do
+      game_rock.compare
+      expect([:Rock, :Paper, :Scissors]).to include(game_rock.computer_choice)
+    end
   end
 
-  it "generates a random choice of rock, paper or scissors" do
-    expect(["Rock", "Paper", "Scissors"]).to include(game_rock.random_rps)
+  context "Results of the game" do
+    it "returns win if user chooses Rock and computer chooses Scissors" do
+      allow(game_rock).to receive(:random_rps).and_return :Scissors
+      expect(game_rock.compare).to eq :win
+    end
+
+    it "returns win if user chooses Scissors and computer chooses Paper" do
+      allow(game_scissors).to receive(:random_rps).and_return :Paper
+      expect(game_scissors.compare).to eq :win
+    end
+
+    it "returns win if user chooses Paper and computer chooses Rock" do
+      allow(game_paper).to receive(:random_rps).and_return :Rock
+      expect(game_paper.compare).to eq :win
+    end
+
+    it "returns lose if user chooses Rock and computer chooses Paper" do
+      allow(game_rock).to receive(:random_rps).and_return :Paper
+      expect(game_rock.compare).to eq :lose
+    end
+
+    it "returns lose if user chooses Scissors and computer chooses Rock" do
+      allow(game_scissors).to receive(:random_rps).and_return :Rock
+      expect(game_scissors.compare).to eq :lose
+    end
+
+    it "returns lose if user chooses Paper and computer chooses Scissors" do
+      allow(game_paper).to receive(:random_rps).and_return :Scissors
+      expect(game_paper.compare).to eq :lose
+    end
+
+    it "returns draw if user and computer choose Rock" do
+      allow(game_rock).to receive(:random_rps).and_return :Rock
+      expect(game_rock.compare).to eq :tie
+    end
+
+    it "returns draw if user and computer choose Paper" do
+      allow(game_paper).to receive(:random_rps).and_return :Paper
+      expect(game_paper.compare).to eq :tie
+    end
+
+    it "returns draw if user and computer choose the Scissors" do
+      allow(game_scissors).to receive(:random_rps).and_return :Scissors
+      expect(game_scissors.compare).to eq :tie
+    end
   end
 
-  it "assigns a random choice of rock, paper or scissors to computer choice" do
-    game_rock.compare
-    expect(["Rock", "Paper", "Scissors"]).to include(game_rock.computer_choice)
-  end
-
-  it "returns win if user chooses Rock and computer chooses Scissors" do
-    allow(game_rock).to receive(:random_rps).and_return "Scissors"
-    expect(game_rock.compare).to eq :win
-  end
-
-  it "returns win if user chooses Scissors and computer chooses Paper" do
-    allow(game_scissors).to receive(:random_rps).and_return "Paper"
-    expect(game_scissors.compare).to eq :win
-  end
-
-  it "returns win if user chooses Paper and computer chooses Rock" do
-    allow(game_paper).to receive(:random_rps).and_return "Rock"
-    expect(game_paper.compare).to eq :win
-  end
-
-  it "returns lose if user chooses Rock and computer chooses Paper" do
-    allow(game_rock).to receive(:random_rps).and_return "Paper"
-    expect(game_rock.compare).to eq :lose
-  end
-
-  it "returns lose if user chooses Scissors and computer chooses Rock" do
-    allow(game_scissors).to receive(:random_rps).and_return "Rock"
-    expect(game_scissors.compare).to eq :lose
-  end
-
-  it "returns lose if user chooses Paper and computer chooses Scissors" do
-    allow(game_paper).to receive(:random_rps).and_return "Scissors"
-    expect(game_paper.compare).to eq :lose
-  end
-
-  it "returns draw if user and computer choose Rock" do
-    allow(game_rock).to receive(:random_rps).and_return "Rock"
-    expect(game_rock.compare).to eq :tie
-  end
-
-  it "returns draw if user and computer choose Paper" do
-    allow(game_paper).to receive(:random_rps).and_return "Paper"
-    expect(game_paper.compare).to eq :tie
-  end
-
-  it "returns draw if user and computer choose the Scissors" do
-    allow(game_scissors).to receive(:random_rps).and_return "Scissors"
-    expect(game_scissors.compare).to eq :tie
-  end
 
 end
