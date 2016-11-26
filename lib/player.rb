440cc1cd2c055da283405e1917c13c59b3c89106
Asuka Ochi <asukaochi@gmail.com>
2016-11-26 11:56:46 +0000

    Choose method added in Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :choice
 
   def initialize( name )
     @name = name
+    @choice
+  end
+
+  def choose( choice )
+    @choice = choice
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,11 +6,19 @@ describe Player do
   subject( :player ){ described_class.new( player_name ) }
   let( :player_name ){ double :name }
 
+  context "#name" do
+    it { is_expected.to respond_to :name }
 
-  it { is_expected.to respond_to :name }
+    it "should return player's name when accessed" do
+      expect( player.name ).to eq player_name
+    end
+  end
 
-  it "should return player's name when accessed" do
-    expect( player.name ).to eq player_name
+  context "#choose" do
+    it "should return player's choice when accessed" do
+      choice = "rock"
+      expect( player.choose( choice ) ).to eq player.choice
+    end
   end
 
 end
