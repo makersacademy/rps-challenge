3982c1f3fad1f704c12fb7faef67c2d25e56aeac
George Eaton <george_eaton@hotmail.co.uk>
2016-11-26 14:49:14 +0000

    added spec_helper

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -2,9 +2,7 @@ require './app'
 
 RSpec.feature "Entering a name", :type => :feature do
   scenario "User enters name" do
-    visit '/'
-    fill_in "player_name", :with => "George"
-    click_button("Save")
+    sign_in_and_play
     expect(page).to have_text("George")
   end
 end
