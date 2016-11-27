e593ee39d2f051e6a4bdf99a6495539352375858
pflarcology <rhrg500@gmail.com>
2016-11-27 23:15:34 +0000

    removes game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,10 @@
-require 'game'
-require 'spec_helper'
-
-describe Game do
-
-  subject(:game) { described_class.new(human) }
-  let(:player) { double :player }
-
-
-end
+# require 'game'
+# require 'spec_helper'
+#
+# describe Game do
+#
+#   subject(:game) { described_class.new(human) }
+#   let(:player) { double :player }
+#
+#
+# end
