27b36657bb6939ca69fd81fc3d471423c474d902
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-28 11:34:30 +0000

    Added feature tests to test win/lose/tie scenarios

--- spec/features/rps_results_spec.rb
+++ spec/features/rps_results_spec.rb
@@ -6,4 +6,25 @@ feature "Results of RPS battle shown" do
     select_choice
     expect(page).to have_content("You chose")
   end
+
+  scenario "When I submit 'Rock' I am told if I have won" do
+    allow_any_instance_of(Game).to receive(:get_computer_choice).and_return(:Scissors)
+    sign_in
+    select_choice
+    expect(page).to have_content("Congratulations, you win!")
+  end
+
+  scenario "When I submit 'Rock' I am told if I have lost" do
+    allow_any_instance_of(Game).to receive(:get_computer_choice).and_return(:Paper)
+    sign_in
+    select_choice
+    expect(page).to have_content("Sorry, you have lost!")
+  end
+
+  scenario "When I submit 'Rock' I am told if I have tied" do
+    allow_any_instance_of(Game).to receive(:get_computer_choice).and_return(:Rock)
+    sign_in
+    select_choice
+    expect(page).to have_content("You have tied!")
+  end
 end
