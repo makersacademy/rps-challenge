6d885cbee589bd4a2c5ef67bcc67a7b042ac2441
Rob Adams <robert.andrew.adams@gmail.com>
2016-11-28 14:37:09 +0000

    logic

--- lib/game.rb
+++ lib/game.rb
@@ -47,9 +47,9 @@ attr_reader :player,:choose_hand,:chose_hand,:winner
   private
 
   def compare_hands(player_hand,oppopent_hand)
-    return @winner = "You Loose!" if HAND_STRENGTHS[player_hand][oppopent_hand] > 0
+    return @winner = "You Loose!" if HAND_STRENGTHS[player_hand][oppopent_hand] < 0
     return @winner = "Draw!" if HAND_STRENGTHS[player_hand][oppopent_hand] == 0
-    return @winner = "You Win!" if HAND_STRENGTHS[player_hand][oppopent_hand] < 0
+    return @winner = "You Win!" if HAND_STRENGTHS[player_hand][oppopent_hand] > 0
   end
 
 
