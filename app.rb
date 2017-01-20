d1a94a0d29d275b33736a1bd14285a9ce2f5bcc6
Justinio14 <jbutwell@hotmail.com>
2017-01-20 18:04:33 +0000

    Initial commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra/base'
+require './lib/player'
+require './lib/game'
+
+class Rps < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    player_1 = params[:marketeer_name]
+    redirect '/play'
+  end
+
+  
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+
+end
