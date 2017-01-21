ae646ba07014d8bd7de0877d8bae6459fda6b5bb
Rick Clark <rkingsleyclark@gmail.com>
2017-01-21 16:00:07 +0000

    added move icons for move selection

---
+++ spec/features/choose_move_spec.rb
@@ -0,0 +1,11 @@
+feature "FEATURE: Choose move" do
+  context "Two player" do
+    before(:each) do
+      play_two_player
+    end
+    scenario "player 1 can choose their move" do
+      
+    end
+  end
+
+end
