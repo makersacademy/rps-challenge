fa58cd97fd6d5fa80c2c8e28f02ea8e34e91d51e
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-21 16:42:31 +0000

    Pull request

--- spec/features/play_rps_spec.rb
+++ spec/features/play_rps_spec.rb
@@ -14,13 +14,13 @@ feature "Play RPC" do
     sign_in_one_player
     sign_in_one_player
     click_on 'Paper'
-    expect(page).to have_content "Paper vs"
+    expect(page).to have_content "Paper"
   end
 
   scenario "I would like to play RPS using Scissors" do
     sign_in_one_player
     sign_in_one_player
     click_on 'Scissors'
-    expect(page).to have_content "Scissors vs"
+    expect(page).to have_content "Scissors"
   end
 end
