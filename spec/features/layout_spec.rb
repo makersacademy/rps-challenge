41646c1ae46dd2a0949037241429baa6ff3c276b
Rick Clark <rkingsleyclark@gmail.com>
2017-01-21 11:14:59 +0000

    Refactored .navbar-brand text test

--- spec/features/layout_spec.rb
+++ spec/features/layout_spec.rb
@@ -1,6 +1,7 @@
 feature "Feature: Layout" do
   scenario "Shows the site title in the header" do
     visit "/"
-    expect(page).to have_content("Rock, Paper, Scissors, Spock, Lizard!")
+    heading_text = page.find(".navbar-brand").assert_text("Rock, Paper, Scissors, Spock, Lizard!")
+    expect(heading_text).to be true
   end
 end
