275d31a803e3926e6afb4652252901db8833f0ae
Rob Brentnall <rob.brentnall@gmail.com>
2017-01-21 19:24:00 +0000

    fixing coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,11 @@
-# ensure the following is AT THE TOP of your spec_helper.rb to get test coverage stats
 require 'coveralls'
 require 'simplecov'
 
 SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
-# replace following line with SimpleCov.start to get coverage stats locally
-SimpleCov.start
+Coveralls.wear! 
 # run `open coverage/index.html` from the command line to view details
 
 ENV['RACK_ENV'] = 'test'
