3919a227074278df321d4785b6f737abc79628e9
Ekaterina Loshchinina <KateLoschinina@ekaterinasmbp6.home>
2017-01-21 19:02:25 +0000

    Adding post-redirect loop.

--- app.rb
+++ app.rb
@@ -1,12 +1,21 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+
+  enable :sessions
+  
   get '/' do
     erb :index
   end
 
-  post '/game' do
-    @name = params[:name]
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/game'
+  end
+
+  get '/game' do
+    @name = session[:name]
+    p @name
     erb :game
   end
 
