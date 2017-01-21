36b243f080191eddc2216e919d173cb8ed83f49e
PeteGH <peter@grant-hay.eu>
2017-01-21 11:23:15 +0100

    User Story 1
    As a marketeer
    So that I can see my name in lights
    I would like to register my name before playing an online game

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions # you have to enable sessions first so that you can later use the session variables
+
   get '/' do
-    'Hello RPS!'
+    erb(:index)
+  end
+
+  post '/name' do
+    session[:name] = params[:name] # here we're saving the name from the the params to the session so we can use it later
+    redirect '/play' # redirecting to play, because we don't want to render a view from post action
+  end
+
+  get '/play' do # defining action play 
+    @name = session[:name] # retreives the name from the session
+    erb :play
   end
 
   # start the server if ruby file executed directly
