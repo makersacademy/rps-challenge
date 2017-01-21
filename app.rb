df00683b84fee7f606b3163b7260c118a16d5c87
Rick Clark <rkingsleyclark@gmail.com>
2017-01-21 18:07:10 +0000

    Player 1 move hidden when chosen

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require './lib/game.rb'
 require './lib/player.rb'
+require './lib/round.rb'
 
 class RockPaperScissors < Sinatra::Base
   get '/' do
@@ -11,16 +12,24 @@ class RockPaperScissors < Sinatra::Base
     player_1 = Player.new(name: params[:p1_name])
     player_2 = Player.new(name: params[:p2_name], human: (params[:player_2_type] == 'human'))
     Game.new(player_1: player_1, player_2: player_2)
+    Round.round_instance = nil
     redirect '/play'
   end
 
   get '/play' do
     @game = Game.game_instance
+    @round = Round.round_instance
     erb :game
   end
 
-  post '/move' do
+  post '/p1_move' do
     p params
+    Round.new(player_1_move: params[:p1_choice].to_sym)
+    if Game.game_instance.players[:player_2].human == true
+      redirect '/play'
+    else
+      #RESULTS
+    end
   end
 
   # start the server if ruby file executed directly
--- spec/features/choose_move_spec.rb
+++ spec/features/choose_move_spec.rb
@@ -6,7 +6,7 @@ feature "FEATURE: Choose move" do
     scenario "player 1 can choose their move" do
       choose("p1_choice", {:option => "lizard"})
       click_button('p1_move')
-      p1_hider_class = page.find('#p1_move_hider')[:class].include('hidden-xs-up')
+      p1_hider_class = page.find('#p1_move_hider')[:class].include?('hidden-xs-up')
       expect(p1_hider_class).to be false
     end
   end
