b96a25c2dc9d013063ebe81f16658fabe5d600c9
Matthew Perry <thesedatedprince@gmail.com>
2017-01-21 17:24:00 +0000

    Player can now input name

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
-    'Hello RPS!'
+    erb(:index)
+  end
+
+  post '/names' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
   end
 
   # start the server if ruby file executed directly
--- spec/features/player_entry_spec.rb
+++ spec/features/player_entry_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 feature 'allows player to enter name' do
   scenario 'player enters name' do
     visit '/'
-    expect(page).to have_content "Enter your name here"
+    fill_in('player_name', with: 'Mephistopheles')
+    click_button('Submit')
+    expect(page).to have_content 'Mephistopheles'
   end
 end
