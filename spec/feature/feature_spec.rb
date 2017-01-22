e4e4fb6392032ef391ab0cfc286cd8440caab051
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-22 22:11:48 +0000

    Two players coverage

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -7,11 +7,20 @@ RSpec.feature "Name form", "type" => feature do
 	end
 end
 
-RSpec.feature "Play a game", "type" => feature do
+RSpec.feature "One player game", "type" => feature do
 	scenario "Play a game" do
 	name_and_submit
 	click_button("paper")
 	expect(page).to have_content('Play again')
 	end
+end
 
+
+RSpec.feature "Two player game", "type" => feature do
+	scenario "Play a game" do
+	two_names_and_submit
+	click_button("paper")
+	click_button("paper")
+	expect(page).to have_content("It's a draw")
+	end
 end
--- spec/feature/web_helpers.rb
+++ spec/feature/web_helpers.rb
@@ -3,3 +3,10 @@ def name_and_submit
 	fill_in('p1_name', with: "Mabel")
 	click_button('submit')
 end
+
+def two_names_and_submit
+	visit('/')
+	fill_in('p1_name', with: "Mabel")
+	fill_in('p2_name', with: "Dipper")
+	click_button('submit')
+end
