4d7ed836e854c84fff5b5563b75fd450e68311ce
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-22 22:16:56 +0000

    Additional feature tests

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -16,11 +16,29 @@ RSpec.feature "One player game", "type" => feature do
 end
 
 
-RSpec.feature "Two player game", "type" => feature do
+RSpec.feature "Draw", "type" => feature do
 	scenario "Play a game" do
 	two_names_and_submit
 	click_button("paper")
 	click_button("paper")
 	expect(page).to have_content("It's a draw")
+	end
+end
+
+RSpec.feature "Player one winning", "type" => feature do
+	scenario "Play a game" do
+	two_names_and_submit
+	click_button("rock")
+	click_button("paper")
+	expect(page).to have_content("Dipper is the winner")
+	end
+end
+
+RSpec.feature "Player one lose", "type" => feature do
+	scenario "Play a game" do
+	two_names_and_submit
+	click_button("paper")
+	click_button("scissors")
+	expect(page).to have_content("Mabel is the winner")
 	end
 end
