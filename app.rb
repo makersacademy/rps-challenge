b3d2baa60e697f9beb64e98a95496515a1982637
frankie <franklin.shaw@gmail.com>
2017-01-22 23:19:04 +0000

    final corrections and README.md

--- app.rb
+++ app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'shotgun'
 require './lib/game_class'
-#shotgun app.rb -p 4567
 set :session_1, 'first session'
 
 get '/' do
@@ -15,11 +14,7 @@ end
 
 post '/decide' do
   choice = params[:your_move]
-  if choice == 'x'
-    @result = 'u chose x'
-  else
-    @current_game = Game.new(choice)
-    @result = @current_game.decider
-    erb :decide
-  end
+  @current_game = Game.new(choice)
+  @result = @current_game.decider
+  erb :decide
 end
