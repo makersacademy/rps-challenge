db99b1b9493c755dcc3e36deef78d8b417e174bf
Shereen Finney <shereenfinney@gmail.com>
2017-01-22 17:24:31 +0000

    Added scenario 2e - to test for the presence of a back button to take the player back to the regsiter page

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -33,12 +33,12 @@ feature '2.Play' do
     expect(page).to have_content msg
   end
 
-  # scenario "d. Go back to the Register page" do
-  #   sign_in_and_play
-  #   click_button('Attack!')
-  #   click_button('Back!')
-  #   expect(page).to have_content 'Player 1: Enter your name:'
-  # end
+  scenario "e. Go back to the Register page" do
+    sign_in_and_play
+    click_button 'Scissors'
+    click_button 'Back'
+    expect(page).to have_content 'Player 1: Enter your name:'
+  end
 
 
 end
