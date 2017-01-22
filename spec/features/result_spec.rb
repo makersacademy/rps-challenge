cedc585432fd1cb40b2ab681863633a0d4844cd4
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-22 22:07:03 +0000

    missing files

---
+++ spec/features/result_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature "Results page" do
+
+  scenario "Expect page 1 to enter player 1 details" do
+    sign_in_and_make_choices
+    expect(page).to have_content "Player 2 won"
+  end
+
+  scenario "Expect page 2 to enter player 2 details" do
+    visit('/')
+    fill_in :player_name, with: 'Lauren'
+    click_button 'Submit'
+    expect(page).to have_content "Enter player 2 name"
+  end
+
+
+
+end
---
+++ spec/features/two_player_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature "Two player" do
+
+  scenario "Expect page 1 to enter player 1 details" do
+    visit('/')
+    expect(page).to have_content "Enter player 1 name"
+  end
+
+  scenario "Expect page 2 to enter player 2 details" do
+    visit('/')
+    fill_in :player_name, with: 'Lauren'
+    click_button 'Submit'
+    expect(page).to have_content "Enter player 2 name"
+  end
+
+
+
+end
