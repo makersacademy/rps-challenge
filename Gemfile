e960eb39686008d41006da3ff26b1fa26919157f
Jack <92jackhenderson@gmail.com>
2017-01-22 18:54:02 +0000

    rps challenge

--- Gemfile
+++ Gemfile
@@ -1,9 +1,16 @@
 source 'https://rubygems.org'
 
-ruby '2.2.3'
+ruby '2.3.0'
 
 gem 'sinatra'
 gem 'rake'
+gem 'capybara'
+gem 'shotgun'
+gem 'selenium-webdriver'
+gem 'rspec-sinatra'
+gem 'rspec'
+gem 'pry'
+
 
 group :test do
   gem 'rspec'
---
+++ lib/player.rb
@@ -0,0 +1,41 @@
+class Player
+  attr_accessor :your_hand
+
+  def initialize(attack_hand)
+    @your_hand = attack_hand
+  end
+
+  def play
+    puts "you attacked with #{@your_hand}"
+    @hands = [:rock,:paper,:scissors]
+    @AIhand = @hands.sample
+    @AIhand == @your_hand ? draw : not_draw
+  end
+
+  def draw
+    "It's a draw!
+    You both have #{@AIhand}"
+  end
+
+  def win
+    "You won!
+    The AI chose #{@AIhand}"
+  end
+
+  def lose
+    "You lost!
+    The AI chose #{@AIhand}"
+  end
+
+  def not_draw
+    case @your_hand
+      when :rock
+        @AIhand == :paper ? lose : win
+      when :paper
+        @AIhand == :rock ? win : lose
+      when :scissors
+        @AIhand == :paper ? win : lose
+      end
+  end
+
+end
