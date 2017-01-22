731d017bec2f049a2d82d5f43ed809234a89c104
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-22 15:38:46 +0000

    rock paper scissors class

---
+++ lib/rps.rb
@@ -0,0 +1,8 @@
+class RockPaperScissors
+  def rock_paper_scissors
+    OUTLOOKS.sample
+  end
+
+  private
+  OUTLOOKS = [:rock, :paper, :scissors]
+end
--- spec/features/enter_player_name_spec.rb
+++ spec/features/enter_player_name_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 feature 'Entry form'  do
   scenario 'displays player name' do
     sign_in_and_play
-    expect(page).to have_content('Player')
+    expect(page).to have_content('Player Name: Player')
   end
 end
