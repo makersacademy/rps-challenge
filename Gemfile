6595aac367443f6ef078309d562d3cf84a63c43c
Kat Hicks <kmhicks92@gmail.com>
2017-01-22 21:22:31 +0000

    Advanced styling of the app using css and google webfonts

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.4.0'
 
 gem 'sinatra'
 gem 'rake'
+gem 'google-webfonts'
 
 group :test do
   gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra'
+require 'google-webfonts'
 
 require './lib/game.rb'
 
