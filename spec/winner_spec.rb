ff062e279fb81f92d6c3712d73ee55706f18e162
Justinio14 <jbutwell@hotmail.com>
2017-01-22 14:54:10 +0000

    unit tests refctoring

--- spec/winner_spec.rb
+++ spec/winner_spec.rb
@@ -3,8 +3,35 @@ require './lib/winner'
 describe Winner do
   subject(:winner) { described_class.new }
 
-  it 'calculates who has won the game' do
-    expect(winner).to respond_to(:result)
+  context 'Marketter wins the game' do
+    it 'Marketeer chooses Rock opponent chooses Scissors' do
+      expect(winner.result('Rock', 'Scissors')).to eq("Congratulations you win!!")
+    end
+    it 'Marketeer chooses Paper opponent chooses Rock' do
+      expect(winner.result('Paper', 'Rock')).to eq("Congratulations you win!!")
+    end
+
+    it 'Marketeer chooses Scissors opponent chooses Paper' do
+      expect(winner.result('Scissors', 'Paper')).to eq("Congratulations you win!!")
+    end
+  end
+
+  context 'Marketeer loses the came' do
+    it 'Marketeer chooses Scissors opponent chooses Rock' do
+      expect(winner.result('Scissors', 'Rock')).to eq("Unlucky you lose!!")
+    end
+
+    it 'Marketeer chooses Rock opponent chooses Paper' do
+      expect(winner.result('Rock', 'Paper')).to eq("Unlucky you lose!!")
+    end
+
+    it 'Marketeer chooses Paper opponent chooses Scissors' do
+      expect(winner.result('Paper', 'Scissors')).to eq("Unlucky you lose!!")
+    end
+  end
+
+  it 'Game is a draw if both players choose the same item' do
+    expect(winner.result('Rock', 'Rock')).to eq("Game was a draw")
   end
 
 end
