9bedb5555da9dce1c55a68847ea94b608745d0f8
Paul Goodman <paulyjgooders@gmail.com>
2017-01-22 14:31:35 +0000

    two more feature tests added

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -28,3 +28,29 @@ feature 'Display weapon' do
   end
 
 end
+
+feature 'Playing the game' do
+
+  context "when I submit 'Rock' and computer generates 'Scissors'" do
+
+    scenario "my name appears as the winner" do
+
+      allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
+      sign_in_and_play
+      choose("weapon", option: 'Rock')
+      click_button('Submit')
+      expect(page).to have_content "Winner of this match is: Dave"
+    end
+  end
+
+  context "when I submit 'Scissors' and computer generates 'Rock'" do
+    scenario "computer's name appears as the winner" do
+
+      allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
+      sign_in_and_play
+      choose("weapon", option: 'Scissors')
+      click_button('Submit')
+      expect(page).to have_content "Winner of this match is: Deep Blue"
+    end
+  end
+end
