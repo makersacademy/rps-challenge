0d1243b0478fa985608c588e64c96616f4287ef1
Kat Hicks <kmhicks92@gmail.com>
2017-01-22 09:51:52 +0000

    Increased test coverage in player and computer classes

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,4 +8,12 @@ describe Player do
     expect(player).to have_attributes(name: "Kat", choice: :rock)
   end
 
+  it 'should respond to the method select_choice' do
+    expect(player).to respond_to(:select_choice).with(1).argument
+  end
+
+  it 'should change the choice variable when calling select_choice' do
+    expect{ player.select_choice(:paper) }.to change{ player.choice }
+  end
+
 end
