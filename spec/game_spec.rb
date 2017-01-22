77f2caac600671c755b54ea77fb395265a977d7c
Shereen Finney <shereenfinney@gmail.com>
2017-01-22 20:11:27 +0000

    Updated game_spec.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,6 +12,10 @@ describe Game do
     it 'allows a game to receive player 2' do
       expect(game).to respond_to(:player2)
     end
+    it 'calculates the winner' do
+      expect(game).to respond_to(:get_winner)
+    end
+
   end
 
 end
