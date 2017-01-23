515f59f344aa80a5c26de3a40d63466e6eafaa36
Sam Mckay <sammckay12@gmail.com>
2017-01-23 08:21:45 +0000

    updated Readme

--- lib/player.rb
+++ lib/player.rb
@@ -1,24 +1,9 @@
 class Player
 
-  attr_reader :name, :rock, :paper, :scissors
+  attr_reader :name
 
   def initialize(name)
     @name = name
   end
-
-
-
-
-  # def rock
-  #   "Rock"
-  # end
-  #
-  # def paper
-  #   "Paper"
-  # end
-  #
-  # def scissors
-  #   "Scissors"
-  # end
 
 end
