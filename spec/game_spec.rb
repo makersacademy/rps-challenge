f5bde91bfab7c73da9e214a0f62dc9b46890d207
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-23 11:20:36 +0000

    Updating game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,6 +11,22 @@ describe Game do
     end
   end
 
+  describe '#player1_choice' do
+    it "returns correct value" do
+      game.second_player("Andrew")
+      game.player1.set_choice(:Rock)
+      expect(game.player1_choice).to eq :Rock
+    end
+  end
+
+  describe '#player2_choice' do
+    it "returns correct value" do
+    game.second_player("Andrew")
+    game.player2.set_choice(:Rock)
+    expect(game.player2_choice).to eq :Rock
+  end
+  end
+
   describe "#winner" do
       it 'calculates the correct winner' do
         game.second_player("Andrew")
