728a4cceecf24fb21beb35cd457e8967fff77d5b
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-03 23:41:27 +0000

    added views and first capybara test passing

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
   end
 
   # start the server if ruby file executed directly
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,6 +1,12 @@
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+
+
 RSpec.feature 'homepage' do
-  scenario 'checking the infrastructure' do
+  scenario 'invites user to register' do
     visit '/'
-    expect(page).to have_text("Testing infrastructure working!")
+    expect(page).to have_text("Sign-in")
   end
+
 end
