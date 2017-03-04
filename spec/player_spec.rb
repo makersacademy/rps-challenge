b7e3bce7f80547a7b71e4281631412f4f0a70dc9
Ashwini <ashie.oh@gmail.com>
2017-03-04 10:06:29 +0000

    player unit test 2

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,4 +2,11 @@ require 'player'
 
 describe Player do
 
+  subject(:player1) {described_class.new("Dave")}
+  subject(:player2) {described_class.new("Mittens")}
+
+  it "returns players name" do
+    expect(player1.name).to eq "Dave"
+  end
+
 end
