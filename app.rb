03eaf684d311bad835ed86ce2440b69d176fbf62
Christos <chris.paraskeva10@gmail.com>
2017-03-04 13:43:45 +0000

    can begin game after the welcome screen / added some comments

--- app.rb
+++ app.rb
@@ -9,14 +9,20 @@ class Game < Sinatra::Base
   get '/' do
     erb :name
   end
-
+# this method will get the user input for player name and
+# save it to a session before redirecting to the /welcome method
   post '/names' do
     session['player_name'] = params[:player_name]
     redirect '/welcome'
   end
-
+# this method will take the session from /names method and
+# save it to an instance variable, then load up the welcome page
   get '/welcome' do
     @player_name = session['player_name']
     erb :welcome
+  end
+
+  get '/play' do
+    erb :play
   end
 end
