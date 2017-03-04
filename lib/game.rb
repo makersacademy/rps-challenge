78383afd20b496f0f2fd275957d52edd0245d68a
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-04 13:26:50 +0000

    game class has empty judge scores method. test respond to working

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+
+
+class Game
+
+  def initialize(human, computer)
+    @human = human
+    @computer = computer
+  end
+
+  def judge_scores
+  end
+
+end
---
+++ spec/game_spec.rb
@@ -0,0 +1,12 @@
+describe Game do
+  subject(:game) { Game.new(hum, comp) }
+  let(:hum) { double("human") }
+  let(:comp) { double("comp") }
+
+  describe "#judge_scores" do
+    it "responds to judges scores" do
+      expect(game).to respond_to(:judge_scores)
+    end
+  end
+  
+end
