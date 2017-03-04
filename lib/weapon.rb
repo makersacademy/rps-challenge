cf652c934a63e0ee8d40feb91efc148972d261d6
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-04 11:17:20 +0000

    extended weapon class to hold weapons it can beat

--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,9 +1,14 @@
 class Weapon
 
-  attr_reader :name
+  attr_reader :name, :wins
 
   def initialize(name)
     @name = name
+    @wins = []
+  end
+
+  def add_weapon(weapon)
+    @wins << weapon
   end
 
 end
--- spec/weapon_spec.rb
+++ spec/weapon_spec.rb
@@ -2,9 +2,19 @@ require 'weapon'
 
 describe Weapon do
   subject(:weapon){described_class.new('Rock')}
+  let(:scissors){double :scissors}
 
   it 'knows its name' do
     expect(weapon.name).to eq 'Rock'
+  end
+
+  it 'knows who it can beat' do
+    expect(weapon.wins).to be_an_instance_of Array
+  end
+
+  it 'adds weapons it can beat to the array' do
+    weapon.add_weapon(scissors)
+    expect(weapon.wins).to include(scissors)
   end
 
 end
