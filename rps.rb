b64b3feb85513d5ece013bf80d7f569243381141
Ashwini <ashie.oh@gmail.com>
2017-03-04 10:15:04 +0000

    used sessions & post/redirect/get

--- rps.rb
+++ rps.rb
@@ -8,6 +8,14 @@ class RPS < Sinatra::Base
   end
 
   post '/names' do
-    'Dave vs. Mittens'
+    session[:name_1] = params[:name_1]
+    session[:name_2] = params[:name_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name_1 = session[:name_1]
+    @name_2 = session[:name_2]
+    erb :play
   end
 end
