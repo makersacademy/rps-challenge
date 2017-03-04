c5fe7d56555284f6f6793afef5e08f3794687243
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-03-04 16:38:27 +0000

    User Story 1
    As a marketeer
    So that I can see my name in lights
    I would like to register my name before playing an online game

--- app/rps_web.rb
+++ app/rps_web.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
-  get '/' do
-    erb(:index)
+    enable :sessions
+    get '/' do
+    erb :index
+  end
+
+  post '/name' do
+      session[:name] = params[:name]
+      redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
+    erb :play
   end
 
   run if app_file == $0
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 # As a marketeer
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
-
+feature 'name registration' do
+  scenario 'allows player to enter his/her name and see it' do
+      visit '/'
+      fill_in 'name', with: 'Noora'
+      click_button 'Submit'
+      expect(page).to have_content 'Noora'
+  end
+end
 
 
 
