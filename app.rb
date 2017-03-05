2836154f150cc3b57d8758cd96045d4525189ae6
Bernard Malhame <bernard.malhame@gmail.com>
2017-03-05 09:10:48 +0000

    added game class and removed global variable

--- app.rb
+++ app.rb
@@ -1,37 +1,40 @@
 require 'sinatra/base'
 require_relative 'lib/player'
 require_relative 'lib/computer'
+require_relative 'lib/game'
 
 class RPS < Sinatra::Base
 
   enable :sessions
+
+  before do
+    @game = Game.instance
+  end
 
   get '/' do
     erb(:index)
   end
 
   post '/name' do
-    $player = Player.new(params[:player_name])
-    $computer = Computer.new
+    player = Player.new(params[:player_name])
+    computer = Computer.new
+    @game = Game.create(player,computer)
     redirect '/play'
   end
 
   get '/play' do
-    @player = $player
     erb(:play)
   end
 
   post '/game' do
-    @player = $player
-    @computer = $computer
-    $choice = @player.choose(params[:choice])
-    $comp_choice = @computer.choose_hand
+    session[:player_choice] = @game.player.choose(params[:choice])
+    session[:comp_choice] = @game.computer.choose_hand
     redirect '/result'
   end
 
   get '/result' do
-    @choice = $choice
-    @comp_choice = $comp_choice
+    @player_choice = session[:player_choice]
+    @comp_choice = session[:comp_choice]
     erb(:result)
   end
 
