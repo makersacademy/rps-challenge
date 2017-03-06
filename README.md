3597abcfb8e7bc6b165f267f25e4efd11044203e
Nicholas Leacock <nicholas@swissmail.com>
2017-03-06 08:57:23 +0000

    Minor refactor, README update and nested Describes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 #Rock, Paper, Scissors Weekend Challenge
+
+
+I decided to move the Scissors button to the middle of the three even though it doesn't read as expected (Rock, Scissors, Paper?). I did this because being the longest word it stuck out at the end and was graphically jarring. At this eye-candy stage in the game I felt the user would notice the asymmetry before the odd word order.
+
+
+#With more time I would -
+
+Implement a best-of-three game as per the accepted game rules, perhaps using an array to store scores temporarily within the Game class.
+
+The HTML in the .erb files is repetitive. With time I would research how to farm it out to a single file and then link the rest of them to it.
+
+#Issues
+
+The Game class repeats instantiation in every ‘it’ block. I considered using doubles at each expectation but that seemed to be replacing one type of repetition for another. However I need practice with doubles and so opted them in.
+
+I couldn't see any other way to tackle the problem other than by overhauling my code so that the Game class wouldn't expect any arguments.
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,49 +1,63 @@
 
 describe Game do
+
   describe '#result' do
-    it "returns 'draw' when computer and player selection match" do
-      game = Game.new(:ROCK, :ROCK)
-      expect(game.result).to eq(:draw)
+
+    describe 'draw games' do
+
+      it "returns 'draw' when computer and player selection match" do
+        game = Game.new(:ROCK, :ROCK)
+        expect(game.result).to eq(:draw)
+      end
+
+      it "returns 'draw' when computer and player selection match" do
+        game = Game.new(:SCISSORS, :SCISSORS)
+        expect(game.result).to eq(:draw)
+      end
+
+      it "returns 'draw' when computer and player selection match" do
+        game = Game.new(:PAPER, :PAPER)
+        expect(game.result).to eq(:draw)
+      end
+
     end
 
-    it "returns 'draw' when computer and player selection match" do
-      game = Game.new(:SCISSORS, :SCISSORS)
-      expect(game.result).to eq(:draw)
+    describe 'win games' do
+
+      it "returns 'you win' when player beats computer" do
+        game = Game.new(:ROCK, :SCISSORS)
+        expect(game.result).to eq(:win)
+      end
+
+      it "returns 'you win' when player beats computer" do
+        game = Game.new(:PAPER, :ROCK)
+        expect(game.result).to eq(:win)
+      end
+
+      it "returns 'you win' when player beats computer" do
+        game = Game.new(:SCISSORS, :PAPER)
+        expect(game.result).to eq(:win)
+      end
+
     end
 
-    it "returns 'draw' when computer and player selection match" do
-      game = Game.new(:PAPER, :PAPER)
-      expect(game.result).to eq(:draw)
-    end
+    describe 'lose games' do
 
-    it "returns 'you win' when player beats computer" do
-      game = Game.new(:ROCK, :SCISSORS)
-      expect(game.result).to eq(:win)
-    end
+      it "returns 'you lose' when computer beats player" do
+        game = Game.new(:ROCK, :PAPER)
+        expect(game.result).to eq(:lose)
+      end
 
-    it "returns 'you win' when player beats computer" do
-      game = Game.new(:PAPER, :ROCK)
-      expect(game.result).to eq(:win)
-    end
+      it "returns 'you lose' when computer beats player" do
+        game = Game.new(:PAPER, :SCISSORS)
+        expect(game.result).to eq(:lose)
+      end
 
-    it "returns 'you win' when player beats computer" do
-      game = Game.new(:SCISSORS, :PAPER)
-      expect(game.result).to eq(:win)
-    end
+      it "returns 'you lose' when computer beats player" do
+        game = Game.new(:SCISSORS, :ROCK)
+        expect(game.result).to eq(:lose)
+      end
 
-    it "returns 'you lose' when computer beats player" do
-      game = Game.new(:ROCK, :PAPER)
-      expect(game.result).to eq(:lose)
-    end
-
-    it "returns 'you lose' when computer beats player" do
-      game = Game.new(:PAPER, :SCISSORS)
-      expect(game.result).to eq(:lose)
-    end
-
-    it "returns 'you lose' when computer beats player" do
-      game = Game.new(:SCISSORS, :ROCK)
-      expect(game.result).to eq(:lose)
     end
 
   end
