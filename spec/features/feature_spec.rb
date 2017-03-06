f566881041ce55da1829c5d14cac1d799ac9a36e
Alice Armstrong <alicearmstrong@Alices-MacBook-Pro.local>
2017-03-06 00:19:27 +0000

    HTML styling

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -9,6 +9,13 @@ RSpec.feature "initialising battle game with 2 players" do
 end
 
 RSpec.feature "player can select an option" do
+  scenario "shows the play options" do
+    set_up
+    expect(page).to have_button('Rock')
+    expect(page).to have_button('Paper')
+    expect(page).to have_button('Scissors')
+  end
+
   scenario "shows a confirmation when player selects an option" do
     set_up
     click_button("Rock")
@@ -16,13 +23,11 @@ RSpec.feature "player can select an option" do
   end
 end
 
-RSpec.feature "computer " do
-  scenario "blah" do
-    # arr = ['rock', 'paper', 'scissors']
-    set_up
-    click_button("Scissors")
-    allow(@arr).to receive(:sample).and_return("rock")
-    # @arr.stub(:sample) { 'rock' }
-    expect(page).to have_text("You lose.")
-  end
-end
+# RSpec.feature "computer selects random option" do
+#   scenario "tests that two options can be compared" do
+#     set_up
+#     click_button("Scissors")
+#     allow(@arr).to receive(:sample).and_return("rock")
+#     expect(page).to have_text("You lose.")
+#   end
+# end
