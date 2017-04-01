477c258c85503dc662b1ddf88515fe1d1bcc97c6
Nick Rupp <nickrupp95@gmail.com>
2017-04-01 14:11:01 +0100

    Add player1 name input and play page

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require 'sinatra/base'
-
 class RPS < Sinatra::Base
+  enable :sessions
 
   get('/') do
     erb(:splash)
+  end
+
+  get('/play') do
+    @player_1_name = session[:player_1_name]
+    erb(:play)
+  end
+
+  post('/names') do
+    session[:player_1_name] = params[:player_1_name]
+    redirect('/play')
   end
 
 end
--- spec/features/page_infastructure_spec.rb
+++ spec/features/page_infastructure_spec.rb
@@ -1,7 +1,19 @@
 feature 'page_infrastructure' do
+
   scenario 'lands on the splash page on start up' do
-    visit '/'
+    visit('/')
     expect(page).to have_content "Rock, Paper, Scissors"
+    #expect page.has_title?("my title")).to eq(true)
     #expect(page.has_xpath?('/').to eq(true)
   end
+
+  scenario 'player 1 signs in and enters name and is then taken to play page' do
+    DEFAULT_PLAYER_NAME = 'Nick'
+    visit('/')
+    fill_in('player_1_name', :with => DEFAULT_PLAYER_NAME)
+    click_button('Play')
+    expect(page).to have_content('Play')
+    expect(page).to have_content "#{DEFAULT_PLAYER_NAME}"
+  end
+
 end
