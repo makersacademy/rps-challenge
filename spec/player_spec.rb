60129b3e63790d4695327ed56b06116cccdab2e5
emmpak <eiliev05@yahoo.com>
2017-04-01 08:36:07 +0100

    Add a player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,11 @@
 require 'player'
 
 describe Player do
-  subject(:player) { described_class.new }
+  subject(:player) { described_class.new("Yoda") }
+
+  describe '#name' do
+    it 'returns the name of the player' do
+      expect(player.name).to eq "Yoda"
+    end
+  end
 end
