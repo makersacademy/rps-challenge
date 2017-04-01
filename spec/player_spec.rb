066d2ab5a731aa1efa9d8ed791ec9d72de973b09
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-01 10:36:11 +0100

    updated RSpec tests and refactored

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,7 @@
 require 'player'
 
 describe Player do
-  subject(:dave) { Player.new('Dave') }
+  subject(:dave) { described_class.new('Dave') }
 
   describe '#name' do
     it 'returns the name' do
