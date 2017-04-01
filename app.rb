240066ce4a0e772bddcc212afd6544649aeda7b0
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-01 09:49:24 +0100

    player can enter name to play, added web_helper

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
 
-class RPS < Sinatra::Base
+class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
+  end
+
+  post '/name' do
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,19 @@
 # ensure the following is AT THE TOP of your spec_helper.rb to get test coverage stats
 require 'coveralls'
 require 'simplecov'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'byebug'
+require 'features/web_helpers'
+
+ENV['RACK_ENV'] = 'test'
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..','app.rb')
+
+# tell Capybara about our app class
+Capybara.app = RockPaperScissors
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
@@ -9,6 +22,3 @@ SimpleCov.formatters = [
 # replace following line with SimpleCov.start to get coverage stats locally
 SimpleCov.start
 # run `open coverage/index.html` from the command line to view details
-
-require 'byebug'
-require 'capybara/rspec'
