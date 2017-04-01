678be6a98dbbcf43bfd3d93185ce763896b3fe50
Naz <nazmalik08@gmail.com>
2017-04-01 19:48:54 +0100

    update tests with full lizard names

--- spec/features/generate_winner_spec.rb
+++ spec/features/generate_winner_spec.rb
@@ -4,7 +4,7 @@ feature 'generate winner' do
   scenario 'selects lizard' do
     sign_in_and_play
     click_button 'Orange-throat'
-    expect(page).to have_content 'the result is'
+    expect(page).to have_content 'chose the'
   end
 end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,21 +1,21 @@
 require 'game'
 
 describe Game do
-  let(:human) {double(:human, :name => "Naz", :choice => "yellow-throat")}
-  let(:human2) {double(:human2, :name => "Naz", :choice => "orange-throat")}
-  let(:comp) {double(:comp, :name => "comp", :choice => "orange-throat")}
-  let(:comp2) {double(:comp2, :name => "comp", :choice => "blue-throat")}
+  let(:human) {double(:human, :name => "Naz", :choice => "Yellow-throated sneaker")}
+  let(:human2) {double(:human2, :name => "Naz", :choice => "Ultradominant orange-throat")}
+  let(:comp) {double(:comp, :name => "comp", :choice => "Ultradominant orange-throat")}
+  let(:comp2) {double(:comp2, :name => "comp", :choice => "Dominant blue-throat")}
   subject(:game) {described_class.new(human, comp)}
 
   it 'initializes game with a human' do
     expect(game.human).to eq human
   end
 
-  it 'human wins when they select yellow and comp selects orange' do
+  it 'human wins when they select yellow and comp selects Ultradominant orange' do
     expect(game.calculate_winner).to eq "Human wins"
   end
 
-  it 'comp wins when they select blue and human selects yellow' do
+  it 'comp wins when they select Dominant blue and human selects yellow' do
     game2 = Game.new(human, comp2)
     expect(game2.calculate_winner).to eq "Comp wins"
   end
