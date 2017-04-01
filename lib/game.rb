85651e0beee9d86b9983ff362c1a6cbc750798db
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-01 19:40:58 +0100

    add ability to capture players selection

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,18 @@
 class Game
 
-  attr_reader :current_player, :players
+  attr_reader :current_player, :player, :user_selection
 
   def initialize(player)
     @player = player
     @current_player = player
   end
 
-  def player1
-    players.first
+  def player_selection(selection)
+    @user_selection = selection
   end
+
+  private
+  
+  attr_writer :user_selection
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,9 +9,13 @@ describe Game do
     it 'initalizes a game' do
       expect(game.current_player).to eq ruan
     end
-
-    # it 'contains array of players' do
-    #   expect(game.players).to include ruan, robot
-    # end
   end
+
+  describe '#player_selection' do
+    it 'should return the button the player clicked' do
+      game.player_selection('rock')
+      expect(game.user_selection).to eq 'rock'
+    end
+  end
+
 end
