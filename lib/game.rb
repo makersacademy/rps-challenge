e1c85d327c1e1d994ee5cd95039ab5391c6c27b5
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-04-02 21:27:06 +0100

    added results to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,27 @@
+require_relative 'player'
 
 class Game
 
+  attr_reader :player1, :player2
+
   WEAPONS = [:rock, :paper, :scissors]
 
+  RULES = { rock: :scissors,
+          paper: :rock,
+          scissors: :paper }
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def result
+    if RULES[player1.choice] == player2.choice
+      :win
+    elsif RULES[player2.choice] == player1.choice
+      :lose
+    elsif player1.choice == player2.choice
+      :draw
+    end
+  end
 end
