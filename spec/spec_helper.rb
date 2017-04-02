06afdd57f2107592550148ace9399a209a8cb41d
Colin Turner <colinturner34@gmail.com>
2017-04-02 21:11:40 +0100

    Update tree

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
 # ensure the following is AT THE TOP of your spec_helper.rb to get test coverage stats
+ENV['RACK_ENV'] = 'test'
+
+# require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'features/web_helpers.rb'
 require 'coveralls'
 require 'simplecov'
 
@@ -6,6 +14,7 @@ SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
+Coveralls.wear!
 # replace following line with SimpleCov.start to get coverage stats locally
 SimpleCov.start
 # run `open coverage/index.html` from the command line to view details
