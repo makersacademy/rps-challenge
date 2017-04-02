19c1d4bc9b1d587bd96c0198819dcc8caf5812ef
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-02 14:01:24 +0100

    Added sessions

--- app.rb
+++ app.rb
@@ -2,17 +2,19 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_name = params[:player_name]
-    erb(:play)
+    session[:player_name] = params[:player_name]
+    redirect '/play'
   end
 
   get '/play' do
-    @player_name = params[:player_name]
+    @player_name = session[:player_name]
     erb(:play)
   end
 
