1a585899fd19142016fbbde06943a60b4764c322
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-02 12:16:04 +0100

    changed coveralls in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,5 +20,5 @@ SimpleCov.formatters = [
   Coveralls::SimpleCov::Formatter
 ]
 # replace following line with SimpleCov.start to get coverage stats locally
-SimpleCov.start
+Coveralls.wear!
 # run `open coverage/index.html` from the command line to view details
