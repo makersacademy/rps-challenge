020301bbc65e8323e534835329f7422de54804c6
emmpak <eiliev05@yahoo.com>
2017-04-03 07:18:48 +0100

    Fix computer option

--- app.rb
+++ app.rb
@@ -15,22 +15,27 @@ class RPS < Sinatra::Base
 
   post '/name' do
     Player.set_instance(params["name"])
-    Player.set_instance(params["second_name"]) if params["second_name"]
+    p params["second_name"]
+    p Player.set_instance(params["second_name"]) if params["second_name"] != ""
     redirect '/start'
   end
 
   get '/start' do
+    p @player2
     erb(:play)
   end
 
   post '/choice' do
     @player1.save_choice(params["choice"])
+    @player2.save_choice(params["choice2"]) if !!@player2
+    p params["choice"]
     redirect '/result'
   end
 
   get '/result' do
+    p @player2
     if @player2.nil?
-      @game = Game.new(@player, Computer.new)
+      @game = Game.new(@player1, Computer.new)
     else
       @game = Game.new(@player1, @player2)
     end
