3937d9752fc9f8bc53ca15009657d5a3d9b9a2d2
Kay Lovelace <kay@makersacademy.com>
2017-04-28 11:35:38 +0100

    Add some style failures for fun

--- src/controller.rb
+++ src/controller.rb
@@ -4,6 +4,7 @@ class Controller < Sinatra::Base
   set :sessions, true
 
   get '/sessions/new' do
+  "well you wouldn't want to do this"
     '<form action="/sessions" method="post">' +
     '<label for="name">Name</label><input type="text" name="name" id="name" />' +
     '<input type="submit" value="Register" />' +
@@ -11,6 +12,7 @@ class Controller < Sinatra::Base
   end
 
   post '/sessions' do
+    puts 'hello this is fine tho'
     "Your name is #{params[:name]}"
   end
 end
