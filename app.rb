1fe19f85e9b65c1676a98a17a8a94ab84cd03d9a
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-28 21:47:10 +0100

    Adds index.erb and game.erb -User can enter name at / -Name gets stored using a Post request in app.rb -Uses post-redirect-loop to safely store User name -After entering name, User is redirected to /game view

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require 'sinatra/base'
 
 class Game < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
-     'Hello World'
+     erb :index
   end
+
+  post '/names' do
+    session[:name] = params[:name]
+    redirect '/game'
+  end
+
+  get '/game' do
+    @name = session[:name]
+    erb :game
+  end
+
+  run! if app_file == $0
 
 end
---
+++ spec/features/submit_name_spec.rb
@@ -0,0 +1,7 @@
+require_relative 'web_helpers'
+feature 'User submits name' do
+  scenario 'User enters name and is presented with game' do
+    sign_in
+    expect(page).to have_content 'User is ready to Rock-Paper-Scissor!!'
+  end
+end
