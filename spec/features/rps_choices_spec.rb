37d25df10e2aae3db0fcc1c0ccc23058949cb28c
Alex Chalk <alexchalk17@gmail.com>
2017-04-29 18:46:03 -0400

    Refactor views using partials

--- spec/features/rps_choices_spec.rb
+++ spec/features/rps_choices_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../app.rb'
 
-feature 'displaying rps choices' do
-  scenario 'user enters name' do
+feature 'displaying of rps choices' do
+  scenario 'user has signed in' do
     sign_in
     expect(page).to have_content('Rock')
     expect(page).to have_content('Paper')
