31edd2c5271a6e14920c6fc56103dfc825599dd7
david gray <davidseangray@gmail.com>
2017-04-29 16:16:08 +0100

    first test passes for printing to page

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra/base'
 
-class RPS < Sinatra::Base
-  get '/' do
-    'Rock, Paper, Scissors Game!!!!!'
-  end
+class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
+  get '/' do
+    'Rock, Paper, Scissors Game'
+  end
 
   run! if app_file == $0
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+ENV['RACK_ENV'] = 'test'
+
+Capybara.app = RockPaperScissors
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
