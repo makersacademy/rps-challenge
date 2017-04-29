54a9b8bb5cf9f82cb25e2490cfcf58e3b1fd5b9b
JayWebDevCom <jaiye.s@gmail.com>
2017-04-29 01:25:08 +0100

    begin routing

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'sinatra'
 require './lib/game'
 
 class RPSWeb < Sinatra::Application
-  
+
   get '/' do
     erb(:index)
+  end
+
+  post '/play' do
+    @val = params[:weapon]
+    erb(:outcome)
   end
 
 end
