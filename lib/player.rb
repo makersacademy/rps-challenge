0e6f4f5997f3365ba0e6b590d85dca2cda55c73e
Filipe Fasolin <ffasolin@gmail.com>
2017-04-29 17:29:44 +0100

    Adds Player class

---
+++ lib/player.rb
@@ -0,0 +1,17 @@
+class Player
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.create(name)
+    @player = Player.new(name)
+  end
+
+  def self.instance
+    @player
+  end
+
+end
