93d804671fdf30c2b9b21ccbe1f506f44f781e19
Paula <pmuldoon@umich.edu>
2017-04-29 21:07:49 +0100

    defined subject as paula in player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,23 +1,23 @@
 require 'player'
 describe 'Player' do
-  subject = Player.new
+  subject(:paula) { Player.new}
   computer = Computer.new
 
   describe '#score' do
     it 'expects a new player to have a default score of 0' do
-      expect(subject.score).to eq 0
+      expect(paula.score).to eq 0
     end
   end
 
   describe '#attack_with_rock' do
     it 'expects player to respond to #attack_with_rock' do
-      expect(subject).to respond_to(:attack_with_rock).with(1).argument
+      expect(paula).to respond_to(:attack_with_rock).with(1).argument
     end
   end
 
   describe '#attack_with_rock score' do
     it 'expects the player to win when computer plays scissors' do
-      expect{subject.attack_with_rock(computer)}.to change{subject.score}.by 1
+      expect{paula.attack_with_rock(computer)}.to change{paula.score}.by 1
     end
   end
 end
