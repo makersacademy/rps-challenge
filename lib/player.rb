f7a8004d816e4de7332d80adf9edacdce4f049a5
Timothy Robinson <timmyrobby@gmail.com>
2017-04-29 15:40:50 +0100

    Adds default player name of "Player"

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,10 @@
 class Player
   attr_reader :name, :choice, :old_name
+  attr_writer :name
 
   def initialize(name, choice)
     @name = name
+    @name = "Player" if @name == ""
     @choice = choice.to_sym
   end
 
