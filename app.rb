0e20f007c0e13e6256cecff46044c311c49943da
Filipe Fasolin <ffasolin@gmail.com>
2017-04-29 17:30:25 +0100

    Updates /play path and params

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './lib/player'
 
 class Game < Sinatra::Base
 
@@ -6,5 +7,18 @@ class Game < Sinatra::Base
     erb(:home)
   end
 
-run! if app_file == $PROGRAM_NAME
+  post '/name' do
+    @player = Player.create(params[:player])
+    redirect '/play'
+  end
+
+  before do
+    @player = Player.instance
+  end
+
+  get '/play' do
+    erb(:play)
+  end
+
+  run! if app_file == $PROGRAM_NAME
 end
