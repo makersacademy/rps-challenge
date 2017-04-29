bec9bf4665f26e312e0225feb964f3ecac427ff3
Bailey Kursar <bkursar@gmail.com>
2017-04-29 12:33:08 +0100

    Saving progress, with a new test for Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,10 @@
 require 'game'
 
 describe Game do
-
+  describe '#player' do
+    it 'initializes a game with a player' do
+      game_new = Game.new('Bob')
+      expect(game_new.player).to eq 'Bob'
+    end
+  end
 end
