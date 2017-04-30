9acf95e65488d00104baa2c56d2df4669e05e0a1
Timothy Robinson <timmyrobby@gmail.com>
2017-04-30 15:33:33 +0100

    Updates feature test

--- spec/features/controller_spec.rb
+++ spec/features/controller_spec.rb
@@ -11,4 +11,22 @@ feature 'Starting up the app' do
     sign_in_and_play
     expect(page).to have_content 'Trump chose Rock'
   end
+
+  scenario 'can change country to France' do
+    visit '/'
+    click_button 'Play for France'
+    expect(page).to have_content 'Macron'
+  end
+
+  scenario 'can change country to France' do
+    visit '/'
+    click_button 'Play for the UK'
+    expect(page).to have_content 'Corbyn'
+  end
+
+  scenario 'can change country back to USA' do
+    visit '/french'
+    click_button 'Play for America'
+    expect(page).to have_content 'Trump'
+  end
 end
