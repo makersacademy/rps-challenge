a3e71a123f1e2a7cdc4e7fa6d99e639e0559e67a
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-30 19:27:55 +0100

    Refactors RPS #result method -Adds OUTCOME constant for result confirmation -Removes redundant tests

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,41 +1,34 @@
 require_relative 'computer'
 
 class RPS
-  RULES = [ { user: :rock, computer: :scissors },
+  OUTCOME = { draw: 'Its a Draw!', win: 'You win!', lose: 'Computer wins :(' }
+  RULES = [{ user: :rock, computer: :scissors },
             { user: :paper, computer: :rock },
             { user: :scissors, computer: :paper }
           ]
 
   attr_reader :pair
 
-  def initialize(user_weapon = :rock, computer = Computer)
+  def initialize(user_weapon, computer = Computer)
     @pair = {}
     @user_weapon = user_weapon
     @computer = computer.new
     draw_weapons
   end
 
-  def draw_weapons
-    @pair = { user: @user_weapon, computer: random_computer_weapon }
-  end
-
-  def combinations
-    RULES
-  end
-
   def result
-    return "Its a Draw!" if draw?
-    if @pair == RULES[0] || @pair == RULES[1] || @pair == RULES[2]
-      return 'You win!'
-    else
-      return 'Computer wins :('
-    end
+    return OUTCOME[:draw] if draw?
+    RULES.each { |hash| @pair == hash } ? OUTCOME[:win] : OUTCOME[:lose]
   end
 
   private
 
+  def draw_weapons
+    @pair = { user: @user_weapon, computer: random_computer_weapon }
+  end
+
   def draw?
-    self.pair[:user] == self.pair[:computer]
+    @pair[:user] == @pair[:computer]
   end
 
   def random_computer_weapon
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -4,23 +4,4 @@ describe RPS do
   subject(:rps) { described_class.new }
   let(:rules) { RPS::RULES }
 
-  describe '#combinations' do
-    it 'returns winning combinations' do
-      expect(rps.combinations).to eq rules
-    end
-  end
-
-  context 'Tests using stub' do
-
-    describe '#draw_weapons' do
-      let(:pair) { { :user => :rock, :computer => :scissors } }
-
-      it 'pairs user weapon with random computer generated weapon' do
-        rps.stub(:random_computer_weapon) { :scissors }
-        expect(rps.draw_weapons).to eq pair
-      end
-    end
-
-  end
-
 end
