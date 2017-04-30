5fa675a05e034784dc74ea58323dbbb3ea6b4c58
Filipe Fasolin <ffasolin@gmail.com>
2017-05-01 00:09:12 +0100

    Updates tests

--- spec/features/confirmation_spec.rb
+++ spec/features/confirmation_spec.rb
@@ -4,6 +4,6 @@ feature '/confirmation' do
   scenario 'player sees results' do
     sign_in_and_play
     click_button 'Rock'
-    expect(page).to have_content('The Bored Bot wins!')
+    expect(page).to have_content('wins!')
   end
 end
