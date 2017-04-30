392fb7f2f890ef4f214c1fc5049e97c999b6738e
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-30 23:51:33 +0100

    Test passes and also added play erb file to show player name

--- app.rb
+++ app.rb
@@ -5,4 +5,9 @@ class Rockpaperscissors <  Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/playername' do
+    @player_1_name = params[:player_1_name]
+    erb :play
+  end
 end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,6 +1,6 @@
 feature 'Enter name' do
   scenario 'Submiting your name' do
-    vist ('/')
+    visit ('/')
     fill_in :player_1_name, with: 'Adam'
     click_button 'Submit'
     expect(page).to have_content 'Adam dares to challenge the AI!'
