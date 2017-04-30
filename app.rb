7d84376fc1d6e4fc780909bbf799e477819ca5bb
Nigel Said <nigelsaid@gmail.com>
2017-04-30 12:58:50 +0100

    allows user to pick game and enter names

--- app.rb
+++ app.rb
@@ -6,5 +6,19 @@ class RPS < Sinatra::Base
     'Testing infrastructure working!'
   end
 
+  get '/' do
+    erb :start_menu
+  end
+
+  get '/game_start' do
+    erb :names
+  end
+
+  post '/names' do
+    @player_1_name = (params[:player_1_name])
+    @player_2_name = (params[:player_2_name])
+    erb :play
+  end
+
   run! if app_file == $0
 end
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,10 @@
+feature 'Enter names' do
+   scenario 'submitting names' do
+     visit('/')
+     click_button "Let's play!"
+     fill_in :player_1_name, with: 'Nigel'
+     fill_in :player_2_name, with: 'Peter'
+     click_button 'Submit'
+     expect(page).to have_content 'Nigel vs. Peter'
+   end
+ end
