fdfa9a120e5d6eaff82c5331e32dae12a48acf0c
Anthony Crisp <anthony_crisp@hotmail.com>
2017-04-30 17:36:22 +0100

    Player one can choose a weapon

--- spec/features/play_page_spec.rb
+++ spec/features/play_page_spec.rb
@@ -1,8 +1,27 @@
-feature "Play page:" do
+feature "play page:" do
+
+  before { submit_names("Ant") }
 
   scenario "user sees name" do
-    submit_names("Ant")
     expect(page).to have_content "Player One: Ant"
+  end
+
+  scenario "user sees three options" do
+    expect(page).to have_content "ROCK"
+    expect(page).to have_content "PAPER"
+    expect(page).to have_content "SCISSORS"
+  end
+
+  scenario "user can choose rock" do
+    within('div#options') { within('div#rock') { click_button 'Choose' } }
+  end
+
+  scenario "user can choose paper" do
+    within('div#options') { within('div#paper') { click_button 'Choose' } }
+  end
+
+  scenario "user can choose scissors" do
+    within('div#options') { within('div#scissors') { click_button 'Choose' } }
   end
 
 end
