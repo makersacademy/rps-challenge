717ddb2e52f83f77ba944a98b183a0a132eb388b
Sami <sami@Samis-MBP.home>
2017-04-30 18:09:05 +0100

    form allowing player to enter name

--- spec/testing_spec.rb
+++ spec/testing_spec.rb
@@ -2,7 +2,7 @@ feature 'Testing infrastructure' do
 
   scenario 'player name checking' do
     visit('/')
-    expect(page).to have_content 'Test'
+    expect(page).to have_content 'Please Enter Name'
   end
 
 end
