b25214e2d3e39f1c867d7cee2d8f1c13578c081b
Filipe Fasolin <ffasolin@gmail.com>
2017-04-30 15:21:25 +0100

    Adds weapons

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,8 +1,15 @@
 require_relative 'web_helper'
 
 feature '/play' do
-  scenario 'player can choose weapon' do
+  scenario 'player prompted to choose weapon' do
     sign_in_and_play
     expect(page).to have_content('Choose your weapon:')
+  end
+
+  scenario 'player gets to choose a weapon' do
+    sign_in_and_play
+    expect(page).to have_content('Rock')
+    expect(page).to have_content('Paper')
+    expect(page).to have_content('Scissors')
   end
 end
