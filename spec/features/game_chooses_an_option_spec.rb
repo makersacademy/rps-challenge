64b44120e8125a9b46eb291ddac290f379236a9e
Robert Jones <robert.g.jones@gmail.com>
2017-04-30 22:26:36 +0100

    Removes superceded feature test

--- spec/features/game_chooses_an_option_spec.rb
+++
@@ -1,9 +0,0 @@
-require './rps_game.rb'
-
-xfeature 'Computer chooses a shape' do
-  scenario 'Chooses rock' do
-    sign_in_and_play
-    click_button 'Rock'
-    expect(page).to have_content 'Computer threw: Rock'
-  end
-end
