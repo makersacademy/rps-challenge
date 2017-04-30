e124eab4553ffadff5e16a1e74b05d39dd05e843
JayWebDevCom <jaiye.s@gmail.com>
2017-04-30 22:31:35 +0100

    add hound refactor javascript

--- public/site.js
+++ public/site.js
@@ -1,16 +1,16 @@
 function validateForm() {
   var x = document.getElementById("name1").value;
   if (x == "") {
-      alert("Name must be filled out");
+      window.alert("Name must be filled out");
       return false;
   }
 }
 
-function validateForm_2() {
+function validateForm2() {
   var x = document.getElementById("name1").value;
   var y = document.getElementById("name2").value;
   if (x === "" || y === "" ) {
-      alert("All form fields must be filled out");
+      window.alert("All form fields must be filled out");
       return false;
   }
 }
