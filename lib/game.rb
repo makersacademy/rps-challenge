5180b86fee94f3134865daba4c83a37b1b41c940
milesillsley <milesillsley@hotmail.co.uk>
2017-05-01 19:42:06 +0100

    test for player 2 name entry

--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,19 @@
 class Game
-
-  def initialize
-    @weapons = ["Rock", "Paper", "Scissors"]
-  end
+  WEAPONS = ["Rock", "Paper", "Scissors"]
 
   def random_weapon
-    @weapons.sample
+    WEAPONS.sample
   end
 
   def check(weapon_1, weapon_2)
 
     return "DRAW!" if weapon_1 == weapon_2
 
-    until @weapons[@weapons.size / 2] == weapon_1 do
-      @weapons.unshift(@weapons.pop)
+    until WEAPONS[WEAPONS.size / 2] == weapon_1 do
+      WEAPONS.unshift(WEAPONS.pop)
     end
 
-    @weapons.index(weapon_1) > @weapons.index(weapon_2) ? weapon_1 : weapon_2
+    WEAPONS.index(weapon_1) > WEAPONS.index(weapon_2) ? weapon_1 : weapon_2
 
   end
 
--- spec/features/name_entry_spec.rb
+++ spec/features/name_entry_spec.rb
@@ -1,6 +1,17 @@
 feature "Name Entry" do
+
   scenario "Marketeer can register their name" do
     dwayne_signs_in
     expect(page).to have_content "Dwayne 'The Marketeer' Johnson"
   end
+
+  scenario "Two Marketeers can register their name" do
+    dwayne_signs_in
+    choose "Rock"
+    click_button "Add player"
+    fill_in :player_2_name, with: "Edward Marketeerhands"
+    click_button "Submit"
+    expect(page).to have_content "Edward Marketeerhands"
+  end
+
 end
