2e233b5a4ea6ddbd805665ef6e29587908c8a0a0
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-01 20:04:03 +0100

    Moves outcome test to game_spec

--- spec/features/opponent_feature_spec.rb
+++ spec/features/opponent_feature_spec.rb
@@ -29,12 +29,5 @@ feature 'Opponent randomly selects a shape' do
        expect(page).to have_content "The other player chose Scissors!"
      end
 
-     scenario 'Player wins' do
-       allow_any_instance_of(Opponent).to receive(:randomise).and_return(:scissors)
-       visit '/'
-       fill_in 'name', with: 'Ben'
-       click_button 'Submit'
-       click_button 'Rock'
-       expect(page).to have_content "You win!"
-     end
+     
   end
