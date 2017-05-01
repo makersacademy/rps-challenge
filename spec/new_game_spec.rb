518ba51e5d912b299a0ed86eeb09ab69aab8e418
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-05-01 23:13:50 +0100

    adds full tests for NewGame class

--- spec/new_game_spec.rb
+++ spec/new_game_spec.rb
@@ -13,5 +13,15 @@ describe NewGame do
       game = NewGame.new('scissors', 'rock')
       expect(game.winner?).to eq 'You lost'
     end
+
+    it 'returns "won" if the player wins the game' do
+      game = NewGame.new('rock', 'scissors')
+      expect(game.winner?).to eq 'You won'
+    end
+
+    it 'returns "draw" if the player draws with the opponent' do
+      game = NewGame.new('rock', 'rock')
+      expect(game.winner?).to eq 'You drew with your opponent'
+    end
   end
 end
