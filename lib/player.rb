1e9f9bbf6638f6572c9474a0f3b6759b0af04862
milesillsley <milesillsley@hotmail.co.uk>
2017-05-01 23:51:44 +0100

    implement player choosing weapon

--- lib/player.rb
+++ lib/player.rb
@@ -2,10 +2,19 @@ class Player
 
   def initialize(name)
     @name = name
+    @weapon
   end
 
   def name
     @name
+  end
+
+  def choose(weapon)
+    @weapon = weapon
+  end
+
+  def weapon
+    @weapon
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,16 @@ require 'player'
 
 describe Player do
 
-  subject(:Dwayne) {Player.new("Dwayne 'The Marketeer' Johnson")}
+  subject(:dwayne) {Player.new("Dwayne 'The Marketeer' Johnson")}
+  let(:weapon) {double(:weapon)}
 
   it "has a name" do
     expect(subject.name).to eq("Dwayne 'The Marketeer' Johnson")
+  end
+
+  it "chooses a weapon" do
+    dwayne.choose(weapon)
+    expect(dwayne.weapon).to eq(weapon)
   end
 
 end
