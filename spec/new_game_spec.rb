71ef3e96a39ae63351323262e2f257dae21469fd
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-05-01 22:53:53 +0100

    adds opponent_choice? test

--- spec/new_game_spec.rb
+++ spec/new_game_spec.rb
@@ -1,5 +1,9 @@
 require 'new_game'
 
 describe NewGame do
-
+  describe '#opponent_choice?' do
+    it 'returns rock, paper or scissors' do
+      expect(['rock', 'paper', 'scissors']).to include(subject.opponent_choice?)
+    end
+  end
 end
