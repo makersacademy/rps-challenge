7ebc4d1a392ef8155d732929aa4e912e1005c388
SimonTanner <simonthetanner@hotmail.com>
2017-05-01 12:23:01 +0100

    If only 1 person playing player 2 defaults to Machine so user plays the computer

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,14 @@
+require './lib/player'
+
 class Game
   attr_reader :players
 
-  def initialize(player_1, player_2)
-    @players = [player_1, player_2]
+  def initialize(*players)
+    if players.length < 2
+      @players = [players[0], Player.new('Machine')]
+    else
+      @players = players
+    end
   end
 
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,5 +9,10 @@ describe Game do
     expect(game.players).to include(player_1).and include(player_2)
   end
 
+  it 'If 1 arg is supplied upon instantiation the player 2 default is \'Machine\'' do
+    game = Game.new(player_1)
+    expect(game.players[1].name ).to include('Machine')
+  end
+
 
 end
