a2b95373079566ccf61a23b215ed31211937225d
milesillsley <milesillsley@hotmail.co.uk>
2017-05-01 15:40:21 +0100

    redirect '/names', add weapons controller and view

--- app.rb
+++ app.rb
@@ -2,13 +2,21 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Application
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    params[:player_1_name]
+    session[:player_1_name] = params[:player_1_name]
+    redirect to('/weapons')
+  end
+
+  get '/weapons' do
+    erb :weapons
   end
 
 run! if app_file == $0
+
 end
