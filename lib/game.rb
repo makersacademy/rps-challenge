33dfc53887f9822bf01c489583d1a4dbce15ab90
milesillsley <milesillsley@hotmail.co.uk>
2017-05-01 18:21:14 +0100

    Implemented basic rules

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,17 @@
 class Game
 
+  @@weapons = ["Rock","Paper","Scissors"]
+
   def random_weapon
-    ["Rock","Paper","Scissors"].sample
+    @@weapons.sample
+  end
+
+  def check(weapon_1,weapon_2)
+    return "DRAW!" if weapon_1 == weapon_2
+    until @@weapons[@@weapons.size/2] == weapon_1 do
+      @@weapons.unshift(@@weapons.pop)
+    end
+    @@weapons.index(weapon_1) > @@weapons.index(weapon_2) ? weapon_1 : weapon_2
   end
 
 end
