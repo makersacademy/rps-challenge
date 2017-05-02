aa5bfaa5fedfdbb3a0dc865e4d8e4fbcfcb7abbf
JayWebDevCom <jaiye.s@gmail.com>
2017-05-02 13:49:52 +0100

    restructure result determiner methodology

--- lib/result_determiner.rb
+++ lib/result_determiner.rb
@@ -14,13 +14,24 @@ class ResultDeterminer
     :spock => :lizard, :rock => :rock }
   }
 
+
   def self.calculate(player_1, player_2)
     return :draw if player_1.choice == player_2.choice
+    self.winning_name(player_1, player_2)
+  end
+
+  private
+
+  def self.winning_hand(player_1, player_2)
+    RULES[player_1.choice][player_2.choice]
+  end
+
+  def self.winning_name(player_1, player_2)
     hash = {
       player_1.choice => player_1.name,
       player_2.choice => player_2.name,
      }
-    hash[RULES[player_1.choice][player_2.choice]]
+     hash[self.winning_hand(player_1, player_2)]
   end
 
 end
