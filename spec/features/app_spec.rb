305b0650de4f99b390d3884894c8657fc6c43a5a
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-05-02 10:40:32 +0100

    adds final testing functionality

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -10,5 +10,17 @@ feature RPS do
   end
 
   it 'allows the player to enter their name' do
+    visit '/'
+    fill_in 'player_name', with: 'Tim'
+    click_button 'Submit'
+    expect(page).to have_content 'Please choose Rock Paper or Scissors'
+  end
+
+  it 'allows the player to enter their name' do
+    visit '/'
+    fill_in 'player_name', with: 'Tim'
+    click_button 'Submit'
+    click_button 'Rock'
+    expect(page).to have_content 'You chose rock, Tim!'
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ require_relative '../app'
 ENV['RACK_ENV'] = 'test'
 Capybara.app = RPS
 
-
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
