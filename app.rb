a9a702ffc19033729e4bad87e16632274721a6d8
Sami <sami@Samis-MacBook-Pro.local>
2017-05-02 17:59:45 +0100

    Buttons Added

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
+  post '/selector' do
+    session['player'] = params['player']
+    erb(:selector)
+  end
 
 end
---
+++ lib/game.rb
@@ -0,0 +1,10 @@
+class Game
+
+attr_reader :player, :computer
+
+def initialize
+  @player = player
+  @computer = computer
+end
+
+def
