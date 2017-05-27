37429c36e20b56f711b58a4abc07e2d1b95c61ae
RSijelmass <R.Sijelmass@gmail.com>
2017-05-27 20:07:40 +0100

    Let #play_hand randomly choose hand for computer

--- lib/computer.rb
+++ lib/computer.rb
@@ -3,9 +3,11 @@ class Computer
 
   def initialize
     @hand = false
+    @hands = ['Rock', 'Paper', 'Scissors']
   end
 
   def play_hand
+    @hand = @hands.sample 
   end
 
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,10 +2,17 @@ require 'computer'
 
 describe Computer do
   subject(:computer) { described_class.new }
-
+  let(:possible_hands) { ['Rock','Paper','Scissors'] }
+ 
+  
   describe '#play_hand' do
     it 'starts of with an empty hand' do
       expect(computer.hand).to be false
+    end
+    
+    it 'plays a random hand(r/p/s)' do
+      computer.play_hand
+      expect(possible_hands).to include(computer.hand)
     end
   end
 end
