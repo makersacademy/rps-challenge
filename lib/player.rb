a1f16aaa2043712cc8a56cd48d0f8d83109c2e1c
Kavita <kkavita92@gmail.com>
2017-05-27 20:40:01 +0100

    Raise error if player chooses invalid weapon

--- lib/player.rb
+++ lib/player.rb
@@ -6,7 +6,14 @@ class Player
   end
 
   def weapon=(weapon)
+    validate_weapon(weapon)
     @weapon = weapon
+  end
+
+  private
+
+  def validate_weapon(weapon)
+    raise 'Not valid weapon' unless Game::WEAPONS.include?(weapon)
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,9 +7,19 @@ describe Player do
     expect(player.name).to eq 'Kavita'
   end
 
-  it 'allow player should choose a weapon' do
-    player.weapon=(:rock)
-    expect(player.weapon).to eq :rock
+  describe 'choose weapon' do
+
+    it 'allow player should choose a weapon' do
+      player.weapon=(:rock)
+      expect(player.weapon).to eq :rock
+    end
+
+    it 'raises error if player chooses invalid weapon' do
+      expect { player.weapon=(:potato) }.to raise_error('Not valid weapon')
+    end
+
   end
-  
+
+
+
 end
