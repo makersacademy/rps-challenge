ee398d9022085589721a48e64ed8f60a0ebee828
Kavita <kkavita92@gmail.com>
2017-05-27 17:55:34 +0100

    Set up Capybara

---
+++ app/rps_web.rb
@@ -0,0 +1,4 @@
+require 'sinatra/base'
+
+class RPS < Sinatra::Base
+end 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,10 +9,15 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 SimpleCov.start
 
+ENV['RACK_ENV'] = 'test'
+
+Capybara.app = RPS 
+
 RSpec.configure do |config|
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+
 end
