537d77b5c96ce7c46d90c4748556dc9782666930
Kavita <kkavita92@gmail.com>
2017-05-28 00:11:42 +0100

    DRY up result_spec.rb

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,41 +1,35 @@
 feature 'outcome of game is displayed correctly' do
   scenario 'player is taken to results page after choosing weapon' do
-    sign_in
-    click_link('Rock')
+    sign_in_play_rock
     expect(page).to have_content 'You chose'
   end
 
   scenario 'player can see their choice on results page' do
-    sign_in
-    click_link('Rock')
+    sign_in_play_rock
     expect(page).to have_content 'You chose Rock'
   end
 
   scenario 'player can see choice of computer on results page' do
     allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
-    sign_in
-    click_link('Rock')
+    sign_in_play_rock
     expect(page).to have_content 'The computer chose Scissors'
   end
 
-  scenario 'player wins when player puts Rock and computer puts scissors' do
+  scenario 'player wins when player puts Rock and computer puts Scissors' do
     allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
-    sign_in
-    click_link('Rock')
+    sign_in_play_rock
     expect(page).to have_content 'You won.'
   end
 
-  scenario 'player loses when player puts Rock and computer puts paper' do
+  scenario 'player loses when player puts Rock and computer puts Paper' do
     allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
-    sign_in
-    click_link('Rock')
+    sign_in_play_rock
     expect(page).to have_content 'You lose.'
   end
 
-  scenario 'player draws when player puts Rock and computer puts rock' do
+  scenario 'player draws when player puts Rock and computer puts Rock' do
     allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
-    sign_in
-    click_link('Rock')
+    sign_in_play_rock
     expect(page).to have_content 'You draw.'
   end
 
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -3,3 +3,10 @@ def sign_in
   fill_in('player1_name', with: 'Kavita')
   click_button('Submit')
 end
+
+def sign_in_play_rock
+  visit ('/')
+  fill_in('player1_name', with: 'Kavita')
+  click_button('Submit')
+  click_link('Rock')
+end
