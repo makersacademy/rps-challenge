fa97f2c0eb74fbf8917226a1daacc1b4cf296e7a
Kavita <kkavita92@gmail.com>
2017-05-27 18:16:55 +0100

    Allow player to view his/her name upon starting game

--- app/rps_web.rb
+++ app/rps_web.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
-  #enable :sessions 
+  #enable :sessions
 
   get '/' do
     erb(:index)
+  end
+
+  post '/play' do
+    @player_name = params[:player1_name]
+    erb(:play)
   end
 
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -4,4 +4,11 @@ feature 'test infrastructure' do
     fill_in('player1_name', with: 'Kavita')
     click_button('Submit')
   end
+
+  scenario 'player can view name' do
+    visit ('/')
+    fill_in('player1_name', with: 'Kavita')
+    click_button('Submit')
+    expect(page).to have_content 'Welcome, Kavita'
+  end
 end
