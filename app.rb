34ed31c6c1e0e2383fe625d4f823bc49fa248d83
Christopher Harrop <bannastre@gmail.com>
2017-05-27 21:20:41 +0100

    Player can register their name - Add /index and /play views - For created to submit player_name

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
 
   get '/' do
-    'Hello, world!'
+    erb :index
+  end
+
+  post '/register' do
+    @player = params[:player_name]
+    erb :play
   end
 
   run! if app_file == $0
