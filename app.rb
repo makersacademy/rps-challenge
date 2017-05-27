7c718a2eea3538c1d4e1c2e5565de3c72db2ab53
RSijelmass <R.Sijelmass@gmail.com>
2017-05-27 20:22:53 +0100

    /play shows randomly drawn hand from computer when player draws

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/player'
+require './lib/computer'
 
 class Rps < Sinatra::Base
   enable :sessions
@@ -10,29 +11,34 @@ class Rps < Sinatra::Base
 
   post '/login' do
     session[:player] = Player.new(params[:player])
+    session[:computer] = Computer.new
     redirect '/play'
   end
 
   get '/play' do
     @player = session[:player]
+    @computer = session[:computer]
     erb :play
   end
 
   post '/rock' do
     @player = session[:player]
     @player.draw_rock
+    session[:computer].play_hand
     redirect '/play'
   end
 
   post '/scissors' do
     @player = session[:player]
     @player.draw_scissors
+    session[:computer].play_hand
     redirect '/play'
   end
 
   post '/paper' do
     @player = session[:player]
     @player.draw_paper
+    session[:computer].play_hand
     redirect '/play'
   end
 
