930317d56f25746aa9464d8015e29751bf999e17
jade <jadealvares@hotmail.co.uk>
2017-05-27 12:31:21 +0100

    able to select obeject you want to be

--- app.rb
+++ app.rb
@@ -14,12 +14,18 @@ class RPS < Sinatra::Base
 
   get '/play' do
     @player = session[:player]
+    @object = session[:object]
     erb(:play)
   end
 
   post '/name' do
     #@player = params[:player]
     session[:player] = params[:player]
+    redirect '/play'
+  end
+
+  post '/play' do
+    session[:object] = params[:object]
     redirect '/play'
   end
 
--- spec/features/Choices_presented_spec.rb
+++ spec/features/Choices_presented_spec.rb
@@ -3,10 +3,4 @@ feature 'View choices' do
     sign_in_and_play
     expect(page).to have_content 'Rock / Paper / Scissors'
   end
-
-  scenario 'should be able to select a choice' do
-    sign_in_and_play
-    click_button 'Rock'
-    expect(page).to have_content 'You are Rock'
-  end
 end
