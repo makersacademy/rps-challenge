11908126ee6c3700209d602af4e09b60d047f35a
Kavita <kkavita92@gmail.com>
2017-05-27 20:46:22 +0100

    Improve test coverage for Computer class

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -4,8 +4,7 @@ describe Computer do
   subject(:computer) { described_class.new }
 
   it 'allow Computer to choose a weapon' do
-    allow(computer).to receive(:weapon) { :scissors }
-    expect(computer.weapon).to eq :scissors
+    expect(computer.weapon).to satisfy { |w| w == :scissors || w == :rock || w == :paper }
   end
   
 end
