50e51d921de6230aec9f28fae6c26f25f77b521a
Simon Ashbery <si.ashbery@gmail.com>
2017-05-28 17:28:48 +0100

    Added the ability for players to enter their name and have it displayed back to them

--- app.rb
+++ app.rb
@@ -1,17 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/player_name' do
-    player_name = params[:player_name]
+    session[:player_name] = params[:player_name]
     redirect '/play_rps'
   end
 
   get '/play_rps' do
+    @player_name = session[:player_name]
     erb :play_rps
   end
 
--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -7,4 +7,10 @@ feature "Player can submit their name", :type => :feature do
     click_button 'Submit'
     expect(page).to have_content 'Player: Digby!'
   end
+  scenario "The player can submit a different name to Digby" do
+    visit '/'
+    fill_in('player_name', with: "Ceasar")
+    click_button 'Submit'
+    expect(page).to have_content 'Player: Ceasar!'
+  end
 end
