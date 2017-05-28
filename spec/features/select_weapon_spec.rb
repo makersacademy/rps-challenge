303e5b45f084c32f06b9c664cc32bea6464792e0
wemmm <jenny.wem@gmail.com>
2017-05-28 16:35:08 +0100

    Refactored test

--- spec/features/select_weapon_spec.rb
+++ spec/features/select_weapon_spec.rb
@@ -3,5 +3,7 @@ feature 'Select a weapon' do
   scenario 'player can view available weapons' do
     sign_in_and_play
     expect(page).to have_button "Rock"
+    expect(page).to have_button "Paper"
+    expect(page).to have_button "Scissors"
   end
 end
