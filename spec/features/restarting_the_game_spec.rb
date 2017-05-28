8ed65be68be10e4f1aee17c0aa284ba6d3b3eef0
Kynosaur <kye.bracey@gmail.com>
2017-05-28 13:22:11 +0100

    Add feature test for Nobody winning in second round (I know, doing everything in reverse, whoopsie)

--- spec/features/restarting_the_game_spec.rb
+++ spec/features/restarting_the_game_spec.rb
@@ -6,4 +6,14 @@ feature 'The game restarts after playing a round' do
     click_button('OK')
     expect(page).to have_content('Kye: choose your weapon!')
   end
+
+  scenario 'The winner is declared as "Nobody" in the second round' do
+    sign_in_and_play
+    choose('Rock')
+    click_button('Go!')
+    click_button('OK')
+    choose('Scissors')
+    click_button('Go!')
+    expect(page).to have_content('Nobody wins!')
+  end
 end
