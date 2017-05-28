0512d1b25792c5f3f137d4a7b5e6dca2cbb8b967
Panteha <panteha.ahmadi@gmail.com>
2017-05-28 18:40:30 +0100

    Redirected to play page

--- app.rb
+++ app.rb
@@ -2,17 +2,21 @@ require 'sinatra'
 
 class RPS < Sinatra::Base
 
-  enable :session
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/name' do
-    session[:player_name] = params[:player_name]
-    session[:player_name]
+    session[:name]= params[:player_name]
+    redirect '/play'
   end
 
+  get '/play' do
+      @name = session[:name]
+    erb :play
+  end
 
 run! if $0 == __FILE__
 end
