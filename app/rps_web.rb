9849936cd74abd18cd5de8608e48bfe4830d4610
Kavita <kkavita92@gmail.com>
2017-05-28 09:34:03 +0100

    Dry out rps_web.rb

--- app/rps_web.rb
+++ app/rps_web.rb
@@ -7,6 +7,10 @@ class RPS < Sinatra::Base
   enable :sessions
   set :static, true
 
+  before do
+    @player1 = Player.instance
+  end
+
   get '/' do
     erb(:index)
   end
@@ -17,16 +21,14 @@ class RPS < Sinatra::Base
   end
 
   get '/play' do
-    @player1 = Player.instance
     erb(:play)
   end
 
   get '/result' do
-    player1 = Player.instance
-    player1.weapon=(params[:weapon])
+    @player1.weapon=(params[:weapon])
     computer = Computer.create
     computer.choose_weapon
-    @game = Game.create(player1, computer)
+    @game = Game.create(@player1, computer)
     @outcome = @game.battle
     erb(:result)
   end
