98047a74068b8004f18000db0e50c826e93b4c3c
Lubos Michalic <lnmichalic@gmail.com>
2017-05-28 20:06:07 +0100

    Write code to fulfill Choice tests

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -14,17 +14,17 @@ class RockPaperScissors < Sinatra::Base
 
   post '/name' do
     session[:player_1_name] = params[:name]
-    # p params[:name]
-    # p session[:player_1_name]
-    # # p 'Get Visibility'
-    # p player_1_name
     redirect '/play'
   end
 
   get '/play' do
-    # puts session[:player_1_name]
     @player = session[:player_1_name]
     erb :play
+  end
+
+  post '/turn' do
+    @choice = params[:choice]
+    erb :turn
   end
 
 end
