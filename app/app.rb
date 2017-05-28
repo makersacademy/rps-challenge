02a9b9cdfcd3f17f1b6a002219664c94ae36648d
wemmm <jenny.wem@gmail.com>
2017-05-28 15:02:25 +0100

    Player name is now stored as instance variable and displayed

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,21 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
 
+  attr_reader :player_name
+
   enable :sessions
 
   get '/' do
     erb :index
+  end
+
+  post '/names' do
+    @player_name = params[:player_name]
+    erb :play
+  end
+
+  get '/play' do
+    erb :play
   end
 
   run! if app_file == $0
