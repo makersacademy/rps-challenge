f383f93992cac232852359e3d84b2875974e5b3f
Kavita <kkavita92@gmail.com>
2017-05-28 18:04:12 +0100

    Integrate multiplayer functionality within web_helpers.rb

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,5 +1,6 @@
 def sign_in
   visit ('/')
+  choose('mode_1')
   click_button('Submit')
   fill_in('player1_name', with: 'Kavita')
   click_button('Submit')
@@ -7,6 +8,7 @@ end
 
 def sign_in_play_rock
   visit ('/')
+  choose('mode_1')
   click_button('Submit')
   fill_in('player1_name', with: 'Kavita')
   click_button('Submit')
@@ -15,6 +17,7 @@ end
 
 def sign_in_play_scissors
   visit ('/')
+  choose('mode_1')
   click_button('Submit')
   fill_in('player1_name', with: 'Kavita')
   click_button('Submit')
