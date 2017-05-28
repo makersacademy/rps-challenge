6dbe64da11d2ff6ec7645731a9bcbe9d58067f42
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-28 12:42:59 +0100

    Updates game spec & game to show computers weapon & player & computers combined weapons

--- lib/game.rb
+++ lib/game.rb
@@ -9,12 +9,31 @@ class Game
     @player_weapon = player_weapon
   end
 
-# TODO refactor to be a guard clause not a conditional
+  # TODO refactor to be a guard clause not a conditional
   def start(player_weapon)
     if @weapons.has_key?(player_weapon)
       @player_weapon << @weapons.fetch(player_weapon)
     end
-    @player_weapon
+  end
+
+  WIN = [
+    [:rock, :scissors],
+    [:paper, :rock],
+    [:scissors, :paper]
+  ]
+
+  DRAW = [
+    [:rock, :rock],
+    [:paper, :paper],
+    [:scissors, :scissors]
+  ]
+
+  def computer_weapons
+    @computer.weapon_choice
+  end
+
+  def game_weapons
+    @player_weapon << computer_weapons()
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -30,4 +30,23 @@ describe Game do
       expect(game.player_weapon).to eq []
     end
   end
+
+  describe '#computer_weapons' do
+    it 'stores the computer weapons' do
+      # TODO improve this double test to be a spy or work at least 
+      rock_double = double(:computer)
+      allow(rock_double).to receive(:computer_weapons).and_return(:rock)
+      expect(rock_double.computer_weapons).to eq :rock
+    end
+  end
+  describe '#game_weapons' do
+    it 'stores the player and computer weapons in an array' do
+      # TODO must be a spy that works correctly
+      game.start("Rock")
+      rock_double = double(:computer)
+      allow(rock_double).to receive(:computer_weapons).and_return(:rock)
+      rock_double.computer_weapons
+      expect(game.game_weapons).to eq [:rock, :rock]
+    end
+  end
 end
