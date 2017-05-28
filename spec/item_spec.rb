b9e6a3d1a29c679358517e89a9b4260755ded885
Panteha <panteha.ahmadi@gmail.com>
2017-05-28 22:42:53 +0100

    tested Rock beats Scissors

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,8 +1,10 @@
 require 'item'
 
 describe Item do
-  let(:rock){Item.new}
-  let(:scissors){Item.new}
+
+  let(:rock){Item.new('Rock', 0)}
+  let(:scissors){Item.new('Scissors', 1)}
+  
   it 'Rock beats Scissors' do
     expect(rock.beats(scissors)).to eq true
   end
