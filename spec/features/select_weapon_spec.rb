644c64b43b7843851ce71f6ab5d99fafeae3add6
wemmm <jenny.wem@gmail.com>
2017-05-28 16:38:46 +0100

    User can now select a weapon

--- spec/features/select_weapon_spec.rb
+++ spec/features/select_weapon_spec.rb
@@ -2,8 +2,8 @@
 feature 'Select a weapon' do
   scenario 'player can view available weapons' do
     sign_in_and_play
-    expect(page).to have_button "Rock"
-    expect(page).to have_button "Paper"
-    expect(page).to have_button "Scissors"
+    expect(page).to have_content "Rock"
+    expect(page).to have_content "Paper"
+    expect(page).to have_content "Scissors"
   end
 end
