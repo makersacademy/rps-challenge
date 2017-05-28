b40652cb3d43a7f88d56676f4e63f18a32d027a8
Christopher Harrop <bannastre@gmail.com>
2017-05-28 11:32:32 +0100

    Refactor #choose_hand

--- lib/opponent.rb
+++ lib/opponent.rb
@@ -1,23 +1,15 @@
 class Opponent
 
   def initialize
-    @choice = 'Rock'
+    @weapons = ['Rock', 'Paper', 'Scissors']
   end
 
   def choose_hand
-    case random_pick
-      when 1
-        @choice = 'Rock'
-      when 2
-        @choice = 'Paper'
-      when 3
-        @choice = 'Scissors'
-      end
-    @choice
+    @weapons[random_pick - 1]
   end
 
   private
-  
+
   def random_pick
     1 + rand(3)
   end
