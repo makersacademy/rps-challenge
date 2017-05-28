8469177b6bb92c2d9fc339ae9ac9f97510d890a5
Panteha <panteha.ahmadi@gmail.com>
2017-05-28 22:45:15 +0100

    Tested scissors beats paper

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,9 +4,14 @@ describe Item do
 
   let(:rock){Item.new('Rock', 0)}
   let(:scissors){Item.new('Scissors', 1)}
-  
+  let(:paper){Item.new('Paper', 2)}
+
   it 'Rock beats Scissors' do
     expect(rock.beats(scissors)).to eq true
+  end
+
+  it 'Scissors beats Paper' do
+    expect(scissors.beats(paper)).to eq true
   end
 
 end
