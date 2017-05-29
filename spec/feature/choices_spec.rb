5f9f8d77a801ad57dbdb9c84e334b1372552c25f
Panteha <panteha.ahmadi@gmail.com>
2017-05-29 17:55:35 +0100

    Added the beat test for game class

--- spec/feature/choices_spec.rb
+++ spec/feature/choices_spec.rb
@@ -2,21 +2,21 @@ feature 'Player has different choices' do
 
   scenario 'Player can choose Rock' do
     visit('/')
-    fill_in :player_name, with: 'Tim'
+    fill_in :player_1_name, with: 'Tim'
     click_button :Enter
     expect(page).to have_button('Rock')
   end
 
   scenario 'Player can choose Paper' do
     visit('/')
-    fill_in :player_name, with: 'Tim'
+    fill_in :player_1_name, with: 'Tim'
     click_button :Enter
     expect(page).to have_button('Paper')
   end
 
   scenario 'Player can choose Scissors' do
     visit('/')
-    fill_in :player_name, with: 'Tim'
+    fill_in :player_1_name, with: 'Tim'
     click_button :Enter
     expect(page).to have_button('Scissors')
   end
---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+require 'game.rb'
+
+describe Game do
+
+  let(:item_1) { double :item }
+  let(:item_2) { double :item }
+  let(:player_2) { double :player, item: item_2 , choose: 'Rock' }
+  let(:player_1) { double :player, item: item_1, choose: 'Paper' }
+  let(:subject) { Game.new(player_1, player_2) }
+
+  it 'returns win if player_1 beats player_2' do
+    allow(item_1).to receive(:beats).and_return(true)
+    expect(subject.play(item_1)).to eq Game::WIN
+  end
+
+end
