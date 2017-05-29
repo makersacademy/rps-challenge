eb8c2b73616373cc87ad7916f16beb75a7978e29
Spencer <spencer.bartonfisher@me.com>
2017-05-29 12:51:52 +0100

    functionality added to computer class and rspec tests passed

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,4 +1,13 @@
 
 class Computer
 
+  def computer_name
+    @computer_name = "Mr Computer"
+  end
+
+  def computer_move
+    @computer_move = ["rock", "paper", "scissors"]
+   @computer_move.sample
+  end
+
 end
