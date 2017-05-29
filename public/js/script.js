885e0efba2c1b25ebcd2f3c84c9795a9d52e93fc
Michael Jacobson <michael.jacobson89@gmail.com>
2017-05-29 21:21:33 +0100

    • begin implementing theme options

--- public/js/script.js
+++ public/js/script.js
@@ -1,28 +1,55 @@
 
 Mousetrap.bind('r', function(){
     $("#rules_popup").css("visibility", "visible");
-    $("#rules_popup, #rules_contents").click(function() {
+    $(".close_button").click(function() {
         $("#rules_popup").css("visibility", "hidden");
     })
 });
 
 Mousetrap.bind('s', function(){
     $("#scores_popup").css("visibility", "visible");
-    $("#scores_popup, #scores_contents").click(function() {
+    $(".close_button").click(function() {
         $("#scores_popup").css("visibility", "hidden");
     })
 });
 
 Mousetrap.bind('t', function(){
     $("#themes_popup").css("visibility", "visible");
-    $("#themes_popup, #themes_contents").click(function() {
+    $(".close_button").click(function() {
         $("#themes_popup").css("visibility", "hidden");
     })
 });
 
 Mousetrap.bind('q u a c k', function(){
     $("#easter_egg_popup").css("visibility", "visible");
-    $("#easter_egg_popup, #easter_egg_contents").click(function() {
+    $(".close_button").click(function() {
         $("#easter_egg_popup").css("visibility", "hidden");
     })
+});
+
+$(document).ready(function(){
+    $("#grey").click(function(){
+        $(".popup_contents").css({ backgroundColor: "#303E4E" });
+        $("html").css({ backgroundColor: "#303E4E" });
+    });
+    $("#lilac").click(function(){
+        $(".popup_contents").css({ backgroundColor: "#776D94" });
+        $("html").css({ backgroundColor: "#776D94" });
+    });
+    $("#yellow").click(function(){
+        $(".popup_contents").css({ backgroundColor: "#FEBF13" });
+        $("html").css({ backgroundColor: "#FEBF13" });
+    });
+    $("#blue").click(function(){
+        $(".popup_contents").css({ backgroundColor: "#4285F4" });
+        $("html").css({ backgroundColor: "#4285F4" });
+    });
+    $("#green").click(function(){
+        $(".popup_contents").css({ backgroundColor: "#34A853" });
+        $("html").css({ backgroundColor: "#34A853" });
+    });
+    $("#red").click(function(){
+        $(".popup_contents").css({ backgroundColor: "#D34644" });
+        $("html").css({ backgroundColor: "#D34644" });
+    });
 });
