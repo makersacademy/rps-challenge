94369a3a5029f55fde149562dc96e1f4ed0f213b
wemmm <jenny.wem@gmail.com>
2017-05-29 20:32:50 +0100

    Passed failing test

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,12 @@
 class Player
 
-  attr_reader :name, :wins
+  attr_reader :name, :wins, :losses, :draws
 
   def initialize(player_name)
     @name = player_name
     @wins = 0
+    @losses = 0
+    @draws = 0
   end
 
 end
