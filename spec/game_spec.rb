71a317376059d04eae0019ce3cae5a00deb2824b
Colin Scally <colin.scally@gmail.com>
2017-05-29 09:32:44 +0100

    re-added testing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,10 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+
+  let(:player) { double(:player, name: 'Bob', score: 0) }
+  let(:opponent) { double(:player, name: 'Computer', score: 0) }
+  let(:game) { Game.new(player, opponent) }
 
   describe '#rps' do
     it 'chooses one of rock, paper or scissors for the opponent' do
@@ -11,7 +14,17 @@ describe Game do
 
   describe '#compete' do
     it 'compares the two choices to determine who won' do
+      allow(player).to receive(:score=)
+      allow(opponent).to receive(:score=)
       expect(game.compete('Rock', 'Paper')).to include("You lose!")
+      expect(game.compete('Rock', 'Rock')).to include("A draw!")
+      expect(game.compete('Paper', 'Paper')).to include("A draw!")
+      expect(game.compete('Paper', 'Rock')).to include("You win!")
+      expect(game.compete('Rock', 'Scissors')).to include("You win!")
+      expect(game.compete('Scissors', 'Scissors')).to include("A draw!")
+      expect(game.compete('Scissors', 'Paper')).to include("You win!")
+      expect(game.compete('Scissors', 'Rock')).to include("You lose!")
+      expect(game.compete('Paper', 'Scissors')).to include("You lose!")
     end
   end
 
