77a7116439fdfb3db493a8dea250b1695399cbc2
Guillermo <mrog0@outlook.com>
2017-05-29 08:16:12 +0100

    Fix typo and Add a web_helper.rb with #sign_in_and_play

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+require_relative './features/web_helpers.rb'
+
 Capybara.app = RPSWeb
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
