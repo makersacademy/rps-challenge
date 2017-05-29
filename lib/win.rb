f272eb2d7d68c259620963c989fc6de926a4bc57
RSijelmass <R.Sijelmass@gmail.com>
2017-05-29 15:16:20 +0100

    Create new method #declare_method in Win class - delegate from #find_winner

--- lib/win.rb
+++ lib/win.rb
@@ -3,19 +3,17 @@ class Win
 
   def find_winner(current, opponent)
     return nil unless hands_drawn?(current, opponent)
-    if current.hand == opponent.hand 
-        @winner = "Tie!"       
-      elsif current.wins_from.include? opponent.hand 
-         current.wins
-         @winner = "#{current.name} wins!" 
-      else
-         opponent.wins
-        @winner = "#{opponent.name} wins!" 
-      end 
-    end
+    return @winner = "Tie!" if current.hand == opponent.hand 
+    current.wins_from.include? opponent.hand ? declare_winner(current) : declare_winner(opponent) 
+  end
 
   private
   def hands_drawn?(current, opponent)
     current.hand && opponent.hand
+  end
+
+  def declare_winner(player)
+    player.wins
+    @winner = "#{player.name} wins!" 
   end
 end
