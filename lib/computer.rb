24c594270d43fbc4c1de7fe85f1b23c23c61ee0b
Kavita <kkavita92@gmail.com>
2017-05-29 18:22:29 +0100

    Fix error in saving game on 1 player mode

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,10 +2,14 @@ require_relative 'game'
 
 class Computer
 
-  attr_reader :weapon
+  attr_reader :weapon, :name
+
+  def initialize
+    @name = :Computer
+  end
 
   def choose_weapon
     @weapon = Game::WEAPONS.sample
   end
-  
+
 end
