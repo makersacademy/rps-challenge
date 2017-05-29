22100820a59a8ad4260749383ce75987b1c29819
Prabu Sathan <prabu.sathan@gmail.com>
2017-05-29 21:25:58 +0100

    Weapon choice is returned to user, redirected to a post play

--- app.rb
+++ app.rb
@@ -14,9 +14,15 @@ class RPS < Sinatra::Base
 
   get '/play' do
     @player_name = session[:player]
+    @player_weapon = session[:weapon]
     erb(:play)
   end
-  
+
+  post '/play' do
+    session[:weapon] = params[:weapon]
+    redirect '/play'
+  end
+
    run! if app_file == $0
 
 end
--- spec/features/in_game_spec.rb
+++ spec/features/in_game_spec.rb
@@ -1,10 +1,28 @@
 
-feature 'in game' do
+feature 'In-game' do
 
-  scenario 'user is presented with weapon choices' do
+  scenario 'User is presented with weapon choices' do
     log_in
     expect(page).to have_button 'ROCK'
     expect(page).to have_button 'PAPER'
     expect(page).to have_button 'SCISSORS'
+  end
+
+  scenario 'User is able to select Rock' do
+    log_in
+    click_button('ROCK')
+    expect(page).to have_content 'Weapon Choice: ROCK?'
+  end
+
+  scenario 'User is able to select Paper' do
+    log_in
+    click_button('PAPER')
+    expect(page).to have_content 'Weapon Choice: PAPER?'
+  end
+
+  scenario 'User is able to select Scissors' do
+    log_in
+    click_button('SCISSORS')
+    expect(page).to have_content 'Weapon Choice: SCISSORS?'
   end
 end
