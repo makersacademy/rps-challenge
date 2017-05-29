265bf80155d6d7c413a819502d5c4a96d7958f78
Mário Ribeiro <mail@marioribeiro.com>
2017-05-29 22:20:06 +0100

    Update look and feel and add form validation

--- spec/feature/app_spec.rb
+++ spec/feature/app_spec.rb
@@ -3,6 +3,6 @@ require './app'
 feature 'Application load' do
   scenario 'Root route is defined and display welcome message' do
     visit '/'
-    expect(page).to have_content "Let's play RPS!"
+    expect(page).to have_content "Let's play!"
   end
 end
