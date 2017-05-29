b0e6ca1bfad571a0ab72909e4855fd40c0ef06f8
Kavita <kkavita92@gmail.com>
2017-05-29 13:35:24 +0100

    Refactor feature tests to remove any_instance_of

--- spec/features/oneplayer_result_spec.rb
+++ spec/features/oneplayer_result_spec.rb
@@ -10,7 +10,7 @@ feature 'infrastructure of 1 player result page is set up properly' do
   end
 
   scenario 'player can see choice of computer on results page' do
-    allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
+    allow(Game::WEAPONS).to receive(:sample) { :scissors }
     one_player_sign_in_play_rock
     expect(page).to have_content 'The computer chose Scissors.'
   end
@@ -18,25 +18,25 @@ end
 
 feature 'outcome of 1 player game is displayed correctly' do
   scenario 'player wins when player puts Rock and computer puts Scissors' do
-    allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
+    allow(Game::WEAPONS).to receive(:sample) { :scissors }
     one_player_sign_in_play_rock
     expect(page).to have_content 'You won.'
   end
 
   scenario 'player loses when player puts Rock and computer puts Paper' do
-    allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
+    allow(Game::WEAPONS).to receive(:sample) { :paper }
     one_player_sign_in_play_rock
     expect(page).to have_content 'You lose.'
   end
 
   scenario 'player draws when player puts Rock and computer puts Rock' do
-    allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+    allow(Game::WEAPONS).to receive(:sample) { :rock }
     one_player_sign_in_play_rock
     expect(page).to have_content 'You draw.'
   end
 
   scenario 'player draws when player puts Scissors and computer puts Scissors' do
-    allow_any_instance_of(Array).to receive(:sample).and_return(:scissors)
+    allow(Game::WEAPONS).to receive(:sample) { :scissors }
     one_player_sign_in_play_scissors
     expect(page).to have_content 'You draw.'
   end
@@ -44,7 +44,7 @@ end
 
 feature 'player can restart 1 player game' do
   scenario 'player can click New Game button to restart' do
-    allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+    allow(Game::WEAPONS).to receive(:sample) { :scissors }
     one_player_sign_in_play_rock
     click_button('New Game')
   end
