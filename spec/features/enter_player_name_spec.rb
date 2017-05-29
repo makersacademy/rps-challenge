c7c52d138ddf31f32e7ac6ef177a7432f9469386
Andrew Clarke <andrewclarke982@gmail.com>
2017-05-29 13:41:19 +0100

    Refactor name submission page

--- spec/features/enter_player_name_spec.rb
+++ spec/features/enter_player_name_spec.rb
@@ -3,6 +3,6 @@ feature 'Enter player name' do
     visit('/')
     fill_in :player_1_name, with: 'Andrew'
     click_button 'Submit'
-    expect(page).to have_content 'Prepare for hand-to-hand combat Andrew'
+    expect(page).to have_content 'Prepare for hand-to-hand combat Andrew!'
   end
 end
