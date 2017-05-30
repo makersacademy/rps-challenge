8bbc986a26f6ec9e2358bd516098cd8369252dc8
Alex <alex.satur89@gmail.com>
2017-05-30 17:24:55 +0100

    Final commit

--- app.rb
+++ app.rb
@@ -1,27 +1,30 @@
 require 'sinatra/base'
-require_relative './lib/game'
-require_relative './lib/player'
+require_relative 'lib/opponent'
+require_relative 'lib/turn'
 
 class RPS < Sinatra::Base
-
+  
   enable :sessions
-
-  before do
-    @game = Game.instance
-  end
 
   get '/' do
     erb :index
   end
 
-  post '/names' do
-    @game = Game.create(Player.new(params[:name]))
+  post '/name' do
+    session[:player_name] = params[:name]
     redirect '/play'
-    end
-
+  end
 
   get '/play' do
+    @turn = Turn.new(session)
     erb :play
+  end
+
+
+  post '/play' do
+    session[:player_shape] = params[:shape].downcase.to_sym
+    session[:opponent_shape] = Opponent.new.shape
+    redirect '/play'
   end
 
   # start the server if ruby file executed directly
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,14 @@
 feature "Testing user enter names" do
   scenario "Allows users to submit name and see them in lights" do
     sign_in_and_play
-    expect(page).to have_content "Rock + Paper + Scissors"
+    expect(page).to have_content "Owliver"
+  end
+end
+
+feature "Pick a weapon" do
+  scenario "Allows users to select a name" do
+    sign_in_and_play
+    click_button('Rock')
+    expect(page).to have_content "You have selected"
   end
 end
