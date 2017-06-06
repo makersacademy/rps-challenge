bad64abd967d5daabbded8bb7ac4076fb91eb1e5
alwinruby <alwinsolanky@gmail.com>
2017-06-06 20:59:41 +0100

    delete and start again

--- lib/turn.rb
+++
@@ -1,9 +0,0 @@
-class Turn
-  attr_reader :player_name, :player_choice, :opposition_choice
-
-  def initialize(options)
-    @player_name = options["player_name"]
-    @player_choice = options["player_choice"]
-    @opposition_choice = options["opposition_choice"]
-  end
-end
