464bdd97035cef005796477085db6261dfd3d0ed
jinimcoroneo <jinim1983@gmail.com>
2017-06-24 19:33:34 +0100

    computer can choose a hardcoded option

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
   enable :sessions
-  
+
   get '/' do
     erb :index
   end
@@ -14,9 +14,21 @@ class RPS < Sinatra::Base
 
   get '/welcome' do
     @name = session[:player_name]
+    session[:choice] = params[:choice]
     erb :welcome
   end
 
+  post '/play' do
+    session[:choice] = params[:choice]
+    session[:opponent_choice] = :paper
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_choice = session[:choice]
+    @opponent_choice = session[:opponent_choice] = :paper
+    erb :play
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -11,11 +11,31 @@ end
 
 # the marketeer will be presented the choices (rock, paper and scissors)
 
-feature 'choose rock, paper or scissors' do
-  scenario 'Can sign in and choose an option: rock, paper, or scissors' do
+feature 'have a choice of rock, paper or scissors' do
+  scenario 'Can sign in and see options for rock, paper, or scissors' do
     sign_in_and_play
     expect(page).to have_button 'rock'
     expect(page).to have_button 'paper'
     expect(page).to have_button 'scissors'
+  end
+end
+
+# the marketeer can choose one option
+
+feature 'choose an option' do
+  scenario 'Can sign in and choose an option: rock, paper, or scissors' do
+    sign_in_and_play
+    click_button('rock')
+    expect(page).to have_content 'You chose rock!'
+  end
+end
+
+# the game will choose a random option
+feature 'computer chooses an option' do
+  scenario 'Computer can choose an option: rock, paper, or scissors' do
+    sign_in_and_play
+    click_button('rock')
+    expect(page).to have_content 'You chose rock!'
+    expect(page).to have_content 'Your opponent chose paper!'
   end
 end
