acc132fc7d31e8729ab9f4d489806712f3316fec
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-24 15:27:51 +0100

    First commit, setup testing infrastructure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,13 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+ENV['RACK_ENV'] = 'test'
+require "rspec"
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = RPS
+
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
