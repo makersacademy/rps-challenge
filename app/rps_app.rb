4e5f5273719143e682a26db0b3c264730c0d8af1
MarySalemme <marysalemme1@gmail.com>
2017-06-24 18:55:13 +0100

    Refactoring

--- app/rps_app.rb
+++ app/rps_app.rb
@@ -1,5 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
+  get '/' do
+    erb(:index)
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb(:play)
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,6 @@ ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app', 'rps_app.rb')
 require File.join(File.dirname(__FILE__), 'feature_tests', 'web_helper.rb')
-
-
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
@@ -11,13 +9,6 @@ Capybara.app = RPS
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
-
-RSpec.configure do |config|
-  config.after(:suite) do
-
-  end
-end
