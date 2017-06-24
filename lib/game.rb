ff62bc368765923ebc8d1db7b6c2b35dbb6c49c1
Jasper King-Harman <j.kingharman@googlemail.com>
2017-06-24 14:04:53 +0100

    Implementing singleton pattern for game class - wiping spec for now because unsure how to test this pattern

--- lib/game.rb
+++ lib/game.rb
@@ -2,11 +2,31 @@
 
 class Game
 
-  attr_reader :player_one, :player_two
+  RULES = {rock: {rock: :draw, paper: :lose, scissors: :win},
+           paper: {rock: :win, paper: :draw, scissors: :lose},
+           scissors: {rock: :lose, paper: :win, scissors: :draw}}
 
-  def initialize(player_one, player_two)
-    @player_one = player_one
-    @player_two = player_two
+  attr_reader :player, :computer
+
+  def initialize(player, computer)
+    @player = player
+    @computer = computer
   end
+
+  def self.create(player, computer)
+    @game = Game.new(player, computer)
+  end
+
+  def self.instance
+    @game
+  end
+
+  def result
+    RULES[@player.weapon][@computer.weapon]
+  end
+
+  private
+
+  :new
 
 end
