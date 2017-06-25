d67ce18f12b8b51f8d12ee51ead88d47c7fb5764
Jasper King-Harman <j.kingharman@googlemail.com>
2017-06-25 11:24:28 +0100

    Adding specs for possible outcomes

--- spec/features/outcome_displayed_spec.rb
+++ spec/features/outcome_displayed_spec.rb
@@ -4,7 +4,30 @@
 
 feature "game outcome is displayed" do
 
-  scenario do
+  before(:each) do
+    sign_in_and_play
+    allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+  end
+
+  scenario "player picks rock" do
+    click_on("Pick your weapon")
+    find(:css, "#rock").set true
+    click_on("Submit")
+    expect(page).to have_content("Maker draw!")
+  end
+
+  scenario "player picks paper" do
+    click_on("Pick your weapon")
+    find(:css, "#paper").set true
+    click_on("Submit")
+    expect(page).to have_content("Maker win!")
+  end
+
+  scenario "player picks scissors" do
+    click_on("Pick your weapon")
+    find(:css, "#scissors").set true
+    click_on("Submit")
+    expect(page).to have_content("Maker lose!")
   end
 
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -4,3 +4,9 @@ def sign_in_and_play
   fill_in 'name', with: 'Maker'
   click_on("Submit")
 end
+
+def pick_rock
+  click_on("Pick your weapon")
+  find(:css, "#rock").set true
+  click_on("Submit")
+end
