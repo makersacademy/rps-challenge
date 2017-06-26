85e40401fe2f956e2c3da98d92d022d04044f90f
Verity <vhonebon@gmail.com>
2017-06-26 22:29:20 +0100

    Tweaked so all tests passing - 98% coverage

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,36 +15,36 @@ describe Game do
   describe "#calculate_result" do
     context "when both players throw the same move" do
       it "should return a draw" do
-        allow(player_1).to receive(:move).and_return("rock")
-        allow(player_2).to receive(:move).and_return("rock")
+        allow(game.player_1).to receive(:move).and_return("rock")
+        allow(game.player_2).to receive(:move).and_return("rock")
         expect(game.calculate_result(player_1, player_2)).to eq('draw')
       end
     end
     context "when the player has won" do
       it "should show a winning message" do
-        allow(player_1).to receive(:move).and_return("rock")
-        allow(player_2).to receive(:move).and_return("scissors")
+        allow(game.player_1).to receive(:move).and_return("rock")
+        allow(game.player_2).to receive(:move).and_return("scissors")
         expect(game.calculate_result(player_1, player_2)).to eq('win')
       end
     end
     context "when the player has won" do
       it "should show a winning message" do
-        allow(player_1).to receive(:move).and_return("paper")
-        allow(player_2).to receive(:move).and_return("rock")
+        allow(game.player_1).to receive(:move).and_return("paper")
+        allow(game.player_2).to receive(:move).and_return("rock")
         expect(game.calculate_result(player_1, player_2)).to eq('win')
       end
     end
     context "when the player has won" do
       it "should show a winning message" do
-        allow(player_1).to receive(:move).and_return("scissors")
-        allow(player_2).to receive(:move).and_return("paper")
+        allow(game.player_1).to receive(:move).and_return("scissors")
+        allow(game.player_2).to receive(:move).and_return("paper")
         expect(game.calculate_result(player_1, player_2)).to eq('win')
       end
     end
     context "when the player has lost" do
       it "should show a winning message" do
-        allow(player_1).to receive(:move).and_return("paper")
-        allow(player_2).to receive(:move).and_return("scissors")
+        allow(game.player_1).to receive(:move).and_return("paper")
+        allow(game.player_2).to receive(:move).and_return("scissors")
         expect(game.calculate_result(player_1, player_2)).to eq('lose')
       end
     end
