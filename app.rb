f57a30ebbcd9f2d864e39687b34ccff844f37ca3
Cong Cong Bo <cong@hippocampus.ai>
2017-07-22 18:40:40 +0100

    config,ru file added so rackup can be used

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/' do
-  erb :index
+class RPS < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post 'play' do
+    @name = params[:name]
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
