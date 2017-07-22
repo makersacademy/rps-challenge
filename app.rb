957f1fc3866d06af8863f803d54601f429ecf0cc
kitkat119 <gprydal@yahoo.co.uk>
2017-07-22 21:11:04 +0100

    can view the points of the player and the computer

--- app.rb
+++ app.rb
@@ -2,13 +2,19 @@ require 'sinatra/base'
 
 
 class Rps < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/name' do
-    @player_name = params[:player_name]
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
     erb :play
   end
 
