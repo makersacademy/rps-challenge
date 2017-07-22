7d9a5e87217a9d926f88d7e7363f1303a00c8f2a
Toby Hamand <tobyhamand@gmail.com>
2017-07-22 15:27:22 +0100

    passing first feature test

--- app.rb
+++ app.rb
@@ -3,4 +3,17 @@ require 'sinatra/base'
 class RPS < Sinatra::Base
 
   run! if app_file == $0
+
+  get '/' do
+    erb(:index)
+  end
+
+  post '/names' do
+    "#{params[:player_1_name]} vs. the computer"
+    # redirect'/play'
+  end
+
+  get '/play' do
+    erb :play
+  end
 end
