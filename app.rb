fe1f6c7b853bc2f3cf302a5af9743eff238637aa
paulmillen <pwmillen@gmail.com>
2017-07-22 11:31:39 +0100

    player can now enter name, redirected to battle.erb

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
-    erb(:home)
+    erb :home
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player_name]
+    redirect '/battle'
+  end
+
+  get '/battle' do
+    @player_name = session[:player_name]
+    erb :battle
   end
 
   run! if app_file == $0
--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,7 +1,14 @@
 feature 'player can register name' do
 
-  scenario 'before an online game player can enter their name' do
+  scenario 'player is prompted to enter name' do
     visit '/'
     expect(page).to have_content 'Welcome player - please enter your name'
+  end
+
+  scenario 'player can enter their name' do
+    visit '/'
+    fill_in :player_name, with: 'Dave'
+    click_button 'Submit'
+    expect(page).to have_content 'OK Dave - choose your weapon!'
   end
 end
