152eac2fb13f54e624db76aad4cafd8b15516ee9
Tambi-Mat <caicaratambi@gmail.com>
2017-07-22 20:45:13 +0100

    created sessions for name objects.

--- app/app.rb
+++ app/app.rb
@@ -4,12 +4,19 @@ class RpsWeb < Sinatra::Base
 
   run! if app_file == $0
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
     erb :play
   end
 
