6fa9e7b1a54623c01c3f9bf5a42b4068dbf91471
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-22 17:00:01 +0100

    Introduced game class

--- app.rb
+++ app.rb
@@ -1,19 +1,26 @@
 require 'sinatra/base'
+require_relative './lib/game'
 
 class RockPaperScissors < Sinatra::Base
+
+  before do
+    @game = Game.instance
+  end
 
   get '/' do
     erb(:index)
   end
 
   get '/play' do
-    @player_1_name = $player_1_name
-    @player_2_name = "Computer"
+    @player_1_name = @game.player_1
+    @player_2_name = @game.player_2
     erb(:play)
   end
 
   post '/enter_name' do
-    $player_1_name = params[:player_1_name]
+    player_1 = params[:player_1_name]
+    player_2 = "Computer"
+    Game.create(player_1, player_2)
     redirect '/play'
   end
 
