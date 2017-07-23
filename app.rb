e6071d00da1315f73ffae885a7ae0fd877e779fe
EOSullivanBerlin <esully666@gmail.com>
2017-07-23 17:44:21 +0200

    Fully functional RPS with both single and double player functionality it requires a clean up

--- app.rb
+++ app.rb
@@ -8,34 +8,73 @@ class RPS < Sinatra::Base
     erb :index
   end
 
-  post '/name' do
-    p params
+  get '/name_p1' do
+    erb :name_p1
+  end
+
+  get '/name_p2' do
+    erb :name_p2
+  end
+
+  post '/name_player_one' do
     $game = Game.new(params[:player_name])
     redirect '/selection'
   end
 
-  post '/selection' do
-    @player_name = $game.player_1_name
-    $game.selection(params[:selection])
-    @selection = $game.return_selection
-    redirect '/swap_players'
+
+  post '/name_two_players' do
+    $game = Game.new(params[:player_one_name], params[:player_two_name] )
+    redirect '/selection'
   end
 
   get '/swap_players' do
+    @turn = $game.increment_count
     @player_name = $game.player_2_name
     @number_of_players = $game.number_of_players
-    #if num = 2 redirect to selection otherwise computer selction
     erb :swap_players
   end
 
-  get '/selection' do
-    @player_name = $game.player_1_name
-    erb :selection
+  get '/computer_selection' do
+    @computer_selection = $game.computer_selection
+    redirect '/result'
+  end
+
+  get '/result' do
+    @player_2_selection = $game.computer_selection
+    @player_1_selection = $game.return_selection
+    @player_1_name = $game.player_1_name
+    @player_2_name = $game.player_2_name
+    @result = $game.declares_a_winner
+
+    erb :result
   end
 
   get '/selection' do
-    @player_name = $game.player_1_name
+    @turn = $game.increment_count
+    @number_of_players = $game.number_of_players
+    @player_1_name = $game.player_1_name
+    @player_2_name = $game.player_2_name
     erb :selection
+  end
+
+  get '/selection2' do
+    @turn = $game.increment_count
+    @number_of_players = $game.number_of_players
+    @player_1_name = $game.player_1_name
+    @player_2_name = $game.player_2_name
+    erb :selection
+  end
+
+
+  post '/selection' do
+    $game.selection(params[:selection])
+    $game.increment_count
+    redirect '/swap_players'
+  end
+
+  post '/selection2' do
+    $game.selection_2(params[:selection])
+    redirect '/result'
   end
 
   run! if app_file == $0
--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-  attr_reader :player_1_name, :player_2_name
+  attr_reader :player_1_name, :player_2_name, :selection, :count
 
   def initialize(name_1, name_2 = "Rock Paper Sissors bot 2000")
     @player_1_name = name_1
@@ -8,21 +8,36 @@ class Game
     #@num = num
     #@player_1_name = p_1.name
     #@player_2_name = p_2.name
-    @selection = []
+    @rps_array = ["Rock", "Paper", "Sissors"]
+    @count = 1
+  end
+
+  def increment_count
+    @count += 1
   end
 
   def selection(rps)
-    @selection << rps
+    @selection = ""
+    @selection = rps
+  end
+
+  def selection_2(rps)
+    @selection_2 = ""
+    @selection_2 = rps
   end
 
   def number_of_players
-      return 2 if @player_2_name == "Rock Paper Sissors bot 2000"
-      1
+      return 1 if @player_2_name == "Rock Paper Sissors bot 2000"
+      2
   end
 
+  def computer_selection
+    @selection_2 = ""
+    @selection_2 = @rps_array.sample
+  end
 
   def return_selection
-    @selection[-1]
+    @selection
   end
 
   def rules(player_1, player_2)
@@ -34,7 +49,7 @@ class Game
     return [lose, win]  if player_1 == "Rock" && player_2 == "Paper" || player_1 == "Sissors" && player_2 == "Rock" || player_1 == "Paper" && player_2 == "Sissors"
   end
 
-  def declares_a_winner(player_1, player_2)
+  def declares_a_winner(player_1 = @selection, player_2 = @selection_2)
     array = rules(player_1,player_2)
     return "#{@player_1_name} wins!"  if array[0] > array[1]
     return "#{@player_2_name} wins!"  if array[0] < array[1]
