599144e9f56947bb15bc89821940998ae233b9fd
Costas Kiteou <ckiteou22@gmail.com>
2017-07-23 11:59:16 +0100

    Seperation of concerns. Created Player class (Model layer) in anticipation of applying  appliciation logic. Params(player 'names') now stored as instance variables for the player class in the Model layer

--- app.rb
+++ app.rb
@@ -1,21 +1,24 @@
 require 'sinatra/base'
+require './lib/player'
 
 class RPS < Sinatra::Base
-  enable :sessions
+  #enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    session[:player_one] = params[:player_one]
-    session[:player_two] = params[:player_two]
+    #session[:player_one] = params[:player_one]
+    #session[:player_two] = params[:player_two]
+    $player_one = Player.new(params[:player_one])
+    $player_two = Player.new(params[:player_two])
     redirect '/play'
   end
 
   get '/play' do
-    @player_one = session[:player_one]
-    @player_two = session[:player_two]
+    @player_one = $player_one.name
+    @player_two = $player_two.name
     erb :play
   end
 
