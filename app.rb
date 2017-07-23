2d1698ed64aa4e66e8c48aa46f6b0a3f93c3c1b1
Cong Cong Bo <cong@hippocampus.ai>
2017-07-23 19:48:38 +0100

    POST redirect GET for name set up and working

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/name' do
-    @name = params[:name]
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
     erb :play
   end
 
