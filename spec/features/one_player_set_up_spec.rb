d3cf16cad801fd5ae41ebe42a49f46d6d6a228f9
annalaise <annalaise.gibbons@gmail.com>
2017-07-23 16:02:12 +0100

    one player mode, computer selects move test

--- spec/features/one_player_set_up_spec.rb
+++ spec/features/one_player_set_up_spec.rb
@@ -5,4 +5,10 @@ feature 'One Player Set Up' do
     expect(page).to have_content 'Sir R Spec'
   end
 
+  scenario 'player 1 picks their move' do
+    one_player_battle_setup
+    click_button "Rock"
+    expect(page). to have_content 'Computer has chosen its move'
+  end
+
 end
