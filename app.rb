d67c7c7163d95b3c23ca6d877d1b92a357b3e99c
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-23 17:31:20 +0100

    refactored code using POST-redirect-GET loop

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
