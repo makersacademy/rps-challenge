1bbffce5ebf361a3481a95baa7855d364a1ae36e
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-23 21:48:08 +0100

    Start front-end integration

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -2,7 +2,7 @@ feature 'Enter name' do
   scenario 'Submit player name' do
     visit '/'
     fill_in('player_name', with: 'Oleg ')
-    click_button 'Submit'
+    click_button 'Play'
     expect(page).to have_content 'Oleg'
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,5 +1,5 @@
 def sign_in_and_play
   visit '/'
   fill_in('player_name', with: 'Oleg')
-  click_button 'Submit'
+  click_button 'Play'
 end
