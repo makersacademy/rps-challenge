d9d81592dfcb8953a5c364759cca78c117a85de4
Adam Romano <manoadamro.imp@gmail.com>
2017-08-19 15:22:36 +0100

    clean up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,18 +5,18 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 Capybara.app = RPS
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
