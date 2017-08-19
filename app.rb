bbafb8ca38e2aaade8081f427d22de4ec8549a65
Adam Romano <manoadamro.imp@gmail.com>
2017-08-19 11:38:59 +0100

    player can not perform actions

--- app.rb
+++ app.rb
@@ -17,7 +17,14 @@ class RPS < Sinatra::Base
   get '/game' do
     redirect to('/') if session[:player].nil?
     @player = session[:player]
+    @action = session[:action]
+    session[:action] = nil
     erb(:game)
+  end
+
+  post '/play' do
+    session[:action] = params[:action]
+    redirect to('/game')
   end
 
   run! if app_file == $PROGRAM_NAME
--- spec/features/game_page_spec.rb
+++ spec/features/game_page_spec.rb
@@ -10,4 +10,19 @@ RSpec.feature "Game Page", type: :feature do
     visit('/game')
     expect(page).to have_text("Welcome")
   end
+
+  scenario "Contains rock" do
+    submit_name
+    expect(page).to have_button("Rock")
+  end
+
+  scenario "Contains paper" do
+    submit_name
+    expect(page).to have_button("Paper")
+  end
+
+  scenario "Contains scissors" do
+    submit_name
+    expect(page).to have_button("Scissors")
+  end
 end
