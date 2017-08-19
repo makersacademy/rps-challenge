e2bcb0bc06b4198eedd1432b8587293635189d2d
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-08-19 16:28:32 +0100

    Player form submission added

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
+require './lib/player'
 
 class RPS < Sinatra::Base
 
   get '/' do
     erb(:index)
   end
+
+  post '/play' do
+    @name = Player.new(params[:name])
+    erb(:game)
+  end
+
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
+require_relative './web_helper'
 
-feature "Home page", type: :feature do
-  scenario "it says hello world" do
-    visit "/"
-    expect(page).to have_text("Hello, world!")
+feature 'Home page', type: :feature do
+  scenario "it says Welcome" do
+    visit'/'
+    expect(page).to have_text('Welcome to Rock, Paper, Scissors')
+  end
+
+  scenario 'it shows your name once signed in' do
+    sign_in_and_play
+    expect(page).to have_text('Hello Lizzie - please choose Rock, Paper or Scissors to play')
   end
 end
