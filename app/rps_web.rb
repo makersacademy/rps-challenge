a7d7381f440f90a999548dc1f2d6d2f8cae22008
Nick Otter <nicholas.a.otter@gmail.com>
2017-08-20 17:21:36 +0100

    Player submit name tests passed

--- app/rps_web.rb
+++ app/rps_web.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
+require './lib/player'
 
 class RPSWeb < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    "Hello, world!"
+    erb :index
+  end
+
+  post '/game' do
+    @player = Player.new(params[:player_name])
+    erb :game
   end
 end
