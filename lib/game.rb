2502c5d8f26afb8d2b75f9b4fa26419996dc7cc2
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-08-21 01:36:42 +0100

    Revised indentation

--- lib/game.rb
+++ lib/game.rb
@@ -16,21 +16,21 @@ class Game
   end
 
   private
-    def draw
-      @user == @computer.comp
-    end
+  def draw
+    @user == @computer.comp
+  end
 
-    def scissors_win
-      (@user == "scissors" && @computer.comp == "paper")
-    end
+  def scissors_win
+    (@user == "scissors" && @computer.comp == "paper")
+  end
 
-    def rock_win
-      (@user == "rock" && @computer.comp == "scissors")
-    end
+  def rock_win
+    (@user == "rock" && @computer.comp == "scissors")
+  end
 
-    def paper_win
-      (@user == "paper" && @computer.comp == "rock")
-    end
+  def paper_win
+    (@user == "paper" && @computer.comp == "rock")
+  end
 end
 
 # test=Game.new("paper","rock")
