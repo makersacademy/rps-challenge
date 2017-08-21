a8ee05bb66dc8bb99e0cc3b81ca7b6cacec62192
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-08-21 14:48:14 +0100

    Refactored and added if statements

--- app.rb
+++ app.rb
@@ -10,59 +10,40 @@ class Battle < Sinatra::Base
   end
 
   post '/pick_one' do
-    # session[:name1] = params[:name0]
-    @name1 = params[:name0]
-    @name2 = "RPSSL Master"
-    @players = Players.create(@name1, @name2)
+    @players = Players.create(params[:name0], "RPSSL Master")
     redirect '/pick_one'
   end
 
-  get '/pick_one' do
-    # session[:name2] = "RPSSL Master"
+  post '/pick_two' do
+    @players = Players.create(params[:name1], params[:name2])
+    redirect '/pick_two'
+  end
+
+  before do
     @players = Players.instance
+  end
+
+  get '/pick_one' do
     erb(:player1)
   end
 
   post '/single_result' do
-    @players = Players.instance
-    # @name1 = session[:name1]
-    # @name2 = session[:name2]
-    session[:player1] = params[:user1]
-    @outcome = Game.create(session[:player1])
+    @outcome = Game.create(params[:user1])
     @outcome.result
     erb(:result)
   end
 
-  post '/pick_two' do
-    # session[:name1] = params[:name1]
-    # session[:name2] = params[:name2]
-    @name1 = params[:name1]
-    @name2 = params[:name2]
-    @players = Players.create(@name1, @name2)
-    redirect '/pick_two'
-  end
-
   get '/pick_two' do
-    @players = Players.instance
-    # @name1 = session[:name1]
-    # @name2 = session[:name2]
     erb(:player1_in_double)
   end
 
   post '/double_choice' do
-    @players = Players.instance
-    # @name1 = session[:name1]
-    # @name2 = session[:name2]
     session[:player1] = params[:user1]
     erb(:player2_in_double)
   end
 
   post '/double_result' do
-    @players = Players.instance
-    session[:player2] = params[:user2]
-    # @name1 = session[:name1]
-    # @name2 = session[:name2]
-    @outcome = Game.create(session[:player1], session[:player2])
+    @outcome = Game.create(session[:player1], params[:user2])
     @outcome.result
     erb(:result)
   end
