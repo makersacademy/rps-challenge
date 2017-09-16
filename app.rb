3d46c586451223c4836d243588fc3b5f9fd69dc8
robincollins <racollins1@me.com>
2017-09-16 14:28:52 +0100

    post/redirect/get, and enbale sessions

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require "sinatra/base"
 
 class Rps < Sinatra::Base
-
+  enable :sessions
 
   # get "/" do
   #   "testing infrastructure working!"
@@ -12,8 +12,15 @@ class Rps < Sinatra::Base
   end
 
   post "/names" do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = "Computer"
+    session[:player_1_name] = params[:player_1_name]
+    # @player_2_name = "Computer"
+    session[:player_2_name] = "Computer"
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
