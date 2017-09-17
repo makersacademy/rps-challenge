2ed16316215c5335d23e96315a6c9c02e4f00bbf
keith(mac) <ka33145@googlemail.com>
2017-09-17 12:02:38 +0100

    Corrected typo in theme, added route /opponent to rps and added opponent.erb

--- rps.rb
+++ rps.rb
@@ -5,6 +5,16 @@ class RPS < Sinatra::Base
   get '/' do
     erb(:index)
   end
+
+  post '/name' do
+    session[:player1] = params[:player1_name]
+    redirect('/opponent')
+  end
+
+  get '/opponent' do
+    @player1 = session[:player1]
+    erb(:opponent)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $PROGRAM_NAME
 end
