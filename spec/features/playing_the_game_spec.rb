abc9b28a91fa17476e45dc7ec22383ea3e4222e3
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-09-17 12:02:41 +0100

    Update playing the game feature spec

--- spec/features/playing_the_game_spec.rb
+++ spec/features/playing_the_game_spec.rb
@@ -3,9 +3,24 @@ require_relative 'web_helpers'
 
 feature 'Playing the game' do
   scenario "When I submit 'Rock' I am told if I have won" do
-    allow_any_instance_of(Game::Weapons).to receive(:sample).and_return('Scissors')
+    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
     sign_in_and_play
     click_on 'Rock'
     expect(page).to have_content "Winner: Zoe"
   end
+
+  scenario "When I submit 'Rock' I am told if I have lost" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Paper')
+    sign_in_and_play
+    click_on 'Rock'
+    expect(page).to have_content "Winner: Computer"
+  end
+
+  scenario "When I submit 'Rock' I am told if I have drawn" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
+    sign_in_and_play
+    click_on 'Rock'
+    expect(page).to have_content "It's a draw!"
+  end
+
 end
