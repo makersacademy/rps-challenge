b5a9cfd3dd85554ac9b35ac9736068f06b1a7110
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-17 09:32:33 +0100

    Buttons pass players and their weapons to page

--- spec/features/combat_page_spec.rb
+++ spec/features/combat_page_spec.rb
@@ -3,8 +3,10 @@ feature 'combat page displays the fight' do
   scenario 'see names of combatants and their weapons' do
     give_name_heraldo
     click_button 'Rock'
-
     expect(page).to have_content('Heraldo sends Rock in to fight')
+    give_name_heraldo
+    click_button 'Paper'
+    expect(page).to have_content('Heraldo sends Paper in to fight')
   end
 
 end
