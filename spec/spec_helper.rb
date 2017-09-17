96e509391ae63523785c77015b8270cfd2e62511
keith(mac) <ka33145@googlemail.com>
2017-09-17 09:59:54 +0100

    Added reference to Capybara.app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,16 @@
+ENV['RACK_ENV'] = 'test'
+require File.join(File.dirname(__FILE__), '..', 'rps.rb')
+
+require 'capybara'
+require 'rspec'
+
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = RPS
+
+
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
