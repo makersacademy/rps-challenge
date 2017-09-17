6d8642b07d4fad1ed477b79757b20ea692e2f913
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-17 21:32:03 +0100

    Updated README

--- spec/features/combat_page_spec.rb
+++ spec/features/combat_page_spec.rb
@@ -19,16 +19,11 @@ feature 'combat page displays the fight' do
   end
 
   scenario "cancelling out randomness - making sure right return value" do
-  p RPS::DATABASE.contents
     reset_database
-    p RPS::DATABASE.contents
     allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
     visit('/')
-    p RPS::DATABASE.contents
     give_name_heraldo
-    p RPS::DATABASE.contents
     click_button 'Rock'
-    p RPS::DATABASE.contents
     expect(page).to have_content "Heraldo sends Rock in to fight Roborory's Scissors 1 point for Heraldo! Shall we play again?"
   end
 
