b7423b72cbbac81637c13dddba3065da8920d9dc
Marcus Rands <mrands@hotmail.co.uk>
2017-09-17 17:23:01 +0100

    All tests now passing

--- lib/game.rb
+++ lib/game.rb
@@ -10,20 +10,20 @@ class Game
     @computer_choice = ["Rock", "Paper", "Scissors"].sample
   end
 
-  def rps_game
+  def rps_game(computer_choice = @computer_choice)
     case @choice
     when "Rock"
-      return "Game is a draw" if @computer_choice == "Rock"
-      return "You win!"  if @computer_choice == "Scissors"
-      return "You lose!" if @computer_choice == "Paper"
+      return "Game is a draw" if computer_choice == "Rock"
+      return "You win!"  if computer_choice == "Scissors"
+      return "You lose!" if computer_choice == "Paper"
     when "Paper"
-      return "Game is a draw" if @computer_choice == "Paper"
-      return "You win!"  if @computer_choice == "Rock"
-      return "You lose!" if @computer_choice == "Scissors"
+      return "Game is a draw" if computer_choice == "Paper"
+      return "You win!"  if computer_choice == "Rock"
+      return "You lose!" if computer_choice == "Scissors"
     when "Scissors"
-      return "Game is a draw" if @computer_choice == "Scissors"
-      return "You win!"  if @computer_choice == "Paper"
-      return "You lose!" if @computer_choice == "Rock"
+      return "Game is a draw" if computer_choice == "Scissors"
+      return "You win!"  if computer_choice == "Paper"
+      return "You lose!" if computer_choice == "Rock"
     end
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,7 @@ require './lib/game'
 
 describe Game do
   let(:player) {Player.new("Marcus")}
+  let (:computer_choice) {"Scissors"}
   subject(:game) {described_class.new(:player, "Rock")}
 
   it 'is initialized with the player choice' do
@@ -15,8 +16,7 @@ describe Game do
 
   describe '#rps_game' do
     it 'is expected to return an outcome for the game' do
-      allow(game).to receive(:computer_choice) { 'Paper' }
-      expect(game.rps_game).to eq "You win!"
+      expect(game.rps_game(computer_choice)).to eq "You win!"
     end
   end
 end
