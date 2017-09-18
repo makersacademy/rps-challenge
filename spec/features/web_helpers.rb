dde93c82957189255bdc7bfc5c4e879886591bb4
Thomas Ikimi <themerc01@gmail.com>
2017-09-18 10:27:26 +0100

    corrected tests to pass for icons instead of text links

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,13 +1,13 @@
 def sign_in
   visit('/')
-  click_button 'VS COMPUTER'
+  click_link('', :href => '/computer')
   fill_in :player_1_name, with: 'Thomas'
   click_button 'Submit'
 end
 
 def sign_in_2
   visit('/')
-  click_button 'HEAD 2 HEAD'
+  click_link('', :href => '/human')
   fill_in :player_1_name, with: 'Thomas'
   fill_in :player_2_name, with: 'Zoe'
   click_button 'Submit'
