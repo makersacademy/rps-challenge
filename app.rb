d9d2d9f9d30a301ae17d97b76defbfecf550ec5b
Tim Jones <timjones10@hotmail.com>
2017-10-14 10:08:44 +0100

    RPS buttons added

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/name' do
-    @player_name = params[:player_name]
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
+    @shape = session[:shape]
     erb :play
+  end
+
+  post '/play' do
+    session[:shape] = params[:shape]
+    redirect '/play'
   end
 
   run! if app_file == $0
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature 'Enter name' do
   scenario 'The player can enter their name' do
     visit ('/')
-    fill_in :player_name, with: "Tim"
+    fill_in 'name', with: 'Tim'
     click_button 'Submit'
-    expect(page).to have_content "Tim"
+    expect(page).to have_content 'Tim'
   end
 end
