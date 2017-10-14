172f4dc3a3da4097ff38d591a1fc625ed9267404
ainsleybc <ab.chang@me.com>
2017-10-14 13:44:59 +0100

    added unit tests for class methods

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,11 +3,29 @@ require 'game'
 describe Game do
 
   subject(:game) { Game.new('Ainsley') }
+  player = 'Ainsley'
 
   describe '#player' do
 
     it 'returns the current game' do
       expect(game.player).to eq 'Ainsley'
+    end
+
+  end
+
+  describe '#self.create_game' do
+
+    it 'creates a new game instance' do
+      expect(Game.create_game(player)).to be_instance_of Game
+    end
+
+  end
+
+  describe '#self.current_game' do
+    
+    it 'returns the current game instance' do
+      Game.create_game(player)
+      expect(Game.current_game).to be_instance_of Game
     end
 
   end
