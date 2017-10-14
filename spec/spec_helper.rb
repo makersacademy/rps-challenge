1b2512ca49cc088046e272eb04dc0788e91b8344
Alex <alex@scotttonge.com>
2017-10-14 11:21:20 +0100

    Set up directory and added passing feature test for homepage working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../app.rb'
+
+require 'capybara'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = RPS
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
