710539ab046981a4d646128c620c445dedbe5c15
Jon Sanders <jonsanders101@aol.com>
2017-10-14 13:11:53 +0100

    Add #versus implementation for paper

--- lib/paper.rb
+++ lib/paper.rb
@@ -1,4 +1,11 @@
 require 'weapon'
 
 class Paper < Weapon
+
+  def initialize
+    @strong_against = [Rock]
+    @weak_against = [Scissors]
+    @draws_against = [Paper]
+  end
+
 end
--- spec/paper_spec.rb
+++ spec/paper_spec.rb
@@ -1,4 +1,30 @@
 require 'paper'
 
 describe Paper do
+
+  subject(:paper) { described_class.new }
+  let(:rock) { double(:rock) }
+  let(:scissors) { double(:scissors) }
+
+  describe "#versus"
+    it "returns lose against scissors" do
+      allow(scissors).to receive(:is_a?).with(Paper) { false }
+      allow(scissors).to receive(:is_a?).with(Scissors) { true }
+      allow(scissors).to receive(:is_a?).with(Rock) { false }
+      expect(paper.versus(scissors)).to eq("Lose")
+    end
+
+    it "returns win against rock" do
+      allow(rock).to receive(:is_a?).with(Paper) { false }
+      allow(rock).to receive(:is_a?).with(Scissors) { false }
+      allow(rock).to receive(:is_a?).with(Rock) { true }
+      expect(paper.versus(rock)).to eq("Win")
+    end
+
+    it "returns draw against paper" do
+      allow(paper).to receive(:is_a?).with(Paper) { true }
+      allow(paper).to receive(:is_a?).with(Scissors) { false }
+      allow(paper).to receive(:is_a?).with(Rock) { false }
+      expect(paper.versus(paper)).to eq("Draw")
+    end
 end
