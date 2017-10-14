f72c34cdb3ec1e363bc293e7efcb0570ea401e64
David Halewood <davidnhalewood@hotmail.com>
2017-10-14 15:50:35 +0100

    adding buttons for paper and scissors

--- spec/features/game_page_spec.rb
+++ spec/features/game_page_spec.rb
@@ -3,4 +3,12 @@ feature 'Game screen' do
     ready_to_play
     expect(page).to have_button 'Rock'
   end
+  scenario 'there is an option for paper' do
+    ready_to_play
+    expect(page).to have_button 'Paper'
+  end
+  scenario 'there is an option for scissors' do
+    ready_to_play
+    expect(page).to have_button 'Scissors'
+  end
 end
