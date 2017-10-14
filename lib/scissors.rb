a30a4eccaecfec8a4767df6c740e1ad05fee61ea
Jon Sanders <jonsanders101@aol.com>
2017-10-14 13:08:10 +0100

    Add versus implementation for Scissors

--- lib/scissors.rb
+++ lib/scissors.rb
@@ -2,4 +2,11 @@ require 'weapon'
 
 
 class Scissors < Weapon
+
+  def initialize
+    @strong_against = [Paper]
+    @weak_against = [Rock]
+    @draws_against = [Scissors]
+  end
+
 end
--- spec/scissors_spec.rb
+++ spec/scissors_spec.rb
@@ -1,4 +1,29 @@
 require 'scissors'
 
 describe Scissors do
+  subject(:scissors) { described_class.new }
+  let(:paper) { double(:paper) }
+  let(:rock) { double(:rock) }
+
+  describe "#versus"
+    it "returns lose against rock" do
+      allow(rock).to receive(:is_a?).with(Paper) { false }
+      allow(rock).to receive(:is_a?).with(Scissors) { false }
+      allow(rock).to receive(:is_a?).with(Rock) { true }
+      expect(scissors.versus(rock)).to eq("Lose")
+    end
+
+    it "returns win against paper" do
+      allow(paper).to receive(:is_a?).with(Paper) { true }
+      allow(paper).to receive(:is_a?).with(Scissors) { false }
+      allow(paper).to receive(:is_a?).with(Rock) { false }
+      expect(scissors.versus(paper)).to eq("Win")
+    end
+
+    it "returns draw against scissors" do
+      allow(scissors).to receive(:is_a?).with(Paper) { false }
+      allow(scissors).to receive(:is_a?).with(Scissors) { true }
+      allow(scissors).to receive(:is_a?).with(Rock) { false }
+      expect(scissors.versus(scissors)).to eq("Draw")
+    end
 end
