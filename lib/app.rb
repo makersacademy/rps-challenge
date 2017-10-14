2a4271f0a58ab8d120792461d533ccf703ae8b2d
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-14 17:35:47 +0100

    Added feature test for players entering names and passed tests.

--- lib/app.rb
+++ lib/app.rb
@@ -7,4 +7,17 @@ class RPS < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/players' do
+    $player_1 = params[:player_1]
+    $player_2 = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = $player_1
+    @player_2 = $player_2
+    erb :play
+  end
+
 end
--- spec/feature/infrastructure_spec.rb
+++ spec/feature/infrastructure_spec.rb
@@ -1,9 +1,17 @@
-require "capybara/rspec"
-require_relative "../../lib/app"
+# require 'capybara/rspec'
+require_relative '../../lib/app'
 
-feature 'testing infrastructure' do
-  scenario 'can run app load index' do
+RSpec.feature 'testing infrastructure' do
+  scenario 'can run app and load index' do
     visit '/'
     expect(page).to have_content 'Welcome to Rock/Paper/Scissors'
+  end
+
+  scenario 'can sign up' do
+    visit '/'
+    fill_in :player_1, with: 'Leonard'
+    fill_in :player_2, with: 'Sheldon'
+    click_button 'Submit'
+    expect(page).to have_content 'Leonard vs. Sheldon'
   end
 end
