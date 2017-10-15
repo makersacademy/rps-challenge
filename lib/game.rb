98f3ca41929a367d32c27cf29d8b58643b2f2034
oliverpople <oliverpople@gmail.com>
2017-10-15 16:27:42 +0100

    Integrating hound amends

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-attr_reader :move, :counter, :game_result
+  attr_reader :move, :counter, :game_result
 
   def initialize(move)
     @move = move
@@ -11,21 +11,21 @@ attr_reader :move, :counter, :game_result
   end
 
   def result(move, counter)
-      if (move == counter)
-       @game_result = "It's a draw"
-      elsif (move == "Rock" && counter == "Scissors")
-       @game_result = "computer choice is: " + counter + ", you win! :)"
-      elsif (move == "Rock" && counter == "Paper")
-       @game_result = "computer choice is: " + counter + ", computer wins :("
-      elsif (move == "Paper" && counter == "Scissors")
-       @game_result = "computer choice is: " + counter + ", computer wins :("
-      elsif (move == "Paper" && counter == "Rock")
-       @game_result = "computer choice is: " + counter + ", you win! :)"
-      elsif (move == "Scissors" && counter == "Rock")
-       @game_result = "computer choice is: " + counter + ", computer wins :("
-      elsif (move == "Scissors" && counter == "Paper")
-       @game_result = "computer choice is: " + counter + ", you win! :)"
-      end
+    if (move == counter)
+      @game_result = "It's a draw"
+    elsif (move == "Rock" && counter == "Scissors")
+      @game_result = "computer choice is: " + counter + ", you win! :)"
+    elsif (move == "Rock" && counter == "Paper")
+      @game_result = "computer choice is: " + counter + ", computer wins :("
+    elsif (move == "Paper" && counter == "Scissors")
+      @game_result = "computer choice is: " + counter + ", computer wins :("
+    elsif (move == "Paper" && counter == "Rock")
+      @game_result = "computer choice is: " + counter + ", you win! :)"
+    elsif (move == "Scissors" && counter == "Rock")
+      @game_result = "computer choice is: " + counter + ", computer wins :("
+    elsif (move == "Scissors" && counter == "Paper")
+      @game_result = "computer choice is: " + counter + ", you win! :)"
+    end
   end
 
 end
--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -2,8 +2,7 @@ feature 'takes users move and returns result' do
   scenario 'user chooses a move' do
     sign_in_and_play
     click_button "Rock"
-    expect(page).to satisfy{|page| page.has_content?
-    ("computer choice is:") && (("you win! :)" ) or ("computer wins :(")) or page.has_content?("It's a draw")}
+    expect(page).to satisfy { |page| page.has_content?("computer choice is:") && (("you win! :)" ) or ("computer wins :(")) or page.has_content?("It's a draw")}
     expect(page).to have_content("Play again")
   end
 end
