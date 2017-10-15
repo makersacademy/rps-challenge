001af7a8a3e1163e8f8c0f3ca9bf8cf2b3539327
Chris <cdunham1989@gmail.com>
2017-10-15 12:27:32 +0100

    Homepage setup and environment structure laid out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../app.rb'
+# require_relative './features/web_helpers.rb'
+
+require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = RockPaperScissors
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
