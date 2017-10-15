db643fcf7e7668d3ff378e14d3ac1f4deea53bd4
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-15 23:41:58 +0100

    Added specific css file

--- spec/features/buttons_spec.rb
+++ spec/features/buttons_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../lib/app'
 require_relative 'spec_helper'
 
 feature 'Buttons for choosing weapon' do
-  before { sign_in_and_play}
+  before { sign_in_and_play }
   context 'player chooses various buttons' do
     scenario 'rock button' do
       expect(page).to have_button('Rock')
