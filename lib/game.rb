dc84a5a6c34a3c4361aac1093df1af825a5f8819
Aram Simonian <aram.contacts@gmail.com>
2017-10-15 22:27:54 +0100

    removed whitespace

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
   attr_reader :player_1, :player_2
-  
+
   def self.create(player_1, player_2)
     @game = Game.new(player_1, player_2)
   end
@@ -13,4 +13,5 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
   end
+
 end
