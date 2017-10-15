28b921b0ee31a61cab9c02c2fbfe306d4207ff99
David Halewood <davidnhalewood@hotmail.com>
2017-10-15 18:27:07 +0100

    adding two_player_version web helper

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -4,3 +4,11 @@ def one_player_version
   fill_in :name, with: 'David'
   click_button 'Submit'
 end
+
+def two_player_version
+  visit '/'
+  click_button "Human"
+  fill_in :player1, with: "David"
+  fill_in :player2, with: "Scotty"
+  click_button 'Submit'
+end
