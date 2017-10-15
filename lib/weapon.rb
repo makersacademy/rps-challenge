c566f4d87eae2c4eecc09fba2d5840bab8b500c6
Jon Sanders <jonsanders101@aol.com>
2017-10-15 13:32:35 +0100

    Change computer choice to opponent choice

--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,9 +1,9 @@
 class Weapon
 
-  def versus(computer_choice)
-    @strong_against.each {|loser| return "Wins" if computer_choice.is_a? loser }
-    @weak_against.each {|winner| return "Loses" if computer_choice.is_a? winner}
-    @draws_against.each {|equal| return "Draw" if computer_choice.is_a? equal }
+  def versus(opponent_choice)
+    @strong_against.each {|loser| return "Wins" if opponent_choice.is_a? loser }
+    @weak_against.each {|winner| return "Loses" if opponent_choice.is_a? winner}
+    @draws_against.each {|equal| return "Draw" if opponent_choice.is_a? equal }
   end
 
 end
