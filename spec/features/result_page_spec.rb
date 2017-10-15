685722395f4c139cf1b11ec63452ed245e21ca07
David Halewood <davidnhalewood@hotmail.com>
2017-10-15 19:30:32 +0100

    adding final tests and two player results page view

--- spec/features/result_page_spec.rb
+++ spec/features/result_page_spec.rb
@@ -1,22 +1,40 @@
 feature 'Result page' do
-  scenario 'the users choice is displayed' do
-    one_player_version
-    click_button 'Rock'
-    expect(page).to have_content "rock"
+  context 'one player' do
+    scenario 'the users choice is displayed' do
+      one_player_version
+      click_button 'Rock'
+      expect(page).to have_content "rock"
+    end
+    scenario 'the computer choice is displayed' do
+      one_player_version
+      click_button "Spock"
+      expect(page).to have_content(/rock|paper|scissors|lizard|spock/) 
+    end
+    scenario 'the result is displayed' do
+      one_player_version
+      click_button 'Paper'
+      expect(page).to have_content(/Won|Lost|Drew/)
+    end
+    scenario 'the player can choose to play again' do
+      one_player_version
+      click_button 'Lizard'
+      expect(page).to have_button "Again?"
+    end
   end
-  scenario 'the computer choice is displayed' do
-    one_player_version
-    click_button "Spock"
-    expect(page).to have_content(/rock|paper|scissors|lizard|spock/) 
-  end
-  scenario 'the result is displayed' do
-    one_player_version
-    click_button 'Paper'
-    expect(page).to have_content(/Won|Lost|Drew/)
-  end
-  scenario 'the player can choose to play again' do
-    one_player_version
-    click_button 'Lizard'
-    expect(page).to have_button "Again?"
+  context 'two player' do
+    before do
+      two_player_version
+      click_button 'Rock'
+      click_button 'Spock'
+    end
+    scenario 'the users choices are displayed' do
+      expect(page).to have_content "rock" && "spock"
+    end
+    scenario 'the result is displayed' do
+      expect(page).to have_content(/Won|Draw/)
+    end
+    scenario 'the player can choose to play again' do
+      expect(page).to have_button "Again?"
+    end
   end
 end
