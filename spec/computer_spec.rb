9004977bff3b79d26fcbd78a7f046e7301aa981e
Jon Sanders <jonsanders101@aol.com>
2017-10-15 17:44:23 +0100

    Add test for Computer#name

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -4,9 +4,16 @@ describe Computer do
 
   subject(:computer) { described_class.new }
 
-  describe "#computer_choose" do
-    it "returns either Rock, Paper or Scissors" do
-      expect(Computer::WEAPONS).to include computer.choose
+  describe "#choose" do
+    it "assigns @weapon_choice a weapon" do
+      computer.choose
+      expect(Computer::WEAPONS).to include computer.weapon_choice.class.to_s
+    end
+  end
+
+  describe "#name" do
+    it "returns default name" do
+      expect(subject.name).to eq Computer::COMPUTER_NAME
     end
   end
 
