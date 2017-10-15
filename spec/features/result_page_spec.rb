1d265db6e48d9eee61d2eeb963d3a5b549091890
David Halewood <davidnhalewood@hotmail.com>
2017-10-15 19:57:31 +0100

    adding a button to restart the game

--- spec/features/result_page_spec.rb
+++ spec/features/result_page_spec.rb
@@ -1,26 +1,34 @@
 feature 'Result page' do
   context 'one player' do
-    scenario 'the users choice is displayed' do
+    before do
       one_player_version
       click_button 'Rock'
+    end
+    scenario 'the users choice is displayed' do
       expect(page).to have_content "rock"
     end
     scenario 'the computer choice is displayed' do
-      one_player_version
-      click_button "Spock"
       expect(page).to have_content(/rock|paper|scissors|lizard|spock/) 
     end
     scenario 'the result is displayed' do
-      one_player_version
-      click_button 'Paper'
       expect(page).to have_content(/Won|Lost|Drew/)
     end
     scenario 'the player can choose to play again' do
-      one_player_version
-      click_button 'Lizard'
       expect(page).to have_button "Again?"
     end
+    scenario 'the again button returns the player to the choice screen' do
+      click_button "Again?"
+      expect(page).to have_current_path '/play'
+    end
+    scenario 'the player can choose to go back to the start page' do
+      expect(page).to have_button "Restart"
+    end
+    scenario 'the again button returns the player to the choice screen' do
+      click_button "Restart"
+      expect(page).to have_current_path '/'
+    end
   end
+  
   context 'two player' do
     before do
       two_player_version
@@ -35,6 +43,17 @@ feature 'Result page' do
     end
     scenario 'the player can choose to play again' do
       expect(page).to have_button "Again?"
+    end
+    scenario 'the again button returns the player to the choice screen' do
+      click_button "Again?"
+      expect(page).to have_current_path '/two_play'
+    end
+    scenario 'the player can choose to go back to the start page' do
+      expect(page).to have_button "Restart"
+    end
+    scenario 'the again button returns the player to the choice screen' do
+      click_button "Restart"
+      expect(page).to have_current_path '/'
     end
   end
 end
