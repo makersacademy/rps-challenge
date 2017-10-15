30f88eba1e07f380a7d5e2b8daf23bc37b26d029
David Halewood <davidnhalewood@hotmail.com>
2017-10-15 17:16:49 +0100

    adding passing tests for player two display

--- spec/features/two_player_page_spec.rb
+++ spec/features/two_player_page_spec.rb
@@ -1,7 +1,15 @@
 feature 'Two player screen' do
-  scenario 'the title page loads' do
+  before do
     visit '/'
     click_button "Human"
+  end
+  scenario 'the title page loads' do
     expect(page).to have_content "Rock Paper Scissors Lizard Spock"
+  end
+  scenario 'player 1 can enter their name' do
+    expect(page).to have_field "player1"
+  end
+  scenario 'player 2 can enter their name' do
+    expect(page).to have_field "player2"
   end
 end
