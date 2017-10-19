cdbdb86ce69251fdf0214ab188b477a7bbbc2600
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-19 14:07:46 +0100

    Got tests passing by fixing the double

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,30 +2,31 @@ require 'game'
 require 'move'
 
 describe Game do
-  let(:player_1) { double(:player_1) }
-  let(:player_2) { double(:player_2) }
+  let(:player_1) { double(:player_1, name: "name_1") }
+  let(:player_2) { double(:player_2, name: "name_2") }
   # let(:move_1) { double(:move_1, beats?: false ) }
   # let(:move_2) { double(:move_2, beats?: true ) }
-  subject(:game) { Game.new(player_1, player_2) }
+  subject(:game) { Game.create(player_1, player_2) }
+# These tests now need to be on class varaibles rather than instance variables
 
   describe '#result' do
     it 'makes Paper beat Rock' do
       allow(player_1).to receive(:move).and_return(Move.new('Rock'))
       allow(player_2).to receive(:move).and_return(Move.new('Paper'))
       # allow(move_1).to receive(:beats?).with(move_2).and_return(false)
-      expect(game.result).to eq "#{player_2} wins!"
+      expect(game.result).to eq "name_2 wins!"
     end
 
     it 'makes Scissors beat Paper' do
       allow(player_1).to receive(:move).and_return(Move.new('Scissors'))
       allow(player_2).to receive(:move).and_return(Move.new('Paper'))
-      expect(game.result).to eq "#{player_1} wins!"
+      expect(game.result).to eq "name_1 wins!"
     end
 
     it 'makes Rock beat Scissors' do
       allow(player_1).to receive(:move).and_return(Move.new('Scissors'))
       allow(player_2).to receive(:move).and_return(Move.new('Rock'))
-      expect(game.result).to eq "#{player_2} wins!"
+      expect(game.result).to eq "name_2 wins!"
     end
 
     it 'makes Paper tie with Paper' do
