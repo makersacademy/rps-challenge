feab923f3e06b5f72bd5cba14f4467f0fa24d47a
Freddie Ofori <FrederickOfori@email.com>
2017-11-06 11:29:28 +0000

    Removed game global variable, replaced with class variable

--- app.rb
+++ app.rb
@@ -5,34 +5,35 @@ require './lib/game.rb'
 
 class RPS < Sinatra::Base
   enable :sessions
+  @@game = nil
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    $game = Game.new(Player.new(params[:player_1]))
+    @@game = Game.new(Player.new(params[:player_1]))
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $game.player_1.name
+    @player_1_name = @@game.player_1.name
     $player_2 = Computer.new
     @player_2_name = $player_2.name
     erb(:play)
   end
 
   post '/selection' do
-    $game.player_1.weapon = params[:action]
+    @@game.player_1.weapon = params[:action]
     redirect '/result'
   end
 
   get '/result' do
-    @player_1_name = $game.player_1.name
-    @player_1_weapon = $game.player_1.weapon
+    @player_1_name = @@game.player_1.name
+    @player_1_weapon = @@game.player_1.weapon
     @player_2_weapon = $player_2.weapon
     @player_2_name = $player_2.name
-    @rules = $game.rules
+    @rules = @@game.rules
     @result = @rules[@player_1_weapon][@player_2_weapon]
     erb(:result)
   end
