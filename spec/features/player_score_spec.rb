b00c807253028e1f7999eb35dce6ab6d3e64b866
majdeddine <majda-jambouri@hotmail.fr>
2017-11-11 20:00:53 +0000

    add Computer score

--- spec/features/player_score_spec.rb
+++ spec/features/player_score_spec.rb
@@ -1,6 +1,10 @@
 feature 'viewing player score' do
   scenario 'player should start with 0 points' do
     sign_in_and_play
-    expect(page).to have_content 'Score : 0'
+    expect(page).to have_content 'Jo Score : 0'
+  end
+  scenario 'computer should start with 0 points' do
+    sign_in_and_play
+    expect(page).to have_content 'Computer Score : 0'
   end
 end
