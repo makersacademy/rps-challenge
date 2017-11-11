171d9deb147b378c633c6be2084ddccb200373d1
Tabish Raza <tabishraza@gmail.com>
2017-11-11 16:00:37 +0000

    user selection is displayed

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ class Rps < Sinatra::Base
   enable  :sessions
 
   get '/' do
+    session[:choose] = nil
     erb(:index)
   end
 
@@ -15,7 +16,14 @@ class Rps < Sinatra::Base
 
   get '/play' do
     @player = session[:name]
+    @choose = session[:choose]
     erb(:play)
+  end
+
+  get '/select' do
+    @choose = params[:choose]
+    session[:choose] = @choose
+    redirect '/play'
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -21,4 +21,22 @@ describe Rps, :type => :feature do
       expect(page).to have_button('Scissor')
     end
   end
+
+  feature 'clicking options' do
+    scenario 'User chooses rock' do
+      sign_in_and_play
+      click_button('Rock')
+      expect(page).to have_text('Rock')
+    end
+    scenario 'User chooses paper' do
+      sign_in_and_play
+      click_button('Paper')
+      expect(page).to have_text('Paper')
+    end
+    scenario 'User chooses scissor' do
+      sign_in_and_play
+      click_button('Scissor')
+      expect(page).to have_text('Scissor')
+    end
+  end
 end
