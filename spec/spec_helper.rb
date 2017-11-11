429656e08323bcfed2c06d11124fd14136d0cf53
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2017-11-11 12:41:11 +0000

    First commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'sinatra'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
