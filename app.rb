20394fd135b1cbd0d61e80a5cecc85412a8240af
Robert Clayton <robbie.c.clayton@gmail.com>
2017-11-11 22:33:45 +0000

    player can make a choice and see it on screen

--- app.rb
+++ app.rb
@@ -16,4 +16,13 @@ class RockPaperScissors < Sinatra::Base
       erb :multi_game
     end
   end
+
+  post '/result' do
+    @rock = params[:Rock]
+    @paper = params[:Paper]
+    @scissors = params[:Scissors]
+    @lizard = params[:Lizard]
+    @spock = params[:Spock]
+    erb :result
+  end
 end
--- spec/features/testing_infrastucture_spec.rb
+++ spec/features/testing_infrastucture_spec.rb
@@ -9,3 +9,31 @@ feature 'Enter names' do
     expect(page).to have_content('Robert VS Evelina')
   end
 end
+
+feature 'Make a choice' do
+  scenario 'Player can choose Rock and see it on screen' do
+    sign_in_and_play
+    click_on('Rock')
+    expect(page).to have_content('Rock')
+  end
+  scenario 'Player can choose Paper and see it on screen' do
+    sign_in_and_play
+    click_on('Paper')
+    expect(page).to have_content('Paper')
+  end
+  scenario 'Player can choose Scissors and see it on screen' do
+    sign_in_and_play
+    click_on('Scissors')
+    expect(page).to have_content('Scissors')
+  end
+  scenario 'Player can choose Lizard and see it on screen' do
+    sign_in_and_play
+    click_on('Lizard')
+    expect(page).to have_content('Lizard')
+  end
+  scenario 'Player can choose Spock and see it on screen' do
+    sign_in_and_play
+    click_on('Spock')
+    expect(page).to have_content('Spock')
+  end
+end
