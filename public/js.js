1ea2d623ab8e828db21877f11fa78b67788d9b05
Allan Wazacz <cazwazacz@gmail.com>
2017-11-12 14:07:18 +0000

    Changes user colour in multiplayer when button is pressed

--- public/js.js
+++ public/js.js
@@ -20,10 +20,12 @@ player2keys = {
 document.onkeypress = function(e) {
   if (e.keyCode == 100 || e.keyCode == 115 || e.keyCode == 97 || e.keyCode == 119 || e.keyCode == 113) {
     player1move = player1keys[e.keyCode];
+    document.getElementById("player1name").style.color = "blue";
     document.getElementById("player1").value = player1move;
   }
   if (e.keyCode == 108 || e.keyCode == 107 || e.keyCode == 106 || e.keyCode == 112 || e.keyCode == 111) {
     player2move = player2keys[e.keyCode];
+    document.getElementById("player2name").style.color = "blue";
     document.getElementById("player2").value = player2move;
   }
   if (player1move && player2move) {
