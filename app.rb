c8297872bc15e63c029063f844fd392bb1d9d5bb
Robert Clayton <robbie.c.clayton@gmail.com>
2017-11-12 00:13:32 +0000

    refactored to show on main screen

--- app.rb
+++ app.rb
@@ -16,6 +16,9 @@ class RockPaperScissors < Sinatra::Base
 
   get '/game' do
     @player1 = session[:player_1]
+    @result_message = session[:result_message]
+    @choice = session[:player1_choice]
+    @computer_choice = session[:player2_choice]
     erb :game
   end
 
@@ -26,30 +29,36 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post '/result' do
-    @choice = params[:Rock] unless params[:Rock] == nil
-    @choice = params[:Paper] unless params[:Paper] == nil
-    @choice = params[:Scissors] unless params[:Scissors] == nil
-    @choice = params[:Lizard] unless params[:Lizard] == nil
-    @choice = params[:Spock] unless params[:Spock] == nil
+    @choice = params[:Rock] unless params[:Rock].nil?
+    @choice = params[:Paper] unless params[:Paper].nil?
+    @choice = params[:Scissors] unless params[:Scissors].nil?
+    @choice = params[:Lizard] unless params[:Lizard].nil?
+    @choice = params[:Spock] unless params[:Spock].nil?
 
     computer_random = [:Rock, :Paper, :Scissors, :Lizard, :Spock]
     @computer_choice = computer_random[rand(4)]
 
     result = Hash.new
-    result['Rock'] = {Rock: nil, Paper: false, Scissors: true, Lizard: true, Spock: false}
-    result['Paper'] = {Rock: true, Paper: nil, Scissors: false, Lizard: false, Spock: true}
-    result['Scissors'] = {Rock: false, Paper: true, Scissors: nil, Lizard: true, Spock: false}
-    result['Lizard'] = {Rock: false, Paper: true, Scissors: false, Lizard: nil, Spock: true}
-    result['Spock'] = {Rock: true, Paper: false, Scissors: true, Lizard: false, Spock: nil}
+    result['Rock'] = { Rock: nil, Paper: false, Scissors: true, Lizard: true, Spock: false }
+    result['Paper'] = { Rock: true, Paper: nil, Scissors: false, Lizard: false, Spock: true }
+    result['Scissors'] = { Rock: false, Paper: true, Scissors: nil, Lizard: true, Spock: false }
+    result['Lizard'] = { Rock: false, Paper: true, Scissors: false, Lizard: nil, Spock: true }
+    result['Spock'] = { Rock: true, Paper: false, Scissors: true, Lizard: false, Spock: nil }
 
     if result[@choice][@computer_choice] == true
       @match_result = 'won'
+      session[:result_message] = 'You won!'
     elsif result[@choice][@computer_choice] == false
       @match_result = 'lost'
-    else result[@choice][@computer_choice] == nil
-      @match_result = 'drew'
+      session[:result_message] = 'You lost!'
+    elsif result[@choice][@computer_choice].nil?
+      @match_result = 'draw'
+      session[:result_message] = 'You draw!'
     end
 
-    erb :result
+    session[:player1_choice] = @choice
+    session[:player2_choice] = @computer_choice
+
+    redirect('/game')
   end
 end
