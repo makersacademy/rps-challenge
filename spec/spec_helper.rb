2964fbaeaeb2e1b9225812e3a1dd231a28f79ac0
Peter Johnstone <peter.wdj@gmail.com>
2017-11-13 08:55:39 +0000

    Moved simplecov to top of spec_helper per instructions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
+require 'simplecov'
+require 'simplecov-console'
+
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
-require 'simplecov'
-require 'simplecov-console'
-
 require 'features/web_helpers'
 
 Capybara.app = RPS
@@ -13,7 +13,7 @@ Capybara.app = RPS
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
