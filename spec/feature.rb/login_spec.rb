f95314a54c35de3fb656a0c6f3123d190d102826
umairb1 <umairbashir95@hotmail.com>
2017-12-08 23:31:31 +0000

    Completed app setup and created login spec file

---
+++ spec/feature.rb/login_spec.rb
@@ -0,0 +1,5 @@
+feature 'login' do
+  scenario 'it should allow players to enter names before playing the game' do
+    visit('/')
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,21 @@
+require './app'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
 RSpec.configure do |config|
+  ENV['RACK_ENV'] = 'test'
+  set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+
+  Capybara.app = RPS
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
