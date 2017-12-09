8f6d6ef596f98e5e07a62e40c608367e372e68be
Lan Pham <lanpham312@gmail.com>
2017-12-09 22:50:50 +0000

    amending to 1 player vs Computer

--- app.rb
+++ app.rb
@@ -2,13 +2,19 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
     erb :play
   end
 
--- spec/features/entering_names_spec.rb
+++ spec/features/entering_names_spec.rb
@@ -1,9 +1,8 @@
 feature 'Entering names' do
   scenario "players can enter their names" do
     visit('/')
-    fill_in :player_1_name, with: 'Lan'
-    fill_in :player_2_name, with: 'Albion'
+    fill_in :player_name, with: 'Lan'
     click_button 'Submit'
-    expect(page).to have_content 'Lan vs. Albion'
+    expect(page).to have_content 'Lan vs. Computer'
   end
 end
