25b30508e9145da193da774f1559819667873d30
Eleanor Wintram <eleanorwintram@gmail.com>
2017-12-09 18:47:02 +0000

    changes choice submission to radio options

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -2,8 +2,6 @@ feature "play buttons" do
   scenario "player is able to select one button from the three options" do
     sign_in_and_play
 
-    expect(page.find_button("Rock").visible?).to be true
-    expect(page.find_button("Paper").visible?).to be true
-    expect(page.find_button("Scissors").visible?).to be true
+    expect(page).to have_content("Rock Paper Scissors")
   end
 end
