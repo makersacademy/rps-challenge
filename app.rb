86f64f1cfee219c5492b1d9cd53e3d93c1846c2d
James Lizamore <jwlizamore@gmail.com>
2017-12-10 14:17:34 +0000

    Moved game creation logic to play post

--- app.rb
+++ app.rb
@@ -11,15 +11,20 @@ class RPS < Sinatra::Base
   post '/play' do
     session[:p1_name] = params[:p1_name]
     session[:p2_name] = params[:p2_name]
+    player_1 = Player.new(params[:p1_name])
+    player_2 = session[:p2_name].nil? ? nil : Player.new(params[:p2_name])
+    Game.create_new({player_1: player_1,
+                     player_2: player_2})
     redirect '/play'
   end
 
   get '/play' do
     @p1_name, @p2_name = session[:p1_name], session[:p2_name]
-    player_1 = Player.new(@p1_name)
-    player_2 = session[:p2_name].nil? ? nil : Player.new(@p2_name)
-    game = Game.new({player_1: player_1,
-                     player_2: player_2})
+    # player_1 = Player.new(@p1_name)
+    # player_2 = session[:p2_name].nil? ? nil : Player.new(@p2_name)
+    # Game.create_new({player_1: player_1,
+    #                  player_2: player_2})
+    p Game.instance
     erb(:play)
   end
 end
