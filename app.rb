b2acfe474245642e84f7754571a7292f7d8c37f9
Keran <kerbraich@gmail.com>
2017-12-10 18:39:31 +0000

    add winner page and class instance variable

--- app.rb
+++ app.rb
@@ -1,13 +1,24 @@
 require 'sinatra/base'
+require './lib/game.rb'
 
 class RPS < Sinatra::Base
+
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb(:index)
   end
 
   post '/challenge' do
-    @player_name = params[:player_name]
+    @game = Game.create(params[:player_name])
     erb(:play)
+  end
+
+  post '/winner' do
+    @game.result(params[:weapon_choice])
+    erb(:result)
   end
 
   run! if app_file == $0
