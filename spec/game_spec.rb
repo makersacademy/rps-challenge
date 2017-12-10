99a660eda5f8a7fdeaeb1f6ff10f2aec4aaa5005
Calum-W <cal7907@hotmail.co.uk>
2017-12-10 20:35:08 +0000

    Now passes all tests and works in rackup

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,7 @@ require 'game'
 
 describe Game do
   let(:game) { Game.new("John") }
+  let(:game2) { Game.new("Steve", "Roy") }
   describe '#fight_ralph' do
     it 'calls a tie when both players have the same weapon' do
       game.P1.choose(:rock)
@@ -14,6 +15,24 @@ describe Game do
     it 'sometimes calls a win for the player' do
       game.P1.choose(:scissors)
       expect(game.fight_ralph(:paper)).to eq "You defeated Ralph! For now..."
+    end
+  end
+
+  describe '#multiplayer_fight' do
+    it 'sometimes calls a win for player 1' do
+      game2.P1.choose(:scissors)
+      game2.P2.choose(:paper)
+      expect(game2.multiplayer_fight).to eq "Steve wins!"
+    end
+    it 'sometimes calls a win for player 2' do
+      game2.P1.choose(:paper)
+      game2.P2.choose(:scissors)
+      expect(game2.multiplayer_fight).to eq "Roy wins!"
+    end
+    it 'sometimes calls a win for player 1' do
+      game2.P1.choose(:paper)
+      game2.P2.choose(:paper)
+      expect(game2.multiplayer_fight).to eq "It's a tie!"
     end
   end
 end
