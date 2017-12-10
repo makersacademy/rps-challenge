80f535a158cafd822dac4e68a93d99186700ed3f
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-10 19:13:08 +0000

    Feature Test - user enters their name and can choose what they want to use - passed

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require "sinatra/base"
 
 
 class RPS < Sinatra::Base
+enable :sessions
+set :session_secret, "sessionkey"
 
   get "/" do
     erb(:index)
   end
 
   post "/name" do
-    "name entered"
+    session[:player_one_name] = params[:player_one_name]
+    redirect "/play"
+
+  end
+
+  get "/play" do
+    @player_one_name = session[:player_one_name]
+    erb(:play)
   end
 
 
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -8,20 +8,21 @@ feature "Signing Up" do
     visit ("/")
     expect(page).to have_field ("player_one_name")
   end
+end
 
+feature "User enters name and chooses hand" do
   scenario "Player has entered their name" do
     visit ("/")
     fill_in :player_one_name, with: "Nikolaos"
     click_button ("Start the game")
-    expect(page).to have_text("Welcome Nikolaos")
-
+    expect(page).to have_text "Welcome Nikolaos!"
   end
 
   scenario "Player needs to choose their hand" do
     visit ("/")
     fill_in :player_one_name, with: "Nikolaos"
     click_button ("Start the game")
-    expect(page).to have_text("Choose what you wish to use for this round:")
+    expect(page).to have_text ("Please select your hand.")
   end
 
 end
