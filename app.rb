82b2a7d8c8511b2e27fb7eec5630b6441e489df0
James Lizamore <jwlizamore@gmail.com>
2017-12-10 12:54:45 +0000

    Added displaying of player names

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
   get '/' do
-    'Hi'
+    erb(:index)
+  end
+
+  post '/play' do
+    session[:p1_name] = params[:p1_name]
+    session[:p2_name] = params[:p2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    p session
+    @p1_name = session[:p1_name]
+    @p2_name = session[:p2_name]
+    @test = "This is a test string"
+    erb(:play)
   end
 end
---
+++ spec/features/display_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Displays names as appropriate' do
+  scenario 'displays both names when two players are playing' do
+    visit '/'
+    fill_in('p1_name', with: "James")
+    fill_in('p2_name', with: "Mary")
+    click_on "Submit"
+    expect(page).to have_content "James Mary"
+  end
+end
