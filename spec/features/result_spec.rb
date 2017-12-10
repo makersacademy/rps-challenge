525a0b4bfc33a1f18c3414557befc409b022d8e6
Eleanor Wintram <eleanorwintram@gmail.com>
2017-12-10 17:37:36 +0000

    updates web_helper.rb

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -2,8 +2,7 @@ feature "results" do
   scenario "the player's choice of rock is displayed on the results page" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:rock)
     sign_in_and_play
-    choose("Rock")
-    click_button("Go")
+    player_choose_rock
 
     expect(page).to have_content "Ellie chose rock"
   end
@@ -11,8 +10,7 @@ feature "results" do
   scenario "the player's choice of paper is displayed on the results page" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:rock)
     sign_in_and_play
-    choose("Paper")
-    click_button("Go")
+    player_choose_paper
 
     expect(page).to have_content "Ellie chose paper"
   end
@@ -20,8 +18,7 @@ feature "results" do
   scenario "the player's choice of scissors is displayed on the results page" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:rock)
     sign_in_and_play
-    choose("Scissors")
-    click_button("Go")
+    player_choose_scissors
 
     expect(page).to have_content "Ellie chose scissors"
   end
@@ -29,8 +26,7 @@ feature "results" do
   scenario "the computer's choice of scissors is displayed on the results page" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:rock)
     sign_in_and_play
-    choose("Rock")
-    click_button("Go")
+    player_choose_rock
 
     expect(page).to have_content "Computer chose rock"
   end
@@ -38,8 +34,7 @@ feature "results" do
   scenario "the computer's choice of scissors is displayed on the results page" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:paper)
     sign_in_and_play
-    choose("Rock")
-    click_button("Go")
+    player_choose_rock
 
     expect(page).to have_content "Computer chose paper"
   end
@@ -47,8 +42,7 @@ feature "results" do
   scenario "the computer's choice of scissors is displayed on the results page" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:scissors)
     sign_in_and_play
-    choose("Rock")
-    click_button("Go")
+    player_choose_rock
 
     expect(page).to have_content "Computer chose scissors"
   end
@@ -56,8 +50,7 @@ feature "results" do
   scenario "the computer's name is posted to screen if the computer wins" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:scissors)
     sign_in_and_play
-    choose("Paper")
-    click_button("Go")
+    player_choose_paper
 
     expect(page).to have_content "Computer won!"
   end
@@ -65,8 +58,7 @@ feature "results" do
   scenario "the computer's name is posted to screen if the computer wins" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:scissors)
     sign_in_and_play
-    choose("Rock")
-    click_button("Go")
+    player_choose_rock
 
     expect(page).to have_content "Ellie won!"
   end
@@ -74,8 +66,7 @@ feature "results" do
   scenario "the computer's name is posted to screen if the computer wins" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:scissors)
     sign_in_and_play
-    choose("Scissors")
-    click_button("Go")
+    player_choose_scissors
 
     expect(page).to have_content "Draw!"
   end
@@ -83,8 +74,7 @@ feature "results" do
   scenario "the 'Play again' and 'New player' buttons are visible" do
     allow_any_instance_of(Computer).to receive(:choice).and_return(:scissors)
     sign_in_and_play
-    choose("Scissors")
-    click_button("Go")
+    player_choose_scissors
 
     expect(page).to have_button "Play again"
     expect(page).to have_button "New player"
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -10,3 +10,18 @@ def sign_in_and_play_no_name
 
   click_button "Play"
 end
+
+def player_choose_rock
+  choose("Rock")
+  click_button("Go")
+end
+
+def player_choose_paper
+  choose("Paper")
+  click_button("Go")
+end
+
+def player_choose_scissors
+  choose("Scissors")
+  click_button("Go")
+end
