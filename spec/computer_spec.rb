2580238c805b532f690e7881adf2b70db87aba26
Keran <kerbraich@gmail.com>
2017-12-10 13:58:23 +0000

    add unit test for displaying correct computer weapon choice

---
+++ spec/computer_spec.rb
@@ -0,0 +1,19 @@
+require 'computer'
+
+describe Computer do
+  subject(:computer) { described_class.new }
+  context '#weapon' do
+    it 'should display rock when rock is chosen' do
+      allow(computer).to receive(:weapon).and_return(:rock)
+      expect(computer.weapon).to eq(:rock)
+    end
+    it 'should display paper when paper is chosen' do
+      allow(computer).to receive(:weapon).and_return(:paper)
+      expect(computer.weapon).to eq(:paper)
+    end
+    it 'should display scissors when scissors is chosen' do
+      allow(computer).to receive(:weapon).and_return(:scissors)
+      expect(computer.weapon).to eq(:scissors)
+    end
+  end
+end
