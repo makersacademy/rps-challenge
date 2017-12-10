97a14ae2aeeb07d6a8c437290910d7ca1ecc02db
Calum-W <cal7907@hotmail.co.uk>
2017-12-10 17:31:48 +0000

    Adds a multiplayer name entry page

--- app.rb
+++ app.rb
@@ -14,22 +14,32 @@ class Battle < Sinatra::Base
      erb(:p1_name_entry)
   end
 
+  post '/p1_weapon_select' do
+    @current_game = Game.create(params[:Challenger])
+    erb(:p1_weapon_select)
+  end
+
+  post '/fight_ralph' do
+    @current_game = Game.instance
+    @current_game.P1.choose(params[:weapon])
+    @result = @current_game.fight_ralph
+    erb(:fight_ralph)
+  end
+
+  #Multiplayer pages
+
   get '/multiplayer_name_entry' do
     erb(:multiplayer_name_entry)
   end
 
-  post '/p1_weapon_select' do
-    @current_game = Game.create(params[:Challenger])
-    erb(:p1_weapon_select)
+  post 'multiplayer_weapon_p1' do
+    @current_game = Game.create(params[:Player1], params[:Player2])
+    erb(:multiplayer_weapon_p1)
   end
 
   before do
     @current_game = Game.instance
   end
 
-  post '/fight_ralph' do
-    @current_game.P1.choose(params[:weapon])
-    @result = @current_game.fight_ralph
-    erb(:fight_ralph)
-  end
+
 end
--- spec/enter_names_spec.rb
+++ spec/enter_names_spec.rb
@@ -8,4 +8,20 @@ feature 'name entry' do
     click_button "Battle!"
     expect(page).to have_content "Welcome, young Gerald!"
   end
+  scenario 'Player names appear on weapon selection' do
+    visit '/'
+    click_button "2 Player"
+    fill_in 'Player1', with: "Maude"
+    fill_in 'Player2', with: "Dorothy"
+    click_button "Choose weapons"
+    expect(page).to have_content "WEAPONS"
+  end
+  scenario 'Player names appear on weapon selection' do
+    visit '/'
+    click_button "2 Player"
+    fill_in 'Player1', with: "Maude"
+    fill_in 'Player2', with: "Dorothy"
+    click_button "Choose weapons"
+    expect(page).to have_content "You choose first, Maude"
+  end
 end
