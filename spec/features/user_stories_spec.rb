f6845a9686258025d1aea4028b1f7d862bae5a8c
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-10 19:24:21 +0000

    Feature Test - has three option in the form of buttons to choose from  - fail

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,7 +1,7 @@
 feature "Signing Up" do
   scenario "Player is greeted to the game" do
     visit ("/")
-    expect(page).to have_text ("Welcome to Rock-Paper-Scissor Online Game")
+    expect(page).to have_text ("Welcome to Rock-Paper-Scissors Online Game")
   end
 
   scenario "Player needs to enter their name" do
@@ -19,6 +19,24 @@ feature "User enters name and chooses hand" do
   scenario "Player needs to choose their hand" do
     log_in
     expect(page).to have_text ("Please select your hand.")
+  end
+
+  context "User has 3 options" do
+    scenario "use can choose Rock" do
+      log_in
+      expect(page).to have_button('rock')
+    end
+
+    scenario "use can choose Paper" do
+      log_in
+      expect(page).to have_button('paper')
+    end
+
+    scenario "use can choose Scissors" do
+      log_in
+      expect(page).to have_button('scissor')
+    end
+
   end
 
 end
