66ac2a074af7197ede18471ad67a80b5467dd65c
Samuel Johnson <samjohnson@Samuels-MacBook-Pro.local>
2017-12-11 10:01:44 +0000

    Added methods to see random weapon for computer

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,14 +1,21 @@
 
 class Computer
 
-  attr_reader :comp_weapon
-
-  def initialize
-    chosen_weapon
-  end
 
   def chosen_weapon
     @comp_weapon = ['rock', 'paper', 'scissors'].sample
   end
+
+  def comp_rock?
+    @comp_weapon == 'rock'
+  end
+
+  def comp_paper?
+    @comp_weapon = 'paper'
+  end
+
+  def comp_scissors?
+    @comp_weapon = 'scissors'
+  end 
 
 end
