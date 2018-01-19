86c5b8221737236446ddcf0f39bd0d568fb42531
ealitten <ea.litten@gmail.com>
2018-01-19 14:06:27 +0000

    Add player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ describe Player do
 
   describe "#initialize" do
     it "should return player_1 name as Ed" do
-      expect(player_1.name).to eq("Ed")
+      expect(player.name).to eq("Ed")
     end
   end
 end
