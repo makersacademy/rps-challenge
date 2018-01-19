ea37e883cba07f784357f7cb95cd954d7d3569a4
ealitten <ea.litten@gmail.com>
2018-01-19 14:58:23 +0000

    Typo in test

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -39,6 +39,6 @@ feature 'Determining winner' do
     allow_any_instance_of(Computer).to receive(:choose_move).and_return(:scissors)
     enter_name_and_submit 
     click_button 'Rock'
-    expect(page).to have_content("Computer won!")
+    expect(page).to have_content("Player won!")
   end
 end
