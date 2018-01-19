4e57e89dd919add71f92dbd0b3fbe62f3e0e63be
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-19 19:41:28 +0000

    requiring gems in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+ENV['RACK_ENV']="test"
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'rspec'
+
+Capybara.app = Rps
+
+
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
