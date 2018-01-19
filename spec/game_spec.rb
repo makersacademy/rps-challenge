b6dbe472f1b8e1f644802b9e848b8a3c5a997f34
ealitten <ea.litten@gmail.com>
2018-01-19 14:38:52 +0000

    Add test for new Game method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,5 +10,12 @@ describe Game do
       expect(game.player).to eq player
     end
   end
+
+  describe "#make_move" do
+    it 'stores player move' do
+      game.make_move("rock")
+      expect(game.player_move).to eq "rock"
+    end
+  end
 end
 
