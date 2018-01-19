1452c92616fc8a8035596ee3a9fb153346173f2e
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-19 21:40:45 +0000

    rubocop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,12 @@
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
-ENV['RACK_ENV']="test"
+ENV['RACK_ENV'] = "test"
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'rspec'
 
 Capybara.app = Rps
-
-
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
