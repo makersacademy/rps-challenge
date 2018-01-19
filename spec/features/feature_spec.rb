eb8200cc7a8c51fdf7520ce736bfb16eb8bb599a
ealitten <ea.litten@gmail.com>
2018-01-19 13:54:54 +0000

    Add test for options presenter to player

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -6,3 +6,14 @@ feature 'Sign-in' do
     expect(page).to have_content("Welcome to Rock, Paper, Scissors Ed!")
   end
 end
+
+feature 'Making a selection' do
+  scenario 'Player is presented with a choice of moves' do
+    visit('/')
+    fill_in 'player_name', with: "Ed"
+    click_button 'Submit'
+    expect(page).to have_button("Rock")
+    expect(page).to have_button("Paper")
+    expect(page).to have_button("Scissors")
+  end
+end
