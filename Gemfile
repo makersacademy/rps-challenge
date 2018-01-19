8cfcdaf8f45300f38f2609610adf8ae15a4425e5
ealitten <ea.litten@gmail.com>
2018-01-19 11:55:30 +0000

    Initial commit

--- Gemfile
+++ Gemfile
@@ -5,10 +5,12 @@ ruby '2.4.0'
 gem 'rake'
 gem 'sinatra'
 
+
 group :test do
   gem 'capybara'
   gem 'rspec'
   gem 'rubocop'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'pry'
 end
---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require './lib/game'
+require 'pry'
+
+class RockPaperScissors < Sinatra::Base
+
+  enable :sessions
+
+   # start the server if ruby file executed directly
+   run! if app_file == $0
+
+   get '/' do
+    erb :index
+   end
+
+end
