b37af121604653ff9e613b7c70825152b5dde2c7
Tom Grand <thomas.a.grand@gmail.com>
2018-01-20 20:37:35 +0000

    add tests to make sure winner gives right result

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,52 @@ require "game"
 
 describe Game do
 
+  let(:game) { described_class.new("Derek") }
+
   describe "#initialize" do
     it "should store a players name" do
-      game = Game.new("Derek")
       expect(game.player).to eq "Derek"
-      p Game.current
+    end
+  end
+
+  # describe "#WEAPONS" do
+  #   it "should include rock" do
+  #     expect(Game::WEAPONS).to include("Rock")
+  #   end
+  #   it "should include rock" do
+  #     expect(Game::WEAPONS).to include("Paper")
+  #   end
+  #   it "should include rock" do
+  #     expect(Game::WEAPONS).to include("Rock")
+  #   end
+  # end
+  describe "#winnner" do
+    it "should return draw when paper and paper is given" do
+      expect(game.winner("paper", "paper")).to eq "Draw"
+    end
+    it "should return draw when paper and paper is given" do
+      expect(game.winner("scissors", "scissors")).to eq "Draw"
+    end
+    it "should return draw when scissors and scissors is given" do
+      expect(game.winner("scissors", "scissors")).to eq "Draw"
+    end
+    it "should return rock when rock and scissors is given" do
+      expect(game.winner("rock", "scissors")).to eq "rock"
+    end
+    it "should return rock when scissors and rock is given" do
+      expect(game.winner("scissors", "rock")).to eq "rock"
+    end
+    it "should return paper when paper and rock is given" do
+      expect(game.winner("paper", "rock")).to eq "paper"
+    end
+    it "should return rock when rock and paper is given" do
+      expect(game.winner("rock", "paper")).to eq "paper"
+    end
+    it "should return scissors when paper and scissors is given" do
+      expect(game.winner("paper", "scissors")).to eq "scissors"
+    end
+    it "should return scissors when scissors and paper is given" do
+      expect(game.winner("paper", "scissors")).to eq "scissors"
     end
   end
 end
