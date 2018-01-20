8b5954dd525a146738a392f68d6e4a15034b53ec
Leigh-ann <leighann.gant@gmail.com>
2018-01-20 13:06:22 +0000

    Web helpers updated

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -3,3 +3,8 @@ def enter_name_and_confirm
   fill_in 'player_name', :with => 'Leigh-ann'
   click_button "Let's go"
 end
+
+def player_weapon_choice(weapon)
+  enter_name_and_confirm
+  select weapon, :from => "player_weapon"
+end
