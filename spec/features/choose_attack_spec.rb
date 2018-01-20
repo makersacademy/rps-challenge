7f93f00cce33f5a5665da8f18ec5d4e13c271deb
Tom Grand <thomas.a.grand@gmail.com>
2018-01-20 15:21:46 +0000

    tidy up attack_spec

--- spec/features/choose_attack_spec.rb
+++ spec/features/choose_attack_spec.rb
@@ -1,27 +1,27 @@
 
 feature "after signing in, you choose attack" do
 
-   def make_attack_and_check_it_matched_message(attack)
-     choose(attack)
-     click_button "Attack your opponent"
-     check_its_not_error_page
-     expect(page).to have_content("Derek chose #{attack}")
-   end
+  def make_attack_and_check_it_matched_message(attack)
+    choose(attack)
+    click_button "Attack your opponent"
+    check_its_not_error_page
+    expect(page).to have_content("Derek chose #{attack}")
+  end
 
-   background do
-     login_with_derek
-    end
+  background do
+    login_with_derek
+  end
 
   scenario "after choosing rock it should confirm your choice" do
-      make_attack_and_check_it_matched_message("rock")
+    make_attack_and_check_it_matched_message("rock")
   end
 
   scenario "after choosing paper it should confirm your choice" do
-      make_attack_and_check_it_matched_message("paper")
+    make_attack_and_check_it_matched_message("paper")
   end
 
   scenario "after choosing scissors it should confirm your choice" do
-      make_attack_and_check_it_matched_message("scissors")
+    make_attack_and_check_it_matched_message("scissors")
   end
 
   scenario "rock is default" do
@@ -29,5 +29,4 @@ feature "after signing in, you choose attack" do
     check_its_not_error_page
     expect(page).to have_content("Derek chose rock")
   end
-
 end
