0fc4f60e8ba53ab7f53a3acc055ba85a8a1d2c36
Anjana <anjana.lpn@gmail.com>
2018-01-20 12:32:13 +0000

    refactor player instructions

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -2,9 +2,9 @@ feature 'play the game' do
 
   scenario 'the choices (rock, paper, scissors) are presented' do
     register_and_play
-    expect(page).to have_content 'Rock'
-    expect(page).to have_content 'Paper'
-    expect(page).to have_content 'Scissors'
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Paper'
+    expect(page).to have_button 'Scissors'
   end
 
 end
