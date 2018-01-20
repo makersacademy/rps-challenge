906a10b1f538d7a1db478fb17e43b860ed7c4475
Tom Grand <thomas.a.grand@gmail.com>
2018-01-20 19:16:30 +0000

    add tests for class methods for game

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,13 @@
 class Game
 
+  def self.create(player)
+    @running = Game.new(player)
+  end
+
+  def self.current
+    @running
+  end
+
   attr_reader :player
 
   def initialize(player)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,3 +9,20 @@ describe Game do
     end
   end
 end
+
+describe Game do
+
+  describe "#current" do
+    it "start with current game being nil" do
+      expect(Game.current).to be nil
+    end
+  end
+
+  describe "#create" do
+    it "should create a new game" do
+      Game.create("Derek")
+      expect(Game.current).to be_a(Game)
+    end
+  end
+
+end
