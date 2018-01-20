688115b0d8563392532c5e230fdbef4c1e367190
Tom Grand <thomas.a.grand@gmail.com>
2018-01-20 20:37:18 +0000

    add winner method to pick rock paer or scissors

--- lib/game.rb
+++ lib/game.rb
@@ -8,10 +8,18 @@ class Game
     @running
   end
 
+  WEAPONS = ["Rock", "Paper", "Scissors"]
+
   attr_reader :player
 
   def initialize(player)
     @player = player
+  end
+
+  def winner(answer_1, answer_2)
+    return "Draw" if answer_1 == answer_2
+    results = { "rock" => "paper", "paper" => "scissors", "scissors" => "rock" }
+    results[answer_1] == answer_2 ? answer_2 : answer_1
   end
 
 end
