33347575d053a6bbe93ba6d023dba315777f415d
Leigh-ann <leighann.gant@gmail.com>
2018-01-20 16:26:38 +0000

    Web helpers updated

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -17,3 +17,10 @@ def multi_player_names_and_confirm
   fill_in 'player_2_name', :with => 'Andy'
   click_button "Let's go"
 end
+
+def multi_player_pick_weapons_and_confirm(weapon_1, weapon_2)
+  multi_player_names_and_confirm
+  select weapon_1, :from => "player_1_weapon"
+  select weapon_2, :from => "player_2_weapon"
+  click_button "Lets get started!"
+end
