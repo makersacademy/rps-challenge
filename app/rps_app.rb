7766e8099b75c16d56180f9ae24d33c9ebfbae19
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-20 12:51:05 +0000

    homepage feature test green

--- app/rps_app.rb
+++ app/rps_app.rb
@@ -1,11 +1,14 @@
-require 'Sinatra/base'
+require 'sinatra/base'
 
 class RPSWeb < Sinatra::Base
   enable :sessions
 
   get '/' do
     erb :index
+  end
 
+  get '/play' do
+    erb :play
   end
 
   # start the server if ruby file executed directly
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,5 +1,5 @@
 def sign_in_and_play
   visit '/'
-  fill_in :player_1, with: 'Felix'
+  fill_in 'player_1', with: 'Felix'
   click_button 'Submit'
 end
