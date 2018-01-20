5196f141fb1ebc413d3bfd2964bd9ac768abf9ac
Leigh-ann <leighann.gant@gmail.com>
2018-01-20 17:02:50 +0000

    Test added to confirm no list option for 2nd player -  single player game

--- spec/features/computer_player_plays_spec.rb
+++ spec/features/computer_player_plays_spec.rb
@@ -13,3 +13,10 @@ feature 'Computer choses random weapon choice' do
     expect(page).to have_content "Computer chose ROCK!"
   end
 end
+feature 'When playing against computer - no option for player_2 weapon choice' do
+  scenario 'One player signs in, game defaults to player vs computer' do
+    enter_name_and_confirm
+    expect(page).to have_select("player_1_weapon", :with_options => ["Rock", "Scissors", "Paper","Spock","Lizard"])
+		expect(page).not_to have_select("player_2_weapon", :with_options => ["Rock", "Scissors", "Paper","Spock","Lizard"])
+	end
+end
