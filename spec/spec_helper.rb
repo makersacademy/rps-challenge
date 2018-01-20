7cfca9f1385693ed133ed0b22fbfb3c94c204d22
Ellie <elliewem@gmail.com>
2018-01-20 15:37:27 +0000

    Update ruby version in gemfile, update spechelper to change RACK_ENV

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'capybara'
+require 'rspec'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = RockPaperScissors
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
