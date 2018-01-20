261966ee7dfd8a5a84ef5eeb59e3b255f6a9b0f1
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-20 16:22:22 +0000

    buttons for choices

--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -14,29 +14,65 @@ feature 'Testing name is assigned' do
   end
 end
 
-# feature 'moves are available to make' do
-#   scenario 'move options include rock' do
-#     visit('/')
-#     fill_in 'player_name', with: 'Jennifer'
-#     click_button "Let's play!!"
-#     expect(page).to have_button "ROCK"
-#   end
-# end
-#
-# feature 'moves are available to make' do
-#   scenario 'move options include paper' do
-#     visit('/')
-#     fill_in 'player_name', with: 'Jennifer'
-#     click_button "Let's play!!"
-#     expect(page).to have_button "PAPER"
-#   end
-# end
-#
-# feature 'moves are available to make' do
-#   scenario 'move options include scissors' do
-#     visit('/')
-#     fill_in 'player_name', with: 'Jennifer'
-#     click_button "Let's play!!"
-#     expect(page).to have_button "SCISSORS"
-#   end
-# end
+feature 'Player Wins with Rock vs Scissors' do
+  scenario 'page shows win for player' do
+    visit('/')
+    fill_in 'player_name', with: 'Jennifer'
+    click_button "Let's play!!"
+    fill_in 'player_choice', with: 'ROCK'
+    allow_any_instance_of(Game).to receive(:computer_chooses).and_return("SCISSORS")
+    click_button "GO"
+    expect(page).to have_content "You chose:  ROCK The computer chose:  SCISSORS YOU WON!"
+  end
+end
+
+feature 'Player Loses with Rock vs Paper' do
+  scenario 'page shows win for player' do
+    visit('/')
+    fill_in 'player_name', with: 'Jennifer'
+    click_button "Let's play!!"
+    fill_in 'player_choice', with: 'ROCK'
+    allow_any_instance_of(Game).to receive(:computer_chooses).and_return("PAPER")
+    click_button "GO"
+    expect(page).to have_content "You chose:  ROCK The computer chose:  PAPER YOU LOST!"
+  end
+end
+
+feature 'Player Draws with Rock vs Rock' do
+  scenario 'page shows win for player' do
+    visit('/')
+    fill_in 'player_name', with: 'Jennifer'
+    click_button "Let's play!!"
+    fill_in 'player_choice', with: 'ROCK'
+    allow_any_instance_of(Game).to receive(:computer_chooses).and_return("ROCK")
+    click_button "GO"
+    expect(page).to have_content "You chose:  ROCK The computer chose:  ROCK YOU DREW!"
+  end
+end
+
+feature 'moves are available to make' do
+  scenario 'move options include rock' do
+    visit('/')
+    fill_in 'player_name', with: 'Jennifer'
+    click_button "Let's play!!"
+    expect(page).to have_button "ROCK"
+  end
+end
+
+feature 'moves are available to make' do
+  scenario 'move options include paper' do
+    visit('/')
+    fill_in 'player_name', with: 'Jennifer'
+    click_button "Let's play!!"
+    expect(page).to have_button "PAPER"
+  end
+end
+
+feature 'moves are available to make' do
+  scenario 'move options include scissors' do
+    visit('/')
+    fill_in 'player_name', with: 'Jennifer'
+    click_button "Let's play!!"
+    expect(page).to have_button "SCISSORS"
+  end
+end
