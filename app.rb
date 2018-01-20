0f29d7add8052243304d8a99a5f5be710a4711c9
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-20 16:21:28 +0000

    attribute reader

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ class Rps < Sinatra::Base
 
   enable :sessions
 
+  attr_reader :player_choice, :computer_choice
+
   run! if app_file == $0
 
   get '/' do
@@ -18,15 +20,40 @@ class Rps < Sinatra::Base
 
   get '/play' do
     @player = session[:player_name]
+    $game = Game.new
     erb(:play)
   end
 
   post '/selection' do
-    session[:selection] = params[:selection]
-    computer_choice = ["ROCK","PAPER","SCISSORS"].sample
-    
-    # redirect 'win if'
-    # redirect ''
+    @game = $game
+    p @game
+    session[:player_choice] = params[:player_choice]
+    session[:computer_choice] = @game.computer_chooses
+    @player_choice = session[:player_choice]
+    @computer_choice = session[:computer_choice]
+    result = @game.rps(@player_choice,@computer_choice)
+    p @computer_choice
+    redirect '/win' if result == "Player wins"
+    redirect '/lose' if result == "Computer wins"
+    redirect '/draw' if result == "DRAW"
+  end
+
+  get '/win' do
+    @player_choice = session[:player_choice]
+    @computer_choice = session[:computer_choice]
+    erb(:win)
+  end
+
+  get '/lose' do
+    @player_choice = session[:player_choice]
+    @computer_choice = session[:computer_choice]
+    erb(:lose)
+  end
+
+  get '/draw' do
+    @player_choice = session[:player_choice]
+    @computer_choice = session[:computer_choice]
+    erb(:draw)
   end
 
 end
