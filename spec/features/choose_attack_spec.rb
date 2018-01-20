e1ac82e2d3db3bf34f11455bf798c8468bb6ce08
Tom Grand <thomas.a.grand@gmail.com>
2018-01-20 14:54:10 +0000

    add paper button

--- spec/features/choose_attack_spec.rb
+++ spec/features/choose_attack_spec.rb
@@ -19,4 +19,24 @@ feature "after signing in, you choose attack" do
     expect(page).to have_content("Derek chose rock")
   end
 
+  scenario "after choosing paper it should confirm your choice" do
+    visit "/"
+    fill_in "Enter name", with: "Derek"
+    click_button "Begin..."
+    choose("paper")
+    click_button "Ready to go"
+    expect(page).not_to have_content("AABBYkjahskdh987289")
+    expect(page).to have_content("Derek chose paper")
+  end
+
+  scenario "after choosing scissors it should confirm your choice" do
+    visit "/"
+    fill_in "Enter name", with: "Derek"
+    click_button "Begin..."
+    choose("scissors")
+    click_button "Ready to go"
+    expect(page).not_to have_content("AABBYkjahskdh987289")
+    expect(page).to have_content("Derek chose scissors")
+  end
+
 end
