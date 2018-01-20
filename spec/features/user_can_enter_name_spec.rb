d11cf0dc326c95714b38716028c2d199ebcddd0f
Leigh-ann <leighann.gant@gmail.com>
2018-01-20 10:24:42 +0000

    Feature tests created for entering name

---
+++ spec/features/user_can_enter_name_spec.rb
@@ -0,0 +1,12 @@
+feature "Player can enter name " do
+  scenario "Page confirms instructions to enter player name" do
+    visit('/')
+    expect(page).to have_content 'Welcome to Rock!Paper!Scissors!'
+    expect(page).to have_content 'Please enter your name to play'
+  end
+  scenario "player can enter name and submit" do
+    visit('/')
+    fill_in 'player_name', :with => 'Leigh-ann'
+    expect(page).to have_content 'Leigh-ann, lets play Rock!Paper!Scissors!'
+  end
+end
