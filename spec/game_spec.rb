3ee090a69050f3737ea9835b8200aa78f13c2ff2
Alexy <alexandra.fina@yahoo.com>
2018-01-21 12:15:07 +0000

    Adding Game class and test to return the player and computer's names

---
+++ spec/game_spec.rb
@@ -0,0 +1,9 @@
+require './lib/game.rb'
+
+describe Game do
+  subject(:game) {described_class.new('Tiana', 'Computer') }
+  it 'returns the game with a player and computer\'s names' do
+    expect(game.player).to eq('Tiana')
+    expect(game.computer).to eq('Computer')
+  end
+end
