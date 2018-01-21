62d3b8e82a0c458541dcf5db53b8ca7d3e2d4cb4
Alexy <alexandra.fina@yahoo.com>
2018-01-21 20:05:44 +0000

    Adding feature tests for checking when Paper and Scissors has been clicked by the player

--- spec/features/check_result_options_spec.rb
+++ spec/features/check_result_options_spec.rb
@@ -8,4 +8,22 @@ feature 'Checking the option chosen' do
     expect(page).to have_content 'Tiana chose Rock!'
   end
 
+  scenario 'Check for Paper button' do
+    visit('/')
+    fill_in :player_name, with: "Tiana"
+    click_button 'Let\'s Play!'
+    expect(page).to have_content 'Tiana vs. Computer!'
+    click_button 'Paper'
+    expect(page).to have_content 'Tiana chose Paper!'
+  end
+
+  scenario 'Check for Scissors button' do
+    visit('/')
+    fill_in :player_name, with: "Tiana"
+    click_button 'Let\'s Play!'
+    expect(page).to have_content 'Tiana vs. Computer!'
+    click_button 'Scissors'
+    expect(page).to have_content 'Tiana chose Scissors!'
+  end
+
 end
