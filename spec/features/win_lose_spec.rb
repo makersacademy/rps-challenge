4943be1a6a304df12b1a746281526889bf13f1b5
Tom Grand <thomas.a.grand@gmail.com>
2018-01-21 11:25:49 +0000

    make sure you can win

--- spec/features/win_lose_spec.rb
+++ spec/features/win_lose_spec.rb
@@ -18,6 +18,20 @@ feature "you choose rock, and it gives the result.." do
     expect(page).to have_content("You lose!")
   end
 
+  scenario "win, when the game picks scissors" do
+    allow_any_instance_of(Game).to receive(:choice).and_return("scissors")
+    attack_with_rock
+    expect(page).to have_content("computer choses scissors")
+    expect(page).to have_content("You win!")
+  end
+
+  # scenario "lose, when the game picks paper" do
+  #   allow_any_instance_of(Game).to receive(:choice).and_return("rock")
+  #   attack_with_rock
+  #   expect(page).to have_content("computer choses rock")
+  #   expect(page).to have_content("You draw!")
+  # end
+
   # scenario "win, when the game picks paper" do
   #
   #   expect(page).to have_content("Derek loses!")
