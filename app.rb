4b8c5b024d6f9b00452edf560984682f111b0a72
Alexy <alexandra.fina@yahoo.com>
2018-01-21 11:28:13 +0000

    Writing feature tests for each line on the first page and feature test for player's name

--- app.rb
+++ app.rb
@@ -6,5 +6,10 @@ get '/' do
   erb :index
 end
 
+post '/name' do
+
+  erb :play
+end
+
   run! if app_file == $0
-end 
+end
--- spec/features/testing_homepage_spec.rb
+++ spec/features/testing_homepage_spec.rb
@@ -1,6 +1,17 @@
 feature 'Testing homepage content' do
-  scenario 'Run the app and check page content' do
+  scenario 'Run the app and check page content- first line' do
     visit('/')
-    expect(page).to have_content 'Testing local host connection'
-  end 
+    expect(page).to have_content 'The Rock Paper Scissors Game!'
+  end
+
+  scenario 'Run the app and check page content - second line' do
+    visit('/')
+    expect(page).to have_content 'Please submit your name to play'
+  end
+
+  scenario 'Run the app and check page content - third line' do
+    visit('/')
+    expect(page).to have_content 'Each turn you choose between Rock, Paper, Scissors!'
+  end
+
 end
