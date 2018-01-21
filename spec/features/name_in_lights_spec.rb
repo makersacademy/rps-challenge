a82edab3a817b5a1c9a0b6a4bf5e8b6284cba2cd
Alexander Sergides <alexse5121@gmail.com>
2018-01-21 16:40:22 +0000

    first page

---
+++ spec/features/name_in_lights_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'signing in a name' do
+  scenario 'sign in so i can see my name' do
+    visit '/'
+    fill_in 'name', with: 'Alex'
+    click_button 'Submit'
+    expect(page).to have_content "Alex"
+  end
+end
