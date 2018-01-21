7cc2f80985a63f043ac663329142d3720219cbfb
Tom Grand <thomas.a.grand@gmail.com>
2018-01-21 14:33:07 +0000

    refactor tests to add choices method

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,8 @@
 require "player"
 
 class Game
-  def self.create(player)
-    @running ||= Game.new(player)
+  def self.create(player_1)
+    @running ||= Game.new(player_1)
   end
 
   def self.current
@@ -10,20 +10,21 @@ class Game
   end
 
   WEAPONS = ["rock", "paper", "scissors"]
-  attr_reader :player_1
+  attr_reader :player_1, :player_2
 
-  def initialize(player)
-    @player_1 = player
+  def initialize(player_1, player_2 = Player.new("computer"))
+    @player_1 = player_1
+    @player_2 = player_2
   end
 
   def choice
     WEAPONS.sample
   end
 
-  def winner(answer_1, answer_2)
-    return "draw" if answer_1 == answer_2
+  def winner
+    return "draw" if player_1.choice == player_2.choice
     results = { "rock" => "paper", "paper" => "scissors", "scissors" => "rock" }
-    results[answer_1] == answer_2 ? "lose" : "win"
+    results[player_1.choice] == player_2.choice ? "lose" : "win"
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,21 @@ require "game"
 
 describe Game do
   let(:player) { double :player }
-  let(:game) { described_class.new(player) }
+  let(:computer) { double :player }
+  let(:game) { described_class.new(player, computer) }
+
+  def set_choice(a, b)
+    allow(player).to receive(:choice).and_return(a)
+    allow(computer).to receive(:choice).and_return(b)
+  end
 
   describe "#initialize" do
-    it "should store a player" do
+    it "should store player_1" do
       expect(game.player_1).to eq player
+    end
+
+    it "should store computer player" do
+      expect(game.player_2).to eq computer
     end
   end
 
@@ -22,32 +32,43 @@ describe Game do
     let(:draw) { "draw" }
 
     it "should return draw when paper and paper is given" do
-      expect(game.winner("paper", "paper")).to eq draw
-    end
-    it "should return draw when paper and paper is given" do
-      expect(game.winner("scissors", "scissors")).to eq draw
-    end
-    it "should return draw when scissors and scissors is given" do
-      expect(game.winner("scissors", "scissors")).to eq draw
-    end
-    it "should return Win when rock and scissors is given" do
-      expect(game.winner("rock", "scissors")).to eq win
-    end
-    it "should return Lose when scissors and rock is given" do
-      expect(game.winner("scissors", "rock")).to eq lose
-    end
-    it "should return win when paper and rock is given" do
-      expect(game.winner("paper", "rock")).to eq win
+      set_choice("paper", "paper")
+      expect(game.winner).to eq draw
     end
     it "should return lose when rock and paper is given" do
-      expect(game.winner("rock", "paper")).to eq lose
+      set_choice("rock", "paper")
+      expect(game.winner).to eq lose
     end
     it "should return scissors when paper and scissors is given" do
-      expect(game.winner("paper", "scissors")).to eq lose
+      set_choice("paper", "scissors")
+      expect(game.winner).to eq lose
     end
     it "should return scissors when scissors and paper is given" do
-      expect(game.winner("scissors", "paper")).to eq win
+      set_choice("scissors", "paper")
+      expect(game.winner).to eq win
     end
+
+    it "should return draw when rock and rock is given" do
+      set_choice("rock", "rock")
+      expect(game.winner).to eq draw
+    end
+    it "should return draw when scissors and scissors is given" do
+      set_choice("scissors", "scissors")
+      expect(game.winner).to eq draw
+    end
+    it "should return Win when rock and scissors is given" do
+      set_choice("rock", "scissors")
+      expect(game.winner).to eq win
+    end
+    it "should return Lose when scissors and rock is given" do
+      set_choice("scissors", "rock")
+      expect(game.winner).to eq lose
+    end
+    it "should return win when paper and rock is given" do
+      set_choice("paper", "rock")
+      expect(game.winner).to eq win
+    end
+
   end
 end
 
