2035b12fd830d98a7826fd5e7fffe2d7a76b7dd3
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-21 14:34:43 +0000

    no more game globals

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,27 @@
+require_relative 'player'
+require_relative 'computer'
 
 class Game
 
-  def computer_chooses
-    ["ROCK", "PAPER", "SCISSORS"].sample
+  def self.create(player, computer)
+    @game = Game.new(player, computer)
   end
 
-  def rps(player_choice, computer)
-    return "DRAW" if player_choice == computer
+  def self.instance
+    @game
+  end
+
+  attr_reader :player, :computer, :result
+
+  def initialize(player, computer)
+    @player = player
+    @computer = computer
+  end
+
+  def rps(player_choice, computer_choice)
+    return @result = "DRAW" if player_choice == computer_choice
     computer_win = [["ROCK", "PAPER"], ["PAPER", "SCISSORS"], ["SCISSORS", "ROCK"]]
-    return computer_win.include?([player_choice, computer]) ? "Computer wins" : "Player wins"
+    @result = computer_win.include?([player_choice, computer_choice]) ? "Computer wins" : "Player wins"
   end
-
-  # def result
-  #   "WIN"
 
 end
