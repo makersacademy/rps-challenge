cc0d315d3b9a6d4ad9733e05b637cb930a215248
rcvink <rrvink@gmail.com>
2018-01-22 09:59:35 +0000

    Stub random behaviour

---
+++ spec/feature/playing_the_game_spec.rb
@@ -0,0 +1,21 @@
+feature 'Playing the game' do
+  before(:each) do
+    allow_any_instance_of(Computer).to receive(:choose).and_return(:scissors)
+    sign_in
+  end
+
+  scenario 'player can win' do
+    click_button 'rock'
+    expect(page).to have_content 'rock beats scissors. test name wins!'
+  end
+
+  scenario 'player can lose' do
+    click_button 'paper'
+    expect(page).to have_content 'scissors beats paper. Computer wins!'
+  end
+
+  scenario 'players can tie' do
+    click_button 'scissors'
+    expect(page).to have_content 'Tie! Both chose scissors!'
+  end
+end
