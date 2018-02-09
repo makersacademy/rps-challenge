8dd3c22915ddad7c5b988f676cb79d704bb67c15
Terry Allitt <tallitt88@gmail.com>
2018-02-09 23:35:05 +0000

    Require files to fix feature test errors and pass feature test for entering names

--- app.rb
+++ app.rb
@@ -2,12 +2,22 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    #
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb(:play)
   end
 
   run! if app_file == $0
