679f87a2dd1ac6119fae0a247af5620623c3843d
Laura Chan <laurachan205@gmail.com>
2018-02-10 14:36:07 +0000

    Pass first enter names feature test

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     'Hello world'
+    erb(:name)
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -2,7 +2,7 @@ feature "Enter name" do
   scenario "Allows user to enter name" do
     visit '/'
     fill_in 'Name', with: 'Laura'
-    click('Submit')
+    click_on('Submit')
     expect(page).to have_content("Hello Laura, let's play Rock Paper Scissors!" )
   end
 end
