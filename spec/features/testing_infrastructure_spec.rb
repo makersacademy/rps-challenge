175d928fff5650643a0de94d495b3755662724a5
Ben <ben.challenor@btopenworld.com>
2018-02-10 15:57:45 +0000

    Added rock, paper, stone buttons

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -11,3 +11,10 @@ feature 'Enter players name' do
     expect(page).to have_content 'Ben vs Computer'
   end
 end
+
+feature "Rock, Paper, Scissor buttons" do
+  scenario "Rock button exists" do
+    sign_in_and_play
+    expect(page).to have_selector(:link_or_button, "Rock")
+  end
+end
