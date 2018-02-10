18f2a1c8615e3a71fa06a286945e87028ecfddcd
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-10 19:39:30 +0000

    In progress, stubbing player 2 choice values in capybara win_lose_spec feature test. Not sure how this works yet

--- spec/features/win_lose_spec.rb
+++ spec/features/win_lose_spec.rb
@@ -1,15 +1,34 @@
+# require 'game'
+#
+# describe Game do
+#
+#   let(:game_class) {Game}
+#   let(:game) {game_class.create(first_player, second_player)}
+#   let(:first_player) {double('Player 1', name: 'Marcus', move: 'rock')}
+#   let(:second_player) {double('Player 2', name: 'Gamblore', move: 'scissors')}
+#
+#   let(:alternate_game) {game_class.create(first_player_loses, second_player)}
+#   let(:first_player_loses) {double('Player 1', name: 'Marcus', move: 'paper')}
+#
+#
+#   let(:first_player_draw) {double('Player 1', name: 'Marcus', move: 'rock')}
+#   let(:draw_game) {game_class.create(first_player_draw, second_player)}
+#
+
 feature 'choose RPS option' do
   scenario 'choose rock option, WIN' do
     sign_in_and_play
     click_button 'I wanna rock, ROCK!'
+    # allow(game.player_2).to receive(:move).and_return('scissors')
     expect(page).to have_content 'Marcus wins! \ (•◡•) /'
   end
 
   scenario 'choose rock option, LOSE' do
     sign_in_and_play
     click_button 'I wanna rock, ROCK!'
-    expect(page).to have_content 'Your enemy wins! ಠ╭╮ಠ'
+    expect(page).to have_content 'Your enemy wins! \ (•◡•) /'
   end
+  # ಠ╭╮ಠ
 
   scenario 'choose rock option, DRAW' do
     sign_in_and_play
@@ -17,3 +36,5 @@ feature 'choose RPS option' do
     expect(page).to have_content 'Tie Game! (╯°□°）╯︵ ┻━┻'
   end
 end
+
+# end
