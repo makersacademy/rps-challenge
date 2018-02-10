8a45de8a9cf5fca559110c379bafe24aedbf02b4
Terry Allitt <tallitt88@gmail.com>
2018-02-10 10:36:59 +0000

    Replace select_move file with start_game file

--- spec/features/select_move.rb
+++
@@ -1,7 +0,0 @@
-# feature 'Selecting moves' do
-#   scenario 'Players select their move' do
-#     sign_in_and_play
-#     click_button "Rock"
-#     expect(page).to have_content "You have selected Rock"
-#   end
-# end
---
+++ spec/features/start_game.rb
@@ -0,0 +1,7 @@
+feature 'Starting the game' do
+  scenario 'Player one starts the game' do
+    sign_in_and_play
+    click_button "Start to Play"
+    expect(page).to have_content "Telgi: Take your Pick..."
+  end
+end
