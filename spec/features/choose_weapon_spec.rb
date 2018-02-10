51af9fda914fc81ac26a4a6340b329f0defed1b1
Noel Vock <noel1uk@gmail.com>
2018-02-10 22:20:33 +0000

    Expanded tests to include paper and scissors options

--- spec/features/choose_weapon_spec.rb
+++ spec/features/choose_weapon_spec.rb
@@ -4,4 +4,14 @@ feature "Choose weapon" do
     click_button 'Rock'
     expect(page).to have_content 'Noel: chose Rock'
   end
+  scenario "player chooses paper" do
+    submit_name
+    click_button 'Paper'
+    expect(page).to have_content 'Noel: chose paper'
+  end
+  scenario "player chooses scissors" do
+    submit_name
+    click_button 'Scissors'
+    expect(page).to have_content 'Noel: chose scissors'
+  end
 end
