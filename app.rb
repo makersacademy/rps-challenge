daeabdfb1b7a353fbde10ee1f26a914ff163df2a
Hannah Lillis <hannahamanda16@gmail.com>
2018-02-10 16:44:01 +0000

    Set up a redirect for name feature

--- app.rb
+++ app.rb
@@ -2,12 +2,19 @@ require 'sinatra/base'
 
 class RpsGame < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    @player_name = params[:name]
+  post '/names' do
+    session[:name] = params[:name]
+    redirect('play')
+  end
+
+  get '/play' do
+    @player_name = session[:name]
     erb(:play)
   end
 
