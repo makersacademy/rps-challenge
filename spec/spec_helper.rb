364ccd61abe6bedb44bcc9f65e8ac1b04ed16072
Daniel Keen <keendaniel01@gmail.com>
2018-02-10 11:23:52 +0000

    Add app.rb, config file, set up environment, sinatra working and first feature test passed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,8 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
@@ -8,6 +12,8 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
+Capybara.app = Rps
 
 RSpec.configure do |config|
   config.after(:suite) do
