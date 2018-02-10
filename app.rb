e5cbc83b502d515e1890943a4070bdd03b80624d
Laura Chan <laurachan205@gmail.com>
2018-02-10 14:21:20 +0000

    Write enter name feature test

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+
+class RockPaperScissors < Sinatra::Base
+
+  get '/' do
+    'Hello world'
+  end
+
+  run! if app_file == $0
+
+end
---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,8 @@
+feature "Enter name" do
+  scenario "Allows user to enter name" do
+    visit '/'
+    fill_in 'Name', with: 'Laura'
+    click('Submit')
+    expect(page).to have_content("Hello Laura, let's play Rock Paper Scissors!" )
+  end
+end
