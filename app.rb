a591519105cc5d5d34017e805a714875bb1ee144
kotpsot <zygmunt.justyna@gmail.com>
2018-02-10 21:04:29 +0000

    add multiplayer

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require_relative './lib/player'
 require_relative './lib/single_game'
+require_relative './lib/multi_game'
+
 
 class RockPaperScissors < Sinatra::Base
 enable :sessions
@@ -9,13 +11,11 @@ enable :sessions
     erb(:index)
   end
 
+  ##################################################
+
   get '/single' do
     erb(:single)
   end
-
-  # get '/multi' do
-  #   erb(:multi)
-  # end
 
   post '/name' do
     player = Player.new(params[:player_1_name])
@@ -32,6 +32,35 @@ enable :sessions
     @single_game = SingleGame.instance
     @weapon = params[:weapon]
     erb(:attack)
+  end
+
+  ##################################################
+
+  get '/multi' do
+    erb(:multi)
+  end
+
+  post '/names' do
+    players = Player.new(params[:player_1_name]), Player.new(params[:player_2_name])
+    @multi_game = MultiGame.create(players)
+    redirect '/play_multi_player'
+  end
+
+  get '/play_multi_player' do
+    @multi_game = MultiGame.instance
+    erb(:play_multi_player_1)
+  end
+
+  get '/take_first_weapon' do
+    @multi_game = MultiGame.instance
+    @multi_game.player_1_weapon = params[:weapon1]
+    erb(:play_multi_player_2)
+  end
+
+  get '/attack_multi' do
+    @multi_game = MultiGame.instance
+    @multi_game.player_2_weapon = params[:weapon2]
+    erb(:attack_multi)
   end
 
   run! if app_file == $0
---
+++ lib/multi_game.rb
@@ -0,0 +1,39 @@
+class MultiGame
+
+    attr_accessor :players, :player_1_weapon, :player_2_weapon
+
+    def self.create(players)
+      @multi_game = MultiGame.new(players)
+    end
+
+    def self.instance
+      @multi_game
+    end
+
+    def initialize(players)
+      @players = players
+      @player_1_weapon = nil
+      @player_2_weapon = nil
+    end
+
+    def player_one
+      @players.first
+    end
+
+    def player_two
+      @players.last
+    end
+
+    def player_weapon
+      ['scissors', 'paper', 'rock'].sample
+    end
+
+    def calculate_result(pl1, pl2)
+      return 'Player 1 won' if pl1 == 'scissors' && pl2 == 'paper'
+      return 'Player 1 won' if pl1 == 'paper' && pl2 == 'rock'
+      return 'Player 1 won' if pl1 == 'rock' && pl2 == 'scissors'
+      return 'Draw!' if pl1 == pl2
+      'Player 2 won!'
+    end
+
+end
