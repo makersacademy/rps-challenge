1b80415b2b04b2ccb11a1ee814edf05c7b921530
jose <jennymarin1989@hotmail.com>
2018-02-10 20:55:03 +0000

    Introduction of new post route for rock scissors papper

--- app.rb
+++ app.rb
@@ -2,15 +2,31 @@ require 'sinatra/base'
 require './lib/player'
 
 class Game < Sinatra::Base
+  set :public_folder, File.dirname(__FILE__) + '/static'
+
 
   get "/" do
     erb :index
   end
 
   post "/name" do
-    @name=params[:name]
+    $player_name=Player.new(params[:name])
+    redirect "/play"
+  end
+
+  get "/play" do
     erb :player
   end
+
+  post "/play" do
+
+    "you win"
+  end
+
+
+
+
+
 
   run! if app_file == $0
 
