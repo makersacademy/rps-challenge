069ae185e22ac2b79160cac3a094bd3de12cf16b
Terry Allitt <tallitt88@gmail.com>
2018-02-11 11:20:59 +0000

    Add default name for human player

--- lib/player.rb
+++ lib/player.rb
@@ -4,7 +4,7 @@ class Player
 
   attr_reader :name, :move
 
-  def initialize(name)
+  def initialize(name = "Player")
     @name = name
   end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,14 +3,19 @@ require 'player'
 describe Player do
 
   subject(:player_1) { described_class.new("Telgi") }
+  subject(:default_player) { described_class.new() }
 
   describe '#name' do
     it 'returns the name of the player' do
       expect(player_1.name).to eq "Telgi"
     end
+
+    it 'returns default name if nothing is entered' do
+      expect(default_player.name).to eq "Player"
+    end
   end
 
-  describe 'select_move' do
+  describe '#select_move' do
     it 'allows player one to select rock' do
       player_1.choose("Rock")
       expect(player_1.move).to eq :rock
