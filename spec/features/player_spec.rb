a0749fc4cf32423c920a85cafce18110118a0a17
Kaari Strack <kaari.strack@gmail.com>
2018-02-11 18:25:31 +0000

    failing player test

--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -1,4 +1,12 @@
 require "player"
 
 describe Player do
+  subject(:player) { described_class.new }
+  let(:name) { double("name of player") }
+
+  context "#initialize" do
+    it 'has a name' do
+      expect(player[:name]).to eq name
+    end
+  end
 end
