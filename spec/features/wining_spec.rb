c4d2efcb8a509e1a7c4ffb78671009d30c28d99f
Noel Vock <noel1uk@gmail.com>
2018-02-11 12:48:43 +0000

    Added winning conditions spec

---
+++ spec/features/wining_spec.rb
@@ -0,0 +1,8 @@
+feature 'Playing the game' do
+  scenario "When I submit 'Rock' I am told if I have won" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('scissors')
+    submit_name
+    click_button 'Rock'
+    expect(page).to have_content "Noel won"
+  end
+end
