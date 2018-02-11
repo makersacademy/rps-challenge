c969d5cad00344d531ab2e3ea89ff061f1eca547
Noel Vock <noel1uk@gmail.com>
2018-02-11 13:17:57 +0000

    Added test for loss condition

--- spec/features/winning_spec.rb
+++ spec/features/winning_spec.rb
@@ -5,4 +5,10 @@ feature 'Playing the game' do
     click_button 'Rock'
     expect(page).to have_content "Noel won"
   end
+  scenario "When I submit 'Rock' I am told if I have lost" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('paper')
+    submit_name
+    click_button 'Rock'
+    expect(page).to have_content "Noel lost"
+  end
 end
