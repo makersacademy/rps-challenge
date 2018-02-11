c675f91ce55809d1b5f7fd091e7d7290a59264fe
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-11 09:50:42 +0000

    Fixed minor code formatting 2

--- spec/features/win_lose_spec.rb
+++ spec/features/win_lose_spec.rb
@@ -1,6 +1,6 @@
 feature 'choose RPS option' do
   scenario 'choose rock option, WIN' do
-    srand(67809)
+    srand(67_809)
     sign_in_and_play
     click_button 'I wanna rock, ROCK!'
     expect(page).to have_content 'Marcus wins! \ (•◡•) /'
@@ -11,14 +11,14 @@ feature 'choose RPS option' do
     sign_in_and_play
     click_button 'I wanna rock, ROCK!'
     expect(page).to have_content 'Your enemy wins! ಠ╭╮ಠ'
-    expect(page).not_to have_content 'Marcus wins! \ (•◡•) /'&& 'Tie Game! (╯°□°）╯︵ ┻━┻'
+    expect(page).not_to have_content 'Marcus wins! \ (•◡•) /' && 'Tie Game! (╯°□°）╯︵ ┻━┻'
   end
 
   scenario 'choose rock option, DRAW' do
-    srand(67810)
+    srand(67_810)
     sign_in_and_play
     click_button 'I wanna rock, ROCK!'
     expect(page).to have_content 'Tie Game! (╯°□°）╯︵ ┻━┻'
-    expect(page).not_to have_content 'Marcus wins! \ (•◡•) /'&& 'Your enemy wins! ಠ╭╮ಠ'
+    expect(page).not_to have_content 'Marcus wins! \ (•◡•) /' && 'Your enemy wins! ಠ╭╮ಠ'
   end
 end
