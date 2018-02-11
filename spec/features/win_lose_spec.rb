39a661e46e5eacb0cf77227d7472b663015972df
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-11 08:29:40 +0000

    Correctly fixing outcomes for win/lose using srand. Outcome for a draw created and conditionality across the 3 potential outcomes also working fully. Now to add extra features

--- spec/features/win_lose_spec.rb
+++ spec/features/win_lose_spec.rb
@@ -1,40 +1,21 @@
-# require 'game'
-#
-# describe Game do
-#
-#   let(:game_class) {Game}
-#   let(:game) {game_class.create(first_player, second_player)}
-#   let(:first_player) {double('Player 1', name: 'Marcus', move: 'rock')}
-#   let(:second_player) {double('Player 2', name: 'Gamblore', move: 'scissors')}
-#
-#   let(:alternate_game) {game_class.create(first_player_loses, second_player)}
-#   let(:first_player_loses) {double('Player 1', name: 'Marcus', move: 'paper')}
-#
-#
-#   let(:first_player_draw) {double('Player 1', name: 'Marcus', move: 'rock')}
-#   let(:draw_game) {game_class.create(first_player_draw, second_player)}
-#
-
 feature 'choose RPS option' do
   scenario 'choose rock option, WIN' do
+    srand(67809)
     sign_in_and_play
     click_button 'I wanna rock, ROCK!'
-    # allow(game.player_2).to receive(:move).and_return('scissors')
     expect(page).to have_content 'Marcus wins! \ (•◡•) /'
   end
 
   scenario 'choose rock option, LOSE' do
     sign_in_and_play
     click_button 'I wanna rock, ROCK!'
-    expect(page).to have_content 'Your enemy wins! \ (•◡•) /'
+    expect(page).to have_content 'Your enemy wins! ಠ╭╮ಠ'
   end
-  # ಠ╭╮ಠ
 
   scenario 'choose rock option, DRAW' do
+    srand(67810)
     sign_in_and_play
     click_button 'I wanna rock, ROCK!'
     expect(page).to have_content 'Tie Game! (╯°□°）╯︵ ┻━┻'
   end
 end
-
-# end
