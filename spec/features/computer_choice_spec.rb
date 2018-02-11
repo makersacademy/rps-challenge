9e720dbeff94bf3cadf139a921579b18af574af7
Noel Vock <noel1uk@gmail.com>
2018-02-11 10:54:48 +0000

    Modified scenario description

--- spec/features/computer_choice_spec.rb
+++ spec/features/computer_choice_spec.rb
@@ -1,5 +1,5 @@
 feature "Computer chooses weapon" do
-  scenario "computer chooses a weapon" do
+  scenario "computer choice is displayed on page" do
     submit_name
     click_button 'Rock'
     expect(page).to have_content
