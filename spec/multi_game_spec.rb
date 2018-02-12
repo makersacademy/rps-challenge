77cf5acb52c278de3b93638931bbaf089177e99d
kotpsot <zygmunt.justyna@gmail.com>
2018-02-12 21:50:13 +0000

    add unit test for calculating win in multi game

--- spec/multi_game_spec.rb
+++ spec/multi_game_spec.rb
@@ -1,18 +1,16 @@
+require_relative '../lib/player'
 
 describe MultiGame do
-  subject(:multi_game) { described_class.new(players) }
-  let(:players) {double :players}
-  # let(:players) { double(('Justyna', name: 'Justyna', weapon: 'rock'), ('Igor', name: 'Igor', weapon: 'scissors'))}
-
+  player1 = Player.new("Justyna")
+  player2 = Player.new("Igor")
+  subject(:multi_game) { described_class.new([player1, player2]) }
 
   describe "#calculate_result" do
     it 'calculates the draw' do
       expect(multi_game.calculate_result("rock", "rock")).to eq "Draw!"
     end
     it 'calculates the win' do
-      expect(multi_game.calculate_result("rock", "rock")).to eq "Draw!"
+      expect(multi_game.calculate_result("rock", "scissors")).to eq "Justyna won"
     end
-
   end
-
 end
