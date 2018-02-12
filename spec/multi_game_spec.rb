ed6746c2376284e5ab4576b01bec80bfbe4e4f9d
kotpsot <zygmunt.justyna@gmail.com>
2018-02-12 10:56:32 +0000

    add tests

--- spec/multi_game_spec.rb
+++ spec/multi_game_spec.rb
@@ -1,11 +1,14 @@
 
 describe MultiGame do
   subject(:multi_game) { described_class.new(players) }
-  let(:players) { double :players }
+  let(:players) {double :players}
+  # let(:players) { double(('Justyna', name: 'Justyna', weapon: 'rock'), ('Igor', name: 'Igor', weapon: 'scissors'))}
+
 
   describe "#calculate_result" do
-    it 'calculates the result' do
+    it 'calculates the draw' do
       expect(multi_game.calculate_result("rock", "rock")).to eq "Draw!"
     end
+
   end
 end
