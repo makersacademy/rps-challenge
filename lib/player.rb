579ed71b8f6c6755b2177b2040514e608ce96bd1
Laura Chan <laurachan205@gmail.com>
2018-02-19 19:25:55 +0000

    Add weapon attr to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,14 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :weapon
 
   def initialize(name)
     @name = name
+  end
+
+  def weapon=(weapon)
+    fail 'not a possible weapon' unless [:rock, :paper, :scissors].include? weapon
+    @weapon = weapon
   end
 
 end
