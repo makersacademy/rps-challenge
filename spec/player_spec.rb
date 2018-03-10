3f48545c191bbc16a800ef2fc7bb45622b8a0ed4
danielcampos <ddregalo@gmail.com>
2018-03-10 17:53:38 +0000

    Add add_point method to Player class with passing test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,16 @@ require 'player'
 
 describe Player do
   subject(:subject) { described_class.new('katie')}
+
   it 'initialises with player name' do
     expect(subject.name).to eq 'katie'
+  end
+
+  describe '#add_point' do
+
+    it 'add 1 point to the score' do
+      subject.add_point
+      expect(subject.score).to eq 1
+    end
   end
 end
