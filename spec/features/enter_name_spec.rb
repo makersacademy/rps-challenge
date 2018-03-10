947c57081b47c1b14b876d6e4a432336acc6c29c
Antonio <ciniglioantonio@gmail.com>
2018-03-10 12:32:43 +0000

    Dry up code with test helper

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter name' do
   scenario 'entering user name' do
-    visit('/')
-    fill_in :player_name, with: 'Cleopatra'
-    click_button 'Submit'
-    expect(page).to have_content 'Welcome to RockPaperScissors, Cleopatra!'  
+    sign_in_and_play
+    expect(page).to have_content 'Welcome to RockPaperScissors, Cleopatra!'
   end
 end
