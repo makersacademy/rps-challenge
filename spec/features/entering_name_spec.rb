0087f41e793d8b5d5b093d6c5dedbe79871ba448
Vivian Allen <mrvivianallen@gmail.com>
2018-03-10 18:04:16 +0000

    name form added, red

--- spec/features/entering_name_spec.rb
+++ spec/features/entering_name_spec.rb
@@ -4,7 +4,10 @@ feature 'adding player name' do
     visit('/')
     expect(page).to have_content "Sign in to play"
   end
-
-
-
+  scenario 'entering name' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    click_button 'Play!'
+    expect(page).to have_content "Dave's turn!"
+  end
 end
