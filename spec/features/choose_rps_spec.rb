632a01435bc210f4ee3daf7eaa7f87c0b47fb976
Reena Koudi <reenakoudi@Reenas-MacBook-Pro.local>
2018-03-10 20:51:46 +0000

    Added a radio button for Rock

---
+++ spec/features/choose_rps_spec.rb
@@ -0,0 +1,11 @@
+feature 'Choose RPS' do
+  scenario 'Choosing Rock should be confirmed' do
+    visit '/' do
+      fill_in :name, with: 'Reena'
+      click_button "Start Game"
+      choose("Rock")
+      click_button "Ready to go"
+      expect(page).to have_content "Reena choose Rock"
+    end
+  end
+end
