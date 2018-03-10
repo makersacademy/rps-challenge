3ef5c4fcd535c8aac8210f18d9cfade1ef11bb6a
Reena Koudi <reenakoudi@Reenas-MacBook-Pro.local>
2018-03-10 20:56:48 +0000

    Choose Rock by default

--- spec/features/choose_rps_spec.rb
+++ spec/features/choose_rps_spec.rb
@@ -8,4 +8,12 @@ feature 'Choose RPS' do
       expect(page).to have_content "Reena choose Rock"
     end
   end
+
+  scenario "Rock is default" do
+    visit "/"
+    fill_in :name, with: 'Reena'
+    click_button "Start Game"
+    click_button "Ready to go"
+    expect(page).to have_content "Reena choose Rock"
+  end
 end
