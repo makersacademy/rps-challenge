0143e27190dd57ffbb025641c56c91741680ed3d
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-10 18:47:51 +0000

    Further develpment of tests

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -9,21 +9,21 @@ describe RPS do
   feature 'Present choices' do
     scenario 'slects rock' do
       click_on 'rock'
-      expect(page).to have_content 'You chose'
+      expect(page).to have_content 'You chose rock'
     end
   end
 
   feature 'Present choices' do
     scenario 'slects paper' do
       click_on "paper"
-      expect(page).to have_content 'You chose'
+      expect(page).to have_content 'You chose paper'
     end
   end
 
   feature 'Present choices' do
     scenario 'slects scissors' do
       click_on "scissors"
-      expect(page).to have_content 'You chose'
+      expect(page).to have_content 'You chose scissors'
     end
   end
 
