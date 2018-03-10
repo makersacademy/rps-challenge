f67e368d667d9f7566f1fca47f9713a79bba7601
Reena Koudi <reenakoudi@Reenas-MacBook-Pro.local>
2018-03-10 21:05:16 +0000

    Added radio buttons for Paper and Scissors

--- spec/features/choose_rps_spec.rb
+++ spec/features/choose_rps_spec.rb
@@ -1,5 +1,5 @@
 feature 'Choose RPS' do
-  scenario 'Choosing Rock should be confirmed' do
+  scenario 'Choosing Rock' do
     visit '/' do
       fill_in :name, with: 'Reena'
       click_button "Start Game"
@@ -9,11 +9,31 @@ feature 'Choose RPS' do
     end
   end
 
-  scenario "Rock is default" do
-    visit "/"
+  scenario 'Rock is default' do
+    visit '/'
     fill_in :name, with: 'Reena'
     click_button "Start Game"
     click_button "Ready to go"
     expect(page).to have_content "Reena choose Rock"
+  end
+
+  scenario 'Choosing Paper' do
+    visit '/' do
+      fill_in :name, with: 'Reena'
+      click_button "Start Game"
+      choose("Paper")
+      click_button "Ready to go"
+      expect(page).to have_content "Reena choose Paper"
+    end
+  end
+
+  scenario 'Choosing Scissors' do
+    visit '/' do
+      fill_in :name, with: 'Reena'
+      click_button "Start Game"
+      choose("Scissors")
+      click_button "Ready to go"
+      expect(page).to have_content "Reena choose Scissors"
+    end
   end
 end
