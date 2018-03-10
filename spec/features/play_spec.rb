e78ed74886deabb5a021e0b1e5279a388e79aee6
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-10 18:45:42 +0000

    rspecs adjusted

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,29 +1,29 @@
 feature 'testing player can pick Rock, Paper or Scissors' do
   scenario 'player can see Rock' do
-    sign_in_and_play
+    sign_in_and_play_computer
     expect(page).to have_selector("input[type=submit][value='Rock']")
   end
   scenario 'player can see Paper' do
-    sign_in_and_play
+    sign_in_and_play_computer
     expect(page).to have_selector("input[type=submit][value='Paper']")
   end
   scenario 'player can see Scissors' do
-    sign_in_and_play
+    sign_in_and_play_computer
     expect(page).to have_selector("input[type=submit][value='Scissors']")
   end
 
   scenario 'player chooses Rock, sees confirmation' do
-    sign_in_and_play
+    sign_in_and_play_computer
     click_button 'Rock'
     expect(page).to have_content 'Charlene chose Rock'
   end
   scenario 'player chooses Paper, sees confirmation' do
-    sign_in_and_play
+    sign_in_and_play_computer
     click_button 'Paper'
     expect(page).to have_content 'Charlene chose Paper'
   end
   scenario 'player chooses Scissors, sees confirmation' do
-    sign_in_and_play
+    sign_in_and_play_computer
     click_button 'Scissors'
     expect(page).to have_content 'Charlene chose Scissors'
   end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,5 +1,13 @@
-def sign_in_and_play
+def sign_in_and_play_computer
   visit '/'
   fill_in 'name', with: 'Charlene'
+  click_button 'Play against Computer'
+end
+
+def sign_in_and_play_multiplayer
+  visit '/'
+  fill_in 'name', with: 'Charlene'
+  click_button 'Play multiplayer'
+  fill_in 'name2', with: 'Bob'
   click_button 'Submit'
 end
