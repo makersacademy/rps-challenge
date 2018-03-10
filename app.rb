8a4a43f75270724da02f1e6f319b9e8d8eb3a10a
danielcampos <ddregalo@gmail.com>
2018-03-10 17:41:30 +0000

    Add Rock, Paper, Scissors buttons to play view and send choice to move root- tests passed

--- app.rb
+++ app.rb
@@ -8,8 +8,20 @@ class RockPaperScissors < Sinatra::Base
 
   post '/register_names' do
     $player1 = params[:player_1_name]
+    redirect '/play'
   end
 
+  get '/play' do
+    @player1 = $player1
+    @move = $move
+    erb :play
+  end
+
+  post '/move' do
+    $move = params[:submit];
+    
+    redirect '/play'
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
---
+++ spec/features/play_spec.rb
@@ -0,0 +1,18 @@
+feature 'Register Name Page' do
+  scenario 'has title at top of page' do
+    visit('/')
+    expect(page).to have_content "ROCK PAPER SCISSORS"
+  end
+
+  scenario 'has subtitle below title' do
+    visit('/')
+    expect(page).to have_content "THE TIMELESS CLASSIC"
+  end
+
+  scenario 'has player name below divider line' do
+    visit('/')
+    fill_in :player_1_name, with: 'katie'
+    click_button 'PLAY'
+    expect(page).to have_content 'PLAYER // KATIE'
+  end
+end
