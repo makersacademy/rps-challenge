9e34644e17238e3a7da9b650c22c9bb04bb0d8eb
Antonio <ciniglioantonio@gmail.com>
2018-03-11 17:29:08 +0000

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
-require 'coveralls'
-Coveralls.wear!
-
 ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara/rspec'
+require 'coveralls'
 require 'simplecov'
 require 'simplecov-console'
 require 'features/web_helpers'
+
+Coveralls.wear!
 
 Capybara.app = RPS
 
