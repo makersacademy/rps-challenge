0779fd22808949652e033489079a116cf5d22eca
Vivian Allen <mrvivianallen@gmail.com>
2018-03-11 12:33:19 +0000

    included player name in results page

--- spec/features/picking_a_shape_spec.rb
+++ spec/features/picking_a_shape_spec.rb
@@ -9,6 +9,6 @@ feature 'picking a shape' do
     sign_in_player1_and_play
     choose("shape_choice", option:  '0')
     click_on 'Play'
-    expect(page).to have_content ('You picked Rock')
+    expect(page).to have_content ('Dave picked Rock')
   end
 end
