9826337dcf17453dfa230e5f339e06bcfad3388e
Reena Koudi <reenakoudi@Reenas-MacBook-Pro.local>
2018-03-11 14:32:58 +0000

    Added weapon method to computer class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,22 +1,22 @@
 require 'game'
 
 describe Game do
+  subject(:game_class) { described_class }
 
   it 'Store player name' do
-    game = Game.new("Reena")
+    game = game_class.new("Reena")
     expect(game.player).to eq "Reena"
-  end
-
-  describe "#current" do
-    it "start with current game being nil" do
-      expect(Game.current).to be nil
-    end
+    p game_class.current
   end
 
   describe "#create" do
     it "should create a new game" do
-      Game.create("Reena")
-      expect(Game.current).to be_a(Game)
+      game_class.create("Reena")
+      expect(game_class.current).to be_a(Game)
     end
+  end
+
+  it "should have a game with player name Reena" do
+    expect(game_class.current.player).to eq "Reena"
   end
 end
