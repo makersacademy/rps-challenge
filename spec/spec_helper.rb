ff3a1ea4cf0194c692f47b3bbee68ba5aad7fc24
Gia1987 <giacomo.n27@gmail.com>
2018-03-11 16:56:44 +0000

    marketeer is able to enter and see own name before the game

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,9 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+require_relative '../app.rb'
+# or whatever your controller app file is called
+ENV['RACK_ENV'] = 'test'
+Capybara.app = RPS
+#  or whatever your class is called in your controller
