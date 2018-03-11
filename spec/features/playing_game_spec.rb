7737c70bb0c57fcb9da66ef5ea29cf8809c08988
Vivian Allen <mrvivianallen@gmail.com>
2018-03-11 15:39:47 +0000

    repeat play feature added red->green

--- spec/features/playing_game_spec.rb
+++ spec/features/playing_game_spec.rb
@@ -9,8 +9,15 @@ feature 'playing a game against computer' do
     sign_in_player1_and_play
     choose("shape_choice", option:  '0')
     click_on 'Play!'
-    expect(page).to have_content 'calculating'
     click_on 'What is that robot up to??'
     expect(page).to have_content 'GAME OVER!'
+  end
+  scenario 'starting a new game' do
+    sign_in_player1_and_play
+    choose("shape_choice", option:  '0')
+    click_on 'Play!'
+    click_on 'What is that robot up to??'
+    click_on 'Play again'
+    expect(page).to have_content 'Welcome to a game of competitive handshapes!'
   end
 end
