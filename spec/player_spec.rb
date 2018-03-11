a2ec4d5d1a1d48815a1b3b9db86d768b94803161
Cindy <jl7e12@gmail.com>
2018-03-11 12:45:33 +0000

    Added result class and tests passed

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,7 @@
-require 'player.rb'
+require 'player'
+
+describe '#name' do
+  it 'retuens the name' do
+    expect(Player.new("Cindy").name).to eq "Cindy"
+  end
+end
---
+++ spec/results_spec.rb
@@ -0,0 +1,40 @@
+require 'result'
+
+describe 'result class' do
+  it "returns empty result array" do
+    expect(Result.new.results).to be_empty
+  end
+end
+
+describe '#add_result' do
+  subject(:result) { Result.new }
+  it "adds new result in the results array" do
+    new_result = :won
+    expect(result.add_result(new_result)).to include new_result
+  end
+end
+
+describe '#get_result' do
+  subject(:result) { Result.new }
+  context 'when net score is a positive number' do
+    it "returns won" do
+      result.results = [:won, :lost, :won, :draw]
+      expect(result.get_result).to eq :won
+    end
+  end
+
+  context 'when net score is negative number' do
+    it "returns lost" do
+      result.results = [:won, :lost, :lost, :draw]
+      expect(result.get_result).to eq :lost
+    end
+  end
+
+  context 'when net score is zero' do
+    it "returns draw" do
+      result.results = [:won, :lost, :draw]
+      expect(result.get_result).to eq :draw
+    end
+  end
+
+end
