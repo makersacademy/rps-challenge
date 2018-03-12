f01750a3bd8abc47ab4793ffcab1a725b7a1c5ff
Christopher Ly <demedial@gmail.com>
2018-03-12 01:21:00 +0000

    Beginning to test game results, currently red

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -23,4 +23,26 @@ describe Game do
       expect(described_class.instance).to be_an_instance_of described_class
     end
   end
+
+  describe '#result' do
+    context 'when player picks rock' do
+
+      before(:each) { allow(player).to receive(:weapon) { :rock } }
+
+      it 'returns win if computer picks scissors' do
+        allow(computer).to receive(:weapon) { :scissors }
+        expect(game.result).to eq 'Cleopatra, you win!'
+      end
+
+      it 'returns lose if computer picks paper' do
+        allow(computer).to receive(:weapon) { :paper }
+        expect(game.result).to eq 'Cleopatra, you lose!'
+      end
+
+      it 'returns draw if computer picks rock' do
+        allow(computer).to receive(:weapon) { :rock }
+        expect(game.result).to eq "It's a draw!"
+      end
+    end
+  end
 end
