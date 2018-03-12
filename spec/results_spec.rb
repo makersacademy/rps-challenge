93cb99586ad03f4debe65434945ac349dd0177a4
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-12 12:42:25 +0000

    unit test added with let

--- spec/results_spec.rb
+++ spec/results_spec.rb
@@ -1,16 +1,13 @@
-# require 'results'
-#
-# describe Results do
-#
-#   subject(:results) { described_class.new('Charlene', 'Bob', 'choice1', 'choice2') }
-#   describe '#declare' do
-#     it 'decalres the winner' do
-#       choice1 = double('choice1')
-#       choice2 = double('choice2')
-#       expect(choice1).to receive(:turn).and_return('Paper')
-#       expect(choice2).to receive(:turn).and_return('Scissors')
-#       results.winner
-#       expect(results.declare).to eq 'Bob won'
-#     end
-#   end
-# end
+require 'results'
+
+describe Results do
+  let(:choice1) { double :choice1, turn: 'Paper' }
+  let(:choice2) { double :choice2, turn: 'Scissors' }
+  subject(:results) { described_class.new('Charlene', 'Bob', choice1, choice2) }
+  describe '#declare' do
+    it 'decalres the winner' do
+      results.winner
+      expect(results.declare).to eq 'Bob won'
+    end
+  end
+end
