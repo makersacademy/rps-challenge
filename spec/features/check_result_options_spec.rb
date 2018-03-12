37c8274ac95fa72d3fb7f64292ec07a075a2befa
Somayeh <mail.somayeh@gmail.com>
2018-03-12 00:02:48 +0000

    Adding more test for player's other options

--- spec/features/check_result_options_spec.rb
+++ spec/features/check_result_options_spec.rb
@@ -5,4 +5,18 @@ feature 'checking the chosen options' do
     click_button 'Rock'
     expect(page).to have_content 'Alex chose Rock'
   end
+
+  scenario 'check for Paper button' do
+    sign_in_and_play
+    expect(page).to have_content 'Alex vs. Computer!'
+    click_button 'Paper'
+    expect(page).to have_content 'Alex chose Paper'
+  end
+
+  scenario 'check for Scissors button' do
+    sign_in_and_play
+    expect(page).to have_content 'Alex vs. Computer!'
+    click_button 'Scissors'
+    expect(page).to have_content 'Alex chose Scissors'
+  end
 end
