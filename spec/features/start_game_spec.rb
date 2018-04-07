2a5fe0f6d3792497c41fac8f1e0c1a26095a4d0b
marydomashneva <mary.domashneva@gmail.com>
2018-04-07 23:09:40 +0100

    Define first turn

---
+++ spec/features/start_game_spec.rb
@@ -0,0 +1,9 @@
+require './app'
+
+feature 'Start the game' do
+  scenario 'pressing button "start"' do
+    sign_in_and_play
+    click_button 'Start'
+    expect(page).to have_content 'choose first!'
+  end
+end
