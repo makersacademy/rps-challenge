2a7cd5c66ee8983a516e46f7c2836209f2cc5f19
marydomashneva <mary.domashneva@gmail.com>
2018-04-07 21:32:39 +0100

    Introducing sighn in for palyers. play.rb and game.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 require 'sinatra/base'
+require './lib/player'
+require './lib/game'
 
 class RPS < Sinatra::Base
+
   get '/' do
-    'Hello!'
+    erb :index
+  end
+
+  post '/names' do
+    player_1 = Player.new(params[:player_1])
+    player_2 = Player.new(params[:player_2])
+    @game = Game.create(player_1, player_2)
+    redirect '/play'
+  end
+
+  before do
+    @game = Game.instance
+  end
+
+  get '/play' do
+      erb :play
   end
 
   # start the server if ruby file executed directly
