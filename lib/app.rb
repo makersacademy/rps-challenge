93f52e5cf219728b585954d6948f815ee844951f
Matthew <matt_burgoyne@live.co.uk>
2018-04-07 17:25:39 +0100

    player name displayed on screen

--- lib/app.rb
+++ lib/app.rb
@@ -1,16 +1,37 @@
 require 'sinatra'
+require './lib/player'
+
 
 class App < Sinatra::Base
 
 
 configure do
+
   enable :sessions
   set :session_secret, 'key'
+
 end
 
 get '/signin' do
 
   erb :signin
+
+
+
+end
+
+post '/playrps' do
+
+
+  session[:playername] = params[:'playername']
+  redirect '/playrps'
+
+end
+
+get '/playrps' do
+
+  erb :rps
+
 
 end
 
--- spec/features/rps-spec.rb
+++ spec/features/rps-spec.rb
@@ -4,9 +4,21 @@ require_relative "../../lib/app.rb"
 Capybara.app = App
 
 feature "on signing in to the game of RPS" do
+
   scenario "homepage has an option to submit a name" do
     visit("/signin")
     expect(page).to have_field("playername")
+
+  end
+
+  scenario "player name is displayed on game page" do
+
+    visit("/signin")
+    fill_in 'playername', with: "Ralph"
+    click_button("Submit name")
+    visit("/playrps")
+    expect(page).to have_content "Ralph"
+
   end
 
 end
