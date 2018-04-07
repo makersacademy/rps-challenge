bd171c7a75854623dcadeea3cb40a4c8fafff6a5
marydomashneva <mary.domashneva@gmail.com>
2018-04-07 21:35:20 +0100

    Refactoring using web_helper.rb

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require './app'
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1, with: 'Mary'
-    fill_in :player_2, with: 'Alf'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mary vs. Alf'
   end
 end
