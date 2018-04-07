a5ea1e1da96e4c0d394a261ca57f61aa9840bd8a
Andrew Kemp <akemp850@gmail.com>
2018-04-07 15:51:36 +0100

    Added redirect from POST request and enabled sessions

--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -3,6 +3,15 @@ feature 'Player name' do
     visit('/')
     fill_in :player_name, with: 'Andrew'
     click_button 'Start'
-    expect(page).to have_content 'Hi Andrew, let\'s play Rock, Paper, Scissors!'
+    expect(page).to have_content 'Hi Andrew, let\'s play!'
   end
+
+feature 'View move choices' do
+  scenario 'see what moves I can select from' do
+    visit('/')
+    fill_in :player_name, with: 'Andrew'
+    click_button 'Start'
+    expect(page). to have_content 'Rock, Paper, Scissors'
+  end
+end
 end
