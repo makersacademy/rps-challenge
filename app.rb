f16c181213c540b9d24385dd561bf46dbaa5922f
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-08 11:29:36 +0100

    Added sessions

--- app.rb
+++ app.rb
@@ -1,17 +1,22 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/name' do
-    @name = params[:name]
+    # @name = params[:name]
+    session[:name] = params[:name]
     redirect '/play'
   end
 
   get '/play' do
-    @name = params[:name]
+    # @name = params[:name]
+    @name = session[:name]
     erb :play
   end
 
