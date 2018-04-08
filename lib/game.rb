86dfefe3fdb3faaaf6977cc5f70cf6cfea54a8c1
Matthew <matt_burgoyne@live.co.uk>
2018-04-08 16:36:25 +0100

    game class decides on a winner for the game

--- lib/game.rb
+++ lib/game.rb
@@ -9,5 +9,18 @@ class Game
   end
 
   def who_wins(player, player2)
+    if player.choice == 'Rock' && player2.choice == 'Paper'
+      return player2.name
+    elsif player.choice == 'Paper' && player2.choice == 'Rock'
+      return player.name
+    elsif player.choice = 'Scissors' && player2.choice == 'Paper'
+      return player.name
+    elsif player.choice == 'Paper' && player2.choice == 'Scissors'
+      return player2.name
+    elsif player.choice == 'Rock' && player2.choice == 'Scissors'
+      return player2.name
+    elsif player.choice == 'Scissors' && player2.choice == 'Rock'
+      return player.name
+    end
   end
 end
