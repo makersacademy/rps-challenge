0d8da64fc4396edbb0263b30e93d49a2a3236a37
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-08 23:41:58 +0100

    09: computer | spec fixed

--- spec/features/computer_spec.rb
+++ spec/features/computer_spec.rb
@@ -6,4 +6,10 @@ describe Computer do
   it 'Should return the  default name' do
     expect(subject.name).to eq 'COMPUTER'
   end
+
+  it 'Should return the rock, paper, scissors by randen' do
+    allow(Kernel).to receive(:rand).with(0..2).and_return(0)
+    expect(subject.choose).to eq :rock
+  end
+
 end
