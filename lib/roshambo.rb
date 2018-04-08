9e07d6167419c97b411628d6bf8343ee32c996d6
Jihin Ip <jim_ip20@hotmail.com>
2018-04-08 15:38:43 +0100

    Implementing and testing roll method in Roshambo class

--- lib/roshambo.rb
+++ lib/roshambo.rb
@@ -5,4 +5,9 @@ class Roshambo
   def initialize(input)
     @input = input
   end
+
+  def roll
+    data = ["Rock", "Paper", "Scissors"]
+    data[Kernel.rand(0..2)]
+  end
 end
--- spec/roshambo_spec.rb
+++ spec/roshambo_spec.rb
@@ -9,9 +9,23 @@ describe Roshambo do
     expect(newgame.input).to eq("Rock")
   end
 
-  describe '#randomiser' do
-    it 'randomises between 3 things in an array' do
+  describe '#roll' do
+    it 'rolls to Rock' do
+      newgame = Roshambo.new("dummy")
+      allow(Kernel).to receive(:rand).with(0..2).and_return(0)
+      expect(newgame.roll).to eq("Rock")
+    end
 
+    it 'rolls to Paper' do
+      newgame = Roshambo.new("dummy")
+      allow(Kernel).to receive(:rand).with(0..2).and_return(1)
+      expect(newgame.roll).to eq("Paper")
+    end
+
+    it 'rolls to Scissors' do
+      newgame = Roshambo.new("dummy")
+      allow(Kernel).to receive(:rand).with(0..2).and_return(2)
+      expect(newgame.roll).to eq("Scissors")
     end
   end
 
