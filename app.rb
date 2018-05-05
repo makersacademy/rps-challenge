37784531e10a0c7f7e701a82d91f66e267a8507f
Dione Estabillo <dione.estabillo@gmail.com>
2018-05-05 13:51:01 +0100

    Test infrastructure setup

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra/base'
 class RPSWeb < Sinatra::Base
 
   get '/' do
-    'Hello RPS!'
+    'Hello, RPS!'
   end
 
   run! if app_file == $0
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'rack/test'
+require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = RPSWeb
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
