785c19aae2e6746a7b4f4e946f3d398bc1dcaf07
Charlene <charlenechetcuti@gmail.com>
2018-05-05 09:46:29 +0100

    adding public folder to store css and images, created basic html structure

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require 'sinatra'
+
+set :public_folder, File.dirname(__FILE__)
 
 class RPS < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  get '/game' do
+    erb :game
   end
 
 end
