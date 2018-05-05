e13093bd22f949b8c3426eb069dc6e69cfa60031
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-05 14:40:54 +0100

    Computer makes a random move

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,12 +1,14 @@
 class Rps
-  attr_reader :player, :player_move
+  attr_reader :player, :player_move, :computer_move
 
   def initialize(player_name, computer = 'computer')
     @player = player_name
     @computer = computer
+    @moves = ['rock', 'paper', 'scissors']
   end
 
   def select_move(choice)
     @player_move = choice
+    @computer_move = @moves.sample
   end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -4,6 +4,7 @@ describe Rps, :rps do
   let(:subject) { Rps.new(player) }
   let(:player) { 'sam' }
   let(:choice) { 'rock' }
+  let(:cp_choice) { 'scissors' }
 
   describe '#player' do
     it 'Returns the player name' do
@@ -13,8 +14,10 @@ describe Rps, :rps do
 
   describe '#select_move' do
     it 'returns the players choice' do
+      allow(subject).to receive(:computer_move) { cp_choice }
       subject.select_move(choice)
       expect(subject.player_move).to eq choice
+      expect(subject.computer_move).to eq cp_choice
     end
   end
 end
