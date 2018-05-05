b4d304def9d5ea1130db960e8c00783f4762c185
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-05 11:24:51 +0100

    Display entered player name

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 require 'sinatra/base'
 
-class RPS
+class RPS < Sinatra::Base
+  enable :sessions
 
+  get '/' do
+    erb(:index)
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    erb(:play)
+  end
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,1 +1,10 @@
-require 'app'
+require_relative '../app'
+
+feature 'Allows user to enter name' do
+  scenario 'Displays name' do
+    visit('/')
+    fill_in 'player_name', with: 'Sam'
+    click_button('Submit')
+    expect(page).to have_content "Player 1: Sam"
+  end
+end
