f6b3adf0ca7ac9caec5578f31a256ee2896e38ef
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-05 18:05:16 +0100

    Remove sessions

--- app.rb
+++ app.rb
@@ -6,15 +6,12 @@ class RPS < Sinatra::Base
     @rps = Rps.rps
   end
 
-  enable :sessions
-
   get '/' do
     erb(:index)
   end
 
   post '/name' do
-    session[:player_name] = params[:player_name]
-    @rps = Rps.start(session[:player_name])
+    @rps = Rps.start(params[:player_name])
     redirect '/name2'
   end
 
@@ -28,23 +25,16 @@ class RPS < Sinatra::Base
 
   get '/rock' do
     @rps.select_move('rock')
-    calculate_winner
     erb(:end)
   end
 
   get '/paper' do
     @rps.select_move('paper')
-    calculate_winner
     erb(:end)
   end
 
   get '/scissors' do
     @rps.select_move('scissors')
-    calculate_winner
     erb(:end)
-  end
-
-  def calculate_winner
-    @winner = @rps.outcome
   end
 end
