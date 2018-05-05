cb3cc50a9d4192f118a55d02ad1d06817247baee
George Sykes <georgesykes86@gmail.com>
2018-05-05 20:39:33 +0100

    Adds computer and unit tests

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,3 +1,16 @@
-class Computer
+require_relative './player'
+
+class Computer < Player
+
+  WEAPONS = [:rock, :paper, :scissors, :spock, :lizard]
+
+  def initialize
+    super
+    @name = "The Computer"
+  end
+
+  def set_weapon
+    @weapon = WEAPONS.sample
+  end
 
 end
--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,7 @@ class Player
 
   attr_reader :name, :weapon
 
-  def initialize(name)
+  def initialize(name = "anon")
     @name = name
     @weapon = nil
   end
