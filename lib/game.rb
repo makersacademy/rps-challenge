0ab4084758176212e8b9c043b4f06734fc30b947
George Sykes <georgesykes86@gmail.com>
2018-05-05 19:04:05 +0100

    Game passes all unit tests

--- lib/game.rb
+++ lib/game.rb
@@ -16,9 +16,14 @@ class Game
     @game = self.new(player1, player2)
   end
 
+  attr_reader :score, :result
+
   def initialize(player1, player2)
-    @players = { player1.id.to_sym => player1, player2.id.to_sym => player2 }
+    @player1 = player1.id.to_sym
+    @player2 = player2.id.to_sym
+    @players = { @player1 => player1, @player2 => player2 }
     @score = [0,0]
+    @result = {winner: nil, result: nil}
   end
 
   def ready?
@@ -26,12 +31,18 @@ class Game
   end
 
   def add_weapon(player_id, weapon)
+    raise "Player already has a weapon" if @players[player_id].has_weapon?
     @players[player_id].set_weapon(weapon.to_sym)
     set_computer_weapon if one_player?
   end
 
   def play
-
+    if draw?
+      draw
+    else
+      @result[:winner] = :result
+      @players[@player1].weapon.beats?(@players[@player2].weapon) ? set_winner(@player1) : set_winner(@player2)
+    end
   end
 
   private
@@ -42,6 +53,20 @@ class Game
 
   def set_computer_weapon
     @players.each { |id, player| player.set_weapon if player.is_a?(Computer) }
+  end
+
+  def draw
+    @result[:winner] = nil
+    @result[:result] = :draw
+  end
+
+  def draw?
+    @players[@player1].weapon == @players[@player2].weapon
+  end
+
+  def set_winner(player_id)
+    @result[:winner] = @players[player_id].name
+    player_id == @player1 ? @score[0] += 1 : @score[1] += 1
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,13 @@
 describe Game do
-  let(:player1) { spy :player, id: "player1", has_weapon?: true }
-  let(:player2) { spy :player, id: "player2", has_weapon?: true  }
-  let(:computer) { spy :computer, id: "Computer", has_weapon?: true }
+  let(:player1) { spy :player, id: "player1", has_weapon?: false }
+  let(:player2) { spy :player, id: "player2", has_weapon?: false }
+  let(:computer) { spy :computer, id: "Computer" }
   let(:game) do
     described_class.start_game(player1, player2)
     described_class.game
   end
+  let(:rock) { double :rock }
+  let(:paper) { double :paper }
 
   describe '#self.start_game & self.game' do
     it 'creates and stores a game' do
@@ -15,40 +17,61 @@ describe Game do
 
   context '2 player game' do
     describe '#add_weapon' do
-      before { game.add_weapon(:player1, :rock) }
       it 'adds a weapon to a player' do
+        game.add_weapon(:player1, :rock)
         expect(player1).to have_received(:set_weapon).with(:rock)
       end
 
       it 'fails if the player already has a weapon' do
+        allow(player1).to receive(:has_weapon?).and_return(true)
         expect { game.add_weapon(:player1, :rock) }.to raise_error("Player already has a weapon")
       end
     end
 
     describe '#ready?' do
       it 'returns true when players have their weapons' do
+        allow(player1).to receive(:has_weapon?).and_return(true)
+        allow(player2).to receive(:has_weapon?).and_return(true)
         expect(game.ready?).to be true
       end
 
       it 'returns false when players dont have their weapons' do
-        allow(player2).to receive(:has_weapon?).and_return(false)
         expect(game.ready?).to be false
       end
     end
 
     describe '#play' do
-      before do
-        game.add_weapon(:player1, :rock)
-        game.add_weapon(:player2, :paper)
-        game.play
-      end
-      it 'sets a winner' do
-        expect(game.winner).to be player2
+      context 'different weapons' do
+        before do
+          allow(player1).to receive(:weapon).and_return(rock)
+          allow(player2).to receive(:weapon).and_return(paper)
+          allow(rock).to receive(:beats?).with(paper).and_return(false)
+          game.play
+        end
+        it 'sets a winner' do
+          expect(game.result[:winner]).to be player2
+        end
+
+        it 'updates the score' do
+          expect(game.score).to eq [0, 1]
+        end
       end
 
-      it 'updates the score' do
-        expect(game.score).to be [0, 1]
+      context 'same weapons' do
+        before do
+          allow(player1).to receive(:weapon).and_return(rock)
+          allow(player2).to receive(:weapon).and_return(rock)
+          game.play
+        end
+        it 'sets the result' do
+          expect(game.result[:result]).to be :draw
+        end
+
+        it 'does not update the score' do
+          expect(game.score).to eq [0, 0]
+        end
       end
+
     end
   end
 
