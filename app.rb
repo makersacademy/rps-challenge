70204be89dcadffe3a5b9a4579e058f537b63998
Mark Channer <markchanner@gmail.com>
2018-05-06 23:02:46 +0100

    Adds selection route

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require_relative 'lib/player'
+require_relative 'lib/computer'
+require_relative 'lib/game'
 
 class RockPaperScissors < Sinatra::Base
 
@@ -9,7 +11,19 @@ class RockPaperScissors < Sinatra::Base
 
   post '/name' do
     @player_one = Player.new(params[:player_name])
-    erb(:play)
+    # Using evil global until database module covered
+    $game = Game.new(@player_one, Computer.new)
+    redirect '/selection'
+  end
+
+  get '/selection' do
+    @player_one = $game.player1
+    erb(:selection)
+  end
+
+  post '/selection' do
+    $game.player1.weapon = params[:player_selection]
+    erb($game.result)
   end
 
   # start the server if ruby file executed directly
