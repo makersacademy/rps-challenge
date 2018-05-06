7cd1314f677dfe0f9b7ebf36505b36fb42859eb8
Mark Channer <markchanner@gmail.com>
2018-05-06 21:21:56 +0100

    Tests result method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,17 @@
 require 'game'
 
 describe Game do
+  subject(:game) { described_class.new }
+
   describe 'WEAPONS' do
     it 'returns a weapon' do
       expect(described_class::WEAPONS.first).to eq(:rock)
+    end
+  end
+
+  describe '#result' do
+    it 'gives the result of two weapon arguments' do
+      expect(game.result(:rock, :scissors)).to eq(:win)
     end
   end
 end
