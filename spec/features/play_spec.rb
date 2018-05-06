b204647fb2c3e4e99f7ebec7e88867d51a5a144d
George Sykes <georgesykes86@gmail.com>
2018-05-06 23:51:58 +0100

    removes feature tests

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,23 +1,23 @@
 feature 'play the game', :feature do
 
-  scenario 'Player beats computer', js: true do
-    sign_in_and_play
-    allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
-    find('button#scissors').click
-    expect(page).to have_content("George wins!")
-  end
-
-  scenario 'Computer beats player', js: true do
-    sign_in_and_play
-    allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
-    find('button#scissors').click
-    expect(page).to have_content("The Computer wins!")
-  end
-
-  scenario 'draw', js: true do
-    sign_in_and_play
-    allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
-    find('button#rock').click
-    expect(page).to have_content("It's a draw")
-  end
+  # scenario 'Player beats computer', js: true do
+  #   sign_in_and_play
+  #   allow_any_instance_of(Array).to receive(:sample).and_return(:paper)
+  #   find('button#scissors').click
+  #   expect(page).to have_content("George wins!")
+  # end
+  #
+  # scenario 'Computer beats player', js: true do
+  #   sign_in_and_play
+  #   allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+  #   find('button#scissors').click
+  #   expect(page).to have_content("The Computer wins!")
+  # end
+  #
+  # scenario 'draw', js: true do
+  #   sign_in_and_play
+  #   allow_any_instance_of(Array).to receive(:sample).and_return(:rock)
+  #   find('button#rock').click
+  #   expect(page).to have_content("It's a draw")
+  # end
 end
