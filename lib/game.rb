a868174913b856703748debe49c210b1f618a67b
Asia Antczak <aasiaantczak@gmail.com>
2018-05-06 18:06:23 +0100

    add random weapon for computer

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,9 @@
 class Game
 
-  attr_reader :player_weapon
+  WEAPON = [:rock, :paper, :scissors]
+
+
+  attr_reader :player_weapon, :computer_weapon
 
   def initialize(player = Player.new, computer = Computer.new)
     @player = player
@@ -8,8 +11,12 @@ class Game
     @result = nil
   end
 
-  def add_weapon(choice)
+  def add_player_weapon(choice)
     @player_weapon = choice
+  end
+
+  def add_computer_weapon
+    @computer_weapon = WEAPON.sample
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,10 +9,18 @@ describe Game do
   let(:player) { double :player, name: "Asia" }
   let(:computer) { double :computer }
 
-  describe "#add_weapon" do
-    it 'set weapon for the player' do
-      game.add_weapon('scissors')
+  describe "#add_player_weapon" do
+    it 'sets weapon for the player' do
+      game.add_player_weapon('scissors')
       expect(game.player_weapon).to eq 'scissors'
+    end
+  end
+
+  describe "#add_computer_weapon" do
+    it 'sets random weapon for computer' do
+      allow(game).to receive(:computer_weapon) { :rock }
+      game.add_computer_weapon
+      expect(game.computer_weapon).to eq :rock
     end
   end
 end
