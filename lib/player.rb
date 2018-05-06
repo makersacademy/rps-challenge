7bac776524e2e08bc134115c24d5c5e3d0746c30
Mark Channer <markchanner@gmail.com>
2018-05-06 20:31:41 +0100

    Returns the player's selected weapon if valid

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,16 @@
+require 'game'
+
 class Player
 
-  attr_reader :name
+  attr_reader :name, :weapon
 
   def initialize(name)
     @name = name
   end
-  
+
+  def weapon=(weapon)
+    raise "Invalid weapon" unless Game::WEAPONS.include? weapon
+    @weapon = weapon
+  end
+
 end
