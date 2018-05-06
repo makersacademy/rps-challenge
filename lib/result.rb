ff1271945e3697fb8c6491e9a3127b1dd7f57e67
Ellie Keen <elliekeen95@gmail.com>
2018-05-06 08:45:57 +0100

    Add method for paper move

--- lib/result.rb
+++ lib/result.rb
@@ -6,4 +6,10 @@ class Result
     return 'Draw!' if move == 'ROCK'
   end
 
+  def paper_verdict(move)
+    return 'You lose!' if move == 'SCISSORS'
+    return 'Draw!' if move == 'PAPER'
+    return 'You win!' if move == 'ROCK'
+  end
+
 end
--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -2,7 +2,19 @@ require 'result'
 
 describe Result do
 
-  let(:result) { Result.new('scissors') }
-  let(:move) { "scissors" }
+  let(:result) { Result.new }
+  let(:move) { "SCISSORS" }
+
+  describe '#rock_verdict' do
+    it 'should return "you win"' do
+      expect(result.rock_verdict(move)).to eq 'You win!'
+    end
+  end
+
+  describe '#paper_verdict' do
+    it 'should return "you win"' do
+      expect(result.paper_verdict(move)).to eq 'You lose!'
+    end
+  end
 
 end
