fc84875c44d8a05d7448159be07d44e25c10708a
Mark Channer <markchanner@gmail.com>
2018-05-06 21:54:03 +0100

    Adds tests for result method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,10 @@
 require 'game'
+require 'player'
 
 describe Game do
-  subject(:game) { described_class.new }
+  let(:player_one) { double 'player_one' }
+  let(:player_two) { double 'player_two' }
+  subject(:game) { described_class.new(player_one, player_two)}
 
   describe 'WEAPONS' do
     it 'returns a weapon' do
@@ -10,8 +13,23 @@ describe Game do
   end
 
   describe '#result' do
-    it 'gives the result of two weapon arguments' do
-      expect(game.result(:rock, :scissors)).to eq(:win)
+    it 'returns :win if player1 has winning weapon' do
+      allow(player_one).to receive(:weapon).and_return(:rock)
+      allow(player_two).to receive(:weapon).and_return(:scissors)
+      expect(game.result).to eq(:win)
+    end
+
+    it 'returns :draw if player1 and player2 have same weapons' do
+      allow(player_one).to receive(:weapon).and_return(:paper)
+      allow(player_two).to receive(:weapon).and_return(:paper)
+      expect(game.result).to eq(:draw)
+    end
+
+    it 'returns :lose if player1 has losing weapon' do
+      allow(player_one).to receive(:weapon).and_return(:paper)
+      allow(player_two).to receive(:weapon).and_return(:paper)
+      expect(game.result).to eq(:draw)
     end
   end
+
 end
