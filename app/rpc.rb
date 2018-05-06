ec0cfd2b723631aa940ae1f9a685d3852b39d5bd
Igor Ryabchuk <riabtchuk@gmail.com>
2018-05-06 13:10:46 +0100

    starting to write tests

---
+++ app/rpc.rb
@@ -0,0 +1,32 @@
+require 'sinatra/base'
+require 'haml'
+
+
+class RPC < Sinatra::Base
+  enable :sessions
+
+
+
+  get '/' do
+    haml(:index)
+  end
+
+  get '/game' do
+    @name = session[:name]
+    @choice = session[:choice] || 'None'
+    haml(:game)
+  end
+
+  post '/game' do
+    session[:choice] = params[:name]
+    p params
+    redirect '/game'
+  end
+
+  post '/name' do
+    session[:name] = params[:name]
+    p session[:name]
+    redirect '/game'
+  end
+
+end
