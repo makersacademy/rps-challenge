43c6f78039a2dc3241a20d1a483ae183d904a863
George Sykes <georgesykes86@gmail.com>
2018-05-06 11:36:40 +0100

    Adds further login feature tests

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -10,4 +10,12 @@ feature 'check login', :feature do
     visit '/'
     expect(page).to have_content("Hey there George!")
   end
+
+  scenario 'selects single player mode' do
+    sign_in
+    click_button("1 player")
+    expect(page.title).to eq "RPSLS"
+    expect(page.find('div', class: 'player1_info').text).to eq "George: 0"
+    expect(page.find('div', class: 'player2_info').text).to eq "The Computer: 0"
+  end
 end
