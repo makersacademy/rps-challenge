6b0a4514e2f3f7279234516c0f0d008ee4bfc0ea
anderscodes <anders.j.persson@hotmail.com>
2018-05-07 20:32:59 +0100

    Final commit - refactoring and tests completed

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,30 +1,37 @@
 
-
-
 feature 'See name in lights' do
   scenario 'Player can enter their name' do
     enter_name
     expect(page).to have_text 'Mickey'
   end
+end
 
-  feature 'Player loses' do
-    before do
-      allow_any_instance_of(Computer).to receive(:random_weapon).and_return('Rock')
-      end
-    scenario 'Player chooses scissors' do
-      enter_name
-      click_button "Scissors"
-      expect(page).to have_text 'You Lose :('
-    end
-    scenario 'Player chooses Rock' do
-      enter_name
-      click_button "Rock"
-      expect(page).to have_text 'It\'s a tie!'
-    end
-    scenario 'Player chooses Paper' do
-      enter_name
-      click_button "Paper"
-      expect(page).to have_text 'You Win!'
-    end
+feature 'Plays game' do
+  before do
+    allow_any_instance_of(Computer).to receive(:random_weapon).and_return('Rock')
+  end
+  scenario 'Player chooses scissors' do
+    enter_name
+    click_button "Scissors"
+    expect(page).to have_text 'You Lose :('
+  end
+  scenario 'Player chooses Rock' do
+    enter_name
+    click_button "Rock"
+    expect(page).to have_text 'It\'s a tie!'
+  end
+  scenario 'Player chooses Paper' do
+    enter_name
+    click_button "Paper"
+    expect(page).to have_text 'You Win!'
+  end
+end
+
+feature 'plays again' do
+  scenario 'should start again' do
+    enter_name
+    click_button "Paper"
+    click_button "Play again"
+    expect(page).to have_text 'Welcome to a game of Rock'
   end
 end
