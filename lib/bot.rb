7e9ec79213aab623fe6ae000f2a8704f95c646e3
Muzzi Aldean <amiraldean@gmail.com>
2018-05-07 09:46:52 +0100

    bot randomiser functioning properly now

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,16 +1,20 @@
  class Bot
 
-   attr_reader :name, :move
+   attr_reader :name, :current_move
 
    def initialize
      @name = 'RPS_Bot'
-     @move = rand(3)
+     # @move = rand(3)
+   end
+
+   def move
+     @current_move = rand(3)
    end
 
    def print_move
-     return 'rock' if @move == 0
-     return 'paper' if @move == 1
-     return 'scissors' if @move == 2
+     return 'rock' if @current_move == 0
+     return 'paper' if @current_move == 1
+     return 'scissors' if @current_move == 2
    end
 
  end
