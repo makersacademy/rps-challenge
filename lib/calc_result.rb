f9980b7cae716cc38219604c2c5fd5baf3b61978
julesnuggy <julesnuggy@gmail.com>
2018-05-14 13:30:20 +0100

    Fixed final rubocop issues

--- lib/calc_result.rb
+++ lib/calc_result.rb
@@ -2,11 +2,13 @@ class Result
 
   def initialize(p1_name, p1_move, p2_name, p2_move)
     @moves = [p1_move, p2_move]
-    @names = ["It\'s a draw!", "#{p1_name} is the winner!", "#{p2_name} is the winner!"]
+    @names = ["It\'s a draw!", "#{p1_name} is the winner!",
+              "#{p2_name} is the winner!"]
     @rps_chart = {
       ["rock", "rock"] => 0, ["rock", "paper"] => 2, ["rock", "scissors"] => 1,
-      ["paper", "rock"] => 1, ["paper", "paper"] => 0, ["paper", "scissors"] => 2,
-      ["scissors", "rock"] => 2, ["scissors", "paper"] => 1, ["scissors", "scissors"] => 0
+      ["paper", "rock"] => 1, ["paper", "paper"] => 0,
+      ["paper", "scissors"] => 2, ["scissors", "rock"] => 2,
+      ["scissors", "paper"] => 1, ["scissors", "scissors"] => 0
     }
   end
 
