a3a1e6c3f7b2f6dd10c6451601636277b2caba2f
Robert Faldo <robertfaldo@gmail.com>
2018-06-02 12:34:46 +0100

    Game, DetermineWinner, Randomizer classes all refactored

--- lib/game.rb
+++ lib/game.rb
@@ -19,22 +19,4 @@ class Game
   def winner
     @determinewinner.result(@computers_choice, @players_choice)
   end
-
-  private
-  #
-  # def determine_winner(computers_choice, players_choice)
-  #   return "Draw" if draw?(computers_choice, players_choice)
-  #   return "Player wins" if player_wins?(computers_choice, players_choice)
-  #   "Computer wins"
-  # end
-  #
-  # def draw?(computers_choice, players_choice)
-  #   computers_choice == players_choice
-  # end
-  #
-  # def player_wins?(computers_choice, players_choice)
-  #   return true if players_choice == "Rock" && computers_choice == "Scissors"
-  #   return true if players_choice == "Paper" && computers_choice == "Rock"
-  #   return true if players_choice == "Scissors" && computers_choice == "Paper"
-  # end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -25,22 +25,5 @@ describe Game do
       # Exercise & verify
       expect(game.winner).to eq "Player wins"
     end
-    #
-    # it 'player can lose' do
-    #   # Setup
-    #   game.players_choice("Paper")
-    #   allow(randomizer).to receive(:result).and_return("Scissors")
-    #   game.computers_choice
-    #   # Exercise & verify
-    #   expect(game.winner).to eq "Computer wins"
-    # end
-    # it 'there can be a draw' do
-    #   # Setup
-    #   game.players_choice("Paper")
-    #   allow(randomizer).to receive(:result).and_return("Paper")
-    #   game.computers_choice
-    #   # Exercise & Verify
-    #   expect(game.winner).to eq "Draw"
-    # end
   end
 end
