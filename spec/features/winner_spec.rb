1a02b50d8d9383753d7ae1242c65287372852099
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-02 22:37:56 +0100

    User Story 2 - Update Clickable images added for rock, paper, scissors.

--- spec/features/winner_spec.rb
+++ spec/features/winner_spec.rb
@@ -2,9 +2,6 @@ feature "display winner" do
   scenario "winner is declared" do
     register_to_play
     click_link('Rock')
-    # moves_double = double(sample: "Scissors")
-    # computer = Computer.new(moves_double)
-    # player = Player.new("Pookie", computer)
     expect(page).to have_content "Computer has choosen: #{$player.opponent.move}"
     expect(page).to have_content "Pookie is the winner!"
   end
