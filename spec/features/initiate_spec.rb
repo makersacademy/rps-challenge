783843c6b4904909a943bd4b80ea76dde641b25e
anguspettifer <angus.pettifer@gmail.com>
2018-06-02 13:02:49 +0100

    multiple games

--- spec/features/initiate_spec.rb
+++ spec/features/initiate_spec.rb
@@ -6,4 +6,13 @@ feature 'Initiate' do
     click_button 'Start game'
     expect(page).to have_content 'Please enter your weapon:'
   end
+
+  scenario 'start second game' do
+    sign_in_and_play
+    click_button 'Start game'
+    click_button 'Rock'
+    click_button 'Start another game'
+    expect(page).to have_content 'Please enter your weapon:'
+  end
+
 end
