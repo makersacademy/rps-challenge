8d38ebbaa84b788082c5b7240bf46af9ca3f2498
Benjamin Tomkins <bentomkins@hotmail.com>
2018-06-02 07:50:31 +0100

    warrior choice check

--- app/app.rb
+++ app/app.rb
@@ -3,18 +3,30 @@ require 'sinatra/base'
 
 class RPSLS < Sinatra::Base
 
+	# Root page requests the player's name
+	get '/' do
+		erb :specify_player_name
+	end
 
-  get '/' do
-    erb :specify_player_name
-  end
+	# Store the player's name ad redirects to the game arena
+	post '/save_player_name' do
+		$player_name = params[:player_name] unless $player_name
+		redirect '/choose_your_warrior'
+	end
 
-  post '/save_player_name' do
-    $player_name = params[:player_name] unless $player_name
-    redirect '/enter_the_arena'
-  end
+	# Let the games begin!
+	get '/choose_your_warrior' do
+		erb :choose_your_warrior
+	end
 
-  get '/enter_the_arena' do
-    erb :enter_the_arena
-  end
+	post '/save_warrior_choice' do
+		$player_warrior = params[:player_warrior]
+		redirect '/fight'
+	end
+
+	get '/fight' do
+		erb :fight
+	end
+
 
 end
--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -1,11 +1,20 @@
 feature 'Features' do
 
-# As a marketeer
-# So that I can see my name in lights
-# I would like to register my name before playing an online game
-  scenario 'player can enter a name' do
-    sign_in_and_play
-    expect(page).to have_content('Mr.Spock')
-  end
+	# As a marketeer
+	# So that I can see my name in lights
+	# I would like to register my name before playing an online game
+	scenario 'player can enter a name' do
+		sign_in_and_play
+		expect(page).to have_content('Mr.Spock')
+	  end
+
+	  # As a marketeer
+	# So that I can enjoy myself away from the daily grind
+	# I would like to be able to play rock/paper/scissors
+	scenario 'player can choose a weapon' do
+		sign_in_and_play
+		click_button('Lizard')
+		expect(page).to have_content('you chose Lizard')
+	end
 
 end
