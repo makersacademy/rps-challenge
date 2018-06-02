0094449a9764d16aa3cec66528c4445ec79c5213
Robert Faldo <robertfaldo@gmail.com>
2018-06-02 08:10:15 +0100

    first commit, environment set up, README added and test infrastructure completed and green

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,11 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+ENV['RACK_ENV'] = 'test'
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# tell Capybara about our app class
+Capybara.app = RPS
