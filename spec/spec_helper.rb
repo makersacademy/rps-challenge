eb7b91a7e9afb36ba3cf340be5afac27b314d35b
Jay Khan <jkhan992@gmail.com>
2018-06-29 23:24:05 +0100

    added scaffold and test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require "rspec"
+
+require "capybara"
+
+# tell Capybara about our app class
+ENV["RACK_ENV"] = "test"
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -16,3 +22,9 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), "..", "app.rb")
+
+# tell Capybara about our app class
+Capybara.app = Game
---
+++ spec/test_infrastructure_spec.rb
@@ -0,0 +1,9 @@
+require "./app.rb"
+
+feature 'Testing infrastructure' do
+  scenario 'Can run app and check page content' do
+    visit("/")
+    expect(page).to have_content 'Testing infrastructure is up!'
+  end
+
+end
