e9553109f7da267d59e212c57165164e7c4c6a1a
Daniel Lau <daniellau@Daniels-MBP-2.home>
2018-06-30 14:34:40 +0100

    refactored code to redirect POST and use sessions

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player1_name = params[:player1_name]
+    session['player1'] = params[:player1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1_name = session['player1']
     erb :play
   end
 
