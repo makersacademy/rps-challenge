01a2503ecc644b950c1fd1b3d1e3af86fbd61a54
Daniel Lau <daniellau@Daniels-MBP-2.home>
2018-06-30 13:07:52 +0100

    first commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,13 @@
+require 'capybara'
 require 'capybara/rspec'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+ENV['RACK_ENV'] = 'test'
+
+Capybara.app = RPS
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
