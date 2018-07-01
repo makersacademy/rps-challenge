62a257c9c39e88bec359fa0ac137a3b27eeaa5d3
Fred <fredch87@outlook.com>
2018-07-01 22:12:07 +0100

    All user stories/features added

--- lib/game.rb
+++ lib/game.rb
@@ -13,4 +13,26 @@ class Game
   def self.session
     @session
   end
+
+  def draw?
+    @player_one.selection == @player_two.selection
+  end
+
+  def player_one_win?
+    rock_beats_scissors || paper_beats_rock || scissors_beats_paper
+  end
+
+  private
+
+  def rock_beats_scissors
+    @player_one.selection == 'Rock' && @player_two.selection == 'Scissors'
+  end
+
+  def paper_beats_rock
+    @player_one.selection == 'Paper' && @player_two.selection == 'Rock'
+  end
+
+  def scissors_beats_paper
+    @player_one.selection == 'Scissors' && @player_two.selection == 'Paper'
+  end
 end
