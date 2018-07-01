2c83d6d88844821b07aa9d322194f4848e7212c8
Yvonne Tang <ytang604@gmail.com>
2018-07-01 13:04:51 +0100

    player class

---
+++ spec/player_spec.rb
@@ -0,0 +1,11 @@
+require 'player'
+
+describe Player do
+  subject(:player) { described_class.new("Yvonne")}
+
+  describe "name" do
+    it "returns the player's name" do
+      expect(player.name).to eq "Yvonne"
+    end
+  end
+end
