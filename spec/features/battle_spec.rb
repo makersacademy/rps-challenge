df164d029d6b05ff915fe1934cbd5e126ffb3f93
jeff1108 <jeffhongkong2007@gmail.com>
2018-07-02 12:35:46 +0100

    final

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -1,10 +1,36 @@
 require "./rps_web.rb"
 
 feature "select weapon" do
-  scenario "select one weapon" do
+  scenario "player win" do
     sign_in_and_play
-    fill_in :weapons, with: "rock"
+    fill_in :weapons, with: "scissors"
+    srand(1234)
     click_button("Play")
     expect(page).to have_content("You win")
+  end
+
+  scenario "player lose" do
+    sign_in_and_play
+    fill_in :weapons, with: "rock"
+    srand(1234)
+    click_button("Play")
+    expect(page).to have_content("You lose")
+  end
+
+  scenario "player draw" do
+    sign_in_and_play
+    fill_in :weapons, with: "paper"
+    srand(1234)
+    click_button("Play")
+    expect(page).to have_content("You draw")
+  end
+
+  scenario "return the play" do
+    sign_in_and_play
+    fill_in :weapons, with: "rock"
+    srand(1234)
+    click_button("Play")
+    click_button("Play Again")
+    expect(page).to have_content("Select your weapon")
   end
 end
