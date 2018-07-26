db2bc15ad5eae39efa8b5d5420b1614ca050eddd
limcuili <limcuili@hotmail.com>
2018-07-26 13:33:54 +0100

    Add sessions

--- app.rb
+++ app.rb
@@ -2,13 +2,19 @@ require 'sinatra/base'
 require 'sinatra'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_name = params[:player_name]
+    session[:player_name] = params[:player_name]
+    redirect ('/play')
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
     erb(:play)
   end
 
