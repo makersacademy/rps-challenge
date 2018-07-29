91f7921f260d42814db3285600e7d047e59e9e03
Angela Wolff <warriorwomenblog@gmail.com>
2018-07-29 20:40:36 +0100

    Finished testing - challenge complete

--- spec/features/computer_choice_spec.rb
+++ spec/features/computer_choice_spec.rb
@@ -4,6 +4,7 @@ feature 'Computer makes random move' do
     sign_in_and_play
     choose_rock
     click_button 'OK'
+    expect(page).not_to have_xpath("//img[contains(@src,'question_mark.png')]")
     expect(page).to have_content('computer chose')
   end
 end
