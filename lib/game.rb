9a635ae6d3bc17c757196919fafb8d77ca59a629
limcuili <limcuili@hotmail.com>
2018-07-30 09:10:26 +0100

    Game specs and code

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,31 @@
 class Game
 
+  GAME = {
+     "Rock" => { "Scissors" => :win, "Paper" => :lose },
+     "Paper" => { "Rock" => :win, "Scissors" => :lose },
+     "Scissors" => { "Paper" => :win, "Rock" => :lose }
+   }
+
+  def initialize(player = 'player')
+    @player = player
+  end
+
+  def player_choice
+    player.choice
+  end
+
+  def bot_choice
+    bot.choice
+  end
+
+  def result
+    if player_choice == bot_choice
+      return 'It is a draw!'
+    elsif GAME[player_choice][bot_choice] == :win
+      return 'You Win!'
+    elsif GAME[player_choice][bot_choice] == :lose
+      return 'You Lose!'
+    end
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,20 +1,64 @@
 require 'game'
 
 describe Game do
+  subject(:game) { described_class.new(:player) }
 
-=begin
-Here, we will test that the bot can choose from the 3 options of rps.
-Then, we will test each of 9 outcome of player and bot
-PLAYER-BOT
-rock-rock
-paper-paper
-scissors-scissors
-rock-paper
-rock-scissors
-paper-rock
-paper-scissors
-scissors-rock
-scissors-paper
-=end
+  describe '#result' do
+
+      context "player wins" do
+        it "player chooses rock and bot chooses scissors" do
+          allow(game).to receive(:player_choice).and_return('Rock')
+          allow(game).to receive(:bot_choice).and_return('Scissors')
+          expect(game.result).to eq 'You Win!'
+        end
+        it "player chooses paper and bot chooses rock" do
+          allow(game).to receive(:player_choice).and_return('Paper')
+          allow(game).to receive(:bot_choice).and_return('Rock')
+          expect(game.result).to eq 'You Win!'
+        end
+        it "player chooses scissors and bot chooses paper" do
+          allow(game).to receive(:player_choice).and_return('Scissors')
+          allow(game).to receive(:bot_choice).and_return('Paper')
+          expect(game.result).to eq 'You Win!'
+        end
+      end
+
+      context "player loses" do
+        it "player chooses rock and bot chooses paper" do
+          allow(game).to receive(:player_choice).and_return('Rock')
+          allow(game).to receive(:bot_choice).and_return('Paper')
+          expect(game.result).to eq 'You Lose!'
+        end
+        it "player chooses paper and bot chooses scissors" do
+          allow(game).to receive(:player_choice).and_return('Paper')
+          allow(game).to receive(:bot_choice).and_return('Scissors')
+          expect(game.result).to eq 'You Lose!'
+        end
+        it "player chooses scissors and bot chooses rock" do
+          allow(game).to receive(:player_choice).and_return('Scissors')
+          allow(game).to receive(:bot_choice).and_return('Rock')
+          expect(subject.result).to eq 'You Lose!'
+        end
+      end
+
+      context "return draw" do
+        it "both player and bot chooses rock" do
+          allow(game).to receive(:player_choice).and_return('rock')
+          allow(game).to receive(:bot_choice).and_return('rock')
+          expect(game.result).to eq 'It is a draw!'
+        end
+        it "both player and bot chooses paper" do
+          allow(game).to receive(:player_choice).and_return('paper')
+          allow(game).to receive(:bot_choice).and_return('paper')
+          expect(game.result).to eq 'It is a draw!'
+        end
+        it "both player and bot chooses scissors" do
+          allow(game).to receive(:player_choice).and_return('scissors')
+          allow(game).to receive(:bot_choice).and_return('scissors')
+          expect(game.result).to eq 'It is a draw!'
+        end
+      end
+
+    end
 
 end
