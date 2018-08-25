45531c7854e5484242a1e2697d4b9d11ebaf104b
leekarho <k-h_lee@hotmail.co.uk>
2018-08-25 16:41:28 +0100

    test refactor

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,8 +1,6 @@
 feature 'enter names' do
   scenario 'player can enter their name' do
-    visit('/')
-    fill_in :player, with: 'karho'
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content 'karho please choose your weapon: rock, paper, or scissors'
   end
 end
