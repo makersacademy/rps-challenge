b22340af56b73d6cc672dfe92fa91f5cc7430d81
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-25 14:19:14 +0100

    add web_helper for sign_in_and_play

--- spec/features/rps_features_spec.rb
+++ spec/features/rps_features_spec.rb
@@ -1,11 +1,7 @@
 feature "RPS Features" do
 
   scenario 'players can enter names, submit and see names on screen' do
-    # sign_in_and_play
-    visit('/')
-    fill_in('player_one', with: 'Mick')
-    fill_in('player_two', with: 'Keith')
-    click_on('Submit')
+    sign_in_and_play
     expect(page).to have_content('Mick')
     expect(page).to have_content('Keith')
   end
