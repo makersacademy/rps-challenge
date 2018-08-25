6ccfb0f47d616fdd5fd5bcf3f219850d2663a730
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-25 20:03:38 +0100

    instance variables don't pass between routes so session info has to be set again

--- app.rb
+++ app.rb
@@ -10,26 +10,20 @@ class Rps < Sinatra::Base
   end
 
   post '/names' do
-    # $p1 = Player.new(params[:player_one])
-    # $p2 = Player.new(params[:player_two])
-    p params
     session[:p1_name] = params[:player_one]
     session[:p2_name] = params[:player_two]
-    # $game = Game.new($p1, $p2)
     redirect '/game_start'
   end
 
   get '/game_start' do
     @p1_name = session[:p1_name]
-    @p2_name = session[:p2_name]
+    @p2_name = session[:p2_name] 
     erb(:game_start)
   end
 
   get '/game_home' do
-    # name1 = @p1_name
-    # name2 = @p2_name
-    # @p1 = Player.new(name1)
-    # @p2 = Player.new(name2)
+    @p1_name = session[:p1_name]
+    @p2_name = session[:p2_name]
     @p1 = Player.new(@p1_name)
     @p2 = Player.new(@p2_name)
     @game = Game.new(@p1, @p2)
@@ -37,6 +31,7 @@ class Rps < Sinatra::Base
   end
 
   get '/enter_dragon' do
+    
     visualise player choice 
     generate selection on behalf of machine
     show machine selection
--- lib/game.rb
+++ lib/game.rb
@@ -3,16 +3,11 @@ class Game
   attr_reader :gamer1, :gamer2
 
   def initialize(player1, player2 = DEFAULT_OPPONENT)
-    @gamer1 = player1
-    @gamer2 = player2
+    @p1 = player1
+    @p1 = player2
   end
 
-  # def attack
-  #   @current_player.receive_damage
-  # end
+  
 
-  # def first_mover
-  #   rand(0..1) == 1 ? @current_player = @player1 : @current_player = $p2.name
-  # end
 
 end
