0b3287bf8b91bca17c8549c434e8f5616be4b635
leekarho <k-h_lee@hotmail.co.uk>
2018-08-25 16:25:48 +0100

    raise error if wrong selection and inc web helper

--- app.rb
+++ app.rb
@@ -3,8 +3,6 @@ require './lib/game'
 
 class Rps < Sinatra::Base
 
-  enable :sessions
-
   get '/' do
     erb(:index)
   end
--- spec/features/cpu_spec.rb
+++ spec/features/cpu_spec.rb
@@ -1,10 +1,15 @@
 feature 'player vs game' do
   scenario 'confirming player choice and the game to choose its choice' do
-    visit('/')
-    fill_in :player, with: 'karho'
-    click_button 'submit'
+    sign_in_and_play
     fill_in :weapon, with: 'rock'
     click_button 'submit'
     expect(page).to have_content "karho's choice rock"
+  end
+
+  scenario 'player enters wrong selection' do
+    sign_in_and_play
+    fill_in :weapon, with: 'pie'
+    click_button
+    expect(page).to have_content "THAT IS NOT A WEAPON!"
   end
 end
