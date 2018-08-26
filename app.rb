6e8c8906cbfdd039c99b6b6415cff4fcf8776648
jdm79 <james.malvern@gmail.com>
2018-08-26 17:02:17 +0100

    adds sinatra base and RPS class name, running off rackup now

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 
-get '/' do
-  "<div style='border: 3px solid black; padding-left: 20px'>
+class RPS < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    "<div style='border: 3px solid black; padding-left: 20px'>
     <h1>Hello Rock, Paper and Scissors!</h1>
    </div>"
+  end
 end
