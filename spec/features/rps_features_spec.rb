8b25a413cfde6d0fea1456462ad1cae7e984f957
jdm79 <james.malvern@gmail.com>
2018-08-26 20:55:18 +0100

    fix typo - put expect block on new line

--- spec/features/rps_features_spec.rb
+++ spec/features/rps_features_spec.rb
@@ -8,7 +8,8 @@ feature "RPS Features" do
   scenario 'players can enter names, submit and see names on screen' do
     visit('/')
     fill_in('player_one', with: 'Dave')
-    click_on('Submit') expect(page).to have_content('Dave')
+    click_on('Submit') 
+    expect(page).to have_content('Dave')
   end
 
 end
