2f9e17313cce29b20acca70b0ae7acf6c95488ce
jdm79 <james.malvern@gmail.com>
2018-08-26 20:51:17 +0100

    adds new route which just shows a simple string message for now

--- app.rb
+++ app.rb
@@ -4,8 +4,11 @@ class RPS < Sinatra::Base
   enable :sessions
 
   get '/' do
-    "<div style='border: 3px solid black; padding-left: 20px'>
-    <h1>Hello, world!</h1>
-   </div>"
+    erb(:index)
   end
+
+  get '/names' do
+    "hello world!"
+  end
+
 end
