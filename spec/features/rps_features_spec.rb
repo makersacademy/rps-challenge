51bf3d7fee1ca2775f31e67af217b70104731b9e
jdm79 <james.malvern@gmail.com>
2018-08-26 20:53:50 +0100

    adds new test to check if name is submitted and shown on new play page

--- spec/features/rps_features_spec.rb
+++ spec/features/rps_features_spec.rb
@@ -5,10 +5,10 @@ feature "RPS Features" do
     expect(page).to have_content('Welcome to Rock, Paper and Scissors!')
   end
   
-  # scenario 'players can enter names, submit and see names on screen' do
-  #   visit('/')
-  #   fill_in('player_one', with: 'Dave')
-  #   click_on('Submit') expect(page).to have_content('Dave')
-  # end
+  scenario 'players can enter names, submit and see names on screen' do
+    visit('/')
+    fill_in('player_one', with: 'Dave')
+    click_on('Submit') expect(page).to have_content('Dave')
+  end
 
 end
