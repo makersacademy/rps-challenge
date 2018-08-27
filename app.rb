08f0290c17cb8193a80d010a0af41561204a5354
Samir Soormally <samirsoormally@gmail.com>
2018-08-27 09:37:53 +0100

    Class extraction to Game + rTests Extracted most functionality to go through Game class.
    Next step: Declare winner.

--- app.rb
+++ app.rb
@@ -9,20 +9,24 @@ class RPS < Sinatra::Base
   end
 
   post '/name' do
-    $player = Player.new(params[:player])
-    @player = $player
+     player1 = Player.new(params[:player])
+     @game = Game.create(player1)
+    # $player = Player.new(params[:player])
+    # @player = $player
     erb(:name)
   end
   # could make ai class to pass to game
 
   get '/game' do
-    @player = $player
+    @game = Game.instance
+    # @player = $player
     erb(:game)
   end
 
   post '/match' do
-    @player = $player
-    @player.choice_log << params[:choice]
+    @game = Game.instance
+    # @player = $player
+    @game.player.choice_log << params[:choice]
     erb(:match)
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,18 @@
 class Game
 
+attr_reader :player
+
   def initialize(player)
     @player = player
+    # needed?
+  end
+
+  def self.create(player)
+    @game = Game.new(player)
+  end
+
+  def self.instance
+    @game
   end
 
   def ai_choice
