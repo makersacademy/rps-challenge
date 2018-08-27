7dcd097c61aa33d0a63a37a7a1aed5dbcefea861
jdm79 <james.malvern@gmail.com>
2018-08-27 09:28:44 +0100

    name variable now being set as a session from the params and displayed on /play page

--- app.rb
+++ app.rb
@@ -8,12 +8,13 @@ class RPS < Sinatra::Base
   end
 
   post '/names' do
-    @name = params[:player_one]
+    session['player_one'] = params[:player_one]
+    p session['player_one']
     redirect '/play'
   end
 
   get '/play' do
-    @name = session[:player_one]
+    @name = session['player_one']
     erb(:play)
   end
 
