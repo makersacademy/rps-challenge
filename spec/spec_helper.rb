eb12383ea67947b5cc413a297905185a4ad70a58
shweta patil <shwetapatil@shwetas-MacBook-Pro.local>
2018-09-21 10:59:16 +0100

    updates spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
+# rack setup
+ENV['RACK_ENV'] = 'test'
+
+# require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require './app.rb'
+require 'capybara'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'rspec'
+
+Capybara.app = RPS
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
