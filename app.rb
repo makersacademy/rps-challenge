d84f5ee492613bc78fb6c5bd1ba6ede2d46696b2
Vu Le <core.safecrlf>
2018-09-21 15:11:56 +0100

    implemented code to pass test

--- app.rb
+++ app.rb
@@ -4,4 +4,9 @@ class RPS < Sinatra::Base
   get '/' do
     erb(:index)
   end
+
+  get '/play' do
+    @player1_name = params[:player1_name]
+    erb(:play)
+  end
 end
--- spec/play_spec.rb
+++ spec/play_spec.rb
@@ -1,3 +1,7 @@
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+
 feature 'update screen with player name' do
   scenario 'expect that player to be able to fill their name in a form and see their name after submitting it' do
     visit('/')
