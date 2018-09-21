525dc23f5354740c61418187931257ee222a2e6e
Robbie Campbell <roberto251983@gmail.com>
2018-09-21 16:02:51 +0100

    basic one player game complete

--- app.rb
+++ app.rb
@@ -1,15 +1,31 @@
 require 'sinatra/base'
 
+
 class Rps < Sinatra::Base
+  enable :sessions
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
     erb :play
   end
 
+  post '/rock' do
+    erb :rock
+  end
+  post '/paper' do
+    erb :paper
+  end
+  post '/scissors' do
+    erb :scissors
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/testing_infrastructure_working_spec.rb
+++ spec/features/testing_infrastructure_working_spec.rb
@@ -10,6 +10,41 @@ feature 'Enter names' do
     visit('/')
     fill_in :player_1_name, with: 'Robbie'
     click_button 'Submit'
-    expect(page).to have_content 'Robbie, PREPARE TO FIGHT!!'
+    expect(page).to have_content 'Robbie PREPARE TO FIGHT!!'
   end
+
+  scenario 'check page has RPS buttons' do
+    visit('/')
+    fill_in :player_1_name, with: 'Robbie'
+    click_button 'Submit'
+    expect(page).to have_selector(:link_or_button, 'Rock')
+    expect(page).to have_selector(:link_or_button, 'Paper')
+    expect(page).to have_selector(:link_or_button, 'Scissors')
+  end
+
+end
+
+feature 'fight' do
+  scenario 'choosing Rock' do
+    visit('/')
+    fill_in :player_1_name, with: 'Robbie'
+    click_button 'Submit'
+    click_button 'Rock'
+    expect(page).to have_content 'You chose rock!!'
+  end
+  scenario 'choosing Paper' do
+    visit('/')
+    fill_in :player_1_name, with: 'Robbie'
+    click_button 'Submit'
+    click_button 'Paper'
+    expect(page).to have_content 'You chose paper!!'
+  end
+  scenario 'choosing Rock' do
+    visit('/')
+    fill_in :player_1_name, with: 'Robbie'
+    click_button 'Submit'
+    click_button 'Scissors'
+    expect(page).to have_content 'You chose scissors!!'
+  end
+
 end
