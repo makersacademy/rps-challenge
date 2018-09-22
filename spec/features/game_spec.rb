ee2a7f2a41aac0ba94f9ab03407020ef25b64657
Andres Faraone <andresfaraone@outlook.com>
2018-09-22 22:59:33 +0100

    Mulitplayer test working

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,8 +1,23 @@
 require_relative 'web_helper'
-feature 'checks that name appears on play page' do
-  scenario 'player lose' do
-    sign_in
+feature 'multiplayer game' do
+  scenario 'p1 wins' do
+    sign_in_multiplayer
+    click_on 'paper'
     click_on 'rock'
-    expect(page).to have_content('has won')
+    expect(page).to have_content('John has won')
+  end
+
+  scenario 'p2 wins' do
+    sign_in_multiplayer
+    click_on 'scissors'
+    click_on 'rock'
+    expect(page).to have_content('James has won')
+  end
+
+  scenario 'tie' do
+    sign_in_multiplayer
+    click_on 'paper'
+    click_on 'paper'
+    expect(page).to have_content('The game is tied!')
   end
 end
