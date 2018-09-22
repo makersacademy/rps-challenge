fa5e97238d5de598944a4213a02994982b828ea5
Andres Faraone <andresfaraone@outlook.com>
2018-09-22 11:47:13 +0100

    Test for #move and #name

--- app.rb
+++
@@ -1,5 +0,0 @@
-require 'sinatra/base'
-
-class RPS < Sinatra::Base
-  run! if app_file == $0
-end
---
+++ spec/units/player_spec.rb
@@ -0,0 +1,18 @@
+require './app/models/player'
+
+describe Player do
+  subject { described_class.new('James')}
+  describe 'instantiation' do
+    it 'name is set to argument' do
+      expect(subject.name).to eq('James')
+    end
+  end
+
+  describe '#move' do
+    it { is_expected.to respond_to(:move)}
+    it '@move is set to argument' do
+      subject.move = :rock
+      expect(subject.move).to eq(:rock)
+    end
+  end
+end
