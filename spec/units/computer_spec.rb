78aa64f469150d19c6fba51c13b497b4f7687d0e
Andres Faraone <andresfaraone@outlook.com>
2018-09-22 17:21:18 +0100

    test for #move

--- spec/units/computer_spec.rb
+++ spec/units/computer_spec.rb
@@ -6,4 +6,12 @@ describe Computer do
   it '@name is set to "Computer"' do
     expect(subject.name).to eq("Computer")
   end
+
+  describe '#move=' do
+    it 'sets move to #choice' do
+      allow(subject).to receive(:choice).and_return(:rock)
+      subject.move = ""
+      expect(subject.move).to eq(:rock)
+    end
+  end
 end
