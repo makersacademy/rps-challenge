351e22509e9e5a5c7572db306d189813796b8e47
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-22 16:51:11 +0000

    added #who_won method to Game

--- lib/game.rb
+++ lib/game.rb
@@ -2,11 +2,32 @@ class Game
 
   attr_reader :players
 
-  def initialize(player1)
-    @players = [player1]
+  def initialize(player1, player2 = "AI")
+    @players = [player1, player2]
   end
 
   def store_move(selected_move)
     @players.first.store_move(selected_move)
+  end
+
+  def who_won
+    player1 = @players.first
+    player2 = @players.last
+    return :Draw if draw?(player1.move, player2.move)
+    return player1 if player1_wins?(player1_move, player2_move)
+    player2
+  end
+
+  private
+
+  def draw?(move1, move2)
+    move1 == move2
+  end
+
+  def player1_wins?(move1, move2)
+    return true if move1 == :rock && move2 == :scissors
+    return true if move1 == :scissors && move2 == :paper
+    return true if move1 == :paper && move2 == :rock
+    false
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,12 +2,13 @@ require 'game'
 
 describe Game do
 
-  let(:player1) { double :Player }
-  subject(:game) { described_class.new(player1) }
+  let(:player1) { double :Player, move: :rock }
+  let(:ai_player) { double :AI, move: :scissors }
+  subject(:game) { described_class.new(player1, ai_player) }
 
   context "#initialize" do
     it "should store players in an Array" do
-      expect(game.players).to eq [player1]
+      expect(game.players).to eq [player1, ai_player]
     end
   end
 
@@ -15,6 +16,12 @@ describe Game do
     it "calls Player#store_move" do
       expect(player1).to receive(:store_move)
       game.store_move("Rock")
+    end
+  end
+
+  context "#who_won" do
+    it "calculates who won" do
+      expect(game.who_won).to eq player1
     end
   end
 end
