072173e394071f8602e7516a8695c88228be6d98
Florence Hapgood <flohapgood@icloud.com>
2018-09-22 18:21:47 +0100

    Results paeg capybara tested

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -1,1 +1,11 @@
 
+feature "Results" do
+  scenario "Shows the player the result of the game" do
+    visit('/')
+    fill_in :name, with: "Florence"
+    click_button "Submit"
+    fill_in :choice, with: "Rock"
+    click_button "Submit"
+    expect(page).to have_content("The computer chose")
+  end
+end
