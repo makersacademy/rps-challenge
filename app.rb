9425193eebbfa7a70b42380310883cf32ecba6ff
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-22 13:20:33 +0000

    Added '/play' route

--- app.rb
+++ app.rb
@@ -12,4 +12,15 @@ class RPS < Sinatra::Base
   get '/names_input' do
     erb :names
   end
+
+  post '/store_names' do
+    player1 = Player.new(params[:player1_name])
+    session[:game] = Game.new(player1)
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:game].players.first.name
+    erb :play
+  end
 end
