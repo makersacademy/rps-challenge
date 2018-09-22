606d9f686cba8ec950b43bad290855b73068920e
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-22 17:33:05 +0000

    added AI victory test

--- spec/features/winner_spec.rb
+++ spec/features/winner_spec.rb
@@ -1,8 +1,15 @@
 require_relative 'web_helpers'
 
 feature "Winner page" do
-  scenario "displays the winner" do
+  scenario "displays the winner (Human)" do
+    allow(Kernel).to receive(:rand).and_return(2)
     from_welcome_to_winner
-    expect(page).to have_content "And the winner is"
+    expect(page).to have_content "And the winner is Dave"
+  end
+
+  scenario "displays the winner (AI)" do
+    allow(Kernel).to receive(:rand).and_return(1)
+    from_welcome_to_winner
+    expect(page).to have_content "And the winner is RPSbot"
   end
 end
