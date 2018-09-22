b69b1415d88ebb0a37b69ff260cddca39e71ea35
Andres Faraone <andresfaraone@outlook.com>
2018-09-22 18:19:25 +0100

    refactored with web_helper

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -1,8 +1,7 @@
+require_relative 'web_helper'
 feature 'checks that name appears on play page' do
   scenario do
-    visit('/')
-    fill_in 'name', with: 'John'
-    click_on('Go')
+    sign_in
     expect(page).to have_content('Hello John')
   end
 end
