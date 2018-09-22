02d906b9d1f652de0c6f16d94774cfb5b98ecbf6
NadiaAiraf <aidanfaria@gmail.com>
2018-09-22 17:02:30 +0100

    created winner method that fails if the players in the game have no move set

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,14 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
   end
+
+  def winner
+    raise 'Players have not made a move' if no_move
+  end
+
+  # private #----------------------------
+
+  def no_move
+    !(@player_1.move && @player_2.move)
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,6 +5,10 @@ describe Game do
   let(:scissor_sisters) { double :player }
   let(:rps) { described_class.new(dwayne_johnson, scissor_sisters) }
 
+  let(:no_move_1) { double :player1, move: nil }
+  let(:no_move_2) { double :player2, move: nil }
+  let(:rps_no_move) { described_class.new(no_move_1, no_move_2)}
+
   describe '#player_1' do
     it 'returns the player 1 object' do
       expect(rps.player_1).to eq dwayne_johnson
@@ -14,6 +18,12 @@ describe Game do
   describe '#player_2' do
     it 'returns the player 2 object' do
       expect(rps.player_2).to eq scissor_sisters
+    end
+  end
+
+  describe '#winner' do
+    it 'raises an error if either player does not have a move' do
+      expect{ rps_no_move.winner }.to raise_error 'Players have not made a move'
     end
   end
 end
