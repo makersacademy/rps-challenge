41a5857e7a54987269856312d48f963511395b26
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-22 15:39:58 +0000

    created new method to jump to winner page

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -2,3 +2,11 @@ def move_past_welcome
   visit '/'
   click_button 'Enter'
 end
+
+def from_welcome_to_winner
+  visit '/'
+  click_button 'Enter'
+  fill_in :player1_name, with: "Dave"
+  click_button "Play"
+  click_button "Rock"
+end
