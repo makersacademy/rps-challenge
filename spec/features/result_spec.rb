4974f05ba9acfb3f7c3b57c511e72f718f55590c
Damion Hardy <damionhardy@Damos-MBP-8.home>
2018-09-22 16:18:50 +0100

    committing new buttons as move choice instead of typing text, and related test changes

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -4,8 +4,7 @@ feature 'results page' do
   scenario 'declaring a drawn result' do
     begin_and_submit
     click_button 'START!'
-    fill_in :choice, with: 'r'
-    click_button 'Submit'
+    click_button 'rock'
     expect(page).to have_content("Please press start to play again...")
   end
 end
