8fd93b7f70386d1b29f506cb7299b7ea16759e71
shweta patil <shwetapatil@shwetas-mbp.home>
2018-09-22 12:26:27 +0100

    adds sessions and userstories

--- app.rb
+++ app.rb
@@ -1,21 +1,33 @@
 require 'sinatra/base'
-
+require './lib/player'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
   
-  post '/players' do
-    $player = params[:name]
-    erb(:players)
+  post '/play' do
+    session[:player_one] = Player.new(params[:player_one])
+    redirect '/play'
   end
   
-  post '/move' do
-    @move = params[:move]
-    puts @move
-    erb(:players)
+  get '/play' do
+    $player_one = session[:player_one]
+    erb(:play)
   end
+
+  post '/result' do
+    session[:player1_move] = params[:move]
+    redirect '/result'
+  end
+
+  get '/result' do
+    @player1_move = session[:player1_move]
+    erb(:result)
+  end
+
+ 
   run! if app_file == $0
 end
--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -1,19 +1,45 @@
 feature 'landing page' do
+
+  # As a marketeer
+  # So that I can enjoy myself away from the daily grind
+  # I would like to welcome to play rock/paper/scissors
   scenario 'Welcome message' do
     visit('/')
     expect(page).to have_content 'Welcome to Rock Paper Scissors'
   end
 
+  # As a marketeer
+  # So that I can see my name in lights
+  # I would like to register my name before playing an online game
+
   scenario 'updates form and registers player' do
     visit('/')
-    fill_in('name', with: "SHWETA")
+    fill_in('player_one', with: "SHWETA")
     click_button "Start game!"
     expect(page).to have_content 'Welcome : SHWETA!'
   end
 
+  # As a marketeer
+  # So I can play a game
+  # I want to see the moves I can play
+  scenario 'see the  options' do
+    visit('/')
+    fill_in('player_one', with: "SHWETA")
+    click_button 'Start game!'
+    expect(page).to have_selector("input[value='rock']")
+    expect(page).to have_selector("input[value='paper']")
+    expect(page).to have_selector("input[value='scissors']")
+    expect(page).to have_button 'Submit'
+    
+  end
+
+  # As a marketeer
+  # So I can play a game
+  # I want to choose a shape to play
+
   scenario 'Player chooses move and gets confirmation' do
     visit('/')
-    fill_in('name', with: "SHWETA")
+    fill_in('player_one', with: "SHWETA")
     click_button 'Start game!'
     choose 'rock'
     click_button 'Submit'
