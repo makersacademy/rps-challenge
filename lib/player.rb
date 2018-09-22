02d6de5e3e11243d1395a54b6c16ed4142531464
Florence Hapgood <flohapgood@icloud.com>
2018-09-22 11:42:23 +0100

    Player class with name

---
+++ lib/player.rb
@@ -0,0 +1,10 @@
+
+class Player
+
+  attr_reader :player
+
+  def initialize(name)
+    @player = name
+  end
+
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,10 @@
 require "./lib/player"
+
+describe Player do
+  describe "#initialize" do
+    it "knows it's own name" do
+      new_player = Player.new("Florence")
+      expect(new_player.player).to eq("Florence")
+    end
+  end
+end
