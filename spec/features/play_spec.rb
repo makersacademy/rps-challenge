92522ea991e574317f2b9c08a0b844d6b58c000d
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-22 13:33:00 +0000

    added move options to play view

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,11 +1,20 @@
 require_relative 'web_helpers'
 
 feature "Play page" do
-  scenario "entered name to be displayed on Play page" do
+  scenario "displays entered name" do
     move_past_welcome
     fill_in :player1_name, with: "Dave"
     click_button "Play"
     expect(page).to have_content "Dave vs Computer"
+  end
+
+  scenario "displays variety of possible moves" do
+    move_past_welcome
+    fill_in :player1_name, with: "Dave"
+    click_button "Play"
+    expect(page).to have_button "Rock"
+    expect(page).to have_button "Paper"
+    expect(page).to have_button "Scissors"
   end
 
 end
