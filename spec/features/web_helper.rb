3c801a43b2d9a1cc06c0374168465b277bc0d5c6
Andres Faraone <andresfaraone@outlook.com>
2018-09-22 23:10:38 +0100

    mutiplayer methods

--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -3,3 +3,14 @@ def sign_in
   fill_in 'name', with: 'John'
   click_on('Go')
 end
+
+def sign_in_multiplayer
+  visit('/')
+  fill_in 'name', with: 'John'
+  fill_in 'name2', with: 'James'
+  click_on('Go')
+end
+
+def make_choice(*choices)
+  choices.each { |choice| click_on choice } 
+end
