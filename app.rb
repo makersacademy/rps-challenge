f61230cc2612c5762f09606609529a41415d711d
NadiaAiraf <aidanfaria@gmail.com>
2018-09-22 15:18:24 +0100

    added rock/paper/scissors buttons and pages for each

--- app.rb
+++ app.rb
@@ -8,4 +8,8 @@ end
 get '/singleplayer' do
   erb :singleplayer
 end
+
+get '/singleplayer-game' do
+  erb :singleplayer_game
+end
 end
--- spec/features/infrastructure_spec.rb
+++ spec/features/infrastructure_spec.rb
@@ -11,3 +11,39 @@ feature 'homepage' do
     expect(page).to have_content "Rock Paper Scissors - Single Player"
   end
 end
+
+feature 'singleplayer' do
+  scenario 'lets you enter your name into the single player game' do
+    visit '/'
+    click_button 'singleplayer'
+    fill_in 'name', with: 'Dwayne Johnson'
+    click_button 'submit'
+    expect(page).to have_content 'Welcome Dwayne Johnson, Please enter your move:'
+  end
+end
+
+feature 'RPS - singleplayer' do
+  scenario 'has a button called rock' do
+    visit '/'
+    click_button 'singleplayer'
+    fill_in 'name', with: 'Dwayne Johnson'
+    click_button 'submit'
+    expect(page).to have_button 'rock'
+  end
+
+  scenario 'has a button called paper' do
+    visit '/'
+    click_button 'singleplayer'
+    fill_in 'name', with: 'Dwayne Johnson'
+    click_button 'submit'
+    expect(page).to have_button 'paper'
+  end
+
+  scenario 'has a button called scissors' do
+    visit '/'
+    click_button 'singleplayer'
+    fill_in 'name', with: 'Dwayne Johnson'
+    click_button 'submit'
+    expect(page).to have_button 'scissors'
+  end
+end
