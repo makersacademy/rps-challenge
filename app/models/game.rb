29c2a838f5ccdf292a4e4007b404b314e0ecfece
Andres Faraone <andresfaraone@outlook.com>
2018-09-22 17:21:53 +0100

    #calculate_winner, #tie?, #make_move

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,24 +1,54 @@
-class Game
-  attr_reader :p1, :p2, :players, :turn, :winner
+require_relative 'computer'
+require_relative 'player'
 
-  def initialize(p1, p2)
-    @p1       = p1
-    @p2       = p2
-    @players  = [@p1, @p2]
-    @turn     = @p1
+class Game
+  P1_WIN  = [[:rock, :scissors], [:scissors, :paper], [:paper, :rock]]
+
+  attr_reader :player1, :player2, :players, :on_turn, :winner
+
+  def initialize(player1, player2)
+    @player1  = player1
+    @player2  = player2
+    @players  = [@player1, @player2]
+    @on_turn  = @player1
   end
 
   def switch
-    self.turn = opposite_of(turn)
+    self.on_turn = off_turn
   end
 
-  def opposite_of(player)
-    players.detect { |choice| choice != player }
+  def off_turn
+    opposite_of(on_turn)
   end
 
   def loser
     opposite_of(winner)
   end
 
-  attr_writer :turn, :winner
+  def tie?
+    self.winner == :tie
+  end
+
+  def make_move(p1choice, p2choice)
+    player1.move = (p1choice)
+    player2.move = (p2choice)
+  end
+
+  def calculate_winner
+    play = [player1.move, player2.move]
+    if play[0] == play[1]
+      self.winner = :tie
+    elsif P1_WIN.include?(play)
+      self.winner = player1
+    else
+      self.winner = player2
+    end
+  end
+
+  private
+  attr_writer :on_turn, :winner
+
+  def opposite_of(player)
+    players.detect { |choice| choice != player }
+  end
 end
