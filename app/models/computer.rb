2aebf1c795256f1234c569b264315b338b24e1dc
Andres Faraone <andresfaraone@outlook.com>
2018-09-22 17:20:53 +0100

    add #move

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -4,4 +4,14 @@ class Computer < Player
   def initialize
     @name = "Computer"
   end
+
+  def move=(ignore)  #how to avoid parameter?
+    @move = choice
+  end
+
+  private
+  
+  def choice
+    [:rock, :paper, :scissors].sample
+  end
 end
