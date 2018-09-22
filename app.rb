98040d44f9e50e52852494c13eb152665f8c48de
djstephenson7 <dan_0502@hotmail.co.uk>
2018-09-22 12:00:48 +0100

    Submit and display player name

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ class Player < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/name' do
+    @name = params[:name]
+    puts params
+    erb :play
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,4 +8,13 @@ describe Player do
       expect(page).to have_content('Hi')
     end
   end
+
+  feature 'Player name' do
+    scenario 'entering the name' do
+      visit('/')
+      fill_in :name, with: "Daniel"
+      click_button "Submit"
+      expect(page).to have_content('Welcome, Daniel')
+    end
+  end
 end
