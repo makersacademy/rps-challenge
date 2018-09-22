693dc77da1d5662dd2029f5597cce092bc0998d8
Andres Faraone <andresfaraone@outlook.com>
2018-09-22 23:10:58 +0100

    multiplayer fixed

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -2,22 +2,33 @@ require_relative 'web_helper'
 feature 'multiplayer game' do
   scenario 'p1 wins' do
     sign_in_multiplayer
-    click_on 'paper'
-    click_on 'rock'
+    make_choice('paper', 'rock')
     expect(page).to have_content('John has won')
   end
 
   scenario 'p2 wins' do
     sign_in_multiplayer
-    click_on 'scissors'
-    click_on 'rock'
+    make_choice('scissors', 'rock')
     expect(page).to have_content('James has won')
   end
 
   scenario 'tie' do
     sign_in_multiplayer
-    click_on 'paper'
-    click_on 'paper'
+    make_choice('paper', 'paper')
     expect(page).to have_content('The game is tied!')
+  end
+
+  scenario 'new game started succesfully' do
+    sign_in_multiplayer
+    make_choice('paper', 'paper')
+    click_on 'Click here to start a new game!'
+    expect(page).to have_content('Player 1 - please enter your name!')
+  end
+
+  scenario 'new round started succesfully' do
+    sign_in_multiplayer
+    make_choice('paper', 'paper')
+    click_on 'Click here to play again!'
+    expect(page).to have_content('John vs. James')
   end
 end
--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -1,7 +1,12 @@
 require_relative 'web_helper'
 feature 'checks that name appears on play page' do
-  scenario do
+  scenario 'singleplayer' do
     sign_in
     expect(page).to have_content('John vs. Computer')
+  end
+
+  scenario 'multiplayer' do
+    sign_in_multiplayer
+    expect(page).to have_content('John vs. James')
   end
 end
