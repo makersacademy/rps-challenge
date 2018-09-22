085394b6ed59c55ce17a7a8c2a5d5fa1e1911caa
Antonio <antoniodaria@msn.com>
2018-09-22 17:39:04 +0100

    Player selects an option and gets confirmation

--- app.rb
+++ app.rb
@@ -15,8 +15,15 @@ class RpsWeb < Sinatra::Base
 
   get '/play' do
     @name = session[:name]
+    @shape = session[:shape]
     erb :play
   end
+
+  post '/play' do
+    session[:shape] = params[:shape]
+    redirect '/play'
+  end
+
 
   run! if app_file == $0
 end
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -4,9 +4,18 @@ feature "playing the game" do
   context "when plays the game" do
     scenario "player sees the options available" do
       visit('/play')
-      expect(page).to have_content 'Rock'
-      expect(page).to have_content 'Paper'
-      expect(page).to have_content 'Scissors'
+      expect(page).to have_button 'Rock'
+      expect(page).to have_button 'Paper'
+      expect(page).to have_button 'Scissors'
+    end
+  end
+
+  context "when player must choose an option" do
+    scenario "player selects a shape" do
+      visit('/play')
+      click_button "Rock"
+      #choice = "paper"
+      expect(page).to have_content 'You chose Rock!!'
     end
   end
 end
