553e473be2e24a57ce6af82a3bee015b2b577c78
Jon Urry <jon@urry.me>
2018-09-23 11:46:35 +0100

    Player chooses rock, paper, or scissors

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,23 @@
 class Player
   attr_reader :choice, :name, :score
-
+  CHOICES = [:rock, :paper, :scissors]
+  
   def initialize(name = '')
     @name = name
     @choice = nil
     @score = 0
+  end
+
+  def choose(choice)
+    msg = "Invalid choice. Should be one of " +
+          "#{Player::CHOICES}"
+    raise msg if invalid_choice?(choice)
+    @choice = choice
+  end
+
+  private
+
+  def invalid_choice?(choice)
+    !Player::CHOICES.include?(choice)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,4 +15,31 @@ describe Player do
       expect(player.choice).to be_nil
     end
   end
+
+  describe '#choose' do
+    context 'valid choices' do
+      it 'should accept :rock' do
+        player.choose(:rock)
+        expect(player.choice).to be :rock
+      end
+
+      it 'should accept :paper' do
+        player.choose(:paper)
+        expect(player.choice).to be :paper
+      end
+
+      it 'should accept :scissors' do
+        player.choose(:scissors)
+        expect(player.choice).to be :scissors
+      end
+    end
+
+    context 'invalid choice' do
+      it 'should not accept :banana' do
+        msg = "Invalid choice. Should be one of " +
+              "#{Player::CHOICES}"
+        expect { player.choose(:banana) }.to raise_error(msg)
+      end
+    end
+  end
 end
