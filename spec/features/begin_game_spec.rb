98f0facc138eac55f20cfd47f68259d0810f45bc
Emma Albury <emma_albury@hotmail.com>
2018-09-23 20:35:30 +0100

    wrote feature tests for submit and go buttons

--- spec/features/begin_game_spec.rb
+++ spec/features/begin_game_spec.rb
@@ -5,4 +5,12 @@ feature 'Begin Game' do
     submit_name_and_play
     expect(page).to have_content "Choose rock, paper or scissors."
   end
+
+  scenario 'User Chooses rock, paper or scissors' do
+    submit_name_and_play
+    fill_in :user_chooses, with: 'rock'
+    click_button 'GO!'
+    expect(page).to have_content 'You chose rock.'
+  end
+
 end
