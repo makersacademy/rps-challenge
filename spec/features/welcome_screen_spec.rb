061096833099f43580d026ade5fbeb20c0d66385
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-23 10:28:44 +0000

    added multiplayer option form

--- spec/features/welcome_screen_spec.rb
+++ spec/features/welcome_screen_spec.rb
@@ -3,10 +3,20 @@ feature "Welcome screen" do
     visit "/"
     expect(page).to have_content("Welcome to Rock, Paper, Scissors")
   end
-  
+
   scenario "submit button takes you to Name Input page" do
     visit "/"
+    select "Single player", :from => "play_mode"
     click_button "Enter"
     expect(page).to have_content("Enter your name:")
+  end
+
+  scenario "displays multiplayer option" do
+    visit "/"
+    select "Multiplayer", :from => "play_mode"
+    click_button "Enter"
+    expect(page).not_to have_content("Enter your name:")
+    expect(page).to have_content("Player 1 your name:")
+    expect(page).to have_content("Player 2 your name:")
   end
 end
