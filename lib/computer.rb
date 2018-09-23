2f60e2ede3ab086940c78b861251bdecd3328a4d
Darcie <darciewalsh@hotmail.com>
2018-09-23 11:09:07 +0100

    Computer now initialized with @choice and can be accessed

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,7 +1,14 @@
 class Computer
+  attr_accessor :choice
+
   CHOICES = [:rock, :paper, :scissors]
+
+  def initialize
+    @choice = rand_choice
+  end
 
   def rand_choice
     CHOICES.sample
   end
+
 end
