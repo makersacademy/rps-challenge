292ea05b50e201da7826cf04342621e2a5168adf
Cameron Whitehead <camjw119@gmail.com>
2018-09-23 18:59:48 +0100

    Added css, only README left to write

--- spec/features/replay_button_spec.rb
+++ spec/features/replay_button_spec.rb
@@ -2,7 +2,6 @@ feature 'The user can be consoled for a defeat' do
   scenario 'once the user has selected a move' do
     sign_in_one_player
     select_move('Paper')
-    expect(page).to have_content 'Unlucky, Billy.'
-    expect(page).to have_selector("input[type=submit][value='Go!']")
+    expect(page).to have_selector("input[type=submit][value='Yes!']")
   end
 end
