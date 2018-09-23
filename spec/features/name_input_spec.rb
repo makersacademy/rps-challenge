5494972d460405fb0e2553d6152e405467f7ae3a
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-23 11:01:42 +0000

    added Multiplayer name inputs

--- spec/features/name_input_spec.rb
+++ spec/features/name_input_spec.rb
@@ -2,8 +2,19 @@ require_relative 'web_helpers'
 
 feature "Name input page" do
   scenario "can have a name entered into a form" do
-    move_past_welcome
+    visit '/'
+    click_button 'Enter'
     fill_in :player1_name, with: "Dave"
+    expect(page).to have_button "Play"
+  end
+
+  scenario "multiplayer option can have a names entered into a form" do
+    visit "/"
+    select "Multiplayer", :from => "play_mode"
+    click_button "Enter"
+    fill_in :player1_name, with: "Dave"
+    fill_in :player2_name, with: "Tim"
     click_button "Play"
+    expect(page).to have_content "Dave vs Tim"
   end
 end
