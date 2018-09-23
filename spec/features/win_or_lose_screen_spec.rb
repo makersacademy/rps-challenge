d57f10d49bb597b5887ce9dc464783396c8b9e9f
Cameron Whitehead <camjw119@gmail.com>
2018-09-23 11:40:31 +0100

    Removed randomness from feature test

--- spec/features/win_or_lose_screen_spec.rb
+++ spec/features/win_or_lose_screen_spec.rb
@@ -1,7 +1,8 @@
 feature "Once a move is selected" do
   scenario "Users can see if they won or lost" do
     sign_in_and_play
+    srand(3)
     select_move("Rock")
-    expect(page).to have_content("You won, Billy!")
+    expect(page).to have_content("Well done, Billy!")
   end
 end
