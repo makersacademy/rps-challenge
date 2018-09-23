81a2a06e754bf4b326e510a72214cb8cac0b2a50
Andres Faraone <andresfaraone@outlook.com>
2018-09-23 01:26:38 +0100

    test for new move= method

--- spec/units/computer_spec.rb
+++ spec/units/computer_spec.rb
@@ -10,8 +10,14 @@ describe Computer do
   describe '#move=' do
     it 'sets move to #choice' do
       allow(subject).to receive(:choice).and_return(:rock)
-      subject.move = ""
+      subject.move = [:rock, :paper, :scissors]
       expect(subject.move).to eq(:rock)
+    end
+  end
+
+  describe '#computer?' do
+    it 'returns true' do
+      expect(subject.computer?).to eq(true)
     end
   end
 end
