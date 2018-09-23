84cb01ac14f774b5a1b6baceddb0e099afa7c443
Andres Faraone <andresfaraone@outlook.com>
2018-09-23 01:26:20 +0100

    tidied up

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,15 @@
 class Player
-  attr_reader :name
+  attr_reader   :name
   attr_accessor :move
-  
   def initialize(name)
     @name = name
+  end
+
+  def process_move(choice)
+    @move = choice
+  end
+
+  def computer?
+    false
   end
 end
