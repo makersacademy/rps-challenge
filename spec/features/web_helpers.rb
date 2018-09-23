e8ca5d65b46d9b48ff9b1dd5c6f9ce2194a65036
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-23 11:03:19 +0000

    added multiplayer web helper

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,12 +1,20 @@
 def move_past_welcome
   visit '/'
   click_button 'Enter'
+  fill_in :player1_name, with: "Dave"
+  click_button "Play"
 end
 
 def from_welcome_to_winner
+  move_past_welcome
+  click_button "Rock"
+end
+
+def move_past_welcome_multiplayer
   visit '/'
+  select "Multiplayer", :from => "play_mode"
   click_button 'Enter'
   fill_in :player1_name, with: "Dave"
+  fill_in :player2_name, with: "Tim"
   click_button "Play"
-  click_button "Rock"
 end
