56fbb95999c8c14aba40ca82448b4d85db8f7c68
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-09-24 11:26:37 +0100

    post code review rubric... added an extra feature test in results_spec

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -8,3 +8,14 @@ feature 'results page' do
     expect(page).to have_content("Please press start to play again...")
   end
 end
+
+feature 'results page' do
+  scenario 'correct drawn result on screen' do
+    allow_any_instance_of(Computer).to receive(:random_move).and_return('rock')
+    begin_and_submit
+    click_button 'START!'
+    # select('rock', from: 'choice')
+    click_button 'rock'
+    expect(page).to have_content("A DRAW!")
+  end
+end
