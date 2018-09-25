e85290f193e2952357138cf7d0004c658a0cc7d3
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-25 09:28:10 +0100

    refactor based on reviews

--- lib/game.rb
+++ lib/game.rb
@@ -4,14 +4,14 @@ class Game
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @key_beats_value = { 'Rock' => ['Scissors', 'Lizard'], 'Paper' =>
-      ['Rock', 'Spock'], 'Scissors' => ['Paper', 'Lizard'],
-      'Spock' => ['Scissors', 'Rock'], 'Lizard' => ['Spock', 'Paper'] }
+    @key_beats_value = { :Rock => [:Scissors, :Lizard], :Paper =>
+      [:Rock, :Spock], :Scissors => [:Paper, :Lizard],
+      :Spock => [:Scissors, :Rock], :Lizard => [:Spock, :Paper] }
   end
 
   def play
-    p1 = @player1.player_choice
-    p2 = @player2.player_choice
+    p1 = @player1.player_choice.to_sym
+    p2 = @player2.player_choice.to_sym
     if @key_beats_value[p1].include?(p2)
       'player1'
     elsif @key_beats_value[p2].include?(p1)
@@ -22,6 +22,6 @@ class Game
   end
 
   def computer_choice
-    ['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard'].sample
+    [:Rock, :Paper, :Scissors, :Spock, :Lizard].sample
   end
 end
