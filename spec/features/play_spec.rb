756dc1b2d7241902b212a3b6155fe67c629b4588
Emma Albury <emma_albury@hotmail.com>
2018-09-27 19:09:02 +0100

    added button feature tests

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,10 +1,24 @@
 require_relative 'web_helpers'
 
+
 feature 'Play' do
-  scenario 'User chooses to play again' do
+  scenario 'User chooses rock and to play again' do
     submit_name_and_play
     click_button 'ROCK!'
     click_button 'Play Again'
     expect(page).to have_content "Choose rock, paper or scissors."
   end
+
+  scenario 'Display users choice' do
+    submit_name_and_play
+    click_button 'PAPER!'
+    expect(page).to have_content "You chose PAPER!"
+  end
+
+  # scenario 'Display users choice' do
+  #   submit_name_and_play
+  #   allow(Player2).to receive(:random_turn).and_return("SCISSORS!")
+  #   visit('/play')
+  #   expect(page).to have_content "I chose SCISSORS!"
+  # end
 end
