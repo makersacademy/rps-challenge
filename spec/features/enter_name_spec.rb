e823197e5927c903e53c31339ba48eac32254245
Aimée Craig <aimeecraig1@gmail.com>
2018-10-18 11:07:51 +0100

    added feature test

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,17 @@
 feature 'Name' do
-  scenario 'enters a name' do
+  scenario 'enters a name and returns a welcome message' do
     visit('/')
     fill_in 'name', with: 'Gizmo'
-    click_button('Submit')
+    click_button 'Submit'
     expect(page).to have_content 'Welcome Gizmo, select a move!'
+  end
+
+  scenario 'displays the options for rock, paper, scissors' do
+    visit('/')
+    fill_in 'name', with: 'Gizmo'
+    click_button 'Submit'
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Paper'
+    expect(page).to have_button 'Scissors'
   end
 end
