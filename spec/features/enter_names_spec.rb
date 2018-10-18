e42433f87e4a2456f416bf817a89dedaffc14743
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-10-18 12:16:59 +0100

    fixed tests to display correct message, tests passing

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,6 +1,6 @@
 feature 'Enter name' do
   scenario 'Player can fill in their name' do
     sign_in_and_play
-    expect(page).to have_content "Marketeer: Imogen"
-  end 
+    expect(page).to have_content "Imogen vs Computer"
+  end
 end
