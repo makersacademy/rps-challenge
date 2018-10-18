1a3e768ea47ee64cbc966e713eb0b03a92c41427
Aimée Craig <aimeecraig1@gmail.com>
2018-10-18 10:38:33 +0100

    infrastructure test removed

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class Game < Sinatra::Base
   get '/' do
-    'Testing infrastructure working'
+    erb :index
   end
 
   run! if app_file == $0
--- spec/features/infrastructure_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Infrastructure' do
-  scenario 'tests the testing infrastructure' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working'
-  end
-end
