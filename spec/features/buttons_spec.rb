95993a12c201f35f90c00cd40133a6c1049cddf7
jebax <joel.eliot.baxter@gmail.com>
2018-10-18 16:06:30 +0100

    added weapon buttons

--- spec/features/buttons_spec.rb
+++ spec/features/buttons_spec.rb
@@ -1,6 +1,20 @@
 feature "button" do
-  scenario "should have a 'rock' button" do
+  before do
     enter_name_and_play
-    expect(page).to have_selector("ROCK")
+  end
+
+  scenario "should have a 'rock' button" do
+    rock = "input[type=submit][value='ROCK']"
+    expect(page).to have_selector rock
+  end
+
+  scenario "should have a 'paper' button" do
+    paper = "input[type=submit][value='PAPER']"
+    expect(page).to have_selector paper
+  end
+
+  scenario "should have a 'scissors' button" do
+    scissors = "input[type=submit][value='SCISSORS']"
+    expect(page).to have_selector scissors
   end
 end
