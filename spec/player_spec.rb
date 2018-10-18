98212f6e9f3a0f215b935cfaa3790b8c9fd3b5bf
Todd Langford-Archer <toddpla@gmail.com>
2018-10-18 12:02:23 +0100

    player class added - tests passing

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ describe Player do
   subject {described_class.new(name: 'James')}
 
   it 'should create a player with given name' do
-    expect(subject.name).to eq 'Todd'
+    expect(subject.name).to eq 'James'
   end
 
 end
