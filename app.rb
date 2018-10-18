e1b4da4acf68b6db0f635edac73ba2971c753886
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-18 18:26:09 +0100

    18-10-2018 @6pm: Tests for first two pages written and passing. Some html refactoring required.

--- app.rb
+++ app.rb
@@ -8,9 +8,24 @@ class Controller < Sinatra::Base
     erb :index
   end
 
-  post '/names' do
+  post '/name' do
     session[:name] = params[:player_name]
-    # redirect '/greet'
+    redirect 'game_type'
+  end
+
+  get '/game_type' do
+    @player_1 = session[:name]
+    erb :game_type
+  end
+
+  post '/game_option' do
+    session[:game] = params[:game]
+    redirect '/rps_game'
+  end
+
+  get '/rps_game' do
+    @game = session[:game]
+    erb :rps_game
   end
 
   run! if app_file == $0
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -5,7 +5,14 @@ feature 'Enter name' do
   end
 
   scenario 'submitting names' do
+    sign_in
     expect(page).to have_content 'Ned Stark'
   end
 
+  scenario 'choosing game "Human vs. Machine"' do
+    sign_in
+    click_button('Human Vs. Machine')
+    expect(page).to have_content 'Human Vs. Machine'
+  end
+  
 end
