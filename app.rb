20ccf3a5655e169de8a013e221e0585776012983
Caitlin Cooling <ccooling97@gmail.com>
2018-10-19 08:46:18 +0100

    Fleshed out the winner tests to test ever scenario

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/base'
+require_relative './lib/game'
+require_relative './lib/winner'
 
 class RPS < Sinatra::Base
 
@@ -16,17 +18,29 @@ class RPS < Sinatra::Base
   end
 
    post '/game/rock' do
-     $user_move = "rock"
+     @user_move = "rock"
+     game = Game.new(['rock', 'paper', 'scissors'] )
+     winner = Winner.new
+     @game_move = game.move
+     @winner = winner.determine(game.move, @user_move)
      erb(:win_or_lose)
    end
 
    post '/game/paper' do
-     $user_move = "paper"
+     @user_move = "paper"
+     game = Game.new(['rock', 'paper', 'scissors'] )
+     winner = Winner.new
+     @game_move = game.move
+     @winner = winner.determine(game.move, @user_move)
      erb(:win_or_lose)
    end
 
    post '/game/scissors' do
-     $user_move = "scissors"
+     @user_move = "scissors"
+     game = Game.new(['rock', 'paper', 'scissors'] )
+     winner = Winner.new
+     @game_move = game.move
+     @winner = winner.determine(game.move, @user_move)
      erb(:win_or_lose)
    end
 
--- spec/winner_spec.rb
+++ spec/winner_spec.rb
@@ -3,22 +3,57 @@ require 'winner'
 describe Winner do
   let(:winner) { Winner.new }
 
-  it 'should determine user as the winner' do
-    game = double(:game, move: "rock")
-    user_move = "paper"
-    expect(winner.determine(game.move, user_move)).to eq "You won!"
+  context 'game is rock' do
+    let(:game) { double(:game, move: "rock") }
+    it 'should determine user as the winner' do
+      user_move = "paper"
+      expect(winner.determine(game.move, user_move)).to eq "You won!"
+    end
+
+    it 'should determine user as the loser' do
+      user_move = "scissors"
+      expect(winner.determine(game.move, user_move)).to eq "You lost!"
+    end
+
+    it 'should determine that it is a draw' do
+      user_move = "rock"
+      expect(winner.determine(game.move, user_move)).to eq "It's a draw!"
+    end
   end
 
-  it 'should determine user as the loser' do
-    game = double(:game, move: "rock"
-    user_move = "scissors"
-    expect(winner.determine(game.move, user_move)).to eq "You lost!"
+  context 'game is paper' do
+    let(:game) { double(:game, move: "paper") }
+    it 'should determine that it is a draw' do
+      user_move = "paper"
+      expect(winner.determine(game.move, user_move)).to eq "It's a draw!"
+    end
+
+    it 'should determine user as the winner' do
+      user_move = "scissors"
+      expect(winner.determine(game.move, user_move)).to eq "You won!"
+    end
+
+    it 'should determine user as the loser' do
+      user_move = "rock"
+      expect(winner.determine(game.move, user_move)).to eq "You lost!"
+    end
   end
 
-  it 'should determine that it is a draw' do
-    game = double(:game, move: "rock")
-    user_move = "rock"
-    expect(winner.determine(game.move, user_move)).to eq "It's a draw!"
-  end
+  context 'game is scissors' do
+    let(:game) { double(:game, move: "scissors") }
+    it 'should determine user as the loser' do
+      user_move = "paper"
+      expect(winner.determine(game.move, user_move)).to eq "You lost!"
+    end
 
+    it 'should determine its a draw' do
+      user_move = "scissors"
+      expect(winner.determine(game.move, user_move)).to eq "It's a draw!"
+    end
+
+    it 'should determine user as the winner' do
+      user_move = "rock"
+      expect(winner.determine(game.move, user_move)).to eq "You won!"
+    end
+  end
 end
