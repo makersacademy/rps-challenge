4c2e8a41b637cd0dd79a0793aad99551ee9be14e
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-19 14:36:35 +0100

    Added rock paper scissors winner logic

--- lib/game.rb
+++ lib/game.rb
@@ -7,4 +7,35 @@ class Game
     @player_2 = player_2
   end
 
+  def winner
+    case @player_1.choice
+    when 'rock'
+      case @player_2.choice
+      when 'rock'
+        'draw'
+      when 'paper'
+        @player_2
+      when 'scissors'
+        @player_1
+      end
+    when 'paper'
+      case @player_2.choice
+      when 'rock'
+        @player_1
+      when 'paper'
+        'draw'
+      when 'scissors'
+        @player_2
+      end
+    when 'scissors'
+      case @player_2.choice
+      when 'rock'
+        @player_2
+      when 'paper'
+        @player_1
+      when 'scissors'
+        'draw'
+      end
+    end
+  end
 end
