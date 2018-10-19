521918799d504c0f8606b73eed99061585990c24
Todd Langford-Archer <toddpla@gmail.com>
2018-10-19 22:54:25 +0100

    feature test for selecting an option added

---
+++ spec/features/player_selects_option_spec.rb
@@ -0,0 +1,8 @@
+feature 'player selects option' do
+  scenario 'selects rock' do
+    sign_in_and_play
+    choose('Rock')
+    click_button('Submit')
+    expect(page).to have_content('Current Player: James')
+  end
+end
