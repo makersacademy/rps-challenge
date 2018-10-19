199fbe1021c28fe05a8a6034428d995d11af350c
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-10-19 15:10:07 +0100

    sessions enabled

--- rps_app.rb
+++ rps_app.rb
@@ -1,15 +1,21 @@
 require 'sinatra/base'
 
 class Game < Sinatra::Base
-
+  enable :sessions
   get '/' do
-    erb(:index)
+    erb :index
   end
 
   post '/names' do
-    @player_one_name = params[:player_one_name]
-    @player_two_name = params[:player_two_name]
-    erb(:play)
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,7 @@
+Capybara.app = Game
 feature 'Testing infrastructure' do
-
   scenario 'can run app and check page content' do
     visit('/')
     expect(page).to have_content
   end
-
 end
