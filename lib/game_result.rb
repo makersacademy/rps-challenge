c9f2a18e0dfec2b40adf26e87d2227d6429bce44
shannongamby <shannongamby@hotmail.com>
2018-10-19 22:57:54 +0100

    refactor

--- lib/game_result.rb
+++ lib/game_result.rb
@@ -1,26 +1,25 @@
 class GameResult
 
+  @possible_outcomes = {
+                         ['Rock', 'Paper'] => 'win',
+                         ['Rock', 'Scissors'] => 'lose',
+                         ['Paper', 'Scissors'] => 'win',
+                         ['Paper', 'Rock'] => 'lose',
+                         ['Scissors', 'Rock'] => 'win',
+                         ['Scissors', 'Paper'] => 'lose',
+                         ['Scissors', 'Scissors'] => 'tie',
+                         ['Paper', 'Paper'] => 'tie',
+                         ['Rock', 'Rock'] => 'tie',
+                       }
+
   def self.calculate(player_choice, game_choice)
     @player_choice = player_choice
     @game_choice = game_choice
-    when_choices_are_not_equal
-    when_choices_are_equal
+    check_possible_outcomes
     @result
   end
 
-  def self.when_choices_are_not_equal
-    outcomes = {
-                  ['Rock', 'Paper'] => 'win',
-                  ['Rock', 'Scissors'] => 'lose',
-                  ['Paper', 'Scissors'] => 'win',
-                  ['Paper', 'Rock'] => 'lose',
-                  ['Scissors', 'Rock'] => 'win',
-                  ['Scissors', 'Paper'] => 'lose',
-                }
-    @result = outcomes[[@player_choice, @game_choice]]
-  end
-
-  def self.when_choices_are_equal
-    @result = 'tie' if @player_choice == @game_choice
+  def self.check_possible_outcomes
+    @result = @possible_outcomes[[@player_choice, @game_choice]]
   end
 end
