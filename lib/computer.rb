34f24ef30fda2f8883e16ea9308feb768e31ba04
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-19 14:51:14 +0100

    Moved computer's choice to instance variable

--- lib/computer.rb
+++ lib/computer.rb
@@ -2,13 +2,13 @@ class Computer
 
   CHOICES = ['rock', 'paper', 'scissors']
 
-  attr_reader :name
+  attr_reader :name, :choice
 
   def initialize
     @name = "Computer"
   end
 
-  def choice
+  def random_choice
     @choice = CHOICES.sample
   end
 
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -10,9 +10,18 @@ describe Computer do
 
   end
 
+  describe '#random_choice' do
+
+    it 'should return a choice' do
+      expect(subject.random_choice).to eq('rock').or eq('scissors').or eq('paper')
+    end
+
+  end
+
   describe '#choice' do
 
     it 'should return a choice' do
+      subject.random_choice
       expect(subject.choice).to eq('rock').or eq('scissors').or eq('paper')
     end
 
