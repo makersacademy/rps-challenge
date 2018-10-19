c072ceb17b1dadac20ab7093860cc6bd09df6680
hurc87 <zoe1987@btinternet.com>
2018-10-19 14:42:10 +0100

    more tidying up

--- app.rb
+++ app.rb
@@ -1,30 +1,68 @@
-require 'sinatra'
-require 'sinatra/base'
-require './lib/player.rb'
-
-class RPS < Sinatra::Base
-
-  enable :sessions
-
-  get '/' do
-    erb :index
-  end
-
-  post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    redirect '/play'
-  end
-
-  get '/play' do
-    @player_1_name = $player_1.name
-    erb :play
-  end
-
-  get '/attack' do
-    @player_1_name = $player_1.name
-    erb:attack
-  end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+class Game
+def initialize
+choice = [:rock, :paper, :scissors]
+@draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
+@win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
+@outcome = []
 end
+
+def win(player)
+player.win_point
+end
+
+def computers_turn(arr)
+arr.sample
+@player_1 = computers_turn(choice)
+end
+
+def player_1_turn(arr)
+arr.sample
+@player_2 = player_1_turn(choice)
+end
+
+def outcome
+@outcome.push(@player_1, @player_2)
+end
+
+def is_draw
+@draw.include?@outcome
+end
+
+def play_1_win
+@win_play_1.include?@outcome
+end
+
+def player_1_outcome
+puts "Player 1 has won"
+end
+
+def player_2_outcome
+puts "Player 2 has won"
+end
+
+def result
+  if is_draw == true
+    puts "Its a draw"
+  elsif play_1_win == true
+    player_1_outcome
+  else
+    player_2_outcome
+  end
+end
+
+end
+
+game = Game.new
+game.result
+
+# class Player
+# DEFAULT_WINS = 0
+# attr_reader :name, :wins
+
+# def initialize(name, wins = DEFAULT_WINS)
+# @name = name
+# @wins = wins
+# end
+
+
+# end
--- messy_game.rb
+++ messy_game.rb
@@ -1,46 +1,120 @@
-choice = [:rock, :paper, :scissors]
-@draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
-@win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
-@outcome = []
+# choice = [:rock, :paper, :scissors]
+# @draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
+# @win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
+# @outcome = []
 @player_1_wins = 0
 @player_2_wins = 0
 
 
+# def computers_turn(arr)
+#   arr.sample
+# end
+#
+# def player_1_turn(arr)
+#   arr.sample
+# end
+
+# @player_1 = computers_turn(choice)
+# @player_2 = player_1_turn(choice)
+#
+# @outcome.push(@player_1, @player_2)
+#
+# def is_draw
+#   @draw.include?@outcome
+# end
+#
+# def play_1_win
+#   @win_play_1.include?@outcome
+# end
+#
+# def player_1_outcome
+#   @player_1_wins += 1
+#   puts "Player 1 has won"
+# end
+#
+# def player_2_outcome
+#   @player_2_wins += 1
+#   puts "Player 2 has won"
+# end
+
+# def result
+#   if is_draw == true
+#     puts "Its a draw"
+#   elsif play_1_win == true
+#     player_1_outcome
+#   else
+#     player_2_outcome
+#   end
+# end
+
+def score
+  result
+  puts "Player 1 has scored #{@player_1_wins} and Player 2 has scored #{@player_2_wins}"
+end
+
+
+class Game
+def initialize
+choice = [:rock, :paper, :scissors]
+@draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
+@win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
+@outcome = []
+@player_1 = computers_turn(choice)
+@player_2 = player_1_turn(choice)
+end
+
+def win(player)
+player.win_point
+end
+
 def computers_turn(arr)
-  arr.sample
+arr.sample
 end
 
 def player_1_turn(arr)
-  arr.sample
+arr.sample
 end
-
-@player_1 = computers_turn(choice)
-@player_2 = player_1_turn(choice)
 
 @outcome.push(@player_1, @player_2)
 
 def is_draw
-  @draw.include?@outcome
+@draw.include?@outcome
 end
 
 def play_1_win
-  @win_play_1.include?@outcome
+@win_play_1.include?@outcome
+end
+
+def player_1_outcome
+puts "Player 1 has won"
+end
+
+def player_2_outcome
+puts "Player 2 has won"
 end
 
 def result
   if is_draw == true
     puts "Its a draw"
   elsif play_1_win == true
-    @player_1_wins += 1
-    puts "Player 1 has won"
+    player_1_outcome
   else
-    @player_2_wins += 1
-    puts "Player 2 has won"
+    player_2_outcome
   end
 end
 
-def score
-  result
-  puts @player_1_wins
-  puts @player_2_wins
 end
+
+
+
+# class Player
+# DEFAULT_WINS = 0
+# attr_reader :name, :wins
+
+# def initialize(name, wins = DEFAULT_WINS)
+# @name = name
+# @wins = wins
+# end
+
+
+# end
