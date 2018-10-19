d6bde5bc3c293cf0abdcef4b1b57d81f3fc03e72
hurc87 <zoe1987@btinternet.com>
2018-10-19 12:42:14 +0100

    changed to symbols

--- messy_game.rb
+++ messy_game.rb
@@ -1,4 +1,4 @@
-arr = ["rock", "paper", "scissors"]
+arr = [:rock, :paper, :scissors]
 
 def computers_turn(arr)
   arr.sample
@@ -12,17 +12,17 @@ player_1 = computers_turn(arr)
 player_2 = player_1_turn(arr)
 
 
-if player_1 == "rock" && player_2 == "rock"
+if player_1 == :rock && player_2 == :rock
   puts "It's a draw"
-elsif player_1 == "paper" && player_2 == "paper"
+elsif player_1 == :paper && player_2 == :paper
   puts "It's a draw"
-elsif player_1 == "scissors" && player_2 == "scissors"
+elsif player_1 == :scissors && player_2 == :scissors
   puts "It's a draw"
-elsif player_1 == "rock" && player_2 == "scissors"
+elsif player_1 == :rock && player_2 == :scissors
   puts "Player 1 wins"
-elsif player_1 == "paper" && player_2 == "rock"
+elsif player_1 == :paper && player_2 == :rock
   puts "Player 1 wins"
-elsif player_1 == "scissors" && player_2 == "paper"
+elsif player_1 == :scissors && player_2 == :paper
   puts "Player 1 wins"
 else
   puts "Player 2 wins"
