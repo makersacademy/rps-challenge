0640b84ac8294f8854928efb52e3bcfd34ffdc76
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 14:00:07 +0100

    reordered tests

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,18 +1,18 @@
 require 'computer'
 
 describe Computer do
-  context '#move' do
-    it 'generates a move' do
-      srand(1)
-      expect(subject.pick_move).to eq "Paper"
-    end
-  end
-
   context '#initialize' do
     it 'retrieves a move' do
       srand(1)
       subject.pick_move
       expect(subject.move).to eq "Paper"
+    end
+  end
+  
+  context '#move' do
+    it 'generates a move' do
+      srand(1)
+      expect(subject.pick_move).to eq "Paper"
     end
   end
 end
