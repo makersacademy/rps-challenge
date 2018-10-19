6efdaf1c82a425082575bc38dd7737d095b75b15
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-19 14:00:02 +0100

    Added move method to player

--- lib/player.rb
+++ lib/player.rb
@@ -3,7 +3,11 @@ class Player
   attr_reader :name
 
   def initialize(name)
-    @name
+    @name = name
+  end
+
+  def move(choice)
+    @choice = choice
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,4 +2,14 @@ require 'player'
 
 describe Player do
 
+  subject { Player.new("Mel") }
+
+  it 'should return a name' do
+    expect(subject.name).to eq("Mel")
+  end
+
+  it 'should return a move' do
+    expect(subject.move('rock')).to eq('rock')
+  end
+
 end
