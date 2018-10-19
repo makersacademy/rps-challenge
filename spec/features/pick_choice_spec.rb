0890292ef82eecf42fbcecf47f5b8ae87bed7191
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-19 11:05:09 +0100

    Added tests for selecting paper or scissors

--- spec/features/pick_choice_spec.rb
+++ spec/features/pick_choice_spec.rb
@@ -7,4 +7,23 @@ feature 'Selecting choice' do
     click_button 'Submit'
     expect(page).to have_content 'Mel chose rock'
   end
+
+  scenario 'Player can select Paper' do
+    visit('/')
+    fill_in :name, with: 'Mel'
+    click_button 'Submit'
+    page.select 'Paper', from: 'choice'
+    click_button 'Submit'
+    expect(page).to have_content 'Mel chose paper'
+  end
+
+  scenario 'Player can select Scissors' do
+    visit('/')
+    fill_in :name, with: 'Mel'
+    click_button 'Submit'
+    page.select 'Scissors', from: 'choice'
+    click_button 'Submit'
+    expect(page).to have_content 'Mel chose scissors'
+  end
+
  end
