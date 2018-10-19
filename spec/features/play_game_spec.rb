8b948b2024f39786a47a5cb36ecee8db39cac25a
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 15:53:18 +0100

    feature test added

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,11 +1,22 @@
 feature 'Play game' do
   scenario 'player chooses a move and gets a win screen' do
+    allow_any_instance_of(Array).to receive(:sample) { "Scissors" }
     visit '/'
     fill_in 'name', with: "Mittens"
     click_button 'Submit'
     select 'Rock', from: 'move'
-    allow_any_instance_of(Array).to receive(:sample) { "Scissors" }
     click_button 'Submit'
     expect(page).to have_content 'You win!'
+  end
+
+  scenario "page shows the player's and the computer's move" do
+    allow_any_instance_of(Array).to receive(:sample) { "Scissors" }
+    visit '/'
+    fill_in 'name', with: "Mittens"
+    click_button 'Submit'
+    select 'Rock', from: 'move'
+    click_button 'Submit'
+    expect(page).to have_content 'You picked Rock'
+    expect(page).to have_content 'The computer picked Scissors'
   end
 end
