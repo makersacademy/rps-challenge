88aea8772be8074551087aac5c30b0ef598c9a5e
hurc87 <zoe1987@btinternet.com>
2018-10-19 21:07:00 +0100

    added second player

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/base'
 require './lib/player.rb'
 require './lib/game.rb'
+require './lib/game_2.rb'
 require './lib/computer.rb'
 
 class RPS < Sinatra::Base
@@ -9,6 +10,8 @@ class RPS < Sinatra::Base
   enable :sessions
 
   $wins = 0
+  $wins_p1 = 0
+  $wins_p2 = 0
 
   get '/' do
     erb :index
@@ -19,8 +22,18 @@ class RPS < Sinatra::Base
     redirect '/play'
   end
 
+  post '/names_double' do
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    redirect '/play_2'
+  end
+
   get '/single_player' do
     erb :single_player
+  end
+
+  get '/double_player' do
+    erb :double_player
   end
 
   get '/play' do
@@ -28,10 +41,28 @@ class RPS < Sinatra::Base
     erb :play
   end
 
+  get '/play_2' do
+    $player_1_name = $player_1.name
+    $player_2_name = $player_2.name
+    erb :play_2
+  end
+
   post '/choice' do
     p params[:choice]
     $player_1_choice = params[:choice]
     redirect '/attack'
+  end
+
+  post '/choice_2' do
+    p params[:choice]
+    $player_1_choice = params[:choice]
+    erb :play_2_2
+  end
+
+  post '/choice_3' do
+    p params[:choice]
+    $player_2_choice = params[:choice]
+    redirect '/attack_2'
   end
 
   get '/computer_choice' do
@@ -43,8 +74,17 @@ class RPS < Sinatra::Base
 
   get '/attack' do
     $player_1_name = $player_1.name
-    @player1_choice = $game
+    $player1_choice = $game
     erb :attack
+  end
+
+  get '/attack_2' do
+    $player_1_name = $player_1.name
+    $player_2_name = $player_2.name
+    p $player_1_choice
+    p $player_2_choice
+    $winner_two = Two_player.new.winner_2
+    erb :attack_2
   end
 
   get '/game_over' do
---
+++ lib/game_2.rb
@@ -0,0 +1,42 @@
+class Two_player
+
+  def winner_2
+    if $player_1_choice == $player_2_choice
+      return "Its a draw"
+    elsif $player_1_choice == "Rock" && $player_2_choice == "Scissors"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Rock" && $player_2_choice == "Lizard"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Paper" && $player_2_choice == "Rock"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Paper" && $player_2_choice == "Spock"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Scissors" && $player_2_choice == "Paper"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Scissors" && $player_2_choice == "Lizard"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Spock" && $player_2_choice == "Scissors"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Spock" && $player_2_choice == "Rock"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Lizard" && $player_2_choice == "Spock"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    elsif $player_1_choice == "Lizard" && $player_2_choice == "Paper"
+      $wins_p1 +=1
+      return "#{$player_1_name} wins"
+    else
+      $wins_p2 +=1
+      return "#{$player_2_name} wins"
+    end
+  end
+
+end
