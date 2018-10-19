a57d812651779ef47cd4b2bf0d1909d9c5534c12
Muna <mmhussein125@gmail.com>
2018-10-19 16:13:47 +0100

    User Story 1: Register name before playing

---
+++ app/rps_web.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+
+class RPSWeb < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+
+end
