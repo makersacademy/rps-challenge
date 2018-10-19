f50f3589674b85793f32c6886daafcd3712e4fc7
hurc87 <zoe1987@btinternet.com>
2018-10-19 18:38:04 +0100

    changed game completely, back to if statement, but can do full round

--- lib/game.rb
+++ lib/game.rb
@@ -1,17 +1,21 @@
 class Game
-DEFAULT_WINS = 0
 
-  def initialize(name, wins = DEFAULT_WINS)
-    @name = name
-    @wins = wins
-  end
+  RULES = { rock: :scissors,
+            paper: :rock,
+            scissors: :paper }
 
-  def win(player)
-    player.win_point
-  end
-
-  def player_1_turn
-
+  def winner
+    if $player_1_choice == $computer_choice
+      return "Its a draw"
+    elsif $player_1_choice == :rock && $computer_choice == :scissors
+      return "Player 1 wins"
+    elsif $player_1_choice == :paper && $computer_choice == :rock
+      return "Player 1 wins"
+    elsif $player_1_choice == :scissors && $computer_choice == :paper
+       return "Player 1 wins"
+    else
+      return "The computer wins"
+    end
   end
 
 end
--- messy_game.rb
+++ messy_game.rb
@@ -1,51 +1,3 @@
-# choice = [:rock, :paper, :scissors]
-# @draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
-# @win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
-# @outcome = []
-@player_1_wins = 0
-@player_2_wins = 0
-
-
-# def computers_turn(arr)
-#   arr.sample
-# end
-#
-# def player_1_turn(arr)
-#   arr.sample
-# end
-
-# @player_1 = computers_turn(choice)
-# @player_2 = player_1_turn(choice)
-#
-# @outcome.push(@player_1, @player_2)
-#
-# def is_draw
-#   @draw.include?@outcome
-# end
-#
-# def play_1_win
-#   @win_play_1.include?@outcome
-# end
-#
-# def player_1_outcome
-#   @player_1_wins += 1
-#   puts "Player 1 has won"
-# end
-#
-# def player_2_outcome
-#   @player_2_wins += 1
-#   puts "Player 2 has won"
-# end
-
-# def result
-#   if is_draw == true
-#     puts "Its a draw"
-#   elsif play_1_win == true
-#     player_1_outcome
-#   else
-#     player_2_outcome
-#   end
-# end
 
 def score
   result
@@ -54,14 +6,13 @@ end
 
 
 class Game
-def initialize
-choice = [:rock, :paper, :scissors]
-@draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
-@win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
-@outcome = []
-@player_1 = computers_turn(choice)
-@player_2 = player_1_turn(choice)
-end
+# def initialize
+# @draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
+# @win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
+# @outcome = []
+# @player_1 = computers_turn(choice)
+# @player_2 = player_1_turn(choice)
+# end
 
 def win(player)
 player.win_point
@@ -121,7 +72,6 @@ end
 
 class Game
 def initialize
-choice = [:rock, :paper, :scissors]
 @draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
 @win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
 @outcome = []
