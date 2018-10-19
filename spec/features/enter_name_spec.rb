49447afff399c1481c71cc4fbb168f84a6b88328
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 10:36:02 +0100

    code nuked

--- spec/features/enter_name_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Name' do
-  scenario 'enters a name and returns a welcome message' do
-    input_name
-    expect(page).to have_content 'Welcome Gizmo, select a move!'
-  end
-end
--- spec/features/web_helpers.rb
+++
@@ -1,5 +0,0 @@
-def input_name
-  visit('/')
-  fill_in 'name', with: 'Gizmo'
-  click_button 'Submit'
-end
