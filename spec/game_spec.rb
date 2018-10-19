fef9bb9222eb14afb4b56b3fbd35e062ef474ca1
Anouska Hopkins <anouskahopkins@gmail.com>
2018-10-19 17:59:17 +0100

    Added 2 more tests to game spec to test for you lose and you draw

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,5 +9,17 @@ describe Game do
       game.player.pick("paper")
       expect(game.outcome).to eq "You win"
     end
+    it 'should tell the player you win' do
+      computer = double("computer", :current_pick => "paper")
+      game = Game.new("steve", computer)
+      game.player.pick("rock")
+      expect(game.outcome).to eq "You lose"
+    end
+    it 'should tell the player you win' do
+      computer = double("computer", :current_pick => "scissors")
+      game = Game.new("steve", computer)
+      game.player.pick("scissors")
+      expect(game.outcome).to eq "You draw"
+    end
   end
 end
