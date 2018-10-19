061215f99b13a959944f696c93b4cb87c008d0d1
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 11:27:26 +0100

    unit test added

---
+++ spec/game_spec.rb
@@ -0,0 +1,13 @@
+require 'game'
+
+describe Game do
+  let(:player) { double :player }
+  let(:computer) { double :computer }
+
+  context '#initialize' do
+    it 'creates a new game with 1 player and 1 computer' do
+      expect(subject.player).to eq player
+      expect(subject.computer).to eq computer
+    end
+  end
+end
