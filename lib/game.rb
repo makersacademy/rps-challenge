e8e26cc2d6d43eee8b5691c35e65521b271a8fe9
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 11:29:34 +0100

    code amended to pass unit test

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,8 @@
 class Game
-
+  attr_reader :player, :computer
+  
+  def initialize(player, computer = Computer.new)
+    @player = player
+    @computer = computer
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,7 @@ require 'game'
 describe Game do
   let(:player) { double :player }
   let(:computer) { double :computer }
+  subject { Game.new(player, computer) }
 
   context '#initialize' do
     it 'creates a new game with 1 player and 1 computer' do
