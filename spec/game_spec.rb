e34ddacfa7515942ec49855229ab9d75fdaef8d8
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 11:53:47 +0100

    unit test added

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,20 @@
 require 'game'
 
 describe Game do
-  let(:player) { double :player }
-  let(:computer) { double :computer }
+  let(:player) { double :player, :move => "Rock" }
+  let(:computer) { double :computer, :move => "Scissors" }
   subject { Game.new(player, computer) }
 
   context '#initialize' do
     it 'creates a new game with 1 player and 1 computer' do
       expect(subject.player).to eq player
       expect(subject.computer).to eq computer
+    end
+  end
+
+  context '#result' do
+    it 'returns a result' do
+      expect(subject.result).to eq "Win"
     end
   end
 end
