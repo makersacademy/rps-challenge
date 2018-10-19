1c7d2d2fa05b14a1811466e8b89cd23b60a38c8e
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-19 14:10:27 +0100

    First commit

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+
+class RPSapp < Sinatra::Base
+
+  get '/' do
+    'Hello Battle!'
+  end
+
+    # start the server if ruby file executed directly
+  run! if app_file == $0
+
+end
---
+++ spec/features/add_name_spec.rb
@@ -0,0 +1,9 @@
+feature "Adds names of players" do
+  scenario "Enter names" do
+    visit('/')
+    fill_in :player_1_name, with: 'Edyta'
+    fill_in :player_2_name, with: 'Caio'
+    click_button 'Submit'
+    expect(page).to have_content 'Edyta vs Caio'
+  end
+end
