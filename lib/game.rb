0939fe257a64c14a443d2ea821cf34881a3b1e6b
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 12:08:04 +0100

    code amended for all possibilities

--- lib/game.rb
+++ lib/game.rb
@@ -9,7 +9,15 @@ class Game
   def result
     if @player.move == "Rock" && @computer.move == "Scissors"
       "Win"
+    elsif @player.move == "Scissors" && @computer.move == "Paper"
+      "Win"
+    elsif @player.move == "Paper" && @computer.move == "Rock"
+      "Win"
+    elsif @player.move == "Paper" && @computer.move == "Scissors"
+      "Lose"
     elsif @player.move == "Scissors" && @computer.move == "Rock"
+      "Lose"
+    elsif @player.move == "Rock" && @computer.move == "Paper"
       "Lose"
     elsif @player.move == @computer.move
       "Draw"
