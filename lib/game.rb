e8f54f6b81be8f38fcfb023f0c4bfab5ceb3eaad
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-10-19 15:13:46 +0100

    Refactored winner method

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 class Game
 
+  WINS = [['rock', 'scissors'], ['paper', 'rock'], ['scissors', 'paper']]
+
   attr_reader :player_1, :player_2
 
   def initialize(player_1, player_2 = Computer.new)
@@ -8,34 +10,8 @@ class Game
   end
 
   def winner
-    case @player_1.choice
-    when 'rock'
-      case @player_2.choice
-      when 'rock'
-        'draw'
-      when 'paper'
-        @player_2
-      when 'scissors'
-        @player_1
-      end
-    when 'paper'
-      case @player_2.choice
-      when 'rock'
-        @player_1
-      when 'paper'
-        'draw'
-      when 'scissors'
-        @player_2
-      end
-    when 'scissors'
-      case @player_2.choice
-      when 'rock'
-        @player_2
-      when 'paper'
-        @player_1
-      when 'scissors'
-        'draw'
-      end
-    end
+    return @player_1 if WINS.include?([@player_1.choice, @player_2.choice])
+    return @player_2 if WINS.include?([@player_2.choice, @player_1.choice])
+    'draw'
   end
 end
