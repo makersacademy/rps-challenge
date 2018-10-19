6349a15de136de96f96b04ba6205a4419cdf6df0
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 15:47:35 +0100

    result method refactored

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 class Game
   attr_reader :player, :computer
 
+  WINS = { "Scissors" => "Paper", "Rock" => "Scissors", "Paper" => "Rock" }
+
   def initialize(player, computer)
     @player = player
     @computer = computer
@@ -16,20 +18,12 @@ class Game
 
   def result
     @computer.pick_move
-    if @player.move == "Rock" && @computer.move == "Scissors"
+    if @player.move == WINS.key(@computer.move)
       "Win"
-    elsif @player.move == "Scissors" && @computer.move == "Paper"
-      "Win"
-    elsif @player.move == "Paper" && @computer.move == "Rock"
-      "Win"
-    elsif @player.move == "Paper" && @computer.move == "Scissors"
-      "Lose"
-    elsif @player.move == "Scissors" && @computer.move == "Rock"
-      "Lose"
-    elsif @player.move == "Rock" && @computer.move == "Paper"
-      "Lose"
     elsif @player.move == @computer.move
       "Draw"
+    else
+      "Lose"
     end
   end
 
