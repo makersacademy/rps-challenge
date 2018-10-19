d8f5e6b18962b258c38169f75b5ffc87d4433986
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-19 16:56:11 +0100

    added methods for each move

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,19 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :move
   def initialize(name)
     @name = name
+    @move = nil
+  end
+
+  def choose_rock
+    @move = 'Rock'
+  end
+
+  def choose_paper
+    @move = 'Paper'
+  end
+
+  def choose_scissors
+    @move = 'Scissors'
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,4 +5,25 @@ describe Player do
   it "should return the name of the player" do
     expect(imtiyaz.name).to eq "Imtiyaz"
   end
+
+  describe "#choose_rock" do
+    it "should return player's move as rock" do
+      imtiyaz.choose_rock
+      expect(imtiyaz.move).to eq "Rock"
+    end
+  end
+
+  describe "#choose_paper" do
+    it "should return player's move as paper" do
+      imtiyaz.choose_paper
+      expect(imtiyaz.move).to eq "Paper"
+    end
+  end
+
+  describe "#choose_scissors" do
+    it "should return player's move as scissors" do
+      imtiyaz.choose_scissors
+      expect(imtiyaz.move).to eq "Scissors"
+    end
+  end
 end
