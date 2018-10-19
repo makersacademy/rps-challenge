61f670d391d2c733f73ee314c40ec421751616a3
Ben <rapiidz1@gmail.com>
2018-10-19 11:26:00 +0000

    user can enter their name

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+class Battle < Sinatra::Base
+   
+  get '/' do
+    erb :index
+  end
+  
+  post '/names' do
+     @player_1_name = params[:player_1_name]
+     erb :play
+  end
+
+ 
+  run! if app_file == $0 
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,11 @@
+require 'player'
+
+describe Player do 
+   subject(:ben) { Player.new('Ben') }
+   
+   describe '#name' do 
+      it 'returns the name' do 
+         expect(ben.name).to eq 'Ben' 
+      end
+   end
+end
