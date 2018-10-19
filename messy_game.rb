9081cf4b1204ede25bc05c5653859d541fa03088
hurc87 <zoe1987@btinternet.com>
2018-10-19 13:24:02 +0100

    first stage of tidying up messy game

--- messy_game.rb
+++ messy_game.rb
@@ -1,4 +1,8 @@
 arr = [:rock, :paper, :scissors]
+@draw = [[:rock, :rock], [:paper, :paper], [:scissors, :scissors]]
+@win_play_1 = [[:paper, :rock], [:rock, :scissors], [:scissors, :paper]]
+
+@outcome = []
 
 def computers_turn(arr)
   arr.sample
@@ -8,22 +12,29 @@ def player_1_turn(arr)
   arr.sample
 end
 
-player_1 = computers_turn(arr)
-player_2 = player_1_turn(arr)
+@player_1 = computers_turn(arr)
+@player_2 = player_1_turn(arr)
 
+@outcome.push(@player_1, @player_2)
 
-if player_1 == :rock && player_2 == :rock
-  puts "It's a draw"
-elsif player_1 == :paper && player_2 == :paper
-  puts "It's a draw"
-elsif player_1 == :scissors && player_2 == :scissors
-  puts "It's a draw"
-elsif player_1 == :rock && player_2 == :scissors
-  puts "Player 1 wins"
-elsif player_1 == :paper && player_2 == :rock
-  puts "Player 1 wins"
-elsif player_1 == :scissors && player_2 == :paper
-  puts "Player 1 wins"
-else
-  puts "Player 2 wins"
+def is_draw
+  print @outcome
+  @draw.include?@outcome
 end
+
+def play_1_win
+  print @outcome
+  @win_play_1.include?@outcome
+end
+
+def result
+  if is_draw == true
+    puts "Its a draw"
+  elsif play_1_win
+    puts "Player 1 has won"
+  else
+    puts "Player 2 has won"
+  end
+end
+
+result
