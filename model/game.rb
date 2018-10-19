d5275c3c84ac266932df108f5a02d5c520b8a4fa
Aimée Craig <aimeecraig1@gmail.com>
2018-10-19 10:24:33 +0100

    tests added

--- model/game.rb
+++ model/game.rb
@@ -1,5 +1,20 @@
 class Game
+  def move
+    move = cpu_move
+    if move == 0
+      "Rock"
+    elsif move == 1
+      "Paper"
+    elsif move == 2
+      "Scissors"
+    end
+  end
+
   def cpu_move
     rand(2)
+  end
+
+  def cpu_result
+    true
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../model/game'
 
 describe Game do
+  let(:player) { double :player, :move => "" }
   context '#cpu_move' do
     it 'returns a random number between 0 and 2' do
       srand(1)
@@ -12,6 +13,13 @@ describe Game do
     it 'returns a move based on cpu_move' do
       srand(1)
       expect(subject.move).to eq "Paper"
+    end
+  end
+
+  context '#cpu_result' do
+    it 'calculates who wins out of player and cpu' do
+      player.move("Scissors")
+      expect(subject.cpu_result).to eq true
     end
   end
 end
