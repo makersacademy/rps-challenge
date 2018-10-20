3e32fd3200addae36ef0cb14d9d993c80967e5b3
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-20 14:11:33 +0100

    added a default player_2 object named 'Computer'

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,8 @@
 class Game
-  attr_reader :player
-  def initialize(player:)
-    @player = player
+  attr_reader :player_1, :player_2
+  def initialize(player_1:, player_2: Player.new("Computer"))
+    @player_1 = player_1
+    @player_2 = player_2
     @possible_moves = ["Rock", "Paper", 'Scissors']
   end
 
@@ -9,8 +10,8 @@ class Game
     @possible_moves.dup
   end
 
-  def self.create(player:)
-    @game = Game.new(player: Player.new(player))
+  def self.create(player_1:, player_2: "Computer")
+    @game = Game.new(player_1: Player.new(player_1), player_2: Player.new(player_2))
   end
 
   def self.instance
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,22 +1,28 @@
 require 'game'
 
 describe Game do
-  let(:player) { double :player }
-  subject { described_class.new(player: player) }
+  let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2 }
+  subject { described_class.new(player_1: player_1) }
 
-  it "should return the player" do
-    expect(subject.player).to eq player
+  it "should return the player_1" do
+    expect(subject.player_1).to eq player_1
+  end
+
+  it "should return the player_2" do
+    subject = Game.new(player_1: player_1, player_2: player_2)
+    expect(subject.player_2).to eq player_2
   end
 
   describe "#self.create" do
     it "should return a Game" do
-      expect(Game.create(player: player)).to be_a Game
+      expect(Game.create(player_1: player_1)).to be_a Game
     end
   end
 
   describe "self.instance" do
     it "should return the created Game" do
-      game = Game.create(player: player)
+      game = Game.create(player_1: player_1)
       expect(Game.instance).to eq game
     end
   end
