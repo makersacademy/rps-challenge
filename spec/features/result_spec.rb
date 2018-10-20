5c5bffff527bf04535276d0741c849d6fcf54664
Muna <mmhussein125@gmail.com>
2018-10-20 17:14:19 +0100

    Added play-again-button

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -58,4 +58,19 @@ feature 'Winner declared' do
     click_button 'Rock'
     expect(page).to have_content 'It\'s a draw!'
   end
+
+  scenario 'Can play again' do
+    allow_any_instance_of(Array).to receive(:sample).and_return("Rock")
+    sign_in_and_play
+    click_button 'Rock'
+    expect(page).to have_button 'Play again'
+  end
+
+  scenario 'Can start a new game' do
+    allow_any_instance_of(Array).to receive(:sample).and_return("Rock")
+    sign_in_and_play
+    click_button 'Rock'
+    click_button 'Play again'
+    expect(page).to have_content 'Enter your name to play'
+  end
 end
