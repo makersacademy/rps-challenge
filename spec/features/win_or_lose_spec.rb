8af74058f6e1847d286d934c4e50c9b3e3834039
Caitlin Cooling <ccooling97@gmail.com>
2018-10-20 10:11:35 +0100

    Fleshed out winner tests and refeactored win_or_lose

--- spec/features/win_or_lose_spec.rb
+++ spec/features/win_or_lose_spec.rb
@@ -1,14 +1,17 @@
 feature 'Win or lose' do
+  let(:players) { Players.new("Caitlin", "James") }
+  let(:winner) { Winner.new(players) }
+
   scenario 'player2 wins' do
     sign_in_and_play
     find_button('rock').click
     find_button('paper').click
-    expect(page).to have_content "James won!"
+    expect(page).to have_content winner.player2_won
   end
   scenario 'player1 wins' do
     sign_in_and_play
     find_button('scissors').click
     find_button('paper').click
-    expect(page).to have_content "Caitlin won!"
+    expect(page).to have_content winner.player1_won
   end
 end
--- spec/winner_spec.rb
+++ spec/winner_spec.rb
@@ -1,16 +1,60 @@
 require 'winner'
 
 describe Winner do
-  let(:winner) { Winner.new(players) }
-  let(:players) { double(:players, move1: "rock", move2: "paper", name1: "Caitlin", name2: "James") }
 
   it 'should determine name1 as the winner' do
+    players = double(:players, move1: "rock", move2: "paper", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
     expect(winner.determine).to eq "James won!"
   end
 
+  it 'should determine name2 as the winner' do
+    players = double(:players, move1: "rock", move2: "scissors", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
+    expect(winner.determine).to eq "Caitlin won!"
+  end
 
+  it 'should determine a draw' do
+    players = double(:players, move1: "rock", move2: "rock", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
+    expect(winner.determine).to eq "It's a draw!"
+  end
 
+  it 'should determine name1 as the winner' do
+    players = double(:players, move1: "paper", move2: "scissors", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
+    expect(winner.determine).to eq "James won!"
+  end
 
+  it 'should determine name2 as the winner' do
+    players = double(:players, move1: "paper", move2: "rock", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
+    expect(winner.determine).to eq "Caitlin won!"
+  end
+
+  it 'should determine a draw' do
+    players = double(:players, move1: "paper", move2: "paper", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
+    expect(winner.determine).to eq "It's a draw!"
+  end
+
+  it 'should determine name1 as the winner' do
+    players = double(:players, move1: "scissors", move2: "rock", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
+    expect(winner.determine).to eq "James won!"
+  end
+
+  it 'should determine name2 as the winner' do
+    players = double(:players, move1: "scissors", move2: "paper", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
+    expect(winner.determine).to eq "Caitlin won!"
+  end
+
+  it 'should determine a draw' do
+    players = double(:players, move1: "scissors", move2: "scissors", name1: "Caitlin", name2: "James")
+    winner = Winner.new(players)
+    expect(winner.determine).to eq "It's a draw!"
+  end
 
 
 
