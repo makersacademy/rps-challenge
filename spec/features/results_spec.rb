87f2356a0568bdea3286251dadc719822f8c4cc2
jebax <joel.eliot.baxter@gmail.com>
2018-10-20 12:25:23 +0100

    added further tests for results page

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -16,3 +16,33 @@ feature "results page" do
     expect(page).to have_content(/Alice wins!|RPSbot wins!|It's a draw!/)
   end
 end
+
+feature "displaying correct result" do
+  before do
+    enter_name_and_play
+  end
+
+  scenario "should confirm other player's choice on results page" do
+    allow_any_instance_of(Array).to receive(:sample).and_return "ROCK"
+    click_button "ROCK"
+    expect(page).to have_content "RPSbot chose ROCK"
+  end
+
+  scenario "displays correct result when computer chooses rock" do
+    allow_any_instance_of(Array).to receive(:sample).and_return "ROCK"
+    click_button "ROCK"
+    expect(page).to have_content "It's a draw!"
+  end
+
+  scenario "displays correct result when computer chooses paper" do
+    allow_any_instance_of(Array).to receive(:sample).and_return "PAPER"
+    click_button "ROCK"
+    expect(page).to have_content "RPSbot wins!"
+  end
+
+  scenario "displays correct result when computer chooses scissors" do
+    allow_any_instance_of(Array).to receive(:sample).and_return "SCISSORS"
+    click_button "ROCK"
+    expect(page).to have_content "Alice wins!"
+  end
+end
