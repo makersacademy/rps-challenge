336b018907a60e89b32878ea76d0978f87d03362
Muna <mmhussein125@gmail.com>
2018-10-20 18:12:56 +0100

    Ammended spec_helper to have test coverage stats generated on PR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
 
 ENV['RACK_ENV'] = 'test'
 
@@ -7,18 +16,9 @@ require File.join(File.dirname(__FILE__), '..', 'rps_web.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-require 'simplecov'
-require 'simplecov-console'
 require 'features/web_helpers'
 
 Capybara.app = RPSWeb
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
