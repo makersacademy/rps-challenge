47c0d3dd36601199cc133ecd32647370f98ebbd8
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-21 19:47:25 +0100

    Display player 1 turn

--- spec/features/start_game_spec.rb
+++ spec/features/start_game_spec.rb
@@ -3,7 +3,10 @@ feature "Start game" do
     visit('/')
     fill_in :player_1_name, with: 'Edyta'
     fill_in :player_2_name, with: 'Caio'
+
     click_button 'Start the game!'
-    expect(page).to have_content 'Edyta vs Caio'
+
+    expect(page).to have_content "Edyta vs Caio"
+    expect(page).to have_content "Edyta's turn"
   end
 end
