5da5c96b93bd2c62506ee65d856677449441de06
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-21 13:46:04 +0100

    updated to include game name

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,13 +1,13 @@
 feature "Enter player name" do
   scenario "submitted name should appear on screen in 1 player mode" do
     one_player_sign_in_and_play
-    expect(page).to have_content "Imtiyaz vs Computer"
+    expect(page).to have_content "Rock, Paper, Scissors! Imtiyaz vs Computer"
   end
 
   context "two player mode" do
     scenario "submitted names should appear on screen in 2 player mode" do
       two_player_sign_in_and_play
-      expect(page).to have_content "Mario vs Luigi"
+      expect(page).to have_content "Rock, Paper, Scissors! Mario vs Luigi"
     end
 
     context "'Computer' entered into a name field" do
