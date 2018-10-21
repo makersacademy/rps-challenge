80f955fc4778ba4f443317141da4653e6be011b4
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-21 19:35:57 +0100

    Rename button

--- spec/features/start_game_spec.rb
+++ spec/features/start_game_spec.rb
@@ -3,7 +3,7 @@ feature "Start game" do
     visit('/')
     fill_in :player_1_name, with: 'Edyta'
     fill_in :player_2_name, with: 'Caio'
-    click_button 'Submit'
+    click_button 'Start the game!'
     expect(page).to have_content 'Edyta vs Caio'
   end
 end
