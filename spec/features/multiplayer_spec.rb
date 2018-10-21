abebaf41337ebfc3f3e70e898c193dfc74e5b088
Aimée Craig <aimeecraig1@gmail.com>
2018-10-21 14:12:16 +0100

    feature test added

--- spec/features/multiplayer_spec.rb
+++ spec/features/multiplayer_spec.rb
@@ -8,4 +8,17 @@ feature 'Multiplayer' do
     click_button 'Submit'
     expect(page).to have_content 'Mittens vs. Dave!'
   end
+
+  scenario 'player 1 wins' do
+    visit '/'
+    select 'Two Player', from: 'players'
+    click_button 'Submit'
+    fill_in 'Player 1 Name', with: "Mittens"
+    fill_in 'Player 2 Name', with: "Dave"
+    click_button 'Submit'
+    select 'Rock', from: 'player_1_move'
+    select 'Scissors', from: 'player_2_move'
+    click_button 'Submit'
+    expect(page).to have_content 'Mittens wins!'
+  end
 end
