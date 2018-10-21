199d2d952ea07ed389e3375a42e3b67d191ed060
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-21 12:05:03 +0100

    added #outcome which returns string based on outcome of the game

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,12 @@ class Game
     @player_2 = player_2
   end
 
+  def outcome
+    return 'You Draw :|' if both_players_have_same_move?
+    return player_2_wins if player_2_beat_player_1?
+    player_1_wins
+  end
+
   def self.create(player_1:, player_2: "Computer")
     @game = Game.new(player_1: Player.new(player_1),
     player_2: Player.new(player_2))
@@ -12,5 +18,28 @@ class Game
 
   def self.instance
     @game
+  end
+
+  private
+  def both_players_have_same_move?
+    (@player_1.chose_rock? && @player_2.chose_rock?) ||
+    (@player_1.chose_paper? && @player_2.chose_paper?) ||
+    (@player_1.chose_scissors? && @player_2.chose_scissors?)
+  end
+
+  def player_2_beat_player_1?
+    (@player_1.chose_rock? && @player_2.chose_paper?) ||
+    (@player_1.chose_paper? && @player_2.chose_scissors?) ||
+    (@player_1.chose_scissors? && @player_2.chose_rock?)
+  end
+
+  def player_2_wins
+    return "You Lose :(" if @player_2.computer?
+    "#{@player_2.name} Wins! :D" unless @player_2.computer?
+  end
+
+  def player_1_wins
+    return "You Win! :D" if @player_2.computer?
+    "#{@player_1.name} Wins! :D" unless @player_2.computer?
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,234 @@
 require 'game'
-
+require 'pry'
 describe Game do
   let(:player_1) { double :player_1 }
   let(:player_2) { double :player_2 }
-  subject { described_class.new(player_1: player_1) }
+  subject { Game.new(player_1: player_1, player_2: player_2) }
 
   it "should return the player_1" do
     expect(subject.player_1).to eq player_1
   end
 
   it "should return the player_2" do
-    subject = Game.new(player_1: player_1, player_2: player_2)
     expect(subject.player_2).to eq player_2
+  end
+
+  describe "#outcome" do
+    context "both players pick rock" do
+      before {
+        allow(player_1).to receive(:chose_rock?).and_return(true)
+        allow(player_2).to receive(:chose_rock?).and_return(true)
+      }
+      it "should return 'You Draw :|'" do
+        expect(subject.outcome).to eq "You Draw :|"
+      end
+    end
+
+    context "both players pick paper" do
+      before {
+        allow(player_1).to receive(:chose_rock?).and_return(false)
+        allow(player_2).to receive(:chose_rock?).and_return(false)
+        allow(player_1).to receive(:chose_paper?).and_return(true)
+        allow(player_2).to receive(:chose_paper?).and_return(true)
+      }
+      it "should return 'You Draw :|'" do
+        expect(subject.outcome).to eq "You Draw :|"
+      end
+    end
+
+    context "both players pick scissors" do
+      before {
+        allow(player_1).to receive(:chose_rock?).and_return(false)
+        allow(player_2).to receive(:chose_rock?).and_return(false)
+        allow(player_1).to receive(:chose_paper?).and_return(false)
+        allow(player_2).to receive(:chose_paper?).and_return(false)
+        allow(player_1).to receive(:chose_scissors?).and_return(true)
+        allow(player_2).to receive(:chose_scissors?).and_return(true)
+      }
+      it "should return 'You Draw :|'" do
+        expect(subject.outcome).to eq "You Draw :|"
+      end
+    end
+
+    context "one player mode" do
+      before {
+        allow(player_2).to receive(:computer?).and_return(true)
+      }
+      context "player 1 chose rock, computer chose paper" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(true)
+          allow(player_2).to receive(:chose_rock?).and_return(false)
+          allow(player_1).to receive(:chose_paper?).and_return(false)
+          allow(player_2).to receive(:chose_paper?).and_return(true)
+          allow(player_1).to receive(:chose_scissors?).and_return(false)
+          allow(player_2).to receive(:chose_scissors?).and_return(false)
+        }
+        it "returns 'You Lose :('" do
+          expect(subject.outcome).to eq "You Lose :("
+        end
+      end
+
+      context "player 1 chose paper, computer chose rock" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(false)
+          allow(player_2).to receive(:chose_rock?).and_return(true)
+          allow(player_1).to receive(:chose_paper?).and_return(true)
+          allow(player_2).to receive(:chose_paper?).and_return(false)
+          allow(player_1).to receive(:chose_scissors?).and_return(false)
+          allow(player_2).to receive(:chose_scissors?).and_return(false)
+        }
+        it "returns 'You Win! :D'" do
+          expect(subject.outcome).to eq "You Win! :D"
+        end
+      end
+
+      context "player 1 chose paper, computer chose scissors" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(false)
+          allow(player_2).to receive(:chose_rock?).and_return(false)
+          allow(player_1).to receive(:chose_paper?).and_return(true)
+          allow(player_2).to receive(:chose_paper?).and_return(false)
+          allow(player_1).to receive(:chose_scissors?).and_return(false)
+          allow(player_2).to receive(:chose_scissors?).and_return(true)
+        }
+        it "returns 'You Lose :('" do
+          expect(subject.outcome).to eq "You Lose :("
+        end
+      end
+
+      context "player 1 chose scissors, computer chose paper" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(false)
+          allow(player_2).to receive(:chose_rock?).and_return(false)
+          allow(player_1).to receive(:chose_paper?).and_return(false)
+          allow(player_2).to receive(:chose_paper?).and_return(true)
+          allow(player_1).to receive(:chose_scissors?).and_return(true)
+          allow(player_2).to receive(:chose_scissors?).and_return(false)
+        }
+        it "returns 'You Win! :D'" do
+          expect(subject.outcome).to eq "You Win! :D"
+        end
+      end
+
+      context "player 1 chose scissors, computer chose rock" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(false)
+          allow(player_2).to receive(:chose_rock?).and_return(true)
+          allow(player_1).to receive(:chose_paper?).and_return(false)
+          allow(player_2).to receive(:chose_paper?).and_return(false)
+          allow(player_1).to receive(:chose_scissors?).and_return(true)
+          allow(player_2).to receive(:chose_scissors?).and_return(false)
+        }
+        it "returns 'You Lose :('" do
+          expect(subject.outcome).to eq "You Lose :("
+        end
+      end
+
+      context "player 1 chose rock, computer chose scissors" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(true)
+          allow(player_2).to receive(:chose_rock?).and_return(false)
+          allow(player_1).to receive(:chose_paper?).and_return(false)
+          allow(player_2).to receive(:chose_paper?).and_return(false)
+          allow(player_1).to receive(:chose_scissors?).and_return(false)
+          allow(player_2).to receive(:chose_scissors?).and_return(true)
+        }
+        it "returns 'You Win! :D'" do
+          expect(subject.outcome).to eq 'You Win! :D'
+        end
+      end
+    end
+
+    context "two player mode" do
+      before {
+        allow(player_2).to receive(:computer?).and_return(false)
+        allow(player_1).to receive(:name).and_return("Mario")
+        allow(player_2).to receive(:name).and_return("Luigi")
+      }
+      context "player 1 chose rock, player 2 chose paper" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(true)
+          allow(player_2).to receive(:chose_rock?).and_return(false)
+          allow(player_1).to receive(:chose_paper?).and_return(false)
+          allow(player_2).to receive(:chose_paper?).and_return(true)
+          allow(player_1).to receive(:chose_scissors?).and_return(false)
+          allow(player_2).to receive(:chose_scissors?).and_return(false)
+        }
+        it "returns 'Luigi Wins! :D'" do
+          expect(subject.outcome).to eq "Luigi Wins! :D"
+        end
+      end
+
+      context "player 1 chose paper, player 2 chose rock" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(false)
+          allow(player_2).to receive(:chose_rock?).and_return(true)
+          allow(player_1).to receive(:chose_paper?).and_return(true)
+          allow(player_2).to receive(:chose_paper?).and_return(false)
+          allow(player_1).to receive(:chose_scissors?).and_return(false)
+          allow(player_2).to receive(:chose_scissors?).and_return(false)
+        }
+        it "returns 'Mario Wins! :D'" do
+          expect(subject.outcome).to eq "Mario Wins! :D"
+        end
+      end
+
+      context "player 1 chose paper, player 2 chose scissors" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(false)
+          allow(player_2).to receive(:chose_rock?).and_return(false)
+          allow(player_1).to receive(:chose_paper?).and_return(true)
+          allow(player_2).to receive(:chose_paper?).and_return(false)
+          allow(player_1).to receive(:chose_scissors?).and_return(false)
+          allow(player_2).to receive(:chose_scissors?).and_return(true)
+        }
+        it "returns 'Luigi Wins! :D'" do
+          expect(subject.outcome).to eq "Luigi Wins! :D"
+        end
+      end
+
+      context "player 1 chose scissors, player 2 chose paper" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(false)
+          allow(player_2).to receive(:chose_rock?).and_return(false)
+          allow(player_1).to receive(:chose_paper?).and_return(false)
+          allow(player_2).to receive(:chose_paper?).and_return(true)
+          allow(player_1).to receive(:chose_scissors?).and_return(true)
+          allow(player_2).to receive(:chose_scissors?).and_return(false)
+        }
+        it "returns 'Mario Wins! :D'" do
+          expect(subject.outcome).to eq "Mario Wins! :D"
+        end
+      end
+
+      context "player 1 chose scissors, player 2 chose rock" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(false)
+          allow(player_2).to receive(:chose_rock?).and_return(true)
+          allow(player_1).to receive(:chose_paper?).and_return(false)
+          allow(player_2).to receive(:chose_paper?).and_return(false)
+          allow(player_1).to receive(:chose_scissors?).and_return(true)
+          allow(player_2).to receive(:chose_scissors?).and_return(false)
+        }
+        it "returns 'Luigi Wins! :D'" do
+          expect(subject.outcome).to eq "Luigi Wins! :D"
+        end
+      end
+
+      context "player 1 chose rock, player 2 chose scissors" do
+        before {
+          allow(player_1).to receive(:chose_rock?).and_return(true)
+          allow(player_2).to receive(:chose_rock?).and_return(false)
+          allow(player_1).to receive(:chose_paper?).and_return(false)
+          allow(player_2).to receive(:chose_paper?).and_return(false)
+          allow(player_1).to receive(:chose_scissors?).and_return(false)
+          allow(player_2).to receive(:chose_scissors?).and_return(true)
+        }
+        it "returns 'Mario Wins! :D'" do
+          expect(subject.outcome).to eq "Mario Wins! :D"
+        end
+      end
+    end
   end
 
   describe "#self.create" do
