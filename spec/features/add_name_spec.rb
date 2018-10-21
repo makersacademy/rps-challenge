82a0f48f30835db62b569ee5e71b52685b077f27
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-21 17:48:17 +0100

    Removed redundant get request

--- spec/features/add_name_spec.rb
+++ spec/features/add_name_spec.rb
@@ -4,7 +4,6 @@ feature "Adds names of players" do
     fill_in :player_1_name, with: 'Edyta'
     fill_in :player_2_name, with: 'Caio'
     click_button 'Submit'
-
     expect(page).to have_content 'Edyta vs Caio'
   end
 end
