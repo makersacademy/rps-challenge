3bea9cb4753a0589cbf061ecca2b49153a6d40bd
Aimée Craig <aimeecraig1@gmail.com>
2018-10-22 11:26:34 +0100

    stub removed from feature tests

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,14 +1,14 @@
 feature 'Play game' do
   scenario 'player chooses a move and gets a win screen' do
-    allow_any_instance_of(Array).to receive(:sample) { :Scissors }
+    srand(1)
     enter_name_and_play
     expect(page).to have_content 'Mittens wins!'
   end
 
   scenario "page shows the player's and the computer's move" do
-    allow_any_instance_of(Array).to receive(:sample) { :Scissors }
+    srand(1)
     enter_name_and_play
     expect(page).to have_content 'Mittens picked Rock'
-    expect(page).to have_content 'The computer picked Scissors'
+    expect(page).to have_content 'The computer picked Lizard'
   end
 end
