720e4a52ecb6e422af2aae5fbffaa33ae7ad6629
Will Wayman <will.jdw@gmail.com>
2018-11-16 16:26:47 +0000

    fixed feature test but need to stub randomness

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -2,6 +2,6 @@ feature 'play the game' do
   scenario 'selects and option and gets the result' do
     sign_in
     click_button "Rock"
-    expect(page).to have_text("You won!")
+    expect(page).to have_text("wins!")
   end
 end
