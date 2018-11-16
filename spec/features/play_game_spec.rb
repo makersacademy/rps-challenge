a44c31fd1725a9c0f49251f9df614bcaf152de65
Will Wayman <will.jdw@gmail.com>
2018-11-16 19:37:24 +0000

    updated feature test in line with user story

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,7 +1,17 @@
-feature 'play the game' do
-  scenario 'selects and option and gets the result' do
+feature 'play rock/paper/scissors' do
+  scenario 'select rock' do
     sign_in
     click_button "Rock"
-    expect(page).to have_text("wins!")
+    expect(page).to have_text("chose rock")
+  end
+  scenario 'select paper' do
+    sign_in
+    click_button "Paper"
+    expect(page).to have_text("chose paper")
+  end
+  scenario 'select scissors' do
+    sign_in
+    click_button "Scissors"
+    expect(page).to have_text("chose scissors")
   end
 end
