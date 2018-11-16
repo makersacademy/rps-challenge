742aea2a8cb2e562108976e1282b7742e953d712
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-16 14:13:00 +0000

    adds test for calculating winner

--- lib/adjudicator.rb
+++ lib/adjudicator.rb
@@ -1,9 +1,9 @@
 class Adjudicator
-  attr_reader :result, :p1move, :p2move
+  attr_reader :result, :play1, :play2
 
   def initialize(play1, play2)
-    @p1move = play1.move
-    @p2move = play2.move
+    @play1 = play1
+    @play2 = play2
   end
 
 end
--- spec/adjudicator_spec.rb
+++ spec/adjudicator_spec.rb
@@ -5,11 +5,18 @@ describe Adjudicator do
   let(:paperer) { double :paperer, move: "PAPER" }
   let(:scissorator) { double :scissorator, move: "SCISSORS"}
 
-  describe '#pXmove' do
+  describe '#playX.move' do
     it 'returns the player moves as strings' do
       judge = Adjudicator.new(rocker, paperer)
-      expect(judge.p1move).to eq("ROCK")
-      expect(judge.p2move).to eq("PAPER")
+      expect(judge.play1.move).to eq("ROCK")
+      expect(judge.play2.move).to eq("PAPER")
+    end
+  end
+
+  describe '#who_wins' do
+    it 'returns the paper player when rock vs paper' do
+      judge = Adjudicator.new(rocker, paperer)
+      expect(judge.who_wins).to eq(paperer)
     end
   end
 end
