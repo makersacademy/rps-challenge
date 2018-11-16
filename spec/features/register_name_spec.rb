9cc2279b79b94c0795140a577f7a96a014b64b51
Will Wayman <will.jdw@gmail.com>
2018-11-16 10:12:33 +0000

    feature test for registering a name

---
+++ spec/features/register_name_spec.rb
@@ -0,0 +1,8 @@
+feature 'user can register their name' do
+  scenario 'displays the users name' do
+    visit '/'
+    fill_in "name", with: "Will"
+    click_button "Submit"
+    expect(page).to have_text "Welcome to Rock, Paper, Scissors Will!"
+  end
+end
