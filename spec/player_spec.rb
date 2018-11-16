fe59508736347363cebc9f41ed4b7959c3e88ddb
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-16 13:29:35 +0000

    adds tests for player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,3 +1,18 @@
 require 'player'
 
 describe Player do
+  let(:player) { Player.new("Henry") }
+
+  describe '#name' do
+    it 'returns a name when asked' do
+      expect(player.name).to eq("Henry")
+    end
+  end
+
+  describe '#make_move' do
+    it 'returns ROCK when the move is made' do
+      player.make_move("ROCK")
+      expect(player.move).to eq("ROCK")
+    end
+  end
+end
