c206ccf6031ce475e792f8703d8594f403a36ca7
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-11-17 09:24:20 +0000

    set up app and erbs to pass first feature test

--- app.rb
+++ app.rb
@@ -4,9 +4,20 @@ require 'capybara'
 Capybara.app
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    "Hello!!"
+    erb :index
+  end
+
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/result'
+  end
+
+  get '/result' do
+    @name = session[:name]
+    erb :result
   end
 
 run! if app_file == $0
