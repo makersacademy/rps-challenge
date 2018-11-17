98b48e424e9c2cc9a14eba953d2839240f8766a7
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-17 11:15:14 +0000

    weapon choice added

---
+++ spec/features/play_feature_spec.rb
@@ -0,0 +1,11 @@
+feature 'user is welcomed and makes first move' do
+  scenario 'user inputs their name and is welcomed' do
+    sign_in_and_play
+    expect(page).to have_content "Welcome, Chris!"
+  end
+  scenario 'user has weapon options' do
+    sign_in_and_play
+    expect(page).to have_content "Please make your move..."
+    expect(page).to have_content "Weapon choice"
+  end
+end
