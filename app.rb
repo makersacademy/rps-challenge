ee02f5944c3f0ad24afeb45b2e8098fe5aad7f7f
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-17 19:14:35 +0000

    adding random selection for computer

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
-require 'sinatra/base'
+require 'sinatra'
 
 
-class RPS < Sinatra::Base
-  get '/' do
-    erb :index
-  end
-
-  post '/result' do
-    @player_1 = params[:player_1]
-    @choice = params[:options]
-    erb :result
-  end
-
+# class RPS < Sinatra::Base
+get '/' do
+  erb :index
 end
+
+post '/result' do
+  @player_1 = params[:player_1]
+  @choice = params[:options]
+  erb :result
+end
+
+# end
