e4df216ee1514bfa2647b2ee2fa59f7237ede3bf
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-17 12:46:50 +0000

    computer player tests tidied up

--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -4,12 +4,17 @@ describe ComputerPlayer do
 
   subject(:computer_player) { described_class.new }
 
-  it 'has a selection of weapons' do
-    expect(computer_player.weapons).to eq ["rock", "paper", "scissors"]
+  describe '#weapons' do
+    it 'has a selection of weapons' do
+      expect(computer_player.weapons).to eq ["rock", "paper", "scissors"]
+    end
   end
 
-  it 'randomly chooses a wepaon' do
-    allow(computer_player.weapons).to receive(:sample).and_return("paper")
-    expect(computer_player.weapon).to eq "paper"
+  describe '#weapon' do
+    it 'randomly chooses a wepaon' do
+      allow(computer_player.weapons).to receive(:sample).and_return("paper")
+      expect(computer_player.weapon).to eq "paper"
+    end
   end
+
 end
