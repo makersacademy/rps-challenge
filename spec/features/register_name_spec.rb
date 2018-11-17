b16381291ee6c1359e00fce0e926f3f5a535ba02
Will Wayman <will.jdw@gmail.com>
2018-11-17 15:03:03 +0000

    fixed bug in feature test

--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -1,6 +1,6 @@
 feature 'register a name' do
   scenario 'displays the users name' do
     sign_in
-    expect(page).to have_text "Welcome to Rock, Paper, Scissors Will!"
+    expect(page).to have_text "Welcome, Will!"
   end
 end
