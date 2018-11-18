0137bd8f6ab6e72358b3c85e1ffd31245b48f49c
Tomas Doherty <tomasdoh1@gmail.com>
2018-11-18 17:45:22 +0000

    User Story 1 complete - name registration

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
-  
+
   get '/' do
     'Welcome to Rock, Paper, Scissors!'
+    erb(:index)
+  end
+
+  post '/game' do
+    @player_name = params[:name]
+    erb(:game)
   end
 
   run! if app_file == $0
