ecf1704aaceb79af5af3f38758a780ec39ae7262
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-18 10:26:57 +0000

    breaking down play_game method, adding a method to get users index location, and another for the game playusing

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 class Game
 
   attr_reader :player_1, :computer_weapon, :winner
+  WEAPONS = ["Rock", "Scissors", "Paper"]
 
   def initialize(player_1)
     @player_1 = player_1
@@ -9,20 +10,18 @@ class Game
   end
 
   def computer_random_choice
-    randon_selection = ['Rock', 'Paper', 'Scissors'].sample
-    @computer_weapon = randon_selection
+    @computer_weapon = WEAPONS.sample
+  end
+
+  def players_index_location
+    @computer_weapon = computer_random_choice
+    @computer_index = WEAPONS.index(@computer_weapon)
+    @player_1_index = WEAPONS.index(@player_1.weapon)
   end
 
   def play_game
-    if (@player_1.weapon == "Rock" && @computer_weapon == "Scissors") ||
-        (@player_1.weapon == "Paper" && @computer_weapon == "Rock") ||
-        (@player_1.weapon == "Scissors" && @computer_weapon == "Paper")
-      @winner = @player_1.name
-    elsif (@computer_weapon == "Rock" && @player_1.weapon == "Scissors") ||
-        (@computer_weapon == "Paper" && @player_1.weapon == "Rock") ||
-        (@computer_weapon == "Scissors" && @player_1.weapon == "Paper")
-    else
-      @winner = "It's a draw"
-    end
+    players_index_location
+    return @winner = @player_1.name if WEAPONS[@computer_index - 1] == WEAPONS[@player_1_index]
+    return @winner = "It's a draw" if @computer_index == @player_1_index
   end
 end
