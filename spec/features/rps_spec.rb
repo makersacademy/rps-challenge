470ad12aef09a2334f5861ed2ab664bfdaf2ef94
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-18 00:24:41 +0000

    adding more feature test

--- spec/features/rps_spec.rb
+++ spec/features/rps_spec.rb
@@ -7,4 +7,13 @@ feature 'rock paper scissors' do
     click_button('PLAY')
     expect(page).to have_content "Luisa choose Rock"
   end
+
+  scenario 'Will show player name and weapon of choice and computer weapon' do
+    visit('/')
+    fill_in 'player_1', with: 'Luisa'
+    find("option[value='Rock']").click
+    click_button('PLAY')
+    expect(page).to have_content "Luisa choose Rock"
+    expect(page).to have_content "Computer choose"
+  end
 end
