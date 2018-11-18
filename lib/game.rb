21d67ac1f7b6979a50dffe00d31ce865c39baab9
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-18 16:43:27 +0000

    refactoring play method

--- lib/game.rb
+++ lib/game.rb
@@ -18,22 +18,18 @@ class Game
   # Return nil if it's a draw otherwise return the winner
   def resolve_winner(player_1_weapon, player_2_weapon)
 
-    if player_1_weapon == player_2_weapon
-      nil
-    else
-      WEAPON_WIN[player_1_weapon] == player_2_weapon ? @player_1 : @player_2
-    end
+    return nil if player_1_weapon == player_2_weapon
+    WEAPON_WIN[player_1_weapon] == player_2_weapon ? @player_1 : @player_2
 
   end
 
   def play
+
     @winner = resolve_winner(@player_1.weapon, @player_2.weapon)
-    if @winner.nil?
-      @winner_name = "It's a draw"
-    else
-      @winner.add_to_score
-      @winner_name = @winner.name
-    end
+    return @winner_name = "It's a draw" if @winner.nil?
+    @winner.add_to_score
+    @winner_name = @winner.name
+
   end
 
 end
