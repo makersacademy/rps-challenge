334d5d0fe61c1dda384102938678f82c712a572f
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-18 00:53:19 +0000

    introducing logic to the game and announcing winner

--- app.rb
+++ app.rb
@@ -2,16 +2,17 @@ require 'sinatra'
 require './lib/player'
 require './lib/game'
 
-class RPS < Sinatra::Base
-  get '/' do
-    erb :index
-  end
-
-  post '/result' do
-    player_1 = Player.new(params[:player_1], params[:options])
-    @game = Game.new(player_1)
-    @game.computer_random_choice
-    erb :result
-  end
-
+# class RPS < Sinatra::Base
+get '/' do
+  erb :index
 end
+
+post '/result' do
+  player_1 = Player.new(params[:player_1], params[:options])
+  @game = Game.new(player_1)
+  @game.computer_random_choice
+  @game.play_game
+  erb :result
+end
+
+# end
--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,28 @@
 class Game
 
-  attr_reader :player_1, :computer_weapon
+  attr_reader :player_1, :computer_weapon, :winner
 
   def initialize(player_1)
     @player_1 = player_1
-    @computer_weapon = []
+    @computer_weapon = "Paper"
+    @winner = "Computer"
   end
 
   def computer_random_choice
     randon_selection = ['Rock', 'Paper', 'Scissors'].sample
-    @computer_weapon << randon_selection
+    @computer_weapon = randon_selection
   end
 
+  def play_game
+    if (@player_1.weapon == "Rock" && @computer_weapon == "Scissors") ||
+        (@player_1.weapon == "Paper" && @computer_weapon == "Rock") ||
+        (@player_1.weapon == "Scissors" && @computer_weapon == "Paper")
+      @winner = @player_1.name
+    elsif (@computer_weapon == "Rock" && @player_1.weapon == "Scissors") ||
+        (@computer_weapon == "Paper" && @player_1.weapon == "Rock") ||
+        (@computer_weapon == "Scissors" && @player_1.weapon == "Paper")
+    else
+      @winner = "It's a draw"
+    end
+  end
 end
