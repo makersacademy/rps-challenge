c70a2af9a044909bde9b7e2c7895fd552b9a96f2
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-18 00:34:58 +0000

    unit tests for game and player model layer

---
+++ spec/game_spec.rb
@@ -0,0 +1,15 @@
+require 'game'
+
+describe Game do
+  let(:player_1) { double :player }
+  let(:game) { Game.new(player_1) }
+
+  describe '#attack' do
+    it 'Should return a random weapon' do
+      allow(game).to receive(:computer_weapon).and_return('Paper')
+      expect(game.computer_weapon).to eq 'Paper'
+    end
+
+  end
+
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,15 @@
+require 'player'
+
+describe Player do
+  let(:luisa) { Player.new('Luisa', 'Rock') }
+
+
+  it 'Should return player name' do
+    expect(luisa.name).to eq 'Luisa'
+  end
+
+  it 'returns the weapon' do
+    expect(luisa.weapon).to eq 'Rock'
+  end
+
+end
