7bd6e441b879be4ebca2d333a5d12219f34664c5
cesare camurani <cesare.camurani@gmail.com>
2018-11-19 10:45:47 +0000

    Tests passing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,12 +4,12 @@ describe Game do
   let(:cesare) { double :player_name }
   let(:rock) { double :player_move }
   subject(:game) { described_class.new(cesare, rock) }
-  let(:pc_move) { double Game::PC_MOVES }
+
 
   describe '#au_pair' do
     it "Shows a 'It's a draw!' message" do
       allow(rock).to receive(:to_sym) { :rock }
-      allow(pc_move).to receive(:sample) { :rock }
+      allow(Game::PC_MOVES).to receive(:sample) { :rock }
       expect(game.draw).to eq "It's a Draw!"
     end
   end
@@ -17,16 +17,16 @@ describe Game do
   describe '#player_wins' do
     it "Shows a 'Player Wins!' message" do
       allow(rock).to receive(:to_sym) { :rock }
-      allow(pc_move).to receive(:sample) { :scissors }
-      expect(game.draw).to eq "#{cesare} Wins!"
+      allow(Game::PC_MOVES).to receive(:sample) { :scissors }
+      expect(game.showdown).to eq "#{cesare} Wins!"
     end
   end
 
   describe '#pc_wins' do
     it "Shows a 'PC Wins!' message" do
       allow(rock).to receive(:to_sym) { :rock }
-      allow(pc_move).to receive(:sample) { :paper }
-      expect(game.draw).to eq "PC Wins!"
+      allow(Game::PC_MOVES).to receive(:sample) { :paper }
+      expect(game.showdown).to eq "PC Wins!"
     end
   end
 
