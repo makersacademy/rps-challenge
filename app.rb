b9a19e8a0a5489a6547aa29817346f4ca75b16a8
Jethro Whaley <jethro.whaley@gmail.com>
2018-12-15 15:49:20 +0000

    added post /names, enabled sessions to store name

--- app.rb
+++ app.rb
@@ -2,12 +2,20 @@ require 'sinatra/base'
 
 class Rps < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/game'
+  end
+
   get '/game' do
-    'Samson'
+    @player_1 = session[:player_1_name]
+    erb :game
   end
 
   run! if app_file == $0
--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -1,7 +1,7 @@
 feature 'Players can enter names' do
   scenario 'and see them when battles starts' do
     visit('/')
-    fill_in :name, with: 'Samson'
+    fill_in :player_1_name, with: 'Samson'
     click_button 'Submit'
     expect(page).to have_content 'Samson'
   end
