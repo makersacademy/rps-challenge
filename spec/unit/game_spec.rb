a24d84e96ef3bf0cd4ebaec06b8e0a23be3b14ce
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-15 19:51:07 +0000

    Created game class that can instatiate with one player

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -4,5 +4,9 @@ describe Game do
   subject(:game) { described_class.new(player_1) }
   let(:player_1) { double :player }
 
-  
+  describe '#player_1' do
+    it 'retrieves player 1' do
+      expect(game.player_1).to eq player_1
+    end
+  end
 end
