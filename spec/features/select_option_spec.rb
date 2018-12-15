c957548c88cb893fcca83baccf83644f85018fda
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-15 15:46:07 +0000

    Player can see 3 choices: Rock, Paper and Scissors

--- spec/features/select_option_spec.rb
+++ spec/features/select_option_spec.rb
@@ -3,4 +3,14 @@ feature 'Viewing choices' do
     sign_in_and_play
     expect(page).to have_content('Rock')
   end
+
+  scenario 'seeing paper' do
+    sign_in_and_play
+    expect(page).to have_content('Paper')
+  end
+
+  scenario 'seeing scissors' do
+    sign_in_and_play
+    expect(page).to have_content('Scissors')
+  end
 end
