4a9e32222ce33ee830e9cc3b56c56191e5d81d04
celine kaslin <celine.kaslin@gmail.com>
2018-12-15 20:07:28 +0000

    project set up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../app.rb'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'features/web_helpers'
+
+Capybara.app = Rps
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
