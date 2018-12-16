6825e5dd4fc3eac2c9635d3e427e11a2ef5d1937
Huxley_Millard <gerogiamillardccm@gmail.com>
2018-12-16 16:19:23 +0000

    used srand succefully to show computr choosing random move

--- lib/battle.rb
+++ lib/battle.rb
@@ -1,9 +1,10 @@
 class Battle 
-    MOVES = ['']
+    MOVES = ['garnet', 'amythyst', 'pearl', 'steven', 'lapiz'].freeze
 
-    attr_reader :player
+    attr_reader :player, :opponent_move
 
     def initialize(player_obj)
         @player = player_obj
+        @opponent_move = MOVES.sample
     end
 end
--- spec/unit_testing/battle_spec.rb
+++ spec/unit_testing/battle_spec.rb
@@ -3,11 +3,20 @@ require 'battle'
 describe Battle do 
     let(:player) { double :name => PLAYER_NAME, :move=> PLAYER_MOVE }
     let(:game){ Battle.new(player)}
+    
 
     it 'on initialize takes the player object' do
         expect(game.player.name).to eql(PLAYER_NAME)
     end
 
-    it 'should select a random move from the array of moves' do 
+    it 'should select a random move, pearl, from the array of moves' do 
+        srand(4)
+        expect(game.opponent_move).to eql('pearl')
+    end
+
+
+    it 'should select a random move, steven, from the array of moves' do 
+        srand(5)
+        expect(game.opponent_move).to eql('steven')
     end
 end
