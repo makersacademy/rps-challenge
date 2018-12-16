428d0e1d997e3b6c371e630f1d78fb71c7c28085
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2018-12-16 14:35:35 +0000

    Completed functioning aspp

--- app.rb
+++ app.rb
@@ -19,7 +19,15 @@ class Rps < Sinatra::Base
   end
 
   post '/choice' do
+  $player.randomchoice
   $player.choose(params[:choice])
+  redirect '/final'
+  end
+
+  get '/final' do
+  @result = $player.game_result
+  @computer_choice = $player.computer_choice
+  erb(:final)
   end
 
   run! if app_file == $0
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -13,10 +13,13 @@ feature 'Able to enter name' do
   end
 end
 
-# feature 'Able to play rock paper scissors' do
-#    scenario 'can enter scissors and told if lost, drew or won' do
-#       sign_in_and_play
-#       click_button 'scissors'
-#       expect(page).to scissors_options
-#       end
-#    end
+feature 'Able to play rock paper scissors' do
+   scenario 'can enter scissors and told if lost, drew or won' do
+      sign_in_and_play
+      click_button 'scissors'
+      a = "Computer choses rock: you lose"
+      b = "Computer choses scissors: you draw"
+      c = "Computer choses paper: you win"
+      page.should satisfy {|page| page.has_content?(a) or page.has_content?(b) or page.has_content?(c)}
+      end
+   end
