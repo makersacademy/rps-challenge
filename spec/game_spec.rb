e74f7eeefd89c030999b923f50ee32c52b76d1ea
Jethro Whaley <jethro.whaley@gmail.com>
2018-12-16 23:28:07 +0000

    #draw returns true if moves are equal. Also previous commit had refactored out some unused methods in Game class and game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,9 +5,13 @@ describe Game do
   let(:computer) { double :computer, name: :computer, move: :paper}
   let(:game) { Game.new(player, computer) }
 
-  describe '#draw' do
+  describe '#draw?' do
     it 'returns false if attacks arent equal' do
       expect(game.draw?).to eq false
+    end
+    it 'returns true if attacks are equal' do
+      player_2 = double(:computer, name: :computer, move: :rock)
+      expect(Game.new(player, player_2).draw?).to eq true
     end
   end
 end
