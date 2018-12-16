f4a465c273ea0c07baec917ab973c1c8e66775fd
Huxley_Millard <gerogiamillardccm@gmail.com>
2018-12-16 20:29:56 +0000

    required battle class in app.rb to work online, set up page titles added play again link

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require_relative 'lib/player'
+require_relative 'lib/battle'
 
 set :session_secret, 'super secret' ## this line is allowing us to work with shotgun
 
@@ -15,23 +16,31 @@ class Game < Sinatra::Base
 
   get '/' do 
     'Testing infrastructure working!'
-    erb :home
+    erb :home, locals:{
+      :page_name => 'Begin'
+    }
   end
 
   post '/name' do 
     @player = Player.store_name(params['player_name'])
     erb :play, locals:{
-      :name => params['player_name']
+      :name => params['player_name'],
+      :page_name => 'Choose'
     }
     
   end
 
   post '/move' do 
+    ## here creting the singleton
     @player = Player.create_player(params['char'])
+    ## battle logic
     fight = Battle.new(@player)
+    ## fight play sets outcome variable which i thought was better then also returning it in one
     fight.play
+
     erb :end, locals:{
-      :outcome => fight.outcome
+      :outcome => fight.outcome,
+      :page_name => 'Results'
     }
   end
 
