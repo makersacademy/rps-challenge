95cea04969fc7222e207b6376b661cee1885b779
stonefarmer9 <g.p.levaillant@gmail.com>
2018-12-16 10:35:57 +0000

    3rd test complete, user can now enter a choice for the game

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 
 class Game  < Sinatra::Base
   enable :sessions
+
   get '/' do
     erb :index
   end
@@ -14,6 +15,17 @@ class Game  < Sinatra::Base
   get '/play' do
     @player = session[:player]
     erb :play
+  end
+
+  post '/choice' do
+    session[:player_choice] = params[:player_choice]
+    redirect '/game'
+  end
+
+  get '/game' do
+    @player = session[:player]
+    @player_choice = session[:player_choice]
+    erb :game
   end
 
   run! if app_file == $0
