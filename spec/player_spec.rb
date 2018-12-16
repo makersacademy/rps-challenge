25d1b99ca5835b48c5432157988ad1e2baff7e28
Jethro Whaley <jethro.whaley@gmail.com>
2018-12-16 13:29:57 +0000

    player attack choice added to player

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,4 +7,10 @@ describe Player do
   it 'is created with a name' do
     expect(player.name).to eq("Samson")
   end
+
+  describe '#attack' do
+    it 'contains the players attack' do
+      expect(player.attack("Rock")).not_to be_empty
+    end
+  end
 end
