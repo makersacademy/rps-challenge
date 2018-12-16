e73676dad4aca3508f04271f63bc492afd395836
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2018-12-16 13:43:32 +0000

    Added win/lose/draw calculator

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
 
-  attr_reader :name, :choice
+  attr_reader :name, :choice, :computer_choice
 
   def initialize(name)
     @name = name
@@ -10,4 +10,28 @@ class Player
     @choice = choice
   end
 
+  def randomchoice
+    @computer_choice = ["rock", "paper", "scissors"].sample
+  end
+
+  def game_result
+    a = "rock"
+    b = "paper"
+    c = "scissors"
+
+    lose1 = choice == a && computer_choice == b
+    lose2 = choice == b && computer_choice == c
+    lose3 = choice == c && computer_choice == a
+    win1 = choice == a && computer_choice == c
+    win2 = choice == b && computer_choice == a
+    win3 = choice == c && computer_choice == b
+
+    if computer_choice == choice
+      "draw"
+    elsif lose1 || lose2 || lose3
+      "lose"
+    elsif win1 || win2 || win3
+      "win"
+    end
+  end
 end
