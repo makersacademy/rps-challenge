52d9e66b8326577ecf2794b7ddf97dbbdc8be6d7
Simon Cass <scass1991@gmail.com>
2018-12-16 19:33:49 +0000

    Further tweaks)

--- app.rb
+++ app.rb
@@ -4,19 +4,23 @@ require "./lib/player.rb"
 
 class Rpsgame < Sinatra::Base
 
+  enable :sessions
+
+  before { @game = Game.instance }
+
   get "/" do
     erb(:index)
   end
 
   post "/hold" do
     @player_1 = params[:player_1]
-    @player_2 = Player.new("computer", "")
+    @player_2 = Player.new
     @game = Game.create(@player_1, @player_2)
-    erb(:hold)
+    erb :hold
   end
 
   post "/game" do
-    erb(:game)
+    erb :game
   end
 
   run! if app_file == $0
