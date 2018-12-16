78029cd0dd05f17c1d181039993e1c9e762e2e3f
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2018-12-16 15:09:44 +0000

    Stub test created for choice method which returns R/P/S at random

--- lib/game.rb
+++ lib/game.rb
@@ -3,11 +3,11 @@ class Game
   attr_reader :options
 
   def initialize
-    @options = ["Rock", "Paper", "Scissors"]
+    @options = { 1 => "Rock", 2 => "Paper", 3 => "Scissors" }
   end
 
   def choice
-
+    @options[rand(1..3)]
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,9 +2,16 @@ require "game"
 
 describe Game do
 
+  subject(:game) {described_class.new}
+  # let(:rock) { double :rock}
+
   it "expects game class to be initialised with an array of RPS choices" do
-    expect(subject.options).to eq ["Rock", "Paper", "Scissors"]
+    expect(game.options).to eq({ 1 => "Rock", 2 => "Paper", 3 => "Scissors" })
   end
 
+  it "expects a random selection of rock, paper, or scissors" do
+    allow(game).to receive(:choice) { "Rock" }
+    expect(game.choice).to eq "Rock"
+  end
 
 end
