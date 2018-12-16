13f9118cf0149e9b0d1414ae723846cb9b5faea9
Alina Goaga <goaga.alina@gmail.com>
2018-12-16 13:30:35 +0000

    Player can choose one option

--- rps.rb
+++ rps.rb
@@ -10,16 +10,20 @@ class Rps < Sinatra::Base
 
   post '/name' do
     player = Player.new(params[:player_name])
-    session[:player_name] = player.name
+    session[:player] = player
     redirect '/play'
   end
 
   get '/play' do
-    @player_name = session[:player_name]
+    @player_name = session[:player].name
     erb :play
   end
 
-  post '/player_selection' do
+  post '/selection' do
+    @player_name = session[:player].name
+    @player = session[:player]
+    @player.selected_option = params[:option]
+    erb :selection
   end
 
   run! if app_file == $PROGRAM_NAME
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -11,10 +11,10 @@ feature 'Options' do
     expect(page).to have_content("Mario vs Computer!\nChoose from these different options:\nrock paper scissors")
   end
 
-  # scenario 'user chooses one option' do
-  #   sign_in_and_play
-  #   choose('rock')
-  #   click_button 'Submit'
-  #   expect(page).to have_content('Mario chose rock! Let us see what the computer chooses!')
-  # end
+  scenario 'user chooses one option' do
+    sign_in_and_play
+    choose('rock')
+    click_button 'Submit'
+    expect(page).to have_content('Mario chose rock! Let us see what the computer chooses!')
+  end
 end
