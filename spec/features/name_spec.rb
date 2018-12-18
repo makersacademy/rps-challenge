060140a25c07ee0a8a6929f4232cc06c0f2f4afd
Shaneil Clarke <shaneilske@yahoo.com>
2018-12-18 11:24:42 +0000

    The player will be presented the choices (rock, paper and scissors)

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -3,4 +3,13 @@ feature 'Homepage' do
     sign_in_and_play
     expect(page).to have_content 'Welcome Socks!'
   end
+
+  scenario 'play game' do
+    sign_in_and_play
+    expect(page).to have_content 'Choose your attack:'
+    expect(page).to have_content 'Computer vs. Socks'
+    select 'Rock', from: 'user_choice'
+    click_button 'Go'
+  end
+
 end
