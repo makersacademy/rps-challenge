48e82553ed00891d6952dc7df2b91f9d60f0c363
becc-mu <becc.mulugeta@gmail.com>
2018-12-26 16:22:39 +0000

    rock beaten by paper logic

--- spec/rps_game_spec.rb
+++ spec/rps_game_spec.rb
@@ -8,39 +8,9 @@ describe RpsGame do
     expect(game.move('rock', 'scissors')).to eq RpsGame::P1_WINS
   end
 
-  # it 'rock beaten by paper' do
-  #   expect(game.move('rock', 'paper')).to eq RpsGame::P2_WINS
-  # end
-  #
-  # # paper
-  # it 'paper beats rock' do
-  #   expect(game.move('paper', 'rock')).to eq RpsGame::P1_WINS
-  # end
-  #
-  # it 'paper beaten by scissors' do
-  #   expect(game.move('paper', 'scissors')).to eq RpsGame::P2_WINS
-  # end
-  #
-  # # scissors
-  # it 'scissors beats paper' do
-  #   expect(game.move('scissors', 'paper')).to eq RpsGame::P1_WINS
-  # end
-  #
-  # it 'scissors beaten by rock' do
-  #   expect(game.move('scissors', 'rock')).to eq RpsGame::P2_WINS
-  # end
-  #
-  # # same move --> draw
-  # it 'draw' do
-  #   ['rock', 'paper', 'scissors'].each do |m|
-  #     expect(game.move(m, m)).to eq RpsGame::DRAW
-  #   end
-  # end
-  #
-  # # wrong value(s)
-  # it 'supplied wrong move' do
-  #   expect { game.move('foobar', 'rock') }.to raise_error("invalid move")
-  #   expect { game.move('paper', 'inalid') }.to raise_error("invalid move")
-  #   expect { game.move('foobar', 'invalid') }.to raise_error("invalid move")
-  # end
+  it 'rock beaten by paper' do
+    expect(game.move('rock', 'paper')).to eq RpsGame::P2_WINS
+  end
+
+  
 end
