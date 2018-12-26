77937b056bd273172fb5a2319d7ba46b7e264153
becc-mu <becc.mulugeta@gmail.com>
2018-12-26 15:30:55 +0000

    added player registration form

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,13 @@
-feature 'Entering a name' do
-  scenario 'user register by submitting name' do
+require 'rps_game'
+feature 'Player registers' do
+  scenario 'player submit a name' do
     visit '/'
     fill_in :player_name, with: 'Sam'
     click_button 'Submit'
     expect(page).to have_content 'Sam'
+  end
+
+  scenario 'has radio buttons to select a move' do
+    expect(page).to have_field 'move'
   end
 end
