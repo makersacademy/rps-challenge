c49cfc9d522f49b383d03338ac022861f023450f
becc-mu <becc.mulugeta@gmail.com>
2018-12-26 16:20:07 +0000

    game logic pass rock beats scissors

---
+++ lib/rps_game.rb
@@ -0,0 +1,23 @@
+class RpsGame
+
+  P1_WINS = 0
+  P2_WINS = 1
+  DRAW    = 2
+
+  def move(p1_move, p2_move)
+
+    case p1_move
+    when 'rock'
+      case p2_move
+      when 'rock'
+        return DRAW
+      when 'paper'
+        return P2_WINS
+      when 'scissors'
+        return P1_WINS
+      end
+    end
+
+    raise 'invalid move'
+  end
+end
