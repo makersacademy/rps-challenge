72e62ee5696d5610dd50c2f1c4775c9521086228
becc-mu <becc.mulugeta@gmail.com>
2018-12-27 15:57:22 +0000

    refactored user_plays and added helper methods

--- spec/features/user_plays_spec.rb
+++ spec/features/user_plays_spec.rb
@@ -1,33 +1,16 @@
 feature 'Play' do
-  before do
-    visit '/'
-    fill_in :player_name, with: 'Sam'
-    click_button 'Submit'
-  end
-  scenario 'A checkbox' do
-    visit '/play'
 
+  scenario 'A checkbox' do
+    sign_in
+    visit '/play'
     expect(page).to have_field('move_rock', with: 'rock')
     expect(page).to have_field('move_paper', with: 'paper')
     expect(page).to have_field('move_scissors', with: 'scissors')
   end
 
   scenario 'user submits move and gets his move displayed' do
-    visit '/play'
+    sign_in_and_play
+    choose('move_rock')
     expect(page).to have_selector("input[value='rock']")
-    has_checked_field?('move_rock')
-    #within_fieldset
-    #ßclick_on 'rock'
-    expect(page).to have_field('move_rock', with: 'rock')
-    expect(page).to have_checked_field('rock')
-
-  end
-
-  scenario 'user gets you win or loose message' do
-    move_index =
-    expect(page).to have_selector("input[value='rock']")
-    expect(page).to have_field('move_rock', with: 'rock')
-    visit '/do_play'
-    expect(page).to have_content('You Win').or have_content('You Lose')
   end
 end
---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,12 @@
+def sign_in
+  visit '/'
+  fill_in :player_name, with: 'Sam'
+  click_button 'Submit'
+end
+
+def sign_in_and_play
+  visit '/'
+  fill_in :player_name, with: 'Sam'
+  click_button 'Submit'
+  visit '/play'
+end
