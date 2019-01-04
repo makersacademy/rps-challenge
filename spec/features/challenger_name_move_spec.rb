35a8c839afe955c5a18a3b4facd50aaf7f3daa7f
CKKH <chriskkhassan@gmail.com>
2019-01-04 14:27:24 +0000

    Tidied feature directory

--- spec/features/challenger_name_move_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Challenger input' do
-  scenario 'challenger submits name and move' do
-    sign_in_and_select_move
-    expect(page).to have_content 'Link chose rock'
-  end
-end
--- spec/features/win_scenario_spec.rb
+++
@@ -1,7 +0,0 @@
-feature 'Loss' do
-  scenario 'challenger move results in loss' do
-    allow_any_instance_of(Computer).to receive(:choice).and_return(:paper)
-    sign_in_and_select_move
-    expect(page).to have_content "Link chose rock Computer chose paper paper beats rock, Link defeated!"
-  end
-end
