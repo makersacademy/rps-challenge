cab559a06034c50f36ce3e3c04f11c18845be72b
Vaith Schmitz <vaithschmitz@Vaiths-MacBook-Pro.local>
2019-01-25 18:09:02 +0000

    show choices

--- app.rb
+++ app.rb
@@ -9,11 +9,16 @@ get '/' do
 end
 
 post '/names' do
-  @p1name = params[:p1name]
-  @p2name = params[:p2name]
-  erb :play
+  session[:p1name] = params[:p1name]
+  session[:p2name] = params[:p2name]
+  redirect '/play'
 end
 
+get '/play' do
+  @p1name = session[:p1name]
+  @p2name = session[:p2name]
+  erb(:play)
+end
 
 # start the server if ruby file executed directly
 run! if app_file == $0
