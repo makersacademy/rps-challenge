ccbe62f124366f7c412f0c9930274e62b32ee143
habakkukyong <habakkukyong@gmail.com>
2019-01-25 11:13:04 +0000

    Test name registration

--- spec/features/app_feature_spec.rb
+++ spec/features/app_feature_spec.rb
@@ -4,6 +4,11 @@ require 'selenium-webdriver'
 feature RPSServer do
   scenario 'client makes get request to root' do
     visit '/'
-    expect(page).to have_content "Rock Paper Scissors"
+    expect(page).to have_content 'Rock Paper Scissors'
+  end
+
+  scenario 'client enters name at root and clicks submit' do
+    visit_root_sign_in_as_habakkuk
+    expect(page).to have_content 'Habakkuk'
   end
 end
