44ec4314e2f20586892c65b1f44c354f7b44326b
isabelcooper <isabel@triptease.com>
2019-01-25 15:54:59 +0000

    player can store wins and losses in hash

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,25 @@
 class Player
 
-  attr_reader :name, :moves
+  attr_reader :name, :moves, :outcomes
 
   def initialize(name)
     @name = name
     @moves = []
+    @outcomes = { "won" =>[],
+      "lost" => [],
+      "draw" => []
+    }
   end
 
   def store_move(move)
     @moves << move
+  end
+
+  def add_outcome(result, game)
+    @outcomes[result] << game
+  end
+
+  def count_outcome(category)
+    @outcomes[category].count
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,6 +3,7 @@ require 'player'
 describe Player do
 
   let(:subject) { Player.new("Lily") }
+  let(:round) {double(:round,)}
 
   it 'should initialize a player with a name' do
     expect(subject.name).to eq 'Lily'
@@ -12,4 +13,15 @@ describe Player do
     subject.store_move('rock')
     expect(subject.moves).to eq ['rock']
   end
+
+  it 'should store games by win/loss category' do
+    subject.add_outcome("won", round)
+    expect(subject.outcomes).to eq ({"won"=>[round], "lost"=>[], "draw"=>[]})
+  end
+
+  it 'should count number of wins' do
+    subject.add_outcome("won", round)
+    expect(subject.count_outcome("won")).to eq 1
+  end
+
 end
