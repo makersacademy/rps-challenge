b3faa8626a89062916348b6f14f511237f716862
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-01-25 13:47:13 +0000

    added rspec lines to increase coverage. needs refactoring

--- lib/battle.rb
+++ lib/battle.rb
@@ -3,10 +3,17 @@ class Battle
     @player1 = player1
     @player2 = player2
   end
+
   def winner
     return result[:draw] ? :draw : result[:win]
   end
+
+  def loser
+    return result[:draw] ? :draw : result[:lose]
+  end
+
   private
+
   def result
     lookup = {
       rock: {rock: :draw, paper: :lose, scissors: :win},
@@ -14,9 +21,9 @@ class Battle
       scissors: {rock: :lose, paper: :win, scissors: :draw}
     }
     case lookup[@player1.move][@player2.move]
-    when :draw then return {win: nil, lose: nil, draw: true}
-    when :win then return {win: @player1, lose: @player2, draw: false}
-    when :lose then return {win: @player2, lose: @player1, draw: false}
-  end
+      when :draw then return {win: nil, lose: nil, draw: true}
+      when :win then return {win: @player1, lose: @player2, draw: false}
+      when :lose then return {win: @player2, lose: @player1, draw: false}
+    end
   end
 end
--- spec/battle_spec.rb
+++ spec/battle_spec.rb
@@ -9,11 +9,47 @@ describe Battle do
       allow(@player).to receive(:move) { :rock }
       allow(@computer).to receive(:move) { :scissors }
       expect(Battle.new(@player,@computer).winner).to eq(@player)
+      allow(@player).to receive(:move) { :paper }
+      allow(@computer).to receive(:move) { :rock }
+      expect(Battle.new(@player,@computer).winner).to eq(@player)
+      allow(@player).to receive(:move) { :scissors }
+      allow(@computer).to receive(:move) { :paper }
+      expect(Battle.new(@player,@computer).winner).to eq(@player)
     end
     it "returns :draw if it's a draw" do
       allow(@player).to receive(:move) { :rock }
       allow(@computer).to receive(:move) { :rock }
       expect(Battle.new(@player,@computer).winner).to eq(:draw)
+      allow(@player).to receive(:move) { :paper }
+      allow(@computer).to receive(:move) { :paper }
+      expect(Battle.new(@player,@computer).winner).to eq(:draw)
+      allow(@player).to receive(:move) { :scissors }
+      allow(@computer).to receive(:move) { :scissors }
+      expect(Battle.new(@player,@computer).winner).to eq(:draw)
+    end
+  end
+  describe "#loser" do
+    it "returns the loser" do
+      allow(@player).to receive(:move) { :rock }
+      allow(@computer).to receive(:move) { :scissors }
+      expect(Battle.new(@player,@computer).loser).to eq(@computer)
+      allow(@player).to receive(:move) { :paper }
+      allow(@computer).to receive(:move) { :rock }
+      expect(Battle.new(@player,@computer).loser).to eq(@computer)
+      allow(@player).to receive(:move) { :scissors }
+      allow(@computer).to receive(:move) { :paper }
+      expect(Battle.new(@player,@computer).loser).to eq(@computer)
+    end
+    it "returns :draw if it's a draw" do
+      allow(@player).to receive(:move) { :rock }
+      allow(@computer).to receive(:move) { :rock }
+      expect(Battle.new(@player,@computer).loser).to eq(:draw)
+      allow(@player).to receive(:move) { :scissors }
+      allow(@computer).to receive(:move) { :scissors }
+      expect(Battle.new(@player,@computer).loser).to eq(:draw)
+      allow(@player).to receive(:move) { :paper }
+      allow(@computer).to receive(:move) { :paper }
+      expect(Battle.new(@player,@computer).loser).to eq(:draw)
     end
   end
 end
