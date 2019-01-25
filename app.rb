f4b66623b1b8819dead4c891502a2c4595888a4f
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-01-25 11:55:40 +0000

    RGR feature test 'see player name in lights'

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+  get "/" do
+    erb(:enterplayer)
+  end
+  post "/registername" do
+    session['player1name'] = params['player1name']
+    redirect '/match'
+  end
+  get "/match" do
+    @player1name = session['player1name']
+    erb(:match)
+  end
   run! if app_file == $0
 end
