10ab449e081244212e18bb7fee7e885486390a9f
Paul <paul.ayling@bglgroup.co.uk>
2019-01-25 11:18:08 +0000

    Feature tests working

--- spec/features/test_infrastructure_spec.rb
+++ spec/features/test_infrastructure_spec.rb
@@ -10,4 +10,28 @@ feature 'Testing infrastructure' do
     enter_names_and_play
     expect(page).to have_content "Paul vs. Computer"
   end
+
+  scenario 'Can display the correct move input' do
+    visit('/')
+    enter_names_and_play
+    click_button 'Rock'
+    expect(page).to have_content "Paul picked: Rock"
+  end
+
+  scenario 'Can display the correct score on a win' do
+    allow_any_instance_of(Computer).to receive(:move) { "Scissors" }
+    visit('/')
+    enter_names_and_play
+    click_button 'Rock'
+    expect(page).to have_content "Paul wins this round!"
+  end
+
+  scenario 'Can display game_over screen and winner on a win' do
+    allow_any_instance_of(Computer).to receive(:move) { "Scissors" }
+    visit('/')
+    enter_names_and_play
+    2.times { rock_and_ok }
+    click_button 'Rock'
+    expect(page).to have_content "Game Over! Paul Wins!"
+  end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,11 @@
 def enter_names_and_play
-  visit ('/')
   fill_in('player1', with: 'Paul')
+  fill_in('rounds', with: '5')
   # fill_in('player2', with: 'John')
   click_button('Play!')
+end
+
+def rock_and_ok
+  click_button 'Rock'
+  click_button 'Ok'
 end
