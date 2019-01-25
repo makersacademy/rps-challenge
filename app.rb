f07202f2b0ef98fc187e78d098f6ccbc7b126ca3
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-25 18:10:35 +0000

    Add routes to app.rb

--- app.rb
+++ app.rb
@@ -8,6 +8,16 @@ class RockPaperScissors < Sinatra::Base
     erb :index
   end
 
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/game'
+  end
+
+  get '/game' do
+    @name = session[:name]
+    erb :game
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
