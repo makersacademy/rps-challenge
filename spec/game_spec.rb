4d91c2ac923b05b3fc9186bcc106319feec35640
sg1110 <saule.guzyte@tesco.com>
2019-01-25 13:57:02 +0000

    Game class tests updated

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,16 +1,21 @@
 describe Game do
 
-  let(:marketeer) {double(:player1)}
+  let(:marketeer) {double(:player1, hand: "Rock")}
   let(:game) {Game.create(marketeer)}
 #tests for ?create and instance methods?
-
 
   it 'Game initializes with player 1 object' do
     expect(game.player1).to eq marketeer
   end
 
-  it 'Can receive computers_hand, (selects rock)' do
+  # it 'Can receive computers_hand, (selects rock)' do
+  #   allow_any_instance_of(Game).to receive(:computers_hand) { "Rock" }
+  #   expect(game.computers_hand).to eq "Rock"
+  # end
+
+  it 'Will confirm if the player chose a winning hand' do
     allow_any_instance_of(Game).to receive(:computers_hand) { "Rock" }
-    expect(game.computers_hand).to eq "Rock"
+    expect(game.gaming).to eq "Play again, no one wins (rock,rock)"
   end
+
 end
