5dd8fc3eb40ea696c852894a970bfef6a6e2779f
habakkukyong <habakkukyong@gmail.com>
2019-01-25 11:13:44 +0000

    Pass test name registration; allow app to be executed directly

--- app.rb
+++ app.rb
@@ -2,7 +2,22 @@ require 'sinatra/base'
 
 # Handle http requests
 class RPSServer < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
+
+  post '/rps' do
+    session[:name] = params[:name]
+    redirect '/rps'
+  end
+
+  get '/rps' do
+    @name = session[:name]
+    erb :rps
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
