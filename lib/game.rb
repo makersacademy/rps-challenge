bcf808a6a5f110b60afdc989d6b50238cf67f1cf
Katie Cornish <katielcornish@gmail.com>
2019-01-25 12:09:02 +0000

    Game defaults to 1 player if only one player argument is passed to it

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,7 @@
 class Game
 
+  attr_reader :player_1, :player_2
+
   def initialize(player_1, player_2 = Player.new("Computer"))
     @player_1 = player_1
     @player_2 = player_2
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,17 @@ require 'game'
 
 describe Game do
 
-  let(:player_1) { double(:player_1) }
-  let(:player_2) { double(:player_2) }
+  let(:player_1)      { double(:player_1, name: "Player 1") }
+  let(:player_2)      { double(:player_2, name: "Player 2") }
+  let(:one_player_game) { described_class.new(player_1) }
+  let(:two_player_game) { described_class.new(player_1, player_2) }
 
   it 'Accepts two players as arguments' do
     expect(described_class).to respond_to(:new).with(2).arguments
+  end
+
+  it 'Defaults to a 1 player game against the computer if only one player is passed on initialization' do
+    expect(one_player_game.player_2.name).to eq "Computer"
   end
 
 end
