87becf450eadbda57ad890b922169d1e1aea51e0
Paul <paul.ayling@bglgroup.co.uk>
2019-01-25 15:08:22 +0000

    Multiplayer fully feature tested

--- spec/features/test_infrastructure_spec.rb
+++ spec/features/test_infrastructure_spec.rb
@@ -15,14 +15,14 @@ feature 'Testing infrastructure' do
     scenario 'Can accept and display names given' do
       visit('/')
       single_player
-      enter_names_and_play
+      enter_one_name_and_play
       expect(page).to have_content "Paul vs. Computer"
     end
 
     scenario 'Can display the correct move input' do
       visit('/')
       single_player
-      enter_names_and_play
+      enter_one_name_and_play
       click_button 'Rock'
       expect(page).to have_content "Paul picked: Rock"
     end
@@ -31,7 +31,7 @@ feature 'Testing infrastructure' do
       allow_any_instance_of(Computer).to receive(:move) { "Scissors" }
       visit('/')
       single_player
-      enter_names_and_play
+      enter_one_name_and_play
       click_button 'Rock'
       expect(page).to have_content "Paul wins this round!"
     end
@@ -41,10 +41,47 @@ feature 'Testing infrastructure' do
       allow_any_instance_of(Computer).to receive(:move) { "Scissors" }
       visit('/')
       single_player
-      enter_names_and_play
+      enter_one_name_and_play
       2.times { rock_and_ok }
       click_button 'Rock'
       expect(page).to have_content message
     end
+  # End of Single-player
   end
+
+  describe '#Multiplayer' do
+    scenario 'Can run app and check page content' do
+      visit('/')
+      multi_player
+      expect(page).to have_content 'Player 2 Name :'
+    end
+
+    scenario 'Can accept and display both given names' do
+      visit('/')
+      multi_player
+      enter_two_names_and_play
+      expect(page).to have_content 'Paul vs. John'
+    end
+
+    scenario 'Can switch turns correctly' do
+      visit('/')
+      multi_player
+      enter_two_names_and_play
+      click_button 'Rock'
+      expect(page).to have_content "John's Turn"
+    end
+
+    scenario 'Can display game_over screen and winner on a win' do
+      message = "Game Over! :( John Wins! Final Score: Paul = 0 John = 3"
+      visit('/')
+      multi_player
+      enter_two_names_and_play
+      2.times { scissors_and_rock }
+      click_button 'Scissors'
+      click_button 'Rock'
+      expect(page).to have_content message
+    end
+  # End of Multi-player
+  end
+# End of infrastructure
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,9 +1,10 @@
-# Fills In Player and Round Fields
+# Selects singleplayer
 def single_player
   click_button('Single-Player')
 end
 
-def enter_names_and_play
+# Fills in One Player and 5 rounds
+def enter_one_name_and_play
   fill_in('player1', with: 'Paul')
   fill_in('rounds', with: '5')
   # fill_in('player2', with: 'John')
@@ -12,6 +13,26 @@ end
 
 # Clicks Rock and Ok
 def rock_and_ok
+  click_button 'Rock'
+  click_button 'Ok'
+end
+
+# Selects multiplayer
+def multi_player
+  click_button('Multi-Player')
+end
+
+# Fills in Two Player and 5 rounds
+def enter_two_names_and_play
+  fill_in('player1', with: 'Paul')
+  fill_in('player2', with: 'John')
+  fill_in('rounds', with: '5')
+  click_button('Play!')
+end
+
+# Player 1 Picks Scissors, Player 2 Picks Rock
+def scissors_and_rock
+  click_button 'Scissors'
   click_button 'Rock'
   click_button 'Ok'
 end
