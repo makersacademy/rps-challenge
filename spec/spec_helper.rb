5b713211cd8888550c442257e51079807b38668e
Tom Damant <tom@gluon.local>
2019-01-25 09:49:03 +0000

    first commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
-require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require File.join(File.dirname(__FILE__),'..','app.rb')
+
+ENV['RACK_ENV'] = 'test'
+Capybara.app = Rps
+
+
+
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
