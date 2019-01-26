89dc87035425279e3b8b38376a66b3fa813e1ece
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-26 20:26:04 +0000

    Implement second player names

--- lib/app.rb
+++ lib/app.rb
@@ -12,11 +12,13 @@ class RPS < Sinatra::Base
 
   post '/name' do
     session[:player_one_name] = params["player_one_name"]
+    session[:player_two_name] = params["player_two_name"]
     redirect '/play'
   end
 
   get '/play' do
     @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
     @player_one_weapon = session[:weapon]
     erb :play
   end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,7 @@
 def sign_in_and_play
   visit('/')
   fill_in('player_one_name', with: 'Jill')
+  fill_in('player_two_name', with: 'Jack')
   click_button('Submit')
 end
 
