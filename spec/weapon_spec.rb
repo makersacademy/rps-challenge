b911fee47630f6e0f2cd37d5230000168ebbe81a
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-26 21:49:39 +0000

    Lizard and Spock tests

--- spec/weapon_spec.rb
+++ spec/weapon_spec.rb
@@ -21,8 +21,16 @@ describe Rock do
     expect(Rock.compare(Scissors)).to eq(:win)
   end
 
+  it 'should win against lizard' do
+    expect(Rock.compare(Lizard)).to eq(:win)
+  end
+
   it 'should lose against paper' do
     expect(Rock.compare(Paper)).to eq(:lose)
+  end
+
+  it 'should lose against spock' do
+    expect(Rock.compare(Spock)).to eq(:lose)
   end
 end
 
@@ -36,8 +44,16 @@ describe Scissors do
     expect(Scissors.compare(Paper)).to eq(:win)
   end
 
+  it 'should win against lizard' do
+    expect(Scissors.compare(Lizard)).to eq(:win)
+  end
+
   it 'should lose against rock' do
     expect(Scissors.compare(Rock)).to eq(:lose)
+  end
+
+  it 'should lose against spock' do
+    expect(Scissors.compare(Spock)).to eq(:lose)
   end
 end
 
@@ -51,7 +67,59 @@ describe Paper do
     expect(Paper.compare(Rock)).to eq(:win)
   end
 
+  it 'should win against spock' do
+    expect(Paper.compare(Spock)).to eq(:win)
+  end
+
   it 'should lose against scissors' do
     expect(Paper.compare(Scissors)).to eq(:lose)
+  end
+
+  it 'should lose against lizard' do
+    expect(Paper.compare(Lizard)).to eq(:lose)
+  end
+end
+
+describe Spock do
+  it 'should draw to Spock' do
+    expect(Spock.compare(Spock)).to eq(:draw)
+  end
+
+  it 'should win against scissors' do
+    expect(Spock.compare(Scissors)).to eq(:win)
+  end
+
+  it 'should win against rock' do
+    expect(Spock.compare(Rock)).to eq(:win)
+  end
+
+  it 'should lose against lizard' do
+    expect(Spock.compare(Lizard)).to eq(:lose)
+  end
+
+  it 'should lose against paper' do
+    expect(Spock.compare(Paper)).to eq(:lose)
+  end
+end
+
+describe Lizard do
+  it 'should draw to Lizard' do
+    expect(Lizard.compare(Lizard)).to eq(:draw)
+  end
+
+  it 'should win against paper' do
+    expect(Lizard.compare(Paper)).to eq(:win)
+  end
+
+  it 'should win against spock' do
+    expect(Lizard.compare(Spock)).to eq(:win)
+  end
+
+  it 'should lose against scissors' do
+    expect(Lizard.compare(Scissors)).to eq(:lose)
+  end
+
+  it 'should lose against rock' do
+    expect(Lizard.compare(Rock)).to eq(:lose)
   end
 end
