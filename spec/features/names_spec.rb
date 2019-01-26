be6e97d22aba1b0876e40777cb1331f858ec5a3f
ellie turnock <ebturnock@gmail.com>
2019-01-26 13:38:06 +0000

    Test: player can enter their name

---
+++ spec/features/names_spec.rb
@@ -0,0 +1,8 @@
+feature 'Player Names' do
+  scenario '1. Players can enter their names' do
+    visit ('/')
+    fill_in :player, with: 'Neville'
+    click_button 'Submit'
+    expect(page).to have_content 'Welcome to the game, Neville.'
+  end
+end
