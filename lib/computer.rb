f3601f1c8128789ac1f448d6af29aa2abc1c0ef8
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-01-26 13:27:06 +0000

    Added Computer class tests.

---
+++ lib/computer.rb
@@ -0,0 +1,8 @@
+require_relative 'player.rb'
+
+class Computer < Player
+  def initialize
+    @name = "computer"
+  end
+
+end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,3 +1,13 @@
 require 'computer'
-describe NonPlayer do
+describe Computer do
+  it "has name value 'computer'" do
+    expect(subject.name).to eq("computer")
+  end
+
+  describe "#choose_move" do
+    it "doesn't require arguments" do
+      expect {subject.choose_move}.to_not raise_error
+    end
+    it "chooses move randomly"
+  end
 end
