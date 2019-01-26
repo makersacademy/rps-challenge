527423c412fbf8ea7d124d6981fad780cfa2ad6b
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-26 16:37:34 +0000

    Test: User sees his name on the result page

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -3,7 +3,7 @@ require './app'
 RSpec.describe Rps do
 
   feature 'Testing UI registration form' do
-    scenario 'User can enter name' do
+    scenario 'User can enter a name' do
       visit('/')
       name = 'name'
       fill_in 'your_name', with: name
@@ -23,8 +23,17 @@ RSpec.describe Rps do
     end
   end
 
-
-
+  feature 'Testing name appearance on the result page' do
+    scenario 'User sees his name on the result page' do
+      visit('/')
+      name = 'name'
+      fill_in 'your_name', with: name
+      click_button 'Register'
+      select('Rock', from: 'rock_paper_scissors')
+      click_button 'Play'
+      expect(page).to have_content('Rock')
+    end
+  end
 
 
 
