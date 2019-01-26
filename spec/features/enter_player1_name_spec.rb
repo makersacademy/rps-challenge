a45be0e3a72a0f32dd86deb83dd0ea9b1e64218d
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-01-26 19:45:27 +0000

    deleted obselete rspec test

--- spec/features/enter_player1_name_spec.rb
+++
@@ -1,8 +0,0 @@
-describe "Player 1 can enter their name", type: :feature do
-  it "Puts the players name up in lights" do
-    visit '/'
-    fill_in "player1name", with: "someguy"
-    click_on "Play!"
-    expect(page).to have_content("someguy")
-  end
-end
