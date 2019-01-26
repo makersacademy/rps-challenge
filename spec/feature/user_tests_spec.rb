50e66531250dd4e3d196336f0e2fcbbfa1a4860e
Simon Green <simon.green@thielsen.co.uk>
2019-01-26 14:36:55 +0000

    Created test for name function

--- spec/feature/user_tests_spec.rb
+++ spec/feature/user_tests_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Rps do
     expect(page).to have_content("Welcome to RPS")
   end
 
+  scenario 'confirms we can enter name and be welcomed' do
+    visit('/')
+    fill_in "name", :with => "Bob"
+    click_button "Choose weapon"
+    expect(page).to have_content("Bob, please choose your weapon")
+  end
+
 end
