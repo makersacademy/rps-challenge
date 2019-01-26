ed95b804dfbde5e43673470a70fe645511757d63
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-01-26 12:41:32 +0000

    first feature test passes: player gets greeted by name (i.e. name is stored)

--- app.rb
+++ app.rb
@@ -3,9 +3,14 @@ require 'sinatra'
 class RPS < Sinatra::Base
   enable :sessions
 
-get '/' do
-  @player1_name = params[:player1_name]
-  erb(:index)
-end
+  get '/' do
+    @player1_name = params[:player1_name]
+    erb(:index)
+  end
+
+  post '/game' do
+    @player1_name = params[:player1_name]
+    erb(:game)
+  end
 
 end
--- spec/rps_challenge_spec.rb
+++ spec/rps_challenge_spec.rb
@@ -1,6 +1,6 @@
 feature RPS do
-scenario 'Initial page allows user to input username' do
-  visit('/')
-  expect(page).to have_field("player1_name")
-end
+  scenario 'Initial page allows user to input username' do
+    visit('/')
+    expect(page).to have_field("player1_name")
+  end
 end
