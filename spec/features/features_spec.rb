a0e42cf39ccbb02a753836e1bbc3f4ca2d958385
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-01-26 11:42:56 +0000

    First commit, very basic feature test constructed (does not pass yet as app file not present)

---
+++ spec/features/features_spec.rb
@@ -0,0 +1,17 @@
+
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+feature 'Takes and stores Player 1 username' do
+  scenario 'Can load app, sign in with a username and get presented with it' do
+    visit('/')
+    fill_in :player1_name, with: "Marketeer 1"
+    click_button 'Play'
+    expect(page).to have_content "Welcome to Makers RPS, Marketeer 1"
+  end
+end
+
+
+# As a marketeer
+# So that I can enjoy myself away from the daily grind
+# I would like to be able to play rock/paper/scissors
