acf18401f8b194108d23c9b33377f9b04e7d8551
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-26 19:30:18 +0000

    add feature test to check rock/paper/scissors on /game

--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -7,11 +7,22 @@ feature 'Capybara tests...' do
     expect(page).to have_content 'Wilson'
   end
 
+  scenario 'Player can see all the options as buttons after signing in' do
+    visit('/')
+    fill_in 'name', with: 'Wilson'
+    click_button 'Go!'
+    expect(page).to have_selector(:link_or_button, 'Rock')
+    expect(page).to have_selector(:link_or_button, 'Paper')
+    expect(page).to have_selector(:link_or_button, 'Scissors')
+
+  end
+
   scenario 'Player is able to play rock/paper/scissors' do
     visit('/')
     fill_in 'name', with: 'Wilson'
     click_button 'Go!'
     click_button 'Rock'
-    expect(page).to have_content 'Results'
+    expect(page).to have_content 'Play again'
+    expect(page).to have_link('', href: '/game')
   end
 end
