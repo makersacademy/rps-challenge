dc2d767e5f08cc9b8a87fca262f87832ba3cd693
ellie turnock <ebturnock@gmail.com>
2019-01-26 13:23:49 +0000

    set up testing environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = RockPaperScissors
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
