2442c5c1872732e78ed31becb9017f822cfb4f01
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-26 09:57:10 +0000

    Add feature test for 'player can play game'

--- spec/features/capybara_spec.rb
+++ spec/features/capybara_spec.rb
@@ -1,8 +1,18 @@
 feature 'Capybara tests...' do
+
   scenario 'Player can see their name when they enter it and submit' do
     visit('/')
     fill_in 'name', with: 'Wilson'
     click_button 'Go!'
     expect(page).to have_content 'Wilson'
+  end
+
+  scenario 'Player is able to play rock/paper/scissors' do
+    visit('/')
+    fill_in 'name', with: 'Wilson'
+    click_button 'Go!'
+    click_button 'Rock'
+    expect(page).to have_content 'beats'
+    expect(page).to have_content 'win'
   end
 end
