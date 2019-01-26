64da7a76688c0d80559996613caf2c1c35c1bbf8
William Dunk <williamdunk@gmail.com>
2019-01-26 19:23:20 +0000

    player can enter name

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ class RPSapp < Sinatra::Base
 
   get '/' do
     'Testing'
+    erb :index
+  end
+
+  post '/names' do
+    @p1_name = params[:player_1_name]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,6 +1,16 @@
 feature 'Testing:' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Testing'
+    expect(page).to have_content 'Rock Paper Scissors'
+  end
+end
+
+feature 'Name entry:' do
+  # the marketeer should be able to enter their name before the game
+  scenario 'user can enter name' do
+    visit('/')
+    fill_in :player_1_name, with: 'Dog'
+    click_button 'Submit'
+    expect(page).to have_content 'Dog vs. Computer'
   end
 end
