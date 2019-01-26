29b5f8556b243580c732104f0c69d526535c1209
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-26 15:40:12 +0000

    add tests and code to check making moves and saving moves

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,20 @@
 class Game
-  attr_reader :players
+  attr_reader :players, :player1, :player2, :player1_move
 
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
     @players = [@player1, @player2]
+    @player1_move = nil
+    @player2_move = nil
   end
+
+  def make_move(player, move)
+    @player1_move = player.make_move(move)
+  end
+
+  def computer_move      
+    @player2_move = @player2.make_move
+  end
+  
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe Game do
     expect(game.players).to eq([player1, player2])
   end
 
+  it 'can take a move from player1' do
+    player1 = double('player1')
+    allow(player1).to receive(:make_move).and_return('rock')
+    player2 = double('player2')
+    move = 'rock'
+    game = Game.new(player1, player2)
+    expect(game.make_move(player1, move)).to eq('rock')
+  end
+
+  it 'can save a move made by player1' do
+    player1 = double('player')
+    player2 = double('player')
+    game = Game.new(player1, player2)
+    allow(player1).to receive(:make_move).and_return('rock')
+    game.make_move(player1, 'rock')
+    expect(game.player1_move).to eq('rock')
+  end
 end
