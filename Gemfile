278714417df89f4daff5e1eabc50465c5b9b47e3
ellie turnock <ebturnock@gmail.com>
2019-01-26 15:29:26 +0000

    refactored

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@ source 'https://rubygems.org'
 
 ruby '2.5.0'
 
-gem 'rake'
-gem 'sinatra'
-gem 'shotgun'
 gem 'rack'
+gem 'rake'
+gem 'shotgun'
+gem 'sinatra'
 
 group :test do
   gem 'capybara'
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,8 @@
-require './app'
-
-feature 'Testing Infrastructure' do
-  scenario 'testing environment should show' do
-    visit('/')
-    expect(page).to have_content 'Testing!'
-  end
-end
+# require './app'
+#
+# feature 'Testing Infrastructure' do
+#   scenario 'testing environment should show' do
+#     visit('/')
+#     expect(page).to have_content 'Testing!'
+#   end
+# end
