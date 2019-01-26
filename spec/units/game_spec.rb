ba190246832988ac8ef091b774380582e91e565d
Brooke Woolley <brooke.woolley@gmail.com>
2019-01-26 12:30:12 +0000

    Created computer class which generates a random move

---
+++ spec/units/game_spec.rb
@@ -0,0 +1,12 @@
+# require 'game'
+#
+# RSpec.describe Game do
+#   subject(:game) { Game.new("player1", "player2")}
+#
+#   describe '#computer_move' do
+#     it 'generates random move for computer' do
+#       expect(game).to receive(:move).and return
+#     end
+#   end
+#
+# end
