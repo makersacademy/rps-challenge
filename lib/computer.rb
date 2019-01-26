12d0a7d0866f58a56e30ae7f3a7ab115db6a48e7
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-26 15:52:33 +0000

    add tests and passing code for Computer initializing w name & making random moves

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,7 +1,12 @@
 class Computer
-  attr_reader :name
-  
-  def initialize(name = "RPC Bot")
+  attr_reader :name, :move
+
+  def initialize(name = "RPS Bot")
     @name = name
+    @move = nil
+  end
+
+  def make_move
+    @move = ['rock', 'paper', 'scissors'].sample
   end
 end
