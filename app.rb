ac6ac4ff77f8f6a2a0476e9f0fac862c2868e537
Diptis Halder <diptis.halder@gmail.com>
2019-01-27 19:40:13 +0000

    v1.5 Set the image links on /names to the /played result page passing the player's choice

--- app.rb
+++ app.rb
@@ -18,6 +18,15 @@ class RockPaperScissors < Sinatra::Base
     erb(:names)   
   end
 
+  get '/names' do
+    @player_1 = params[:player_1]
+    @rps_hash = { 
+      "Rock" => "http://www.pngall.com/wp-content/uploads/2016/06/Rock-Free-PNG-Image-180x180.png",
+      "Paper" => "http://sr.photos2.fotosearch.com/bthumb/CSP/CSP992/k13421890.jpg",
+      "Scissors" => "http://www.pngall.com/wp-content/uploads/2016/03/Scissor-PNG-HD-180x180.png" }
+    erb(:names)   
+  end
+
   get '/played' do
     @player_1 = params[:player_1]
     @player_1_choice = params[:player_1_choice]
