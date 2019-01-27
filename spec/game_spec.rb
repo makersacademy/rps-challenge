794f475c6ada226181c16773f1fa5a94cf706e28
Aiden Gleave <aidengleave@gmail.com>
2019-01-27 22:29:32 +0000

    Added stub to check if weapon choice was random

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,5 +4,10 @@ describe Game do
   it 'returns a weapon from the list' do
     weapon = Game.choose_weapon
     expect(Game::WEAPONS).to include(weapon)
-  end 
+  end
+  
+  it 'should return a random weapon' do
+    expect(Game::WEAPONS).to receive(:sample).and_return(:scissors)
+    Game.choose_weapon
+  end
 end
