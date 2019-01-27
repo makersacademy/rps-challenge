d822d3746021d6af4bd4f987824db2c87bb311e0
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-27 15:30:26 +0000

    USER STORY 2 - the marketeer can choose one option Updated rspec scenarios to reflect user stories

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -2,16 +2,16 @@ require_relative '../../app.rb'
 require 'spec_helper.rb'
 
 feature 'Game Options' do
-  scenario "View Rock Paper Scissors Choices" do
+  scenario "The user can view Rock Paper Scissors Choices" do
     sign_in_and_play
     expect(page).to have_button("Rock")
     expect(page).to have_button("Paper")
     expect(page).to have_button("Scissors")
   end
 
-  scenario "Select option to play game" do 
+  scenario "The user can select option to play game" do 
     sign_in_and_play
     click_button 'Rock'
     expect(page).to have_content("You Selected Rock")
-  end 
+  end  
 end
