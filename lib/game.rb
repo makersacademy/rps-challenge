3a9a2a6e6ef3849cd1f0dc9684449383d34f1bbc
thomasmarano <thomas.marano2@gmail.com>
2019-01-27 13:55:12 +0000

    Commit after refactoring rules into hash

--- lib/game.rb
+++ lib/game.rb
@@ -1,34 +1,24 @@
 class Game
-    attr_accessor :player1, :computer, :winner1
+    attr_accessor :player1, :computer, :outcome
 
     def initialize(player1, computer)
         @player1 = player1
         @computer = computer
-        @winner1
+        @outcome
+        @rules = {
+            "Rock" => "Scissors",
+            "Scissors" => "Paper",
+            "Paper" => "Rock"
+        }
     end
-
 
     def determine_winner
         if @player1.move == @computer.move
-            @winner1 = 'Draw'
-        elsif @player1.move == 'Rock'
-            if @computer.move == 'Scissors'
-                @winner1 = 'Player 1'
-            else
-                @winner1 = 'Computer'
-            end
-        elsif @player1.move == 'Paper'
-            if @computer.move == 'Rock'
-                @winner1 = 'Player 1'
-            else
-                @winner1 = 'Computer'
-            end
+            @outcome = "draw"
+        elsif @rules[@player1.move] == @computer.move
+            @outcome = "Player 1"
         else
-            if @computer.move == 'Paper'
-                @winner1 = 'Player 1'
-            else @computer.move == 'Rock'
-                @winner1 = 'Computer'
-            end
+            @outcome = "Computer"
         end
     end
 end
