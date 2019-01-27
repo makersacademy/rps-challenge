115756417f618aad628e7e87eb0a0a84f48abca3
Sherif Shendidy <shendidy@Sherifs-MacBook-Pro.local>
2019-01-27 23:39:08 +0000

    Prepared the registration page

--- app.rb
+++ app.rb
@@ -2,14 +2,9 @@ require 'sinatra'
 
 class Rps < Sinatra::Base
 
-
   get '/' do
-    "Starting the app design"
-
     erb :index
   end
-
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
