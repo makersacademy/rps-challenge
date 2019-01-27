84267101d19a9bd3ac700a2afa47d4969217636c
Simon Green <simon.green@thielsen.co.uk>
2019-01-27 16:42:21 +0000

    refactor of game class

--- models/game.rb
+++ models/game.rb
@@ -1,16 +1,22 @@
 class Game
 
-WEAPONS = ["rock", "paper", "scissors"]
+  WEAPONS = ["rock", "paper", "scissors"]
 
   def self.computer_weapon
     WEAPONS.sample
   end
 
-  def self.result(player, computer)
-    return "Draw!" if player == computer
-    return "Computer wins!" if computer == "rock" && player == "scissors"
-    return "Computer wins!" if computer == "paper" && player == "rock"
-    return "Computer wins!" if computer == "scissors" && player == "paper"
-    "Player wins!"
+  def self.result(player1, player2)
+    return "Draw!" if player1 == player2
+    return "Player wins!" if player1 == winning_weapon(player2)
+    "Computer wins!"
+  end
+
+  private_class_method
+
+  def self.winning_weapon(weapon)
+    return "rock" if weapon == "scissors"
+    return "paper" if weapon == "rock"
+    return "scissors" if weapon == "paper"
   end
 end
