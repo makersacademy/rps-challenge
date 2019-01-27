c0381d76c81b47d3faca9eda1f8bec2c2884bddb
Simon Green <simon.green@thielsen.co.uk>
2019-01-27 14:13:45 +0000

    Test for computer win

--- models/game.rb
+++ models/game.rb
@@ -5,4 +5,11 @@ WEAPONS = ["rock", "paper", "scissors"]
   def self.computer_weapon
     WEAPONS.sample
   end
+
+  def self.result(player, computer)
+    return "Draw!" if player == computer
+    return "Computer wins!" if computer == "rock" && player == "scissors"
+    return "Computer wins!" if computer == "paper" && player == "rock"
+    return "Computer wins!" if computer == "scissors" && player == "paper"
+  end
 end
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Game do
   it 'provides a rock, paper or scissors as the computer weapon' do
     expect(Game.computer_weapon).to eq("rock") | eq("paper") | eq("scissors")
   end
+
+  it 'returns a draw when there is a match' do
+    expect(Game.result("rock", "rock")).to eq "Draw!"
+    expect(Game.result("scissors", "scissors")).to eq "Draw!"
+    expect(Game.result("paper", "paper")).to eq "Draw!"
+  end
+
+  it 'returns a compouter win when computer wins' do
+    expect(Game.result("scissors", "rock")).to eq "Computer wins!"
+    expect(Game.result("paper", "scissors")).to eq "Computer wins!"
+    expect(Game.result("rock", "paper")).to eq "Computer wins!"
+  end
 end
