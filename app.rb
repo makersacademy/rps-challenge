52aa3c3e7521bd12a464ff5332378a3355fe36a4
Sherif Shendidy <shendidy@Sherifs-MacBook-Pro.local>
2019-01-27 23:59:41 +0000

    Refactored controlers file and created Players class

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require 'sinatra'
+require_relative './lib/player.rb'
 
 class Rps < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  post '/names' do
+    $player_1 = Player.new(params[:player_1_name])
+    @player_1 = $player_1
+
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = $player_1
+
+    erb :play
   end
 
   # start the server if ruby file executed directly
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,7 +1,15 @@
 feature 'Enter names' do
-  scenario 'submitting names' do
+  scenario 'Registration page' do
     visit('/')
 
     expect(page).to have_content 'Play Rock Paper Scissors'
+  end
+
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Sherif'
+    click_button 'Start'
+
+    expect(page).to have_content 'Welcome Sherif, let\'s start'
   end
 end
