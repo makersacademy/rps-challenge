3d8b2985ed418c187426a4331bd335f08a453935
Simon Green <simon.green@thielsen.co.uk>
2019-01-27 15:20:01 +0000

    Addes session for name

--- rps.rb
+++ rps.rb
@@ -2,17 +2,23 @@ require 'sinatra/base'
 
 class Rps < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/choose' do
     @name = params[:name]
+    session[:name] = @name
     erb(:choose)
   end
 
   post '/result' do
-    p params
+    @weapon = params[:weapon]
+    @name = session[:name]
+    @computer_weapon = Game.computer_weapon
+    @result = Game.result(@weapon, @computer_weapon)
     erb(:result)
   end
 
