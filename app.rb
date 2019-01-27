defc6645f3f8a3808c18c064b2e8674df6b07d82
James <james.rudrum@hyperisland.se>
2019-01-27 21:43:19 +0000

    Test for being able to enter name. Created a web helper for future tests. Added new roots

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require 'sinatra/base'
 
 class Rps < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
+  end
+
+  post '/playername' do
+    session[:name] = params[:name]
+
+    redirect '/playgame'
+  end
+
+  get '/playgame' do
+    @name = session[:name]
+    erb(:play)
   end
 
   run if app_file == $0
---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,7 @@
+feature 'Can submit player name' do
+  scenario 'check to submit player name' do
+    visit('/')
+    fill_in :player_name, with: 'James'
+    click_button 'Lets Play!'
+    expect(page).to have_content 'Player: James'
+end
