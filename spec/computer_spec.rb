da92b437abba7de3f2b7c65e54a6d2e7173ad66a
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-27 21:01:46 +0000

    add let to DRY test code

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,20 +1,18 @@
 require 'computer'
 
 RSpec.describe Computer do
+  let(:computer) { Computer.new("HAL") }
   it 'initilizes with a name' do
-    computer = Computer.new("HAL")
     expect(computer.name).to eq("HAL")
   end
 
   describe '#make_move' do
     it 'returns a move' do
-      computer = Computer.new("HAL")
       allow(computer).to receive(:make_move).and_return('rock')
       expect(computer.make_move).to eq('rock')
     end
 
     it 'chooses a random move from the given options' do
-      computer = Computer.new("HAL")
       expect(['rock', 'paper', 'scissors']).to include(computer.make_move)
     end
   end
