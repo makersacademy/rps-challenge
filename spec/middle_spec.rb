9c7b868984a6b8404208623db9551a6e3a55c3ec
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-27 21:59:31 +0000

    add additional tests for middle.rb

--- spec/middle_spec.rb
+++ spec/middle_spec.rb
@@ -1,10 +1,35 @@
 require 'middle'
+require 'player'
+require 'computer'
 
 RSpec.describe Middle do
-  it 'can create a new game' do
-    player1 = double('player1')
-    player2 = double('player2')
+  let(:player1) { double('player1') }
+  let(:player2) { double('player2') }
+  let(:real_player) { Player.new('Wilson') }
+  let(:computer) { Computer.new('HAL') }
+
+  it 'can create a new game instance and store it' do
     Middle.create_game(player1, player2)
     expect(Middle.game_instance.players).to eq([player1, player2])
+  end
+
+  it 'can make a move for a player' do
+    Middle.create_game(real_player, player2)
+    Middle.make_move(real_player, 'rock')
+    expect(real_player.move).to eq('rock')
+  end
+
+  it 'can make a move for a computer' do
+    Middle.create_game(player1, computer)
+    Middle.computer_move
+    expect(['rock', 'paper', 'scissors']).to include(computer.make_move)
+  end
+
+  it 'can calculate the winner' do
+    Middle.create_game(real_player, computer)
+    Middle.make_move(real_player, 'rock')
+    Middle.game_instance.instance_variable_set(:@player2_move, 'scissors')
+    Middle.calculate_winner
+    expect(Middle.game_instance.winner).to eq(real_player)
   end
 end
