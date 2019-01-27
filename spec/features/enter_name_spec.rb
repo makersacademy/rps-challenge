b6711efe80fa5603538280b99b2a2ebb2fd48ce4
James <james.rudrum@hyperisland.se>
2019-01-27 21:44:37 +0000

    Refactored entering name test

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,7 +1,6 @@
 feature 'Can submit player name' do
   scenario 'check to submit player name' do
     visit('/')
-    fill_in :player_name, with: 'James'
-    click_button 'Lets Play!'
+    include sign_in_and_play
     expect(page).to have_content 'Player: James'
 end
