f329898818b49e57d31e8e9a02d51a94433f04d2
Ibrahim Butt <ibrahimbutt@me.com>
2019-01-27 23:07:43 +0000

    Present player with rps options

--- spec/features_spec.rb
+++ spec/features_spec.rb
@@ -6,3 +6,16 @@ feature 'Register' do
     expect(page).to have_content 'Ibrahim Vs. NPC'
   end
 end
+
+feature 'Actions' do
+  scenario 'User is presented with options' do
+    visit '/'
+    fill_in :username, with: 'Ibrahim'
+    click_button 'Register'
+    
+    expect(page).to have_selector(:link_or_button, 'Rock')
+    expect(page).to have_selector(:link_or_button, 'Paper')
+    expect(page).to have_selector(:link_or_button, 'Scissors')
+  end
+end
+
