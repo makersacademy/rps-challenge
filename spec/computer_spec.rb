d12b00341e4877ed7b607870f208823e0c9c89d7
Nikhil Vijayan <nkhilv@gmail.com>
2019-01-27 21:03:40 +0000

    remove test that tested state instead of behaviour

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,15 +2,10 @@ require 'computer'
 
 RSpec.describe Computer do
   let(:computer) { Computer.new("HAL") }
+
   it 'initilizes with a name' do
     expect(computer.name).to eq("HAL")
   end
-
-  describe '#make_move' do
-    it 'returns a move' do
-      allow(computer).to receive(:make_move).and_return('rock')
-      expect(computer.make_move).to eq('rock')
-    end
 
     it 'chooses a random move from the given options' do
       expect(['rock', 'paper', 'scissors']).to include(computer.make_move)
