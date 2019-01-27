587ca5a6731add2f4311d08f4ceeed2d64ddd0be
Diptis Halder <diptis.halder@gmail.com>
2019-01-27 18:43:35 +0000

    v1.4 Put image URLs in @rps_hash and created new route /played for game result

--- app.rb
+++ app.rb
@@ -11,7 +11,21 @@ class RockPaperScissors < Sinatra::Base
 
   post '/names' do
     @player_1 = params[:player_1]
+    @rps_hash = { 
+      "Rock" => "http://www.pngall.com/wp-content/uploads/2016/06/Rock-Free-PNG-Image-180x180.png",
+      "Paper" => "http://sr.photos2.fotosearch.com/bthumb/CSP/CSP992/k13421890.jpg",
+      "Scissors" => "http://www.pngall.com/wp-content/uploads/2016/03/Scissor-PNG-HD-180x180.png" }
     erb(:names)   
+  end
+
+  get '/played' do
+    @player_1 = params[:player_1]
+    @player_1_choice = params[:player_1_choice]
+    @rps_hash = { 
+      "Rock" => "http://www.pngall.com/wp-content/uploads/2016/06/Rock-Free-PNG-Image-180x180.png",
+      "Paper" => "http://sr.photos2.fotosearch.com/bthumb/CSP/CSP992/k13421890.jpg",
+      "Scissors" => "http://www.pngall.com/wp-content/uploads/2016/03/Scissor-PNG-HD-180x180.png" }
+    erb(:played)
   end
 
   # start the server if ruby file executed directly
