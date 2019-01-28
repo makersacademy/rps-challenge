d5f4a500bef26ea1b83e234dc1404d58e947cd6f
Aiden Gleave <aidengleave@gmail.com>
2019-01-28 08:52:38 +0000

    updated README

--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,14 +1,14 @@
 class Weapon
 
-  PLAYER_WIN_MESSAGE = "Congratulations! You beat the computer."
-  DRAW_MESSAGE = "It's a draw!"
-  COMPUTER_WIN_MESSAGE = "Computer Wins."
+  # PLAYER_WIN_MESSAGE = "Congratulations! You beat the computer."
+  # DRAW_MESSAGE = "It's a draw!"
+  # COMPUTER_WIN_MESSAGE = "Computer Wins."
   
-  def self.resolve(p_weapon, c_weapon)
-    player_weapon = convert_string_to_weapon(p_weapon)
-    computer_weapon = convert_string_to_weapon(c_weapon)
-    @result = player_weapon.compare(computer_weapon)
-  end
+  # def self.resolve(p_weapon, c_weapon)
+  #   player_weapon = convert_string_to_weapon(p_weapon)
+  #   computer_weapon = convert_string_to_weapon(c_weapon)
+  #   @result = player_weapon.compare(computer_weapon)
+  # end
 
   @wins = []
   @loses = []
