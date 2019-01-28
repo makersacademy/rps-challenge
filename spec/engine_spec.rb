2f9cb0c0399388e0a955a009e45c1592216c18e4
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-28 12:31:44 +0000

    Engine tests refactoring - testing randomness

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe Engine do
 
   context 'returns Rock, Paper, Scissors at random' do
     it 'returns Rock' do
-      allow(subject).to receive(:random).and_return('Rock')
+      allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
       expect(subject.random).to eq('Rock')
     end
     it 'returns Paper' do
-      allow(subject).to receive(:random).and_return('Paper')
+      allow_any_instance_of(Array).to receive(:sample).and_return('Paper')
       expect(subject.random).to eq('Paper')
     end
     it 'returns Scissors' do
-      allow(subject).to receive(:random).and_return('Scissors')
+      allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
       expect(subject.random).to eq('Scissors')
     end
   end
