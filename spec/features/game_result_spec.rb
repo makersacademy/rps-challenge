07f4b9cba6ec2caae3769fb928f05bbea02e2dc8
matt <mwhitaker1006@gmail.com>
2019-02-20 13:49:50 +0000

    added test to return computer move (test failing)

--- spec/features/game_result_spec.rb
+++ spec/features/game_result_spec.rb
@@ -7,6 +7,9 @@ feature 'plays game' do
   end
 
   scenario 'returns a move from the computer' do
-
+    sign_in
+    choose('move_choice', option: 'Rock')
+    click_button "Let's play!"
+    expect(page).to have_content 'The computer chose Paper!'
   end
 end
