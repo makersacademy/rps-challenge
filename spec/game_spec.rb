6c8f3787d390c9ca5792cd59bcd1726e62c19966
matt <mwhitaker1006@gmail.com>
2019-02-20 19:41:34 +0000

    added failing test for comparing computer and player moves

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,6 +1,13 @@
 describe Game do
   it 'selects a move' do
     subject.computer_choice
-    expect(["Rock","Paper","Scissors"]).to include(subject.computer_move)
+    expect(["Rock", "Paper", "Scissors"]).to include(subject.computer_move)
+  end
+
+  it 'computer wins a game' do
+    srand(67809)
+    subject.player_move("Scissors")
+    subject.computer_choice
+    expect(subject.game_result).to eq("Computer wins!")
   end
 end
