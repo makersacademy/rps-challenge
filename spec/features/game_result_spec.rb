ff23c3ebeba0fbd4326482e12d2ca370618bdf92
matt <mwhitaker1006@gmail.com>
2019-02-20 18:59:00 +0000

    refactored player choosing a move into helper method

--- spec/features/game_result_spec.rb
+++ spec/features/game_result_spec.rb
@@ -1,15 +1,13 @@
 feature 'plays game' do
   scenario 'player chooses a move' do
     sign_in
-    choose('move_choice', option: 'Rock')
-    click_button "Let's play!"
+    player_chooses_rock
     expect(page).to have_content('You chose Rock')
   end
 
   scenario 'returns a move from the computer' do
     sign_in
-    choose('move_choice', option: 'Rock')
-    click_button "Let's play!"
+    player_chooses_rock
     expect(page).to have_content 'The computer chose Paper!'
   end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -3,3 +3,8 @@ def sign_in
   fill_in :player_name, with: 'Mark'
   click_button 'Submit'
 end
+
+def player_chooses_rock
+  choose('move_choice', option: 'Rock')
+  click_button "Let's play!"
+end
