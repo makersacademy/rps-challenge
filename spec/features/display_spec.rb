3c658a9af3edd652d1fc26857f402b680543196b
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-22 23:05:32 +0000

    display rock paper scissor options

--- spec/features/display_spec.rb
+++ spec/features/display_spec.rb
@@ -7,4 +7,10 @@ feature "Display" do
     click_button 'Submit'
     expect(page).to have_content("Mark Eteer")
   end
+  scenario "shows rock, paper, scissors options" do
+    visit '/'
+    fill_in 'name', with: "Mark Eteer"
+    click_button 'Submit'
+    expect(page).to have_field("rps")
+  end
 end
