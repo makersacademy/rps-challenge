bd369bcbe06bfcadb664a4b57d69b7896c4b30d8
David Thai <davidthai@dafuloth.me>
2019-02-23 17:14:17 +0000

    updated spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
+require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+# add this to spec/spec_helper.rb
+
+ENV['RACK_ENV'] = 'test'
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# tell Capybara about our app class
+Capybara.app = RockPaperScissors
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
