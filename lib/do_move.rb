e1d0fb30dfb3ee52137813b9174ce4aa51886da3
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-23 21:06:55 +0000

    user selection working

---
+++ lib/do_move.rb
@@ -0,0 +1,6 @@
+class DoMove
+
+def pc_plays_rock(move)
+end
+
+end
--- rps_web.rb
+++ rps_web.rb
@@ -1,5 +1,5 @@
 require "sinatra/base"
-require_relative './lib/player.rb'
+require_relative "./lib/player.rb"
 
 class RPS_web < Sinatra::Base
 enable :sessions
@@ -8,25 +8,44 @@ get '/' do
   erb(:index)
 end
 
-post '/player' do
-  session[:name] = Player.new(params[:name]).name
+post '/player_name' do
+  session[:player_name] = params[:name]
+  # session[:player] = Player.new(params[:name])
+  # session[:name] = session[:player].name
   redirect('/play')
 end
 
 get '/play' do
-  @name = session[:name]
+  @name = session[:player_name]
   erb(:player)
 end
 
 post '/first' do
-  @name = session[:name]
-  redirect('/second')
+  erb(:first)
 end
 
-get '/second' do
-session[:move] = (Player.new(session[:name])).player_move(params[:move])
-erb(:first)
+post '/first_play' do
+  @player = Player.new(@name)
+  session[:choice] = params[:choice].to_sym
+  # @move = session[:move]
+  # session[:status] = @player.player_status
+  redirect('/first_result')
 end
+
+get '/first_result' do
+  @name = session[:player_name]
+  # @player = Player.new(@name)
+  @choice = session[:choice]
+  erb(:first_result)
+end
+
+post '/second' do
+@name = session[:player_name]
+@move = session[:move]
+# @status = session[:status]
+erb(:second)
+end
+
 
 run! if app_file == $0
 end
