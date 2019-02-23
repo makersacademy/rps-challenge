50e65e66bca07d248ee3d9134f5ff4798479dec7
Lizzie Coady <lizziecoady@hotmail.co.uk>
2019-02-23 15:27:03 +0000

    Added moves and basic feature tests

--- app.rb
+++ app.rb
@@ -11,7 +11,25 @@ class RockPaperScissors < Sinatra::Base
     erb(:index)
   end
 
-  
+  post '/names' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb(:play)
+  end
+
+  post '/move' do
+
+    redirect '/win'
+    # if win, redirect to '/win'
+  end
+
+  get '/win' do
+    'You won'
+  end
 
   run! if app_file == $0
 
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,40 @@
 feature 'test home page is working' do
-
   scenario 'home page shows text' do
     visit('/')
-    expect(page).to have_content 'Hello, I am working'
+    expect(page).to have_content 'Rock, Paper, Scissors'
+  end
+end
+
+feature 'Enter name' do
+  scenario 'submitting name' do
+    visit('/')
+    fill_in('player_name', with: 'Lizzie')
+    click_button 'Submit'
+    expect(page).to have_content 'Hi, Lizzie'
+  end
+end
+
+feature 'Choose a move' do
+
+  before(:each) do
+    visit('/')
+    fill_in('player_name', with: 'Lizzie')
+    click_button 'Submit'
+  end
+
+  scenario 'choose rock' do
+    click_button 'Rock'
+    expect(page).to have_content 'You won'
+  end
+
+  scenario 'choose paper' do
+    click_button 'Paper'
+    expect(page).to have_content 'You won'
+  end
+
+  scenario 'choose scissors' do
+    click_button 'Scissors'
+    expect(page).to have_content 'You won'
   end
 
 end
