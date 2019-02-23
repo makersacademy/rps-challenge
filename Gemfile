27904a1ebb58196886cd740299212f16cee9c494
“Brenda” <odonnell.brenda@gmail.com>
2019-02-23 10:12:11 +0000

    Post/redirect/get user name.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ source 'https://rubygems.org'
 ruby '2.5.0'
 
 gem 'rake'
-gem 'sinatra'
+gem 'sinatra', '~> 2.0','>=2.0.5'
+gem 'selenium-webdriver', '~> 2.53', '>= 2.53.4'
 
 group :test do
   gem 'capybara'
---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra/base'
+
+class Rps < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    session[:player1] = params[:player1]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
+    erb :play
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
