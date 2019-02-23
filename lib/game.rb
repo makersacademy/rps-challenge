4c94e7b5695b7cbc5a825e20c16214589a474efc
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-23 11:18:36 +0000

    pass test for deciding win, lose or draw

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,15 @@
 class Game
+  
+  def result(player1_move, player2_move)
+    
+    if (player1_move == 'ROCK' && player2_move == 'PAPER') || (player1_move == 'PAPER' && player2_move == 'SCISSORS') || (player1_move == 'SCISSORS' && player2_move == 'PAPER') 
+      2
+    elsif (player1_move == 'ROCK' && player2_move == 'SCISSORS') || (player1_move == 'PAPER' && player2_move == 'ROCK') || (player1_move == 'SCISSORS' && player2_move == 'PAPER')
+      1
+    elsif (player1_move == 'ROCK' && player2_move == 'ROCK') || (player1_move == 'PAPER' && player2_move == 'PAPER') || (player1_move == 'SCISSORS' && player2_move == 'SCISSORS')
+      0
+    end
+
+  end
+
 end
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -6,12 +6,27 @@ describe Game do
   let(:player1) { double :player }
   let(:player2) { double :player }
 
-  it 'can decide the winner' do
+  it 'can tell if player 2 wins' do
 
     allow(player1).to receive(:move) { 'ROCK' }
     allow(player2).to receive(:move) { 'PAPER' }
-    allow(player2).to receive(:name) { 'Ozzy' }
-    expect(game.result(player1.move, player2.move)).to eq "#{player2.name} wins!"
+    expect(game.result(player1.move, player2.move)).to eq 2
+
+  end
+
+  it 'can tell if player 1 wins' do
+
+    allow(player1).to receive(:move) { 'ROCK' }
+    allow(player2).to receive(:move) { 'SCISSORS' }
+    expect(game.result(player1.move, player2.move)).to eq 1
+
+  end
+
+  it 'can tell if there is a draw' do
+
+    allow(player1).to receive(:move) { 'ROCK' }
+    allow(player2).to receive(:move) { 'ROCK' }
+    expect(game.result(player1.move, player2.move)).to eq 0
 
   end
 
