2e19a95fb822a48e21082bbfd545ca5315152601
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-23 08:45:07 +0000

    pass test for checking if index page is serving content

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
 
+  get '/' do
+    'Rock! Paper! Scissors!'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
