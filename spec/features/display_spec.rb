64f225be52370e2c90337722b074503e4649552e
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-23 17:48:13 +0000

    refactor tests to pull steps to enter name out into a register web helper

--- spec/features/display_spec.rb
+++ spec/features/display_spec.rb
@@ -2,22 +2,16 @@ require './app'
 
 feature "Display" do
   scenario "shows player's name after they register" do
-    visit '/'
-    fill_in 'name', with: "Mark Eteer"
-    click_button 'Submit'
+    register
     expect(page).to have_content("Mark Eteer")
   end
   scenario "shows rock, paper, scissors options" do
-    visit '/'
-    fill_in 'name', with: "Mark Eteer"
-    click_button 'Submit'
+    register
     expect(page).to have_field("rps")
   end
   scenario "shows the game's choice of rps" do
     srand(3)
-    visit '/'
-    fill_in 'name', with: "Mark Eteer"
-    click_button 'Submit'
+    register
     choose 'paper'
     click_button 'Play'
     expect(page).to have_content("The game chose scissors")
--- spec/features/game_play_spec.rb
+++ spec/features/game_play_spec.rb
@@ -3,27 +3,21 @@ require './app'
 feature 'game play' do
   scenario 'player chooses default rock and game chooses scissors => player wins' do
     srand(3)
-    visit '/'
-    fill_in 'name', with: "Mark Eteer"
-    click_button 'Submit'
+    register
     choose 'rock'
     click_button 'Play'
     expect(page).to have_content("WIN")
   end
   scenario 'player chooses paper and game chooses scissors => player loses' do
     srand(3)
-    visit '/'
-    fill_in 'name', with: "Mark Eteer"
-    click_button 'Submit'
+    register
     choose 'paper'
     click_button 'Play'
     expect(page).to have_content("LOSE")
   end
   scenario 'player chooses scissors and game chooses scissors => player draws' do
     srand(3)
-    visit '/'
-    fill_in 'name', with: "Mark Eteer"
-    click_button 'Submit'
+    register
     choose 'scissors'
     click_button 'Play'
     expect(page).to have_content("DRAW")
