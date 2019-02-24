a55eb0b342a459416081367ad484d7b134aa55f9
David Thai <davidthai@dafuloth.me>
2019-02-24 05:42:04 +0000

    added /play route

--- app.rb
+++ app.rb
@@ -7,6 +7,11 @@ class RockPaperScissors < Sinatra::Base
     erb :index
   end
 
+  post '/play' do
+    @player_name = params[:player_name]
+    erb :play
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
