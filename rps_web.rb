81aa2247a29078aad6d62709f25d5083e757fc12
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-24 14:55:01 +0000

    CHanged player weapon choices to radio buttons on play view and added additional check to give error message when game is played without both weapons

--- rps_web.rb
+++ rps_web.rb
@@ -27,7 +27,7 @@ class RPSWeb < Sinatra::Base
     @player1_selection = ""
     @player2_selection = ""
     @winning_message = ""
-
+    @input_missing_message = ""
     erb(:play)
 
   end
@@ -36,13 +36,18 @@ class RPSWeb < Sinatra::Base
 
     @game = session[:game]
 
+    if !params.keys.include?("player_1_weapon") or !params.keys.include?("player_2_weapon")
+      @input_missing_message = "Both players must select a weapon each to play. Please try again"
+    else
 
-    @game.player1.choose_weapon(params[:player_1_weapon].to_sym)
-    @game.player2.choose_weapon(params[:player_2_weapon].to_sym)
-    @player1_selection = "#{@game.player1.name} selected #{@game.player1.weapon}"
-    @player2_selection = "#{@game.player2.name} selected #{@game.player2.weapon}"
-    @game.play
-    @winning_message = @game.winner == nil ? "It's a draw" : "#{@game.winner.name} wins!"
+      @game.player1.choose_weapon(params[:player_1_weapon].to_sym)
+      @game.player2.choose_weapon(params[:player_2_weapon].to_sym)
+      @player1_selection = "#{@game.player1.name} selected #{@game.player1.weapon}"
+      @player2_selection = "#{@game.player2.name} selected #{@game.player2.weapon}"
+      @game.play
+      @winning_message = @game.winner == nil ? "It's a draw" : "#{@game.winner.name} wins!"
+    end
+    
     erb(:play)
 
   end
--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,11 +1,20 @@
 feature 'Play game' do
-  scenario 'allow player1 to select weapon and play game ' do
+  scenario 'allow playerers to select weapon and play game ' do
     signup_and_play
-    select "scissors", from: "player_1_weapon"
-    select "paper", from: "player_2_weapon"
+    choose ("scissors1")
+    choose ("paper2")
     click_button('Play')
     expect(page).to have_content("Ant selected scissors")
     expect(page).to have_content("Elephant selected paper")
     expect(page).to have_content("Ant wins!")
+  end
+
+  scenario "Tell players that there should be weapons selected by both players to play the game if either player does not select a weapon before playing" do
+    signup_and_play
+    choose ("scissors1")
+    click_button('Play')
+    expect(page).not_to have_content("Ant selected scissors")
+    expect(page).not_to have_content("Elephant selected paper")
+    expect(page).not_to have_content("Ant wins!")
   end
 end
