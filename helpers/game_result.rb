30ce1d6197de04d2d3032d9164c84598d5e6e989
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-24 22:57:09 +0000

    added helpers to make the controller slim. This does not use Player class methods, the players and the play mode is still held in session variables

---
+++ helpers/game_result.rb
@@ -0,0 +1,48 @@
+module GameResult
+  def play_game
+    @game = Game.instance
+    if @play_mode != "solo"
+      if !params.keys.include?("player_1_weapon") or !params.keys.include?("player_2_weapon")
+        @input_missing_message = "Both players must select a weapon each to play. Please try again"
+      else
+
+        @player1.choose_weapon(params[:player_1_weapon].to_sym)
+        @player2.choose_weapon(params[:player_2_weapon].to_sym)
+        @winning_weapon = @game.play(@player1.weapon, @player2.weapon)
+        case @winning_weapon
+          when nil
+            @winning_message = "It's a draw"
+          when @player1.weapon
+            @winning_message = "#{@player1.name} wins!"
+          else
+            @winning_message = "#{@player2.name} wins!"
+        end
+      end
+    else
+      if !params.keys.include?("player_1_weapon")
+        @input_missing_message = "You must select a weapon to play. Please try again"
+      else
+        @player1.choose_weapon(params[:player_1_weapon].to_sym)
+        @winning_weapon = @game.play(@player1.weapon, @player2.weapon)
+
+        case @winning_weapon
+          when nil
+            @winning_message = "It's a draw"
+          when @player1.weapon
+            @winning_message = "#{@player1.name} wins!"
+          else
+            @winning_message = "#{@player2.name} wins!"
+        end
+      end
+    end
+  end
+
+  def display_game_result_and_play_again
+    if @play_mode == "solo"
+      erb(:play_solo)
+    else
+      erb(:play_multi)
+    end
+  end
+
+end
--- rps_web.rb
+++ rps_web.rb
@@ -2,8 +2,10 @@ require 'sinatra/base'
 require './lib/player'
 require './lib/game'
 require './lib/computer'
+require './helpers/game_result'
 
 class RPSWeb < Sinatra::Base
+  helpers GameResult
   enable :sessions
 
   get "/" do
@@ -29,17 +31,15 @@ class RPSWeb < Sinatra::Base
     @play_mode = session[:play_mode]
 
     @game = Game.create
+
     @player1 = Player.new(params[:player1])
 
     if @play_mode == "solo"
-
       @player2 = Computer.new
-
     else
-
       @player2 = Player.new(params[:player2])
-
     end
+
     session[:play_mode] = @play_mode
     session[:player1] = @player1
     session[:player2] = @player2
@@ -53,19 +53,15 @@ class RPSWeb < Sinatra::Base
     @play_mode = session[:play_mode]
     @player1 = session[:player1]
     @player2 = session[:player2]
-    @player1_selection = ""
-    @player2_selection = ""
+
     @winning_message = ""
     @input_missing_message = ""
+
     session[:play_mode] = @play_mode
     session[:player1] = @player1
     session[:player2] = @player2
 
-    if @play_mode == "solo"
-      erb(:play_solo)
-    else
-      erb(:play_multi)
-    end
+    display_game_result_and_play_again
 
   end
 
@@ -73,50 +69,14 @@ class RPSWeb < Sinatra::Base
     @play_mode = session[:play_mode]
     @player1 = session[:player1]
     @player2 = session[:player2]
-    @game = Game.instance
-    if @play_mode != "solo"
-      if !params.keys.include?("player_1_weapon") or !params.keys.include?("player_2_weapon")
-        @input_missing_message = "Both players must select a weapon each to play. Please try again"
-      else
 
-        @player1.choose_weapon(params[:player_1_weapon].to_sym)
-        @player2.choose_weapon(params[:player_2_weapon].to_sym)
-        @winning_weapon = @game.play(@player1.weapon, @player2.weapon)
-        case @winning_weapon
-          when nil
-            @winning_message = "It's a draw"
-          when @player1.weapon
-            @winning_message = "#{@player1.name} wins!"
-          else
-            @winning_message = "#{@player2.name} wins!"
-        end
-      end
-    else
-      if !params.keys.include?("player_1_weapon")
-        @input_missing_message = "You must select a weapon to play. Please try again"
-      else
-        @player1.choose_weapon(params[:player_1_weapon].to_sym)
-        @winning_weapon = @game.play(@player1.weapon, @player2.weapon)
+    play_game
 
-        case @winning_weapon
-          when nil
-            @winning_message = "It's a draw"
-          when @player1.weapon
-            @winning_message = "#{@player1.name} wins!"
-          else
-            @winning_message = "#{@player2.name} wins!"
-        end
-      end
-    end
     session[:play_mode] = @play_mode
     session[:player1] = @player1
     session[:player2] = @player2
 
-    if @play_mode == "solo"
-      erb(:play_solo)
-    else
-      erb(:play_multi)
-    end
+    display_game_result_and_play_again
 
   end
 
