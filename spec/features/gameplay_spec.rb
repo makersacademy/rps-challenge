59c9060cdb95ded575a169f81e693d0f24280440
David Thai <davidthai@dafuloth.me>
2019-02-24 20:10:31 +0000

    rock/paper/scissors links now exist. Also, CSS refined

--- spec/features/gameplay_spec.rb
+++ spec/features/gameplay_spec.rb
@@ -1,8 +1,20 @@
 feature 'Playing the game' do
+  scenario 'Player plays "Rock"' do
+    sign_in_and_play
+    click_on 'rock'
+    expect(page).to have_content 'You chose "Rock"'
+  end  
+
   scenario 'Player plays "Paper"' do
     sign_in_and_play
-    click_button 'paper'
+    click_on 'paper'
     expect(page).to have_content 'You chose "Paper"'
+  end
+
+  scenario 'Player plays "Scissors"' do
+    sign_in_and_play
+    click_on 'scissors'
+    expect(page).to have_content 'You chose "Scissors"'
   end  
 
   scenario 'Player plays "Paper" and wins' do
