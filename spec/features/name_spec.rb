dfcb01dde9d27b96ee4abbb9bfae0d08c99dfafb
Amina Jusic <amina_jusic@yahoo.com>
2019-02-24 22:51:01 +0000

    Edit name_spec, attack options presented

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -1,13 +1,13 @@
 feature 'Homepage' do
   scenario 'registers name' do
     sign_in_and_play
-    expect(page).to have_content 'Welcome Socks!'
+    expect(page).to have_content 'Welcome Ana!'
   end
 
-  scenario 'play game' do
+  scenario 'selects attack' do
     sign_in_and_play
     expect(page).to have_content 'Choose your attack:'
-    expect(page).to have_content 'Computer vs. Socks'
+    expect(page).to have_content 'Computer vs. Ana'
     select 'Rock', from: 'user_choice'
     click_button 'Go'
   end
