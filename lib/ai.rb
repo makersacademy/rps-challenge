b9d4cb8a7dec7ad01de80b8e8f782cd8f419ece9
Luke Phyall <lukeprospect@yahoo.com>
2019-02-24 10:30:11 +0000

    Adds AI opponent with half-decent test.

--- lib/ai.rb
+++ lib/ai.rb
@@ -1,7 +1,7 @@
 class AI
 
   def play
-    choice = rand(1...3)
+    choice = rand(1..3)
     choice == 1 ? "Rock" : choice == 2 ? "Paper" : "Scissors"
   end
 
--- spec/ai_spec.rb
+++ spec/ai_spec.rb
@@ -5,7 +5,9 @@ describe AI do
   let (:hal) { AI.new }
 
   it "returns a random RPS choice" do
-    expect(hal.play).to eq("Rock").or eq("Paper").or eq("Scissors")
+    results = []
+    10000.times { results << hal.play }
+    expect(results).to include("Rock", "Paper", "Scissors")
   end
 
 end
