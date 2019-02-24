e47e089fab348f383d2ae6507498fa72c80d3efd
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-02-24 18:55:41 +0000

    one and two players working

--- rps_web.rb
+++ rps_web.rb
@@ -6,11 +6,6 @@ require_relative "./lib/computer.rb"
 class RPS_web < Sinatra::Base
   enable :sessions
 
-
-  # before do
-  #   @instance_game = Game.instance
-  # end
-
   get '/' do
     erb(:index)
   end
@@ -26,28 +21,52 @@ class RPS_web < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
-    @@instance_game = Game.new(Player.new(session[:player_1_name]), Player.new(session[:player_2_name]))
-    print @@instance_game.players_names
+    if session[:player_count] == "1 player"
+      session[:player_1_name] = params[:player_1_name]
+      @@player2 = Computer.new
+      @@instance_game = Game.new(Player.new(session[:player_1_name]), @@player2)
+      print @@instance_game.players_names
+    else
+      session[:player_1_name] = params[:player_1_name]
+      session[:player_2_name] = params[:player_2_name]
+      @@instance_game = Game.new(Player.new(session[:player_1_name]), Player.new(session[:player_2_name]))
+      print @@instance_game.players_names
+    end
     redirect to '/play'
   end
 
   get '/play' do
-    @name1 = session[:player_1_name]
-    @name2 = session[:player_2_name]
-    if @@instance_game.p1_choice.nil?
-      @player = session[:player_1_name]
-    elsif @@instance_game.p2_choice.nil?
-      @player = session[:player_2_name]
+    if session[:player_count] == "1 player"
+      @name1 = session[:player_1_name]
+      @name2 = @@player2.name
+      if @@instance_game.p1_choice.nil?
+        @player = session[:player_1_name]
+      elsif @@instance_game.p2_choice.nil?
+        @player = @@player2.nme
+      end
+    else
+      @name1 = session[:player_1_name]
+      @name2 = session[:player_2_name]
+      if @@instance_game.p1_choice.nil?
+        @player = session[:player_1_name]
+      elsif @@instance_game.p2_choice.nil?
+        @player = session[:player_2_name]
+      end
     end
+
     erb(:play)
   end
 
   post '/choice' do
+
     if @@instance_game.p1_choice.nil?
       @@instance_game.p1_choice = params[:choice]
-      redirect to @@instance_game.next_play
+      if session[:player_count] == "2 players"
+        redirect to @@instance_game.next_play
+      else
+         @@instance_game.p2_choice = @@player2.computer_move_do
+         redirect to '/endgame'
+      end
     elsif @@instance_game.p2_choice.nil?
       @@instance_game.p2_choice = params[:choice]
       redirect to '/endgame'
