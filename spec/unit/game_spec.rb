6938651a00a1417b3583f24811e05d3422b940e8
Steve Kahn <steve@kahn.org.uk>
2019-02-24 09:04:59 +0000

    Added test for game for storing player name

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -1,8 +1,14 @@
 require 'game'
 
   describe Game do
+    let(:player_1){double :player_1}
     it "Stores an instance of itself" do
-      Game.start
+      Game.start(player_1)
       expect(Game.current_game.class).to eq Game
+    end
+
+    it "Holds the player name" do
+      Game.start(player_1)
+      expect(Game.current_game.name).to eq player_1
     end
   end
