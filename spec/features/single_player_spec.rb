96ceb41eb71f9545deabe814dfe361db8d2b16d5
David Thai <davidthai@dafuloth.me>
2019-02-24 03:45:57 +0000

    created form, added style

--- spec/features/single_player_spec.rb
+++ spec/features/single_player_spec.rb
@@ -2,7 +2,7 @@ feature 'Registering names' do
   scenario 'Player name displayed before playing an online game' do
     visit('/')
     fill_in :player_name, with: 'Winston'
-    click_button 'Start'
+    click_button 'Play'
     expect(page).to have_content 'Winston'
   end
 end
