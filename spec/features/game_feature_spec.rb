8055e43bf3e2b01c128cd8ab72fc731c48abfbf5
Simon <01secn@gmail.com>
2019-02-25 10:59:54 +0000

    Refactored game feature tests to be more parameterised

--- spec/features/game_feature_spec.rb
+++ spec/features/game_feature_spec.rb
@@ -7,47 +7,54 @@ feature 'Play game' do
       player_name = 'Simon'
       register_player(player_name: player_name)
     end
-    
-    scenario 'Should display rock option' do
-      expect(page.find_by_id('move_options')).to have_content("Rock") 
+
+    def self.test_move_option_displayed(expected_option:)
+      scenario "Should display #{expected_option} option" do
+        expect(page.find_by_id('move_options')).to have_content(expected_option) 
+      end
     end
 
-    scenario 'Should display paper option' do
-      expect(page.find_by_id('move_options')).to have_content("Paper") 
-    end
+    test_move_option_displayed(expected_option: 'Rock')
 
-    scenario 'Should display scissors option' do
-      expect(page.find_by_id('move_options')).to have_content("Scissors") 
-    end
+    test_move_option_displayed(expected_option: 'Paper')
+
+    test_move_option_displayed(expected_option: 'Scissors')
 
     describe 'When player selects an option and selects to play, ' do
       before(:each) do
         choose('rock')
       end
 
-      scenario 'Should, if the player loses, inform the player of the result' do
-        allow(Computer).to receive(:rand).and_return(1)
+      def self.test_result_message(expected_result:, expected_message:, computer_move:)
 
-        click_button("play")
-
-        expect(page).to have_content("SUCK IT, LOSER!") 
+        scenario "Should, if result is #{expected_result}, inform the player of the result" do
+          no_for_computer_win_move = Computer::MOVE_OPTIONS.find_index(computer_move)
+          allow(Computer).to receive(:rand).and_return(no_for_computer_win_move)
+  
+          click_button("play")
+  
+          expect(page).to have_content(expected_message) 
+        end
+        
       end
 
-      scenario 'Should, if the player wins, inform the player of the result' do
-        allow(Computer).to receive(:rand).and_return(2) 
+      test_result_message(
+        expected_result: 'player loss',
+        expected_message: 'SUCK IT, LOSER!', 
+        computer_move: 'paper'
+      )
 
-        click_button("play")
+      test_result_message(
+        expected_result: 'player win',
+        expected_message: 'Truly, you are the champion.', 
+        computer_move: 'scissors'
+      )
 
-        expect(page).to have_content("Truly, you are the champion.") 
-      end
-
-      scenario 'Should, if the player draws, ask the player to play again' do
-        allow(Computer).to receive(:rand).and_return(0) 
-
-        click_button("play")
-
-        expect(page).to have_content("A draw? I THINK NOT! WE WILL BATTLE TILL TIME DOTH END!") 
-      end
+      test_result_message(
+        expected_result: 'draw',
+        expected_message: 'A draw? I THINK NOT! WE WILL BATTLE TILL TIME DOTH END!', 
+        computer_move: 'rock'
+      )
     end
   end
 end
