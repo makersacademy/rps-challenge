2e09f108e8270462864713437953864e658df9a5
David Thai <davidthai@dafuloth.me>
2019-02-25 11:39:14 +0000

    tidy code, remove debug stuff, satisfied rubocop

--- lib/game.rb
+++ lib/game.rb
@@ -7,14 +7,7 @@ class Game
     scissors: :paper
   }
 
-  def initialize
-    
-  end
-
   def self.result(player1_choice, player2_choice)
-    p player1_choice.to_sym
-    p player2_choice.to_sym
-    
     return :draw if player1_choice.to_sym == player2_choice.to_sym
     WINS[player1_choice.to_sym] == player2_choice.to_sym ? :win : :lose
   end
