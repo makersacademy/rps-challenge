8933ec5d010baf6c05b9cb201a57a087ce96a672
David Thai <davidthai@dafuloth.me>
2019-02-25 00:33:37 +0000

    TDD/code done for class method Game.result

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,18 @@
 class Game
   CHOICES = [:rock, :paper, :scissors]
 
+  WINS = {
+    rock: :scissors,
+    paper: :rock,
+    scissors: :paper
+  }
+
   def initialize
     
   end
 
   def self.result(player1_choice, player2_choice)
-    
+    return :draw if player1_choice == player2_choice
+    WINS[player1_choice.to_sym] == player2_choice.to_sym ? :win : :lose
   end
 end
