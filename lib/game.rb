8acf8aeb06fcd34ea8317a114135c5f29e8a6bd0
David Thai <davidthai@dafuloth.me>
2019-02-25 00:55:25 +0000

    fixed "draw" result

--- lib/game.rb
+++ lib/game.rb
@@ -12,7 +12,10 @@ class Game
   end
 
   def self.result(player1_choice, player2_choice)
-    return :draw if player1_choice == player2_choice
+    p player1_choice.to_sym
+    p player2_choice.to_sym
+    
+    return :draw if player1_choice.to_sym == player2_choice.to_sym
     WINS[player1_choice.to_sym] == player2_choice.to_sym ? :win : :lose
   end
 end
