4c59a23be50b81bd09cde641936feeca7695fc19
Jimjule <roadknightj@gmail.com>
2019-02-28 12:28:28 +0000

    Refactored name feature test to use an existing method, eliminating unnecessary lines

--- spec/features/name_feature_spec.rb
+++ spec/features/name_feature_spec.rb
@@ -1,8 +1,6 @@
 feature "register name" do
   scenario "A player can register their name before playing" do
-    visit('/')
-    fill_in :name, with: "Spidey"
-    click_button("That's my name!")
+    sign_in
     expect(page).to have_content "Te llamas Spidey"
   end
 end
