4d3022271526203b7d1c9fc984b0c14a012cdc5b
masha-b <mashabudryte@gmail.com>
2019-03-22 13:06:49 +0000

    test for rps buttons refactored

--- spec/features/choosing_rps_spec.rb
+++ spec/features/choosing_rps_spec.rb
@@ -1,16 +1,8 @@
 feature 'player can choose rock paper scissors' do
-  scenario 'page has a button for rock' do
+  scenario 'page has a button for rock, paper or scissors' do
     sign_in_and_play
     expect(page).to have_selector(:link_or_button, 'Rock')
-  end
-
-  scenario 'page has a button for paper' do
-    sign_in_and_play
     expect(page).to have_selector(:link_or_button, 'Paper')
-  end
-
-  scenario 'page has a button for scissors' do
-    sign_in_and_play
     expect(page).to have_selector(:link_or_button, 'Scissors')
   end
 
