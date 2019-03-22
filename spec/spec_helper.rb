d4864c6409b18cdd76e8e03f6f19021b33cc41e4
James Palmer <jamesdcpalmer1@gmail.com>
2019-03-22 11:20:52 +0000

    Refactored through web helpers file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'features/web_helpers.rb'
 
 # tell Capybara about our app class
 Capybara.app = RPS
