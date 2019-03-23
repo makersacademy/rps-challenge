4d5a11810d229c869843fbd8c8cdca5ca2f779f4
Paul Martin <communty.hivemind@gmail.com>
2019-03-23 18:40:42 +0000

    Player objects can store a name

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,4 +1,11 @@
 require 'player'
 
 describe Player do
+  subject(:player) { described_class.new("Shelly") }
+
+  describe '#name' do
+    it "returns the player's name" do
+      expect(player.name).to eq "Shelly"
+    end
+  end
 end
