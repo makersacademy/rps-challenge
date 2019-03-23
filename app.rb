96a7891960654a81d2666c2f84f01a3714d0a4de
Student <student@makerss-mbp.home>
2019-03-23 16:57:48 +0000

    Added basic structure

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+
+class RPS < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb(:index)
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
