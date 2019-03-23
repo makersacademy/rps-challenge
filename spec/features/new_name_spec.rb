3aab7daa1523b3e6e338c6447a3d00ac7c10e119
Student <student@makerss-mbp.home>
2019-03-23 17:21:19 +0000

    Uploading login background image

---
+++ spec/features/new_name_spec.rb
@@ -0,0 +1,8 @@
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+
+feature "Player 1" do
+  scenario "Register name" do
+  end
+end
