2802acb945bce7052c59e148bfb611a452519ba2
Paul Martin <communty.hivemind@gmail.com>
2019-03-23 18:44:21 +0000

    Player object can store a player's choice

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,4 +8,11 @@ describe Player do
       expect(player.name).to eq "Shelly"
     end
   end
+
+  describe '#choose' do
+    it "stores the player's choice" do
+      player.choose(2)
+      expect(player.choice).to eq 2
+    end
+  end
 end
