06eb5dd6bcb6eef8301e98a65b21fcb67f7092a9
Riya Dattani <riya_dattani@hotmail.com>
2019-03-23 19:39:23 +0000

    Player's name is shown in lights

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
-  # get '/' do
-  #   erb :index
-  # end
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  post '/info' do
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
+    erb :play
+  end
+
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,1 +1,14 @@
 
+feature 'Name' do
+  scenario 'ask for name' do
+    visit '/'
+    expect(page).to have_content "Enter your name:"
+  end
+
+  scenario 'show name in lights' do
+    visit '/'
+    fill_in :name, with: 'Riya'
+    click_button 'Go'
+    expect(page).to have_content "Are you ready Riya?"
+  end
+end
