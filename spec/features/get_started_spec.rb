d86e0256a43c978ec3071237938932b1951babc1
saypop <tome.m.dj@gmail.com>
2019-03-23 11:55:35 +0000

    Functionlity hint 2: - the marketeer will be presented the choices (rock, paper and scissors)

--- spec/features/get_started_spec.rb
+++ spec/features/get_started_spec.rb
@@ -4,9 +4,30 @@
 
 feature 'See name' do
   scenario 'after you enter it' do
-    visit('/')
-    fill_in 'player_1_name', with: 'Name'
-    click_button 'Go!'
-    expect(page).to have_content('Name')
+    enter_name_and_click_go
+    expect(page).to have_content('NAME')
+  end
+end
+
+# the marketeer will be presented the choices (rock, paper and scissors)
+
+feature 'Be able to select rock' do
+  scenario 'on the play page' do
+    enter_name_and_click_go
+    expect(page).to have_button('ROCK!')
+  end
+end
+
+feature 'Be able to select paper' do
+  scenario 'on the play page' do
+    enter_name_and_click_go
+    expect(page).to have_button('PAPER!')
+  end
+end
+
+feature 'Be able to select scissors' do
+  scenario 'on the play page' do
+    enter_name_and_click_go
+    expect(page).to have_button('SCISSORS!')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,7 @@ require 'capybara/rspec'
 require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'features/web_helper'
 
 Capybara.app = RPS
 
