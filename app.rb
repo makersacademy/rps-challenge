298b233410765c8b183854035928c91a2cb38dba
Paul Martin <communty.hivemind@gmail.com>
2019-03-23 14:20:05 +0000

    Refactor registration form to use post method

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :register
   end
 
+  post '/enter-name' do
+    session[:player1_name] = params['player1_name']
+    redirect '/play'
+  end
+
   get '/play' do
-    @player1_name = params['player1_name']
+    @player1_name = session[:player1_name]
     erb :play
   end
 end
