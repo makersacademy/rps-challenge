5552591d3d3371cd8aed1510374688fe62b896cf
Paul Martin <communty.hivemind@gmail.com>
2019-03-23 18:17:56 +0000

    Game.create instantiates a singleton

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,12 @@
 class Game
+  def self.create(name)
+    @game = self.new(name)
+  end
+
+  def self.instance
+    @game
+  end
+
   attr_reader :player1
 
   def initialize(name)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,17 @@ require 'game'
 describe Game do
   subject(:game) { described_class.new("China") }
 
-  it "can return player 1's name" do
-    expect(game.player1).to eq "China"
+  describe '.create' do
+    it "creates an instance of itself" do
+      Game.create("John")
+      expect(described_class.instance.player1).to eq "John"
+    end
   end
+
+  describe '#player1' do
+    it "returns player 1's name" do
+      expect(game.player1).to eq "China"
+    end
+  end
+
 end
