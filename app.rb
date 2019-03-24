b8b7f15818fca04a1ab0844a315b3a8854a15df2
Lauren <laurenqurashi@me.com>
2019-03-24 19:33:41 +0000

    attempted to impliment a function that chooses a winner.

--- app.rb
+++ app.rb
@@ -38,6 +38,29 @@ class RPS < Sinatra::Base;
 
   get '/results' do
     @pick = session[:pick]
+    @computer = compchoice
+    @winner = decider
+
+    def compchoice
+      choosy = ["Rock", "Paper", "Scissors"].sample
+      return choosy
+    end
+
+    def decider
+      if (@pick == "Rock" && @computer == "Scissors") ||
+        (@pick == "Scissors" && @computer == "Paper") ||
+        (@pick == "Paper" && @computer == "Rock")
+          return @player_1_name
+      elsif (@computer == "Rock" && @pick == "Scissors") ||
+        (@computer == "Scissors" && @pick == "Paper") ||
+        (@computer == "Paper" && @pick == "Rock")
+          return "Computer"
+      elsif (@computer == "Rock" && @pick == "Rock") ||
+        (@computer == "Scissors" && @pick == "Scissors") ||
+        (@computer == "Paper" && @pick == "Paper")
+          return "It's a Draw!"
+      end
+    end
     erb :results
   end
 end
