47d2ec045800a24e83151b80e27f984be1f1f057
Wardah <wlt_93@hotmail.com>
2019-03-24 18:32:53 +0000

    Checks if player loses by having Scissors chosen against Rock.Passes test.

--- spec/features/winner_spec.rb
+++ spec/features/winner_spec.rb
@@ -13,3 +13,16 @@ feature 'Game Results' do
   end
 end
 
+feature 'Game Results' do
+  scenario 'checks if player wins by having Scissors chosen against Rock' do
+    allow_any_instance_of(Auto).to receive(:random_pick).and_return('Rock')
+    visit '/'
+    fill_in('name', with: 'Yoyo')
+    click_button 'Register'
+    click_button 'Lets Play'
+    find("option[value='Scissors']").select_option
+    click_button 'Confirm'
+    expect(page).to have_content "You Lost!"
+  end
+end
+
