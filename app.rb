536bb20c3a84be48dd3a1433a62d9b2ef356936d
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-24 14:16:13 +0000

    Restart after failure but with battle code incorporated

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 require "sinatra/base"
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
+    session[:name] = params[:name]
   end
+
+  post 'entered' do
+    erb :entered
 end
 
   # start the server if ruby file executed directly
   # run! if app_file == $0
+end 
