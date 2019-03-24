9d0b0831f96afdb63d34abef9f5f534537f10fc4
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-03-24 17:56:40 +0000

    Test for welcome message passes! click_on was the missing command

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,10 +1,8 @@
-# feature 'Welcome' do
-#   scenario 'Says the players name back to them after entry' do
-#     visit('/')
-#     fill_in 'p1name', with: 'Suzy'
-#     p "aaaaaaaaaaaa"
-#     click_button "Submit"
-#     p "bbbbbbbbbbbb"
-#     expect().to have_content "Welcome Suzy!!! Have a shiney light!!!"
-#   end
-# end
+feature 'Welcome' do
+  scenario 'Says the players name back to them after entry' do
+    visit('/')
+    fill_in 'p1name', with: 'Suzy'
+    click_on "Submit"
+    expect(page).to have_content "Welcome Suzy!!! Have a shiney light!!!"
+  end
+end
