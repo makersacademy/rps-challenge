17a81cd797c2895fbe8265db7b76da3fe043c835
justcallmegoyo <justcallmegoyo@gmail.com>
2019-03-24 19:22:05 +0000

    Tested rock paper scissors dropdown

--- app.rb
+++ app.rb
@@ -4,19 +4,17 @@ require "./randomchoice.rb"
 class RPS < Sinatra::Base
   enable :sessions
 
-  get '/' do
+  get "/" do
     erb :index
   end
 
-  post '/entered' do
+  post "/entered"do
     session[:name] = params[:name]
     erb :entered
   end
 
-  post '/response' do
-    erb :chosen
+  post "/response" do
     session[:chosen] = params[:chosen]
-    @computer = RandomChoice.new
     erb :chosen
     end
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,15 +1,25 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
+feature "Testing infrastructure" do
+  scenario "Can run app and check page content" do
+    visit("/")
     expect(page).to have_content "Good day to you, Marketeer - lookin' good!"
   end
 
-  feature 'Enter names' do
-  scenario 'submitting names' do
-    visit('/')
+  feature "Enter names" do
+  scenario "submitting names" do
+    visit("/")
     fill_in :name, with: "Greg"
     click_button "Submit"
     expect(page).to have_content("Oh hello, Greg!")
   end
+end
+
+  feature "Enter game choice" do
+  scenario "Submit rock, paper or scissors" do
+    visit("/")
+    fill_in :name, with: "Greg"
+    click_button "Submit"
+    click_on "Submit"
+    expect(page).to have_content "You chose rock"
+end
 end
 end
