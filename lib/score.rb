1965fd208da47e5e4c4d4dcfff70bc520db84097
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-03-24 09:33:56 +0000

    Refactors score#result and last linting offences

--- lib/score.rb
+++ lib/score.rb
@@ -4,6 +4,8 @@ class Score
   def initialize(player_score, player_name)
     @player_score = player_score
     @player_name = player_name
+    @win = [["Rock", "Scissors"], ["Scissors", "Paper"], ["Paper", "Rock"]]
+    @lose = [["Scissors", "Rock"], ["Paper", "Scissors"], ["Rock", "Paper"]]
   end
 
   def play_computer
@@ -14,12 +16,12 @@ class Score
     @computer_score = play_computer
     options = [@player_score, @computer_score]
 
-    if options == ["Rock", "Paper"] || options == ["Paper", "Scissors"] || options == ["Scissors", "Rock"]
-      "Computer wins!"
-    elsif options == ["Paper", "Rock"] || options == ["Scissors", "Paper"] || options == ["Rock", "Scissors"]
-      "Congratulations, you win #{@player_name}!"
-    else
-      "A draw!"
+    @win.each do |pair|
+      return "Congratulations, you win #{@player_name}!" if options == pair
     end
+    @lose.each do |pair|
+      return "Computer wins!" if options == pair
+    end
+    return "A draw!"
   end
 end
