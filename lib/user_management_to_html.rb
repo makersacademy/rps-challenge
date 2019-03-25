1f02e7cddee65cbc4b2d623c9132119ec6d780f8
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-03-25 10:57:29 +0000

    Fixed linting errors

--- lib/user_management_to_html.rb
+++ lib/user_management_to_html.rb
@@ -3,7 +3,7 @@ module UserManagementToHTML
   include HTMLBuilder
 
   def self.users_to_html_list(users_obj, html_builder = HTMLBuilder)
-    user_names_array = users_obj.users.map { |user| user.name}
+    user_names_array = users_obj.users.map { |user| user.name }
     html_builder.array_to_list(user_names_array)
   end
 end
--- spec/user_management_to_html_spec.rb
+++ spec/user_management_to_html_spec.rb
@@ -3,10 +3,10 @@ require 'user_management_to_html'
 describe UserManagementToHTML do
 
   subject(:user_mgmt_to_html) { described_class }
-  let (:html_builder) { double :html_builder }
-  let(:user1) {  double :user, name: 'Andy'}
+  let(:html_builder) { double :html_builder }
+  let(:user1) {  double :user, name: 'Andy' }
   let(:user2) {  double :user, name: 'Sam' }
-  let (:users) { double :users, users: [user1, user2] }
+  let(:users) { double :users, users: [user1, user2] }
 
   describe '#users_to_html_list' do
     it 'should create list of users in html form from users object' do
