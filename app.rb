a9c6fc7d9051793eb4f77ebc22ad6171823399a9
fo0s <fo0sies@gmail.com>
2019-03-25 00:35:07 +0000

    Added weapons screen. Needs finishing

--- app.rb
+++ app.rb
@@ -1,17 +1,36 @@
 require 'sinatra/base'
 require './lib/game'
+require './lib/player'
 
 class RPS < Sinatra::Base
   enable :sessions
 
+  # Homepage
   get '/' do
     erb(:index)
   end
 
+  # Homepage input referer
   post '/players' do
     @players = params[:players]
     session[:players] = @players.to_i
     erb(:index)
+  end
+
+  # Name saving, bootup game and redirect
+  post '/names' do
+    player1 = Player.new(params[:player1_name])
+    player2_name = params[:player2_name].nil? ? "Computer" : params[:player2_name]
+    player2 = Player.new(player2_name)
+    @rps = RPSGame.create(player1, player2)
+    redirect '/weapons'
+  end
+
+  # Weapon choosing
+  get '/weapons' do
+    @rps = RPSGame.instance
+    @message = session[:message]
+    erb(:weapons)
   end
 
   # start the server if ruby file executed directly
