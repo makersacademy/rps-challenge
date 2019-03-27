5c842f032a59ce8df8437e05d259bea73253c218
Riya Dattani <riya_dattani@hotmail.com>
2019-03-27 11:19:34 +0000

    added styling and additional tests on winner_spec

--- spec/features/winner_spec.rb
+++ spec/features/winner_spec.rb
@@ -1,11 +1,28 @@
 feature 'winner' do
-  scenario 'display the winner after player and computer made a move' do
+  scenario 'display the winner if player chooses rock and computer is stubbed at paper' do
     srand(678_910)
     sign_in_and_play
     click_button 'Rock'
-    visit '/result'
     expect(page).to have_content "Your move: Rock"
     expect(page).to have_content "Computer's move: Paper"
     expect(page).to have_content "The computer wins!"
+  end
+
+  scenario 'display the winner if player chooses paper and computer is stubbed at paper' do
+    srand(678_910)
+    sign_in_and_play
+    click_button 'Paper'
+    expect(page).to have_content "Your move: Paper"
+    expect(page).to have_content "Computer's move: Paper"
+    expect(page).to have_content "Draw!"
+  end
+
+  scenario 'display the winner if player chooses scissors and computer is stubbed at paper' do
+    srand(678_910)
+    sign_in_and_play
+    click_button 'Scissors'
+    expect(page).to have_content "Your move: Scissors"
+    expect(page).to have_content "Computer's move: Paper"
+    expect(page).to have_content "You win!"
   end
 end
