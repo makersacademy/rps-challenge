3cb6068f785b17ba2a115e61ba6e18c4d37b1478
Jose Macias <josemariamacias@gmail.com>
2019-04-05 14:10:31 +0100

    Moved names and plays variables access inside views

--- lib/app.rb
+++ lib/app.rb
@@ -16,8 +16,6 @@ class Rps < Sinatra::Base
   end
 
   post "/name" do
-    # $player_1 = Player.new(params[:player_name])
-    # session[:player_name] = params[:player_name]
     player_1 = Player.new(params[:player_name])
     player_2 = PcPlayer.new
     @game = Game.create(player_1, player_2)
@@ -25,29 +23,19 @@ class Rps < Sinatra::Base
   end
 
   get "/play" do
-    @player_name = @game.player_1.name
-    # @player_name = session[:player_name]
     erb :play
   end
 
   post "/check" do
-
-    # session[:selection] = params[:selection]
     @game.player_1.set_play(params[:selection])
     @game.player_2.set_play
-    # session[:pc_selection] = PcPlayer.play
     session[:result] = ResultChecker.check(@game.player_1.play, @game.player_2.play)
 
     redirect("/result")
   end
 
   get "/result" do
-    # @selection = session[:selection]
-    @selection = @game.player_1.play
-    @pc_selection = @game.player_2.play
     @result = session[:result]
-    # @player_name = session[:player_name]
-    @player_name = @game.player_1.name
     erb :result
   end
 end
