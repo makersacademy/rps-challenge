44ad70312469cb7b82869de486cd86bc15728457
Soleyman Shahir <Soleymanshahir@yahoo.com>
2019-04-07 19:51:44 +0100

    updated game spec with doubles and scenarios for player 1 and player 2 wins

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,27 @@
 require 'game'
 
 describe Game do
+subject(:game) { Game.new("Joe", "Ollie")}
 
-  it 'Player selects rock' do
-    subject.rock
-    expect(subject).to eq rock
+let (:weapon1) { double :controller, :weapon => "Rock"}
+let (:weapon2) { double :controller, :weapon => "Paper"}
+let (:weapon3) { double :controller, :weapon => "Scissors"}
+
+  it 'Joe wins' do
+    subject.player_1.weapon1
+    subject.player_2.weapon3
+    expect(subject.winnner).to eq "#{subject.player_1.pname} wins"
+  end
+
+  it 'Ollie wins' do
+    subject.player_1.weapon1
+    subject.player_2.weapon2
+    expect(subject.winnner).to eq "#{subject.player_2.pname} wins"
+  end
+
+  it 'Draw' do
+    subject.player_1.weapon2
+    subject.player_2.weapon2
+    expect(subject.winner).to eq "Draw"
   end
 end
