d8abf9449e44422c45588d113b5f0fe3a2b63438
Phil <phil.board90@googlemail.com>
2019-04-10 11:13:44 +0100

    added tests using srand that stop the randomisation

--- spec/randomiser_spec.rb
+++ spec/randomiser_spec.rb
@@ -5,5 +5,26 @@ describe Randomiser do
       randomiser = Randomiser.new
       expect(["Rock", "Paper", "Scissors"]).to include(randomiser.computer_choice)
     end
+
+    it 'returns Paper' do
+      srand(9999999)
+      randomiser = Randomiser.new
+      puts randomiser.computer_choice
+      expect(randomiser.computer_choice).to eq 'Paper'
+    end
+
+    it 'returns Scissors' do
+      srand(67809)
+      randomiser = Randomiser.new
+      puts randomiser.computer_choice
+      expect(randomiser.computer_choice).to eq 'Scissors'
+    end
+
+    it 'returns Rock' do
+      srand(67811)
+      randomiser = Randomiser.new
+      puts randomiser.computer_choice
+      expect(randomiser.computer_choice).to eq 'Rock'
+    end
   end
 end
--- spec/who_wins_spec.rb
+++ spec/who_wins_spec.rb
@@ -1,9 +1,24 @@
-#describe RockPaperScissors do
-#  describe '#who_wins?' do
-#    it 'picks the correct winner' do
-#      game = RockPaperScissors.new("Rock")
-#      allow(game).to receive(:computer_choice).and_return("Scissors")
-#      expect(game.who_wins?).to eq "user"
-#    end
-#  end
-#end
+describe RockPaperScissors do
+  describe '#who_wins?' do
+    it 'returns a tie' do
+      srand(67811)
+      game = RockPaperScissors.new("Rock")
+      puts game.computer_choice
+      expect(game.who_wins?).to eq "tie"
+    end
+
+    it 'returns a user win' do
+      srand(67809)
+      game = RockPaperScissors.new("Rock")
+      puts game.computer_choice
+      expect(game.who_wins?).to eq "user"
+    end
+
+    it 'returns a computer win' do
+      srand(9999999)
+      game = RockPaperScissors.new("Rock")
+      puts game.computer_choice
+      expect(game.who_wins?).to eq "computer"
+    end
+  end
+end
