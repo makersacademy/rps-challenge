0bd288e3830fa6d79a8cb98c54234e5ded190f9f
Hannah Frost <h.7frost@gmail.com>
2019-04-18 13:00:58 +0100

    Added /name route

--- app.rb
+++ app.rb
@@ -2,4 +2,14 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
 enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  get '/name' do
+    @player = params[:name]
+    redirect '/game'
+  end
+
 end
