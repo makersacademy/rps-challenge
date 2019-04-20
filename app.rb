50df02cc7b22a77e9bed26071c17ccd7d1b5b79d
sarpong <abasimisarpong@rocketmail.com>
2019-04-20 12:01:48 +0100

    added start route to controller

--- app.rb
+++ app.rb
@@ -1,7 +1,29 @@
 require 'sinatra/base'
+require 'sass'
+require_relative 'lib/player'
 
 class Rps < Sinatra::Base
+  enable :sessions
+  
+  get '/styles.css' do
+    scss :styles
+  end
+
   get '/' do
-    'hello' 
+    erb :index
+  end
+
+  get '/name' do
+    erb :name
+  end
+
+  get '/start' do
+    @player_one = Player.new(session[:player_name])
+    erb :start
+  end
+
+  post '/play' do
+    session[:player_name] = params[:name]
+    redirect '/start'
   end
 end
