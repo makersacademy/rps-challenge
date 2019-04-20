b14a47cd91daeef5f59bf619168ec474a1246733
sarpong <abasimisarpong@rocketmail.com>
2019-04-20 12:02:06 +0100

    All test passing

--- spec/features/rps_spec.rb
+++ spec/features/rps_spec.rb
@@ -1,8 +1,31 @@
 
-
-RSpec.feature 'checking setup' do
-  scenario 'when on page' do
+RSpec.feature 'Nav bar logo' do
+  scenario 'user can see nav bar logo' do
     visit('/')
     expect(page).to have_content('RPS')
+  end
+end
+
+RSpec.feature 'Welcome message' do
+  scenario 'user can see a welcome message' do
+    visit('/')
+    expect(page).to have_content('Welcome to the Treasure Land  press start to begin.')
+  end
+end
+
+RSpec.feature 'Button' do
+  scenario 'user must be able to click on button' do
+    visit('/')
+    click_link('getstarted')
+    expect(page).to have_content('Enter Your Name')
+  end
+end
+
+RSpec.feature 'Form' do
+  scenario 'user should be able to fill in form' do
+    visit('./name')
+    fill_in 'name', with: 'chris'
+    click_button('submit')
+    expect(page).to have_content('chris')
   end
 end
