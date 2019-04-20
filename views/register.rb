209b0b85776142d50a1bf59b4926af66d130cb4b
Stuart <stuart.pethurst@gmail.com>
2019-04-20 14:31:30 +0100

    User story 1 complete. Unable to test fully as rack testing not working, I will try to solve and resubmit my commit

---
+++ views/register.rb
@@ -0,0 +1,18 @@
+require "sinatra/base"
+
+class RPS < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    erb :play
+  end
+
+  run! if app_file == $0
+end
