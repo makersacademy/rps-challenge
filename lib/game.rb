4fa1c747e557784b72aa2771fa2ddff9e347e46d
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-20 20:29:06 +0100

    Refactored Game using singleton pattern

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,18 @@
 class Game
 
   attr_reader :player_name
-  
+
+  def self.create(player)
+    @game = Game.new(player)
+  end
+
+  def self.instance
+    @game
+  end
+
+  private
   def initialize(player)
     @player_name = player
   end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,9 @@
 require 'game'
 
 describe Game do
-  let (:subject) { described_class.new("Ayelisha") }
+  let(:subject) { described_class.create("Ayelisha") }
 
-  it 'stores a players name'do
+  it 'stores a players name' do
     expect(subject.player_name).to eq "Ayelisha"
   end
 end
