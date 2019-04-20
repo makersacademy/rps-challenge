785593d4e30b461acb4dbcc5ccbd0ffd52a0c2b6
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-20 20:08:22 +0100

    Added random computer choice

--- app.rb
+++ app.rb
@@ -12,4 +12,19 @@ class RockPaperScissors < Sinatra::Base
     erb :play
   end
 
+  post '/names' do
+    @player_name = params[:player_name]
+    erb :play
+  end
+
+  post '/choice' do
+    @choice = params[:choice]
+    erb :choice
+  end
+
+  get '/computer_choice' do
+    @computer_choice = ["Rock","Paper","Scissors"].sample
+    erb :computer_choice
+  end
+
 end
