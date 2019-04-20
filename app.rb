f20be2c76ef8b49dab27a0a18c2c63047ec81651
Jake Napper <napperjlg@gmail.com>
2019-04-20 20:21:26 +0100

    player can enter their name and it is displayed on the next page

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
-  
+
 get '/' do
-  "Hello world"
+  erb :index
+end
+
+post '/play' do
+  @name = params[:name]
+  erb :play
 end
 
 run! if app_file == $0
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,8 @@
+feature 'Entering player names' do
+  scenario 'player name is entered and saved' do
+    visit('/')
+    fill_in('name', with: 'Jake')
+    click_on('Submit!')
+    expect(page).to have_content('Welcome Jake')
+  end
+end
