f2ae1001b55459a2a6375b2247a6911254c98def
domw30 <domw30@gmail.com>
2019-04-21 12:35:53 +0100

    amend feature test to expect refactored string, update selection view

--- spec/features/declare_winner_spec.rb
+++ spec/features/declare_winner_spec.rb
@@ -2,6 +2,6 @@ feature 'Declare A Winner' do
   scenario 'Winner displayed on page' do
     sign_in_and_click_play
     click_button(['ROCK', 'PAPER', 'SCISSORS'].sample)
-    expect(page).to have_content 'IS THE WINNER!' || 'DRAW!'
+    expect(page).to have_content 'WINS!'
   end
 end
