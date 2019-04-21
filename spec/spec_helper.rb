cd9cf1092dc8e12779cf2e2f655c00ca2e117146
shektor <44544977+shektor@users.noreply.github.com>
2019-04-21 16:58:55 +0100

    Add test configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
+ENV['RACK_ENV'] = 'test'
+
+require './app.rb'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = Rps
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
