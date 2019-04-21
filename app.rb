7b0e15786c15c4b609937fb1c41fa2469986bbbf
Hannah Frost <h.7frost@gmail.com>
2019-04-21 15:11:07 +0100

    game route added

--- app.rb
+++ app.rb
@@ -9,13 +9,18 @@ enable :sessions
 
   post '/name' do
     session[:player_name] = params[:player_name]
-    redirect '/game'
+    redirect '/welcome'
+  end
+
+  get '/welcome' do
+    @player_name = session[:player_name]
+    erb :welcome
   end
 
   get '/game' do
-    @player_name = session[:player_name]
     erb :game
   end
 
   run! if app_file == $0
+
 end
