d872601aaaca7881cf3ddc17923d338db1ca630b
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-21 14:02:17 +0100

    pass test to display computers move

--- spec/winner_spec.rb
+++ spec/winner_spec.rb
@@ -6,11 +6,14 @@ feature 'Winner' do
     click_button "Rock"
     expect(page).to have_content "You have selected Rock."
   end
-  # scenario "displays the computer's move" do
-  #
-  #   expect(page).to have_content "Computer move: #{ @computer_move }"
-  # end
-  #
+  scenario "displays the computer's move" do
+    visit ('/')
+    fill_in :name, :with => "Tara"
+    click_button "Submit"
+    click_button "Rock"
+    expect(page).to have_content "Computer move: #{ @computer_move }"
+  end
+
   # scenario "displays winner when computer wins" do
   #
   #   allow(computer).to receive(:move).and_return(@computer_move)
