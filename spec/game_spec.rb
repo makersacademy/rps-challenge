32cf8fdd72f9004c3103cd5ec3aebfe01b5d2aa2
Jordan <jordanroberts@talk21.com>
2019-04-21 18:47:16 +0100

    Added very basic HTML - need to learn CSS

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,19 +4,19 @@ describe Game do
   subject(:game) { Game.new("rock", "scissors") }
 
   describe '#player' do
-    it 'shows the player hand' do
+    it 'shows the players move' do
       expect(game.player).to eq "rock"
     end
   end
 
   describe '#computer' do
-    it 'shows computer hand' do
+    it 'shows computer move' do
       expect(game.computer).to eq "scissors"
     end
   end
 
-  describe 'winner?' do
-    it 'shows winner' do
+  describe '#winner?' do
+    it 'shows the winner' do
     expect(game.winner?).to eq "You chose rock and the computer chose scissors! Well done!"
   end
 end
