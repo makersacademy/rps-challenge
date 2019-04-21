06071b3269c5477bbd5588ed727fee73a3764206
domw30 <domw30@gmail.com>
2019-04-21 11:53:44 +0100

    amend mistake on if statement

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 class Game
 
   def result(player1_move, player2_move)
-    if (player1_move == 'ROCK' && player2_move == 'PAPER') || (player1_move == 'PAPER' && player2_move == 'SCISSORS') || (player1_move == 'SCISSORS' && player2_move == 'PAPER')
+    if (player1_move == 'ROCK' && player2_move == 'PAPER') || (player1_move == 'PAPER' && player2_move == 'SCISSORS') || (player1_move == 'SCISSORS' && player2_move == 'ROCK')
       2
     elsif (player1_move == 'ROCK' && player2_move == 'SCISSORS') || (player1_move == 'PAPER' && player2_move == 'ROCK') || (player1_move == 'SCISSORS' && player2_move == 'PAPER')
       1
