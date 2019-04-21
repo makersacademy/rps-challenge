58003ecba8ca144358d3c9bdb6d91d4d2fdf2e21
sejinkay <sejinkay@gmail.com>
2019-04-21 19:35:38 +0100

    added player model, (horrible) global variable for now

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
+require_relative 'lib/player'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player = params[:player]
+    # $player = Player.new(params[:player])
+    redirect to('/play')
+  end
+
+  post '/play' do
+    $player = Player.new(params[:player])
+    @player = $player.name
     erb(:play)
   end
 
