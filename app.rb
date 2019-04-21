baf8f688bc467dc11841b4fc72e9cc9b9e302d61
Xain Storey <xhs893@gmail.com>
2019-04-21 10:44:49 +0100

    Adds sinatra sessions to store name on /play route. Stores player_1_name params in session.

--- app.rb
+++ app.rb
@@ -2,12 +2,20 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
-  post 'names' do
-    
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    redirect "/play"
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    erb :play
   end
 
 end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -4,3 +4,12 @@ feature 'Shows submit form for name' do
     fill_in :player_1_name, with: 'Xain'
   end
 end
+
+feature 'Displays name in lights' do
+  scenario 'user logs in and sees name' do
+    visit("/")
+    fill_in :player_1_name, with: 'Xain'
+    click_button 'Log in!'
+    expect(page).to have_content "Xain"
+  end
+end
