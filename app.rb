aa250633e33aa2b979bf98b896a35748b700738e
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-21 13:56:41 +0100

    create /winners which displays both moves and tell you who won

--- app.rb
+++ app.rb
@@ -1,4 +1,8 @@
 require 'sinatra/base'
+require 'sinatra'
+require './lib/computer'
+require './lib/player'
+require './lib/winner'
 
 class RPS < Sinatra::Base
   enable :sessions
@@ -17,5 +21,14 @@ class RPS < Sinatra::Base
     @name = session[:name]
     erb(:play)
   end
+
+  post '/winner' do
+	    @player_move = params[:player_move]
+	    computer = Computer.new
+	    @computer_move = computer.move
+	    winner = Winner.new(@player_move, @computer_move)
+	    @result = winner.result(@player_move, @computer_move)
+	    erb :winner
+	  end
 
 end
