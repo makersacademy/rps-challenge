6d4c758d2e08e3d0074bdab472bfb947d359d4f5
Jordan <jordanroberts@talk21.com>
2019-04-21 07:08:40 +0100

    Changed links for buttons

--- spec/features/player_choice_spec.rb
+++ spec/features/player_choice_spec.rb
@@ -1,26 +1,26 @@
 feature 'Given options for RPS' do
   scenario 'player sees buttons for rock paper scissors' do
     sign_in_and_play
-    expect(page).to have_selector(:link,'Rock')
-    expect(page).to have_selector(:link,'Paper')
-    expect(page).to have_selector(:link,'Scissors')
+    expect(page).to have_selector(:button,'Rock')
+    expect(page).to have_selector(:button,'Paper')
+    expect(page).to have_selector(:button,'Scissors')
   end
 
   scenario 'player chooses rock' do
     sign_in_and_play
-    click_link('Rock')
-    expect(page).to have_content('rock')
+    click_button'Rock'
+    expect(page).to have_content'rock'
   end
 
   scenario 'player chooses paper' do
     sign_in_and_play
-    click_link('Paper')
-    expect(page).to have_content('paper')
+    click_button'Paper'
+    expect(page).to have_content'paper'
   end
 
   scenario 'player chooses rock' do
     sign_in_and_play
-    click_link('Scissors')
-    expect(page).to have_content('scissors')
+    click_button'Scissors'
+    expect(page).to have_content'scissors'
   end
 end
