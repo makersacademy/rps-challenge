98babccc6f24c581422e005638059345036e0357
shektor <44544977+shektor@users.noreply.github.com>
2019-04-22 17:31:43 +0100

    Update route handlers to use sinatra filter

--- app.rb
+++ app.rb
@@ -2,31 +2,30 @@ require 'sinatra/base'
 
 class Rps < Sinatra::Base
 
-  enable :sessions
+  before do
+    @game = Game.instance
+  end
 
   get '/' do
     erb(:index)
   end
 
   post '/name' do
-    Game.create(params[:player_name])
+    @game = Game.create(params[:player_name])
     redirect '/play'
   end
 
   get '/play' do
-    @game = Game.instance
     erb(:play)
   end
 
   post '/decide-winner' do
-    @game = Game.instance
     @game.player_picks(params[:choice])
     @game.decide_winner
     redirect '/result'
   end
 
   get '/result' do
-    @game = Game.instance
     erb(:result)
   end
 
