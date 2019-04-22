0c2d8fdcd3dbd198ef276b526f2efcf8d60b244c
yadlra <yadi.sb1@gmail.com>
2019-04-22 17:20:17 +0100

    stylesheet css

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require_relative 'player'
+require_relative 'console'
 
 class Game
   attr_reader :player, :console
@@ -7,4 +8,16 @@ class Game
     @player = player
     @console = console
   end
+
+  #  RULESMAP = { "Paper" <= "Scissors", "Scissors" <= "Rock", "Rock" <= "Paper" }
+  #
+  # def winner
+  #   if @player.move == RULESMAP[@player.move]
+  #     "Console wins"
+  #   elsif @player.move == @console.console_choice
+  #     "Draw"
+  #   else
+  #     "You win"
+  #   end
+  # end
 end
---
+++ spec/features/winner_spec.rb
@@ -0,0 +1,8 @@
+# feature 'champion' do
+#   scenario 'returns game champion' do
+#     srand(678909)
+#     sign_in_and_play
+#     click_button 'Rock'
+#     expect(page).to have_content "You lose!"
+#   end
+# end
