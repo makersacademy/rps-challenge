64c63e764cdfc4150fdaa4204dac04d914a9ecc6
Calum Darroch <calum_darroch@hotmail.com>
2019-04-22 14:03:01 +0100

    web_helpers added to feature tests (DRY)

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -1,8 +1,6 @@
 feature "Enter name" do
   scenario "submit name" do
-    visit('/')
-    fill_in :name, with: 'Calum'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Calum'
   end
 end
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,8 +1,6 @@
 feature "Choose move" do
   scenario "allows the player to choose Rock, Paper or Scissors" do
-    visit('/')
-    fill_in :name, with: 'Calum'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_button("Rock")
     expect(page).to have_button("Paper")
     expect(page).to have_button("Scissors")
