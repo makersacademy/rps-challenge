241e18c3c26084c31e5fd0648fb37723add2da58
Calum Darroch <calum_darroch@hotmail.com>
2019-04-22 13:30:51 +0100

    Implement 'POST/redirect/GET' pattern, added sessions

--- app.rb
+++ app.rb
@@ -2,13 +2,19 @@ require 'sinatra'
 require_relative './lib/player.rb'
 
 class Game < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/name' do
-    @player = Player.new(params[:name])
+    session[:name] = params[:name]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player = Player.new(session[:name])
     erb :play
   end
 
