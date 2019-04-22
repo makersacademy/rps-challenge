fa77cc179879464eec4cb87fcb60412baf476177
Kiah Hewitt <hewittkiah@gmail.com>
2019-04-22 13:22:16 +0100

    Fixed feature tests/browser errors

--- app.rb
+++ app.rb
@@ -1,21 +1,29 @@
 require 'sinatra/base'
+require_relative 'lib/player'
 
 class RockPaperScissors < Sinatra::Base
+  set :session_secret, 'super secret'
   enable :sessions
 
   get "/" do
     erb :index
   end
 
-  get "/names" do
-    $player = Player.new(params[:player])
-    redirect '/play'
-  end
-
-  get "/play" do
-    @player = $player
+  post "/play" do
+    @player = params[:player]
     erb :play
   end
 
-  run if app_file == $0
+  get '/move' do
+    @player = $player
+    @player.move
+    erb :move
+    redirect '/results'
+  end
+
+  get '/secret' do
+    'This is a secret page'
+  end
+
+  run! if app_file == $0
 end
