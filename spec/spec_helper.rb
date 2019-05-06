feac652fcc0e56e62653f6b4550e7ab906e71ae1
Cosmin <mirceacosmin@gmail.com>
2019-05-06 23:20:51 +0100

    Updated Readme badge

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,22 +5,14 @@ require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 require './spec/features/web_helpers_spec'
+require 'shields_badge'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb') # requires sinatra app file
 ENV['RACK_ENV'] ||= 'test' 
 Capybara.app = Game
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
 SimpleCov.start
-
-RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
-end
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::ShieldsBadge,
+  SimpleCov::Formatter::Console,
+]
