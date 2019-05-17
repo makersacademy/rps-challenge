f58dd4892e054bf7d763d05b34e30ec59be5d766
Lucy Barber <lbarber729@hotmail.co.uk>
2019-05-17 21:35:38 +0100

    Addd .css file and result page

--- app.rb
+++ app.rb
@@ -1,15 +1,24 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :game
   end
 
-  post '/play' do
-    p params
-    @player_name = params[:player_name]
+  post '/player' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
     erb :play
+  end
+
+  post '/result' do
+    erb :result
   end
 
 end
