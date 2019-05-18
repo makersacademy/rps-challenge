6ad8819817472615f69f825b6b6baf393ba2c44d
Max Van Aken <mtvanaken@gmail.com>
2019-05-18 19:48:47 +0100

    rspec tests update

--- spec/features/make_move_spec.rb
+++ spec/features/make_move_spec.rb
@@ -7,17 +7,17 @@ feature "MakeMove" do
     expect(page).to have_content 'Halsey played Rock'
   end
 
-  # scenario "choose paper" do
-  #   sign_in_and_play
-  #   click ("Paper")
-  #   click_button 'Submit'
-  #   expect(page).to have_content 'Halsey played Paper'
-  # end
-  #
-  # scenario "choose scissors" do
-  #   sign_in_and_play
-  #   click ("Scissors")
-  #   click_button 'Submit'
-  #   expect(page).to have_content 'Halsey played Paper'
-  # end
+  scenario "choose paper" do
+    sign_in_and_play
+    choose("Paper")
+    click_button 'Submit'
+    expect(page).to have_content 'Halsey played Paper'
+  end
+
+  scenario "choose scissors" do
+    sign_in_and_play
+    choose("Scissors")
+    click_button 'Submit'
+    expect(page).to have_content 'Halsey played Scissors'
+  end
 end
