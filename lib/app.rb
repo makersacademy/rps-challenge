bed557ac6e30a779ce793b850847a8ec86cb85d2
Michael Hughes <mhughes36@live.co.uk>
2019-05-18 15:16:19 +0100

    Display player's name

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,19 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb(:index)
+  end
+
+  post '/name' do
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    erb(:play)
+  end
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,7 +1,8 @@
 feature 'enter names' do
   scenario 'submit names' do
+    visit('/')
     fill_in :player_1_name, with: 'Evita'
     click_button 'Continue'
-    expect(page).to have_content('Welcome Evita!')
+    expect(page).to have_content('Evita')
   end
 end
