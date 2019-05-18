0dcecaee14b00f0185559267af3e8d88fdf37873
Francesca Chater <francescachater@gmail.com>
2019-05-18 09:34:51 +0100

    Player class with name method added, rspec test passes

---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+require 'player'
+
+describe Player do
+  subject(:francesca) { Player.new('Francesca')}
+
+  describe '#name' do
+    it 'returns name of player' do
+      expect(francesca.name).to eq('Francesca')
+    end
+  end
+
+end
