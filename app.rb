22f00edf756338140a1e8f0ad124a1579b07eceb
Francesca Chater <francescachater@gmail.com>
2019-05-18 09:25:58 +0100

    Closing user story 1: player can enter and see name

--- app.rb
+++ app.rb
@@ -1,11 +1,21 @@
 require 'sinatra/base'
 
+class Rps < Sinatra::Base
+
 # Welcome page to enter name
-get '/' do
-end
+  get '/' do
+    erb :index
+  end
 
-post '/play' do
-end
+  post '/play' do
+    p params
+    @name = params[:name]
+    erb :play
+  end
 
-post '/result' do
+  post '/result' do
+    erb :result
+  end
+
+  run! if app_file == $0
 end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,6 +1,8 @@
 feature 'Enter name' do
   scenario 'submitting name' do
-    sign_in_and_play
+    visit('/')
+    fill_in :name, with: 'Francesca'
+    click_button 'Submit'
     expect(page).to have_content 'Francesca'
   end
 end
