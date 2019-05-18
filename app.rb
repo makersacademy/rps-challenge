5ddfdb5751c8b5468024c4dbc7c55f379b15d551
Sam <samkitchen94@gmail.com>
2019-05-18 17:21:40 +0100

    player class works and redirects properly now

--- app.rb
+++ app.rb
@@ -2,13 +2,20 @@ require 'sinatra/base'
 require_relative './lib/player'
 
 class Rps < Sinatra::Base
+  enable :sessions
+
   get "/" do
     "Welcome to Rock, Paper, Scissors!"
     erb :index
   end
 
   post "/names" do
-    @player = Player.new(params[:player_name])
+    $player = Player.new(params[:player_name])
+    redirect '/play'
+  end
+
+  get "/play" do
+    @player = $player
     erb :play
   end
 
