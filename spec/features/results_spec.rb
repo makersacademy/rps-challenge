836938256fbb6745cb37d9d75185819b06f36ca6
Sam <samkitchen94@gmail.com>
2019-05-19 23:19:35 +0100

    feature tests added to check for random outcomes

--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -1,8 +1,23 @@
+require './lib/computer'
+
 feature "results" do
-  scenario "it tells the user if they have won" do
+  scenario "it tells the user what they chose" do
     sign_in_and_play
-    srand(2200)
     click_button("rock")
     expect(page).to have_content "Sam chose rock"
   end
+  scenario "it tells user what the computer chose" do
+    srand(100)
+    sign_in_and_play
+    click_button("rock")
+    expect(page).to have_content "Computer chose rock!!"
+  end
+  scenario "it tells user if they have won, drawn or lost" do
+    srand(569592)
+    sign_in_and_play
+    click_button("rock")
+    expect(page).to have_content "You beat the computer!"
+  end
 end
+
+#need more tests here about computer choosing randomly and page letting you know whether youve won, lost or drawn
