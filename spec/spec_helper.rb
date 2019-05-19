a5bf80c8f1e43089095c22367782651883a240f5
Student <magnusarbuthnott@gmail.com>
2019-05-19 16:31:49 +0100

    Modified to enable rack

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+Capybara.app = Rps
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
