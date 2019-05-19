20ad595bcbe749bcbacc59f4e766142d7e59d93b
Sam <samkitchen94@gmail.com>
2019-05-19 10:36:05 +0100

    test added to see that it selects an option randomly

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,7 +1,11 @@
 class Computer
   attr_reader :options
-  
+
   def initialize
     @options = ["Rock", "Paper", "Scissors"]
+  end
+
+  def selection
+    @options.sample
   end
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -4,7 +4,13 @@ describe Computer do
   subject(:comp) { Computer.new }
   describe '#initialize' do
     it "initializes with an array of options to select" do
-      expect(subject.options).to include("Rock" && "Paper"  && "Scissors")
+      expect(subject.options).to include("Rock" && "Paper" && "Scissors")
+    end
+  end
+
+  describe '#selection' do
+    it "selects an option randomly" do
+      expect(subject.options).to include(subject.selection)
     end
   end
 end
