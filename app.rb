1a6872c093a3dd1381f5576cd8ccd0a3fe7eff57
Oliver Brownlow <olliebrownlow@gmail.com>
2019-05-20 09:50:08 +0100

    maintains a running score for players

--- app.rb
+++ app.rb
@@ -36,20 +36,17 @@ class RPS < Sinatra::Base
   end
 
   get '/game-choice' do
-
-    @marketer_1 = session[:marketer_1]
-    @marketer_2 = session[:marketer_2]
+    marketer_1 = Player.new(session[:marketer_1])
+    @marketer_1 = marketer_1.name
+    marketer_2 = Player.new(session[:marketer_2])
+    @marketer_2 = marketer_2.name
+    @game = RPSGame.create(marketer_1, marketer_2)
     erb :game_choice
   end
 
   get '/play-rps' do
-    marketer_1 = Player.new(session[:marketer_1])
-    @marketer_1 = marketer_1.name
-    @marketer_1_points = marketer_1.points
-    marketer_2 = Player.new(session[:marketer_2])
-    @marketer_2 = marketer_2.name
-    @marketer_2_points = marketer_2.points
-    @game = RPSGame.create(marketer_1, marketer_2)
+    @marketer_1 = @game.marketer_1.name
+    @marketer_2 = @game.marketer_2.name
     erb :play
   end
 
@@ -62,8 +59,6 @@ class RPS < Sinatra::Base
   get '/results-rps' do
     @marketer_1 = @game.marketer_1.name
     @marketer_2 = @game.marketer_2.name
-    @marketer_1_points = @game.marketer_1.points
-    @marketer_2_points = @game.marketer_2.points
     @player_weapon = @game.marketer_1.weapon
     @bot_weapon = @game.marketer_2.weapon
     erb :result
