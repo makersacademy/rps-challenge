eed049d09713494f0ffbdd12dfaeac0d9bbf313d
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-05-20 10:17:43 +0100

    refactored tests for mocking

--- spec/features/game_outcome_spec.rb
+++ spec/features/game_outcome_spec.rb
@@ -1,7 +1,14 @@
 feature 'outcome of the game' do
   scenario "the winner is shown" do
+    allow_any_instance_of(Game).to receive(:choice).and_return('Scissors')
     start_game
     click_button 'Rock'
-    expect(page).to have_content('wins')
+    expect(page).to have_content('Rock wins!') 
+  end
+  scenario "a draw is shown" do
+    allow_any_instance_of(Game).to receive(:choice).and_return('Rock')
+    start_game
+    click_button 'Rock'
+    expect(page).to have_content("It's a draw!")
   end
 end
