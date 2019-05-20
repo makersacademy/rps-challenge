609a6e8675233a605b2883525f99e56a1f72c1d7
Student <magnusarbuthnott@gmail.com>
2019-05-20 07:09:08 +0100

    Added web_helpers.rb file

--- spec/features/submit_name_spec.rb
+++ spec/features/submit_name_spec.rb
@@ -2,13 +2,9 @@
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
 
-require 'capybara/rspec'
-
 feature 'enter name' do
   scenario 'the player can submit their name and be redirected to a welcome page' do
-    visit '/'
-    fill_in :player_1_name, with: 'James'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Welcome James'
   end
 end
