5110e8bc3f62d214967b4ca3de8cfcfc14c6ff7e
David Thai <dafuloth@users.noreply.github.com>
2019-05-23 00:45:28 +0100

    added line after guard clause, as suggested by rubocop

--- lib/game.rb
+++ lib/game.rb
@@ -9,6 +9,7 @@ class Game
 
   def self.result(player1_choice, player2_choice)
     return :draw if player1_choice.to_sym == player2_choice.to_sym
+    
     WINS[player1_choice.to_sym] == player2_choice.to_sym ? :win : :lose
   end
 end
