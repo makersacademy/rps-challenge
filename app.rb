8dab83a3c2dc2266accb32d603ff9622d64d9269
Anna <annas.wieclaw@gmail.com>
2019-05-31 11:09:01 +0100

    Test drive entering players name with PostRedirectGet method

--- app.rb
+++ app.rb
@@ -2,9 +2,19 @@ require 'sinatra'
 require 'sinatra/base'
 	
 class RPS < Sinatra::Base
-	
+	enable :sessions
 	  get '/' do
-	    "Let's play Rock Paper Scissors!"
+	    erb :index
+    end
+    
+    post '/name' do
+      session[:name_message] = params[:player]
+      redirect '/play'
+    end
+
+    get '/play' do
+      @name = session[:name_message]
+	    erb :play
 	  end
 	  # start the server if ruby file executed directly
 	  run! if app_file == $0
--- spec/features/test_infrastructure_work_spec.rb
+++ spec/features/test_infrastructure_work_spec.rb
@@ -1,7 +1,12 @@
 describe "homepage", type: :feature do
-	
-  it "shows up" do
-    visit '/'
-    expect(page).to have_content "Let's play Rock Paper Scissors!" 
+  
+  feature "Player" do
+    scenario "can enter name" do
+      visit '/'
+      fill_in :player, with: "Bob"
+
+      click_button "Play!"
+      expect(page).to have_content "Hi Bob!" 
+    end
   end
 end
