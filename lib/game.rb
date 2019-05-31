e318b50b6d661e456e9524440ed0bc248bc1052e
edwardreeve <edreeve+git@gmail.com>
2019-05-31 18:59:33 +0100

    first refactor of RPS game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 # does the RPS business
 class Game
   attr_reader :player1, :player2, :winner
+  GAME_OPTIONS = ['rock', 'paper', 'scissors']
 
   def initialize(player1, player_class = Player)
     @player_class = player_class
@@ -10,24 +11,14 @@ class Game
   end
 
   def play
-    # w1 = @player1.weapon
-    # w2 = @player2.choose_weapon
-    # player_index = GAME_OPTIONS.index(w1)
-    # winning_choice = options[player_index - 1]
-    # @winner = 'no-one... It\'s a draw!' if w1 == w2
-    # if w2 == winning_choice
-    #   @winner = @player2
-    # else @winner = @player1
-    # end
     w1 = @player1.weapon
     w2 = @player2.choose_weapon
-    if w1 == w2
-      @winner = 'no-one... It\'s a draw'
-    elsif (w1 == 'rock' && w2 == 'scissors') || 
-          (w1 == 'scissors' && w2 == 'paper') ||
-          (w1 == 'paper' && w2 == 'rock')
-      @winner = @player1.name
-    else @winner = @player2.name
+    player_index = GAME_OPTIONS.index(w1)
+    winning_choice = GAME_OPTIONS[player_index - 1]
+    @winner = 'no-one... It\'s a draw!' if w1 == w2
+    if w2 == winning_choice
+      @winner = @player2.name
+    else @winner = @player1.name
     end
   end
 
