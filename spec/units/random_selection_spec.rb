fc434b876b6947e65ccf3bb66021772c73b6e807
JAYDA KADIR <jydkdr@gmail.com>
2019-05-31 16:01:02 +0100

    Refactor unit test for computer selection

--- spec/units/random_selection_spec.rb
+++ spec/units/random_selection_spec.rb
@@ -1,21 +1,21 @@
 require 'random_selection'
 
 describe RandomSelection do
+
+  let(:choose) { RandomSelection.new }
+
   it 'can select rock', focus: true do
     srand(1)
-    r = RandomSelection.new
-    expect(r.computer_choice).to eq("paper")
+    expect(choose.computer_choice).to eq("paper")
   end
 
   it 'can select rock', focus: true do
     srand(2)
-    r = RandomSelection.new
-    expect(r.computer_choice).to eq("rock")
+    expect(choose.computer_choice).to eq("rock")
   end
 
   it 'can select rock', focus: true do
     srand(3)
-    r = RandomSelection.new
-    expect(r.computer_choice).to eq("scissors")
+    expect(choose.computer_choice).to eq("scissors")
   end
 end
