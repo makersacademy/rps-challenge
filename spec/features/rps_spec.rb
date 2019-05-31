717212278b001a84d12eaf418f035b9b186032ac
Megan Goode <megan.e.goode@hotmail.com>
2019-05-31 11:27:40 +0100

    Player presented with choices

--- spec/features/rps_spec.rb
+++ spec/features/rps_spec.rb
@@ -1,8 +1,31 @@
 feature 'Enter player name' do
   scenario 'Can submit a form to check name and display on screen' do
-    visit('/')
-    fill_in 'name', with: 'Player1'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content("Player1")
   end
 end
+
+feature 'Player presented with choices' do
+  scenario 'displays a choice of Rock' do
+    sign_in_and_play
+    expect(page).to have_content("Rock")
+  end
+
+  scenario 'displays a choice of Paper' do
+    sign_in_and_play
+    expect(page).to have_content("Paper")
+  end
+
+  scenario 'displays a choice of Scissors' do
+    sign_in_and_play
+    expect(page).to have_content("Scissors")
+  end
+end
+
+# Test template:
+
+# feature '' do
+#   scenario '' do
+#
+#   end
+# end
