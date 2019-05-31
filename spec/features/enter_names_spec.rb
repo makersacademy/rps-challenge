c5f190bfc8155041fb17ed1c365fd86b15ec8abc
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-05-31 10:41:02 +0100

    Testing for user input name

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,4 +9,12 @@ feature 'testing homepage' do
     # save_and_open_page
     expect(page).to have_content('Testing infrastructure working!')
   end
+
+  scenario 'users inputs name game' do
+    visit('/')
+    fill_in :player_name, with: 'Bob'
+    click_on 'Submit'
+    expect(page).to have_content 'Bob vs. The RPS Champion'
+  end
+
 end
