79dd35e4f9a107588d65b6c8bacb2f732c55b09c
Melanie Rogan <melanie.rogan@ft.com>
2019-06-01 07:04:34 +0200

    leaderboard logic

---
+++ lib/leaderboard.rb
@@ -0,0 +1,28 @@
+class Leaderboard
+  def self.build
+    @leaderboard = Leaderboard.new
+  end
+  
+  def self.instance
+    @leaderboard
+  end
+
+  attr_reader :results
+
+  def initialize
+    @results = []
+  end
+
+  def add(result)
+    @results << result
+  end
+
+  def player_wins(player)
+    results.count(player)
+  end
+
+  def rounds
+    results.length
+  end
+
+end
