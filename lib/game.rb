98c4f84cd4a02199c93f5d8a0f45fdf24c690ef9
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-03 09:28:52 +0100

    Add RPS game logic

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,24 @@
 class Game
 
+  DRAW = "It's a draw!"
+  WIN = "You win!"
+  LOSE = "You lose!"
+
+  RULES = {
+  :rock     => {:rock => DRAW, :paper => LOSE, :scissors => WIN,
+  :paper    => {:rock => WIN, :paper => DRAW, :scissors =>  LOSE},
+  :scissors => {:rock => LOSE, :paper => WIN, :scissors => DRAW}
+  }}
+
+  def computer
+    [:rock, :paper, :scissors].sample
+  end
+
+  def play(p1, computer)
+    RULES[p1][computer]
+  end
 end
+
+# puts play(:rock, :paper)        # :paper
+# puts play(:scissors, :rock)     # :rock
+# puts play(:scissors, :scissors) # :draw
