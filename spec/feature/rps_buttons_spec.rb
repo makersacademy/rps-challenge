10e15d8c3ecacc0a3224d07cf4d7ff9da7198332
Student <student@MAKERSs-MacBook-Pro-5.local>
2019-06-13 14:50:25 +0100

    Rock, Paper, Scissor buttons

---
+++ spec/feature/rps_buttons_spec.rb
@@ -0,0 +1,24 @@
+require './app.rb'
+require './spec/feature/web_helpers.rb'
+
+RSpec.feature 'RPS buttons' do
+
+  scenario 'display rock button' do
+    insert_user_name
+
+    expect(page).to have_button('Rock')
+  end
+
+  scenario 'display paper button' do
+    insert_user_name
+
+    expect(page).to have_button('Paper')
+  end
+
+  scenario 'display scissor' do
+    insert_user_name
+
+    expect(page).to have_button('Scissor')
+  end
+
+end
