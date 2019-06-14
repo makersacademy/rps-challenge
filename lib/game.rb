3f334a3dce1532c5594cd4e11b8f66800273bb70
Alexis Josephides <ajosephides@gmail.com>
2019-06-14 22:44:53 +0100

    added a create player method to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,15 @@
+require_relative './player.rb'
+
 class Game
 
   attr_reader :player
 
-  def initialize(player)
-    @player = player
+  def initialize(player_class= Player)
+    @player_class = player_class
+  end
+
+  def create_player(name)
+    @player = @player_class.new(name)
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,15 @@ require 'game'
 
 describe Game do
 
-  let(:player_double) {double:'player'}
-  subject(:game) { Game.new(player_double) }
+  let(:player_double) { double:'player' }
+  let(:player_class_double) { double:'player_class' }
+  subject(:game) { Game.new(player_class_double) }
 
-  describe '#initialize' do
-    it 'is initialised with a Player' do
+  describe '#create_player' do
+    it 'creates a new instance of player and saves it as an instance variable' do
+      allow(player_class_double).to receive(:new).with('Mama').and_return(player_double)
+      game.create_player('Mama')
       expect(game.player).to eq(player_double)
     end
   end
-
 end
