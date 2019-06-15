72b66521de3338445a2d0bbadd1553a7f2fb295c
Celia <celiafrom12@gmail.com>
2019-06-15 14:18:07 +0100

    game chose between rock, paper and scissors

--- app.rb
+++ app.rb
@@ -16,11 +16,13 @@ class RockPaperScissors < Sinatra::Base
   get '/play' do
     @player_1_name = session[:player_1_name]
     @option = session[:option]
+    @game_option = session[:game_option]
     erb(:play)
   end
 
   post'/play' do
     session[:option] = params[:option]
+    session[:game_option] = :rock
     redirect '/play'
   end
 
--- spec/features/play_a_game_spec.rb
+++ spec/features/play_a_game_spec.rb
@@ -13,8 +13,18 @@ feature 'playing a game' do
     expect(page).to have_button 'Scissors'
   end
 
-  scenario 'choose between rock, paper and scissors' do
+  scenario 'marketeer to choose between rock, paper and scissors' do
     click_button 'Rock'
-    expect(page).to have_content 'You choose Rock'
+    expect(page).to have_content 'You chose Rock'
+  end
+
+  scenario 'game to choose "Rock"' do
+    click_button 'Rock'
+    message = find(:css, "#opponent").text.strip#retrive an element from page and see content
+    expect(different_options).to include message
+  end
+
+  def different_options
+    [:rock, :paper, :scissors].map { |option| "Opponent chose #{option.to_s.capitalize}"}
   end
 end
