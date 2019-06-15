142df656e22ca0dc4916584c3f3816600be0e7d4
Danielandro <mtme13@hotmail.com>
2019-06-15 20:32:10 +0100

    Add web helper to register name & start game

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -1,8 +1,6 @@
 feature "Enter player's name" do
   scenario "Player registers name before playing game" do
-    visit "/"
-    fill_in(:player, with: "Tolu")
-    click_button "Play"
+    register_name_and_play
     expect(page).to have_content("Welcome to the game Tolu")
   end
 end
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,9 +1,6 @@
 feature "Player can choose option" do
   scenario "Player can choose between Rock, Paper or Scissors" do
-    visit "/"
-    fill_in(:name, with: "Tolu")
-    click_button "Play"
-    
+    register_name_and_play    
     page.has_link?("Rock")
     page.has_link?("Paper")
     page.has_link?("Scissors")
