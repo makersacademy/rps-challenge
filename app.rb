581e98a935d94de892db0306383bf7339f4c7bce
Ruth Baker <ruthmoog@gmail.com>
2019-06-15 14:10:26 +0100

    Enable sessions

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    erb(:names)
+    session[:player_1] = params[:player_1]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    erb(:play)
   end
 
   # this will start the server automatically if the app.rb file is run in the
