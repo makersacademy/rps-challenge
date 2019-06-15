f5c053545e60d9501fe4652d522e4db633adcf77
Student <kaczanowskamonika@gmail.com>
2019-06-15 16:22:18 +0100

    players can input their names

--- app.rb
+++ app.rb
@@ -6,4 +6,15 @@ class RPS < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/names' do
+    @names=params[:names]
+    erb :names
+  end
+
+
+
+
+  run! if app_file == $0
+
 end
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,6 +1,13 @@
 feature 'RPS' do
   scenario 'welcome player on the homepage' do
     visit('/')
-    expect(page).to have_content "Hello there,  player! \n welcome to the RPS game, please fill in your name"
+    expect(page).to have_content "Hello there,  player! \n welcome to the RPS game, please fill in your name down below:"
+  end
+
+  scenario 'player fills in the name form' do
+    visit('/')
+    fill_in :names, with: "Jane"
+    click_button 'Submit'
+    expect(page).to have_content "Hi, Jane! Excited to have you here!"
   end
 end
