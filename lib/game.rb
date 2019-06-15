76b5dd58122704bfde24642369070986b898ed66
Alexis Josephides <ajosephides@gmail.com>
2019-06-15 19:53:43 +0100

    initialise game (for now) with ai opponent

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,13 @@
 require_relative './player.rb'
+require_relative './ai.rb'
 
 class Game
 
-  attr_reader :player
+  attr_reader :player, :player2
 
-  def initialize(player_class= Player)
+  def initialize(player_class= Player, player2 = Ai)
     @player_class = player_class
+    @player2 = player2.new
   end
 
   def create_player(name)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,13 +4,21 @@ describe Game do
 
   let(:player_double) { double:'player' }
   let(:player_class_double) { double:'player_class' }
-  subject(:game) { Game.new(player_class_double) }
+  let(:ai_double) { double:'ai' }
+  let(:ai_class_double) { double:'ai_class', new: ai_double }
+  subject(:game) { Game.new(player_class_double, ai_class_double) }
 
   describe '#create_player' do
     it 'creates a new instance of player and saves it as an instance variable' do
       allow(player_class_double).to receive(:new).with('Mama').and_return(player_double)
       game.create_player('Mama')
       expect(game.player).to eq(player_double)
+    end
+  end
+
+  describe '#initialize' do
+    it 'games starts with with a computer oppenent by default' do
+      expect(game.player2).to eq(ai_double)
     end
   end
 end
