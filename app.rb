172dabdd9d3dafbba6992595a32649a52612ec52
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-15 12:23:53 +0100

    Feature Test 1: Enter Names PASS

--- app.rb
+++ app.rb
@@ -3,9 +3,14 @@ require 'sinatra/base'
 class RPS < Sinatra::Base
 
   get '/' do
-    'Hello RPS'
+    erb :index
   end
-  
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    erb :play
+  end
+
   run! if app_file == $0
 
 end
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature "Enter Name" do
+  scenario "player submits name" do
+    visit ('/')
+
+    fill_in :player_1_name, with: "Rachel"
+    click_button "Submit"
+    expect(page).to have_content "Rachel vs. Computer"
+  end
+end
