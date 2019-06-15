6fda3c6dfab4b3c04df5b105b6c033e2435d5ed8
Ruth Baker <ruthmoog@gmail.com>
2019-06-15 13:55:46 +0100

    Include user stories with feature tests

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,4 +1,8 @@
-describe "Add a player", type: :feature do
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+
+describe "Register name before playing game", type: :feature do
   it "accepts a player name in a form" do
     visit '/'
     fill_in('player_1', with: 'Dwayne Johnson')
@@ -6,3 +10,7 @@ describe "Add a player", type: :feature do
     expect(page).to have_content 'Dwayne Johnson V Computer'
   end
 end
+
+# As a marketeer
+# So that I can enjoy myself away from the daily grind
+# I would like to be able to play rock/paper/scissors
