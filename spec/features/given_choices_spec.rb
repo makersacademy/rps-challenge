c1436a57b0459076988197b26eae19184c492164
LucyMHall <lucymhall1995@gmail.com>
2019-06-15 08:13:41 +0100

    Add web helper document with sign_in_and_play method

--- spec/features/given_choices_spec.rb
+++ spec/features/given_choices_spec.rb
@@ -1,13 +1,12 @@
 require "././app.rb"
+require_relative 'web_helpers.rb'
 
 feature "given_choices" do
   scenario "lists Rock, Paper, Scissors" do
-    visit ('/')
-    fill_in :name, with: "Tim"
-    click_on 'submit'
-    click_on 'play_game'
+    sign_in_and_play
     expect(page).to have_content ("Rock")
     expect(page).to have_content ("Paper")
     expect(page).to have_content ("Scissors")
   end
+  scenario ''
 end
---
+++ spec/features/web_helpers_spec.rb
@@ -0,0 +1,6 @@
+def sign_in_and_play
+  visit ('/')
+  fill_in :name, with: "Tim"
+  click_on 'submit'
+  click_on 'play_game'
+end
