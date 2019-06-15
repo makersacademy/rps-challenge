a9da18bd9b0f5c0d6f468631297e05ea2e3c016b
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-15 12:35:37 +0100

    Refactor: Enable Sessions

--- app.rb
+++ app.rb
@@ -2,12 +2,19 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
     erb :play
   end
 
