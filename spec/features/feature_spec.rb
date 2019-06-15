0e80c44334b6ecfc579be3ea4770ee58c0b924cf
Ruth Baker <ruthmoog@gmail.com>
2019-06-15 14:27:41 +0100

    Present player with choice, refactor with web helpers

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -2,15 +2,22 @@
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
 
-describe "Register name before playing game", type: :feature do
-  it "accepts a player name in a form" do
-    visit '/'
-    fill_in('player_1', with: 'Dwayne Johnson')
-    click_button('Submit')
-    expect(page).to have_content 'Dwayne Johnson V Computer'
-  end
-end
-
 # As a marketeer
 # So that I can enjoy myself away from the daily grind
 # I would like to be able to play rock/paper/scissors
+
+describe "Play an online game of Rock Paper Scissors", type: :feature do
+  context "when registering to play" do
+    it "accepts a player name in a form" do
+      sign_in_and_play
+      expect(page).to have_content 'Dwayne Johnson V Computer'
+    end
+  end
+
+  context "when in game" do
+    it "gives a choice of rock or paper or scissors" do
+      sign_in_and_play
+      expect(page).to have_content 'Rock Paper Scissors'
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'features/web_helpers'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
