632e8c283d8d120575b9fbfc6884642182160443
Danielandro <mtme13@hotmail.com>
2019-06-15 17:17:12 +0100

    Enable seesions in controller

--- app.rb
+++ app.rb
@@ -1,18 +1,20 @@
 require 'sinatra'
 
 class Rps < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/name' do    
-    session[:player] = params[:player]
+    session[:player] = params[:player]    
     redirect '/play'
   end
 
-  get '/play' do
+  get '/play' do    
     @player = session[:player] 
+    p "PLAYER: #{@player}"
     erb :play
   end
 end
