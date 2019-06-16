9226560d117e5955fe965b5b1b2ab18681afb4d2
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-06-16 23:04:06 +0100

    plays RPS, logic in controller

--- app.rb
+++ app.rb
@@ -11,17 +11,42 @@ class RpsApp < Sinatra::Base
 
   post '/name' do
     $player = Player.new(params[:player_name])
+    $computer = Player.new("Computer")
     redirect to('/play')
   end
 
   get '/play' do
     @player_name = $player.name
     @player_move = $player.move
+    @computer_move = $computer.move
+    if @player_move == @computer_move
+      @outcome = "It's a draw!"
+    elsif @player_move == "Rock"
+      if @computer_move == "Paper"
+        @outcome = "You lose!"
+      else
+        @outcome = "You win!"
+      end
+    elsif @player_move == "Paper"
+      if @computer_move == "Scissors"
+        @outcome = "You lose!"
+      else
+        @outcome = "You win!"
+      end
+    elsif @player_move == "Scissors"
+      if @computer_move == "Rock"
+        @outcome = "You lose!"
+      else
+        @outcome = "You win!"
+      end
+    end
     erb :play
   end
 
   post '/move' do
     $player.choose(params[:move])
+    computer_move = ["Rock", "Paper", "Scissors"].sample
+    $computer.choose(computer_move)
     redirect to('/play')
   end
 
--- spec/features/battle_time_spec.rb
+++ spec/features/battle_time_spec.rb
@@ -6,17 +6,30 @@ feature 'Play a round of RPS' do
   scenario 'choose Rock' do
     choose_rock
 
-    expect(page).to have_content("Rocky went with Rock")
+    expect(page).to have_content("You went with Rock")
   end
 
-  let(:computer) { double :player }
-
   scenario 'lose if computer chooses Paper' do
-    allow(computer).to receive(:move).and_return("Paper")
+    allow_any_instance_of(Array).to receive(:sample).and_return('Paper')
     choose_rock
-    computer.move
 
     expect(page).to have_content("Computer chose Paper")
     expect(page).to have_content("You lose!")
+  end
+
+  scenario 'win if computer chooses Scissors' do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
+    choose_rock
+
+    expect(page).to have_content("Computer chose Scissors")
+    expect(page).to have_content("You win!")
+  end
+
+  scenario 'draw if computer chooses Rock' do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
+    choose_rock
+
+    expect(page).to have_content("Computer chose Rock")
+    expect(page).to have_content("It's a draw!")
   end
 end
