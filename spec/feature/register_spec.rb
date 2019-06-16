edecc7f1aeaed7b6be9daef4668e91e7ab911371
petraartep <petra.pogacsas@gmail.com>
2019-06-16 11:18:50 +0100

    register a name

---
+++ spec/feature/register_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+feature 'registering name' do
+    scenario 'register and see my name' do
+    visit '/'
+    fill_in :name, with: 'Jane'
+    click_button 'Submit'
+    expect(page).to have_content 'Jane'
+  end
+end
