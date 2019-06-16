807a4d0ed7a9cf427b3482d47736d9dc9090dbb8
petraartep <petra.pogacsas@gmail.com>
2019-06-16 13:53:24 +0100

    added results - win, lose, draw

--- lib/result.rb
+++ lib/result.rb
@@ -1,10 +1,35 @@
 class Result
 
+  RULES = {
+    rock: { rock: :draw, paper: :lose, scissors: :win },
+    paper: { rock: :win, paper: :draw, scissors: :lose },
+    scissors: { rock: :lose, paper: :win, scissors: :draw }
+  }
+
   attr_reader :name, :choice, :computer_choice
 
   def initialize(options)
     @name = options[:name]
     @choice = options[:choice]
     @computer_choice = options[:computer_choice]
+  end
+
+  def win?
+    result == :win
+  end
+
+  def lose?
+    result == :lose
+  end
+
+  def draw?
+    result == :draw
+  end
+
+  private
+
+  def result
+    return if @computer_choice.nil?
+    RULES[@choice][@computer_choice]
   end
 end
--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -3,7 +3,7 @@ require 'result'
 describe Result do
   subject(:result) { described_class.new(options) }
 
-  let(:options) { {name: "Jane", choice: :rock, computer_choice: :scissors} }
+  let(:options) { { name: "Jane", choice: :paper, computer_choice: :scissors } }
 
   describe '#name' do
     it 'returns player name' do
@@ -13,13 +13,41 @@ describe Result do
 
   describe '#choice' do
     it 'returns player choice' do
-      expect(result.choice).to eq :rock
+      expect(result.choice).to eq :paper
     end
   end
 
   describe '#computer_choice' do
     it 'return computer choice' do
       expect(result.computer_choice).to eq :scissors
+    end
+  end
+
+  context 'results' do
+    subject(:win)  { described_class.new(win_game) }
+    subject(:lose) { described_class.new(lose_game) }
+    subject(:draw) { described_class.new(draw_game) }
+
+    let(:win_game) { { name: "Jane", choice: :scissors, computer_choice: :paper } }
+    let(:lose_game) { { name: "Jane", choice: :scissors, computer_choice: :rock } }
+    let(:draw_game) { { name: "Jane", choice: :scissors, computer_choice: :scissors } }
+
+    describe '#win?' do
+      it 'returns true if Player chose Rock and Computer chose Scissors' do
+        expect(win.win?).to eq true
+      end
+    end
+
+    describe '#lose?' do
+      it 'returns true if Player chose Rock and Computer chose Paper' do
+        expect(lose.lose?).to eq true
+      end
+    end
+
+    describe '#draw?' do
+      it 'returns true if Player chose Rock and Computer chose Rock' do
+        expect(draw.draw?).to eq true
+      end
     end
   end
 end
