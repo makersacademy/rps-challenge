253e92737a2fdfeaa3046c1ba88b6147f8a7fa7d
Danielandro <mtme13@hotmail.com>
2019-06-16 19:53:28 +0100

    Refactor to use Player class

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require './lib/computer'
-
+require './lib/player'
 
 class Rps < Sinatra::Base
   enable :sessions
@@ -10,18 +10,18 @@ class Rps < Sinatra::Base
   end
 
   post '/name' do    
-    session[:player] = params[:player]    
+    session[:player] = Player.new(params[:player])
     redirect '/choose_weapon'
   end
 
   get '/choose_weapon' do    
-    @player = session[:player]      
+    @player = session[:player]    
     erb :choose_weapon
   end
 
   get '/play/:weapon' do  
     @player = session[:player]   
-    @player_weapon = params[:weapon]  
+    @player.choose_weapon(params[:weapon])    
     @computer_weapon = Computer.new.weapon
     erb :play
   end  
