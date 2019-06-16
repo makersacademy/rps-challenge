08d3cbcf3cafe9a658444928600a52bfa62e7e94
Student <student@MAKERSs-MacBook-Pro-6.local>
2019-06-17 00:33:44 +0100

    Player can register their name

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'sinatra/base'
+
+class RPS < Sinatra::Base
+  enable :sessions
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    session[:player_name] = params[:player_name]
+    redirect 'start'
+  end
+
+  get '/start' do
+    @player_name = session[:player_name]
+    erb :name
+  end
+
+  run! if app_file == $0
+end
