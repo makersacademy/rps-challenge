2cc05abb0ebc2f84c2bf4b2d06f392749f9965b1
Student <student@MacBook-Pro-5.local>
2019-06-17 01:35:49 +0100

    Players can choose and choice displayed

--- app.rb
+++ app.rb
@@ -3,20 +3,27 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
   enable :sessions
-  
+
   get '/' do
     erb :index
   end
 
   post '/' do
     session[:player_name] = params[:player_name]
-    redirect 'start'
+    redirect '/start'
   end
 
   get '/start' do
     @player_name = session[:player_name]
+    @choice = session[:choice]
     erb :name
   end
+
+  post '/start' do
+    session[:choice] = params[:choice]
+    redirect '/start'
+  end
+
 
   run if app_file == $0
 end
--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -7,4 +7,10 @@ feature 'Player can register name' do
     click_button 'Submit'
     expect(page).to have_content 'Hey, Faisal'
   end
+
+  scenario 'player can choose an option' do
+    visit ('/start')
+    click_button 'ROCK'
+    expect(page).to have_content 'You chose ROCK'
+  end
 end
