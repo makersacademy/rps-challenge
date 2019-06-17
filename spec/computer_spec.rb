762ec33027aab9b857736733e67081bdc7b8c669
joseph townshend <joetownshend@mac.com>
2019-06-17 17:31:36 +0100

    refactored computer unit test with srand

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,10 +1,11 @@
 require 'computer'
 
 describe Computer do
-  let(:computer) { described_class.new }
+  let(:computer) {described_class.new }
   context 'it can play a game of RPS' do
     it 'can choose RPS randomly' do
-      expect(computer.computer_move).to eq(:rock).or eq(:paper).or eq(:scissors)
+      srand(2)
+          expect(computer.computer_move).to eq :rock
     end
   end
 end
