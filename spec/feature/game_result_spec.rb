ad8152e3b0df68e08b0ad3833363172f3a9f9ee4
Luigi <l-palermo@hotmail.it>
2019-06-17 13:55:01 +0100

    Updated random mock

--- spec/feature/game_result_spec.rb
+++ spec/feature/game_result_spec.rb
@@ -3,31 +3,29 @@ require './spec/feature/web_helpers.rb'
 # require "rack_session_access/capybara"
 
 RSpec.feature 'RPS game' do
+  SEED = 647_80
 
   scenario 'button rock return cpu and result' do
     
-    # double_cpu = double(CPU.new)
-    # game = double(Game.new("Player1", double_cpu))
-    # allow(game).to receive(:cpu).and_return(double_cpu)
-    # allow(game).to receive(:random).and_return("Scissor!")
-    # page.set_rack_session(:cpu_input => "Scissor!")
+    srand(SEED)
     insert_user_name
     click_button 'Rock'
 
-    # expect(page).to have_text("Rock\nScissors\nYou Loose!")
-    expect(page).to have_text("Rock")
+    expect(page).to have_text("Rock Scissors You Win!")
   end
 
   scenario 'button paper returns Paper!' do
+    srand(SEED)
     insert_user_name
     click_button 'Paper'
-    expect(page).to have_text("Paper")
+    expect(page).to have_text("Paper Scissors You Loose!")
   end
 
   scenario 'button scissors returns Scissors' do
+    srand(SEED)
     insert_user_name
     click_button 'Scissors'
-    expect(page).to have_text("Scissors")
+    expect(page).to have_text("Scissors Scissors Pair!")
   end
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,12 +7,4 @@ RSpec.describe User do
   it 'returns the selected attribute' do
     expect(subject.get_input(selection)).to eq(selection)
   end
-
-  # it 'returns the score' do 
-  #   expect(subject.score).to eq(10)
-  # end
-
-  # it 'return a score changed by 1' do
-  #   expect { subject.loose }.to change { subject.score }.by(-1)
-  # end 
 end
