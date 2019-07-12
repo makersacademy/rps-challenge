556d3077974a17f953bd511411f112333ced5c2a
Will Howes <williameldenhowes@gmail.com>
2019-07-12 11:37:23 +0100

    First user story: 'I would like to register my name before playing an online game'

--- app.rb
+++ app.rb
@@ -2,10 +2,20 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
 
-  enable :sessions
+  # enable :sessions
 
   get '/' do
     erb :index
+  end
+
+  post '/names' do
+    $player_1_name = params[:player_1_name]
+    redirect('/game')
+  end
+
+  get '/game' do
+    @player_1_name = $player_1_name
+    erb :game
   end
 
   run! if app_file == $0
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -4,6 +4,8 @@
 feature 'Register player names' do
   scenario 'User can enter their name to register for the game' do
     visit('/')
+    fill_in :player_1_name, with: 'Will'
+    click_button 'Submit'
     expect(page).to have_content 'Player 1: Will'
   end
 end
