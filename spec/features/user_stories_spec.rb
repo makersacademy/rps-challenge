da5bb021f0c7aa86b925a714f67c26cee11274c3
Will Howes <williameldenhowes@gmail.com>
2019-07-12 11:04:16 +0100

    First basic test passes

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,7 +1,12 @@
 # As a marketeer
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
-
+feature 'Register player names' do
+  scenario 'User can enter their name to register for the game' do
+    visit('/')
+    expect(page).to have_content 'Player 1: Will'
+  end
+end
 
 # As a marketeer
 # So that I can enjoy myself away from the daily grind
