20a92d61ea713606b6c4066105a084db49af18ed
Will Howes <williameldenhowes@gmail.com>
2019-07-13 16:29:56 +0100

    Refactored some more, amending the RandomWeapon class unit test.

--- spec/random_weapon_spec.rb
+++ spec/random_weapon_spec.rb
@@ -5,7 +5,6 @@ describe RandomWeapon do
   subject(:random_weapon) { described_class.new }
 
   it 'produces a random weapon' do
-    allow(random_weapon).to receive(:rand).and_return(0)
-    expect(random_weapon.weapon).to eq('rock')
+    expect(['rock', 'paper', 'scissors']).to include random_weapon.weapon
   end
 end
