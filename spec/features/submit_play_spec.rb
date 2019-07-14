69b55683adf7895cde77717e9f202629dc7fcb1d
Rianne McCartney <riannemccartney@hotmail.com>
2019-07-14 09:18:50 +0100

    Unit tests fixed and css added

--- spec/features/submit_play_spec.rb
+++ spec/features/submit_play_spec.rb
@@ -2,6 +2,6 @@ feature 'submits players move' do
   scenario "player selects 'rock'" do
     sign_in_and_confirm
     click_button 'Rock'
-    expect(page).to have_content "Rianne selected rock"
+    expect(page).to have_content "Rianne selected Rock"
   end
 end
