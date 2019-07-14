0faa7dbade69a4dc31c1cae0418d3305dd575c07
Rianne McCartney <riannemccartney@hotmail.com>
2019-07-14 12:39:41 +0100

    Tests all passing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,26 +8,27 @@ describe Game do
   it 'accepts one instance of player' do
     expect(game.player).to eq player_double
   end
-end
 
-describe '#result' do
-  it 'returns a draw result' do
-    allow(computer_double).to receive(:computer_turn).and_return(:paper)
-    allow(player_double).to receive(:chosen_item).and_return(:paper)
-    expect(game.result).to eq "It's a draw!"
-  end
 
-  it 'returns a win result' do
-    allow(computer_double).to receive(:computer_turn).and_return(:paper)
-    allow(player_double).to receive(:chosen_item).and_return(:scissors)
-    allow(player_double).to receive(:name).and_return("Rianne")
-    expect(game.result).to eq "Rianne wins!"
-  end
+  describe '#result' do
+    it 'returns a draw result' do
+      allow(computer_double).to receive(:computer_turn).and_return(:paper)
+      allow(player_double).to receive(:chosen_item).and_return(:paper)
+      expect(game.result).to eq "It's a draw!"
+    end
 
-  it 'returns a lose result' do
-    allow(computer_double).to receive(:computer_turn).and_return(:rock)
-    allow(player_double).to receive(:chosen_item).and_return(:scissors)
-    allow(player_double).to receive(:name).and_return("Rianne")
-    expect(game.result).to eq "Rianne loses!"
+    it 'returns a win result' do
+      allow(computer_double).to receive(:computer_turn).and_return(:paper)
+      allow(player_double).to receive(:chosen_item).and_return(:scissors)
+      allow(player_double).to receive(:name).and_return("Rianne")
+      expect(game.result).to eq "Rianne wins!"
+    end
+
+    it 'returns a lose result' do
+      allow(computer_double).to receive(:computer_turn).and_return(:rock)
+      allow(player_double).to receive(:chosen_item).and_return(:scissors)
+      allow(player_double).to receive(:name).and_return("Rianne")
+      expect(game.result).to eq "Rianne loses!"
+    end
   end
 end
