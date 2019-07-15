735b8340fb71b8131b099189b1dd46d085cfc165
iAquino47 <ianaquino47@gmail.com>
2019-07-15 12:51:09 +0100

    fixed buttons

--- lib/result.rb
+++ lib/result.rb
@@ -1,10 +1,10 @@
 class Result
-  WINNERS = [['Scissors', 'Paper'], ['Paper', 'Rock'], ['Rock', 'Scissors']]
-  LOSERS =  WINNERS.map { |i,j| [j,i] }
+  WIN = [['Scissors', 'Paper'], ['Paper', 'Rock'], ['Rock', 'Scissors']]
+  LOSE =  WIN.map { |i,j| [j,i] }
   def result(a, b)
     entry = [a, b]
-    return 'Yay! You have won! 👏🏼' if WINNERS.include?(entry)
-    return 'Pshh. Loser! 🙄' if LOSERS.include?(entry)
+    return 'Yay! You have won! 👏🏼' if WIN.include?(entry)
+    return 'Pshh. Loser! 🙄' if LOSE.include?(entry)
     "It's a draw! 🤝"
   end
 end
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -15,19 +15,19 @@ feature 'I would like to play rock/paper/scissors.' do
   context 'Computer chooses paper' do
     scenario 'User chooses Rock and gives confirmation' do
       visit_fill_name
-      click_button 'Rock'
+      click_button '👊'
       expect(page).to have_text 'You chose Rock'
     end
     scenario "The computers choice ('Paper') is shown" do
       visit_fill_name
       srand(1)
-      click_button 'Rock'
+      click_button '👊'
       expect(page).to have_content("while your opponent chose Paper")
     end
     scenario "The result is shown if user loses." do
       visit_fill_name
       srand(10)
-      click_button 'Rock'
+      click_button '👊'
       expect(page).to have_content('Pshh. Loser! 🙄')
     end
   end
@@ -36,7 +36,7 @@ feature 'I would like to play rock/paper/scissors.' do
     scenario "The result is shown if user wins." do
       visit_fill_name
       srand(3)
-      click_button 'Rock'
+      click_button '👊'
       expect(page).to have_content('Yay! You have won! 👏🏼')
     end
   end
@@ -45,7 +45,7 @@ feature 'I would like to play rock/paper/scissors.' do
     scenario "The outcome is shown if competition results to a draw." do
       visit_fill_name
       srand(2)
-      click_button 'Rock'
+      click_button '👊'
       expect(page).to have_content("It's a draw! 🤝")
     end
   end
@@ -53,13 +53,13 @@ feature 'I would like to play rock/paper/scissors.' do
 
   scenario 'Can click on button representing paper, and see my choice.' do
     visit_fill_name
-    click_button 'Paper'
+    click_button '✋🏼'
     expect(page).to have_text 'You chose Paper'
   end
 
   scenario 'Can click on button representing scissors, and see my choice.' do
     visit_fill_name
-    click_button 'Scissors'
+    click_button '✌🏼'
     expect(page).to have_text 'You chose Scissors'
   end
 end
