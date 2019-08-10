2c8891511f821bf1d663ee1b8b9d4b6ffb2db7ec
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-08-10 17:50:29 +0100

    README updated

--- lib/game.rb
+++ lib/game.rb
@@ -6,11 +6,11 @@ class Game
 
   def who_won(player1, player2)
     if (player1 == "rock" && player2 == "scissors") || (player1 == "scissors" && player2 == "paper") || (player1 == "paper" && player2 == "rock")
-        "You won!"
-      elsif player1 == player2
-        "DRAW!"
-      else
-        "You lost!"
-      end
+      "You won!"
+    elsif player1 == player2
+      "DRAW!"
+    else
+      "You lost!"
+    end
   end
 end
