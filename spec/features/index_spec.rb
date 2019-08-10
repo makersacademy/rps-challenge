68bc8fb0738f55d0dfdcf87f3cf9fa656337fed2
kierepkae <kierepkaedyta@gmail.com>
2019-08-10 14:32:54 +0100

    update

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -14,10 +14,5 @@ scenario 'player can see enter the name and see it on the website' do
   click_button 'play'
   expect(page).to have_content("Anna")
 end
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> 084168625b0e10c4ff953f0319cc8f2ff00a1b17
 end
