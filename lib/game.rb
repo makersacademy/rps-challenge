d338fce3ef159d923994a3a2b47870e943e93226
Guillaume <liard.guillaume@gmail.com>
2019-08-10 12:49:48 +0100

    Test-driven implementation of Game#winner

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,25 @@
 class Game
 
   attr_reader :player1, :player2
-  
+
+  WINNING_TABLE = {
+    :rock => :scissors,
+    :scissors => :paper,
+    :paper => :rock
+  }
+
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2    
+  end
+
+  def winner
+    return "Draw!" if draw?
+    WINNING_TABLE[player1.move] == player2.move ? player1 : player2 
+  end
+
+  def draw?
+    player1.move == player2.move
   end
 
 end
