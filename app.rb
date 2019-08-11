21a429d4b4c2b0646f9a28dcf9be3e4e2767a01b
EvelinaClayton <clayton.evelina@gmail.com>
2019-08-11 12:02:21 +0100

    rock, paper, scissors - methods added and the final result

--- app.rb
+++ app.rb
@@ -17,7 +17,55 @@ class RockPaperScissors < Sinatra::Base
     erb :play
   end
 
+  get '/rock' do 
+    computer_choice = random_move
+    @name = session['name']
+
+    if computer_choice == 'rock'
+      @result = "It's a draw!"
+    elsif computer_choice == 'paper'
+      @result = "You loose!"
+    elsif computer_choice == 'scissors'
+      @result = "You win!"
+    end    
+    
+    erb :play 
+  end
+
+  get '/paper' do
+    computer_choice = random_move
+    @name = session['name']
+
+    if computer_choice == 'paper'
+      @result = "It's a draw!"
+    elsif computer_choice == 'scissors'
+      @result = "You loose!"
+    elsif computer_choice == 'rock'
+      @result = "You win!"
+    end    
+    
+    erb :play 
+  end
   
+  get '/scissors' do
+    computer_choice = random_move
+    @name = session['name']
+
+    if computer_choice == 'scissors'
+      @result = "It's a draw!"
+    elsif computer_choice == 'rock'
+      @result = "You loose!"
+    elsif computer_choice == 'paper'
+      @result = "You win!"
+    end    
+    
+    erb :play 
+  end
+
+  def random_move 
+    choice = ['rock', 'paper', 'scissors']
+    choice.sample
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
