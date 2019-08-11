0f9bb623edf42c097ea6a89ef5cf5d76458e0a51
EvelinaClayton <clayton.evelina@gmail.com>
2019-08-11 10:17:24 +0100

    user can enter their name and sign in

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :welcome
   end
 
-  post '/names' do 
-    "Welcome, #{params["player_name"]}!"
+  post '/names' do
+    session['name'] = params['player_name']
+    redirect '/play'
   end
+
+  get '/play' do
+    @name = session['name']
+    erb :play
+  end
+
+  
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,10 +1,6 @@
-
-
 feature 'Enter name' do
   scenario 'submitting name' do
-    visit('/')
-    fill_in :player_name, with: 'Julian'
-    click_button 'Submit'
-    expect(page).to have_content 'Welcome, Julian!'
+    enter_name_and_submit
+    expect(page).to have_content 'Hello Julian'
   end
 end
