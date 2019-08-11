0b6760af98a56a4ab1e76891ac40864ba9a25692
dtrts <djt210@gmail.com>
2019-08-12 00:41:33 +0100

    bringing tests upto date with name changes

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -5,23 +5,23 @@ describe 'Home page', type: :feature do
   end
 
   it 'has a text box for users name - single player' do
-    expect(page.has_xpath?('//form[@action="/new_single_player_game"]/input[@name="player_name"]')).to eq(true)
+    expect(page.has_xpath?('//form[@action="/new_game"]/input[@name="player_name"]')).to eq(true)
   end
 
   it 'has a button for starting a game - single_player' do
-    expect(page.has_xpath?('.//form[@action="/new_single_player_game"]/button[@type="submit"]')).to eq(true)
+    expect(page.has_xpath?('.//form[@action="/new_game"]/button[@type="submit"]')).to eq(true)
   end
 
   it 'has a text box for users name - two player' do
-    expect(page.has_xpath?('//form[@action="/new_two_player_game"]/input[@name="player_name"]')).to eq(true)
+    expect(page.has_xpath?('//form[@action="/new_game"]/input[@name="player_name"]')).to eq(true)
   end
 
   it 'has a text box for opponents name' do
-    expect(page.has_xpath?('.//form[@action="/new_two_player_game"]/input[@name="opponent_name"]')).to eq(true)
+    expect(page.has_xpath?('.//form[@action="/new_game"]/input[@name="opponent_name"]')).to eq(true)
   end
 
   it 'has a button for starting a game - two player' do
-    expect(page.has_xpath?('.//form[@action="/new_two_player_game"]/button[@type="submit"]')).to eq(true)
+    expect(page.has_xpath?('.//form[@action="/new_game"]/button[@type="submit"]')).to eq(true)
   end
 
 end
