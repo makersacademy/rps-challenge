47f229c7c8d17b341c4c45d4b36b1da388f83891
tuyetngo1 <tuyetngo136@gmail.com>
2019-08-23 10:48:03 +0100

    getname.erb

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 
 class Play < Sinatra::Base
   get '/' do
-    "Let's get a way from the grind! Enter your name to play!" 
+    "Let's get a way from the grind! Enter your name to play!"
+    erb :getname 
   end
 end
