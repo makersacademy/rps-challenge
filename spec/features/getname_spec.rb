7959024ab55bc3de111e848818546965915d201a
tuyetngo1 <tuyetngo136@gmail.com>
2019-08-23 10:56:24 +0100

    red - welcome the player & invite to play

--- spec/features/getname_spec.rb
+++ spec/features/getname_spec.rb
@@ -3,4 +3,11 @@ feature 'Input' do
     visit '/'
     expect(page).to have_content("Let's get a way from the grind! Enter your name to play!")
   end
-end 
+end
+
+feature 'Welcome' do
+  scenario "welcome and introduce the game" do
+  visit '/thegame'
+  expect(page).to have_content("Welcome Tuyet! Let's play Rock, Paper & Scissors today!")
+end
+end
