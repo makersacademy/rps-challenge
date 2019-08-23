ce2970aa29d9c26801b9123ae7759c5f2d6a0b35
claredowson <clare.dowson@barnardos.org.uk>
2019-08-23 12:26:50 +0100

    website displays

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra/base'
+
+class Mama < Sinatra::Base
+
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
