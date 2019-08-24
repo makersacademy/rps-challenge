6becdc3bf178ffac2dd9f5c12db90c020f47c701
Luigi <l-palermo@hotmail.it>
2019-08-24 22:24:31 +0100

    dev: style and score added

--- lib/game.rb
+++ lib/game.rb
@@ -7,30 +7,63 @@ class Game
   def initialize(user, cpu)
     @user = user
     @cpu = cpu
-    @combo = [['rock', 'scissor'], ['paper', 'rock'], ['scissor', 'paper']]
+    @combo = [['Rock', 'Scissors'], ['Paper', 'Rock'], ['Scissors', 'Paper']]
   end
 
   def user_input
     @user.input
   end
 
-  def result(user_input, cpu)
-    return 'Draw!' if user_input == cpu
-    return 'You win!' if @combo.include?(hand(user_input, cpu))
-    return 'You loose!' if !@combo.include?(hand(user_input, cpu))
+  def cpu_input
+    @cpu.random
   end
 
-  def self.instance
-    @game
+  def result
+    reset
+    if !user_input.nil?
+      return 'Draw!' if user_input == @cpu.input
+      return winner if @combo.include?(hand())
+      return looser if !@combo.include?(hand())
+    end
   end
 
   private
 
-  def hand(user_input, cpu)
-    @hand = [user_input, cpu]
+  def reset
+    p @cpu.score
+    if @cpu.score == 0 || @user.score == 0
+      @cpu.reset_score
+      @user.reset_score
+    end
+  end
+
+  def winner
+    cpu_score
+    return 'You win!'
+  end
+
+  def looser
+    user_score
+    return'You loose!' 
+  end
+
+  def cpu_score
+    @cpu.calc_score 
+  end
+
+  def user_score
+    @user.calc_score
+  end
+
+  def hand
+    [user_input, @cpu.input]
   end
 
   def self.create(user, cpu)
     @game = Game.new(user, cpu)
+  end
+
+  def self.instance
+    @game
   end
 end
--- lib/user.rb
+++ lib/user.rb
@@ -1,16 +1,20 @@
 class User
-  attr_reader :name, :input #:score
+  attr_reader :name, :input, :score
 
   def initialize(name)
     @name = name
-    # @score = 10
+    @score = 10
   end
 
   def get_input(selection)
     @input = selection
   end
 
-  # def loose
-  #   @score -= 1
-  # end
+  def calc_score
+    @score -= 1
+  end
+
+  def reset_score
+    @score = 10
+  end
 end
