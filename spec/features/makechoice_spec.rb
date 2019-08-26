9a96d9d4b424c8fe4b951f09125da40efc92cdf4
tuyetngo1 <tuyetngo136@gmail.com>
2019-08-26 16:00:23 +0100

    passed all 3 test for compare_spec.rb

--- spec/features/makechoice_spec.rb
+++ spec/features/makechoice_spec.rb
@@ -1,7 +1,9 @@
 feature 'choose' do
   scenario "player can choose among 3 options" do
+  visit '/'
+  enter_name_and_play
   visit '/thegame'
-  click_button 'Rock'
-  expect(page).to have_content("vs.")
+  click_button 'ROCK'
+  expect(page).to have_content("Tuyet vs. Computer")
 end
 end
