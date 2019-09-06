33f78f18279f63bbd72640dd6e23e6f5d63c85c4
Ben <ben@benzeng.co.uk>
2019-09-06 14:21:42 +0100

    updated tests to reflect now 1 player

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -3,9 +3,8 @@ feature 'rps_feature tests' do
     scenario 'submitting names returns new game' do
       visit('/')
       fill_in :player1, with: 'Player 1'
-      fill_in :player2, with: 'Player 2'
       click_button "Begin Game"
-      expect(page).to have_content 'Player 1 vs Player 2 Rock Paper Scissors, FIGHT!!'
+      expect(page).to have_content 'Player 1 vs Computer Rock Paper Scissors, FIGHT!!'
     end
   end
 end
