ca82646b099e0aeca938e2d43be511d6c2b99ebe
Ben <ben@benzeng.co.uk>
2019-09-06 16:55:45 +0100

    #attack_with takes optional arguments

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,12 +1,13 @@
 class Computer
-  attr_reader :name, :attack, :possible_attacks
+  attr_reader :attack, :possible_attacks
 
   def initialize
+    @name = "Computer"
     @attack = nil
     @possible_attacks = [:rock, :paper, :scissors]
   end
 
-  def attack_with
+  def attack_with(*)
     @attack = @possible_attacks.sample
   end
 
