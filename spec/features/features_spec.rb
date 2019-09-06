197e6d1f7a314a71e41444bfb6f65530fabb25c6
Ben <ben@benzeng.co.uk>
2019-09-06 11:48:26 +0100

    passed submitting names returns new game

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,11 +1,11 @@
 feature 'rps_feature tests' do
   feature 'Enter names' do
-    scenario 'submitting names' do
+    scenario 'submitting names returns new game' do
       visit('/')
-      fill_in :player_1, with: 'Player 1'
-      fill_in :player_2, with: 'Player 2'
-      click_button 'Submit'
-      expect(page).to have_content 'Player 1 vs Player 2!!'
+      fill_in :player1, with: 'Player 1'
+      fill_in :player2, with: 'Player 2'
+      click_button "Begin Game"
+      expect(page).to have_content 'Player 1 vs Player 2 Rock Paper Scissors, FIGHT!!'
     end
   end
 end
