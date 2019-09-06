97d1c53a517bd871a111a700ddb17e5ba7457fe2
claredowson <clare.dowson@barnardos.org.uk>
2019-09-06 15:04:30 +0100

    rock/paper/scissorsdisplaystill working on button output

--- javascript/jasmine-standalone-3-2/src/RockPaperScissors.js
+++ javascript/jasmine-standalone-3-2/src/RockPaperScissors.js
@@ -1,6 +1,39 @@
+'use strict';
+
 function RockPaperScissors() {
+  this.player2 = "Computer"
 }
-// Player.prototype.play = function(song) {
-//   this.currentlyPlayingSong = song;
-//   this.isPlaying = true;
+// RockPaperScissors.prototype.getPlayer1 = function() {
+//   return this.Player1;
 // };
+RockPaperScissors.prototype.play = function() {
+var compare = function(player1,player2){
+    if(player1===player2){
+        return "No one wins";
+      }
+  if (player1==="Paper"){
+    if(player2==="Rock") {
+    return "Paper wins";
+  }
+  else{
+    return "Scissors wins";
+ }
+}
+  if(player1==="Scissors") {
+    if(player2==="Rock") {
+    return "Rock wins";
+  }
+    else{
+    return "Scissors wins";
+  }
+}
+if(player1==="Rock") {
+  if(player2==="Paper") {
+  return "Paper wins";
+}
+  else{
+  return "Rock wins";
+}
+}
+}
+};
---
+++ javascript/jasmine-standalone-3-2/src/interface.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+$('#Rock').click(function() {
+  console.log("rock");
+  rockPaperScissors.play();
+});
+
+$('#Paper').click(function() {
+  rockPaperScissors.play();
+});
+
+$('#Scissors').click(function() {
+  rockPaperScissors.play();
+});
+});
