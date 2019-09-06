c993e00d4f1098df175013defab45ed27c9543f6
Dan Gyi <daniel.gyi@gmail.com>
2019-09-06 09:21:01 +0100

    Create test framework for game_spec. Need to add tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,40 @@
+describe Game do
+  it 'initializes with 2 new players' do
+  end
+
+  describe '#switch_turns' do
+    it 'outputs different player when #switch_turn is called' do
+    end
+  end
+
+  describe '#both_turns_complete?' do
+    it 'checks that both games are complete' do
+    end
+  end
+
+  describe '#winner' do
+    it 'selects winner based on last 2 elements in array' do
+    end
+  end
+
+  describe '#create' do
+    it 'creates a new Game instance' do
+    end
+  end
+
+  describe '#instance' do
+    it 'returns the instance of @game' do
+    end
+  end
+
+  describe '#current_turn' do
+    it 'describes whose turn it is' do
+    end
+  end
+
+  describe '#off_turn' do
+    it 'describes whose turn it is not' do
+    end
+  end
+
+end
