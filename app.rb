a768730035b3dd02a7b3ce1e6d47319f5d3e8b41
Rafaela <rafalucy3@gmail.com>
2019-09-07 16:23:30 +0100

    Add functionality to save name and redirect to another page

--- app.rb
+++ app.rb
@@ -3,8 +3,20 @@ require 'sinatra/base'
 
 class RPSGame < Sinatra::Base
 
+  enable :session
+
   get '/' do
     erb(:index)
+  end
+
+  post '/name' do
+   @player = params[:player]
+    erb(:play)
+  end
+
+  get '/play' do
+    @player = session[:player]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,6 +1,20 @@
 describe 'website content', type: :feature do
   scenario 'Can run app and check page content' do
     visit '/'
-    expect(page).to have_content 'Rock, Paper and Scissor!'
+    expect(page).to have_content 'Welcome to Rock, Paper and Scissor!'
+  end
+
+  scenario 'display message to user input their names' do
+    visit '/'
+    expect(page).to have_content 'Enter your name to play:'
+  end
+end
+
+describe 'add player name', type: :feature do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in "player", with: 'player'
+    click_button 'Play'
+    expect(page).to have_content 'player'
   end
 end
