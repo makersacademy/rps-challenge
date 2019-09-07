8ffba0b8d7dd1d13edca02c7bc5273c00cd3a1ef
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-07 21:04:55 +0100

    Added button in the results page so that can start the game again

--- spec/features/winning_message_spec.rb
+++ spec/features/winning_message_spec.rb
@@ -4,4 +4,17 @@ feature 'Winning message displayed at the end' do
     sign_in_and_player1_selects
     expect(page).to have_text('Tie! Try again')
   end
+
+  scenario 'the player wins the draw' do
+    srand 2
+    sign_in_and_player1_selects
+    expect(page).to have_content('Player wins')
+  end
+
+  scenario 'the player loses the draw' do
+    srand 3
+    sign_in_and_player1_selects
+    expect(page).to have_content('Computer wins')
+  end
+
 end
