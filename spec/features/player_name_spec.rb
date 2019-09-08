55b47b278e986d694e44590091fe2d13eeeb591c
Mike <mjwalshe@gmail.com>
2019-09-08 22:08:04 +0100

    Add minor font styling

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,6 +1,6 @@
 feature "player name entry" do
   scenario "player name displayed" do
     sign_in_and_play
-    expect(page).to have_content('Pick your move, CHUCK')
+    expect(page).to have_content('Pick your move, Chuck')
   end
 end
