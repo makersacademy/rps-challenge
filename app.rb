2be3a6e8cbc1a0191a03afd9f27730b6fd6674d8
ilianameza <ilianameza530@gmail.com>
2019-09-08 21:08:22 +0100

    Add index and play erb files
    Player can now submit their name.

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
 
-class RPS<Sinatra::Base
+class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    'ROCK PAPER SCISSORS!'
+    erb :index
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/tests_spec.rb
+++ spec/features/tests_spec.rb
@@ -4,3 +4,12 @@ feature 'Testing infrastructure' do
     expect(page).to have_content 'ROCK PAPER SCISSORS!'
   end
 end
+
+feature 'Enter name' do
+  scenario 'will see name in lights' do
+    visit('/')
+    fill_in :player_1_name, with: 'Bobo'
+    click_button 'Submit'
+    expect(page).to have_content 'Bobo get ready for Rock Paper Scissors!'
+  end
+end
