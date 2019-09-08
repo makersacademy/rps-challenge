fe19a6b038ba50915ce5d78ca72e06b0c9a89cef
Daniel Segall <segalld@gmail.com>
2019-09-08 15:19:20 +0100

    Minor code cleanup

--- lib/rules.rb
+++ lib/rules.rb
@@ -5,9 +5,8 @@ class Rules
                           %i[scissors paper]].freeze
 
   def winner(p1, p2)
-    return nil if p1.chosen_move == p2.chosen_move
-
-    WINNING_COMBINATIONS.include?(moves(p1, p2)) ? p1 : p2
+    return nil if draw?(p1, p2)
+    win_for_player1?(p1, p2) ? p1 : p2
   end
 
   def valid_moves
@@ -15,6 +14,14 @@ class Rules
   end
 
   private
+
+  def win_for_player1?(p1, p2)
+    WINNING_COMBINATIONS.include?(moves(p1, p2))
+  end
+
+  def draw?(p1, p2)
+    p1.chosen_move == p2.chosen_move
+  end
 
   def moves(p1, p2)
     [p1.chosen_move, p2.chosen_move]
