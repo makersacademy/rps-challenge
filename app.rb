31f2f13177585579f120e9b5eea2c327b7075cf9
Ben <ben@benzeng.co.uk>
2019-09-08 17:52:43 +0100

    added global variable for Game class, renamed initialise to game.erb

--- app.rb
+++ app.rb
@@ -1,15 +1,33 @@
 require 'sinatra/base'
+require './lib/game.rb'
+require './lib/player.rb'
+require './lib/computer.rb'
 
 class Rps < Sinatra::Base
 enable :sessions
+
+before do
+  @game = Game.the_game
+end
 
 get '/' do
   erb(:index)
 end
 
 post '/initialise' do
-  @player1 = params[:player1]
-  erb(:initialise)
+  player1 = Player.new(params[:player1])
+  player2 = Computer.new
+  @game = Game.create(player1, player2)
+  redirect '/game'
+end
+
+get '/game' do
+
+  erb(:game)
+end
+
+post '/result' do
+  erb(:result)
 end
 
 run! if app_file == $0
