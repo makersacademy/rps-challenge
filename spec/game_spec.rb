81049f744c8651686e2e863c350053292c1af18c
Ben <ben@benzeng.co.uk>
2019-09-08 19:42:06 +0100

    fixed tests after victory message change

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,30 +17,30 @@ describe Game do
 
   describe '#attack' do
     it 'attack with rock' do
-      allow(player1).to receive(:attack_with).and_return(:rock)
-      expect(game.attack(player1,:rock)).to eq(:rock)
+      allow(player1).to receive(:attack_with).and_return(:Rock)
+      expect(game.attack(player1,:Rock)).to eq(:Rock)
     end
   end
 
   describe '#who_won?' do
     it 'returns no one if draw' do
-      allow(player1).to receive(:attack).and_return(:rock)
-      allow(player2).to receive(:attack).and_return(:rock)
+      allow(player1).to receive(:attack).and_return(:Rock)
+      allow(player2).to receive(:attack).and_return(:Rock)
       expect(game.who_won?).to eq("It's a draw!")
     end
 
     it 'returns @player1 if player1 wins (p1 rock p2 scissor)' do
-      allow(player1).to receive(:attack).and_return(:rock)
+      allow(player1).to receive(:attack).and_return(:Rock)
       allow(player1).to receive(:name).and_return('Bob')
-      allow(player2).to receive(:attack).and_return(:scissors)
-      expect(game.who_won?).to eq('Bob')
+      allow(player2).to receive(:attack).and_return(:Scissors)
+      expect(game.who_won?).to eq('Bob won!')
     end
 
     it 'returns @player2 if player2 wins (p1 rock p2 paper)' do
-      allow(player1).to receive(:attack).and_return(:rock)
-      allow(player2).to receive(:attack).and_return(:paper)
+      allow(player1).to receive(:attack).and_return(:Rock)
+      allow(player2).to receive(:attack).and_return(:Paper)
       allow(player2).to receive(:name).and_return('Computer')
-      expect(game.who_won?).to eq('Computer')
+      expect(game.who_won?).to eq('Computer won!')
     end
 
   end
