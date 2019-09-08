ee8f8a18c95287a0f46613a8a51ab36a91fb40be
Ben <ben@benzeng.co.uk>
2019-09-08 17:53:40 +0100

    using accessing @game using instance variable

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,15 @@
 class Game
   attr_reader :player1, :player2, :logic
 
+  def self.the_game
+    @game
+  end
+
+  def self.create(player1, player2)
+    @game = Game.new(player1, player2)
+  end
+
+
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
