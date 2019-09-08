62e3e0a45e42ccd4a1f862bf507b28b9ebc35892
Ben <ben@benzeng.co.uk>
2019-09-08 19:39:43 +0100

    fixed tests after capitalising rock paper and scissors

--- spec/features/computer_spec.rb
+++ spec/features/computer_spec.rb
@@ -4,15 +4,15 @@ describe Computer do
   describe '#attack' do
     it 'to return rock' do
       srand(67810)
-      expect(subject.attack_with).to eq :rock
+      expect(subject.attack_with).to eq :Rock
     end
     it 'to return rock' do
       srand(67808)
-      expect(subject.attack_with).to eq :paper
+      expect(subject.attack_with).to eq :Paper
     end
     it 'to return rock' do
       srand(67809)
-      expect(subject.attack_with).to eq :scissors
+      expect(subject.attack_with).to eq :Scissors
     end
 
   end
