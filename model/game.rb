71ce786938d14ef009cb232c2c9ecb6771c0af55
LeoRoma <xiajtn@gmail.com>
2019-09-26 09:21:58 +0100

    buttons have images

--- model/game.rb
+++ model/game.rb
@@ -20,7 +20,7 @@ class Game
     computer_move = @player_2.move
     return "#{@player_1.name}'s move: #{@player_1.attack}\nCPU's move: #{@player_2.move}\nIt's a Draw".gsub(/\n/, '<br/>') if player_1 == computer_move
     return "#{@player_1.name}'s move: #{@player_1.attack}\nCPU's move: #{@player_2.move}\nYou won".gsub(/\n/, '<br/>') if BEATMAP[player_1] == computer_move
-    "#{@player_1.name}'s move: #{@player_1.attack}\nCPU's move: #{@player_2.move}\nCPU won".gsub(/\n/, '<br/>')
+    "#{@player_1.name}'s move: #{@player_1.attack}\nCPU's move: #{@player_2.move}\nCPU won.gsub(/\n/, '<br/>')"
   end
 
 end
