a81bb01a7af2c5c724cfaf0c396b0abf9d578bf1
robertamangiapane <roberta.mangiapane@gmail.com>
2019-10-04 16:36:46 +0100

    index test pass, form implemented

--- rps_app.rb
+++ rps_app.rb
@@ -3,7 +3,12 @@ require 'sinatra/base'
 # require './lib/game'
 class RPS < Sinatra::Base
   get '/' do
+    erb(:index)
+  end
 
+  post '/names' do
+    # player_1 = Player.new(params[:player_name])
+    erb(:names)
   end
   run! if app_file == $0
 end
