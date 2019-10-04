7bdac62bbe74ec061e644258197615f12551e790
sarahhhayle <sarah.vandervliet@bglgroup.co.uk>
2019-10-04 12:00:00 +0100

    Refactored to redirect to a get request

--- app.rb
+++ app.rb
@@ -1,13 +1,20 @@
 require 'sinatra/base'
 
 class Game < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    "Welcome, #{params[:player_1]}"
+  post '/name' do
+    session[:player_1] = params[:player_1]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    erb(:play)
   end
 
 end
