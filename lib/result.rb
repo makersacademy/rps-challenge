dddf4ddc6038ba43d83ee6e6928290e682bd59bf
sarahhhayle <sarah.vandervliet@bglgroup.co.uk>
2019-10-05 18:27:18 +0100

    Refactor result.rb to remove unnecessary variables

--- lib/result.rb
+++ lib/result.rb
@@ -1,25 +1,18 @@
 class Result
 
-  attr_accessor :computer_wins, :draw, :user_wins, :outcome, :options
-
    def initialize(choice)
      @options = ["Rock", "Scissors", "Paper"]
      @computer_choice = @options.sample
      @user_choice = choice
-     @user_index = @options.index(@user_choice)
-     @computer_index = @options.index(@computer_choice)
    end
 
    def outcome
      if @computer_choice == @user_choice
-       @draw = "You said #{@user_choice}. Computer said #{@computer_choice}. You drew!"
-       @draw
-     elsif  @options[@user_index - 1] == @options[@computer_index] # if the user puts rock this makes the index the same for paper which meams the computer wins
-       @computer_wins = "You said #{@user_choice}. Computer said #{@computer_choice}. Computer won!"
-       @computer_wins
+      "You said #{@user_choice}. Computer said #{@computer_choice}. You drew!"
+     elsif  @options[@options.index(@user_choice) - 1] == @options[@options.index(@computer_choice)] # if the user puts rock this makes the index the same for paper which meams the computer wins
+       "You said #{@user_choice}. Computer said #{@computer_choice}. Computer won!"
      else
-       @user_wins = "You said #{@user_choice}. Computer said #{@computer_choice}. You won!"
-       @user_wins
+       "You said #{@user_choice}. Computer said #{@computer_choice}. You won!"
      end
    end
 end
