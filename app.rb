e0a8dfe9e4e94d5706013b52d515a57f32da021a
Nabille Helal <nabilleh@outlook.com>
2019-10-05 18:05:11 +0100

    Add weapon class and make choice

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require './lib/player'
+require './lib/weapon'
 
 class RPS < Sinatra::Base
 
@@ -7,13 +9,36 @@ class RPS < Sinatra::Base
   end
 
   post '/name' do
-    @player_name = params[:player_name]
-    erb :play
+    $player = Player.new(params[:player_name])
+    redirect '/play'
+    # erb :play
   end
 
   get '/play' do
-    @player_name = params[:player_name]
+    @player_name = $player.name
     erb :play
+  end
+
+  post '/rock' do
+    $weapon = Weapon.new(:rock)
+    redirect '/choice'
+  end
+
+  post '/paper' do
+    $weapon = Weapon.new(:paper)
+    redirect '/choice'
+  end
+
+  post '/scissors' do
+    $weapon = Weapon.new(:scissors)
+    redirect '/choice'
+  end
+
+  get '/choice' do
+    @player_name = $player.name
+    @weapon = $weapon.choice
+    p @weapon
+    erb :choice
   end
 
 end
---
+++ lib/weapon.rb
@@ -0,0 +1,9 @@
+class Weapon
+
+  attr_reader :choice
+
+  def initialize(weapon)
+    @choice = weapon
+  end
+
+end
