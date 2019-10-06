336bbf69f92b7995f7668f0062d034d301a89f35
Melvin Lau <lau.melvin@icloud.com>
2019-10-06 15:11:04 +0100

    Fixed random stubs in computer_move_spec

--- spec/features/computer_move_spec.rb
+++ spec/features/computer_move_spec.rb
@@ -1,23 +1,23 @@
 feature "Computer's move" do
 
   scenario "when computer chooses Rock, result page confirms this" do
+    allow_any_instance_of(Object).to receive(:rand) { 0 }
     sign_in
     click_button "Rock"
-    allow_any_instance_of(Object).to receive(:rand) { 0 }
     expect(page).to have_content "Computer's move: Rock"
   end
 
   scenario "when computer chooses Paper, result page confirms this" do
+    allow_any_instance_of(Object).to receive(:rand) { 1 }
     sign_in
     click_button "Rock"
-    allow_any_instance_of(Object).to receive(:rand) { 1 }
     expect(page).to have_content "Computer's move: Paper"
   end
 
   scenario "when computer chooses Scissors, result page confirms this" do
+    allow_any_instance_of(Object).to receive(:rand) { 2 }
     sign_in
     click_button "Rock"
-    allow_any_instance_of(Object).to receive(:rand) { 2 }
     expect(page).to have_content "Computer's move: Scissors"
   end
 
