98334913b272663db4d38b132f71faa5d599337f
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-06 16:44:11 +0100

    Added win counter

--- lib/game.rb
+++ lib/game.rb
@@ -5,28 +5,46 @@ class Game
   def initialize(player1 = Player.new, player2 = Player.new)
     @player1 = player1
     @player2 = player2
+    @p1score = 0
+    @p2score = 0
   end
 
   def winner
       win_condition
   end
 
+  def player1_score
+    @p1score
+  end
+
+  def player2_score
+    @p2score
+  end
+
   private
 
   def win_condition
     if @player1.final_move == @player2.final_move
+      @p1score +=1
+      @p2score +=1
       "Draw!"
     elsif @player1.final_move == 'rock' && @player2.final_move == 'scissors'
+      @p1score +=1
       "#{@player1.name} wins!"
     elsif @player1.final_move == 'rock' && @player2.final_move == 'paper'
+      @p2score +=1
       "#{@player2.name} wins!"
     elsif @player1.final_move == 'paper' && @player2.final_move == 'rock'
+      @p1score += 1
       "#{@player1.name} wins!"
     elsif @player1.final_move == 'paper' && @player2.final_move == 'scissors'
+      @p2score += 1
       "#{@player2.name} wins!"
     elsif @player1.final_move == 'scissors' && @player2.final_move == 'paper'
+      @p1score += 1
       "#{@player1.name} wins!"
     elsif @player1.final_move == 'scissors' && @player2.final_move == 'rock'
+      @p2score += 1
       "#{@player2.name} wins!"
     end
   end
