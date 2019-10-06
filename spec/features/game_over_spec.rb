fe6449b7e8b999df2baa436822836381d26a5d78
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-10-06 11:17:15 +0100

    champion declared when a players score = 2

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -1,13 +1,10 @@
 feature 'Game ends when player score reaches 2' do
   scenario 'player 1 wins round' do
     play_single
-    2.times { player1_wins }
+    player1_wins
+    choose(option: "Rock")
+    allow_any_instance_of(Computer).to receive(:weapon).and_return('Scissors')
+    click_button('Play')
     expect(page).to have_button 'Play Again'
   end
-
-  # scenario 'player 2 wins round' do
-  #   play_single
-  #   player2_wins
-  #   expect(page).to have_content '0-1'
-  # end
 end
