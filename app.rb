938dc76869b60097e515922a39142dad13e6cc96
Student <student@Makerss-MBP.home>
2019-10-06 17:18:52 +0100

    Domain modeling in ReadMe. and basic MVC structure set up

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+require './lib/game'
+
+class RPS < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb :index
+  end
+  # start the server if ruby file executed directly
+  run! if app_file == $PROGRAM_NAME
+end
---
+++ config.ru
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'rack'
+
+require_relative './app'
+run RPS
