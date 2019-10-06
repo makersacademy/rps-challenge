6dc429a2ec2f641418415264d53c410590fe4656
Student <student@Makerss-MBP.home>
2019-10-06 21:52:55 +0100

    refactored code

--- app.rb
+++ app.rb
@@ -1,29 +1,23 @@
 require 'sinatra/base'
 require './lib/game'
-require './lib/player'
 
 class Rps < Sinatra::Base
-  enable :sessions
+
+  before do
+    @game = Game.instance
+  end
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    #session[:player_1_name] = params[:player_1_name]
-    #@player_1_name = session[:player_1_name]
     player_1 = Player.new(params[:player_1_name])
     @game = Game.create(player_1)
     erb :play
   end
 
-  get '/play' do
-    @game = Game.instance
-  end
-
   get '/game_result' do
-    @game = Game.instance
-    @player_1_name = session[:player_1_name]
     erb :game_result
   end
 
