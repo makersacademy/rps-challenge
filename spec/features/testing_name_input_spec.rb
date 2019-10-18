5058e79b04ead66b7de7fe66e29047010f0aff52
Student <Student@Admins-MacBook-Air.local>
2019-10-18 12:33:06 +0100

    Final tests for scenario 1 complete

--- spec/features/testing_name_input_spec.rb
+++ spec/features/testing_name_input_spec.rb
@@ -1,3 +1,10 @@
+feature 'Testing name form loads' do
+  scenario 'check /enter-name route loads' do
+    visit('/enter-name')
+    expect(page).to have_content 'Enter your name below:'
+  end
+end
+
 feature 'Testing name input' do
   scenario 'check page content has Hello Luke' do
     visit('/enter-name')
