e1dda34681dcaa1591a60ca506b123867241e422
albie-b <55197619+albie-b@users.noreply.github.com>
2019-10-21 11:13:43 +0100

    fixed a rubocop error

--- spec/feature/rps_spec.rb
+++ spec/feature/rps_spec.rb
@@ -21,7 +21,7 @@ feature 'can play rps' do
     expect(page).to have_content 'Dave wins'
   end
   scenario 'player 1 enter rock, player_2 enters nothing' do # should give random answer if input if nil
-    srand(67809)
+    srand(67_809)
     sign_in_and_play
     fill_in :player_1_card, with: 'rock'
     fill_in :player_2_card, with: ''
