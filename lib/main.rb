77a28631d686e84302e1d4a672f4c9c064530b10
albie-b <55197619+albie-b@users.noreply.github.com>
2019-10-22 09:07:22 +0100

    moved function into class

--- lib/main.rb
+++ lib/main.rb
@@ -7,7 +7,11 @@ class Check
   end
 
   def confirm
-    checker
+    if @options[@player_1_card] == @player_2_card
+      "#{@player_2_name} wins"
+    elsif @options[@player_2_card] == @player_1_card
+      "#{@player_1_name} wins"
+    else "Tie" end
   end
 
 end
