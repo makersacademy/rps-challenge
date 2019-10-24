986f1c9d03c928ca7cb476f4134e1e7621e6142e
Student <Student@Admins-MacBook-Air.local>
2019-10-24 11:22:20 +0100

    Game fucntionally complete

--- lib/app.rb
+++ lib/app.rb
@@ -4,6 +4,7 @@ class Rps < Sinatra::Base
 
   get '/' do
     'Test!'
+    redirect '/enter-name'
   end
 
   get '/enter-name' do
@@ -15,4 +16,55 @@ class Rps < Sinatra::Base
     erb :display
   end
 
+  before do
+    computer = Computer.new
+    @com_value = computer.computer_choice
+    @outcome = "-"
+  end
+
+  post '/rock' do
+
+    if @com_value == "Rock"
+      @outcome = "Its a draw"
+    elsif @com_value == "Paper"
+      @outcome = "You Loose!"
+    elsif @com_value == "Scissors"
+      @outcome = "You win!"
+    end
+
+    erb :rock
+  end
+
+  post '/paper' do
+
+    if @com_value == "Rock"
+      @outcome = "You win!"
+    elsif @com_value == "Paper"
+      @outcome = "Its a draw!"
+    elsif @com_value == "Scissors"
+      @outcome = "You Loose!"
+    end
+
+    erb :paper
+  end
+
+  post '/scissors' do
+
+    if @com_value == "Rock"
+      @outcome = "You Loose!"
+    elsif @com_value == "Paper"
+      @outcome = "You Win!"
+    elsif @com_value == "Scissors"
+      @outcome = "Its a draw!"
+    end
+
+    erb :scissors
+  end
+end
+
+
+class Computer
+  def computer_choice
+    @choice = ["Rock", "Paper", "Scissors"].sample
+  end
 end
--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Testing' do
-  scenario 'check page content' do
-    visit('/')
-    expect(page).to have_content 'Test!'
-  end
-end
