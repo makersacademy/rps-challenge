d8a38da253f79806416eaa48993dde87a2a194d1
adam robinson <adam.james.robinson@hotmail.co.uk>
2019-10-24 11:10:27 +0100

    corrected rspec error

--- spec/features/choosing_a_move_spec.rb
+++ spec/features/choosing_a_move_spec.rb
@@ -3,19 +3,19 @@ feature 'choose an option' do
     scenario 'the player chooses rock' do
       sign_in_and_press_submit
       click_button "Choose Rock"
-      expect(page).to have_content "U choose rock"
+      expect(page).to have_content "You choose rock"
     end
 
     scenario 'the player chooses paper' do
       sign_in_and_press_submit
       click_button "Choose Paper"
-      expect(page).to have_content "U choose paper"
+      expect(page).to have_content "You choose paper"
     end
 
     scenario 'the player chooses scissors' do
       sign_in_and_press_submit
       click_button "Choose Scissors"
-      expect(page).to have_content "U choose scissors"
+      expect(page).to have_content "You choose scissors"
     end
   end
-end 
+end
