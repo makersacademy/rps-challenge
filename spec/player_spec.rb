7cbcfa1d40343fe2cca5236273e688c71a754ebb
Rhys <mrevinz@gmail.com>
2019-10-31 10:26:49 +0000

    tested all edge cases

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -17,4 +17,14 @@ describe Player do
         subject.MakeMove("Rock")
         expect(subject.CheckWon("Scissors")).to eq true
     end
+
+    it "should allow the player to lose" do
+        subject.MakeMove("Rock")
+        expect(subject.CheckWon("Paper")).to eq false
+    end
+
+    it "should allow the player to draw" do
+        subject.MakeMove("Scissors")
+        expect(subject.CheckWon("Scissors")).to eq "Draw"
+    end
 end
