f686119892b376c8459479ee3c555f956a1e54a9
liatmoss <liatmoss@hotmail.com>
2019-10-31 10:31:24 +0000

    Changed some styling on choose page

--- spec/features/choose_spec.rb
+++ spec/features/choose_spec.rb
@@ -2,6 +2,6 @@ feature 'Choose option' do
   scenario 'choose rock, paper or scissors' do
     sign_in_to_play
     click_button "Click here to continue"
-    expect(page).to have_content 'Dave choose Rock, Paper or Scissors'
+    expect(page).to have_content 'Dave choose rock, paper or scissors'
   end
 end
