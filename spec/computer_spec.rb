9456fd6cd750a2a7b7e8aacaea3da38be1cc5dbf
Rhys <mrevinz@gmail.com>
2019-10-31 10:07:44 +0000

    written computer test

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -3,5 +3,8 @@ require './src/Computer'
 
 describe Computer do
     subject {Computer.new}
-    
+
+    it "should allow the computer to auto generate a move" do
+        expect(subject.move).not_to eq nil
+    end
 end
