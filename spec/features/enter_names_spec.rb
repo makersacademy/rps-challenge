6a5a24780413d937f358ad4c0ca39750ef950f65
liatmoss <liatmoss@hotmail.com>
2019-10-31 10:11:36 +0000

    added web helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter name' do
   scenario 'Submit name' do
-    visit('/')
-    fill_in :player_name, with: "Dave"
-    click_button 'Submit'
+    sign_in_to_play
     expect(page).to have_content 'Welcome Dave!'
   end
 end
