f473451d73a04aa93fabeed5686dc63533884f17
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-01 11:30:34 +0000

    refactored app.rb to create instance of class when submitting name

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 class RockPaperScissors < Sinatra::Base
     enable :sessions
@@ -8,12 +9,12 @@ class RockPaperScissors < Sinatra::Base
     end
 
     post '/enter_names' do
-        @player_1_name = params[:player_1]
-        erb :play
+        $player_1 = Player.new(params[:player_1])
+        redirect :play
     end
 
     get '/play' do
-        @player_1_name = session[:player_1]
+        @player_1_name = $player_1.name
         erb :play
     end
 
