795a7f1ec5bab61f258553bcb51d60d05eb45689
Rob Baile <robbaile12@outlook.com>
2019-11-01 13:31:38 +0000

    finished game to say who has won the game

--- app.rb
+++ app.rb
@@ -28,6 +28,39 @@ class RockPaperScissors < Sinatra::Base
     get '/result' do 
         @player_move = session[:player_move]
         @computer_move = session[:computer_move]
+        if player_rock_win? || player_paper_win? || player_scissors_win?
+            @result = "Player"
+        elsif computer_rock_win? || computer_rock_win? || computer_rock_win?
+            @result = "Computer"
+        else
+            @result = "Draw"
+        end
+        @result = 
         erb(:result)
+    end
+
+
+    def player_rock_win?
+        @player_move == "rock" && @computer_move == "scissors"
+    end
+
+    def player_paper_win?
+        @player_move == "paper" && @computer_move == "rock"
+    end
+
+    def player_scissors_win?
+        @player_move == "scissors" && @computer_move == "paper"
+    end
+
+    def computer_rock_win?
+        @computer_move == "rock" && @player_move == "scissors"
+    end
+
+    def computer_paper_win?
+        @computer_move == "paper" && @player_move == "rock"
+    end
+
+    def computer_scissors_win?
+        @computer_move == "scissors" && @player_move == "paper"
     end
 end
