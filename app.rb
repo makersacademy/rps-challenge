cde262fb54657173e6aee9750a295c755a90c0c5
slongboro <susanlongley@gmail.com>
2019-11-01 11:31:39 +0000

    added players feature test and code

--- app.rb
+++ app.rb
@@ -2,9 +2,17 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base
     get '/' do
-
-    "Hello Rock Paper Scissors"
+        erb :index
     end
 
+
+      post '/names' do
+        @player_1_name = params[:player_1_name]
+        @player_2_name = params[:player_2_name]
+        erb :play
+      end 
+
     run! if app_file == $0
+
+
 end
---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'Enter names' do
+    scenario 'submitting names' do
+      visit('/')
+      fill_in :player_1_name, with: 'Ruby'
+      fill_in :player_2_name, with: 'Computer'
+      click_button 'Submit'
+      expect(page).to have_content 'Ruby vs. Computer'
+    end
+  end
