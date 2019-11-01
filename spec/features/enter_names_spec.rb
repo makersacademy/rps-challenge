df69fb133fdd38d7c1dfa6f30a48aa180b2838de
slongboro <susanlongley@gmail.com>
2019-11-01 16:06:39 +0000

    not sure what changed

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,7 +2,6 @@ feature 'Enter names' do
     scenario 'submitting names' do
       visit('/')
       fill_in :player_1_name, with: 'Ruby'
-      #fill_in :player_2_name, with: 'Computer'
       click_button 'Submit'
       expect(page).to have_content 'Ruby vs. Computer'
     end
