cc803cc74680a041318f6ee0a24ad33e3463896b
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-01 11:03:18 +0000

    added buttons for user to select

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,8 @@
 feature 'Enter names' do
     scenario 'submitting names' do
         visit('/')
-        fill_in :player_1, with: 'Jade'
-        fill_in :player_2, with: 'Stu'
+        fill_in :player_1, with: 'Khan'
         click_button 'Submit'
-        expect(page).to have_content 'Jade vs. Stu'
+        expect(page).to have_content 'Khan vs. CPU'
     end
 end
