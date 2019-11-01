f710fd86fa72660ee5d1077668dcad394a1a0d8d
krisn64209 <krisnorris@gmail.com>
2019-11-01 11:13:10 +0000

    Refactored

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
@@ -8,7 +10,15 @@ class RPS < Sinatra::Base
   run! if app_file == $0
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
+    #takes name entered, & stores it in the session
+    #then redirects to /play
+    session[:player_1_name] = params[:player_1_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    #takes name previously stored in session, and passes it to variable
+    @player_1_name = session[:player_1_name]
     erb :play
   end
 
