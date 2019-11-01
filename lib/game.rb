ec32391968502a935a8185c942d219a81de2a046
Rob Baile <robbaile12@outlook.com>
2019-11-01 09:05:48 +0000

    got player taking move test to pass

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,10 @@
 class Game
-    attr_reader :player_name
+    attr_reader :player_name, :player_move
     def initialize(player_name)
         @player_name = player_name    
+    end
+
+    def take_go(move)
+        @player_move = move        
     end
 end
