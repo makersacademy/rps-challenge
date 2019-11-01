4d84b23c31f04d9f1bc1c160ace5f481697676d4
Student <student@Admins-MacBook-Air.local>
2019-11-01 10:19:15 +0000

    can add player 1 name before playing

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require "sinatra/base"
 
 class Rps < Sinatra::Base
   get "/" do
-    "Testing infrastructure working!"
+    erb :index
+  end
+
+  post "/names" do
+    @player_1_name = params[:player_1_name]
+    erb :play
   end
 
   run! if app_file == $0
