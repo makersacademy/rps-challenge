1104a6ab500d982e33c1bf8d520e7610e7b87c4c
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-01 10:25:32 +0000

    test to check that names are returned

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,5 +4,6 @@ feature 'Enter names' do
         fill_in :player_1, with: 'Jade'
         fill_in :player_2, with: 'Stu'
         click_button 'Submit'
+        expect(page).to have_content 'Jade vs. Stu'
     end
 end
