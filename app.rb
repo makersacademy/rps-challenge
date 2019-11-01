54e47a1cfaf1d6b2c3f171127f1201b7cc0f1a11
jtsalva <tim@jtsalva.dev>
2019-11-01 12:50:52 +0000

    add /play

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require 'sinatra/base'
+require_relative 'lib/game'
+require_relative 'lib/player'
 
 class RPS < Sinatra::Base
   enable :sessions
 
   get '/' do
     erb :index
+  end
+
+  post '/sign-in' do
+    name = params[:name]
+
+    session[:name] = name
+    player = Player.new(name)
+    bot = Player.new('Bot')
+    session[:game] = Game.new(player, bot)
+
+    redirect '/play'
+  end
+
+  get '/play' do
+    @game = session[:game]
+    @this_player = @game.player_one
+    @other_player = @game.player_two
+    erb :play
   end
 end
