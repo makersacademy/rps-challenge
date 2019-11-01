e7a35d2aabd8a866e7ae7749f403f7f197063764
slongboro <susanlongley@gmail.com>
2019-11-01 11:38:43 +0000

    refactored teat and code for player v computer

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class RPS < Sinatra::Base
 
       post '/names' do
         @player_1_name = params[:player_1_name]
-        @player_2_name = params[:player_2_name]
+        #@player_2_name = params[:player_2_name]
         erb :play
       end 
 
