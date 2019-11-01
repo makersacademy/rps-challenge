22f05a4181474ed041600c755ef5f3831af01e72
DMartinBGL <david.martin@bglgroup.co.uk>
2019-11-01 13:53:45 +0000

    passing class init test

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -3,6 +3,6 @@ feature 'Enter names' do
     visit ('/')
     fill_in :player_1_name, with: 'Dave'
     click_button 'Submit'
-    expect(page).to have_content "Registered!"
+    expect(page).to have_content "Dave"
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,5 @@
+require 'game'
+
 describe Game do
   it 'creates an instance of game' do
     expect(subject).to be_an_instance_of Game
