6623224bdf496dbb583f3e18e77e7f9ea4cb6bd8
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-01 11:28:05 +0000

    tests pass

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,13 @@
 require 'player'
 
 describe Player do
+    subject(:player) { Player.new('player') }
+
     it 'creates an instance of the player class' do
         expect(subject).to be_an_instance_of(Player)
+    end
+
+    it 'allows user to enter their name' do
+        expect(subject.name).to eq 'player'    
     end
 end
