b5ce006f1e7a1807f9a18cdcb292dfe57ed2b59e
slongboro <susanlongley@gmail.com>
2019-11-01 15:34:06 +0000

    changed the test accept user input, to pass

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,17 @@
 class Game
-    attr_reader :selection
+    attr_reader :selection, :input
+    @game = Game.new
 
     def initialize
+
         @input
+        @game
+        @selection
     end
 
-    def selection
-        input = "Rock"
+    def selection(input)
+        @input = input
+      
     end
 end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,12 @@
 require 'game'
 
 describe Game do
+    before :each do
+    @game = Game.new
+    end
+
  it "should accept user input of either rock paper scissors" do
-    game = Game.new
-    expect(game.selection).to eq("Rock")
+    @game.selection("Rock")
+    expect(@game.selection("Rock")).to eq("Rock")
  end
 end
