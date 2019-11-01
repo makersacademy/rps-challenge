0d5befed67c1851b13c628ac05b4f4c7455c9f16
Joe Ball <joe.ball@bglgroup.co.uk>
2019-11-01 14:39:31 +0000

    added styling, and tests for filling in names

--- spec/features/play_rps_spec.rb
+++ spec/features/play_rps_spec.rb
@@ -1,10 +1,10 @@
-# require "./spec/features/web_helpers"
+require "./spec/features/web_helpers"
 
-# feature "Select rock, paper, or scissors" do
-#   #   let(:player_1) { double(:player, name: "Bowie") }
+feature "Select rock, paper, or scissors" do
+  #   let(:player_1) { double(:player, name: "Bowie") }
 
-#   scenario "#names entered" do
-#     log_names
-#     expect(page).to have_content "Player 1: Bowie vs. Player 2: Computer"
-#   end
-# end
+  scenario "#names entered" do
+    log_names
+    expect(page).to have_content "Player 1: Bowie vs. Player 2: Computer"
+  end
+end
