78633d162b2b8acf2c034c4ae5b7f8cc18685100
jtsalva <tim@jtsalva.dev>
2019-11-01 12:50:21 +0000

    add sign in spec

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,8 +2,8 @@ feature 'sign in' do
   let(:name) { 'Bob Ross' }
 
   scenario 'player enters name' do
-    sign_in
+    sign_in(name)
 
-    expect(page.find('#this-player .name').to have_content name)
+    expect(page.find('#this-player .name')).to have_content name
   end
 end
