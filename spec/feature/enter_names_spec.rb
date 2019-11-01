81e1d27bce8709838ca0f3ad8af4ee061ab85bb9
Marc Blenkinsop <marcblenky@hotmail.com>
2019-11-01 09:41:34 +0000

    wrote first test, currently failing

---
+++ spec/feature/enter_names_spec.rb
@@ -0,0 +1,10 @@
+require "./app.rb"
+
+feature "the player can enter their name" do
+  scenario "the user can see their name displayed" do
+    visit("/")
+    fill_in :player_name, with: "Marc"
+    click_button "Submit"
+    expect(page).to have_content "Marc"
+  end
+end
