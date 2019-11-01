48d2592926fc4e4d439ea5f8a446b6756badafcf
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-11-01 11:33:33 +0000

    refactor POST request to a redirect loop

--- app.rb
+++ app.rb
@@ -10,10 +10,15 @@ class RPS < Sinatra::Base
   end
 
   post "/name" do
-    @player_1_name = params[:player_1_name]
-    erb :start_game
+    session[:player_1_name] = params[:player_1_name]
+    redirect "/start_game"
   end
 
+  get "/start_game" do
+    @player_1_name = session[:player_1_name]
+    erb :start_game
+  end
+  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
