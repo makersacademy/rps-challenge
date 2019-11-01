00149ec8c887ba6f995143bcfd0c60d5c4e5b89f
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-11-01 14:30:42 +0000

    frobnicating with styles

--- app.rb
+++ app.rb
@@ -18,7 +18,18 @@ class RPS < Sinatra::Base
     @player_1_name = session[:player_1_name]
     erb :start_game
   end
-  
+
+  post "/choice" do
+    @player_1_name = session[:player_1_name]
+    session[:player_1_choice] = params[:player_1_choice]
+    redirect "/result"
+  end
+
+  get "/result" do
+    @player_1_name = session[:player_1_name]
+    @player_1_choice = session[:player_1_choice]
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
