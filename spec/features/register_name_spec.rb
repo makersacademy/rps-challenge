c04b4a3a4a2a5b2555f16096cc721f32611543cb
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-11-01 11:39:15 +0000

    add web helpers

--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -3,9 +3,7 @@ feature "Register name" do
   # So that I can see my name in lights
   # I would like to register my name before playing an online game
   scenario "Can enter a name and see name" do
-    visit("/")
-    fill_in :player_1_name, with: "Batman"
-    click_button "Submit"
+    enter_batman
     expect(page).to have_content "Batman"
   end
 end
--- spec/features/see_opponent_spec.rb
+++ spec/features/see_opponent_spec.rb
@@ -1,11 +1,9 @@
 feature "See opponent" do
-    # As a marketeer
-    # So that I know who I'm up against
-    # I would like to see my opponent
-    scenario "Sees an opponent's name" do
-      visit("/")
-      fill_in :player_1_name, with: "Batman"
-      click_button "Submit"
-      expect(page).to have_content "Joker"
-    end
+  # As a marketeer
+  # So that I know who I'm up against
+  # I would like to see my opponent
+  scenario "Sees an opponent's name" do
+    enter_batman
+    expect(page).to have_content "Joker"
   end
+end
