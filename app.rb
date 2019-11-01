14a79bdd61642153ccc41e63ede6bf392ca09163
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-01 10:23:43 +0000

    made test pass for entering names

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+    enable :sessions
     get '/' do
         erb :index
     end
+
+    post 'enter_names' do
+        @player_1_name = params[:player_1]
+        @player_2_name = params[:player_2]
+        erb :play
+    end
+
+    # get 'play' do
+    #     @player_1_name = session[:player_1]
+    #     @player_2_name = session[:player_2]
+    #     erb :play
+    # end
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,6 @@
-feature 'Testing infrastructure' do
-    scenario 'page returns text in a view' do
-        visit('/')
-        expect(page).to have_content 'This is a page'
-    end
-end
+# feature 'Testing infrastructure' do
+#     scenario 'page returns text in a view' do
+#         visit('/')
+#         expect(page).to have_content 'This is a page'
+#     end
+# end
