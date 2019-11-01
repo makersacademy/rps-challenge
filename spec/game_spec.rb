1206a30a406c08b07b6997f49ed6fbaaa2aa1cd5
Rob Baile <robbaile12@outlook.com>
2019-11-01 08:54:03 +0000

    game should have a player name test written

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,12 @@
 require 'game'
 
 describe Game do 
+    subject { Game.new("Rob") }
     it 'should be an instance of game' do
         expect(subject).to be_an_instance_of(Game)
+    end
+
+    it 'should have a player name attached to it' do 
+        expect(subject.player).to eq "Rob"
     end
 end
