1756b083641e6615cd782cf9ee627d0c940087db
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-01 14:59:23 +0000

    added test to chack that cpu has array of 3 items

--- spec/cpu_spec.rb
+++ spec/cpu_spec.rb
@@ -4,4 +4,8 @@ describe Cpu do
     it 'returns an instance of the Cpu class' do
         expect(subject).to be_an_instance_of(Cpu)
     end
+
+    it 'has a an array of 3 items' do
+        expect(subject.options).to include('rock', 'paper', 'scissors')
+    end
 end
