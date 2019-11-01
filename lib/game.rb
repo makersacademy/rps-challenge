159d9a5e8d1d62d363a035e21274eb85be0ba4dd
DMartinBGL <david.martin@bglgroup.co.uk>
2019-11-01 14:31:19 +0000

    passing test for player instance

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,7 @@
 class Game
+  attr_reader :player1
+
+  def initialize(name)
+    @player1 = name
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,13 @@
 require 'game'
 
 describe Game do
+  subject { Game.new("Dave")}
+
   it 'creates an instance of game' do
     expect(subject).to be_an_instance_of Game
+  end
+
+  it 'expects intance to create a player' do
+    expect(subject.player1).to eq "Dave"
   end
 end
