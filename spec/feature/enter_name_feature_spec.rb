f00f71f19c18fff3206c422a04c5babcf235f4a8
Rob Baile <robbaile12@outlook.com>
2019-11-01 14:17:52 +0000

    added css, couldn't get external file to work

--- spec/feature/enter_name_feature_spec.rb
+++ spec/feature/enter_name_feature_spec.rb
@@ -7,14 +7,14 @@ feature "Rock, Paper, Scissors enter name" do
     scenario 'Can enter name and get name displayed on screen' do 
         visit('/')
         fill_in 'name',  :with => "Rob"
-        click_on 'play'
+        click_on 'Play'
         expect(page).to have_content "Rob vs. computer"
     end
 
     scenario 'Can enter name and get name displayed on screen' do 
         visit('/')
         fill_in 'name',  :with => "Rob"
-        click_on 'play'
+        click_on 'Play'
         click_on 'scissors'
         expect(page).to have_content "You played: scissors"
     end
