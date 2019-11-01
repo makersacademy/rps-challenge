b5e7d12dedc55dedd59f542bf6e82f512dd5268a
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-11-01 10:51:35 +0000

    Test to enter a name passing

--- app.rb
+++ app.rb
@@ -9,6 +9,11 @@ class RPS < Sinatra::Base
     erb :index
   end
 
+  post "/name" do
+    @player_1_name = params[:player_1_name]
+    erb :start_game
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
