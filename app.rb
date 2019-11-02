22aa5bfe11b5e5ccd0aed4c2273b41c2728eafa6
Amy Balmforth <amy.balmforth@gmail.com>
2019-11-02 10:30:52 +0000

    used sessions

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/name' do
-    @player_name = params[:player_name]
-    erb :hello
+    session[:player_name] = params[:player_name]
+    #@player_name = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb :play
   end
 
   run! if app_file == $0
