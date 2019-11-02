3b112763161abb1841d409a52cacc9184d177166
Emma Van Oss <e.vanoss@gmail.com>
2019-11-02 14:42:21 +0000

    clicking on "rock" leads to "play" page

--- spec/features/choose_option_spec.rb
+++ spec/features/choose_option_spec.rb
@@ -5,4 +5,10 @@ feature 'choosing an option' do
     expect(page).to have_selector(:link_or_button, 'Play PAPER')
     expect(page).to have_selector(:link_or_button, 'Play SCISSORS')
   end
+
+  scenario 'choosing an option leads to the "play" page' do
+    sign_in
+    click_button('Play ROCK')
+    expect(current_path).to eq('/play')
+  end
 end
