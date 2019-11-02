ae8d6eb2146e072c6cefd38e2c462c9c3263b80a
Natalie <49326857+natyeo@users.noreply.github.com>
2019-11-02 12:39:47 +0000

    Added enter and submit name functionality

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
-    'Hello RPS!'
+    erb :index
+  end
+
+  post '/name' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -1,6 +1,15 @@
 feature 'testing infrastructure' do
   scenario 'can run app and check page content' do
     visit('/')
-    expect(page).to have_text 'Hello RPS!'
+    expect(page).to have_text 'Welcome to Rock Paper Scissors!'
+  end
+end
+
+feature 'entering names' do
+  scenario 'player can submit name' do
+    visit('/')
+    fill_in :player_name, with: 'Nat'
+    click_button 'Submit'
+    expect(page).to have_text 'Nat'
   end
 end
