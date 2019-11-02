184dffe039c80769a76ffe3d612c3213aeee4a01
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-11-02 17:31:54 +0000

    refactoring

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -3,6 +3,6 @@ feature 'entering a name' do
     visit('/')
     fill_in :player_name, with: 'Dave'
     click_button 'Submit'
-    expect(page).to have_content 'Welcome Dave'
+    expect(page).to have_content 'Welcome Dave!'
   end
 end
