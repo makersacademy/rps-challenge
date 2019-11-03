082a6cad3c2e7fe6210127004568cd4a8e8d7132
Amy Balmforth <amy.balmforth@gmail.com>
2019-11-03 14:14:35 +0000

    green tests with game logic

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,4 +1,4 @@
-feature 'Result of game' do
+feature 'Result of game, player wins' do
 
   scenario 'See computer choice' do
     allow_any_instance_of(Computer).to receive(:choice).and_return('scissors')
@@ -14,6 +14,26 @@ feature 'Result of game' do
     rock_chosen
     click_button('See result')
     expect(page).to have_content 'Amy wins!'
+  end
+
+end
+
+feature 'Result of game, computer wins' do
+
+  scenario 'See computer choice' do
+    allow_any_instance_of(Computer).to receive(:choice).and_return('paper')
+    sign_in_and_play
+    rock_chosen
+    click_button('See result')
+    expect(page).to have_content 'Computer chose paper'
+  end
+
+  scenario 'Player has won' do
+    allow_any_instance_of(Computer).to receive(:choice).and_return('paper')
+    sign_in_and_play
+    rock_chosen
+    click_button('See result')
+    expect(page).to have_content 'Computer wins!'
   end
 
 end
