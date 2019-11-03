e3cfea1f8fadece61cad95d747d779b0c724869d
fahus <faduma.h@yahoo.com>
2019-11-03 16:57:13 +0000

    added rps functionality to pick option

---
+++ lib/game.rb
@@ -0,0 +1,22 @@
+class Game
+  attr_reader :computer_move, :player_move
+
+  def initialize(player_move, computer_move)
+    @player_move = player_move
+    @computer_move = computer_move
+  end
+
+  def winner
+    if @player_move == @computer_move
+      "It's a draw!"
+    elsif (@player_move == 'Rock' && @computer_move == 'Paper') ||
+      (@player_move == 'Scissors' && @computer_move == 'Rock') ||
+      (@player_move == 'Paper' && @computer_move == 'Scissors')
+      'You lose!'
+    elsif (@player_move == 'Paper' && @computer_move == 'Rock') ||
+      (@player_move == 'Rock' && @computer_move == 'Scissors') ||
+      (@player_move == 'Scissors' && @computer_move == 'Paper')
+      'You win!'
+    end
+  end
+end
