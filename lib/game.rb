bbd50d7be69d7a740918f1f14b847b202b3017aa
Student <student@Makerss-MBP.connect>
2019-11-03 23:00:12 +0000

    Fixed Paper/Scissors logic glitch

--- lib/game.rb
+++ lib/game.rb
@@ -9,8 +9,9 @@ class Game
   def compare_weapons(player_weapon)
     return "It's a tie." if player_weapon == @computer_weapon
     return "You win." if player_weapon == "Scissors" && @computer_weapon == "Paper"
+    return "I win." if player_weapon == "Paper" && @computer_weapon == "Scissors"
     return "You win." if player_weapon < @computer_weapon
-
+    
     "I win."
   end
 
