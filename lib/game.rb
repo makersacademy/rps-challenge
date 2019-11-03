66ab71b0dea6f5d6ecdf1be3c27ad8670392eebd
Andy Jones <andytjones@icloud.com>
2019-11-03 19:37:15 +0000

    Removed some useless tests as refactored structure of app.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,5 @@
 class Game
-  attr_reader :player_1, :player_2, :WEAPONS
-
-  WEAPONS = [:rock, :paper, :scissors]
+  attr_reader :player_1, :player_2
 
   def initialize(player_1, player_2)
     @player_1 = player_1
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,19 +2,20 @@ require 'game'
 
 describe Game do
 
-  context 'initial tests' do
-
-    let(:andy) { double('Player') } 
-    let(:sheldon) { double('Computer') }
-    let(:game) { Game.new('Andy', 'Sheldon') }
-
-    it 'should have a list of weapons' do
-      expect(Game::WEAPONS).to eq [:rock, :paper, :scissors] 
-    end
-
+  context 'initial test' do
     it 'player 1 should be Andy' do
+      game = Game.new("Andy", "Sheldon")
       expect(game.player_1).to eq 'Andy'
     end
+  end
+
+  context 'checking game outcomes' do
+    let(:andy_rock) { double('Player', name: "Andy", selection: :rock) } 
+    let(:andy_paper) { double('Player', name: "Andy", selection: :paper) } 
+    let(:andy_scissors) { double('Player', name: "Andy", selection: :scissors) }
+    let(:sheldon_rock) { double('Player', selection: :rock) } 
+    let(:sheldon_paper) { double('Player', selection: :paper) } 
+    let(:sheldon_scissors) { double('Player', selection: :scissors) }
 
   end
 
