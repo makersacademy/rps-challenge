2c58018e17bd666dd03a26c9ecfb48f1e25c56ea
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-11-03 13:25:03 +0000

    refactored game.result logic to remove conditionals

--- lib/game.rb
+++ lib/game.rb
@@ -6,17 +6,27 @@ class Game
 
   def result(player_choice)
     computer_choice = random_move
+    beatmap = { scissors: :paper, paper: :rock, rock: :scissors }
+
     if player_choice == computer_choice
       :draw
-    elsif player_choice == :rock and computer_choice == :scissors
-      :win
-    elsif player_choice == :paper and computer_choice == :rock
-      :win
-    elsif player_choice == :scissors and computer_choice == :paper
+    elsif beatmap[player_choice] == computer_choice
       :win
     else
       :lose
     end
   end
-  
+  #   if player_choice == computer_choice
+  #     :draw
+  #   elsif player_choice == :rock and computer_choice == :scissors
+  #     :win
+  #   elsif player_choice == :paper and computer_choice == :rock
+  #     :win
+  #   elsif player_choice == :scissors and computer_choice == :paper
+  #     :win
+  #   else
+  #     :lose
+  #   end
+  # end
+
 end
