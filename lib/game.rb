a51b6a2e5ada041a946b89a0cfbde638106523bb
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-11-03 13:25:21 +0000

    refactored game.result logic to remove conditionals

--- lib/game.rb
+++ lib/game.rb
@@ -16,17 +16,4 @@ class Game
       :lose
     end
   end
-  #   if player_choice == computer_choice
-  #     :draw
-  #   elsif player_choice == :rock and computer_choice == :scissors
-  #     :win
-  #   elsif player_choice == :paper and computer_choice == :rock
-  #     :win
-  #   elsif player_choice == :scissors and computer_choice == :paper
-  #     :win
-  #   else
-  #     :lose
-  #   end
-  # end
-
 end
