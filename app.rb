df84c9e4bebe6f62c284f4b0a6e3dce7efdff9ee
Donald Newholm <newholm1@hotmail.com>
2019-11-03 21:55:44 +0000

    Game class created. 3/3 100% no offences

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/game'
 
 class RPS < Sinatra::Base
   enable :sessions
@@ -13,41 +14,30 @@ class RPS < Sinatra::Base
   end
 
   get '/choose' do
+    @@game = Game.new
     @player_name = session[:player_name]
     erb :choose
   end
 
   get '/rock' do
     @player_weapon = "Rock"
-    random_weapon
-    @outcome = compare_weapons(@player_weapon, @computer_weapon)
+    @@game.computer_weapon
+    @outcome = @@game.compare_weapons(@player_weapon)
     erb :outcome
   end
 
   get '/paper' do
     @player_weapon = "Paper"
-    random_weapon
-    @outcome = compare_weapons(@player_weapon, @computer_weapon)
+    @@game.computer_weapon
+    @outcome = @@game.compare_weapons(@player_weapon)
     erb :outcome
   end
 
   get '/scissors' do
     @player_weapon = "Scissors"
-    random_weapon
-    @outcome = compare_weapons(@player_weapon, @computer_weapon)
+    @@game.computer_weapon
+    @outcome = @@game.compare_weapons(@player_weapon)
     erb :outcome
-  end
-
-  def random_weapon
-    @computer_weapon = ["Rock", "Paper", "Scissors"].sample
-  end
-
-  def compare_weapons(player_weapon, computer_weapon)
-    return "It's a tie." if player_weapon == computer_weapon
-    return "You win." if player_weapon == "Scissors" && computer_weapon == "Paper"
-    return "You win." if player_weapon < computer_weapon
-
-    "I win."
   end
 
   run! if app_file == $0
