b846230209da82c0590e98cae80a9f7f263a6506
Donald Newholm <newholm1@hotmail.com>
2019-11-03 23:03:24 +0000

    Refactored tests

--- spec/features/choose_weapon_spec.rb
+++ spec/features/choose_weapon_spec.rb
@@ -1,8 +1,6 @@
 feature 'Weapon chooser' do
   scenario 'displays chosen weapon' do
-    visit('/')
-    fill_in :player_name, with: 'Bejlo'
-    click_button 'Submit'
+    sign_in_and_play
     click_button 'Rock'
     expect(page).to have_content 'You chose Rock'
   end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Name entry' do
   scenario 'displays player name' do
-    visit('/')
-    fill_in :player_name, with: 'Bejlo'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "You're up, Bejlo!"
   end
 end
