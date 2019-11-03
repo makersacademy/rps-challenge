6a046241e8597f67698cbe7e86b4c89a8eab3a7d
Andy Jones <andytjones@icloud.com>
2019-11-03 16:53:50 +0000

    Added web helpers for choosing rock, paper or scissors.

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -3,3 +3,21 @@ def name_and_play
   fill_in :playerName, with: "Andy"
   click_button 'Submit'
 end
+
+def choose_rock
+  name_and_play
+  select 'Rock', from: 'weapon'
+  click_button 'Submit'
+end
+
+def choose_paper
+  name_and_play
+  select 'Paper', from: 'weapon'
+  click_button 'Submit'
+end
+
+def choose_scissors
+  name_and_play
+  select 'Paper', from: 'weapon'
+  click_button 'Submit'
+end
