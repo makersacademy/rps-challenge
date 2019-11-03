164d51abbb12e8823345971ed411489418f3e0b1
Student <student@Makerss-MBP.connect>
2019-11-03 21:55:44 +0000

    Game class created. 3/3 100% no offences

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+class Game
+
+  attr_reader :computer_weapon
+  
+  def initialize
+    @computer_weapon = ["Rock", "Paper", "Scissors"].sample
+  end
+
+  def compare_weapons(player_weapon)
+    return "It's a tie." if player_weapon == @computer_weapon
+    return "You win." if player_weapon == "Scissors" && @computer_weapon == "Paper"
+    return "You win." if player_weapon < @computer_weapon
+
+    "I win."
+  end
+
+end
