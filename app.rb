f7963b087278691e830bbf6b01a38c9dd56596ae
Andy Jones <andytjones@icloud.com>
2019-11-03 13:39:01 +0000

    User story 1 complete without styling. Player can enter name and see name.

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    @player = params[:playerName]
+  post '/name' do
+    session[:player] = params[:playerName]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player = session[:player]
     erb(:play)
   end
+
+  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
