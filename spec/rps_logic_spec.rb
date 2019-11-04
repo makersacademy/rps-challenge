107f900c24e9583b207e17669b546ff0d9129e64
Robert Woolley <rob@robertwoolley.co.uk>
2019-11-04 00:52:34 +0000

    Minor tweak on logic tests for greater clarity.

--- spec/rps_logic_spec.rb
+++ spec/rps_logic_spec.rb
@@ -4,10 +4,16 @@ describe RPSLogic do
   it 'it values rock over scissors' do
     expect(logic.check("Rock", "Scissors")).to eq 1
   end
-  it 'it values scissors over rock' do
+  it 'it values rock over scissors when reversed' do
     expect(logic.check('Scissors', 'Rock')).to eq 2
   end
   it 'it returns a draw when both are Rock' do
     expect(logic.check('Rock', 'Rock')).to eq 0
+  end
+  it 'it values scissors over paper' do
+    expect(logic.check('Scissors', 'Paper')).to eq 1
+  end
+  it 'it values paper over rock' do
+    expect(logic.check('Paper', 'Rock')).to eq 1
   end
 end
