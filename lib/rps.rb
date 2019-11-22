5e1c296510575c2da5a457d2d2dde43da23b5bea
djlonboy <djlonboy@gmail.com>
2019-11-22 11:55:44 +0000

    Passed test for computer to randomly choose Rok, Paper or Scissors

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,3 +1,10 @@
 class Computer
 
+  def choose
+    # Randomly chooses Rock, Paper or Scissors
+    choice = rand(0..2)
+    choices = ["Rock", "Paper", "Scissors"]
+    choices[choice]
+  end
+
 end
--- spec/unit/rps_spec.rb
+++ spec/unit/rps_spec.rb
@@ -5,28 +5,28 @@ describe Computer do
   context "can randomly return" do
     it "'Rock' approximately 1/3 of the time" do
       choices = []
-      100000.times do
+      100_000.times do
         choices << subject.choose
       end
-      fraction = (choices.count("Rock").to_f / 100000).to_f
+      fraction = (choices.count("Rock").to_f / 100_000).to_f
       expect(fraction).to be_within(0.05).of(0.33)
     end
 
     it "'Paper' approximately 1/3 of the time" do
       choices = []
-      100000.times do
+      100_000.times do
         choices << subject.choose
       end
-      fraction = (choices.count("Paper").to_f / 100000).to_f
+      fraction = (choices.count("Paper").to_f / 100_000).to_f
       expect(fraction).to be_within(0.05).of(0.33)
     end
 
     it "'Scissors' approximately 1/3 of the time" do
       choices = []
-      100000.times do
+      100_000.times do
         choices << subject.choose
       end
-      fraction = (choices.count("Scissors").to_f / 100000).to_f
+      fraction = (choices.count("Scissors").to_f / 100_000).to_f
       expect(fraction).to be_within(0.05).of(0.33)
     end
 
