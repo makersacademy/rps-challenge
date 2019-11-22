7a4f65601b08b8fa18bc3dc5bd1167e765838501
Marianne OConnor <marianneoco@gmail.com>
2019-11-22 11:14:56 +0000

    Tests for buttons passing

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -7,17 +7,17 @@ feature 'Play page' do
 
   scenario "Can see the option to play ROCK" do
     sign_in_and_play
-    expect(page).to have_content("ROCK")
+    expect(page).to have_selector(:link_or_button, "ROCK")
   end
 
   scenario "Can see the option to play PAPER" do
     sign_in_and_play
-    expect(page).to have_content("PAPER")
+    expect(page).to have_selector(:link_or_button, "PAPER")
   end
 
   scenario "Can see the option to play SCISSORS" do
     sign_in_and_play
-    expect(page).to have_content("SCISSORS")
+    expect(page).to have_selector(:link_or_button, "SCISSORS")
   end
 
 end
