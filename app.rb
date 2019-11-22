56bb2be041ce317f2f76054ee7c5a24aac7fc47b
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-22 11:20:06 +0000

    Fixed the web helpers, so that the feature test code is drier

--- app.rb
+++ app.rb
@@ -5,14 +5,23 @@ class Rps < Sinatra::Base
   # 'Testing infrastructure working!'
   # end
 
+  enable :sessions
+  set :session_secret, "super secret"
+
   get '/' do
       erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb (:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect'/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
   end
 
   run! if app_file == $0
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Bella'
-    fill_in :player_2_name, with: 'Marjan'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Bella vs. Marjan'
   end
 end
