4a602af212da3d99c1419fe249c2181a6f243592
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-11-22 14:38:50 +0000

    Tried to add button options for rock, paper and scissors, not just rock (WIP)

--- spec/features/pick_rps_spec.rb
+++ spec/features/pick_rps_spec.rb
@@ -1,7 +1,18 @@
 feature 'Pick Rock/Paper/Scissors' do
-  scenario 'pick rock/paper/scissors' do
+  scenario 'pick rock' do
     sign_in_and_play
     click_button 'Rock'
-    expect(page).to have_content 'Bella picked Rock!'
+    expect(page).to have_content 'Bella picked rock!'
   end
+  scenario 'pick paper' do
+    sign_in_and_play
+    click_button 'Paper'
+    expect(page).to have_content 'Bella picked paper!'
+  end 
+  scenario 'pick scissors' do
+      sign_in_and_play
+      click_button 'Scissors'
+      expect(page).to have_content 'Bella picked scissors!'
+  end
+
 end
