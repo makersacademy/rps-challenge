e88114f836d68af43adc47b895bf1674f01007e4
Selina Erhabor <selinaerhabor@gmail.com>
2019-11-22 14:38:03 +0000

    Made tests for returning user name entered by the player into game. Tests are now passing

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb :index
+  end
+
+  post '/' do
+    @user_name = params[:user_name]
+    redirect '/round'
+  end
+
+  post '/round' do
+    @user_name = params[:user_name].capitalize
+    erb :round
   end
 
   run! if app_file == $0
---
+++ lib/user.rb
@@ -0,0 +1,7 @@
+class User
+  attr_reader :user_name
+  
+  def initialize(user_name)
+    @user_name = user_name
+  end
+end
