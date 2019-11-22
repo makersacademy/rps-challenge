dc63d5d7b00a2f8aaadd90c217dc68a5dbfbbed1
Marianne OConnor <marianneoco@gmail.com>
2019-11-22 12:05:10 +0000

    Failing test for computer returning random move

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -4,4 +4,9 @@ describe Computer do
 
   it { should be_an_instance_of(Computer) }
 
+  it "should return a random move" do
+    allow(Kernel).to receiev(:rand).and_return(1)
+    expect(subject.move).to eq :rock
+  end
+
 end
