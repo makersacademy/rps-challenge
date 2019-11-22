4035119a64745ddc34a1cf94f62b6e4a4c1ffd53
Sam King <samuelking94@gmail.com>
2019-11-22 11:56:16 +0000

    Failing unit test to assert that winning player object is returned

--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -1,11 +1,20 @@
 require './lib/game.rb'
 
 describe Game do
-  let(:player_1) { double :player_1 }
-  let(:player_2) { double :player_2 }
   subject(:game) { described_class.new(player_1, player_2) }
 
-  it 'should contain the players' do
-    expect(game.players).to eq [player_1, player_2]
+  context 'player 2 wins' do
+    let(:player_1) { double :player_1, :name => "Sam", :move => "Rock" }
+    let(:player_2) { double :player_2, :name => "Charlie", :move => "Paper"}
+
+    it 'should contain the players' do
+      expect(game.players).to eq [player_1, player_2]
+    end
+
+    describe '#play' do
+      it 'should return the winning player' do
+        expect(game.play).to eq(player_2)
+      end
+    end
   end
 end
