6d658a635e15f5b1a2771e841dfe8fb38dffe966
Sam King <samuelking94@gmail.com>
2019-11-22 09:48:54 +0000

    Names route implemented using post method  and index get route implemented

--- app.rb
+++ app.rb
@@ -5,5 +5,14 @@ class RockPaperScissors < Sinatra::Base
   get '/test-infrastructure' do
     "Test infrastructure correctly functioning"
   end
-  
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @name = params[:name]
+    erb :confirm
+  end
+
 end
