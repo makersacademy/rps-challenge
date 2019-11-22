efca478c4d6abf64b533d0e8bd0dba32d054658c
Marianne OConnor <marianneoco@gmail.com>
2019-11-22 12:20:50 +0000

    Tests all passing, computer can play a random move

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,3 +1,15 @@
 class Computer
 
+  MOVE_HASH = {1 => :rock, 2 => :paper, 3 => :scissors}
+
+  attr_reader :move
+
+  def computer_move
+    move_generator(MOVE_HASH)
+  end
+
+  def move_generator(store)
+    @move = store[Kernel.rand(1..3)]
+  end
+
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -5,7 +5,8 @@ describe Computer do
   it { should be_an_instance_of(Computer) }
 
   it "should return a random move" do
-    allow(Kernel).to receiev(:rand).and_return(1)
+    allow(Kernel).to receive(:rand).and_return(1)
+    subject.computer_move
     expect(subject.move).to eq :rock
   end
 
