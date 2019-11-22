1b344de5f83b71da7f9bc90853979ce766169a6e
Sam King <samuelking94@gmail.com>
2019-11-22 14:23:52 +0000

    Failing feature test for draw scenario

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -12,4 +12,11 @@ feature 'Result' do
     click_button("Rock")
     expect(page).to have_content "Sam wins!"
   end
+
+  scenario 'a draw' do
+    enter_name_and_start_game
+    srand(4)
+    click_button("Scissors")
+    expect(page).to have_content "Draw!"
+  end
 end
