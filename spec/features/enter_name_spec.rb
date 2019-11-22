a7f91bd0b89072f6ead64821546798fbf8af756d
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-22 14:51:47 +0000

    Update 'visit' in enter_name feature test to accommodate new homepage.

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,6 +1,6 @@
 feature 'Enter name' do
   scenario 'submitting name' do
-    visit('/')
+    visit('/enter-name')
     fill_in :player_name, with: "Name"
     click_button 'Submit'
     expect(page).to have_content "Name vs. Computer!"
