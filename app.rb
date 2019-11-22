cb87c0f02e7605ef46da38d166c6dd4b04a6e011
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-11-22 11:43:32 +0000

    redirect after submit

--- app.rb
+++ app.rb
@@ -1,7 +1,29 @@
 require 'sinatra/base'
 
  class RPS < Sinatra::Base
+   enable :sessions
    get '/' do
      erb :index
    end
+
+post '/name' do
+  session[:player] = params[:player]
+  session[:computer] = params[:computer]
+  redirect '/play'
+end
+
+get '/play' do
+     @player = session['player']
+     @computer = session['computer']
+     erb :play
+   end
+
+   # get '/attack' do
+   #      @player1 = session['player1']
+   #      @player2 = session['player2']
+   #
+   #    end
+   # get '/play' do
+   #   erb :play
+   # end
  end
--- spec/features/name_lights_spec.rb
+++ spec/features/name_lights_spec.rb
@@ -1,9 +1,10 @@
 feature 'Enter and display Player name' do
-  scenario 'The player enters their name' do
+  scenario 'The player enters their name and submits form' do
     visit '/'
     within(:css, '#player_name_form') do
        fill_in :player, with: 'Gabby'
        click_button 'Play!'
      end
+     expect(page).to have_content 'Gabby vs. computer'
    end
  end
