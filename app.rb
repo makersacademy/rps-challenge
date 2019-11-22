7f1fcf9c21ee21745f20ba970e5335b470b92f5e
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-22 16:06:37 +0000

    Include enable session.
    '/names now redirects to '/play' via GET method.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class RpsApp < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
@@ -11,7 +12,12 @@ class RpsApp < Sinatra::Base
   end
 
   post '/names' do
-    @player_name = params[:player_name]
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
     erb :play
   end
 
