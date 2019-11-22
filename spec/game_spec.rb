37b199061fbd3721bd2b7cd9c2edf97efd8984d8
Marianne OConnor <marianneoco@gmail.com>
2019-11-22 10:40:46 +0000

    Failing test for new_game method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,12 @@ require './lib/game'
 
 describe Game do
 
+  let (:player) { double "game player" }
+
   it { should be_an_instance_of(Game) }
+
+  it "should have a class instance variable which can store the current game" do
+    expect(Game.new_game(player)).to be_an_instance_of(Game)
+  end
 
 end
