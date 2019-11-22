968c6427e980c188f1d615aff690be3a532243d0
djlonboy <djlonboy@gmail.com>
2019-11-22 10:12:33 +0000

    Added feature test for game taking and displaying user name

--- spec/feature/rps_webapp_spec.rb
+++ spec/feature/rps_webapp_spec.rb
@@ -1,10 +1,19 @@
 require "capybara/rspec"
 
-feature "web app opens correctly:" do
-  scenario "'/' Returns 'Hello World'" do
+feature "Web app opens correctly:" do
+  scenario "'/' Returns welcome text" do
     visit('/')
     expect(page).to have_content "Hello World"
-
   end
 
+end
+
+feature "Game can store user's name:" do
+  scenario "User inputs name and sees it on next screen" do
+    visit('/')
+    Fill_in "name", with: "Test Player"
+    click_button "submit"
+    expect(page).to have_content "Test PLayer"
+
+  end
 end
