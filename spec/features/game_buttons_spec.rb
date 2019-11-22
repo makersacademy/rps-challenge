3ca6563c60c1988bba9123de32347bf82a65aae6
Sam King <samuelking94@gmail.com>
2019-11-22 11:01:14 +0000

    Failing feature test for paper button selection

--- spec/features/game_buttons_spec.rb
+++ spec/features/game_buttons_spec.rb
@@ -4,4 +4,10 @@ feature 'Game buttons' do
     click_button("Rock")
     expect(page).to have_content "You selected Rock!"
   end
+
+  scenario 'paper button pressed' do
+    enter_name_and_start_game
+    click_button("Paper")
+    expect(page).to have_content "You selected Paper!"
+  end
 end
