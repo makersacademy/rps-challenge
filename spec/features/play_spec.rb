6f0bc35f551fd11f540a2a76643a943706d66c14
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-22 11:04:46 +0000

    add test to play rock

---
+++ spec/features/play_spec.rb
@@ -0,0 +1,10 @@
+feature 'Play' do
+  scenario 'Player can select an option from rock paper scissors' do
+    visit('/')
+    fill_in :player_name, with: 'Anh'
+    click_button 'Enter'
+    expect(page).to have_content 'Anh vs. Robot'
+    click_button 'Rock'
+    expect(page).to have_content 'Rock beats scissors. You win!'
+  end
+end
