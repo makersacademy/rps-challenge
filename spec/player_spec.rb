aa648c5d5ff77d538fc2fd0974d3fd4b0711c6f8
Marianne OConnor <marianneoco@gmail.com>
2019-11-22 11:23:45 +0000

    Tests for player class passing

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,5 +3,10 @@ require './lib/player'
 describe Player do
 
   it { should be_an_instance_of Player }
+  subject(:player) { described_class.new("test name") }
+
+  it "can return the players name" do
+    expect(subject.name).to eq "test name"
+  end
 
 end
