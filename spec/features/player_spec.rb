85a919fdce0416b45713176a02c93dc5e0dcf0b3
Simone <simonewlkr@hotmail.com>
2019-11-22 11:54:54 +0000

    Noticed some errors, I also skipped some steps so went back a replaced my feature test

--- spec/features/player_spec.rb
+++ spec/features/player_spec.rb
@@ -1,10 +1,9 @@
 require 'player'
 
 describe Player do
-  subject(:simone) {Player.new('Simone')} #Creates an instance of player, and passes the argument :simone with the string "Simone"
-  describe '#name' do
-    it 'returns the name' do
-      expect(simone.name).to eq 'Simone'
-    end
+  feature 'Enter player name' do
+  visit('/')
+  fill_in :player_name, with: 'Simone'
+  click_button 'Submit'
    end
  end
