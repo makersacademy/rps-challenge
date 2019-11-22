533d1f71c38c5947761a4de2cedc6e095e7a1c2d
Sam King <samuelking94@gmail.com>
2019-11-22 10:07:30 +0000

    Failing feature test written to assert that start game button takes player to the game page

---
+++ spec/features/start_game_spec.rb
@@ -0,0 +1,9 @@
+feature 'Start game' do
+  scenario 'player continues to start game' do
+    visit('/')
+    fill_in :name, with: "Sam"
+    click_button("Enter name")
+    click_button("Start game")
+    expect(page).to have_content "Welcome to Rock Paper Scissors Game!"
+  end
+end
