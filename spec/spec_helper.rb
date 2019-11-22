9581c5debf8fa572b0df6227a4f458fcf83917c9
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-22 11:08:28 +0000

    Set up spec_helper.rbto prepare Capybara and get Capybara to test the app.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,17 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 # For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
+ENV['RACK_ENV'] = 'test'
 
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# tell Capybara about our app class
+Capybara.app = RpsApp
 
 
 RSpec.configure do |config|
