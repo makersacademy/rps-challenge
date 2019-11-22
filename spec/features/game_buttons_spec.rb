74040ff994bbc0314eb0366a8f3a1969ba608a0b
Sam King <samuelking94@gmail.com>
2019-11-22 14:28:19 +0000

    Refactor game button tests

--- spec/features/game_buttons_spec.rb
+++ spec/features/game_buttons_spec.rb
@@ -2,18 +2,18 @@ feature 'Game buttons' do
   scenario 'rock button pressed' do
     enter_name_and_start_game
     click_button("Rock")
-    expect(page).to have_content "You selected Rock!"
+    expect(page).to have_content "Sam selected Rock!"
   end
 
   scenario 'paper button pressed' do
     enter_name_and_start_game
     click_button("Paper")
-    expect(page).to have_content "You selected Paper!"
+    expect(page).to have_content "Sam selected Paper!"
   end
 
   scenario 'scissors button pressed' do
     enter_name_and_start_game
     click_button("Scissors")
-    expect(page).to have_content "You selected Scissors!"
+    expect(page).to have_content "Sam selected Scissors!"
   end
 end
