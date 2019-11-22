734c5a1ed0b087b03f1c0ee16b1d0724ff3c3f7e
Anh Nguyen <anhnguyenis@gmail.com>
2019-11-22 10:42:31 +0000

    add form to enter player name in index.erb, and add play.erb to display name

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra/base'
 
 class Rockpaperscissors < Sinatra::Base
+  # get '/' do
+  #   'Testing infrastructure working!'
+  # end
+
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
+  end
+
+  post '/names' do
+    @player_name = params[:player_name]
+    erb :play
   end
 
   run! if app_file == $0
