4bfeec92c336e9100035871d7c30d7f834d4fdab
Marianne OConnor <marianneoco@gmail.com>
2019-11-22 10:29:06 +0000

    Second failing test, for presence of form on homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -2,7 +2,12 @@ feature 'Homepage' do
 
   scenario "Can visit homepage which loads and displays a message" do
     visit("/")
-    expect(page).to have_content("Rock, Paper, Scissors")
+    expect(page).to have_content("Welcome to Rock, Paper, Scissors!")
   end
-  
+
+  scenario "Can visit homepage which shows a form" do
+    visit("/")
+    expect(page).to have_content("To play, enter your name:")
+  end
+
 end
