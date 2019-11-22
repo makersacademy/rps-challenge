bf441ee98a2ffb9d7eabfad2d46dc8b1129a383e
Sam King <samuelking94@gmail.com>
2019-11-22 09:53:00 +0000

    Revert "Names route implemented using post method"
    This reverts commit 6d658a635e15f5b1a2771e841dfe8fb38dffe966.

--- app.rb
+++ app.rb
@@ -5,14 +5,5 @@ class RockPaperScissors < Sinatra::Base
   get '/test-infrastructure' do
     "Test infrastructure correctly functioning"
   end
-
-  get '/' do
-    erb :index
-  end
-
-  post '/names' do
-    @name = params[:name]
-    erb :confirm
-  end
-
+  
 end
