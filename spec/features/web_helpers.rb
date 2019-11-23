e3532693e1ea310d9e04605d4024d5c02c9d14ca
Rebecca Varley <rebecca.varley@gmail.com>
2019-11-23 20:27:34 +0000

    changing web helpers as had changed to image link

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,14 +1,12 @@
 def sign_in_and_play
-  visit '/'
-  click_on 'One Player'
+  visit '/one-player'
   fill_in :player_1_name, with: "Rebecca"
   fill_in :choice, with: 'ROCK'
   click_button 'Submit'
 end
 
 def sign_in_and_play_multiple
-  visit '/'
-  click_on 'Two Player'
+  visit '/two-player'
   fill_in :player_1_name, with: "Rebecca"
   fill_in :player_2_name, with: "Lily"
   fill_in :choice, with: 'ROCK'
