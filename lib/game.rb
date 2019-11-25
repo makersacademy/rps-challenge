c6bd773aa6ab62a7c538f85576770909e0a0786b
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-25 09:57:54 +0000

    Create Game class to pass unit test.

---
+++ lib/game.rb
@@ -0,0 +1,19 @@
+class Game
+end
+
+# puts "Player 1, enter 'rock', 'paper' or 'scissors'."
+# player_1_input = gets.chomp
+# puts "Player 2, enter 'rock', 'paper' or 'scissors'."
+# player_2_input = gets.chomp
+#
+# if player_1_input == player_2_input
+#   puts "It's a draw!"
+# elsif player_1_input == "rock" && player_2_input == "scissors"
+#   puts "Player 1 wins!"
+# elsif player_1_input == "scissors" && player_2_input == "paper"
+#   puts "Player 1 wins!"
+# elsif player_1_input == "paper" && player_2_input == "rock"
+#   puts "Player 1 wins!"
+# else
+#   puts "Player 2 wins!"
+# end
