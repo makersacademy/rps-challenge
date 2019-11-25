edb5832b052c4c04cafc755acd72543b46163e8b
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-25 09:36:20 +0000

    reminder comments added for whst needs to be done and what i need to try and work out

--- app.rb
+++ app.rb
@@ -24,9 +24,13 @@ class Game < Sinatra::Base
     erb :play
   end
 
+  #  need to figure out how to carry over the stored names and choices for potential best of 3
   #  need to get the controller and the model layer communicating
-  #  form input is posting
+  #  form input is posting and the post data is seen on the browser error form for /results
+  #  is it going to be able to access p2_move from the model layer with the .sample in the def initialize?
+  #  why is /results not opening :(
   get '/result' do
+    #  put the name in here too to add a sentence which uses the name too
     session[:p1_move] = params[:p1_move]
     @p1_move = session[:p1_move]
     @game = RPSGame.new
