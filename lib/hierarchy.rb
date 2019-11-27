f1cde075fe422888e64a75fede2fd4694965678f
Alastair Gilles <agilles91@gmail.com>
2019-11-28 00:10:05 +0100

    Added hierarchy class to decide which move wins

---
+++ lib/hierarchy.rb
@@ -0,0 +1,11 @@
+class Hierarchy
+  def initialize rules
+    @beats = rules 
+  end
+
+  def wins move1, move2
+    return if move1 == move2
+    return move1 if move2 == @beats[move1]
+    move2
+  end
+end
---
+++ spec/hierarchy_spec.rb
@@ -0,0 +1,30 @@
+require 'hierarchy'
+
+describe Hierarchy do
+  let(:hash) { {rock: :scissors, paper: :rock, scissors: :paper} }
+
+  subject { described_class.new hash }
+
+  describe '#wins' do
+    it "should return nil if it's a draw" do
+      expect(subject.wins(:rock, :rock)).to be_nil
+      expect(subject.wins(:paper, :paper)).to be_nil
+      expect(subject.wins(:scissors, :scissors)).to be_nil
+    end
+
+    it "should return :rock if it wins" do
+      expect(subject.wins(:rock, :scissors)).to eq :rock
+      expect(subject.wins(:scissors, :rock)).to eq :rock
+    end
+
+    it "should return :paper if it wins" do
+      expect(subject.wins(:paper, :rock)).to eq :paper
+      expect(subject.wins(:rock, :paper)).to eq :paper
+    end
+
+    it "should return :scissors if it wins" do
+      expect(subject.wins(:paper, :scissors)).to eq :scissors
+      expect(subject.wins(:scissors, :paper)).to eq :scissors
+    end
+  end
+end
