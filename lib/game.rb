a811065508929b3a425e717b03d644e0d1056c8d
Alastair Gilles <agilles91@gmail.com>
2019-11-28 10:00:39 +0100

    Added game class
    The game class takes a hierarchy on initialisation and creates a
    computer player to play against. To play, pass the game the player's
    move, this will return `:player1` if the player wins, `:player2` if
    the computer wins or `nil` if it is a draw.
    The player's and the computers moves can be retrieved through
    reader methods.

---
+++ lib/game.rb
@@ -0,0 +1,21 @@
+class Game
+  attr_reader :player1_move, :player2_move
+
+  def initialize hierarchy, computer = nil
+    @hierarchy = hierarchy
+    @player2 = computer || ComputerPlayer.new(hierarchy)
+  end
+
+  def play player1_move
+    @player1_move = player1_move
+    @player2_move = @player2.move
+    find_winner
+  end
+
+  def find_winner
+    result = @hierarchy.wins(@player1_move, @player2_move)
+    return if result.nil?
+    return :player1 if result == @player1_move
+    :player2
+  end
+end
