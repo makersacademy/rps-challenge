c0ac2e0f2e6c44c59e6a544f8e7ef6216a34fa2d
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-11-30 11:28:31 +0000

    Use redirect to avoid rendering a view directly from a POST request

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
+require_relative './lib/player'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
   get "/" do
     erb :index
   end
 
   post "/play" do
     player = Player.new(params[:player_name])
-    @player_name = player.name
+    session[:player_name] = player.name
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player_name = session[:player_name]
     erb :play
   end
 
--- spec/units/player_spec.rb
+++ spec/units/player_spec.rb
@@ -2,7 +2,7 @@ require "player"
 
 describe Player do
   let(:player) { Player.new("Andrea") }
-  
+
   describe "#name" do
     it "should return the player's name" do
       expect(player.name).to eq "Andrea"
