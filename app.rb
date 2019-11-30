d2e540090c8008ef95fc48d15f3b19fae855757d
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-11-30 10:01:33 +0000

    Add index.erb file in the views folder

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
   get '/' do
-    'Welcome to the best Rock Paper Scissors game you have ever seen!'
+    erb :index
   end
 
   run! if app_file == $0
