33e97216c80b21da0f491b9c9cb01f6116d74bfc
Ellie <elliemmanifold@gmail.com>
2019-11-30 14:44:15 +0000

    Implement enter name functionality, and send through to play page

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
+  end
+
+  post '/details' do
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player = session[:player_name]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,7 +1,7 @@
 feature 'Enter name' do
   scenario 'Player should enter name and it show on next screen' do
     visit('/')
-    fill_in 'name', with: 'Ellie'
+    fill_in 'player_name', with: 'Ellie'
     click_on 'Submit'
     expect(page).to have_content 'Welcome Ellie'
   end
