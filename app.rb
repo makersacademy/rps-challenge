f2426366c47487703bf0747986a87462166d296b
Eve Noirault <student@Makerss-MacBook-Air.local>
2019-12-01 15:14:47 +0000

    implement name form on homepage

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
-require 'sinatra'
+require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  post "/register" do
+    
   end
 
   run! if app_file == $0
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ RSpec.configure do |config|
 
   ENV['RACK_ENV'] = 'test'
   require File.join(File.dirname(__FILE__), '..', 'app.rb')
-
+  
   require 'capybara'
   require 'capybara/rspec'
   require 'rspec'
