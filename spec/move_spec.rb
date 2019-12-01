1a70934bcf43a7a97919b8ab0d9e85ae88ba5970
Student <student@Makerss-Air.connect>
2019-12-01 17:36:52 +0000

    refactored

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -4,7 +4,7 @@ describe Move
 
 context "After clicking a button" do
   it "Initlizes with a players move" do
-    player = Move.new("Rock")
+    player = Move.new "Rock"
     expect(player.value).to eq "Rock"
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,7 +4,7 @@ describe Player
 
 context "After submiting a form" do
   it "Initlizes with a name" do
-    player = Player.new("Dawid")
+    player = Player.new "Dawid"
     expect(player.name).to eq "Dawid"
   end
 end
