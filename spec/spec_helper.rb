8d6955f0ba4dc659016444336b2fc2a480b62989
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-12-01 21:55:22 +0000

    commit before README

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
@@ -24,7 +24,6 @@ ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'lib', 'app.rb')
 
 require 'capybara'
-require 'capybara/rspec'
 require 'rspec'
 require_relative './features/web_helpers'
 
