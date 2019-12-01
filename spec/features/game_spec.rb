56d0739821ee45cf0880355b03e5901fc3c85165
Eve Noirault <student@Makerss-MacBook-Air.local>
2019-12-01 16:15:37 +0000

    choose to play rock, paper or scissors - tests passing

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -6,4 +6,27 @@ feature 'Game' do
     click_on "Start Game!"
     expect(page).to have_content("Welcome, Eve!")
   end
+
+  scenario "can choose to play rock" do
+    visit "/"
+    fill_in :player_name, with: "Eve"
+    click_on "Start Game!"
+    expect(page).to have_button("ROCK")
+  end
+
+  scenario "can choose to play paper" do
+    visit "/"
+    fill_in :player_name, with: "Eve"
+    click_on "Start Game!"
+    expect(page).to have_button("PAPER")
+  end
+
+  scenario "can choose to play scissors" do
+    visit "/"
+    fill_in :player_name, with: "Eve"
+    click_on "Start Game!"
+    expect(page).to have_button("SCISSORS")
+  end
+
+
 end
