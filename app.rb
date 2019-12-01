6b57cef8317ff2f00af2f9bc332faa968602ccd0
Eve Noirault <student@Makerss-MacBook-Air.local>
2019-12-01 15:43:16 +0000

    greet player once registered

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post "/register" do
-    
+    @player_name = params[:player_name]
+    erb :game
   end
 
   run! if app_file == $0
--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -2,7 +2,7 @@ feature 'Game' do
 
   scenario "new game greets player" do
     visit "/"
-    fill_in "player_name", with: "Eve"
+    fill_in :player_name, with: "Eve"
     click_on "Start Game!"
     expect(page).to have_content("Welcome, Eve!")
   end
