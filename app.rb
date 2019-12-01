08e5bdf3131689aeb31a1f3d465232a4b54ac5a1
shafali03 <shaf03sm@gmail.com>
2019-12-01 19:07:15 +0000

    Sign in to play

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base 
+  enable :sessions
  
 
 get '/' do
-  'Testing infrastucture working!'
+  erb :index
+end
+
+
+post '/names' do
+  @player_1_name = params[:player_1_name]
+  erb :play
 end
 
 
   run! if app_file == $0
-
 end
