4a67c79dcbb458f05a93e8f5ab1d1d83382b2486
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-03 17:00:30 +0000

    Add tests for win, draw, lose scenarios

--- spec/features/winner_spec.rb
+++ spec/features/winner_spec.rb
@@ -1,15 +1,30 @@
-feature 'Displaying a win or draw' do
+feature 'Winning, losing, and drawing' do
 
-  it "should display both the player and computer's moves" do
-    player_vs_computer
-    click_button "Rock"
-    expect(page).to have_content "You choose: Rock"
-    expect(page).to have_content "Computer choose:"
-  end
+    scenario "When you choose rock and the computer chooses scissors, you win" do
+        allow_any_instance_of(Array).to receive(:sample).and_return('scissors')
+        player_vs_computer
+        click_button "Rock"
+        expect(page).to have_content "You choose: Rock"
+        expect(page).to have_content "Computer choose: Scissors"
+        expect(page).to have_content "Ben wins!"
+    end
+  
 
-  it "should display the winner or call the game a draw" do
-    player_vs_computer
-    click_button "Rock"
-    expect(page).to have_content "!"
-  end
+    scenario "When both you and the computer choose rock, you draw" do
+      allow_any_instance_of(Array).to receive(:sample).and_return('rock')
+      player_vs_computer
+      click_button "Rock"
+      expect(page).to have_content "You choose: Rock"
+      expect(page).to have_content "Computer choose: Rock"
+      expect(page).to have_content "It's a draw!"
+    end
+  
+    scenario "When you choose scissors and the computer chooses rock, you lose" do
+      allow_any_instance_of(Array).to receive(:sample).and_return('rock')
+      player_vs_computer
+      click_button "Scissors"
+      expect(page).to have_content "You choose: Scissors"
+      expect(page).to have_content "Computer choose: Rock"
+      expect(page).to have_content "Computer wins!"
+    end
 end
