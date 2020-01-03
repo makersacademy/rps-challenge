256c9de65cfec00899a0701629a6ef70a0a5816a
Jonathan Palma <jonathanpalma89@gmail.com>
2020-01-03 22:11:25 +0000

    Refactor game.rb

--- app.rb
+++ app.rb
@@ -8,38 +8,38 @@ class Rps < Sinatra::Base
   end
 
   post '/name' do
-    $game = Game.new(params[:player])
+    @@game = Game.new(params[:player])
     redirect '/home_game'
   end
 
   get '/home_game' do
-    @player = $game.player
-    @player_points = $game.player_points
-    @comp_points = $game.comp_points
+    @player = @@game.player
+    @player_points = @@game.player_points
+    @comp_points = @@game.comp_points
     erb(:home_game)
   end
 
   get '/play_rock' do
-    @player = $game.player
-    player_move = $game.play_rock
-    @computer_move = $game.computer_move
-    @result = $game.result(player_move)
+    @player = @@game.player
+    player_move = @@game.play_rock
+    @computer_move = @@game.computer_move
+    @result = @@game.result(player_move)
     erb(:play_rock)
   end 
 
   get '/play_scissors' do
-    @player = $game.player
-    player_move = $game.play_scissors
-    @computer_move = $game.computer_move
-    @result = $game.result(player_move)
+    @player = @@game.player
+    player_move = @@game.play_scissors
+    @computer_move = @@game.computer_move
+    @result = @@game.result(player_move)
     erb(:play_scissors)
   end
 
   get '/play_paper' do
-    @player = $game.player
-    player_move = $game.play_paper
-    @computer_move = $game.computer_move
-    @result = $game.result(player_move)
+    @player = @@game.player
+    player_move = @@game.play_paper
+    @computer_move = @@game.computer_move
+    @result = @@game.result(player_move)
     erb(:play_paper)
   end
 end
