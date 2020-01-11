5199622f7dcb2b2950fe571e5becb350992ef8d2
AliciaMerlino <acmerlino1@gmail.com>
2020-01-11 22:28:59 +0000

    game class created

--- spec/features/homepage_text_spec.rb
+++ spec/features/homepage_text_spec.rb
@@ -1,6 +1,6 @@
 feature 'Testing infrastructure' do
   scenario 'Run app and check page content' do
-    visit ('/')
+    visit('/')
     expect(page).to have_content 'Rock, Paper, Scissors!'
   end
 end
