636b21c4307d9544de53d3b98acfcd8079a4df03
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-11 13:13:12 +0000

    Removes comments

--- app.rb
+++ app.rb
@@ -2,23 +2,17 @@ require 'sinatra/base'
 
 class RPS < Sinatra::Base 
   enable :sessions 
-  # get '/' do
-    # "Let's play Rock Paper Scissors!"
-  # end
-
+ 
   get '/' do
-    erb(:index) # shows the page with form for entering names 
+    erb(:index) 
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name] # emily is assigned to player_1_name
-    @player_2_name = params[:player_2_name] # lucy us assigned to player_2_name
+    @player_1_name = params[:player_1_name] 
+    @player_2_name = params[:player_2_name] 
     erb(:play)
   end
 
-  #get '/play' do
-   # erb(:play) # this displays Emily vs. Lucy
-  #end
 
 
 
