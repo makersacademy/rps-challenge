2d12531c8e2208c6c69d90fd38ed79590f2544a9
AliciaMerlino <acmerlino1@gmail.com>
2020-01-11 19:50:52 +0000

    spec test to return user name

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,13 @@
 require 'player'
 
 describe Player do
+
+  subject(:player) { Player.new("Alicia") }
+  
+  describe '#name' do
+    it 'should return player name' do
+      expect(subject.name).to eq "Alicia"
+    end
+  end
   
 end
