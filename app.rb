81971acf9b09db72f6922fac7de5ada6fa4a7a37
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-11 13:29:15 +0000

    Adds POST/REDIRECT/GET pattern

--- app.rb
+++ app.rb
@@ -8,9 +8,16 @@ class RPS < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name] 
-    @player_2_name = params[:player_2_name] 
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name] 
+    redirect '/play'
+    #erb :play
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb (:play)
   end
 
 
