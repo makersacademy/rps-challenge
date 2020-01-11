8de3944f19e2ff8208a2ca03c954542d05e6dd58
ratcliffetj <ratcliffetj89@gmail.com>
2020-01-11 17:36:38 +0000

    Added game logic to game class

--- lib/game.rb
+++ lib/game.rb
@@ -3,6 +3,16 @@ class Game
     @player_name = player_name
   end
 
+
+  def determine_result(user_choice, computer_choice)
+    return "You draw!" if user_choice == computer_choice
+    if (user_choice == "Rock" && computer_choice == "Scissors") || (user_choice == "Paper" && computer_choice == "Rock") || (user_choice == "Scissors" && computer_choice == "Paper")
+      "You win!"
+    else
+      "You lose!"
+    end
+  end
+
   def computer_choice
     ["Rock","Paper","Scissors"].sample
   end
--- spec/unit_tests/game_spec.rb
+++ spec/unit_tests/game_spec.rb
@@ -7,4 +7,34 @@ describe Game do
       expect(subject.computer_choice).to eq("Rock").or eq("Paper").or eq("Scissors")
     end
   end
+  describe '#determine_result' do
+    it 'If you enter Rock and computer enters Scissors it returns You win!' do
+      subject = described_class.new("John")
+      expect(subject.determine_result("Rock","Scissors")).to eq("You win!")
+    end
+    it 'If you enter Rock and computer enters Paper it returns You lose!' do
+      subject = described_class.new("John")
+      expect(subject.determine_result("Rock","Paper")).to eq("You lose!")
+    end
+    it 'If you enter Paper and computer enters Rock it returns You win!' do
+      subject = described_class.new("John")
+      expect(subject.determine_result("Paper","Rock")).to eq("You win!")
+    end
+    it 'If you enter Paper and computer enters Scissors it returns You lose!' do
+      subject = described_class.new("John")
+      expect(subject.determine_result("Paper","Scissors")).to eq("You lose!")
+    end
+    it 'If you enter Scissors and computer enters Rock it returns You lose!' do
+      subject = described_class.new("John")
+      expect(subject.determine_result("Scissors","Rock")).to eq("You lose!")
+    end
+    it 'If you enter Scissors and computer enters Paper it returns You win!' do
+      subject = described_class.new("John")
+      expect(subject.determine_result("Scissors","Paper")).to eq("You win!")
+    end
+    it 'If you and the computer enter the same choice it returns You draw!'do
+      subject = described_class.new("John")
+      expect(subject.determine_result("Rock","Rock")).to eq("You draw!")
+    end
+  end
 end
