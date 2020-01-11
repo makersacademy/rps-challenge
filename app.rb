b101de910ee2262786308fbff74209166f07120b
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-11 13:38:03 +0000

    Replaces sessions with global variables

--- app.rb
+++ app.rb
@@ -7,16 +7,17 @@ class RPS < Sinatra::Base
     erb(:index) 
   end
 
+
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name] 
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
     #erb :play
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb (:play)
   end
 
