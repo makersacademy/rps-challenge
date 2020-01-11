8abac1f8b77a8318c8e21159a553c3d9f0ee993e
Student <katierodgers95@gmail.com>
2020-01-11 12:30:44 +0000

    server running

--- app.rb
+++ app.rb
@@ -1,11 +1,10 @@
 require 'sinatra/base'
 
-class Rockpaperscissors  < Sinatra::Base
+class RockPaperScissors < Sinatra::Base
     get '/' do
-        'Hello rock paper scissors!'
+        'Hello Rock paper scissors!'
       end
     
       # start the server if ruby file executed directly
       run! if app_file == $0
     end
-end
