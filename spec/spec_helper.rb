377c9e42daf3e3e72843a8375dfee9ee32f3dc2b
Umberto Valente <uvalente89@gmail.com>
2020-01-11 19:05:37 +0000

    Project setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,10 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Rps
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
@@ -10,8 +14,6 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 # For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
-
-
 
 RSpec.configure do |config|
   config.after(:suite) do
