0487890390eb1da3dbb94bdf97f8e1e8b26f99f4
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-11 21:59:57 +0000

    Removes comments

--- app.rb
+++ app.rb
@@ -9,17 +9,17 @@ class RPS < Sinatra::Base
   end
 
   post '/name' do
-    $player_1 = Player.new(params[:player_1_name]) # name entered by me is passed as an arg and stored in g variable $player_1
+    $player_1 = Player.new(params[:player_1_name]) 
     redirect '/play'
   end
 
   get '/play' do
     @player_1_name = $player_1.name
-    erb (:play) # displays emily vs lucy the computer   lets go    click to see your options and button to bring us to that page - option
+    erb (:play) 
   end
 
 
-  post '/option' do # displays the button that says - click to see your options 
+  post '/option' do 
     $player_1 = Player.new(params[:player_1_name]) 
     redirect '/selection'
   end
@@ -28,10 +28,10 @@ class RPS < Sinatra::Base
     erb(:option)
   end
 
-  post '/selection' do # this page displays our rock paper scissors buttons and brings us to result page
-    $player_1_selection = params[:selection] # what i choose is stored in $player_1_selection 
-    $lucy_the_computer_selection = Player.new('LucyTheComputer').random_selection # not skinny controller
-    redirect '/result' # what the computer selects is stored in $lucy_the_computer_selection
+  post '/selection' do 
+    $player_1_selection = params[:selection] 
+    $lucy_the_computer_selection = Player.new('LucyTheComputer').random_selection 
+    redirect '/result' 
   end 
 
   get '/selection' do
@@ -39,15 +39,15 @@ class RPS < Sinatra::Base
   end
 
   post '/result' do
-    $player_1_selection = params[:selection] # what i choose is stored in $player_1_selection 
-    $lucy_the_computer_selection = Player.new('LucyTheComputer').random_selection # not skinny controller
+    $player_1_selection = params[:selection] 
+    $lucy_the_computer_selection = Player.new('LucyTheComputer').random_selection 
     redirect '/result'
   end 
 
-  get '/result' do # displays the result of my selection and the computers random selection
+  get '/result' do 
     @player_1_selection = $player_1_selection
     @lucy_the_computer_selection = $lucy_the_computer_selection
-    erb(:result)  # currently not display the option I selected or the randomized option for the computer - not interpolating
+    erb(:result)  
   end
 
 
