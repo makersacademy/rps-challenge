5e2ec38694b065a41952c46d97eed4d2a2174486
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-11 13:10:47 +0000

    Sets up basic web app structure. Users can enter their names and see them once submitted. First two tests pass

--- spec/features/entering_players_spec.rb
+++ spec/features/entering_players_spec.rb
@@ -3,7 +3,8 @@ feature 'Entering player' do
     visit('/')
     fill_in :player_1_name, with: 'Emily'
     fill_in :player_2_name, with: 'Lucy'
-    click_button 'Submit'
+    click_button 'Click me!'
     expect(page).to have_content "Emily vs. Lucy"
+    expect(page).to have_content "Let's go!"
   end
 end
