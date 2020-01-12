f9d493b701c93a03956b9b0fab4eeddcef25dde0
basselalsayed <alsayed.bassel@gmail.com>
2020-01-12 13:21:32 +0000

    added player class with unit tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,20 @@
 class Player
+  attr_reader :name, :move
 
   def initialize(name)
     @name = name
   end
 
-  def move(move)
+  def self.create(name)
+    @player = Player.new(name)
+  end
+
+  def self.instance
+    @player
+  end
+
+  def make_move(move)
+    @move = move
   end
 
 end
