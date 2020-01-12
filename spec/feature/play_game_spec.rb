7e67baa1435d60ccf577e91b948c1b61ad0ecb5a
Aaron Diable <aaron.diable@gmail.com>
2020-01-12 12:08:09 +0000

    Added test for other choices, tests passing

--- spec/feature/play_game_spec.rb
+++ spec/feature/play_game_spec.rb
@@ -9,12 +9,28 @@ feature 'Start Game' do
     expect(page).to have_button "Scissors"
   end
 
-  scenario 'Show player vs computer move' do
+  scenario 'Show player vs computer move (Rock)' do
     visit('/')
     fill_in :player_1_name, with: "Kittens"
     click_button 'Submit'
     click_button 'Rock'
     expect(page).to have_content "You picked Rock, the Computer picked Paper"
+  end
+
+  scenario 'Show player vs computer move (Paper' do
+    visit('/')
+    fill_in :player_1_name, with: "Kittens"
+    click_button 'Submit'
+    click_button 'Paper'
+    expect(page).to have_content "You picked Paper, the Computer picked Paper"
+  end
+
+  scenario 'Show player vs computer move (Scissors)' do
+    visit('/')
+    fill_in :player_1_name, with: "Kittens"
+    click_button 'Submit'
+    click_button 'Scissors'
+    expect(page).to have_content "You picked Scissors, the Computer picked Paper"
   end
 
 end
