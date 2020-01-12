0fcaf2b4b315a28f5901ba3e09a48a3011b86049
Aaron Diable <aaron.diable@gmail.com>
2020-01-12 11:49:36 +0000

    updated vague test, added attack types to view

--- spec/feature/play_game_spec.rb
+++ spec/feature/play_game_spec.rb
@@ -1,9 +1,12 @@
 feature 'Start Game' do
-  scenario 'Submit a name and start the game' do
+  scenario 'choose an attack' do
     visit('/')
     fill_in :player_1_name, with: "Kittens"
     click_button 'Submit'
-    click_button 'Start Game'
-    expect(page).to have_content "Please choose a move"
+    expect(page).to have_content "Please choose an attack type to begin"
+    expect(page).to have_button "Rock"
+    expect(page).to have_button "Paper"
+    expect(page).to have_button "Scissors"
   end
+
 end
