12fc4f3e79d67ede7bbd49eeacea3cafd727847b
Aaron Diable <aaron.diable@gmail.com>
2020-01-12 11:54:28 +0000

    Added test, player choose move, display confirmation

--- spec/feature/play_game_spec.rb
+++ spec/feature/play_game_spec.rb
@@ -9,4 +9,12 @@ feature 'Start Game' do
     expect(page).to have_button "Scissors"
   end
 
+  scenario 'Show player vs computer move' do
+    visit('/')
+    fill_in :player_1_name, with: "Kittens"
+    click_button 'Submit'
+    click_button 'Rock'
+    expect(page).to have_content "You picked Rock, the Computer picked Paper"
+  end
+
 end
