72be05544a07437225eda5205297826756915389
Aaron Diable <aaron.diable@gmail.com>
2020-01-12 14:57:55 +0000

    GameEngine class created with initial test passing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,3 +1,8 @@
-describe GameEngine do
+require './lib/game_engine'
 
+describe GameEngine do
+  it 'returns the winner of a game' do
+    expect(subject).to be_an_instance_of(GameEngine)
+
+  end
 end
