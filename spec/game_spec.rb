57a8790fc718a12746d33a440092ce99fddac1e0
Aaron Diable <aaron.diable@gmail.com>
2020-01-12 15:04:27 +0000

    class tests for Game Engine - Failing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,7 @@
 require './lib/game_engine'
 
 describe GameEngine do
-  it 'returns the winner of a game' do
+  it 'returns an instance of itself' do
     expect(subject).to be_an_instance_of(GameEngine)
 
   end
