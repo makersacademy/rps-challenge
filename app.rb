9d89464911e607e21fdfe230d8b2b7663a1b62c1
AliciaMerlino <acmerlino1@gmail.com>
2020-01-12 14:33:12 +0000

    use instance variable to access player name in erb files

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require './lib/game'
+require './lib/player'
 
 class RockPaperScissors < Sinatra::Base
   enable :sessions
@@ -8,7 +10,13 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
+    $player_1 = Player.new(params[:player_1_name])
+    @game = Game.new($player_1)
+    redirect '/play'
+  end
+
+  get'/play' do
+    @player_1 = $player_1
     erb :play
   end
 
