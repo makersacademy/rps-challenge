9d1f97e9fe783073e669318ae8a58d0ba9257c99
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-12 22:55:05 +0000

    update rps

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,9 @@
 class Game
-  attr_reader :player_1, :choice_by_player, :choice_by_pc
+  attr_reader :player_1, :player_2, :choice_by_player, :choice_by_pc
 
-  def initialize(player_1)
+  def initialize(player_1, player_2 = "PC")
     @player_1 = player_1
+    @player_2 = player_2
     @to_win_logic = {"Rock"=>"Scissors",
                   "Paper"=>"Rock",
                   "Scissors"=>"Paper"}
@@ -18,9 +19,9 @@ class Game
   end
 
   def check_winner(choice_by_player, choice_by_pc)
-    return "You win" if @to_win_logic[choice_by_player] == choice_by_pc
-    return "You lose" if @to_win_logic[choice_by_pc] == choice_by_player
-    "It is a draw"
+    return "Congrats! You win!" if @to_win_logic[choice_by_player] == choice_by_pc
+    return "You lose. Don't give up! :)" if @to_win_logic[choice_by_pc] == choice_by_player
+    "It is a draw. Let's try again!"
   end
 
   def rand_pick
