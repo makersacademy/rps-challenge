69c777f4af67cf149e22be4e34c9e39aa160a29e
Aaron Diable <aaron.diable@gmail.com>
2020-01-12 13:41:57 +0000

    Computer player class created and test passing

---
+++ lib/computer.rb
@@ -0,0 +1,14 @@
+class Computer
+
+  attr_writer :random_seed
+
+  def random_seed_gen
+    rand(1_000_000)
+  end
+
+  def guess
+    srand(@random_seed || random_seed_gen)
+    guesses = %w{ Rock Paper Scissors}
+    guesses.sample
+  end
+end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,12 +1,28 @@
 require 'computer'
 
 describe "Computer" do
-  subject(:Computer) { Computer.create('Computer') }
+  subject(:computer) { Computer.new }
 
-  describe "Picks a random move" do
-    it 'returns rock, paper or scissors' do
-      expect(subject.guess).to eq 'Rock'
+  describe '#guess' do
+    it 'returns rock' do
+      computer.random_seed = 0
+      expect(computer.guess).to eq 'Rock'
+    end
+
+    it 'returns rock' do
+      computer.random_seed = 1
+      expect(computer.guess).to eq 'Paper'
+    end
+
+    it 'returns rock' do
+      computer.random_seed = 3
+      expect(computer.guess).to eq 'Scissors'
     end
   end
 
+  describe "#radom_seed_gen" do
+    it 'returns a value between 1 & 1_000_000' do
+      expect(computer.random_seed_gen).to be_between(1, 1_000_000)
+    end
+  end
 end
