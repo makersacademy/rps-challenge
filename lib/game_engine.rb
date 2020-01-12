dd3fc805742594a1d6ae93fd7616149f91497add
Aaron Diable <aaron.diable@gmail.com>
2020-01-12 15:14:56 +0000

    Current progress on testing/implementing

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -1,3 +1,28 @@
 class GameEngine
-  
+
+  attr_reader :guess1, :computer
+
+  def initialize(guess1:, computer:)
+    @guess1 = guess1
+    @computer = computer
+  end
+
+  def winner_is
+    puts guess1
+    if game_rules[guess1.to_sym].include? @computer
+      'Player 1 Wins'
+    elsif game_rules[computer.to_sym].include? @guess1
+      'Player 2 Wins'
+    else
+      "Sorry it was a Tie!"
+    end
+  end
+
+  def game_rules
+    {
+        'Paper': ['Rock'],
+        'Rock': ['Scissors'],
+        'Scissors': ['Paper']
+    }
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,22 @@ require './lib/game_engine'
 
 describe GameEngine do
   it 'returns an instance of itself' do
-    expect(subject).to be_an_instance_of(GameEngine)
+    game_engine = GameEngine.new(guess1: 'Paper', computer: 'Rock')
+    expect(game_engine).to be_instance_of(GameEngine)
+  end
 
+  it 'returns player 1 as winner' do
+    p_1_winner = GameEngine.new(guess1: 'Rock', computer: 'Paper')
+    expect(p_1_winner).to eq('Player 1 Wins')
+  end
+
+  it 'returns player 2 as winner' do
+    p_2_winner = GameEngine.new(guess1: 'Paper', computer: 'Rock')
+    expect(p_2_winner).to eq('Player 1 Wins')
+  end
+
+  it 'returns a tie' do
+    tie = GameEngine.new(guess1: 'Rock', computer: 'Rock')
+    expect(tie).to eq('Player 1 Wins')
   end
 end
