d8ac4623d5a4d236af6649b04919ff61dc1bfc31
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-13 16:23:25 +0000

    Test.2 - test that name given in name shown on next page

--- spec/feature/enter_name_spec.rb
+++ spec/feature/enter_name_spec.rb
@@ -1,12 +1,15 @@
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
 
 
 
-# feature 'name form' do
-#   scenario 'checks name entered equales name from form' do
-#     visit '/'
-#     fill_in 'name', with: "Luke"
-#     click_button 'Play'
+feature 'name form' do
+  scenario 'checks name entered equales name from form' do
+    visit '/'
+    fill_in 'name', with: "Luke"
+    click_button 'Play'
 
-#     expect(page).to have_content "Welcome Luke"
-#   end
-# end 
+    expect(page).to have_content "Welcome Luke"
+  end
+end 
