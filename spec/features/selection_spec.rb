00e5d3389fa1797955c4e27a180544bb88b2a032
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-13 09:43:27 +0000

    Fixed some failing tests

--- spec/features/selection_spec.rb
+++ spec/features/selection_spec.rb
@@ -1,82 +1,91 @@
 feature 'Selecting rock paper or scissors' do
   scenario 'player_1 selects Rock and lucy_the_computer selects Rock' do
+    allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Rock")
     visit('/selection')
     click_button("Rock")
     expect(page).to have_content("You selected Rock!")
-    expect(page).to have_content("Lucy the Computer selected Rock!")
-    expect(page).have_content("Hmm. It's a tie this time")
+    expect(page).to have_content("Lucy The Computer selected Rock!")
+    expect(page).to have_content("Hmm. It's a tie this time")
   end
 
   scenario 'player_1 selects Paper and lucy_the_computer selects Paper' do
+    allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Paper")
     visit('/selection')
     click_button("Paper")
     expect(page).to have_content "You selected Paper!"
-    expect(page).to have_content "Lucy the Computer selected Paper!"
+    expect(page).to have_content "Lucy The Computer selected Paper!"
     expect(page).to have_content "Hmm. It's a tie this time"
   end
 
   scenario 'player_1 selects Scissors and lucy_the_computer selects Scissors' do
+    allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Scissors")
     visit('/selection')
     click_button("Scissors")
     expect(page).to have_content "You selected Scissors!"
-    expect(page).to have_content "Lucy the Computer selected Scissors!"
+    expect(page).to have_content "Lucy The Computer selected Scissors!"
     expect(page).to have_content "Hmm. It's a tie this time"
   end
   
 scenario 'player_1 selects Rock and lucy_the_computer selects Paper' do
+  allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Paper")
   visit('/selection')
   click_button("Rock")
   expect(page).to have_content "You selected Rock!"
-  expect(page).to have_content "Lucy the Computer selected Paper!"
+  expect(page).to have_content "Lucy The Computer selected Paper!"
   expect(page).to have_content "Bad luck! You were beaten this time"
 end
 
 
 scenario 'player_1 selects Paper and lucy_the_computer selects Rock' do
+  allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Computer")
   visit('/selection')
   click_button("Paper")
   expect(page).to have_content "You selected Paper!"
-  expect(page).to have_content "Lucy the Computer selected Rock!"
+  expect(page).to have_content "Lucy The Computer selected Rock!"
   expect(page).to have_content "Congratulations! You beat Lucy the Computer!!!"
 
 end
 
 
 scenario 'player_1 selects Rock and lucy_the_computer selects Scissors' do
+  allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Computer")
   visit('/selection')
   click_button("Rock")
   expect(page).to have_content "You selected Rock!"
-  expect(page).to have_content "Lucy the Computer selected Scissors!"
+  expect(page).to have_content "Lucy The Computer selected Scissors!"
   expect(page).to have_Content "Congratulations! You beat Lucy the Computer!!!"
 end
 
 
 
 scenario 'player_1 selects Paper and lucy_the_computer selects Scissors' do
+  allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Scissors")
   visit('/selection')
   click_button("Paper")
   expect(page).to have_content "You selected Paper!"
-  expect(page).to have_content "Lucy the Computer selected Scissors!"
+  expect(page).to have_content "Lucy The Computer selected Scissors!"
   expect(page).to have_content "Bad luck! You were beaten this time"
 end
 
 
 
 scenario 'player_1 selects Scissors and lucy_the_computer selects Paper' do
+  allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Computer")
   visit('/selection')
   click_button("Scissors")
   expect(page).to have_content "You selected Scissors!"
-  expect(page).to have_content "Lucy the Computer selected Paper!"
+  expect(page).to have_content "Lucy The Computer selected Paper!"
   expect(page).to have_content "Congratulations! You beat Lucy the Computer!!!"
 end
 
 
 
 scenario 'player_1 selects Scissors and lucy_the_computer selects Rock' do
+  allow_any_instance_of(RPSGame).to receive(:computer_selection).and_return("Rock")
   visit('/selection')
   click_button("Scissors")
   expect(page).to have_content "You selected Scissors!"
-  expect(page).to have_content "Lucy the Computer selected Rock!"
+  expect(page).to have_content "Lucy The Computer selected Rock!"
   expect(page).to have_content "Bad luck! You were beaten this time"
 end
 
