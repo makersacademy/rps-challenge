1859cbac9930ffacab358b2b97083b02322548a9
JCWBiles <jcbiles@outlook.com>
2020-01-13 15:42:14 +0000

    added html to /play - there are attack buttons

--- rps_web.rb
+++ rps_web.rb
@@ -9,6 +9,13 @@ class RPSWeb < Sinatra::Base
 
   post '/names' do
     session['player_1'] = params[:player_1]
+    redirect '/play'
   end
 
+  get '/play' do
+    @player_1 = session['player_1']
+    erb :play
+  end
+
+  run! if app_file == $0
 end
