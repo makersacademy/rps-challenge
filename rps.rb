dda8260393cda45f8e5a2eb3fe8127446fa69ea7
Masha N <marianetchaeva@hotmail.com>
2020-01-13 11:19:09 +0000

    player can enter name and see confirmation

--- rps.rb
+++ rps.rb
@@ -3,9 +3,14 @@ require 'sinatra/base'
 
 class Rps < Sinatra::Base
   get '/' do
-    "This is a RPS game -- update"
+    erb :index
   end
 
-  
+  post '/names' do
+    @player = params[:player_name]
+    erb :play
+  end
+
+
 run! if app_file == $0
 end
