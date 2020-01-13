c832aeb8a2d7d13c6fdd9669a61a8fc861f09b8a
Rhodine <rorleans_lindsay@yahoo.co.uk>
2020-01-13 11:17:11 +0000

    I created a an .erb file and within it created a 'player_1_name' field to resolve the previous failing test. Another failing test now appears as no submit button could be found

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 
 class Rps < Sinatra::Base
   get '/' do
-    'Welcome to Rock Paper Scissors!'
+    erb :index
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
