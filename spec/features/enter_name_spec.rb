598ed2380e6a960970121263b7c4973e7688e741
Carmen-Alina <carmen.panu@gmail.com>
2020-01-13 10:59:15 +0000

    Capybara feature test that expects player to fill in his name (in a form), submit that form, and see his name on-screen

---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,8 @@
+feature "Fill in and see Player's name" do
+    scenario "at the begining of the game" do
+      visit '/'
+      fill_in :player_1_name, with: "Carmen"
+      click_button "Register"
+      expect(page).to have_content 'Carmen make your choice!'
+    end
+end
