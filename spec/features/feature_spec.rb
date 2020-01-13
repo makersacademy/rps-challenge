9356fe490abc30906297ecdb5770fac377737a2b
Max O'Sullivan <maxosully@gmail.com>
2020-01-13 14:34:22 +0000

    next feature added

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -7,4 +7,13 @@ feature 'testing features' do
     expect(page).to have_content 'Max'
   end
 
+  scenario 'user should be able to pick an option from rock paper or scissors' do
+    visit('/')
+    fill_in 'name', with: 'Max'
+    click_button 'Start game'
+    click_button 'Rock'
+    expect(page).to have_content 'Your choice: '
+  end
+
+
 end
