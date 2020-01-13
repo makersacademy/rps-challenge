a65dceecd7fa9f4b7088c15cc5e9da28ce20a5c4
Carmen-Alina <carmen.panu@gmail.com>
2020-01-13 13:17:07 +0000

    Capybara feature test that expects player see rock-paper-scissors choices

---
+++ spec/features/choices_spec.rb
@@ -0,0 +1,12 @@
+feature 'Choices' do
+  context 'when the game starts' do
+    scenario 'player is presented 3 choices' do
+      visit '/'
+      fill_in :player_1_name, with: "Carmen"
+      click_button "Register"
+      expect(page).to have_button "Rock"
+      expect(page).to have_button "Paper"
+      expect(page).to have_button "Scissors"
+    end
+  end
+end
