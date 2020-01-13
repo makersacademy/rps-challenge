e6dfecc1af0882040c1910d2b0eb1d9681d68c76
Rhodine <rorleans_lindsay@yahoo.co.uk>
2020-01-13 11:33:19 +0000

    The test now passes as I included a post method in my form which when routed to /names, calls the play.erb file containing the correct content.

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class Rps < Sinatra::Base
     erb :index
   end
 
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    erb :play
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
