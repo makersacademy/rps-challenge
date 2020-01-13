6fb617e89dd88e088cb784347f7ebb8a44470da4
Aaron Redden <aaron.redden01@vodafone.com>
2020-01-13 16:20:26 +0000

    added name for and removed infrastracture test

--- spec/features/enter_names_spec.rb
+++
@@ -1,8 +0,0 @@
-feature 'Enter names' do
-    scenario 'submitting names' do
-      visit('/')
-      fill_in :player_1_name, with: 'Greg'
-      click_button 'Submit'
-      expect(page).to have_content 'Greg to play'
-    end
-  end
