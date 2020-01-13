d8ffb96435aa3a340f1872b0a610b8e5cb57be49
Lucian Boatright <lucian.boatright@gmail.com>
2020-01-13 16:47:05 +0000

    Test.3 - check choice made is shown on next page

--- spec/feature/out_put_spec.rb
+++ spec/feature/out_put_spec.rb
@@ -1,12 +1,14 @@
+# As a marketeer
+# So that I can enjoy myself away from the daily grind
+# I would like to be able to play rock/paper/scissors
 
 
-
-# feature 'Choice selection' do
-#   scenario 'Checks that button choice is selected' do
-#     visit '/'
-#     fill_in 'name', with: "Luke"
-#     click_button 'Play'
-#     click_button 'Rock'
-#     expect(page).to have_content('Rock')
-#   end
-# end
+feature 'Choice selection' do
+  scenario 'Checks that button choice is selected' do
+    visit '/'
+    fill_in 'name', with: "Luke"
+    click_button 'Play'
+    click_button 'Rock'
+    expect(page).to have_content('Rock')
+  end
+end
