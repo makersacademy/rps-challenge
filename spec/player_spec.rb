6e6278ac83afbd35d17dcbc7a910aa0648a11243
Rhodine <rorleans_lindsay@yahoo.co.uk>
2020-01-13 14:44:33 +0000

    I have included a failing test for undefined method 'points'

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,16 @@ require 'player'
 
 describe Player do
   subject(:dave) { Player.new('Dave') }
-  
+  subject (:computer) {Player.new('Computer')}
+
   describe '#name' do
     it 'returns the name' do
       expect(dave.name).to eq 'Dave'
+    end
+  end
+  describe '#scoring points' do
+    it 'shows points' do
+      expect(dave.points).to eq described_class::DEFAULT_POINTS
     end
   end
 end
