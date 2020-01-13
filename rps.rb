e621e617c0fefda1eac4885ae40ad8858273c342
Masha N <marianetchaeva@hotmail.com>
2020-01-13 12:02:36 +0000

    added test to check each move renders correct confirmation

--- rps.rb
+++ rps.rb
@@ -18,6 +18,10 @@ class Rps < Sinatra::Base
     erb :play
   end
 
+  get  '/move' do
+    erb :move
+  end
+
 
 run! if app_file == $0
 end
---
+++ spec/features/move_spec.rb
@@ -0,0 +1,10 @@
+feature 'Choosing a move in the game' do
+  scenario 'confirms which move chosen by player' do
+    visit '/'
+    fill_in :player_name, with: 'Masha'
+    click_button 'Submit'
+    click_button 'Rock'
+    expect(page).to have_content "Your chosen move: Rock"
+    expect(page).not_to have_content "Your chosen move: Paper"
+  end
+end
