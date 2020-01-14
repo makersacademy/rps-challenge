f8a1840f2a96f7fc6a55704a1c5b47f94f0cef6b
Iryna Jurkiv <iryna.jurkiv@gmail.com>
2020-01-14 10:33:14 +0000

    corrected rspec tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,11 +11,11 @@ describe 'Game' do
   end
 
   it "evalutes computer has won"  do
-    expect(game.play("Rock","Paper")).to eq "computer"
+    expect(game.play("Rock","Paper")).to eq "computer wins"
   end
 
   it "evalutes player has won"  do
-    expect(game.play("Scissors","Paper")).to eq "player"
+    expect(game.play("Scissors","Paper")).to eq "player wins"
   end
 
   it "evalutes a draw" do
