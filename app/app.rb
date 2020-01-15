5819d61df57845f7e2de5f9a55f670fe651d81cf
adrian harvey <adrian.harvey@vodafone.com>
2020-01-15 12:43:50 +0000

    enabled Session in app.rb so that the user's name is stored

--- app/app.rb
+++ app/app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
+
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/name' do
     p params
-    @name = params[:name]
-    erb :index
+    session[:name] = params[:name]
+  end
+
+  get '/name' do
+    @name = session[:name]
+    erb :name
   end
 
   run! if app_file == $0
