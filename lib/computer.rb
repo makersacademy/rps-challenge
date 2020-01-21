bab458275f1eedc4ca65f7a8ec21d9b7e900adda
JCWBiles <jcbiles@outlook.com>
2020-01-21 13:18:29 +0000

    player vs computer game working. Next step is to store game wns, loses and draws

---
+++ lib/computer.rb
@@ -0,0 +1,5 @@
+class Computer
+  def weapon
+    ["Rock", "Paper", "Scissors", "Lizard", "Spock"].sample
+  end
+end
--- rps_web.rb
+++ rps_web.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
+require './lib/computer'
+require './lib/game_engine'
 
 class RPSWeb < Sinatra::Base
   enable :sessions
   
   get '/' do
-    erb :index
+    erb :index, :locals => {:player_1 => session['player_1']}
   end
 
   post '/names' do
@@ -13,13 +15,22 @@ class RPSWeb < Sinatra::Base
   end
 
   get '/play' do
-    @player_1 = session['player_1']
-    erb :play
+    erb :play, :locals => {:player_1 => session['player_1']}
   end
 
   post '/result' do
-    @player_1 = session['player_1']
-    erb :result
+    session['choice'] = params[:choice]
+    redirect '/result'
+  end
+
+  get '/result' do
+    @comp_weapon = Computer.new.weapon
+    erb :result, :locals => {:player_1 => session['player_1'],
+                             :choice => session['choice'],
+                             :comp_choice => @comp_weapon,
+                             :winner_is => Game.new(session['choice'], @comp_weapon).winner_is
+                            }
+
   end
 
   run! if app_file == $0
