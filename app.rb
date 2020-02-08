770deaa1996ede255f28fdaa61389066f9b7d919
Paulo Azoia <pazoia.gomes@gmail.com>
2020-02-08 16:11:20 +0000

    Refactored app.rb and enabled sessions
    players is now redirecting to the 'arena erb'

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :home
   end
 
   post '/players' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/arena'
+  end
+
+  get '/arena' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :arena
   end
 
--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -3,7 +3,7 @@ feature 'Register names' do
     visit('/')
     fill_in :player_1_name, with: 'Paulo'
     fill_in :player_2_name, with: 'Helen'
-    click_button 'Move to the arena'
+    click_button 'Lets play'
     expect(page).to have_content 'Paulo vs. Helen'
   end
 end
