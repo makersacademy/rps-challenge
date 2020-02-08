4ef2f0b79e1c3d47cf050c73787bed6aa3bcdf18
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-08 20:09:23 +0000

    Add scissors, paper, rock buttons

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -1,7 +1,10 @@
 feature 'playing a game' do
   scenario 'player has options to choose from' do
     sign_in
-    expect(page).to have_content "Pick an option:"
+    expect(page).to have_content("Pick an option:")
+    expect(page).to have_selector(:link_or_button, "Scissors")
+    expect(page).to have_selector(:link_or_button, "Paper")
+    expect(page).to have_selector(:link_or_button, "Rock")
   end
 end
 
