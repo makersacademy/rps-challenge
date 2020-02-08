343a9d3f7797a56e71a57b281c237a89b2eae57e
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-08 20:48:00 +0000

    Add drop down select list

--- config.ru
+++ config.ru
@@ -1,2 +1,2 @@
-require_relative "./app.rb"
+require "./app/app.rb"
 run RockPaperScissors
--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -2,9 +2,9 @@ feature 'playing a game' do
   scenario 'player has options to choose from' do
     sign_in
     expect(page).to have_content("Pick an option:")
-    expect(page).to have_selector(:link_or_button, "Scissors")
-    expect(page).to have_selector(:link_or_button, "Paper")
-    expect(page).to have_selector(:link_or_button, "Rock")
+    expect(page).to have_content("Scissors")
+    expect(page).to have_content("Paper")
+    expect(page).to have_content("Rock")
   end
 end
 
