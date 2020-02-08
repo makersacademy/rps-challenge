aead4425d88b7451854fc8285e244e85ed6b8ae8
Liz Daly <liz7daly@gmail.com>
2020-02-08 15:32:05 +0000

    Add player class
    Adds passing unit test for player class, with name attribute

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative 'lib/player'
 
 class RockPaperScissors < Sinatra::Base
 
---
+++ spec/player_spec.rb
@@ -0,0 +1,7 @@
+describe Player do
+  let(:name) {double :name}
+  subject {described_class.new(name)}
+  it 'has a name' do
+    expect(subject.name).to eq name
+  end
+end
