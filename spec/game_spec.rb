b127c768164a79e4d213d0fb44028cfb7c506919
Jason <jasylwong@gmail.com>
2020-02-08 15:45:34 +0000

    add Game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe Game do
-  subject(:game) { Game.new(player_1, player_2) }
-  let(:player1) { double :player_1}
-  let(:player2) { double :player_2}
-  
+  subject(:game) { described_class.new(player_1, player_2) }
+  let(:player_1) { double :player_1}
+  let(:player_2) { double :player_2}
+
   describe "#new" do
     it 'initializes with instances of player' do
-      expect(game.player).to eq player_1
+      expect(game.player_1).to eq player_1
     end
   end
 end
