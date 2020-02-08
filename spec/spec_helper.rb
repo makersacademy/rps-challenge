c0028e09725f82de2194540a135ce536b40612bc
Josh Blumberg <jlblumberg@gmail.com>
2020-02-08 09:29:22 +0000

    Set up simplecov and new README

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,13 +5,11 @@ require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
 # For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
-
-
 
 RSpec.configure do |config|
   config.after(:suite) do
