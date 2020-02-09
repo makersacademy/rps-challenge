0ca3bdaafcbd79178d9a4a499fcfd4d228adb16b
Chris Eadie <chris.eadie@outlook.com>
2020-02-09 16:24:28 +0000

    Added storing and returning of player move choice

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,16 @@
 class Game
+  attr_reader :player_move
+
   def initialize(name)
     @name = name
+    @player_move = ""
   end
 
   def player_name
     @name
+  end
+
+  def move(choice)
+    @player_move = choice
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,5 +8,18 @@ describe Game do
       expect(game.player_name).to eq "Foobar"
     end
   end
+  
+  describe '#move(selection)' do
+    it 'should set the player move' do
+      expect(game).to respond_to(:move).with(1).argument
+    end
+  end
+
+  describe '#player_move' do
+    it 'should return the player move' do
+      game.move("rock")
+      expect(game.player_move).to eq("rock")
+    end
+  end
 
 end
