790baae96679601d9d130f160772ada10f3e31fb
Ziad <ziad1991@hotmail.co.uk>
2020-02-09 20:49:58 +0000

    reformat spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,16 @@
+require 'simplecov'
+require 'simplecov-console'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara/rspec'
-require 'simplecov'
-require 'simplecov-console'
 require 'capybara'
 require 'rspec'
 require 'features/web_helpers'
@@ -11,12 +18,7 @@ require 'capybara/dsl'
 
 Capybara.app = RockPaperScissors
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
+
 
 # For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
 
