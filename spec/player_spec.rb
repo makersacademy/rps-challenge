3b0eff5a8b6504c4400bb2e7eaae9dcb11514180
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-09 19:28:28 +0000

    Set up player class with passing name tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,17 @@ require 'player'
 
 describe Player do
 
-  
+  subject(:dave) {Player.new("dave")}
+
+  describe " #name" do
+    it 'should respond to name' do
+      expect(subject).to respond_to(:name)
+    end
+
+    it "should return dave" do
+      expect(dave.name).to eq "dave"
+    end
+
+  end
 
 end
