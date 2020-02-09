446e0a879f886acea10efc0237e83874e22a7580
Jason <jasylwong@gmail.com>
2020-02-09 11:07:01 +0000

    refactor css

--- spec/features/computer_spec.rb
+++ spec/features/computer_spec.rb
@@ -2,14 +2,14 @@ feature 'testing computer as player' do
   scenario 'registers computer as a player' do
     visit('/')
     fill_in('player_1_name', with: "Tango")
-    click_button("Submit")
+    click_button("Play!")
     expect(page).to have_content("Tango versus Computer")
   end
 
   scenario 'computer can make a move' do
     visit('/')
     fill_in('player_1_name', with: "Tango")
-    click_button("Submit")
+    click_button("Play!")
     click_button("Paper")
     expect(page).to have_content("Computer chose ")
   end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -2,5 +2,5 @@ def sign_in_and_play_two_players
   visit('/')
   fill_in('player_1_name', with: "Tango")
   fill_in('player_2_name', with: "Cash")
-  click_button("Submit")
+  click_button("Play!")
 end
