6dcfc9d9db6f8fb082d3ccd9d4d11ed518ca9799
Chris Eadie <chris.eadie@outlook.com>
2020-02-09 17:36:03 +0000

    Updated with rubocop formatting requests solved

--- spec/features/game_results_spec.rb
+++ spec/features/game_results_spec.rb
@@ -4,4 +4,9 @@ feature 'Game results' do
     choose_rock
     expect(page).to have_content "wins!"
   end
+  scenario 'show that someone wins' do
+    start_playing
+    choose_rock
+    expect(page).to have_content "wins!"
+  end 
 end
--- spec/features/selecting_move_spec.rb
+++ spec/features/selecting_move_spec.rb
@@ -1,7 +1,7 @@
 feature 'Selecting move' do
   scenario 'three moves show' do
     start_playing
-    expect(page).to have_select('move', :options => ['Rock', 'Paper','Scissors'])
+    expect(page).to have_select('move', :options => ['Rock', 'Paper', 'Scissors'])
   end
   scenario 'submitting shows move' do
     start_playing
