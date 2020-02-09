1346d0549d725d808bfa90953aee4d1d3d404529
Megan Hayes <student@makerss-air.home>
2020-02-09 08:26:34 +0000

    add outcome erb

--- app.rb
+++ app.rb
@@ -1,12 +1,30 @@
 require 'sinatra/base' 
 require './lib/rps.rb'
+# require './lib/player_name.rb'
+# require './lib/outcome.rb'
 
 class RockPaperScissors < Sinatra::Base
   enable :sessions
-
-    get '/' do
+    get '/' do 
       erb :index
     end
+
+    post '/names' do
+      session['player_name'] = params[:player_name]
+  
+      redirect '/player_move'
+    end
+
+    get '/player_move' do
+      @player_name = session
+      ['player_name']
+
+    erb :player_move
+    end
+
+    get '/outcome' do
+      erb :outcome
+    end  
 
     run! if app_file == $0
 end
---
+++ lib/outcome.rb
@@ -0,0 +1,22 @@
+# class Outcome
+
+
+#     if player_move == 'Scissors' && @move == 'Scissors'
+#       'We drew!'
+#     elsif player_move == 'Scissors' && @move == 'Rock'
+#       'You lose!'
+#     elsif player_move == 'Scissors' && @move == 'Paper'
+#       'You won!'
+#     elsif player_move == 'Rock' && @move == 'Rock'
+#       'We drew!'
+#     elsif player_move == 'Rock' && @move == 'Paper'
+#       'You lose!'
+#     elsif player_move == 'Rock' && @move == 'Scissors'
+#       'You won!'
+#     elsif player_move == 'Paper' && @move == 'Rock'
+#       'You won!'
+#     elsif player_move == 'Paper' && @move == 'Paper'
+#       'We drew!'
+#     elsif player_move == 'Paper' && @move == 'Scissors'
+#       'You lose!'
+#     end
