874ee603bcac0cb694947d4f4a6a3f789021f8a4
DavidStewartLDN <davidstewart.work@gmail.com>
2020-02-09 22:04:48 +0000

    Implemented game class with all tests passing

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/player'
+require './lib/game'
 
 class Rps < Sinatra::Base
 
@@ -10,12 +11,13 @@ class Rps < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
+    player_1 = Player.new(params[:player_1_name])
+    $game = Game.new(player_1)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
+    @game = $game
     erb :play
   end
 
