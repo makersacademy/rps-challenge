59c12d261ab89c527a2330fadde7558549fd74d0
sgneha <sgneha05@gmail.com>
2020-03-07 22:45:53 +0000

    Buttons RPS for choosing

--- spec/features/play_game_spec.rb
+++ spec/features/play_game_spec.rb
@@ -3,4 +3,14 @@ feature 'start playing' do
     sign_in_and_play
     expect(page).to have_content 'Welcome Neha'
   end
+  scenario 'Ask user to choose move' do
+    sign_in_and_play
+    expect(page).to have_content 'Choose your move'
+  end
+  scenario 'Give options to choose' do
+    sign_in_and_play
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Paper'
+    expect(page).to have_button 'Scissors'
+  end
 end
