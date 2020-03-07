0b9c09f750fe160ef6e48b65d63fec37e4ef32c8
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-07 12:28:37 +0000

    Updated spec-helper

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 require './lib/game'
 
-class RockPaperSissors < Sinatra::Base
+class RockPaperScissors < Sinatra::Base
 
   run! if app_file == $PROGRAM_NAME
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@ ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
-Capybara.app = Battle
+Capybara.app = RockPaperScissors
 
 RSpec.configure do |config|
   config.after(:suite) do
