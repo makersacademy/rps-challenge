107f32fc3fb4e397ad5511ed9a14c48af500db67
Jara Santamaria <jarasmar@gmail.com>
2020-03-07 14:28:38 +0000

    correct test to test random computer choice

--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -1,3 +1,5 @@
+OPPONENT_RAND = 0
+
 feature 'Enter names' do
   scenario 'Player can input and submit a name' do
     sign_in_and_play
@@ -12,10 +14,6 @@ feature 'Choose moves' do
     expect(page).to have_button 'Scissors'
     expect(page).to have_button 'Paper'
   end
-
-  scenario 'Computer can choose a random move' do
-
-  end
 end
 
 feature "See result" do
@@ -26,7 +24,10 @@ feature "See result" do
   end
 
   scenario 'I can see my oponent choice' do
-
+    sign_in_and_play
+    srand(OPPONENT_RAND) 
+    click_button 'Rock'
+    expect(page).to have_content 'Your oponent choice is Scissors'
   end
 
   scenario 'I can see the winner' do
