9a25d457148b92268d05ffb275763908ae5bbb1f
liam roberts <liamroberts77@googlemail.comgit config --global user.email liamroberts77@googlemail.com>
2020-03-08 14:29:58 +0000

    added name form and redirect with displayed name

--- spec/feature/register_spec.rb
+++ spec/feature/register_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper.rb'
 
-# As a marketeer
-# So that I can see my name in lights
-# I would like to register my name before playing an online game
-
-feature "register my name "
-scenario "registering and viewing my name"
-vsist "/"
-fill_in "name" with: "Liam"
-click_button "submit"
-expect(page) to have_content "Liam"
+feature "register my name" do
+  scenario "input and view my name" do
+    visit "/"
+    fill_in "name", with: "Liam"
+    click_button "submit"
+    expect(page).to have_content "Liam"
+  end
+end
