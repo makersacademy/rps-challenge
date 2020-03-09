1d88ba751b028b2d945abbd8abc5c4eb0156e0cc
Patrick Oliver <poliver24@hotmail.com>
2020-03-09 09:41:55 +0000

    RPS fixed

--- spec/feature/enter_names_spec.rb
+++ spec/feature/enter_names_spec.rb
@@ -2,7 +2,7 @@ feature 'Enter names' do
   scenario 'submitting names' do
   visit('/')
   fill_in :player_1_name, with: 'Patrick'
-  click_button 'Begin Game'
+  click_button 'One_Off'
   expect(page).to have_content 'Hello Patrick'
   end
 end
