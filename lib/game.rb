d628d63180d1d80cb24e6d7394ac236475147453
srikaanthtb <srikaanth.t.b@gmail.com>
2020-03-09 10:52:51 +0000

    remove redundant code

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,11 @@
 class Game
     attr_reader :choice, :computer_choice, :c_choice
 
-    options = ["Rock", "Paper", "Scissors"]
+    options = ["Rock", "Paper", "Scissors", "Spock", "Lizard"]
 
     def computer_choice
         @computer_choice = options.sample
-        @c_choice = options.sample
+      
     end
 
     def player_choice(choice)
@@ -23,6 +23,14 @@ class Game
             @computer_choice == "Scissors" ? "Computer wins!" :
             "It's a tie!"
         when "Scissors"
+            @computer_choice == "Rock" ? "Computer wins!" :
+            @computer_choice == "Paper" ? "You win!" :
+            "It's a tie!"
+        when "Spock"
+            @computer_choice == "Rock" ? "Computer wins!" :
+            @computer_choice == "Paper" ? "You win!" :
+            "It's a tie!"
+        when "Lizard"
             @computer_choice == "Rock" ? "Computer wins!" :
             @computer_choice == "Paper" ? "You win!" :
             "It's a tie!"
