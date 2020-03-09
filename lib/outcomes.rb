d506ed144923573bffc77839c05bb99958848988
liam roberts <liamroberts77@googlemail.com>
2020-03-09 10:53:24 +0000

    refining

--- lib/outcomes.rb
+++ lib/outcomes.rb
@@ -3,12 +3,11 @@ class Calculator
     RULES = { Rock: [:Scissors],
           Paper: [:Rock],
           Scissors: [:Paper],
-          Lizard: [:Paper],
-          Spock: [:Rock] }
+        }
   
     def calculate_winner
-      return 'Draw' if $computer_choice == $player_choice
-      RULES[$player_choice].include?($computer_choice) ? $player_name.to_s : 'The cpu'
+      return 'NO WINNER its a Draw' if $computer_choice == $player_choice
+      RULES[$player_choice].include?($computer_choice) ? "#{$player_name.to_s} WINS" : "The cpu WINS"
     end
   
   end
