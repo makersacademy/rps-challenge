8b2170fc6246c461069e22a647ce103e86d7966c
Alex <aclifford336@gmail.com>
2020-03-24 12:30:27 +0000

    post makers clean-up refactored lib folder to only have a game class - player class was redundant - added tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,29 +1,23 @@
-require_relative 'player'
-
 class Game
-
-  attr_reader :player_1, :player_2, :game
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
   end
 
-  def self.create(player_1, player_2)
-    @game = Game.new(player_1, player_2)
+  def computer_move
+    ["Rock", "Paper", "Scissors"].sample
   end
 
-  def self.instance
-    @game
-  end
-
-  def winner
-    if @player_1.move == @player_2.move
-      return "It's a draw!"
-    elsif (@player_1.move == "Rock" && @player_2.move == "Scissors") || (@player_1.move == "Scissors" && @player_2.move == "Paper") || (@player_1.move == "Paper" && @player_2.move == "Rock")
-      return @player_1
+  def winner(player_1_move, player_2_move)
+    if player_1_move == player_2_move
+      "It's a draw!"
+    elsif (player_1_move == "Rock" && player_2_move == "Scissors") ||
+      (player_1_move == "Scissors" && player_2_move == "Paper") ||
+      (player_1_move == "Paper" && player_2_move == "Rock")
+      @player_1
     else
-      return @player_2
+      @player_2
     end
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,22 +2,26 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new(player_1, player_2) }
   let(:player_1) { double :player }
   let(:player_2) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
 
-  describe '#player_1' do
-    it 'retrieves the first player' do
-      expect(game.player_1).to eq player_1
-    end
+
+  it 'recognises when player_1 wins' do
+    expect(game.winner("Rock", "Scissors")).to eq(player_1)
   end
 
-  describe '#winner' do
-    it 'returns the winner of the round' do
-      allow(player_1).to receive(:move).and_return("Paper")
-      allow(player_2).to receive(:move).and_return("Rock")
-      expect(game.winner).to eq player_1
-    end
+  it 'recognises when player_2 wins' do
+    expect(game.winner("Paper", "Scissors")).to eq(player_2)
+  end
+
+  it 'recognises when its a draw' do
+    expect(game.winner("Rock", "Rock")). to eq("It's a draw!")
+  end
+
+  it 'can randomly assign a move to the computer' do
+    expect(game).to receive(:computer_move).and_return("Rock")
+    expect(game.computer_move).to eq("Rock")
   end
 
 end
