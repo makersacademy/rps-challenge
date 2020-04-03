489c9559815f0b87a41bc98473e7b3345f467b28
Edward Phillips <edd0ddi0@gmail.com>
2020-04-03 08:37:01 +0100

    RPS changed to roshambo - Roshambo spec test - after choosing to play should be able to choose paper - failing

--- spec/features/playing_roshambo_spec.rb
+++ spec/features/playing_roshambo_spec.rb
@@ -3,4 +3,8 @@ feature "User Story 2 - Playing Roshambo" do
     named_player
     expect(page).to have_button('Rock!')
   end
+  scenario "after choosing to play should be able to choose paper" do
+    named_player
+    expect(page).to have_button('paper!')
+  end
 end
