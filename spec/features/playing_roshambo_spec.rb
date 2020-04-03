3d61c1bd61b70b8f2e99183aabfcc193ef91ad5a
Edward Phillips <edd0ddi0@gmail.com>
2020-04-03 18:27:42 +0100

    test - results page shoudl announce the winner - failing

--- spec/features/playing_roshambo_spec.rb
+++ spec/features/playing_roshambo_spec.rb
@@ -1,15 +1,15 @@
 feature "User Story 2 - Playing Roshambo - Part 1: choose your weapon: " do
   scenario "after choosing to play should be able to choose rock" do
     named_player
-    expect{choose('r')}.to_not raise_error
+    expect { choose('r') }.to_not raise_error
   end
   scenario "after choosing to play should be able to choose paper" do
     named_player
-    expect{choose('p')}.to_not raise_error
+    expect { choose('p') }.to_not raise_error
   end
   scenario "after choosing to play should be able to choose scissors" do
     named_player
-    expect{choose('s')}.to_not raise_error
+    expect { choose('s') }.to_not raise_error
   end
   scenario "You should be able to tell the paper button is for paper." do
     named_player
@@ -32,5 +32,14 @@ feature "User story 2 - Playing Roshambo - Part 2: did I win!?: " do
     click_button('submit')
     expect(page).to have_current_path('/ROSHAMBO')
   end
+  scenario "results page should announce the winner" do
+    # at this point I'm leaving a placeholder test to see if the page has content
+    # as I am unsure of the best way of testing this feature without knowing the winner
+    # in advance - I guess the solution is to force the winner, but I'm not gonna worry about
+    # that at the moment.
+    named_player
+    choose('r')
+    click_button('submit')
+    expect(page).to have_content('The Winner is:') 
+  end
 end
-
