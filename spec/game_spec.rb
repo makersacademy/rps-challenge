0f1c4c9b839fcda3e2da81434f990e4d6c311ad0
Edward Phillips <edd0ddi0@gmail.com>
2020-04-03 20:05:54 +0100

    refactor - refactored previous tests to test each player being held by game separately - passing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,8 +5,12 @@ describe Game do
   let(:player2) { double :player }
   let(:rps) { Game.new(first:player1,second:player2) }
 
-  it " should be able to hold the two game players" do
+  it " should be able to hold the two game players Part 1" do
     expect(rps.player_one).to eq(player1)
   end
-
+  
+  it " should be able to hold the two game players Part 2" do
+    expect(rps.player_two).to eq(player2)
+  end
+  
 end
