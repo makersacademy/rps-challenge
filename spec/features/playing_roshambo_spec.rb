cd55ffa66eb8faf889bfa60841de9270144f89bc
Edward Phillips <edd0ddi0@gmail.com>
2020-04-03 12:28:57 +0100

    refactor - changed buttons in form for rock/paper/scissors to radio option - tests pass

--- spec/features/playing_roshambo_spec.rb
+++ spec/features/playing_roshambo_spec.rb
@@ -1,14 +1,14 @@
-feature "User Story 2 - Playing Roshambo" do
+feature "User Story 2 - Playing Roshamb - Part 1: choose your weapon." do
   scenario "after choosing to play should be able to choose rock" do
     named_player
-    expect(page).to have_button('Rock!')
+    expect(page).to have_content('Rock, an ancient but powerful weapon.')
   end
   scenario "after choosing to play should be able to choose paper" do
     named_player
-    expect(page).to have_button('Paper!')
+    expect(page).to have_content('Paper, the stuff history books are made of.')
   end
   scenario "after choosing to play should be able to choose scissors" do
     named_player
-    expect(page).to have_button('Scissors!')
+    expect(page).to have_content('Scissors, sharp yet fragile.')
   end
 end
