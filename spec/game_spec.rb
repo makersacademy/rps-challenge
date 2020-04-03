179a241cc3509c4120f93dc6aafa8b8a4bb5327a
Edward Phillips <edd0ddi0@gmail.com>
2020-04-03 22:01:06 +0100

    test - Game should be able to access and instance of the game using class methods - failing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -16,4 +16,8 @@ describe Game do
   it " should be able to create an instance of itself using a class method" do
     expect { Game.start_game(one:player1, two:player2) }.to_not raise_error
   end
+  it " should be able to access an instance of the game using class methods" do
+    Game.start_game(one:player1, two:player2)
+    expect(Game.current_game).to be_instance_of(Game)
+  end
 end
