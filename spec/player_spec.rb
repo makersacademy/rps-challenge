cafb9048bb5b68c3b2ad1b076cd2bf09b0ae55f1
Edward Phillips <edd0ddi0@gmail.com>
2020-04-03 22:46:56 +0100

    test - Player should be able to remember the player's name - failing

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,4 +4,8 @@ describe Player do
   it " should be able to store player's name" do
     expect { Player.new("Ed") }.to_not raise_error
   end
+  it " should be able to remember the player's name" do
+    player = Player.new("Ed")
+    expect(player.name).to eq("Ed")
+  end
 end
