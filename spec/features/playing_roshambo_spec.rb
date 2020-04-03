25d239b75f321959823c9059880b008041ba1098
Edward Phillips <edd0ddi0@gmail.com>
2020-04-03 08:42:51 +0100

    Roshambo spec test - after choosing to play should be ablet o choose scissors - failing. Refactor paper to upper case to match rock

--- spec/features/playing_roshambo_spec.rb
+++ spec/features/playing_roshambo_spec.rb
@@ -5,6 +5,10 @@ feature "User Story 2 - Playing Roshambo" do
   end
   scenario "after choosing to play should be able to choose paper" do
     named_player
-    expect(page).to have_button('paper!')
+    expect(page).to have_button('Paper!')
+  end
+  scenario "after choosing to play should be able to choose scissors" do
+    named_player
+    expect(page).to have_button('Scissors!')
   end
 end
