4a2de47b678e3d42eb8efa18b32ff37720092915
Alex L <macbookpro@macbooks-mbp.home>
2020-04-04 16:43:29 +0100

    test - take_turn gives rock - fail

--- spec/ai_turn_spec.rb
+++ spec/ai_turn_spec.rb
@@ -4,4 +4,8 @@ describe Aiplayer do
   it "responds to #take_turn" do
     expect(subject).to respond_to :take_turn
   end
+
+  it "#take_turn gives rock" do
+    expect(subject.take_turn).to eq 'rock'
+  end
 end
