f8beceacc1871fb1a7fcc506baa461f6043ff90a
tommyrharper <tommyrharper@gmail.com>
2020-04-04 21:30:28 +0100

    test failed - computer selects rock, paper or scissors

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -1,5 +1,10 @@
 require 'computer'
 
 describe Computer do
-  
+  subject(:computer) { Computer.new }
+
+  it 'selects randomly either rock, paper or scissors' do
+    expect(computer.move).to eq('Rock').or eq('Paper').or eq('Scissors')
+  end
+
 end
