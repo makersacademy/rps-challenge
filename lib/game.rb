11483d11f53cc947e24abc967747ad07ee8590c7
tommyrharper <tommyrharper@gmail.com>
2020-04-04 22:44:21 +0100

    test passed - player loses with rock against paper

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,32 @@
+require_relative 'player'
+require_relative 'computer'
+
 class Game
 
   attr_reader :result
 
+  RULES = { rock: :scissors,
+    paper: :rock,
+    scissors: :paper }
+
   def initialize(player, computer)
     @player = player
     @computer = computer
-    @result = :win
+    #beats ? (@result = :win ) : (@result = :lose )
+    p @computer.move
+    if beats == true
+      @result = :win 
+    else
+      @result = :lose
+    end
+  end
+
+  def beats
+    if RULES[@player.move] == @computer.move
+      true
+    else
+      false
+    end
   end
 
 end
