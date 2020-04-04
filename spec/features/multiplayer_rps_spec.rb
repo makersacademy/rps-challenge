32e4698afd0ee355a2174486f972c347aa30b8d3
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-04 13:02:16 +0100

    Update feature tests

--- spec/features/multiplayer_rps_spec.rb
+++ spec/features/multiplayer_rps_spec.rb
@@ -1,12 +1,19 @@
 feature 'the game can be played by two players' do
   scenario 'the game allows player 1 to choose a move' do
-    register_two_and_play
+    register_and_play
     expect(page).to have_content 'Choose a move Phil!'
   end
 
   scenario 'the game allows player 2 to choose a move' do
-    register_two_and_play
+    register_and_play
     click_button 'Rock'
     expect(page).to have_content 'Choose a move Su!'
+  end
+
+  scenario 'both players chooses their moves and are shown the result' do
+    register_and_play
+    click_button 'Paper'
+    click_button 'Paper'
+    expect(page).to have_content "It's a draw - both players chose paper"
   end
 end
