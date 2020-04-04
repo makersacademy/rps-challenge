6ccbc6dded3a0c81de03fc45562dcb182b2fd2c8
Harry Turnbull <hturnbull@live.co.uk>
2020-04-04 22:37:36 +0100

    rf to use web helpers

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,15 +1,11 @@
 feature 'Enter Name' do
   scenario 'Enter Dave and submit, see Dave' do
-    visit '/'
-    fill_in('name', with: 'Dave')
-    click_button('Go!')
+    dave_sign_in
     expect(page).to have_content 'Dave'
   end
 
   scenario 'Enter Jim and submit, see Jim' do
-    visit '/'
-    fill_in('name', with: 'Jim')
-    click_button('Go!')
+    jim_sign_in
     expect(page).to have_content 'Jim'
   end
 end
