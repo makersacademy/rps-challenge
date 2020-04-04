3f6aa761163e3fcf68a472648047fb22a382fec3
Faye Carter <fayecarter333@gmail.com>
2020-04-04 10:12:10 +0100

    win? can return a loss

--- lib/weapon.rb
+++ lib/weapon.rb
@@ -16,4 +16,3 @@ class Weapon
   end
 
 end
-
--- spec/weapon_spec.rb
+++ spec/weapon_spec.rb
@@ -6,12 +6,16 @@ describe Weapon do
   let(:cpu) { described_class.new("Scissors") }
 
   it 'initializes with a type' do
-    expect(weapon).to respond_to (:type)    
+    expect(weapon).to respond_to :type  
   end
 
   describe '#win?' do
     it 'can test winner against opponent weapon' do
       expect(weapon.win?(cpu)).to eq true
+    end
+
+    it 'can test for a loser against opponent weapon' do
+      expect(cpu.win?(weapon)).to eq false
     end
   end
   
