2658c101f773e9474c3f38f907ab672893b72269
Faye Carter <fayecarter333@gmail.com>
2020-04-04 12:00:42 +0100

    Game creates a cpu charecter

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,12 @@
+require_relative 'cpu'
+
 class Game
 
-  attr_reader :player
-  
+  attr_reader :player, :cpu
+
   def initialize(player)
     @player = player
+    @cpu = CPU.new
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,12 @@ require 'game'
 describe Game do
   
   let(:game) {described_class.new("Player")}
+
   it 'initailizes with a player' do
     expect(game).to respond_to :player
+  end
+
+  it 'initailizes with a cpu' do
+    expect(game).to respond_to :cpu
   end
 end
