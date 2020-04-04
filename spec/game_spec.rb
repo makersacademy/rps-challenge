594ec98ec42e4c0585efd22c5de74cef90a1b83f
tommyrharper <tommyrharper@gmail.com>
2020-04-05 00:12:14 +0100

    game_spec.rb tests refactored

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,16 +13,14 @@ describe Game do
     player_double = double :Player, move: :rock
     computer_double = double :Computer, move: :scissors
     game = Game.new(player_double, computer_double)
-    expect(game.wins?).to eq true
-    #expect(game.result).to eq :win
+    expect(game.result).to eq :win
   end
 
   it 'player loses with rock against paper' do
     player_double = double :Player, move: :rock
     computer_double = double :Computer, move: :paper
     game = Game.new(player_double, computer_double)
-    expect(game.wins?).to eq false
-    #expect(game.result).to eq :lose
+    expect(game.result).to eq :lose
   end
 
   it 'player draws with rock against rock' do
