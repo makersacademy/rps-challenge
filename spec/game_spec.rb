29803ded309c7e918d4e19f74b927653c8aafdb6
tommyrharper <tommyrharper@gmail.com>
2020-04-04 22:13:43 +0100

    test failed - player won with rock against scissors

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,10 +10,17 @@ describe Game do
   # let(:computer) { double :Computer, move: 'Rock' }
 
   it 'player wins with rock against scissors' do
-    player_double = double :Player, move: 'Rock'
-    computer_double = double :Computer, move: 'Scissors'
+    player_double = double :Player, move: :rock
+    computer_double = double :Computer, move: :scissors
     game = Game.new(player_double, computer_double)
     expect(game.result).to eq :win
+  end
+
+  it 'player loses with rock against paper' do
+    player_double = double :Player, move: :rock
+    computer_double = double :Computer, move: :paper
+    game = Game.new(player_double, computer_double)
+    expect(game.result).to eq :lose
   end
   
 end
