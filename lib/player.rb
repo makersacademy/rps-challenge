4a16cd6605704f15b6a8daa2ca14f389a44fb4e7
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-04 08:34:44 +0100

    Add error checks to player class initialisation
    These were specifically added because the name specified is taken
    from input by the user, and cannot guaranteed to be correct.

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,9 @@ class Player
   attr_reader :name
 
   def initialize(name)
+    raise ArgumentError, 'name must be a string' unless name.is_a? String
+    raise ArgumentError, 'name must not be empty string' if name.empty?
+
     @name = name
   end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,4 +5,12 @@ describe Player do
     player = Player.new('Phil')
     expect(player.name).to eq 'Phil'
   end
+
+  it 'the name must be a string' do
+    expect { Player.new(4) }.to raise_error 'name must be a string'  
+  end
+
+  it 'the name must be at least one character in length' do
+    expect { Player.new('') }.to raise_error 'name must not be empty string'
+  end
 end
