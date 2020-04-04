e3ca1443e85efc9dc0b50d8646c47dfa3feb021d
tommyrharper <tommyrharper@gmail.com>
2020-04-04 22:08:52 +0100

    test failed - game class accepts no arguments

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,19 @@
 require 'game'
 
 describe Game do
+  # one off
+  # player_double = double :Player, move: 'Rock'
+
+  # for whole section
+  # subject(:game) { described_class.new(player, computer) }
+  # let(:player) { double :Player, move: 'Paper' }
+  # let(:computer) { double :Computer, move: 'Rock' }
+
+  it 'player wins with rock against scissors' do
+    player_double = double :Player, move: 'Rock'
+    computer_double = double :Computer, move: 'Scissors'
+    game = Game.new(player_double, computer_double)
+    expect(game.result).to eq :win
+  end
   
 end
