27264ff51077db2b2516fc13f50c5248f5024da7
Harry Turnbull <hturnbull@live.co.uk>
2020-04-04 22:53:03 +0100

    added multiplayer_sign_in

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -9,3 +9,10 @@ def jim_sign_in
   fill_in('name', with: 'Jim')
   click_button('Start Single-player!')
 end
+
+def multiplayer_sign_in
+  visit '/'
+  fill_in('player_1_name', with: 'Dave')
+  fill_in('player_2_name', with: 'Jim')
+  click_button('Start Multi-player!')
+end
