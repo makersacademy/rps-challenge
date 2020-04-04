8843dc9360fab27b0183a39158b3f3a68e80ef28
Alex L <macbookpro@macbooks-mbp.home>
2020-04-04 15:06:32 +0100

    refactor to use sessions

--- rps.rb
+++ rps.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class Rps < Sinatra::Base
+  enable :sessions
+
   get '/' do
     "Welcome to Rock Paper Scissors!"
     erb :index
   end
 
   post '/enter_name' do
-    @player_name = params[:enter_name]
-    erb :play
+    session[:player_name] = params[:enter_name]
+    redirect './start_game'
+  end
+
+  get '/start_game' do
+    @player_name = session[:player_name]
+    erb :start_game
   end
 
   # start the server if ruby file executed directly
