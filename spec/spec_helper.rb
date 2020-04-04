73ca6084c63cffbe6cd63e49b9ce78368a1ddc49
Harry Turnbull <hturnbull@live.co.uk>
2020-04-04 11:24:04 +0100

    testing works

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,16 +5,17 @@ require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  SimpleCov::Formatter::HTMLFormatter
+  # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
-# For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+Capybara.app = Rps
 
 RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
+  # config.after(:suite) do
+  #   puts
+  #   puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+  #   puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  # end
 end
