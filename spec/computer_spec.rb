5617f632f1b29c7bd05234d2acecd418e8e0d3aa
Edward Phillips <edd0ddi0@gmail.com>
2020-04-05 10:33:55 +0100

    test - Computer: making a new instance of computer should have a name

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -4,4 +4,9 @@ describe Computer do
   it " should be able to make a new player." do
     expect { Computer.new }.to_not raise_error
   end
+
+  it " making a new instance of computer should have a name" do
+    marvin = Computer.new
+    expect(marvin).to respond_to :name
+  end
 end
