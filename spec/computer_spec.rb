0f4ef7aebbfc20a0928a4d340c3771d3f6396c9b
Edward Phillips <edd0ddi0@gmail.com>
2020-04-05 10:50:14 +0100

    test refactor - refactored previous test to better describe behaviour - failing

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -9,8 +9,8 @@ describe Computer do
     marvin = Computer.new
     expect(marvin).to respond_to :name
   end
-  it " making a new instance of computer should choose a weapon" do
+  it " Computer should be able to pick a weapon" do
     marvin = Computer.new
-    expect(marvin).to respond_to :weapon
+    expect(marvin).to respond_to(:weapon).with(1).argument
   end
 end
