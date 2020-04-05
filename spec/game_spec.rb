1e766a0973341347bc151a560336a7ba64bd8d98
Alex L <macbookpro@macbooks-mbp.home>
2020-04-05 10:16:18 +0100

    add tests to check player & computer return correctly + describe blocks

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,11 +2,24 @@ require 'game'
 
 describe Game do
   let(:game) {Game.new("player", "computer")}
-  it "responds to #player" do
-    expect(game).to respond_to :player
-  end
 
-  it "responds to #computer " do
-    expect(game).to respond_to :computer
+  describe "#player" do
+    it "responds to #player" do
+      expect(game).to respond_to :player
+    end
+
+    it "gives the player name" do
+      expect(game.player).to eq "player"
+    end
+  end 
+
+  describe "#computer" do
+    it "responds to #computer " do
+      expect(game).to respond_to :computer
+    end
+
+    it "gives the computer name" do
+      expect(game.computer).to eq "computer"
+    end
   end
 end
