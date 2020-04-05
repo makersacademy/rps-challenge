020adf59da35f7cecfc9060acd1e04e9837b48d9
Alex L <macbookpro@macbooks-mbp.home>
2020-04-05 10:11:44 +0100

    test - game responds to player & computer - pass

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,12 @@
 require 'game'
 
 describe Game do
+  let(:game) {Game.new("player", "computer")}
   it "responds to #player" do
-    expect(subject).to respond_to :player
+    expect(game).to respond_to :player
   end
 
   it "responds to #computer " do
-    expect(subject).to respond_to :computer
+    expect(game).to respond_to :computer
   end
 end
