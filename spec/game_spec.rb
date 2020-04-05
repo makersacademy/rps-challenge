17c352ff4aff0c4b6cb3e598f0c2dc9d44c0de16
Alex L <macbookpro@macbooks-mbp.home>
2020-04-05 10:41:54 +0100

    test - player = rock & computer = rock - fail

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,37 +1,42 @@
 require 'game'
 
 describe Game do
-  let(:game1) {Game.new("rock", "paper")}
+  let(:game) {Game.new("rock", "paper")}
 
   describe "#player" do
     it "responds to #player" do
-      expect(game1).to respond_to :player
+      expect(game).to respond_to :player
     end
 
     it "gives the player name" do
-      expect(game1.player).to eq "rock"
+      expect(game.player).to eq "rock"
     end
   end
 
   describe "#computer" do
     it "responds to #computer " do
-      expect(game1).to respond_to :computer
+      expect(game).to respond_to :computer
     end
 
     it "gives the computer name" do
-      expect(game1.computer).to eq "paper"
+      expect(game.computer).to eq "paper"
     end
   end
 
   describe "#decide winner" do
+
+    let(:game2) { Game.new("rock", "rock")}
     it "responds to #decide_winner" do
-      expect(game1).to respond_to :decide_winner
+      expect(game).to respond_to :decide_winner
     end
 
     it "when player = rock and computer = paper, computer wins" do
-      expect(game1.decide_winner).to eq "computer wins"
+      expect(game.decide_winner).to eq "computer wins"
     end
 
+    it "when player = rock & computer = rock, it's a draw" do
+      expect(game.decide_winner).to eq "It's a draw"
+    end
 
   end
 end
