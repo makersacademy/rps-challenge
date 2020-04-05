7ae821ad1a1eea538819d0824f5ee994b86fd44d
Harry Turnbull <hturnbull@live.co.uk>
2020-04-05 09:50:13 +0100

    no peeking prompts now work

--- spec/features/multiplayer_spec.rb
+++ spec/features/multiplayer_spec.rb
@@ -1,28 +1,29 @@
 feature 'Multiplayer' do
   scenario 'Dave and Jim enter names, see Dave vs Jim' do
-    multiplayer_sign_in
+    dave_jim_sign_in
     expect(page).to have_content 'Dave vs Jim'
   end
 
   scenario 'Dave and Jim enter names, see no peeking Jim' do
-    multiplayer_sign_in
+    dave_jim_sign_in
     expect(page).to have_content "Dave's choice. No peeking Jim!"
-  end
-
-  scenario 'Pete and Steve enter names, see Pete vs Steve' do
-    multiplayer_sign_in
-    expect(page).to have_content 'Pete vs Steve'
   end
 
   scenario 'Pete and Steve enter names, see no peeking Steve' do
-    multiplayer_sign_in
-    expect(page).to have_content "Dave's choice. No peeking Jim!"
+    pete_steve_sign_in
+    expect(page).to have_content "Pete's choice. No peeking Steve!"
   end
 
   scenario 'Dave and Jim enter names, Dave picks rock, now Jims turn' do
-    multiplayer_sign_in
+    dave_jim_sign_in
     click_button('Rock')
     expect(page).to have_content "Jim's choice. No peeking Dave!"
+  end
+
+  scenario 'Pete and Steve enter names, Pete picks rock, see no peeking Pete' do
+    pete_steve_sign_in
+    click_button('Rock')
+    expect(page).to have_content "Steve's choice. No peeking Pete!"
   end
 
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -10,9 +10,16 @@ def jim_sign_in
   click_button('Start Single-player!')
 end
 
-def multiplayer_sign_in
+def dave_jim_sign_in
   visit '/'
   fill_in('player_1_name', with: 'Dave')
   fill_in('player_2_name', with: 'Jim')
+  click_button('Start Multi-player!')
+end
+
+def pete_steve_sign_in
+  visit '/'
+  fill_in('player_1_name', with: 'Pete')
+  fill_in('player_2_name', with: 'Steve')
   click_button('Start Multi-player!')
 end
