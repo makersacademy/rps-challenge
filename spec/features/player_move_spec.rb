54a4cbd2f63ec53cae8167e76c78879b5c99fc25
Alexa <alexakearns@outlook.com>
2020-04-06 10:03:02 +0100

    typo fixed for failing test

--- spec/features/player_move_spec.rb
+++ spec/features/player_move_spec.rb
@@ -1,6 +1,6 @@
 feature 'player makes move' do
   scenario 'player makes choice and sees their choice in message' do
-    chose paper
+    chose_paper
     expect(page).to have_content "Nigel, you chose Paper"
   end
 end
