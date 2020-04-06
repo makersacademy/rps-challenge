b04edcc74345b3290e16b4c64a569a1c3d38c019
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-06 11:17:55 +0100

    Rename controller to match containing class name

--- rps.rb
+++ rps.rb
@@ -1,5 +1,4 @@
 require 'sinatra/base'
-require './lib/player'
 require './lib/game'
 
 class RPS < Sinatra::Base
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 require_relative './features/web_helpers.rb'
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', 'rps.rb')
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
