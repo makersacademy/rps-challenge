c3fe780327bcfbac60f5ea1a11f5cab64175aafc
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-01 10:55:04 +0100

    Refactors Manual Test logic to provide more flexibility

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # Global for testing, refactor to use group :test from Gemfile?
 # $manual_test = true
-DEBUG_PLAYER_1_NAME = "Siegfried" if $manual_test
+NUMBER_OF_PLAYERS = 1
+MANUAL_TEST_PLAYER_1_NAME = "PLAYER_1" if $manual_test
 
 require 'sinatra/base'
 
@@ -9,24 +10,23 @@ class RockPaperScissors < Sinatra::Base
   enable :sessions
 
   get '/' do
-    # session[:player_1_name] = DEBUG_PLAYER_1_NAME if $manual_test
-  
-
-    @debug_form_value = "value='#{DEBUG_PLAYER_1_NAME}'" if $manual_test
-  
     puts "in /" if $manual_test
-  
+
+    @debug_form_value = "value='#{MANUAL_TEST_PLAYER_1_NAME}'" if $manual_test
+
     p @debug_form_value if $manual_test
-  
+
     erb :index
   end
   
   post '/names' do
+    puts "in /names" if $manual_test
     session[:player_1_name] = params[:player_1_name]
     redirect to('/play')
   end
   
   get '/play' do
+    puts "in /play" if $manual_test
     erb :play
   end
 
