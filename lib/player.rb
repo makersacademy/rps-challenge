838a3a2a2d9945e7aeee742ab973a4be8aa2b25d
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-01 13:30:33 +0100

    Player returns Jamie Doe if no name supplied

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,13 @@
 class Player
-  attr_reader :name
 
   def initialize(name)
     @name = name
+  end
+
+  def name
+    return "Jamie Doe" if @name == ''
+
+    @name
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,18 @@ require 'player'
 
 describe Player do
   subject(:will) { Player.new('Will') }
+  subject(:empty) { Player.new('') }
 
   describe '#name' do
+    
     it 'returns the name' do
       expect(will.name).to eq 'Will'
     end
+
+    it 'returns Jamie Doe if the name is empty' do
+      expect(empty.name).to eq 'Jamie Doe'
+    end
+
   end
+
 end
