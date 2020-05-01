be1e6df3411f1b84040b15eb2b960245536b8113
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-01 17:59:55 +0100

    Add new line in spec test to pass

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'sinatra/base'
 
 class Game < Sinatra::Base
+
   get '/' do
     erb(:index)
   end
+
 end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 feature 'Homepage' do
   scenario 'Naviagate to homepage' do
     visit('/')
-    expect(page).to have_content("Welcome to Rock, Paper, Scissors! Enter your name to begin: ")
+    expect(page).to have_content("Welcome to Rock, Paper, Scissors!\nEnter your name to begin:")
   end
 end
