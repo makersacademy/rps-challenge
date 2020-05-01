1a9764a512217b02717459cfcb877237483c6868
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-01 12:23:58 +0100

    Changes  global manual_test to verbose, now only outputs additional info

--- app.rb
+++ app.rb
@@ -2,24 +2,21 @@ require 'sinatra/base'
 require_relative 'lib/player'
 
 # Global for testing, refactor to use group :test from Gemfile?
-# $manual_test = true
+# $verbose = true
 # NUMBER_OF_PLAYERS = 1
-# MANUAL_TEST_PLAYER_0_NAME = "PLAYER_0" if $manual_test
-# MANUAL_TEST_PLAYER_0_NAME = "" if $manual_test
 
 class RockPaperScissorsWebGame < Sinatra::Base
   enable :sessions
   set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
 
   get '/' do
-    puts "in /" if $manual_test
+    puts "in /" if $verbose
 
     session[:players] = Array.new
 
     @input_form_player_0 =  "<label for='player_0'>\n"
     @input_form_player_0 += "    First player name:\n"
     @input_form_player_0 += "    <input type='text' name='player_0_name'"
-    @input_form_player_0 += " value='#{MANUAL_TEST_PLAYER_0_NAME}'" if $manual_test
     @input_form_player_0 += ">\n"
     @input_form_player_0 += "  </label>"
 
@@ -27,14 +24,14 @@ class RockPaperScissorsWebGame < Sinatra::Base
   end
   
   post '/names' do
-    puts "in /names" if $manual_test
+    puts "in /names" if $verbose
 
     session[:players].push(Player.new(params[:player_0_name]))
     redirect to('/play')
   end
   
   get '/play' do
-    puts "in /play" if $manual_test
+    puts "in /play" if $verbose
     erb :play
   end
 
