e9d987e478bb6e6a9b571cf861e124869cb333af
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-01 10:47:48 +0100

    Changes  global debug to more meaningful manual_test

--- app.rb
+++ app.rb
@@ -1,3 +1,7 @@
+# Global for testing, refactor to use group :test from Gemfile?
+# $manual_test = true
+DEBUG_PLAYER_1_NAME = "Siegfried" if $manual_test
+
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
@@ -5,6 +9,15 @@ class RockPaperScissors < Sinatra::Base
   enable :sessions
 
   get '/' do
+    # session[:player_1_name] = DEBUG_PLAYER_1_NAME if $manual_test
+  
+
+    @debug_form_value = "value='#{DEBUG_PLAYER_1_NAME}'" if $manual_test
+  
+    puts "in /" if $manual_test
+  
+    p @debug_form_value if $manual_test
+  
     erb :index
   end
   
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,6 +1,14 @@
 feature '/play' do
+
   scenario 'shows player_1_name' do
-    sign_in_and_play
-    expect(page).to have_content TEST_PLAYER_1_NAME
+	   sign_in_and_play
+	   expect(page).to have_content TEST_PLAYER_1_NAME
   end
+
+  scenario 'asks user for input' do
+  	 sign_in_and_play
+  	 expect(page).to have_content "Enter your move:"
+  	 expect(page).to have_button("Submit")
+  end
+
 end
