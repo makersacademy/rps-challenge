79aaba6088d5ad46de5128f9ed743e563ffca351
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-02 21:52:31 +0100

    Displays outcome of the game

--- spec/features/outcome_spec.rb
+++ spec/features/outcome_spec.rb
@@ -29,4 +29,39 @@ feature 'Outcome of game' do
     expect(page).to have_content 'Computer chose: Rock'
   end
 
+  scenario 'Displays result' do
+    srand(6)
+    sign_in_and_play
+    click_button "Rock"
+    expect(page).to have_content 'Human wins!'
+  end
+
+  scenario 'Displays result' do
+    srand(6)
+    sign_in_and_play
+    click_button "Scissors"
+    expect(page).to have_content "It's a draw!"
+  end
+
+  scenario 'Displays result' do
+    srand(6)
+    sign_in_and_play
+    click_button "Paper"
+    expect(page).to have_content "Computer wins!"
+  end
+
+  scenario 'Displays result' do
+    srand(88)
+    sign_in_and_play
+    click_button "Scissors"
+    expect(page).to have_content "Computer wins!"
+  end
+
+  scenario 'Displays result' do
+    srand(88)
+    sign_in_and_play
+    click_button "Paper"
+    expect(page).to have_content 'Human wins!'
+  end
+
 end
