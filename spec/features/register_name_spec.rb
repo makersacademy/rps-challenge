f47091cb32235649668fd6aa0d2f3971e3fb91a9
mbrad26 <mbrad26@gmail.com>
2020-05-02 09:25:26 +0100

    implemented feature test for when the player registers his name, added routes for POST redirect GET

---
+++ spec/features/register_name_spec.rb
@@ -0,0 +1,10 @@
+feature 'Register player name' do
+  scenario 'submit name' do
+    visit '/'
+    fill_in :name, with: 'Marius'
+    # find("#register").click
+    click_button 'Register'
+
+    expect(page).to have_content 'Choose an option:'
+  end
+end
