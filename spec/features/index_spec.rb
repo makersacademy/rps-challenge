4c36e0a1efc68fea77bf02d445470347dbdbf5fe
Ed <lookatemail@gmail.com>
2020-05-02 09:28:58 +0100

    feature to submit name

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -4,4 +4,10 @@ feature '@index' do
     expect(page).to have_content 'Welcome To The Game'
   end
 
+  scenario 'should able to enter name' do
+    visit('/')
+    fill_in :name, with: "Dave"
+    click_button 'Submit'
+  end
+
 end 
