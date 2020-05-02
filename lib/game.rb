47f44bdc525ee0d3cce702a8c771e762cf2f7166
dpwdec <declankolakowski@gmail.com>
2020-05-02 18:44:09 +0100

    removed redundant tests for player switching.

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ class Game
   DRAW = 0
   WIN = 1
 
-  attr_reader :players, :current_player
+  attr_reader :players
 
   def initialize(players, ai = AIPlayer, vm = VictoryMatrix)
     @ai_class = ai
@@ -11,33 +11,16 @@ class Game
     if @players[1].name == ''
       @players[1] = @ai_class.new
     end
-    @current_player = @players[0]
   end
 
   def result
     player_1_choice = @victory_matrix.choice_to_index(@players[0].choice)
     player_2_choice = @victory_matrix.choice_to_index(@players[1].choice)
     @players[@victory_matrix.result(player_1_choice, player_2_choice)]
-    # if @players[0].choice == @players[1].choice
-    #   # nil return type because there is no winner
-    #   return [DRAW, nil]
-    # end
-    #
-    # if (@players[0].choice == 'rock' && @players[1].choice == 'scissors') ||
-    #   (@players[0].choice == 'paper' && @players[1].choice == 'rock') ||
-    #   (@players[0].choice == 'scissors' && @players[1].choice == 'paper')
-    #   return[WIN, @players[0]]
-    # end
-    #
-    # return [WIN, @players[1]]
   end
 
   def single_player?
     return true if @players[1].class == @ai_class.new.class
     return false
-  end
-
-  def switch_turns
-    @current_player = @current_player == @players[0] ? @players[1] : @players[0]
   end
 end
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -55,15 +55,4 @@ describe Game do
       expect(subject.result).to be nil
     end
   end
-
-  describe '#switch_turns' do
-    it 'starts with player 1 as current player' do
-      subject = described_class.new([player1, player3], ai_class, vm_class)
-      expect(subject.current_player).to eq(player1)
-    end
-    it 'switches current players turn' do
-      subject = described_class.new([player1, player3], ai_class, vm_class)
-      expect { subject.switch_turns }.to change { subject.current_player }.to eq(player3)
-    end
-  end
 end
