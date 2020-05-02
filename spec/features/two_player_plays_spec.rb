29f4a7c23760c9fc4d1b81598781a0866bcd2074
dpwdec <declankolakowski@gmail.com>
2020-05-02 15:45:04 +0100

    added different game outcomes for two players.

--- spec/features/two_player_plays_spec.rb
+++ spec/features/two_player_plays_spec.rb
@@ -7,4 +7,28 @@ feature 'two players can play the game' do
     expect(page).to have_content('Dec picked rock.')
     expect(page).to have_content('Dan picked rock.')
   end
+
+  scenario 'players can draw' do
+    two_player_sign_in
+    choose('player_1_rock')
+    choose('player_2_rock')
+    click_button('play')
+    expect(page).to have_content("It's a draw!")
+  end
+
+  scenario 'player 1 can win' do
+    two_player_sign_in
+    choose('player_1_rock')
+    choose('player_2_scissors')
+    click_button('play')
+    expect(page).to have_content("Dec wins")
+  end
+
+  scenario 'player 2 can win' do
+    two_player_sign_in
+    choose('player_1_rock')
+    choose('player_2_paper')
+    click_button('play')
+    expect(page).to have_content("Dan wins")
+  end
 end
