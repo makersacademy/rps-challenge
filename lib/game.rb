045b2a4d0500b3064fb9d7c2c4861b69adbd7be1
dpwdec <declankolakowski@gmail.com>
2020-05-02 12:30:42 +0100

    added result logic to game class.

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,7 @@
 class Game
+  DRAW = 0
+  WIN = 1
+
   attr_reader :players
 
   def initialize(*players)
@@ -6,6 +9,17 @@ class Game
   end
 
   def result
-    [1, @players[0]]
+    if @players[0].choice == @players[1].choice
+      # nil return type because there is no winner
+      return [DRAW, nil]
+    end
+
+    if (@players[0].choice == 'rock' && @players[1].choice == 'scissors') ||
+      (@players[0].choice == 'paper' && @players[1].choice == 'rock') ||
+      (@players[0].choice == 'scissors' && @players[1].choice == 'paper')
+      return[WIN, @players[0]]
+    end
+
+    return [WIN, @players[1]]
   end
 end
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -2,8 +2,15 @@ require 'game'
 
 describe Game do
 
-  let(:player1) { double() }
-  let(:player2) { double() }
+  let(:player1) { double(choice: 'rock') }
+  let(:player2) { double(choice: 'scissors') }
+
+  let(:player3) { double(choice: 'paper') }
+  let(:player4) { double(choice: 'scissors') }
+
+  let(:player5) { double(choice: 'paper') }
+  let(:player6) { double(choice: 'paper') }
+
   let(:subject) { described_class.new(player1, player2) }
 
   describe '#players' do
@@ -15,6 +22,16 @@ describe Game do
   describe '#result' do
     it 'returns a tuple for player 1 winning' do
       expect(subject.result).to eq([1, player1])
+    end
+
+    it 'returns a tuple for player 4 winning' do
+      subject = described_class.new(player3, player4)
+      expect(subject.result).to eq([1, player4])
+    end
+
+    it 'returns a tuple for a draw' do
+      subject = described_class.new(player5, player6)
+      expect(subject.result).to eq([0, nil])
     end
   end
 end
