8fc44c49604dc8f9016c4c0d559ade63da274d35
dpwdec <declankolakowski@gmail.com>
2020-05-02 15:43:03 +0100

    added testing for two players picking.

--- spec/features/two_player_plays_spec.rb
+++ spec/features/two_player_plays_spec.rb
@@ -1,12 +1,10 @@
-# feature 'two players can play the game' do
-#   scenario 'player 1 picks first' do
-#     two_player_sign_in
-#     expect(page).to have_content('Dec, pick your play.')
-#   end
-#
-#   scenario 'player 2 picks after player 1' do
-#     two_player_sign_in
-#     click_button('rock')
-#     expect(page).to have_content('Dan, pick your play.')
-#   end
-# end
+feature 'two players can play the game' do
+  scenario 'both players pick' do
+    two_player_sign_in
+    choose('player_1_rock')
+    choose('player_2_rock')
+    click_button('play')
+    expect(page).to have_content('Dec picked rock.')
+    expect(page).to have_content('Dan picked rock.')
+  end
+end
