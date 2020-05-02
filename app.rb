58c9e4a7d06c38823d4c04d9fc2cefbece9c5591
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-05-02 12:19:45 +0100

    refactor and intro of session/redirect for post

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post "/name" do
-    @name = params[:name]
-    erb(:name)
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name].capitalize
+    erb(:play)
   end
 
   run! if app_file == $0
