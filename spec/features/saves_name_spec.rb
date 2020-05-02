20885997e969e69e8a923dfcf880a13ac2ccf831
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-02 17:52:51 +0100

    Adds a web_helper to make code more DRY

--- spec/features/saves_name_spec.rb
+++ spec/features/saves_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Saves input name' do
   scenario 'Displays user name after submission' do
-    visit('/')
-    fill_in 'player_name', with: 'Andrew'
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content 'Hi Andrew!'
   end
 
--- spec/features/user_choose_option_spec.rb
+++ spec/features/user_choose_option_spec.rb
@@ -1,8 +1,6 @@
 feature 'Testing user game options' do
   scenario 'Asks user to choose an option' do
-    visit('/')
-    fill_in 'player_name', with: 'Andrew'
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content 'What option do you choose:'
   end
 end
