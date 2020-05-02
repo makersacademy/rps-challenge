5a33cb2d8419ab7d3ddc24df0a25bb81858f8386
joannacollier <collj035@gmail.com>
2020-05-02 19:44:38 +0100

    /play has move options

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -11,4 +11,19 @@ feature '/play' do
 		sign_in_and_play
     expect(page).to have_content("Lets begin! Pick Rock, Paper or Scissors...")
 	end
+
+	scenario "Page has a begin button" do 
+    sign_in_and_play
+    expect(page).to have_button("Rock")
+	end 
+	
+	scenario "Page has a begin button" do 
+    sign_in_and_play
+    expect(page).to have_button("Paper")
+	end 
+	
+	scenario "Page has a begin button" do 
+    sign_in_and_play
+    expect(page).to have_button("Scissors")
+  end 
 end 
