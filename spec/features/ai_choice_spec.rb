4b9d6eb991447b128f56b835c2c56ba331d7bc36
dpwdec <declankolakowski@gmail.com>
2020-05-02 12:48:51 +0100

    refactored ai choice spec.

--- spec/features/ai_choice_spec.rb
+++ spec/features/ai_choice_spec.rb
@@ -3,7 +3,6 @@ feature 'ai player makes a choice' do
     sign_in
     srand(3943)
     click_button('rock')
-    expect(page).to have_content('愛 picked rock.')
-    #expect(page).to have_content(/愛 picked [rps][a-z]{3,7}./)
+    expect(page).to have_content(/愛 picked [rps][a-z]{3,7}./)
   end
 end
