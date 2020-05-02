5c6ffa37c331616e901cfc63d665aff38c00d9a9
tanilserbes <serbestanil@gmail.com>
2020-05-02 16:03:05 +0100

    project set up and added first added

---
+++ app.rb
@@ -0,0 +1,9 @@
+# in app.rb
+require 'sinatra/base'
+
+class RPS < Sinatra::Base
+  get '/' 
+    'Testing infrastructure working!'
+  end
+  
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
+require 'capybara'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require './app'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'rspec'
+ENV['RACK_ENV'] = 'test'
+Capybara.app = RPS
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
