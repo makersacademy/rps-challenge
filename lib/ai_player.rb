d071cb2073353106b6f29f7badd8f05e30903884
dpwdec <declankolakowski@gmail.com>
2020-05-02 11:20:32 +0100

    added random choice functionality to AI player.

--- lib/ai_player.rb
+++ lib/ai_player.rb
@@ -1,2 +1,12 @@
 class AIPlayer
+
+  CHOICES = ['rock', 'paper', 'scissors']
+
+  def initialize(random = Random)
+    @random = random
+  end
+
+  def choose_random
+    CHOICES[(@random.rand(0.0...3.0).to_i)]
+  end
 end
--- spec/unit/ai_player_spec.rb
+++ spec/unit/ai_player_spec.rb
@@ -1,5 +1,11 @@
 require 'ai_player'
 
 describe AIPlayer do
-  
+
+  let(:random) { double(:rand => 2) }
+  let(:subject) { described_class.new(random) }
+
+  it 'makes a random choice' do
+    expect(subject.choose_random).to eq('scissors')
+  end
 end
