2c2d472c0a49867d6429e3c10ebb2f71c9e781f7
joannacollier <collj035@gmail.com>
2020-05-02 19:06:22 +0100

    spec_helper updated, 200 OK Homepage with header

--- spec/features/initial_spec.rb
+++ spec/features/initial_spec.rb
@@ -1,1 +1,14 @@
 require 'spec_helper'
+
+feature 'Homepage' do
+
+  scenario "page loads successfully" do
+    visit("/")
+    expect(page.status_code).to eq(200)
+  end
+
+  scenario "Page informs user of Game" do
+    visit("/")
+    expect(page).to have_content("Ready to play Rock, Paper, Scissors?!")
+  end
+end 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
@@ -9,7 +8,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 SimpleCov.start
 
-# For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
+require File.join(File.dirname(__FILE__), '..', './app.rb')
+
+require 'capybara/rspec'
+require 'capybara'
+require 'rspec'
+
+Capybara.app = RockPaperScissors
 
 RSpec.configure do |config|
   config.after(:suite) do
