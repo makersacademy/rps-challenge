421ebc86901f33cb5d74ad79b59382ce4d3d6af5
dpwdec <declankolakowski@gmail.com>
2020-05-02 12:10:06 +0100

    added game player storing with splat arugments.

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -1,4 +1,14 @@
 require 'game'
 
 describe Game do
+
+  let(:player1) { double() }
+  let(:player2) { double() }
+  let(:subject) { described_class.new(player1, player2) }
+
+  describe '#players' do
+    it 'has a record of players' do
+      expect(subject.players).to include(player1, player2)
+    end
+  end
 end
