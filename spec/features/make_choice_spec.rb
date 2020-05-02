81f6fd0ecf80c1587d8ccbf91274df251de21428
dpwdec <declankolakowski@gmail.com>
2020-05-02 11:38:04 +0100

    Added paper and scissors interface choice elements to player page.

--- spec/features/make_choice_spec.rb
+++ spec/features/make_choice_spec.rb
@@ -1,7 +1,19 @@
 feature 'player makes a choice between RPS' do
-  scenario 'picking confirms a choice' do
+  scenario 'picking rock confirms choice' do
     sign_in
     click_button('rock')
     expect(page).to have_content("You picked rock.")
+  end
+
+  scenario 'picking rock confirms choice' do
+    sign_in
+    click_button('paper')
+    expect(page).to have_content("You picked paper.")
+  end
+
+  scenario 'picking rock confirms choice' do
+    sign_in
+    click_button('scissors')
+    expect(page).to have_content("You picked scissors.")
   end
 end
