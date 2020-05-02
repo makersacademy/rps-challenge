ad1970dd0871dcbcbf957e3ab66483cc2d86a0eb
mbrad26 <mbrad26@gmail.com>
2020-05-02 16:17:17 +0100

    added tests for Paper and Scissors

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -9,8 +9,22 @@ feature 'Play page' do
 
   scenario 'can choose Rock as your weapon' do
     visit '/play'
-    click_button 'Rock'
+    find_button(value: "Rock").click
 
     expect(page).to have_content "You have selected Rock!"
+  end
+
+  scenario 'can choose Paper as your weapon' do
+    visit '/play'
+    find_button(value: "Paper").click
+
+    expect(page).to have_content "You have selected Paper!"
+  end
+
+  scenario 'can choose Scissors as your weapon' do
+    visit '/play'
+    find_button(value: "Scissors").click
+
+    expect(page).to have_content "You have selected Scissors!"
   end
 end
--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -2,7 +2,7 @@ feature 'Register player name' do
   scenario 'submiting name' do
     visit '/'
     fill_in :name, with: 'Marius'
-    find("#register").click
+    find_button(value: "Register").click
 
     expect(page).to have_content 'Choose your weapon Marius:'
   end
