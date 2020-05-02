52d2dd6f46eb682ae75f577b13aa44dff7dda2da
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-05-02 17:26:23 +0100

    player can now select Paper

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/game'
 
 class RPS < Sinatra::Base
   enable :sessions
@@ -17,8 +18,14 @@ class RPS < Sinatra::Base
     erb(:play)
   end
 
+   post '/choice' do
+     session[:choice] = params[:choice]
+     redirect '/rps'
+   end
+
   get '/rps' do
     @name = session[:name].capitalize
+    @choice = session[:choice]
     erb(:rps)
   end
 
--- spec/features/choose_rps_spec.rb
+++ spec/features/choose_rps_spec.rb
@@ -1,7 +1,15 @@
-feature 'player selection of Rock, Paper or Scissors' do
+feature 'Player selection of Rock, Paper or Scissors' do
+
   scenario 'player selects Rock' do
     enter_name
     click_button 'Rock'
-    expect(page).to have_content 'Coco: rock'
+    expect(page).to have_content 'Coco: Rock'
   end
+
+  scenario 'player selects Paper' do
+    enter_name
+    click_button 'Paper'
+    expect(page).to have_content 'Coco: Paper'
+  end
+
 end
