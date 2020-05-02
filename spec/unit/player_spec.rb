37eff9469ff814634a29bb74c8cdabf0532cdc54
dpwdec <declankolakowski@gmail.com>
2020-05-02 11:03:31 +0100

    added name functionality to player class.

--- spec/unit/player_spec.rb
+++ spec/unit/player_spec.rb
@@ -1,5 +1,12 @@
 require 'player'
 
 describe Player do
-  
+
+  let(:subject) { described_class.new('Dec') }
+
+  describe '#name' do
+    it 'records the player name' do
+      expect(subject.name).to eq('Dec')
+    end
+  end
 end
