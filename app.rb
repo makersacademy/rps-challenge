048e3adc1e625b1eecd725cf17d2b66c510b6e83
Ellis Trickett <ellis.trickett@gmail.com>
2020-05-03 17:17:10 +0100

    Added scoreboard class, ask to play another round and finalscore erb

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require './lib/player'
 require './lib/move'
 require './lib/cpu'
 require './lib/calculate'
+require './lib/scoreboard'
 
 
 class Rps < Sinatra::Base
@@ -25,10 +26,14 @@ class Rps < Sinatra::Base
     @gametype = $gametype
     $player_1 = Player.new(params[:player1])
     $player_2 = Player.new(params[:player2])
+    $scoreboard = Scoreboard.new
+    @scoreboard = $scoreboard
     redirect '/play'
   end
 
   get '/play' do 
+    @scoreboard = $scoreboard
+    @player_2 = $player_2
     @player_1 = $player_1
     erb :play
   end
@@ -42,6 +47,8 @@ class Rps < Sinatra::Base
   end
 
   get '/play2' do 
+    @scoreboard = $scoreboard
+    @player_1 = $player_1
     @player_2 = $player_2
     erb :play2
   end
@@ -58,7 +65,8 @@ class Rps < Sinatra::Base
     $cpu_move = Cpu.new
     @cpu_move = $cpu_move
     $calculate = Calculate.new(@player_1_move.move, @cpu_move.move)
-    @result = $calculate.result
+    $result = $calculate.result
+    @result = $result
     erb :result
   end
 
@@ -68,7 +76,30 @@ class Rps < Sinatra::Base
     @player_1_move = $player_1_move
     @player_2_move = $player_2_move
     $calculate = Calculate.new(@player_1_move.move, @player_2_move.move)
-    @result = $calculate.result
+    $result = $calculate.result
+    @result = $result
     erb :result2
+  end
+
+  post '/playagain' do 
+    @result = $result
+    @scoreboard = $scoreboard
+    @scoreboard.add_score(@result)
+    redirect '/play'
+  end
+
+  post '/final_score' do  
+    @result = $result   
+    @scoreboard = $scoreboard
+    @scoreboard.add_score(@result)
+    redirect '/goodbye'
+  end
+
+  get '/goodbye' do 
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @scoreboard = $scoreboard
+    @gametype = $gametype
+    erb :goodbye
   end
 end
