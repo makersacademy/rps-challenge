1073f5f4ead3adb06198d5259137381b1bdcc1f2
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-03 23:36:12 +0100

    Refactor rock_paper_scissor.rb to use before hook and add all equality conditions

--- spec/rock_paper_scissor_spec.rb
+++ spec/rock_paper_scissor_spec.rb
@@ -1,19 +1,31 @@
 require 'rock_paper_scissors'
 
 describe RockPaperScissors do
-  # subject(:player_1) { Player.new('Player_1') }
-  # subject(:player_2) { Player.new('Player_2') }
+  before(:each) do
+    @player_1 = Player.new('Player_1')
+    @player_2 = Player.new('Player_2')
+  end
 
   describe '#result' do
 
     it 'returns draw for 🗿 vs 🗿' do
-      player_1 = Player.new('Player_1')
-      player_2 = Player.new('Player_2')
-      player_1.move = '🗿'
-      player_2.move = '🗿'
-      expect(subject.result(player_1, player_2)).to eq "It's a draw!"
+      @player_1.move = '🗿'
+      @player_2.move = '🗿'
+      expect(subject.result(@player_1, @player_2)).to eq "It's a draw!"
     end
 
+    it 'returns draw for 📄 vs 📄' do
+      @player_1.move = '📄'
+      @player_2.move = '📄'
+      expect(subject.result(@player_1, @player_2)).to eq "It's a draw!"
+    end
+
+    it 'returns draw for ✂ vs ✂' do
+      @player_1.move = '✂'
+      @player_2.move = '✂'
+      expect(subject.result(@player_1, @player_2)).to eq "It's a draw!"
+    end
+    
   end
   
 end
