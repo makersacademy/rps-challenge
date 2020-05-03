53626a55846373bbafc69c4add0473de3d2da3d7
mbrad26 <mbrad26@gmail.com>
2020-05-03 07:11:54 +0100

    controller is now using class methods to store params values between HTTP requests

--- app.rb
+++ app.rb
@@ -3,33 +3,28 @@ require './lib/human_player'
 require './lib/rps_game'
 
 class RPS < Sinatra::Base
-  enable :sessions
-
   get '/' do
     erb :index
   end
 
   post '/name' do
-    p params[:name]
     player = HumanPlayer.new(params[:name])
-    @game = RPSGame.create(player, "")
+    RPSGame.create(player, "")
     redirect '/play'
   end
 
   get '/play' do
-    p @game
     @game = RPSGame.instance
     erb :play
   end
 
   post '/weapon' do
-    session[:weapon] = params[:weapon]
+    RPSGame.instance.player1.weapon=(params[:weapon])
     redirect '/action'
   end
 
   get '/action' do
     @game = RPSGame.instance
-    @weapon = session[:weapon]
     erb :action
   end
 
