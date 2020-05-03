9382b82996875931fdea338640545e2be86e1b7d
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-04 00:50:26 +0100

    Refactors RockPaperScissor.result to use Hash to return result

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,15 +4,14 @@ class RockPaperScissors
   RESULT_PAPER = '📄'
   RESULT_SCISSORS = '✂'
 
+  RULES = {
+    '🗿' => { '🗿' => '🙏', '📄' => '📄', '✂' => '🗿' },
+    '📄' => { '🗿' => '📄', '📄' => '🙏', '✂' => '✂' },
+    '✂' => { '🗿' => '🗿', '📄' => '✂', '✂' => '🙏' },
+  }
+  
   def result(player_1, player_2)
-    return RESULT_DRAW if player_1.move == player_2.move
-    return RESULT_ROCK if player_1.move == '🗿' && player_2.move == '✂'
-    return RESULT_PAPER if player_1.move == '🗿' && player_2.move == '📄'
-    return RESULT_PAPER if player_1.move == '📄' && player_2.move == '🗿'
-    return RESULT_SCISSORS if player_1.move == '📄' && player_2.move == '✂'
-    return RESULT_SCISSORS if player_1.move == '✂' && player_2.move == '📄'
-    return RESULT_ROCK if player_1.move == '✂' && player_2.move == '🗿'
-
+    RULES[player_1.move][player_2.move]
   end
 
 end
