0645cbce3cd80fbfc6abfce0a8190736833faf09
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-04 10:57:49 +0100

    added web helpers file to DRY up tests

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter name' do
   scenario 'User can enter name and see it' do
-    visit('/')
-    fill_in :player, with: 'Catriona'
-    click_button 'Submit'
+    sign_in
     expect(page).to have_content 'Welcome Catriona!'
     expect(page).to have_content 'To play, please click on your choice'
   end
@@ -10,18 +8,14 @@ end
 
 feature 'Selection' do
   scenario 'User can make their choice' do
-    visit('/')
-    fill_in :player, with: 'Catriona'
-    click_button 'Submit'
+    sign_in
     expect(page).to have_button 'rock'
     expect(page).to have_button 'paper'
     expect(page).to have_button 'scissors'
   end
 
   scenario 'User can see their choice' do
-    visit('/')
-    fill_in :player, with: 'Catriona'
-    click_button 'Submit'
+    sign_in
     click_button 'rock'
     expect(page).to have_content 'Catriona chose rock'
   end
