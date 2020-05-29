889e032d24eafd933e6d3b222f6b4737cfaed689
Rachael <rachaelewins@hotmail.com>
2020-05-29 22:17:15 +0100

    #result(player_move), returns won, drew or lost

--- lib/result.rb
+++ lib/result.rb
@@ -1,8 +1,16 @@
 class Result
+  @win = { "Rock" => "Scissors", "Paper" => "Rock", "Scissors" => "Paper"}
 
-  def computer_move
+  def random_move
     ["Rock","Paper","Scissors"].sample
   end
 
+  def result(player_move)
+    computer_move = random_move
+    win = { "Rock" => "Scissors", "Paper" => "Rock", "Scissors" => "Paper"}
+    return "drew" if player_move == computer_move
+    return "won" if win[player_move] == computer_move
+    return "lost" if win[computer_move] == player_move
+  end
 
 end
--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -2,11 +2,19 @@ require_relative '../lib/result.rb'
 
 describe Result do
 
-  describe '#computer_move' do
+  describe '#random_move' do
     it "returns random choice of RPS" do
       allow_any_instance_of(Array).to receive(:sample) {"Rock"}
-      expect(subject.computer_move).to eq("Rock")
+      expect(subject.random_move).to eq("Rock")
     end
   end
 
+  describe '#result(player_move)' do
+    it "returns won, drew or lost for player" do
+      allow_any_instance_of(Array).to receive(:sample) {"Rock"}
+      expect(subject.result("Paper")).to eq("won")
+      expect(subject.result("Rock")).to eq("drew")
+      expect(subject.result("Scissors")).to eq("lost")
+    end
+  end
 end
