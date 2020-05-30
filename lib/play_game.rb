d60af73563daa3af91e76e576b0080dbc1e4a250
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-30 13:17:33 +0100

    players choice can be passed in

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -1,4 +1,10 @@
 class PlayGame 
 
   RPSARRAY = ['Rock', 'Paper', 'Scissors']
+
+  attr_reader :player_choice
+
+  def initialize(choice)
+    @player_choice = choice
+  end
 end
--- spec/play_game_spec.rb
+++ spec/play_game_spec.rb
@@ -1,6 +1,7 @@
 require 'play_game'
 
 describe PlayGame do
+  let(:subject) {described_class.new('Paper')}
 
   it 'Should have a PlayGame class' do
     expect(PlayGame).to respond_to(:new)
@@ -8,5 +9,9 @@ describe PlayGame do
 
   it 'Should have an array of rock, paper, scissors' do
     expect(PlayGame::RPSARRAY).to eq(['Rock', 'Paper', 'Scissors']) 
+  end
+
+  it 'Should accept the player choice as an argument' do 
+    expect(subject.player_choice).to eq('Paper')
   end
 end
