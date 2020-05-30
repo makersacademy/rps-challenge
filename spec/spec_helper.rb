96f62dd4abe762056e878b365dd0da179279d2c4
minsi yang <yangminsi@gmail.com>
2020-05-30 21:19:50 +0100

    first commit, setting up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
+ENV['RACK_ENV'] = 'test'
+
+require './../app.rb'
+require 'capybara'
+require 'rspec'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = Rps
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
