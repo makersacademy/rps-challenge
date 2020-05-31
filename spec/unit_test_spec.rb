645c02a2b0d40b45678b88325d48bd4c9d8ce412
Sophie <sophiebrown438@gmail.com>
2020-05-31 09:39:10 +0100

    Refactor of test for emojis

--- spec/unit_test_spec.rb
+++ spec/unit_test_spec.rb
@@ -5,11 +5,11 @@ feature "Player name" do
   visit '/'
   fill_in :player_1, with: 'Sophie'
   click_button("Let's play!")
-  expect(page).to have_content('Welcome, Sophie')
+  expect(page).to have_content("Welcome,\n💡 Sophie 💡")
   end
 
   scenario "move selection" do
     visit '/play'
-    expect(page).to have_content("What's your move?\nRock\nPaper\nScissors")
+    expect(page).to have_content("What's your move?\n🤘 Rock 🤘\n📰 Paper 📰\n✂️ Scissors ✂️")
   end
 end
