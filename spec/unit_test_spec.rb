02940c90a72558530d42635920a9e2bf4c321382
Sophie <sophiebrown438@gmail.com>
2020-05-31 10:29:46 +0100

    Functioning test for losing w rock move

--- spec/unit_test_spec.rb
+++ spec/unit_test_spec.rb
@@ -1,15 +1,22 @@
 require 'sinatra'
 
 feature "Player name" do
-  scenario "display form " do
+  scenario "Log name " do
   visit '/'
   fill_in :player_1, with: 'Sophie'
   click_button("Let's play!")
   expect(page).to have_content("Welcome,\n💡 Sophie 💡")
   end
 
-  scenario "move selection" do
+  scenario "Move options" do
     visit '/play'
     expect(page).to have_content("What's your move?\n🤘 Rock 🤘\n📰 Paper 📰\n✂️ Scissors ✂️")
+  end
+
+  scenario "Player chooses rock, and the computer chooses paper." do
+    visit '/play'
+    allow_any_instance_of(Player).to receive(:move).and_return(:paper)
+    click_button('Rock')
+    expect(page).to have_content("Your opponent chose paper. You lose!")
   end
 end
