20a01e737a26baf8f227c3474a26c1ce3f4f2e92
Tim <TimCPB@github.com>
2020-05-31 23:39:18 +0100

    wrote #play_rps method and passes 'Returns a winner' test in player_spec.rb

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,7 +10,12 @@ describe Player do
     end
   end
 
-  # describe "#play_rps" do
-  #   it "Returns a winner"
+  describe "#play_rps" do
+    it "Returns a winner" do
+      allow_any_instance_of(CompMove).to receive(:move).and_return("scissors")
+      john.play_rps("rock")
+      expect(john.result).to eq("Congrats, your rock beat scissors!")
+    end
+  end
       
 end
