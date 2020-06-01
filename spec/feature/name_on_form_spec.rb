1a8499341e7077669cf333e7d19baa23ffcb2b8a
Joshua Brook <joshuabrookuk@gmail.com>
2020-06-01 10:56:37 +0100

    GREEN: Form displayed, name returned. Both unit and feature pass.

--- spec/feature/name_on_form_spec.rb
+++ spec/feature/name_on_form_spec.rb
@@ -1,10 +1,8 @@
-require 'sinatra'
-
 feature "Enter name on form" do
   scenario "display form" do
     visit '/'
     fill_in :player, with: 'Jeff'
     click_button 'Enter'
-    expect(page).to have_content ('Jeff')
+    expect(page).to have_content 'Jeff'
   end
 end
