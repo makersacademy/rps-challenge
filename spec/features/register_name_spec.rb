46b24d9ca5142be862c1759188bf0fcddd96f1c3
victorvallet <victorvallet8@gmail.com>
2020-06-01 08:40:42 +0200

    Added test to check if name stored

--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -3,4 +3,10 @@ feature "so that the user can register his name to play this amazing game" do
       visit('/')
       expect(page).to have_content('To continue, please enter your name bellow')
     end
+    scenario "store name and displays it" do
+      visit('/')
+      fill_in :name, with: 'Victor'
+      click_button 'Play'
+      expect(page).to have_content('Victor')
+    end
 end
