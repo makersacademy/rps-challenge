1da6373cbebb3ebfe4c413c9c671aff9c3a04272
Emanuele Pace <emanuele.10@outlook.it>
2020-06-01 08:26:07 +0100

    Modifies

--- app.rb
+++ app.rb
@@ -1,17 +1,7 @@
-# require 'sinatra'
-#
-# get '/' do  # INDEX ROUTE
-#   erb :index
-# end                       # All is block, GET is method, / is PATH
-#
-# get '/game' do
-#   'That is the game, my friend.'
-# end
-
-
 require 'sinatra/base'
 
 class Game < Sinatra::Base
+
   get '/' do
     'Hello Battle!'
   end
@@ -20,16 +10,10 @@ class Game < Sinatra::Base
     erb :game
   end
 
-  # start the server if ruby file executed directly
+  post '/names' do
+    erb :play
+  end
+
   run! if app_file == $0
 
 end
-
-
-
-# set :session_secret, 'super secret' # ?????
-#
-# get '/secret' do
-#   'That is the secret game.'
-# end
-# run! if app_file == $0
