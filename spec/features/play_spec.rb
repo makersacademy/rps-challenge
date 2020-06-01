73753a6e42bb4c5dbe8fad24d72121040ae35cdb
victorvallet <victorvallet8@gmail.com>
2020-06-01 09:20:55 +0200

    Added test to check if player's option stored

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -7,10 +7,18 @@ feature "play rock, paper, scissors" do
   scenario "the player can choose paper" do
     expect(page.find("input#paper")).not_to be nil
   end
+
   scenario "the player can choose rock" do
     expect(page.find("input#rock")).not_to be nil
   end
+
   scenario "the player can choose scissors" do
     expect(page.find("input#scissors")).not_to be nil
+  end
+
+  scenario "player chose an option" do
+    choose 'rock'
+    click_button 'Go'
+    expect(page).to have_content('rock')
   end
 end
