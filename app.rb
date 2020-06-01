fa17a3f2170f555adb17bb555d8209a1317f47d3
victorvallet <victorvallet8@gmail.com>
2020-06-01 09:24:32 +0200

    Added new routes

--- app.rb
+++ app.rb
@@ -17,6 +17,16 @@ class Game < Sinatra::Base
     erb :play
   end
 
+  post '/choose' do
+    session[:move] = params[:option]
+    redirect '/results'
+  end
+
+  get 'results' do
+    @move = session[:move]
+    erb :results
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
