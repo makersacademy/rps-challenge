4ea7c28172d61bb9dc9a599bb2b9624656bb1a9e
victorvallet <victorvallet8@gmail.com>
2020-06-01 07:36:29 +0200

    First get route test

--- app.rb
+++ app.rb
@@ -2,4 +2,11 @@ require 'sinatra/base'
 
 class Game < Sinatra::Base
   enable :sessions
+
+  get '/' do
+    "Hello World"
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
