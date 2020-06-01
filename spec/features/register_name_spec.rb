2d89339727482808f9ca82d52bbfd033de216815
victorvallet <victorvallet8@gmail.com>
2020-06-01 07:43:50 +0200

    Updated welcome message to test

--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -1,6 +1,6 @@
 feature "so that the user can register his name to play this amazing game" do
     scenario "submit name" do
       visit('/')
-      expect(page).to have_content('Please enter your name')
+      expect(page).to have_content('To continue, please enter your name bellow')
     end
 end
