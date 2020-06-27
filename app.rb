aadac1050fddc5b3873cc940bca31aebba624f78
Stuart <scpitc@gmail.com>
2020-06-27 12:01:23 +0100

    Feature test for name display written (failing). display.erb created.

--- app.rb
+++ app.rb
@@ -6,6 +6,16 @@ class RockPaperScissors < Sinatra::Base
   get '/' do
       erb:index
   end
+
+  post '/start_game' do
+    $game = Game.new(params[:player_name])
+    redirect '/display'
+  end
+
+  get '/display' do
+    @Player_Name = $game.name
+    erb:display
+  end
   
   run! if app_file == $0
 end 
--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -6,3 +6,12 @@ feature 'Welcome Screen' do
     expect(page).to have_content("Enter Your Name:")
   end 
 end 
+
+feature 'Welcome Screen' do
+  scenario 'User will be welcomed and asked to enter name' do
+    visit '/' 
+    fill_in :player_name, :with => "testplayer"
+    click_button "Submit"
+    expect(page).to have_content("testplayer")
+  end 
+end 
