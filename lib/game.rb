34bd2059d311899ed9bee87060d2d7f2e45a9617
Stuart <scpitc@gmail.com>
2020-06-27 19:23:38 +0100

    Unit tests for Game class complete

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,28 @@
+
 class Game
 
-    attr_reader :name, :result
+  attr_reader :name, :result
 
-    def initialize(name)
-        @name = name
+  def initialize(name)
+    @name = name
+  end
+
+  def go(player_pick)
+    calculate_winner(player_pick, computer_pick)
+  end
+
+  def computer_pick
+    %w[scissors rock paper][rand(3)]
+  end
+
+  def calculate_winner(player1_pick, player2_pick)
+    @result = case [player1_pick, player2_pick]  
+    when ['rock', 'rock'], ['paper', 'paper'], ['scissors', 'scissors']
+      player1_pick + " vs " + player2_pick + ' The result is a draw'
+    when ['rock', 'scissors'], ['paper', 'rock'], ['scissors', 'paper']
+      player1_pick + " vs " + player2_pick + ' You win'
+    when ['paper', 'scissors'], ['scissors', 'rock'], ['rock', 'paper']
+      player1_pick + " vs " + player2_pick + ' You lost'
     end
-
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,4 +7,29 @@ describe Game do
     end
   end
 
+  describe '#calculate_winner' do
+
+    arr1 = ['rock', 'paper', 'scissors']
+    arr2 = ['paper', 'scissors', 'rock']
+
+    arr1.each  do |x|
+      it 'should return draw for ' + x + ' vs ' + x do
+        expect(subject.calculate_winner(x, x)).to include('draw') 
+      end
+    end
+
+    (0).upto(2)  do |x|
+      it 'should return lost for ' + arr1[x] + ' vs ' + arr2[x] do
+        expect(subject.calculate_winner(arr1[x], arr2[x])).to include('lost') 
+      end
+    end
+
+    (0).upto(2)  do |x|
+      it 'should return win for ' + arr2[x] + ' vs ' + arr1[x] do
+        expect(subject.calculate_winner(arr2[x], arr1[x])).to include('win') 
+      end
+    end
+
+  end
+
 end
