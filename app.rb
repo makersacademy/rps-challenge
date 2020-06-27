1f7dbc7451c2bdfadf928bce2dfa1debacf4859f
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-06-27 16:39:25 +0100

    p sessions removed from code to tidy things up

--- app.rb
+++ app.rb
@@ -5,14 +5,10 @@ class RPS < Sinatra::Base
     erb :enter_names
   end
   post '/names' do
-    p session
     session[:name] = params[:Name]
-    p session
-    p session[:name]
     redirect '/names'
   end
   get '/names' do
-    p session
     @name = session[:name]
     erb :letsplay
   end
