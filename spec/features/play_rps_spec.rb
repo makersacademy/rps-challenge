05c2540e8b5054b014d6e732d4ae8daed28d7796
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-06-28 22:43:06 +0100

    Added feature tests that were missing for seeing computer selection on screen, and 3 feature tests covering each scenario of player winning, a draw, and computer winning

--- spec/features/play_rps_spec.rb
+++ spec/features/play_rps_spec.rb
@@ -24,12 +24,25 @@ feature 'play rps' do
     click_button "Scissors"
     expect(page).to have_content("You selected Scissors!")
   end
-  # scenario 'once you made your move, should show text stating what move the computer made' do
-  #   click_button "Scissors"
-  #   expect(computer).to receive(:rps).and_return "Rock"
-  #   expect(page).to have_content("Computer selected Rock!")
-  # end
-  # how can i stub out this randomness. can I mix capybara and unit tests for my computer class as i have tried to above?
-  # also need a test to make sure before a user makes their first move of the session, the screen does not show the computer 
-  # as having made a move. 
+  scenario 'once you made your move, should show text stating what move the computer made' do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
+    click_button "Scissors"
+    # allow(Computer.new).to receive(:rps).and_return "Rock"
+    expect(page).to have_content("Computer selected Scissors!")
+  end
+  scenario 'should see on screen that it is a draw' do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Paper')
+    click_button "Paper"
+    expect(page).to have_content("That's a draw!") 
+  end
+  scenario 'should see on screen that the player has won' do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
+    click_button "Paper"
+    expect(page).to have_content("You beat the computer amigo!") 
+  end
+  scenario 'should see on screen that the player has won' do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
+    click_button "Paper"
+    expect(page).to have_content("The computer wins amigo!") 
+  end
 end
