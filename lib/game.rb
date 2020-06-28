c8497b831c5624de7f5a601aef9ae492da066c18
Katie Jones <katieljones29@gmail.com>
2020-06-28 16:34:34 +0100

    added game class conditions for result method

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,20 @@
 class Game
   WEAPONS = ["rock", "paper", "scissors"]
 
-  def result
-    "You won."
+  def initialize(player = Player.new, computer = Computer.new)
+    @player = player
+    @computer = computer
+  end
+
+  def result(rps)
+  if @player == "rock" && @computer == "scissors"
+    "You won"
+  elsif @player == "scissors" && @computer == "paper"
+    "You won"
+  elsif @player == "paper" && @computer == "rock"
+    "You won"
+  else
+    "Computer won"
+  end
   end
 end
