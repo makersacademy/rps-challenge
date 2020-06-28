b23c18e1265576de35b4b1f94e87b8988f3afb0a
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-06-28 13:44:32 +0100

    Refactored my feature tests to reduce lines of code using a before block using our sign_in_and_play method. Also moved feature tests relating to playing a game out of enter_names_spec and into a new spec called play_rps

--- spec/features/play_rps_spec.rb
+++ spec/features/play_rps_spec.rb
@@ -1,19 +1,14 @@
-feature 'Entering player name' do
-  scenario 'when they visit the homepage, they get a form to fill out with their name, which then greets them with their name on screen' do
+feature 'play rps' do
+  before do 
     sign_in_and_play
-    expect(page).to have_content("Welcome Alex. Let's play Rock Paper Scissors!")
   end
   scenario 'visit page, enter name, see 3 buttons for rock paper and scissors on screen' do
-    sign_in_and_play
     expect(page).to have_button("Rock")
   end
   scenario 'visit page, enter name, see 3 buttons for rock paper and scissors on screen' do
-    sign_in_and_play
     expect(page).to have_button("Paper")
   end
   scenario 'visit page, enter name, see 3 buttons for rock paper and scissors on screen' do
-    sign_in_and_play
     expect(page).to have_button("Scissors")
   end
 end
- 
