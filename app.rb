97c71ade0fa586d6528f711cca8d757fa365460f
Matthew Lock <matthew_lock20@hotmail.com>
2020-06-28 20:00:42 +0100

    Removed sessions

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require './lib/computer'
 require './lib/result'
 
 class RPS < Sinatra::Base
-  enable :sessions
 
   get '/' do
     ":)"
@@ -14,17 +13,13 @@ class RPS < Sinatra::Base
     @playerone = (params[:player_one_name])
     erb :start
   end
- 
+
   post '/game' do
     @playerchoice = (params[:choice])
     @computer = Computer.new.weapon
     @result = Result.new.result(@playerone, @computer)
     erb :game
   end
-
-  # get '/game' do
-  #   erb :game
-  # end
 
   run! if app_file == $0
 end
