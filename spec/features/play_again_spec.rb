27674d5d4aec70dde5092a2f35f63d38aa4de237
Alexa <alexakearns@outlook.com>
2020-07-03 16:34:24 +0100

    Change button to say New Game

--- spec/features/play_again_spec.rb
+++ spec/features/play_again_spec.rb
@@ -2,6 +2,12 @@ feature 'play again' do
   scenario 'button available to play again' do
     sign_in_and_play
     chose_paper
-    expect(page).to have_content 'Play Again?'
+    expect(page).to have_content 'New Game'
   end
+
+  scenario 'button redirects to new game' do
+    sign_in_and_play
+    chose_paper
+    click_on 'New Game'
+    expect(page)
 end
