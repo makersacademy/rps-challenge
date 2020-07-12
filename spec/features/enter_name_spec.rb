9b72535a14f929951f41781cb21b1320250b1a79
Catriona <catrionamairi@blueyonder.co.uk>
2020-07-12 14:40:19 +0100

    Split feature test into two files

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -5,18 +5,3 @@ feature 'Enter name' do
     expect(page).to have_content 'To play, please click on your choice'
   end
 end
-
-feature 'Selection' do
-  scenario 'User can make their choice' do
-    sign_in
-    expect(page).to have_button 'rock'
-    expect(page).to have_button 'paper'
-    expect(page).to have_button 'scissors'
-  end
-
-  scenario 'User can see their choice' do
-    sign_in
-    click_button 'rock'
-    expect(page).to have_content 'Catriona chose rock'
-  end
-end
---
+++ spec/features/player_selection_spec.rb
@@ -0,0 +1,14 @@
+feature 'Selection' do
+  scenario 'User can see their choices' do
+    sign_in
+    expect(page).to have_button 'rock'
+    expect(page).to have_button 'paper'
+    expect(page).to have_button 'scissors'
+  end
+
+  scenario 'User can make their choice' do
+    sign_in
+    click_button 'rock'
+    expect(page).to have_content 'Catriona chose rock'
+  end
+end
