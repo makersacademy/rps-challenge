da3ff6162f52e1e34eee9c95afe94bd948cca41d
Catriona <catrionamairi@blueyonder.co.uk>
2020-07-14 15:34:33 +0100

    Refactor game class to use attr_reader

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,8 @@
 class Game
+  attr_reader :player, :computer
+
   def initialize(player, computer)
     @player = player
     @computer = computer
-  end
-
-  def player
-    @player
-  end
-
-  def computer
-    @computer
   end
 end
