85df142721c158101a860dd875b837ecbf0c1dec
Catriona <catrionamairi@blueyonder.co.uk>
2020-07-16 17:28:47 +0100

    Add start again button

--- spec/features/play_again_spec.rb
+++ spec/features/play_again_spec.rb
@@ -6,4 +6,12 @@ feature 'Play again' do
     click_button 'Play Again'
     expect(page).to have_content 'Welcome Catriona!'
   end
+
+  scenario 'User can play again as someone else' do
+    sign_in
+    click_button 'scissors'
+    expect(page).to have_button 'Start Again'
+    click_button 'Start Again'
+    expect(page).to have_content 'Rock, Paper, Scissors!'
+  end
 end
