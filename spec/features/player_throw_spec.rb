07379f1729da35f2e844855f4626b2eb3cecccbf
Ryan Viegas <rjkviegas@gmail.com>
2020-07-26 10:17:41 +0100

    created features/web_helpers.rb

--- spec/features/player_throw_spec.rb
+++ spec/features/player_throw_spec.rb
@@ -1,8 +1,6 @@
 feature 'player 1 throw confirmation' do
   scenario 'confirmation of throw choice displayed' do
-    visit '/'
-    fill_in :player_1_name, with: 'Nayr'
-    click_button 'Submit'
+    sign_in_and_play
     click_button "Rock!"
     expect(page).to have_content "Nayr chose to throw Rock!"
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'capybara/rspec'
 require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'features/web_helpers'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 Capybara.app = RPS
