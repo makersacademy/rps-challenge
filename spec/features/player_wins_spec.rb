808c9415150295600a2b388bc8f37fa82694161f
DillonBarker <barker.dill98@gmail.com>
2020-07-26 09:13:10 +0100

    added the move comparison tests

--- spec/features/player_wins_spec.rb
+++ spec/features/player_wins_spec.rb
@@ -1,7 +1,12 @@
 feature 'player can win' do
+  let(:game) { double :game }
   scenario 'player wins' do
     sign_in
-    click_button 'rock'
-    
+    click_button 'Rock'
+    # allow(game).to receive(:random_move) { 'scissors' }
+    # allow(game).to receive(:move_comparison) { 'You Win' }
+    # expect(page).to have_content 'You Win'
   end
 end
+
+# cannot implement the randomness correctly at the moment
