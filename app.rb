9dabba96760da16f31c25ecd2f555601293b143c
Melanie Anderson <melanieanderson1995@gmail.com>
2020-07-27 14:05:35 +0100

    Fixed bug for single player, still need to add multiplayer switch turn etc

--- app.rb
+++ app.rb
@@ -11,40 +11,47 @@ class RockPaperScissors < Sinatra::Base
 
   get "/choose_opponent" do
     player_1 = Player.new(params[:player_1_name])
+    @game = Game.create(player_1)
     erb :choose_opponent
   end
 
   get "/names" do
-    player_1 = Player.new(params[:player_1_name])
+    @game = Game.instance
     player_2 = Player.new(params[:player_2_name])
-    $game = Game.new(player_1, player_2)
+    @game.create_player_2(player_2)
+    p @game.player_2.name
+    p @game
+    #@game = Game.create(Player.new(params[:player_1_name]), player_2)
     redirect('/play_multiplayer')
   end
 
   get "/play_multiplayer" do
-    @game = $game
+    @game = Game.instance
     erb :play_multiplayer
   end
 
 
   get "/play_singleplayer" do
-    player_1 = Player.new(params[:player_1_name])
-    player_2 = Player.new("Computer")
-    $game = Game.new(player_1, player_2)
+    computer = Player.new("Computer")
+    @game = Game.instance
+    @game.create_computer(computer)
     erb :play_singleplayer
   end
 
   get "/rock_computer" do
+    @game = Game.instance
     @computer_move = @game.random_selection
     erb :rock_computer
   end
 
   get "/paper_computer" do
+    @game = Game.instance
     @computer_move = @game.random_selection
     erb :paper_computer
   end
 
   get "/scissors_computer" do
+    @game = Game.instance
     @computer_move = @game.random_selection
     erb :scissors_computer
   end
--- lib/game.rb
+++ lib/game.rb
@@ -3,9 +3,9 @@ require_relative "player"
 class Game
   attr_reader :current_turn
 
-  def initialize(player_1, player_2)
+  def initialize(player_1, player_2 = nil)
     @choices = ["rock", "paper", "scissors"]
-    @players = [player_1, player_2]
+    @players = [@player_1 = player_1, @player_2 = player_2]
   end
 
   def random_selection
@@ -18,5 +18,22 @@ class Game
 
   def player_2
     @players.last
+  end
+
+  def self.instance
+    @game
+  end
+
+  def self.create(player_1)
+    #@player_2 = Player.new(player_2)
+    @game = Game.new(player_1)
+  end
+
+  def create_computer(computer)
+    @player_2 = Player.new(computer)
+  end
+
+  def create_player_2(player)
+    @player_2 = Player.new(player)
   end
 end
