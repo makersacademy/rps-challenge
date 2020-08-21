2b5bf0cfffbd9bdca78568fecb33c40d8576a189
Tamara Jeffrey <just-tam@live.com>
2020-08-21 17:38:07 +0100

    testing for player and computer weapons

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,4 +12,19 @@ describe Game do
     end
   end
 
+  describe '#player_choice' do
+    it 'Should store player weapon choice' do
+      game.player_choice(:rock)
+      expect(game.player_choice(:rock)).to eq :rock
+    end
+  end
+
+  describe '#computer_choice' do
+    it 'Should store player weapon choice' do
+      allow(computer).to receive(:random_weapon) {:paper}
+      game.computer_choice
+      expect(game.computer_choice).to eq :paper
+    end
+  end
+
 end
