c23bab3cc093e39f90aa8141b6b63873cec9295b
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-08-21 11:30:10 +0100

    Implimented moveselect.erb and passed rspec tests

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require 'sinatra/base'
 
 class Rps <Sinatra::Base
 
-    get '/' do
-        erb(:index)
-    end
+  get '/' do
+    erb(:index)
+  end
 
+  post '/names' do 
+    name = params[:name]
+    redirect '/move-select'
+  end
+
+  get '/move-select' do
+    erb(:moveselect)
+  end
 end
