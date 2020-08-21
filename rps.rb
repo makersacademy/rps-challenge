2029c97606cabb3b972a128d248501da5f10c7ed
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-21 13:19:35 +0100

    submit name test passed

--- rps.rb
+++ rps.rb
@@ -6,5 +6,15 @@ class Rps < Sinatra::Base
     erb(:index)
   end
 
+  post '/names' do
+    $player_1 = params[:player1]
+    redirect '/game'
+  end
+
+  get '/game' do
+    @player1 = $player_1
+    erb(:game)
+  end
+
   run! if app_file == $0
 end
--- spec/features/entering_name_spec.rb
+++ spec/features/entering_name_spec.rb
@@ -2,6 +2,7 @@ feature 'name displayed after entry' do
   scenario 'player1 enters name ' do
     visit '/'
     fill_in :player1, with: 'Yasmin'
+    click_button 'Submit'
     expect(page).to have_content 'Yasmin'
   end
 end
