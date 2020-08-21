560ac8d12c05543bc167877895cf5f000436f2bb
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-21 23:17:12 +0100

    changed tests but working

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,18 @@
 class Game
 
-  def initialize(player_choice)
-    @player_choice = player_choice
-    @comp_choice = ['Rock','Paper','Scissors'].sample
+  def initialize(player,opponent)
+    @player = player
+    @opponent = opponent
   end
 
   attr_reader :player_choice, :comp_choice
 
   def draw?
-    @player_choice == @comp_choice
+    @player.choice == @opponent.choice
+  end
+
+  def win?
+    'RSPR'.include?(@player.choice[0]+@opponent.choice[0])
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,28 +2,31 @@ require 'game'
 
 describe Game do
 
-  subject(:game) { described_class.new('Rock') }
+  let(:player) { double :player, :choice => 'Rock' }
+  let(:opponent) { double :opponent }
+
+  subject { described_class.new(player,opponent) }
 
   it 'exists' do
-    expect(game).to be_an_instance_of(Game)
+    expect(subject).to be_an_instance_of(Game)
   end
 
   describe '#draw?' do
     it 'returns true if game is a draw' do
-      allow(subject).to receive(:comp_choice) { 'Rock' }
-      expect(game.draw?).to eq true
+      allow(opponent).to receive(:choice) { 'Rock' }
+      expect(subject.draw?).to eq true
     end
   end
 
   describe '#win?' do
     it 'returns true if player has beaten computer' do
-      allow(subject).to receive(:comp_choice) { 'Scissors' }
-      expect(game.win?).to eq true
+      allow(opponent).to receive(:choice) { 'Scissors' }
+      expect(subject.win?).to eq true
     end
 
     it 'returns false if computer has beaten player' do
-      allow(subject).to receive(:comp_choice) { 'Paper'}
-      expect(game.win?).to eq false
+      allow(opponent).to receive(:choice) { 'Paper'}
+      expect(subject.win?).to eq false
     end
   end
 
