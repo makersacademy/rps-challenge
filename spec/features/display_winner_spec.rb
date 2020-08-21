a99e276d81e6ff4c1fe036c9fa33a353d545926d
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-08-21 17:39:10 +0100

    added test for play again? button

--- spec/features/display_winner_spec.rb
+++ spec/features/display_winner_spec.rb
@@ -7,3 +7,13 @@ feature 'Displays winner page' do
     expect(page).to have_content 'Charlotte wins!'
   end
  end
+ feature 'Play again?' do
+  scenario 'Takes player back to the start page' do
+    visit('/')
+    fill_in :name, with: "Charlotte"
+    click_button("Let's play!")
+    click_button('Rock')
+    click_button('Play again?')
+    expect(page).to have_content 'Welcome to Rock, Paper, Scizzors!'
+  end
+ end
