a8e1525540c0bf265ca045686fc4ee15648f510f
Iliyan-Y <maximizebg@gmail.com>
2020-08-21 19:23:08 +0300

    Feature test for user to choose Rock Paper Scissors passed

--- app.rb
+++ app.rb
@@ -1,21 +1,30 @@
 require "sinatra/base"
+require "./lib/player"
 
 class App < Sinatra::Base
 
   enable :sessions
+
+  before do 
+    @player = Player.instance
+  end
 
   get "/" do
     erb :index
   end
 
   post '/get_name' do 
-    @player = Player.new(params[:Player])
+    @player = Player.create(params[:Player])
     redirect "/options"
   end
 
   get "/options" do
-    @player = session[:player]
     erb :options
+  end
+
+  post "/player_choice" do
+    @player.round_choice(params[:option])
+    redirect "/options"
   end
 
   # start the server if ruby file executed directly
--- spec/features/player_options_spec.rb
+++ spec/features/player_options_spec.rb
@@ -7,7 +7,7 @@ feature "User options page" do
   scenario "User choos an option and proceed to play" do 
     sing_n_play
     click_button "Rock"
-    expect(page).to have_content "You choose Rock"
+    expect(page).to have_content "You choose: Rock"
   end
 
 end
