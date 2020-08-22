03b8ecda88e7ceb87839f1a3abbef68b09c5e4a8
Iliyan-Y <maximizebg@gmail.com>
2020-08-22 17:47:57 +0300

    Feature test for second player turn to choose added

--- spec/features/multy_player_spec.rb
+++ spec/features/multy_player_spec.rb
@@ -9,4 +9,11 @@ feature "Game can be played with thwo people" do
     click_link "Confirm Choice"
     expect(page).to have_content "Player_2 turn"
   end
+
+  scenario "Second player turn to choose" do 
+    multy_sign_n_play
+    click_link "Confirm Choice"
+    click_button "Ok"
+    expect(page).to have_content "Welcome warrior Player_2"
+  end
 end
