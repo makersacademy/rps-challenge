a835477ae17933393b647c621f4361739d24e8ad
Iliyan-Y <maximizebg@gmail.com>
2020-08-22 15:14:22 +0300

    Test for game winner passed

--- lib/game.rb
+++ lib/game.rb
@@ -22,4 +22,22 @@ class Game
     @players[1]
   end
 
+  def winner(player_1,player_2)
+    if player_1.choice == "Rock" and player_2.choice == "Paper"
+      return player_2
+    elsif player_1.choice == "Rock" and player_2.choice == "Scissors"
+      return player_1
+    elsif player_1.choice == "Scissors" and player_2.choice == "Rock"
+      return player_2
+    elsif player_1.choice == "Scissors" and player_2.choice == "Paper" 
+      return player_1
+    elsif player_1.choice == "Paper" and player_2.choice == "Rock"
+      return player_1
+    elsif player_1.choice == "Paper" and player_2.choice == "Scissors"
+      return player_2
+    else 
+      return "Draw"
+    end
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,11 @@
 require "game"
 
 describe Game do 
-  let(:game) { Game.new("player1", "player2") }
-  let(:player_1) { double("player_1", choice:"Rcok") }
-  let(:player_2) { double("player_2", choice:"Paper") }
+  let(:player_1) { double("player_1") }
+  let(:player_2) { double("player_2") }
+  let(:game) { Game.new(player_1, player_2) }
 
-  it { expect(game.players).to match_array(["player1", "player2"]) }
+  it { expect(game.players).to match_array([player_1, player_2]) }
   
   describe "#instance" do
     it "return an instance of the created class" do 
@@ -16,19 +16,21 @@ describe Game do
 
   describe "#player_1" do 
     it "return the instance of the first player in the game" do 
-      expect(game.player_1).to eq "player1"
+      expect(game.player_1).to eq player_1
     end
   end
 
   describe "#player_2" do 
     it "return the instance of the second player in the game" do 
-      expect(game.player_2).to eq "player2"
+      expect(game.player_2).to eq player_2
     end
   end
 
   describe "#winner" do 
-    it "return the winer of the game" do 
-      expect(subject.winner(player_1.choice, player_2.choice)).to eq player_2
+    it "return the winer of the game" do
+      allow(player_1).to receive(:choice).and_return("Rock")
+      allow(player_2).to receive(:choice).and_return("Scissors")
+      expect(game.winner(player_1, player_2)).to eq player_1
     end
   end
 
