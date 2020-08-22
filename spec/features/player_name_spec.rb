8b837318360d3bb98657de8c7d6199a1c4062b88
Djura22 <michaeliangroves@gmail.com>
2020-08-22 11:54:27 +0100

    web_helpers added to refactor tests and keep things DRY

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,9 +1,7 @@
 feature 'Player name entry' do
 
 	scenario 'player enters name, name is displayed' do
-		visit('/')
-		fill_in :player_name, with: "Feynman"
-		click_button "Submit"
+		sign_in_and_play
 		expect(page).to have_text("Feynman")
 	end
 
