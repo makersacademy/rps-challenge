7aa96ffb4d7c08b0be344be8a1b89dda6025e1cc
Iliyan-Y <maximizebg@gmail.com>
2020-08-22 11:27:50 +0300

    Game class contains players test passsed

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,5 +2,5 @@ require "game"
 
 describe Game do 
   let(:game) { Game.new("player1", "player2") }
-  it { expect(subject.players.length).to match_array(["player1", "player2"]) }
+  it { expect(game.players).to match_array(["player1", "player2"]) }
 end
