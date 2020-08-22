b215706f8e3a7eed0ab569bdf6c9d9dfed803059
Djura22 <michaeliangroves@gmail.com>
2020-08-22 15:32:22 +0100

    test for Player class written

--- spec/features/rules_spec.rb
+++ spec/features/rules_spec.rb
@@ -1,5 +1,11 @@
 feature 'Rules' do
 
+	scenario 'player name is displayed' do
+		sign_in_and_play
+		visit('/rules')
+		expect(page).to have_text("Feynman")
+	end
+
 	scenario 'Page displays rules of engagement' do
 		visit('/rules')
 		expect(page).to have_text("Rock beats Scissors")
---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require 'player'
+
+describe Player do
+
+	it 'creates instance of Player class' do
+		expect(subject).to be_instance_of Player
+	end
+
+	
+end
