b216c6f89f059df9e26090b02a6e0128b5dec9c8
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-23 17:17:11 +0100

    added move instance variable to player class, gave it an attr_accessor, passes tests

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,9 @@
 class Player
   attr_reader :name
+  attr_accessor :move
   
   def initialize(name = 'Hal3000')
-    @name = name.capitalize 
+    @name = name.capitalize
+    @move = nil
   end
 end
