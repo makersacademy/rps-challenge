bc3bb200add54ee4f1a4cdb72e4ba4ad3cebd5d5
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-23 10:35:41 +0100

    added Text and 1 or 2 player mode buttons to '/', fixed capybara tests, passes tests

--- spec/features/1_or_2_player_spec.rb
+++ spec/features/1_or_2_player_spec.rb
@@ -1,4 +1,7 @@
 feature '1 or 2 player' do
+  before do
+    visit('/')
+  end
   scenario 'choosing single or multiplayer' do
     expect(page).to have_content 'Would you like to play alone or with a friend?'
   end
