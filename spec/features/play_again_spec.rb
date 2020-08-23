ee7bb6c48c53cb8b355d2e276db6128b242bc456
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-23 11:56:52 +0100

    play again tests added and passed

--- spec/features/play_again_spec.rb
+++ spec/features/play_again_spec.rb
@@ -2,14 +2,14 @@ feature 'After choosing, user can play again' do
   scenario 'signs in, plays a game and same player wants to play again' do
     player_1_sign_in_and_play
     click_button 'Rock'
-    click_button 'Play again'
+    click_button 'play_again'
     expect(page).to have_content 'Yasmin what is your choice?'
   end
 
   scenario 'signs in, plays a game and new player wants to play' do
     player_1_sign_in_and_play
     click_button 'Rock'
-    click_button 'New player'
+    click_button 'new_player'
     expect(page).to have_content 'Welcome to Rock, Paper, Scissors'
   end
 end
