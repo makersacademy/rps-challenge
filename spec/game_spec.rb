6d244f504f35a39b9f5b3e5d6a95a31409465a3e
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-24 08:55:19 +0100

    added test for winner method in game class, passes test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,13 +2,22 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.init(player_1, player_2) }
+  subject(:endgame) {described_class.init(spock_player, lizard_player)}
   let(:player_1) {double :player, name: 'narwhal'}
   let(:player_2) {double :player, name: 'horse'}
+  let(:spock_player) {double :player, name: 'horse', move: 'Spock'}
+  let(:lizard_player) {double :player, name: 'horse', move: 'lizard'}
 
   describe '#initialize' do
     it 'takes players as arugments and assigns player to instance varibales' do
       expect(game.player_1).to eq player_1
       expect(game.player_2).to eq player_2
+    end
+  end
+
+  describe '#winner' do
+    it 'returns the name of the winning player based on their move' do
+      expect(endgame.winner).to eq lizard_player.name
     end
   end
 end
