e03cf1658f15bd142c83eaf6b096cd32239485a0
Louis Kirkham <louiskirkham@gmail.com>
2020-09-19 09:14:20 +0100

    allows computer opponent & set up player class

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ class Game
 
   attr_reader :p1, :p2
 
-  def initialize(p1, p2)
+  def initialize(p1, p2 = "AI")
     @p1 = p1
     @p2 = p2
   end
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -6,5 +6,12 @@ describe Game do
     expect(game.p1).to eq "Penn"
     expect(game.p2).to eq "Teller"
   end
+
+  it 'allows a computer opponent' do
+    solo_game = Game.new("Crusoe")
+    expect(solo_game.p1).to eq "Crusoe"
+    expect(solo_game.p2).to eq "AI"
+  end
+
 end
     
