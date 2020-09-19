65430799bdd4576f3ca3f26c4c4ba29fbbfc2135
Veronica Lee <veronicavlee@icloud.com>
2020-09-19 20:30:50 +0100

    Add #play method and tests to Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,15 @@
 class Game
   attr_reader :name
 
+  WINNING_MOVES = {
+    rock: :scissors,
+    scissors: :paper,
+    paper: :rock
+  }
+
   def initialize(name)
     @name = name
+
   end
 
   def self.create(name)
@@ -12,4 +19,25 @@ class Game
   def self.instance
     @game
   end
+  #
+  # def name
+  #   @name
+  # end
+
+  def play(move1, move2 = random_move)
+    if WINNING_MOVES[move1.to_sym] == move2.to_sym
+      'You won!'
+    elsif move1 == move2
+      'You tied'
+    else
+      'You lost'
+    end
+  end
+
+  private
+
+  def random_move
+    ['rock', 'paper', 'scissors'].sample
+  end
+
 end
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -4,6 +4,9 @@ describe Game do
 
   subject(:game) { Game.new(name) }
   let(:name) { 'Veronica' }
+  let(:win_msg) { 'You won!' }
+  let(:tie_msg) { 'You tied' }
+  let(:lose_msg) { 'You lost' }
 
   it 'takes a name argument on initialisation' do
     expect(Game).to respond_to(:new).with(1).argument
@@ -24,6 +27,36 @@ describe Game do
     it 'returns the game instance' do
       Game.create(name)
       expect(Game.instance).to be_an_instance_of(Game)
+    end
+  end
+
+  describe '#play' do
+    it "returns 'You won!' if P1 move = rock and P2 move = scissors" do
+      expect(game.play('rock', 'scissors')).to eq(win_msg)
+    end
+
+    it "returns 'You won!' if P1 move = paper and P2 move = rock" do
+      expect(game.play('paper', 'rock')).to eq(win_msg)
+    end
+
+    it "returns 'You won!' if P1 move = scissors and P2 move = paper" do
+      expect(game.play('scissors', 'paper')).to eq(win_msg)
+    end
+
+    it "returns 'You tied' if P1 move is the same as P2 move" do
+      expect(game.play('rock', 'rock')).to eq(tie_msg)
+    end
+
+    it "returns 'You lost' if P1 move = rock and P2 move = paper" do
+      expect(game.play('rock', 'paper')).to eq(lose_msg)
+    end
+
+    it "returns 'You lost' if P1 move = paper and P2 move = scissors" do
+      expect(game.play('paper', 'scissors')).to eq(lose_msg)
+    end
+
+    it "returns 'You lost' if P1 move = scissors and P2 move = rock" do
+      expect(game.play('scissors', 'rock')).to eq(lose_msg)
     end
   end
 end
