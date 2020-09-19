3419610eb5e10c306ab8f99d34d8fd02f6df9b81
samcolson4 <samcolson4@gmail.com>
2020-09-19 14:21:12 +0100

    Refactored app.rb. Updated readme.

--- app.rb
+++ app.rb
@@ -37,44 +37,35 @@ class Rps < Sinatra::Base
   end
 
   get '/rock' do
-    if @game.again?
-      @game.active_player.shake("rock")
-      @game.switch_players(@game.player_one, @game.player_two)
-      redirect '/rps'
-    elsif @game.multi?
-      @game.active_player.shake("rock")
-      redirect '/MPresult'
-    else
-      @game.active_player.shake("rock")
-      redirect '/result'
+    shake = "rock"
+    if @game.again? #checks that it is multiplayer & player2 has not gone yet.
+      go_again(shake)
+    elsif @game.multi? #if p1 has been, but not p2
+      both_gone(shake)
+    else #anything else means it must be single player.
+      sp_only(shake)
     end
   end
 
   get '/paper' do
+    shake = "paper"
     if @game.again?
-      @game.active_player.shake("paper")
-      @game.switch_players(@game.player_one, @game.player_two)
-      redirect '/rps'
+      go_again(shake)
     elsif @game.multi?
-      @game.active_player.shake("paper")
-      redirect '/MPresult'
+      both_gone(shake)
     else
-      @game.active_player.shake("paper")
-      redirect '/result'
+      sp_only(shake)
     end
   end
 
   get '/scissors' do
+    shake = "scissors"
     if @game.again?
-      @game.active_player.shake("scissors")
-      @game.switch_players(@game.player_one, @game.player_two)
-      redirect '/rps'
+      go_again(shake)
     elsif @game.multi?
-      @game.active_player.shake("scissors")
-      redirect '/MPresult'
+      both_gone(shake)
     else
-      @game.active_player.shake("scissors")
-      redirect '/result'
+      sp_only(shake)
     end
   end
 
@@ -84,6 +75,23 @@ class Rps < Sinatra::Base
 
   get '/MPresult' do
     erb :mpresult
+  end
+
+# controller methods
+  def go_again(shake)
+    @game.active_player.shake(shake)
+    @game.switch_players(@game.player_one, @game.player_two)
+    redirect '/rps'
+  end
+
+  def both_gone(shake)
+    @game.active_player.shake(shake)
+    redirect '/MPresult'
+  end
+
+  def sp_only(shake)
+    @game.active_player.shake(shake)
+    redirect '/result'
   end
 
 end
