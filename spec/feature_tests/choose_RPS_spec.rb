4f45287efdfb1d30f2f746dd00893f420e93930a
farhaan-ali <fa8g12@gmail.com>
2020-09-20 18:26:53 +0100

    feature test for clicking rock

---
+++ spec/feature_tests/choose_RPS_spec.rb
@@ -0,0 +1,8 @@
+require_relative './web_helper.rb'
+feature 'clicking rock paper...' do
+  scenario 'click rock' do
+    sign_in_and_play
+    click_button 'Rock'
+    expect(page).to have_content 'Farhaan Chose Rock'
+  end
+end
