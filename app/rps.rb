1917b03010b503fe925838e1e285d9a6dacb8f77
Meryl Rastall <merylrastall@gmail.com>
2020-10-16 08:13:57 +0100

    r4efactored to have a post/get redirect for the start game

--- app/rps.rb
+++ app/rps.rb
@@ -1,10 +1,21 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+  
   get '/' do
     erb :index
   end
 
+  post '/start_game' do
+    session[:player1] = params[:player1]
+     redirect '/play'
+  end
 
-  run! if app_file == $0
+  get '/play' do
+    @name = session[:player1]
+    erb :welcome_player
+  end
+
+    run! if app_file == $0
 end
