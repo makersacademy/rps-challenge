86faaaa2a77a59a73997dfa38b6583c7cbbdf6e1
Kiril <kiril@olgas-air.mynet>
2020-10-16 14:47:29 +0100

    pass 1st test and wrote test for gtreeting

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
-  
+  get '/' do
+    erb(:index)
+  end
 end
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,4 +1,10 @@
 feature "homepage" do
+  scenario "user visits homepage and is greeted and asked to enter his name" do
+    visit('/')
+    expect(page).to have_content ("Hello!")
+    expect(page).to have_content ("Enter your name to play RPS.")
+  end
+
   scenario "user visits homepage and enters their name" do
     visit("/")
     fill_in "name",	with: "Leo"
