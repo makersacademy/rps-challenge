25319816a3c27dfb85319086a70700b0516d9020
Oli Le Maire <olilemaire85@gmail.com>
2020-10-17 19:06:39 +0100

    added a welcome page with a request for a name to be entered

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
-require 'sinatra'
+require 'sinatra/base'
+
+class PlayerName < Sinatra::Base
+enable :sessions
 
 get '/' do
-  'hello'
+  erb(:home_page)
+end
+
+post '/names' do
+  "hello"
+  session[:player_name] = params[:player_name]
+  redirect '/play'
+end
+
+get '/play' do
+  @player_name = session[:player_name]
+  erb(:play)
+end
+
+run! if app_file == $0
 end
---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,24 @@
+require '././app.rb'
+
+feature 'Testing infrastructure' do
+  scenario 'can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'What is your name?'
+  end
+end
+
+feature 'Enter player name' do
+  scenario 'submitting player name' do
+    visit('/')
+    fill_in :player_name, with: 'Oli'
+    click_button "Enter"
+    expect(page).to have_content 'Welcome Oli'
+  end
+end
+
+# What does the user have to do?
+# What does the user expect to see?
+
+# Visit the home page ('/')
+# Fill in a text field with a name
+# Click a Submit button
