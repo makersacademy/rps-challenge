0cacc0fe1434a3c987488c9fec9ace52b3648de4
Ben <ben.lynch1931@gmail.com>
2020-10-17 17:28:16 +0100

    TEST Unit: can calculate winner based on inputs

--- app/models/logic.rb
+++ app/models/logic.rb
@@ -1,11 +1,43 @@
 class Game
   COMPUTER_NAMES = ["Tanya", "Mike", "Carlos", "Megan", "Beth", "Patricia", "Patrick", "Stewart"]
+  CHOICES = ["ROCK", "PAPER", "SCISSORS"]
 
   def add_names(player_name)
+    @computer_name = COMPUTER_NAMES.sample
     @player_name = player_name
+
   end
 
   def print_players
-    "#{player_name} Vs. #{COMPUTER_NAMES.sample}"
+    "#{player_name} Vs. #{@computer_name.sample}"
+  end
+
+  def calculate_winner(user_input)
+    @computer_input = CHOICES.sample
+    case user_input
+      when "ROCK"
+        return "Noone" if computer == 1
+        return "#{@computer_name}" if computer == 2
+        return "#{@player_name}" if computer == 3
+      when "PAPER"
+        return "Noone" if computer == 2
+        return "#{@computer_name}" if computer == 3
+        return "#{@player_name}" if computer == 1
+      when "SCISSORS"
+        return "Noone" if computer == 3
+        return "#{@computer_name}" if computer == 1
+        return "#{@player_name}" if computer == 2
+      end
+
+  end
+
+  def computer
+    if @computer_input == "ROCK"
+      return 1
+    elsif @computer_input == "PAPER"
+      return 2
+    elsif @computer_input == "SCISSORS"
+      return 3
+    end
   end
 end
--- spec/units/logic_spec.rb
+++ spec/units/logic_spec.rb
@@ -8,4 +8,36 @@ describe Game do
       expect(game.add_names("Ben")).to eq "Ben"
     end
   end
+
+  context '#calculate_winner' do
+    it "puts noone as it's a draw" do
+      srand(45)
+      expect(game.calculate_winner("SCISSORS")).to eq "Noone"
+      srand(50)
+      expect(game.calculate_winner("ROCK")).to eq "Noone"
+      srand(1)
+      expect(game.calculate_winner("PAPER")).to eq "Noone"
+    end
+
+    it "puts Tanya as computer won" do
+      srand(2)
+      game.add_names("Ben")
+      srand(45)
+      expect(game.calculate_winner("PAPER")).to eq "Tanya"
+      srand(50)
+      expect(game.calculate_winner("SCISSORS")).to eq "Tanya"
+      srand(1)
+      expect(game.calculate_winner("ROCK")).to eq "Tanya"
+    end
+
+    it "puts Ben as Ben won" do
+      game.add_names("Ben")
+      srand(45)
+      expect(game.calculate_winner("ROCK")).to eq "Ben"
+      srand(50)
+      expect(game.calculate_winner("PAPER")).to eq "Ben"
+      srand(1)
+      expect(game.calculate_winner("SCISSORS")).to eq "Ben"
+    end
+  end
 end
