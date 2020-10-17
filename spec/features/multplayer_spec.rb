fe9b978397d74170e9c3205e5eb820ec59855eb4
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-17 17:12:38 +0100

    Spelling mistakes corrected on homepage

--- spec/features/multplayer_spec.rb
+++ spec/features/multplayer_spec.rb
@@ -1,5 +1,12 @@
-feature 'multiplayer' do
+feature 'multiplayer option' do
   scenario 'user can choose multiplayer' do
+    visit('/')
+    expect(page).to have_content("Would you like to play multiplayer or solo?")
+  end
+end
+
+feature 'current turn displayed on screen' do
+  scenario "player 1's turn displayed on screen at start" do
     visit('/')
     click_button('Multiplayer')
     fill_in :player_1, with: "Harry"
