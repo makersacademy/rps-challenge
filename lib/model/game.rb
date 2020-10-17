b2bbc155672ec931366b2adecfbc55db7e7b4603
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-17 11:52:19 +0100

    Add #calculate_result method to Game class that does what it says on the tin

--- lib/model/game.rb
+++ lib/model/game.rb
@@ -6,16 +6,36 @@ class Game
     @player = player
   end
 
-  def make_choice
+  def make_robo_choice
     @choice = [:rock, :paper, :scissors].sample
   end
 
-  def return_choice
+  def return_robo_choice
     @choice
   end
 
-  def result
+  def calculate_result
+
+    case
+    when return_robo_choice == @player.choice
+      @result = "It's a draw."
+    when (return_robo_choice == :rock) && (@player.choice == :paper)
+      @result = "You won, ok. Big deal."
+    when (return_robo_choice == :rock) && (@player.choice == :scissors)
+      @result = "YOU LOST, GET OVER IT"
+    when (return_robo_choice == :paper) && (@player.choice == :rock)
+      @result = "YOU LOST, GET OVER IT"
+    when (return_robo_choice == :paper) && (@player.choice == :scissors)
+      @result = "You won, ok. Big deal."
+    when (return_robo_choice == :scissors) && (@player.choice == :rock)
+      @result = "You won, ok. Big deal."
+    when (return_robo_choice == :scissors) && (@player.choice == :paper)
+      @result = "YOU LOST, GET OVER IT"
+    end
+
+    @result
 
   end
+
 
 end
--- spec/unit_tests/game_spec.rb
+++ spec/unit_tests/game_spec.rb
@@ -2,4 +2,45 @@ require 'model/game'
 
 describe Game do
 
+  let(:player) { double :player }
+  let(:game) { described_class.new(player) }
+
+  describe "#make_choice" do
+    it "returns :rock as a symbol" do
+      srand(2)
+      game.make_robo_choice
+      expect(game.return_robo_choice).to eq(:rock)
+    end
+    it "returns :paper as a symbol" do
+      srand(1)
+      game.make_robo_choice
+      expect(game.return_robo_choice).to eq(:paper)
+    end
+    it "returns :scissors as a symbol" do
+      srand(5)
+      game.make_robo_choice
+      expect(game.return_robo_choice).to eq(:scissors)
+    end
+  end
+
+  describe "#calculate_result" do
+    it "returns the correct result: paper vs rock" do
+      srand(2)
+      game.make_robo_choice
+      allow(player).to receive(:choice).and_return(:paper)
+      expect(game.calculate_result).to eq("You won, ok. Big deal.")
+    end
+    it "returns the correct result: paper vs scissors" do
+      srand(5)
+      game.make_robo_choice
+      allow(player).to receive(:choice).and_return(:paper)
+      expect(game.calculate_result).to eq("YOU LOST, GET OVER IT")
+    end
+    it "returns the correct result: rock vs rock" do
+      srand(2)
+      game.make_robo_choice
+      allow(player).to receive(:choice).and_return(:rock)
+      expect(game.calculate_result).to eq("It's a draw.")
+    end
+  end
 end
