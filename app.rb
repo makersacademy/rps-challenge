5acfc7d000f80f5a02ba2608cafb91134bcefae1
Cameron Logie <cameronlogie24@gmail.com>
2020-10-17 18:01:44 +0100

    name of person goes ot play page after submitting form on homepage

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require 'sinatra'
 
 class RPS < Sinatra::Base
 
+    enable :sessions
+
     get '/' do
         erb :homepage
     end
-    
+
+    post '/name' do
+        session[:player_name] = params[:player_name]
+        redirect to('/play')
+    end
+
+    get '/play' do
+        @player_name = session[:player_name]
+        erb :play
+    end
+
+run! if app_file == $0
+
 end
