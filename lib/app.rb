47b4560636d8eaee5de8674b389764ee9795f5dc
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-17 09:02:54 +0100

    Add simple post, redirect, get loop leading to main page (play.erb)

--- lib/app.rb
+++ lib/app.rb
@@ -3,11 +3,22 @@ require 'shotgun'
 
 class RPS < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get "/" do
     erb(:index)
   end
 
   post "/names" do
-    
+    session[:player1_name] = params[:player1_name]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player1_name = session[:player1_name]
+    erb(:play)
+  end
+
 
 end
