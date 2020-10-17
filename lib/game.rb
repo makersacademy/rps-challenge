4e020e482f347c9b0545161f1c88674be227099b
KC Thomas <thisiskcthomas@gmail.com>
2020-10-17 11:57:06 +0100

    Game can now determine winner. Wrote and passed spec tests, refactored.

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,35 @@
 class Game
+
+  def initialize(choice = 'rock')
+    @choice = choice
+    @computer = choose_rps
+  end
+
+  def choose_rps
+    ['rock', 'paper', 'scissors'].sample
+  end
+
+  def compare_choices
+    return 'tie' if choices_equal?
+    return 'Player Wins!' if player_wins?
+    'Computer Wins!'
+  end
+
+  private
+
+  def player_wins?
+    if @choice == 'scissors'
+      return true if @computer == 'paper'
+    elsif @choice == 'paper'
+      return true if @computer == 'rock'
+    else
+      return true if @computer == 'scissors'
+    end
+    false
+  end
+
+  def choices_equal?
+    @choice == @computer
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,28 @@
 require 'game'
 
 describe Game do
+
+  describe '#choose_rps' do
+    it 'selects a random choice from an array' do
+      expect(['rock', 'paper', 'scissors']).to include(subject.choose_rps)
+    end
+  end
+
+  describe '#compare_choices' do
+    before :each do
+      @game = Game.new
+    end
+    it 'says if the choices are tied' do
+      allow(@game).to receive(:choose_rps) { 'rock' }
+      expect(@game.compare_choices).to eq 'tie'
+    end
+    it 'confirms player win' do
+      allow(@game).to receive(:choose_rps) { 'scissors' }
+      expect(@game.compare_choices).to eq 'Player Wins!'
+    end
+    it 'confirms computer win' do
+      allow(@game).to receive(:choose_rps) { 'paper' }
+      expect(@game.compare_choices).to eq 'Computer Wins!'
+    end
+  end
 end
