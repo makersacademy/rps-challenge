a9e7efdb63834a492fafe49115d7c83e9d1c6894
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-18 00:04:41 +0100

    User can view which move they have picked (now for any move chosen)

--- spec/features/moves_feature_spec.rb
+++ spec/features/moves_feature_spec.rb
@@ -5,3 +5,19 @@ feature 'user chooses a move' do
     expect(page).to have_content("Rock")
   end
 end
+
+feature 'user chooses a move' do
+  scenario 'user picks paper' do
+    sign_in_and_play
+    click_button("Paper")
+    expect(page).to have_content("Paper")
+  end
+end
+
+feature 'user chooses a move' do
+  scenario 'user picks scissors' do
+    sign_in_and_play
+    click_button("Scissors")
+    expect(page).to have_content("Scissors")
+  end
+end
