63539a5fea213209a156a8abf0f68c93ece5a764
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-18 15:56:51 +0100

    Created session storing user name and redirected to get /play method

--- lib/app.rb
+++ lib/app.rb
@@ -4,14 +4,19 @@ require 'shotgun'
 class RPS < Sinatra::Base
   enable :sessions
   set :session_secret, 'Rock is my favourite move'
-  
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
     puts params
-    @player1 = params[:player1]
+    session[:player1] = params[:player1]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1 = session[:player1]
     erb(:play)
   end
 end
