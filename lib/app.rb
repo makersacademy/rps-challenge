f27de7e5db16ca0464a4a70be3c6a994e21b5932
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-18 15:47:32 +0100

    After user submits name, it is returned to them on screen - using an instance variable for storage

--- lib/app.rb
+++ lib/app.rb
@@ -2,11 +2,16 @@ require 'sinatra/base'
 require 'shotgun'
 
 class RPS < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'Rock is my favourite move'
+  
   get '/' do
     erb(:index)
   end
 
-  get '/names' do
-    play(:erb)
+  post '/names' do
+    puts params
+    @player1 = params[:player1]
+    erb(:play)
   end
 end
