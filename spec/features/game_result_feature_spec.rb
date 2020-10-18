b540b8f9a576818cf06eecaaa6e845ed3d4fc980
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-18 12:08:45 +0100

    Renamed move page to result to amke more sense, players can view result of the game with basic result for test

---
+++ spec/features/game_result_feature_spec.rb
@@ -0,0 +1,8 @@
+feature 'game result' do
+  scenario 'users can see the result of the game' do
+    sign_in_and_play
+    srand(1)
+    click_button("Rock")
+    expect(page).to have_content ("Game result")
+  end
+end
