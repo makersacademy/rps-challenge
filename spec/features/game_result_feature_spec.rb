0d0053fcaace4f6a8577fe23221233f9e50c5977
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-18 22:44:32 +0100

    Updated feature tests to view results. Added CSS style sheet

--- spec/features/game_result_feature_spec.rb
+++ spec/features/game_result_feature_spec.rb
@@ -3,6 +3,6 @@ feature 'game result' do
     sign_in_and_play
     srand(1)
     click_button("Rock")
-    expect(page).to have_text "Game result"
+    expect(page).to have_text "You lose!"
   end
 end
