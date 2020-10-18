afc3f030d0d69a4f254de14db899b788f71a11c0
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-18 18:52:19 +0100

    changed the unit test to cover different user input of RPS

--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -3,8 +3,6 @@ require 'game.rb'
 describe Game do
   subject(:game) { Game.new }
 
-  it { is_expected.to respond_to :bot_move }
-
   describe 'calculating the winner' do
     context 'player 1 chooses rock and computer output is paper' do
       it 'returns player 1 loses' do
@@ -14,8 +12,8 @@ describe Game do
     end
     context 'player 1 chooses rock and computer output is scissors' do
       it "returns it's a draw" do
-        allow(game).to receive(:bot_move) { "scissors" }
-        expect(game.winner("rock")).to eq("Player 1 wins!")
+        allow(game).to receive(:bot_move) { "paper" }
+        expect(game.winner("scissors")).to eq("Player 1 wins!")
       end
     end 
     context 'player 1 chooses rock and computer output is rock' do
