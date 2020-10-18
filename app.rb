8a73568bb91246b715832f0171edeb48d66af25a
Otis <otisvickersgraver@gmail.com>
2020-10-18 15:26:10 +0100

    player can select a weapon

--- app.rb
+++ app.rb
@@ -14,10 +14,17 @@ class RPS < Sinatra::Base
 
   get '/play' do
     @player_name = session[:player_name]
-    @player_name = session[:player_name]
+    @player_choice = session[:player_choice]
     @computer = 'Computer'
-    erb :play
+    erb(:play)
   end
-  
+
+  get '/battle' do
+    session[:player_choice] = params[:player_choice]
+    @player_choice = session[:player_choice]
+    @player_name = session[:player_name]
+    erb(:battle)
+    redirect '/play'
+  end
   run! if app_file == $0
 end
---
+++ spec/features/select_weapon_spec.rb
@@ -0,0 +1,9 @@
+feature 'Player can select a weapon' do
+  scenario 'shows players weapon on screen' do
+    visit('/')
+    fill_in :player_name, with: 'Otis'
+    click_button 'Submit'
+    click_button 'paper'
+    expect(page).to have_content 'Otis selected paper.'
+  end
+end
