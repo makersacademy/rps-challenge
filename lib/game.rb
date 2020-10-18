816ae99c1d43f74ed88f3febfdac09f4ba2f75fb
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-18 17:12:25 +0100

    tested #winner (and passed) - outputs The computer wins if the computer wins

--- lib/game.rb
+++ lib/game.rb
@@ -1,15 +1,25 @@
 class Game
 
-  winner = {
-      "rock" => "scissors",
-      "scissors" => "paper",
-      "paper" => "scissors"
-  }
+  def initialize
+    @scoring = {
+        "rock" => "scissors",
+        "scissors" => "paper",
+        "paper" => "scissors"
+    }
+  end
   
   def bot_move
     ["rock", "paper", "scissors"].sample
   end
-
-
-
+  
+  def winner(p1move)
+    computer_move = bot_move 
+    if @scoring[p1move] == computer_move
+      "Player 1 wins!"
+    elsif p1move == computer_move
+      "It's a draw!"
+    else
+      "The computer wins!" 
+    end
+  end
 end
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -1,7 +1,16 @@
-require 'game'
+require 'game.rb'
 
 describe Game do
   subject(:game) { Game.new }
 
   it { is_expected.to respond_to :bot_move }
+
+  describe 'calculating the winner' do
+    context 'player 1 chooses rock and computer output is paper' do
+      it 'returns player 1 loses' do
+        allow(game).to receive(:bot_move) { "paper" }
+        expect(game.winner("rock")).to eq("The computer wins!")
+      end
+    end
+  end
 end
