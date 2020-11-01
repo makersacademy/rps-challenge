eb4da3a56e912d97a0de40605b141753c71a64ab
GDCStoney <g.stoney@btinternet.com>
2020-11-01 11:51:32 +0000

    rspec capitalisation updates to pass

--- spec/features/04_result_spec.rb
+++ spec/features/04_result_spec.rb
@@ -2,25 +2,25 @@ feature 'game completed' do
   scenario ' - player 1 one clicks Rock' do
     game_sign_in
     click_button 'Rock'
-    expect(page).to have_content 'Jane: rock'
-    expect(page).not_to have_content 'Jane: paper'
-    expect(page).not_to have_content 'Jane: scissors'
+    expect(page).to have_content 'Jane: Rock'
+    expect(page).not_to have_content 'Jane: Paper'
+    expect(page).not_to have_content 'Jane: Scissors'
   end
 
   scenario ' - player 1 one clicks paper' do
     game_sign_in
     click_button 'Paper'
-    expect(page).not_to have_content 'Jane: rock'
-    expect(page).to have_content 'Jane: paper'
-    expect(page).not_to have_content 'Jane: scissors'
+    expect(page).not_to have_content 'Jane: Rock'
+    expect(page).to have_content 'Jane: Paper'
+    expect(page).not_to have_content 'Jane: Scissors'
   end
 
   scenario ' - player 1 one clicks scissors' do
     game_sign_in
     click_button 'Scissors'
-    expect(page).not_to have_content 'Jane: rock'
-    expect(page).not_to have_content 'Jane: paper'
-    expect(page).to have_content 'Jane: scissors'
+    expect(page).not_to have_content 'Jane: Rock'
+    expect(page).not_to have_content 'Jane: Paper'
+    expect(page).to have_content 'Jane: Scissors'
   end
 
   scenario '- players draw' do
