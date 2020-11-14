b129590005212f501abd191db5de1a44f7519aae
Alex Withington-Smith <alex@awithsmith.com>
2020-11-14 07:37:36 +0000

    wrote the logic for playing a round

--- lib/rockpaperscissors.rb
+++ lib/rockpaperscissors.rb
@@ -2,8 +2,38 @@ class RockPaperScissors
   DEFAULT_ROUNDS = 5
   CHOICES = [:rock, :paper, :scissors]
   COMBINATION_MAP = {
-    :rock => { :rock => 'Draw', :paper => 'Loss', :scissors => 'Win' },
-    :paper => { :rock => 'Win', :paper => 'Draw', :scissors => 'Loss' },
-    :scissors => { :rock => 'Loss', :paper => 'Win', :scissors => 'Draw' }
+    :rock => { :rock => :draw, :paper => :loss, :scissors => :win },
+    :paper => { :rock => :win, :paper => :draw, :scissors => :loss },
+    :scissors => { :rock => :loss, :paper => :win, :scissors => :draw }
   }
+
+  attr_reader :round
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @round = 0
+  end
+
+  def self.new_game(player1, player2)
+    @game = RockPaperScissors.new(player1, player2)
+  end
+
+  def self.instance
+    @game
+  end
+
+  def play_round(p1_choice, p2_choice)
+    @round += 1
+    result = COMBINATION_MAP[p1_choice][p2_choice]
+    return result if result == :draw
+
+    if result == :win
+      @player1.score += 1
+    else
+      @player2.score += 1
+    end
+  
+    result
+  end
 end
--- spec/unit/rockpaperscissors_spec.rb
+++ spec/unit/rockpaperscissors_spec.rb
@@ -1,1 +1,35 @@
 require 'rockpaperscissors'
+
+describe RockPaperScissors do
+  let(:player1) { double :player, name: "Brian", score: 0 }
+  let(:player2) { double :player2, name: "Partario", score: 0 }
+  let(:testgame) { RockPaperScissors.new(player1, player2) }
+
+  it "initalizes with two players" do
+    expect(testgame).to be_an_instance_of(RockPaperScissors)
+  end
+
+  describe "#new_game" do
+    it "instantiates a new game as a class variable and returns it with #instance" do
+      RockPaperScissors.new_game(player1, player2)
+      expect(RockPaperScissors.instance).to be_an_instance_of(RockPaperScissors)
+    end
+  end
+
+  describe "#play_round" do
+    it "identifies a draw and doesn't change anyone's score, but increments round counter" do
+      expect(testgame.play_round(:scissors, :scissors)).to eq(:draw)
+      expect(testgame.round).to eq(1)
+    end
+
+    it "indentifies a win and adds to player1 score" do
+      expect(player1).to receive(:score=).with(1)
+      expect(testgame.play_round(:paper, :rock)).to eq(:win)
+    end
+
+    it "identifies a loss and adds to player2 score" do
+      expect(player2).to receive(:score=).with(1)
+      expect(testgame.play_round(:rock, :paper)).to eq(:loss)
+    end
+  end
+end
