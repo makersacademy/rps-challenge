0631107ba61c818e191378ba9ba7ed9319347a3e
Lucy Stringer <lucyjstringer@gmail.com>
2020-11-14 18:18:16 +0000

    added basic random selection tool for computer

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,9 +1,17 @@
-feature 'logs player selection' do
+feature 'logs player & computer selection' do
   scenario 'Player receives confirmation of their selection' do
     sign_in_and_play
     choose('Rock')
     click_button "Submit"
     expect(page).to have_content 'You chose Rock!'
   end
-  
+
+  scenario 'Player receives confirmation of computer selection' do
+    srand(4)
+    sign_in_and_play
+    choose('Rock')
+    click_button "Submit"
+    expect(page).to have_content 'Computer chose Paper!'
+  end
+
 end
