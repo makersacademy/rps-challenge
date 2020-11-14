4c83a8f1ec4aa62410b13b2d75056d4739c087dc
Alex Withington-Smith <alex@awithsmith.com>
2020-11-14 06:26:48 +0000

    finialized Player class to my current class diagram spec

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,15 @@
 class Player
   attr_reader :name, :ai
-  attr_accessor :score
+  attr_accessor :score, :current_choice
 
   def initialize(name, ai = false)
     @name = name
     @score = 0
     @ai = ai
+    @current_choice = nil
+  end
+
+  def randomize_current_choice
+    @current_choice = RockPaperScissors::CHOICES.sample
   end
 end
--- spec/unit/player_spec.rb
+++ spec/unit/player_spec.rb
@@ -1,8 +1,9 @@
 require 'player'
 
 describe Player do
-  let(:example_player) { Player.new("Brian") }
+  let(:example_player) { Player.new("Brian", false) }
   let(:example_ai) { Player.new("Computer", true) }
+  let(:game_class) { double :game_class, CHOICES: [:rock, :paper, :scissors] }
 
   it "initialises with a name" do
     expect(example_player.name).to eq("Brian")
@@ -22,5 +23,29 @@ describe Player do
     end
   end
 
+  describe "current_choice" do
+    it "initializes as nil" do
+      expect(example_player.current_choice).to eq(nil)
+    end
+
+    it "can be changed" do
+      example_player.current_choice = :rock
+      expect(example_player.current_choice).to eq(:rock)
+    end
+  end
+
+  describe "randomize_current_choice" do
+    it "assigns current_choice a random selection of valid choices from the game class" do
+      choices_made = []
+      stub_const("RockPaperScissors::CHOICES", [:rock, :paper, :scissors])
+
+      while choices_made.uniq.length < 3
+        example_player.randomize_current_choice
+        choices_made << example_player.current_choice
+      end
+
+      expect(choices_made.uniq.length).to eq(3)
+    end
+  end
 
 end
