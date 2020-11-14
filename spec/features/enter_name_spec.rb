dfc49304a085097feb375a17de225b30247c8b71
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-14 16:20:28 +0000

    Added web helper method sign_in_and_play

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'enter name' do
   scenario "the user can submit their name" do
-    visit('/')
-    fill_in(:player_name, with: 'Human')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Human vs AI')
   end
 end
