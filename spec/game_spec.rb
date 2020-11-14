f9a79583a174570d670e048645e80cf1f6c756a2
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-14 13:49:21 +0000

    Added test for .instance to return Game made by .create

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,16 +1,25 @@
 describe Game do
   let(:player) { double('player', name: 'Human') }
-  let(:game) { Game.new(player) }
-
+  
   describe '#initialize' do
+    let(:game) { Game.new(player) }
+
     it 'takes in a Player object' do
       expect(game.player).to eq(player)
     end
   end
 
-  describe '#create' do
+  describe '.create' do
     it 'creates an instance of Game' do
       expect(Game.create(player)).to be_instance_of(Game)
+    end
+  end
+
+  describe '.instance' do
+    let(:created_game) { Game.create(player) }
+
+    it 'returns the instance of Game created by create' do
+      expect(Game.instance).to eq(created_game)
     end
   end
 end
