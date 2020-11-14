34cc59a9e2d8faa6143f25ac362dff69790994a6
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-14 11:32:57 +0000

    Initial commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require_relative '../app.rb'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
