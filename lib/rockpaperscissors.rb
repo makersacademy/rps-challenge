ce2d8092d822cf51d192bfa93b7e011dd7060523
Alex Withington-Smith <alex@awithsmith.com>
2020-11-14 17:05:04 +0000

    minor formatting changes, added rack to gemfile

--- lib/rockpaperscissors.rb
+++ lib/rockpaperscissors.rb
@@ -2,7 +2,7 @@ class RockPaperScissors
   WIN_CONDITION = 3
   CHOICES = [:rock, :paper, :scissors, :lizard, :spock]
   COMBINATION_MAP = {
-    :rock => { :rock => :draw, :paper => :loss, :scissors => :win, :lizard => :win, :spock => :loss},
+    :rock => { :rock => :draw, :paper => :loss, :scissors => :win, :lizard => :win, :spock => :loss },
     :paper => { :rock => :win, :paper => :draw, :scissors => :loss, :lizard => :loss, :spock => :win },
     :scissors => { :rock => :loss, :paper => :win, :scissors => :draw, :lizard => :win, :spock => :loss },
     :lizard => { :rock => :loss, :paper => :win, :scissors => :loss, :lizard => :draw, :spock => :win },
