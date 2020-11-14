d44f0f04f6f612b5650a5758b6ee9a133af06e22
Alex Withington-Smith <alex@awithsmith.com>
2020-11-14 14:32:12 +0000

    minor refactoring and styling changes

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require_relative './lib/player.rb'
 require_relative './lib/rockpaperscissors.rb'
 
 class Game < Sinatra::Base
+  before do
+    @game = RockPaperScissors.instance
+  end
   get '/' do
     erb(:index)
   end
@@ -23,20 +26,16 @@ class Game < Sinatra::Base
   end
 
   post "/single-player-choice" do
-    @game = RockPaperScissors.instance
     player_choice = params[:choice].to_sym
     computer_choice = RockPaperScissors::CHOICES.sample
     result = @game.play_round(player_choice, computer_choice)
-    redirect to("/round-results?result=#{result.to_s}&computer=#{computer_choice}&player=#{player_choice}")
+    redirect("/round-results?result=#{result}&computer=#{computer_choice}&player=#{player_choice}")
   end
 
-  get "/round-results" do
-    @game = RockPaperScissors.instance
-    result = params[:result]
-    
-    if result == 'win'
+  get "/round-results" do    
+    if params[:result] == 'win'
       @winner_name = @game.player1.name
-    elsif result == 'loss'
+    elsif params[:result] == 'loss'
       @winner_name = @game.player2.name
     else
       @winner_name = "no one, it's a draw"
@@ -46,17 +45,14 @@ class Game < Sinatra::Base
   end
 
   get '/play' do
-    @game = RockPaperScissors.instance
     erb(:play)
   end
 
   get '/next-round' do
-    @game = RockPaperScissors.instance
     @game.winner.nil? ? redirect('/play') : redirect('/final-results')
   end
 
   get '/final-results' do
-    @game = RockPaperScissors.instance
     erb(:final_results)
   end
 end
