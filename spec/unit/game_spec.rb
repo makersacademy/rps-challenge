643e59f50e1541d75672b48bd9c1e2ee4f2d7a0d
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-11-15 16:56:39 +0000

    Added rspec tests for playing a game

--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -14,4 +14,21 @@ describe Game do
     allow(game).to receive(:sample_move) { 'Scissors' }
     expect(game.single_match('Scissors')).to eq 'Draw!'
   end
+
+  it 'identifies a single player win' do
+    game.single_match('Scissors')
+    allow(game).to receive(:sample_move) { 'Paper' }
+    expect(game.single_match('Scissors')).to eq 'You won!'
+  end
+
+  it 'identifies a multi-player draw' do
+    allow(game.player_1).to receive(:name) { 'Alex' }
+    allow(game.player_2).to receive(:name) { 'Bill' }
+    expect(game.mp_match('Scissors', 'Scissors')).to eq 'Draw!'
+  end 
+
+  it 'identifies a multi-player win' do
+    allow(game.player_1).to receive(:name) { 'Alex' }
+    expect(game.mp_match('Scissors', 'Paper')).to eq 'Alex won!'
+  end 
 end
