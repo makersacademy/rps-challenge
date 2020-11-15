3a0371e0b833aee0fd6ff2796c80255704e32df9
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-11-15 09:58:39 +0000

    Added Lizard and Spock

--- lib/game.rb
+++ lib/game.rb
@@ -3,37 +3,31 @@ require_relative './bot'
 
 class Game
   attr_reader :player_1, :player_2, :computer_move
+
+  MOVES = { 'rock' => ['scissor', 'lizard'], 'paper' => ['rock', 'spock'],
+           'scissor' => ['paper', 'lizard'], 'lizard' => ['spock', 'paper'],
+           'spock' => ['rock', 'scissor'] }
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
   end
 
   def sample_move
-    %w[Rock Paper Scissors].sample
+    %w[Rock Paper Scissors Lizard Spock].sample
   end
 
   def single_match(move)
-    winning_moves = {
-      'rock' => 'scissors',
-      'scissors' => 'paper',
-      'paper' => 'rock'
-    }
     @computer_move = sample_move
     return 'Draw!' if move == computer_move
 
-    winning_moves[move] == computer_move ? 'You won!' : 'You lost!'
+    MOVES[move] == computer_move ? 'You won!' : 'You lost!'
   end
 
   def mp_match(p1, p2)
     player_1 = @player_1
     player_2 = @player_2
-    winning_moves = {
-      'rock' => 'scissors',
-      'scissors' => 'paper',
-      'paper' => 'rock'
-    }    
     return 'Draw!' if p1 == p2
 
-    winning_moves[p1] == p2 ? "#{player_1.name} won!" : "#{player_2.name} won!"
+    MOVES[p1] == p2 ? "#{player_1.name} won!" : "#{player_2.name} won!"
   end
 end
