ec25baf6cee7898364ec3f2604b23964602fd640
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-11-15 13:25:50 +0000

    More minor refactoring - removed unnecessary storing of moves in sessions

--- app.rb
+++ app.rb
@@ -25,16 +25,13 @@ class RPSApp < Sinatra::Base
   end
 
   post '/fight' do
-    session[:player_1_move] = params[:player_1_move]
-    @round = $game.single_match(session[:player_1_move])
+    @round = $game.single_match(params[:player_1_move])
     @computer_move = $game.computer_move
     erb :outcome
   end
 
   post '/fight_mp' do
-    session[:player_1_move] = params[:player_1_move]
-    session[:player_2_move] = params[:player_2_move]
-    @round = $game.mp_match(session[:player_1_move], session[:player_2_move])
+    @round = $game.mp_match(params[:player_1_move], params[:player_2_move])
     erb :outcomemp
   end
   run! if app_file == $0
