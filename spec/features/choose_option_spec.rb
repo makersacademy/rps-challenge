99c02127470a712dd7498f9fadbbc3124740776e
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-15 09:50:48 +0000

    Added feature test for cpu to choose random choice

--- spec/features/choose_option_spec.rb
+++ spec/features/choose_option_spec.rb
@@ -6,3 +6,12 @@ feature 'choose option' do
     expect(page).to have_content('You chose rock,')
   end
 end
+
+feature 'choose option' do
+  scenario 'computer randomly chooses rock, paper or scissors' do
+    sign_in_and_play
+    choose('choice', option: 'rock')
+    click_button('Submit')
+    expect(page).to have_content("AI chose #{%w[rock paper scissors].sample}")
+  end
+end
