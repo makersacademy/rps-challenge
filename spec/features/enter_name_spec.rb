2bc03e5d881085843cd8f1e41d32daf9c20b0bc7
Joshua Retallick <josh@old-empire.co.uk>
2020-11-15 11:42:12 +0000

    Updated with additional testing

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter name' do
   scenario 'submitting player name' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Hello Dave!'
   end
 end
