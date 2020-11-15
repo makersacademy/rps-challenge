f1c934e3d6d52cd7dc7a62c61bf1765451616575
Samantha <samanthagottlieb31@gmail.com>
2020-11-15 18:09:06 +0000

    Add sessions, add redirect from post to get request

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra/base'
 
 class RPS < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
+    session[:player_1] = params[:player_1]
+    redirect('play')
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
     erb(:play)
   end
 
