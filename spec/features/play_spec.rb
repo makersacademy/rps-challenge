15dfba4af958c36e42ad6d883bf3f54e31598c61
Margarida Martelo <margmartelo@gmail.com>
2020-11-15 21:16:58 +0000

    Fixed tests

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,16 +1,16 @@
 feature "playing" do
   scenario "presented with the different choices" do
     register_and_play
-    expect(page).to have_link("Rock", href: "/game?option=rock")
-    expect(page).to have_link("Paper", href: "/game?option=paper")
-    expect(page).to have_link("Scissors", href: "/game?option=scissors")
-    expect(page).to have_link("Spock", href: "/game?option=spock")
-    expect(page).to have_link("Lizard", href: "/game?option=lizard")
+    expect(page).to have_link(nil, href: "/game?option=rock")
+    expect(page).to have_link(nil, href: "/game?option=paper")
+    expect(page).to have_link(nil, href: "/game?option=scissors")
+    expect(page).to have_link(nil, href: "/game?option=spock")
+    expect(page).to have_link(nil, href: "/game?option=lizard")
   end
 
   scenario "player choose rock" do
     register_and_play
-    click_link('Rock')
+    click_link(nil, href: "/game?option=rock")
     expect(page).to have_content 'John chose rock'
   end
 end
