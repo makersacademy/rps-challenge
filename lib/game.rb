8ea76989b2cb1647a59ca1a97207b60388575857
Michael Schats <michael_schats@hotmail.com>
2020-11-16 09:27:55 +0000

    Final changes Made

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-attr_reader :player_1_name, :player_attack, :computer_move
+  attr_reader :player_1_name, :player_attack, :computer_move
 
   def initialize(player_1_name, player_attack)
     @player_1_name = player_1_name
@@ -8,7 +8,7 @@ attr_reader :player_1_name, :player_attack, :computer_move
   end
 
   def move
-    [ "Rock", "Paper", "Scissors"].sample
+    ["Rock", "Paper", "Scissors"].sample
   end
 
   def play
@@ -17,6 +17,7 @@ attr_reader :player_1_name, :player_attack, :computer_move
     return "You win!" if player_attack == "Rock" && computer_move == "Scissors"
     return "You win!" if player_attack == "Paper" && computer_move == "Rock"
     return "You win!" if player_attack == "Scissors" && computer_move == "Paper"
+
     "You lose!"
   end
 
