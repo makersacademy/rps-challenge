5e864ef9e9d930fcb4dbe97e93fb18acb3e4f21e
xavierloos <xavier.loos04@gmail.com>
2020-12-12 09:27:29 +0000

    Adding ROCK card

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,6 +1,11 @@
 feature "check cards" do
-  scenario "there is a 'rock' card" do
+  scenario "welcome to user" do
     signin
     expect(page).to have_content "Javi please choose your card"
+  end
+
+  scenario "there is a 'rock' card" do
+    signin
+    find_button("ROCK").click
   end
 end
