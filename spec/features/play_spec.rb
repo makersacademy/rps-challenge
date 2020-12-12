6accdcf01a887beae788a486cba53ca4bbffe3e4
AJ <aj.monty@gmail.com>
2020-12-12 19:16:03 +0000

    added /play and 3 submit buttons for rock, paper and scissors

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,6 +1,17 @@
 feature "play page" do
-  scenario "submit button goes to /play" do
+  scenario "has a rock option" do
     click_and_submit
     expect(page).to have_button 'Rock'
   end
+
+  scenario "has a paper option" do
+    click_and_submit
+    expect(page).to have_button 'Paper'
+  end
+
+  scenario "has a scissors option" do
+    click_and_submit
+    expect(page).to have_button 'Scissors'
+  end
+
 end
