ddb30216bd1a763d4e64594dae05deae90dcc2f8
xavierloos <xavier.loos04@gmail.com>
2020-12-13 21:59:37 +0000

    Updating

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,20 @@
-ENV["APP_ENV"] = "test"
+ENV["RACK_ENV"] = "test"
 
 require File.join(File.dirname(__FILE__), "..", "app.rb")
+
 require "capybara"
 require "capybara/rspec"
 require "rspec"
-require "features/web_helpers.rb"
+require "simplecov"
+require "simplecov-console"
+require "features/web_helpers"
+
 Capybara.app = RockPaperScissors
-
-Capybara.default_driver = :selenium
-
-Capybara.server = :webrick
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
 # Want a nice code coverage website? Uncomment this next line!
-# SimpleCov::Formatter::HTMLFormatter,
+# SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
