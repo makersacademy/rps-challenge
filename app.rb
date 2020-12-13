98354aa0a116874a12b5e0ce7b06962c9e1c76f6
Sarah Power <sarahjonipower0@gmail.com>
2020-12-13 20:18:51 +0000

    Player class created

--- app.rb
+++ app.rb
@@ -1,11 +1,23 @@
 require 'sinatra/base'
+require_relative './lib/player'
 
 class RPS < Sinatra::Base
+
+  enable :sessions
   
   get '/' do
     erb :index
   end
 
+  post '/names' do
+    $player = Player.new(params[:player_name])
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player = $player
+    erb :play 
+  end
+
   run! if app_file == $0
 end
- 
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,5 +1,5 @@
 
-feature "There is a homepage asking for a name" do
+feature "There is a homepage" do
     scenario 'Test if homepage is there' do
       visit '/'
       expect(page).to have_text("Welcome to Rock. Paper. SCISSORRSSS!")
