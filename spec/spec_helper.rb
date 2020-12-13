7b318609f194309f5cf092df1d1641ccdb8c1548
xavierloos <xavier.loos04@gmail.com>
2020-12-13 21:51:06 +0000

    Updating

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,12 +11,12 @@ Capybara.default_driver = :selenium
 
 Capybara.server = :webrick
 
-# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-# SimpleCov::Formatter::Console,
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
 # Want a nice code coverage website? Uncomment this next line!
 # SimpleCov::Formatter::HTMLFormatter,
-# ])
-# SimpleCov.start
+])
+SimpleCov.start
 
 # For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
 
