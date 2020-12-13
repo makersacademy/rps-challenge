5d4c89b2389cdd839f4c1009212d973d1845f4c2
Chris Whitehouse <chris.whitehouse@hotmail.com>
2020-12-13 19:52:49 +0000

    User Story 2 working

--- lib/game.rb
+++ lib/game.rb
@@ -22,6 +22,14 @@ class Game
     @rps_hash.keys[random_number]
   end
 
+  def declare_winner
+    if player_1.choice == player_2.choice then
+      "Draw"
+    else
+      @players[@rps_hash[player_1.choice][player_2.choice]].name + " Wins"
+    end
+  end
+
   private
 
   def random_number
@@ -29,41 +37,3 @@ class Game
   end
 
 end
-
-  # attr_reader :player, :computer
-  #
-  # def initialize(player_class = Player)
-  #   @player_class = player_class
-  #   @player_choice_hash = {}
-  #   @rps_array = ['Rock', 'Paper', 'Scissors']
-  # end
-  #
-  # def new_player(name)
-  #   @player = @player_class.new(name)
-  # end
-  #
-  # def add_player_choice(rps)
-  #   @player_choice_hash[@player] = rps
-  #   @player_choice_hash
-  # end
-  #
-  # def player_choice
-  #   @player_choice_hash[@player]
-  # end
-  #
-  # def new_computer_player
-  #   @computer = @player_class.new('Computer')
-  # end
-  #
-  # def add_computer_choice
-  #   @player_choice_hash[@computer] = random_computer_choice
-  # end
-  #
-  # def computer_choice
-  #   @player_choice_hash[@computer]
-  # end
-  #
-  # private
-  #
-  # def random_computer_choice
-  #   @rps_array.sample
--- spec/units/game_spec.rb
+++ spec/units/game_spec.rb
@@ -1,5 +1,5 @@
 describe Game do
-  let(:player_1_double) { double :player, name: "Player 1" }
+  let(:player_1_double) { double :player, name: "Player 1", choice: "Rock" }
   let(:player_2_double) { double :player, name: "Computer" }
   let(:game_class) { described_class }
   let(:game) { game_class.new(player_1_double, player_2_double) }
@@ -33,78 +33,27 @@ describe Game do
       expect(game.random_choice).to eq random_choice
     end
   end
-end
 
-  # let(:name) { "Player 1"}
-  # let(:player_double) { double :player, name: name }
-  # let(:player_class_double) { double :player_calss, new: player_double}
-  #
-  # let(:game_class) { described_class }
-  # let(:game) { game_class.new(player_class_double)}
-  #
-  # let(:rps) { "Rock" }
-  # let(:player_choice_hash) { {player_double => "Rock"} }
-  #
-  # let(:computer_choice) { "Scissors"}
-  #
-  # describe '#new_player' do
-  #   it 'should accept one name argument' do
-  #     expect(game).to respond_to(:new_player).with(1).argument
-  #   end
-  #
-  #   it 'should create instance of a new player' do
-  #     expect(game.new_player(name)).to eq player_double
-  #   end
-  # end
-  #
-  # describe '#player' do
-  #   it 'should return a player object' do
-  #     game.new_player(name)
-  #     expect(game.player).to eq player_double
-  #   end
-  # end
-  #
-  # describe '#add_player_choice' do
-  #   it 'should record the players choice' do
-  #     game.new_player(name)
-  #     expect(game.add_player_choice(rps)).to eq player_choice_hash
-  #   end
-  # end
-  #
-  # describe '#player_choice' do
-  #   it 'should return the player choice' do
-  #     game.new_player(name)
-  #     game.add_player_choice(rps)
-  #     expect(game.player_choice).to eq rps
-  #   end
-  # end
-  #
-  # describe '#new_computer_player' do
-  #   it 'should create and instance of a new player with name computer' do
-  #     expect(game.new_computer_player).to eq player_double
-  #   end
-  # end
-  #
-  # describe '#computer' do
-  #   it 'should should return a player object' do
-  #     game.new_computer_player
-  #     expect(game.computer).to eq player_double
-  #   end
-  # end
-  #
-  # describe '#add_computer_choice' do
-  #   it 'should record a random computer choice' do
-  #     game.new_computer_player
-  #     allow(game).to receive(:random_computer_choice).and_return(computer_choice)
-  #     expect(game.add_computer_choice).to eq computer_choice
-  #   end
-  # end
-  #
-  # describe '#computer_choice' do
-  #   it 'should return the computer choice' do
-  #     game.new_computer_player
-  #     allow(game).to receive(:random_computer_choice).and_return(computer_choice)
-  #     game.add_computer_choice
-  #     expect(game.computer_choice).to eq computer_choice
-  #   end
-  # end
+  describe ".declare_winner" do
+    context "Player 1: Rock vs Player 2: Scissors" do
+      let(:player_2_double) { double :player, name: "Computer", choice: "Scissors" }
+      it 'should return the name of the winning player' do
+        expect(game.declare_winner).to eq 'Player 1 Wins'
+      end
+    end
+
+    context "Player 1: Rock vs Computer: Paper" do
+      let(:player_2_double) { double :player, name: "Computer", choice: "Paper" }
+      it 'should return the name of the winning player' do
+        expect(game.declare_winner).to eq 'Computer Wins'
+      end
+    end
+
+    context "Player 1: Rock vs Computer: Rock" do
+      let(:player_2_double) { double :player, name: "Computer", choice: "Rock" }
+      it 'should return the name of the winning player' do
+        expect(game.declare_winner).to eq 'Draw'
+      end
+    end
+  end
+end
