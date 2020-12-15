740f278a42b297c12ca701b77354980351604f5b
Benedict Lawrence <BTLawrence44@gmail.com>
2020-12-15 13:52:48 +0000

    first commit

---
+++ game.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+
+class Game < Sinatra::Base 
+
+    set :session_secret, 'super secret'  
+
+    enable :sessions
+
+    get '/' do
+        
+        erb :index
+    end 
+
+    post '/name' do
+        session[:name] = params[:name] 
+
+        redirect '/play'
+    end  
+
+    get '/play' do
+        @name = session[:name] 
+        @mes = "here is a name #{ @name }" 
+        erb :game 
+    end    
+
+    post '/play_game' do
+        session[:choice] = params[:choice] 
+
+        redirect '/game_outcome'
+    end  
+
+    get '/game_outcome' do 
+        
+        value = session[:choice]    
+
+        @name = session[:name] 
+
+        computer = ["Rock", "Papaer", "Scissors"].shuffle.first 
+
+        if value == "Rock" && computer == "Scissors" ||  value == "Scissors" && computer == "Paper" || value == "Paper" && computer == "Rock"  
+            @meg = "#{ @name } winnes"
+        elsif computer == "Rock" && value != "Rock" ||  computer == "Scissors" && value == "Paper" || computer == "Paper" && value == "Rock"
+            @meg = "Computer winnes"
+        else  value == computer 
+            @meg = "Draw!"
+        end
+        erb :outcome
+    end
+end 
