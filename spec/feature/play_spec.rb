ea4d394a8c517425afd9478f189d688fc010d077
Sean Edwards <seantrevoredwards@gmail.com>
2021-01-21 20:42:34 +0000

    added rock paper scissors to play.erb to pass second feature test

--- spec/feature/play_spec.rb
+++ spec/feature/play_spec.rb
@@ -6,4 +6,10 @@ feature 'playing a game of RPS' do
       expect(page).to have_content("Paper")
       expect(page).to have_content("Scissors")
   end
+
+  scenario 'choose a move to play' do
+    sign_in_and_play
+    click_button 'Rock'
+    expect(page).to have_content("Rock")
+  end
 end
