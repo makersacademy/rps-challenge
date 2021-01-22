03e0339209d0058ba80ef5a3fc596639c2ec16aa
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-22 19:54:18 +0000

    corrected spelling on session (extra s by accident

--- app.rb
+++ app.rb
@@ -10,11 +10,12 @@ enable :sessions
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
-    redirect '/plays'
+    redirect '/play'
   end
 
   get '/play' do
-  @player_1_name = sessions[:player_1_name]
-  @player_2_name = sessions[:player_2_name]
-  erb(:play)
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
+  end
 end
