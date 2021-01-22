caf64929b2237412b9be3e4abcbd79c41bd8ccd7
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-22 19:50:41 +0000

    Created session to store user names between requests

--- app.rb
+++ app.rb
@@ -8,8 +8,13 @@ enable :sessions
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/plays'
   end
+
+  get '/play' do
+  @player_1_name = sessions[:player_1_name]
+  @player_2_name = sessions[:player_2_name]
+  erb(:play)
 end
