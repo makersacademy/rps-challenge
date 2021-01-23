d037cd9dde3f44ef99842875c80256b5db481eb1
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-23 10:41:56 +0000

    User can now see their name on the next page

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require_relative './lib/player.rb'
 
 class RPS < Sinatra::Base
   get '/' do
@@ -6,7 +7,14 @@ class RPS < Sinatra::Base
   end
 
   post '/name' do
+    $player = Player.new(params[:player_name])
+    redirect '/game_page'
+  end
 
+  get '/game_page' do
+    @name = $player.name
+    p @name
+    erb(:game_page)
   end
 
   run! if app_file == $0
--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
 
-  attr_accessor :name
+  attr_reader :name
 
   def initialize(name)
     @name = name
