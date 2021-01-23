b8088505e67e01ce6dbf5e0cd4d213b7bf8d22a2
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-23 13:41:35 +0000

    Created button for options and displays the option selected by the player

--- app.rb
+++ app.rb
@@ -14,7 +14,13 @@ class RPS < Sinatra::Base
 
   get '/play' do
     @player_1_name = session[:player_1_name]
+    @option = session[:option]
     erb :play
+  end
+
+  post '/play' do
+    session[:option] = params[:option]
+    redirect '/play'
   end
 
   run! if app_file == $0
--- spec/feature/play_spec.rb
+++ spec/feature/play_spec.rb
@@ -4,9 +4,17 @@ feature 'Select Rock, Paper or Scissor' do
     visit('/')
     fill_in(:player_1_name, with:'Oscar')
     click_button('Submit')
-    expect(page).to have_content 'Rock'
-    expect(page).to have_content 'Paper'
-    expect(page).to have_content 'Scissor'
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Paper'
+    expect(page).to have_button 'Scissor'
   end
-  
+
+  scenario 'choose one option' do
+    visit('/')
+    fill_in(:player_1_name, with:'Oscar')
+    click_button('Submit')
+    click_button('Rock')
+    expect(page).to have_content 'You have selected Rock'
+  end
+
 end
