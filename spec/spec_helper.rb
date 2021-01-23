67c0c3746dc72dcd621ae81aa3de06275033a2df
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-23 09:53:23 +0000

    Updated gemfile, because it didn't work which was great

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,12 @@
 ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
 require 'capybara/rspec'
-require 'simplecov'
-require 'simplecov-console'
+require 'rspec'
 
 Capybara.app = RPS
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
-
-# For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
 
 RSpec.configure do |config|
   config.after(:suite) do
