64a76545f5eeca824a8c12bf9e578090aaf07131
PetarSimonovic <petar@simonovic.com>
2021-01-23 16:19:41 +0000

    Characters can be selected via button press

--- app.rb
+++ app.rb
@@ -22,8 +22,27 @@ class RockPaperScissors < Sinatra::Base
     erb(:character_one_player)
   end
 
+  post '/selection_rock' do
+    session[:player_one_character] = params[:rock]
+    redirect '/game_one_player'
+  end
+
+  post '/selection_paper' do
+    session[:player_one_character] = params[:paper]
+    redirect '/game_one_player'
+  end
+
+  post '/selection_scissors' do
+    session[:player_one_character] = params[:scissors]
+    redirect '/game_one_player'
+  end
 
 
+  get '/game_one_player' do
+    @player_one_character = session[:player_one_character]
+    @player_one_name = session[:player_one_name]
+    erb(:game_one_player)
+  end
 
   run! if app_file == $0
 
--- spec/features/character_one_player_spec.rb
+++ spec/features/character_one_player_spec.rb
@@ -1,4 +1,5 @@
 feature "Character selection page" do
+  let(:player) {"Claude"}
 
   scenario "Greets player by name" do
     sign_in
@@ -13,11 +14,30 @@ feature "Character selection page" do
   scenario "displays Paper option" do
     sign_in
     expect(page).to have_button("paper")
-  end 
+  end
 
   scenario "displays Scissors option" do
     sign_in
     expect(page).to have_button("scissors")
+  end
+
+  scenario "paper button selects paper" do
+    sign_in
+    click_on("paper")
+    expect(page).to have_content("#{player} is Paper")
+
+  end
+
+  scenario "rock button selects rock" do
+    sign_in
+    click_on("rock")
+    expect(page).to have_content("#{player} is Rock")
+  end
+
+   scenario "scissors button selects scissors" do
+    sign_in
+    click_on("scissors")
+    expect(page).to have_content("#{player} is Scissors")
   end
 
 
