5ddc403d562f26b752d640bf34cd3ee67df2ccb1
twigz826 <thomastwigden@gmail.com>
2021-01-23 21:59:50 -0300

    Helper method referenced in enter_name_spec, tests pass

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter name' do
   scenario 'submitting the user name' do
-    visit('/')
-    fill_in :name, with: 'Sam'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Sam vs. Harry the bot'
   end
 end
