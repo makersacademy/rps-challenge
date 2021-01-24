246eb7fc6eed4dbd9bf1e77d83120742f66ea164
Katy Day <day.katy@gmail.com>
2021-01-24 18:23:33 +0000

    adding Computer class

--- lib/game.rb
+++ lib/game.rb
@@ -8,4 +8,19 @@ class Game
     @player = player
   end
 
+#need to have user enter a move, feed that move to the model
+#need to have a fake player 2 to give a countermove
+#need to have one of the moves beat the other move
+#need to feed that output to the views and controller
+#   beatmap = { 'scissors' => 'paper', 
+#                       'paper' => 'rock', 
+#                       'rock' => 'scissors' }
+
+# if p1 == p2
+#   puts "It's a draw"
+# elsif beatmap[p1] == p2
+#   puts "Player 1 wins"
+# else
+#   puts "Player 2 wins"
+# end
 end
--- spec/feature/move_spec.rb
+++ spec/feature/move_spec.rb
@@ -2,6 +2,6 @@ feature "entering a move" do
   scenario "player can input rock, paper, or scissors" do
     sign_in_and_play
     click_button"Into the fray!"
-    expect(page).to have_content
+    expect(page).to have_content "Katy has beaten the computer!"
   end
 end
