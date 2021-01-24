7433e45aa1c6226ccc03c1b942ebfac162544b06
twigz826 <thomastwigden@gmail.com>
2021-01-24 14:50:25 -0300

    All tests passing, built in capybara selector included

--- spec/features/bot_choice_spec.rb
+++ spec/features/bot_choice_spec.rb
@@ -2,9 +2,8 @@ feature 'Choose' do
   scenario 'the bot chooses Paper' do
     sign_in_and_play
     click_button("Paper")
-    #This is a built in capybara
-    message =
-
+    # This is a built in capybara selector that selects elements by CSS
+    message = find(:css, "#bot").text
     expect(all_bot_options).to include message
   end
 end
