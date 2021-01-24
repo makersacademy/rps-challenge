ebc07bc23ecd3e03169286663fc577a4afc14ae6
Katy Day <day.katy@gmail.com>
2021-01-24 17:25:33 +0000

    added to player.rb and player_spec

--- spec/unit/player_spec.rb
+++ spec/unit/player_spec.rb
@@ -2,4 +2,10 @@ require 'player'
 
 describe Player do
 
+  subject(:katy) { Player.new('Katy') }
+
+  it "should return an new instance of the player class with a name" do
+    expect(katy.name).to eq "Katy"
+  end
+
 end 
