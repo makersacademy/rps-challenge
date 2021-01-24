59d2f1b27cf59e6aba0e172633eb3b8a90284709
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-01-24 07:01:33 +0000

    Added functionality to enter player name along with associated feature test

--- app.rb
+++ app.rb
@@ -3,7 +3,12 @@ require 'sinatra/base'
 class RPS < Sinatra::Base
 
     get '/' do
-        'Hello!'
+        erb(:index)
+    end
+
+    post '/game' do
+        @player1 = params[:player1]
+        erb(:game)
     end
 
     run! if app_file == $0
--- spec/features/enter_players_spec.rb
+++ spec/features/enter_players_spec.rb
@@ -1,9 +1,8 @@
 feature 'enter player name' do
     scenario 'enter player name' do
         visit('/')
-        # fill_in('player_name', with: 'Boris')
-        # click_button "Let's Play!"
-        # expect(page).to have_content 'Welcome to your game Boris'
-        expect(page).to have_content 'Hello!'
+        fill_in('player1', with: 'Boris')
+        click_button "Let's Play!"
+        expect(page).to have_content 'Welcome to your game Boris'
     end
 end
