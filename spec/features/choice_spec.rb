58a062584f43ef9acce61b5bfe3c7f347dfc23c9
twigz826 <thomastwigden@gmail.com>
2021-01-24 09:48:40 -0300

    Changed feature test in choice_spec to reflect buttons instead of content

--- spec/features/choice_spec.rb
+++ spec/features/choice_spec.rb
@@ -1,9 +1,9 @@
 feature 'Choose' do
   scenario 'show all 3 options being rock, paper and scissors' do
     sign_in_and_play
-    expect(page).to have_content "Rock"
-    expect(page).to have_content "Paper"
-    expect(page).to have_content "Scissors"
+    expect(page).to have_button "Rock"
+    expect(page).to have_button "Paper"
+    expect(page).to have_button "Scissors"
   end
 
   scenario 'choose one of the options' do
