54ad7634f943ab3870d02fdc9d3d05014994b185
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-24 11:37:22 +0000

    Updated player spec and player to change choice from pick random

--- spec/unit/player_spec.rb
+++ spec/unit/player_spec.rb
@@ -29,13 +29,17 @@ describe Player do
 
   describe '#pick_random' do
     let(:options) { [paper, rock, scissors] }
-    it 'returns one of rock, paper or scissors' do
-      expect(options).to include subject.pick_random
+    it 'Sets choice to one of rock, paper or scissors' do
+      subject.pick_random
+      expect(options).to include subject.choice
     end
     context 'when run many times' do
       it 'returns rock, paper and scissors but nothing else' do
         results = []
-        100_000.times { results |= [subject.pick_random] }
+        100_000.times do
+          subject.pick_random
+          results |= [subject.choice]
+        end
         expect(results.sort).to eq options
       end
     end
