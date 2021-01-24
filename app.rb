d83def9dcae8d8384a67b0bf45dfb47491c6a934
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-24 09:04:51 +0000

    Tryint to change global variables into class instance variables

--- app.rb
+++ app.rb
@@ -3,20 +3,28 @@ require_relative './lib/player.rb'
 require_relative './lib/cpu.rb'
 
 class RPS < Sinatra::Base
+
+  before do
+    @game = Game.loader
+  end
+
   get '/' do
     erb(:index)
   end
 
   post '/name' do
-    $player = Player.new(params[:player_name])
+    Game.create(Game.new(Player.new(params[:name]), CPU.new))
     redirect '/game_page'
   end
 
   get '/game_page' do
-    $cpu = CPU.new
-    @name = $player.name
-    @cpu_name = $cpu.name
+    @name, @ai_name = @game.p1_name, @game.p2_name
     erb(:game_page)
+  end
+
+  post '/battle' do
+    @game.player1.choice = params[:pick]
+    redirect '/round_over'
   end
 
   run! if app_file == $0
--- spec/feature/choice_spec.rb
+++ spec/feature/choice_spec.rb
@@ -1,6 +1,13 @@
 feature 'choice' do
-  scenario 'Battle page should give the uesr the choice of: Rock, paper or scissors' do
+  scenario 'home page should give the uesr the choice of: Rock, paper or scissors' do
     sign_in_and_play
     expect(page).to have_content("ROCK") and have_content("PAPER") and have_content("SCISSORS")
+  end
+
+  scenario 'Can view the choice you made on the next page' do
+    sign_in_and_play
+    choose "PAPER"
+    click_button('I AM READY!')
+    expect(page).to have_content('You have chosen the power of PAPER!')
   end
 end
