9270c18e34a801b5c7bd17050cd9baf1df9f6e5d
twigz826 <thomastwigden@gmail.com>
2021-01-24 17:58:43 -0300

    Deleted bot_choice_spec and consolidated into choice_spec

--- spec/features/choice_spec.rb
+++ spec/features/choice_spec.rb
@@ -6,9 +6,25 @@ feature 'Choose' do
     expect(page).to have_button "Scissors"
   end
 
-  scenario 'choose one of the options' do
+  scenario 'user chooses one of the options' do
     sign_in_and_play
     click_button("Rock")
     expect(page).to have_content "You chose Rock!"
+  end
+
+  TEST_SEED_SCISSORS = 12
+  scenario 'the bot chooses Paper' do
+    sign_in_and_play
+    click_button("Paper")
+    # This is a built in capybara selector that selects elements by CSS
+    message = find(:css, "#bot").text
+    expect(all_bot_options).to include message
+  end
+
+  scenario 'the bot chooses a random option' do
+    sign_in_and_play
+    srand(TEST_SEED_SCISSORS)
+    click_button("Rock")
+    expect(page).to have_content "Bot chooses Scissors!"
   end
 end
