9e0b4c638a13cd5d77762fb2d1312b6a5280dd41
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-24 12:25:16 +0000

    Adding exhaustive tests for player 1/2 choices in game.result

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 require_relative 'player'
 
 class Game
-  attr_reader :player_1
+  attr_reader :player_1, :player_2
 
   def initialize(player_1, player_2)
     @player_1 = player_1
@@ -10,6 +10,35 @@ class Game
 
   def play(player_1_choice)
     @player_1.pick_specified player_1_choice
+    @player_2.pick_random
+  end
+
+  def result
+    return "It's a draw!" if draw?
+    p winner
+    "#{winner.name} wins!"
+  end
+
+  private
+
+  def draw?
+    @player_1.choice == @player_2.choice
+  end
+
+  def winner
+    p @player_1.choice, @player_2.choice
+    case @player_1.choice
+    when 'Rock'
+      return @player_1 if @player_2.choice == 'Scissors'
+      p 'getting to returning player 2'
+      @player_2
+    when 'Paper'
+      return @player_1 if @player_2.choice == 'Rock'
+      @player_2
+    when 'Scissors'
+      return @player_1 if @player_2.choice == 'Paper'
+      @player_2
+    end
   end
 
 end
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -2,17 +2,77 @@ require 'game'
 
 describe Game do
 
+  let(:player_2_name) { "Computer" }
   let(:player_1) { Player.new(player_name) }
-  let(:player_2) { Player.new("Computer") }
+  let(:player_2) { Player.new(player_2_name) }
   subject(:game) { described_class.new(player_1, player_2) }
+  let(:rock) { "Rock" }
+  let(:paper) { "Paper" }
+  let(:scissors) { "Scissors" }
 
   describe '#play' do
-    subject { game.play("Rock") }
+    subject { game.play(rock) }
     it 'changes player 1 choice' do
       expect { subject }.to change{ player_1.choice }
     end
-    # it { is_expected.to change { player_2.choice } }
-    # it {}
+    it 'changes player 2 choice' do
+      expect { subject }.to change { player_2.choice }
+    end
+  end
+
+  describe '#result' do
+    subject { game.result }
+    context 'when player 1 & 2 choose the same' do
+      before { allow(player_2).to receive(:choice).and_return rock }
+      it 'returns a draw' do
+        game.play(rock)
+        expect(subject).to eq "It's a draw!"
+      end
+    end
+    context 'when player 1 chooses rock' do
+      context 'when player 2 chooses scissors' do
+        it 'returns player 1 wins' do
+          allow(player_2).to receive(:choice).and_return scissors
+          game.play(rock)
+          expect(subject).to eq "#{player_name} wins!"
+        end
+      end
+      context 'when player 2 chooses paper' do
+        it 'returns player 2 wins' do
+          allow(player_2).to receive(:choice).and_return paper
+          game.play(rock)
+          expect(subject).to eq "#{player_2_name} wins!"
+        end
+      end
+    end
+    context 'when player 1 chooses paper' do
+      context 'when player 2 chooses scissors' do
+        it 'returns player 2 wins' do
+          allow(player_2).to receive(:choice).and_return scissors
+          game.play(paper)
+        end
+      end
+      context 'when player 2 chooses rock' do
+        it 'returns player 1 wins' do
+          allow(player_2).to receive(:choice).and_return rock
+          game.play(paper)
+        end
+      end
+    end
+    context 'when player 1 chooses scissors' do
+      context 'when player 2 choosers rock' do
+        it 'returns player 2 wins' do
+          allow(player_2).to receive(:choice).and_return rock
+          game.play(rock)
+        end
+      end
+      context 'when player 2 chooses paper' do
+        it 'returns player 1 wins' do
+          allow(player_2).to receive(:choice).and_return paper
+          game.play(rock)
+        end
+      end
+    end
   end
 
 
