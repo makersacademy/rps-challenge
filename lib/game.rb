e48131ae06ca20af99d867b5aa136385957a3b64
CodeRed30 <katrinaharradine@gmail.com>
2021-01-24 21:19:07 +0000

    reverted back to original conditional statement

--- lib/game.rb
+++ lib/game.rb
@@ -15,27 +15,36 @@ class Game
     @game
   end
 
-  # def win_lose_draw
-  #   @winner = :player if player.choice == "Rock" && opponent.choice == "Scissors"
-  #   @winner = :player if player.choice == "Paper" && opponent.choice == "Rock"
-  #   @winner = :player if player.choice == "Scissors" && opponent.choice == "Paper"
-  #   @winner = :opponent if player.choice == "Rock" && opponent.choice == "Paper"
-  #   @winner = :opponent if player.choice == "Paper" && opponent.choice == "Scissors"
-  #   @winner = :opponent if player.choice == "Scissors" && opponent.choice == "Rock"
-  #   @winner = :draw
-  # end
-
   def win_lose_draw
-    case player.choice
-    when "Rock" && opponent.choice == "Scissors" then @winner = :player
-    when "Rock" && opponent.choice == "Paper" then @winner = :opponent
-    when "Rock" && opponent.choice == "Rock" then @winner = :draw
-    when "Paper" && opponent.choice == "Rock" then @winner = :player
-    when "Paper" && opponent.choice == "Scissors" then @winner = :opponent
-    when "Paper" && opponent.choice == "Paper" then @winner = :draw
-    when "Scissors" && opponent.choice == "Paper" then @winner = :player
-    when "Scissors" && opponent.choice == "Rock" then @winner = :opponent
-    when "Scissors" && opponent.choice == "Scissors" then @winner = :draw
+    if player.choice == "Rock" && opponent.choice == "Scissors"
+      @winner = :player
+    elsif player.choice == "Paper" && opponent.choice == "Rock"
+      @winner = :player
+    elsif player.choice == "Scissors" && opponent.choice == "Paper"
+      @winner = :player
+    elsif player.choice == "Rock" && opponent.choice == "Paper"
+      @winner = :opponent
+    elsif player.choice == "Paper" && opponent.choice == "Scissors"
+      @winner = :opponent
+    elsif player.choice == "Scissors" && opponent.choice == "Rock"
+      @winner = :opponent
+    else
+      @winner = :draw
     end
   end
+
+  # def win_lose_draw
+  #   case player.choice
+  #   when "Rock" && opponent.choice == "Scissors" then @winner = :player
+  #   when "Rock" && opponent.choice == "Paper" then @winner = :opponent
+  #   when "Rock" && opponent.choice == "Rock" then @winner = :draw
+  #   when "Paper" && opponent.choice == "Rock" then @winner = :player
+  #   when "Paper" && opponent.choice == "Scissors" then @winner = :opponent
+  #   when "Paper" && opponent.choice == "Paper" then @winner = :draw
+  #   when "Scissors" && opponent.choice == "Paper" then @winner = :player
+  #   when "Scissors" && opponent.choice == "Rock" then @winner = :opponent
+  #   when "Scissors" && opponent.choice == "Scissors" then @winner = :draw
+  #   end
+  # end
+
 end
