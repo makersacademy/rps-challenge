f615dafca08dc239f2568cbae2d54bdcbe4a2a9d
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-25 08:37:38 +0000

    Passed Rubocop

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,10 +8,22 @@ describe Game do
       expect(oscar.name).to eq 'Oscar'
     end
   end
+
+  # describe '#option' do
+  #   it 'returns the option' do
+  #     expect(oscar.option).to eq :Rock
+  #   end
+  # end
+
+  # describe '#computer_option' do
+  #   it 'returns the computer option' do
+  #     expect(oscar.computer_option).to eq :Rock
+  #   end
+  # end
+
 end
 
   # describe '#win?' do
   #   it 'return true if player selected Rock and computer select Scissor'
   #     expect(subject.win?).to eq true
   # end
-
