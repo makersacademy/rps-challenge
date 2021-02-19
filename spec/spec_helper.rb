a2f3e69007f5802965025296abf9ee7e0ca0a243
Adam Jonas <adamjonas25@gmail.com>
2021-02-19 21:07:44 +0000

    comment simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,17 +5,17 @@ ENV['RACK_ENV'] = 'test'
 
 require_relative '../app'
 require 'capybara/rspec'
-require 'simplecov'
-require 'simplecov-console'
+# require 'simplecov'
+# require 'simplecov-console'
 
 Capybara.app = RockPaperScissors
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console
+#   # Want a nice code coverage website? Uncomment this next line!
+#   # SimpleCov::Formatter::HTMLFormatter
+# ])
+# SimpleCov.start
 
 # For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
 
