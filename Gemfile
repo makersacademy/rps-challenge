64d25dc1aad47f15ebcc6566ed1c47a1b8eae3b3
Adam Jonas <adamjonas25@gmail.com>
2021-02-19 19:32:28 +0000

    set up project structure

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'sinatra'
+group :development do
+  gem 'shotgun'
+  gem 'sinatra'
+end
 
 group :test do
   gem 'capybara'
--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
 class RockPaperScissors < Sinatra::Base
+  configure do
+    enable :sessions
+    set    :session_secret, ENV['SESSION_SECRET']
+  end
 
+  get '/' do
+
+  end
 end
