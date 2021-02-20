c8bf040e7990f044acfed5447630d4cd241010ab
Christine Chee <christinechee88@hotmail.com>
2021-02-20 17:07:45 +0000

    Modify welcome message

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,8 @@
+feature 'Welcomes the user' do
+  scenario "welcomes and asks for user's name to be shown later" do
+    visit ('/')
+    fill_in :name, with: 'Chrissy'
+    click_button 'Ready'
+    expect(page).to have_content 'Chrissy'
+  end
+end
