4938085441050974bef180040976645fc4d755a6
AdamP2000 <Adam2000Phillips@gmail.com>
2021-02-20 17:06:01 +0000

    Passing first test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,10 +7,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
-
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+Capybara.app = RPS
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
