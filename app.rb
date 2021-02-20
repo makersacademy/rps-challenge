6f053cf3e5f5e4c8b1f2d69737cb0571c75bd2b0
Aniruddh <ojhaa94@gmail.com>
2021-02-20 23:23:06 +0000

    reduces to one possible player

--- app.rb
+++ app.rb
@@ -10,12 +10,18 @@ class RoShamBo < Sinatra::Base
 
 	post '/names' do
 		session[:player_1_name] = params[:player_1_name]	
+		# session[:player_2_name] = params[:player_2_name]
 		redirect '/play'
 	end
 	
 	get '/play' do
 		@player_1_name = session[:player_1_name]
+		# @player_2_name = session[:player_2_name]
 		erb :play 
+	end
+
+	post '/move' do
+		
 	end
 		
 		
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb') # require our sinatra app file
 
 require 'capybara/rspec'
+require 'features/web_helpers'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
