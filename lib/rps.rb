9d48b03b1adb5cbe12ed22e9b34d060f215ca8fb
Christine Chee <christinechee88@hotmail.com>
2021-02-20 22:54:42 +0000

    Add rps result method

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,6 +1,12 @@
+require_relative 'computer'
+require_relative 'weapon'
+
 class RPS
-  def initialize(player)
+  def initialize(player, weapon_class = Weapon, computer = Computer.new)
     @player = player
+    @player_choice = nil
+    @weapon_class = weapon_class
+    @computer = computer
   end
 
   def self.create(player)
@@ -13,5 +19,13 @@ class RPS
 
   def player
     @player
+  end
+
+  def player_choose(choice)
+    @player_choice = @weapon_class.new(choice)
+  end
+
+  def result
+    @player_choice.draw?(@computer) ? :draw : @player_choice.beats?(@computer) ? :win : :lose
   end
 end
