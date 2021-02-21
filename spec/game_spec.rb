0e7005a9f53447f7362a7ae93952061e31ff10f2
olga <itsglimerry@gmail.com>
2021-02-21 19:22:40 +0000

    Added more tests to get tests coverage to 100%.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,13 +5,30 @@ describe Game do
 
   describe '#result' do
     it'returns Its a draw in case of the draw' do
+      player1 = double()
+      player2 = double()
+      game = Game.new(player1,player2)
+      allow(player1).to receive(:move) {'rock'}
+      allow(player2).to receive(:move) {'rock'}
+      expect(game.result).to eq 'Its a draw!'
+    end
+   it'returns correct value in case of the players1 victory' do
+     player1 = double()
+     player2 = double()
+     game = Game.new(player1,player2)
+     allow(player1).to receive(:move) {'paper'}
+     allow(player2).to receive(:move) {'rock'}
+     allow(player1).to receive(:name) {'Pug'}
+     expect(game.result).to eq "#{player1.name} won with #{player1.move}"
+   end
+  it'returns correct value in case of the players2 victory' do
     player1 = double()
     player2 = double()
     game = Game.new(player1,player2)
-    allow(player1).to receive(:move) {'rock'}
+    allow(player1).to receive(:move) {'scissors'}
     allow(player2).to receive(:move) {'rock'}
-    expect(game.result).to eq 'Its a draw!'
-   end
-
+    allow(player2).to receive(:name) {'Pug'}
+   expect(game.result).to eq "#{player2.name} won with #{player2.move}"
+  end
  end
 end
