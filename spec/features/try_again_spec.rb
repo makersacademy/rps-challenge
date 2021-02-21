19cee4aa346f14cad6e2978fbf45afa85a0f4223
Christine Chee <christinechee88@hotmail.com>
2021-02-21 13:50:48 +0000

    Add feature test for game result possibilities and try again button

---
+++ spec/features/try_again_spec.rb
@@ -0,0 +1,8 @@
+feature 'lets the player play the game again' do
+  scenario 'go back to choosing a move after game ends' do
+    sign_in_and_play
+    click_button 'Rock'
+    click_button 'Play again'
+    expect(page).to have_content "choose rock, paper or scissors?"
+  end
+end
