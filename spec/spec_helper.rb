15795c8729ff3e4f2a91a7d4137dd7768255fd38
olga <itsglimerry@gmail.com>
2021-02-22 08:48:20 +0000

    ..

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,7 @@ require 'simplecov-console'
 require 'simplecov'
 require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
+
 
 ENV['RACK_ENV'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
