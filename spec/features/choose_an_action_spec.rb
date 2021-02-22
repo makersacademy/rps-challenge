a24920468a923ac36095f2617c92eb461f46cfac
Nicolas Bermejo <nicbermejo@gmail.com>
2021-02-22 12:35:46 +0100

    added srand seeds to pass feature tests for bot choice

--- spec/features/choose_an_action_spec.rb
+++ spec/features/choose_an_action_spec.rb
@@ -15,8 +15,30 @@ feature 'after submiting you choice' do
   end
 
   scenario 'it shows the bot move' do
+    srand(4)
     sign_in_and_play
     make_a_choice
-    expect(page).to have_content "The Bot chose rock"
+    expect(page).to have_content "The Bot chose scissors"
+  end
+
+  scenario 'it shows Player winning' do
+    srand(4)
+    sign_in_and_play
+    make_a_choice
+    expect(page).to have_content "Rock smashes scissors, you win!"
+  end
+
+  scenario 'it shows Player drawing' do
+    srand(2)
+    sign_in_and_play
+    make_a_choice
+    expect(page).to have_content "You tied!"
+  end
+
+  scenario 'it shows Player losing' do
+    srand(1)
+    sign_in_and_play
+    make_a_choice
+    expect(page).to have_content "Paper covers rock, you lose!"
   end
 end
