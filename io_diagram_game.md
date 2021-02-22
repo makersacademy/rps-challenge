95ff97ea300a181f5fdd8bcf740a8259a65f4ca6
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-22 06:32:13 +0000

    Add calculate_winner method, computer_wins and you_win methods to Game class.

--- io_diagram_game.md
+++ io_diagram_game.md
@@ -3,9 +3,11 @@
 | scissors, scissors | draw |
 | rock, rock | draw |
 | paper, paper | draw |
+
 | scissors, rock | you lose |
 | rock, paper | you lose |
 | paper, scissors | you lose |
+
 | paper, rock | you win |
 | rock, scissors | you win |
 | scissors, paper | you win |
--- lib/game.rb
+++ lib/game.rb
@@ -6,11 +6,28 @@ class Game
   end
 
   def calculate_winner
-    if @player == @computer
+
+    case @player
+    when "rock"
+      you_win if @computer == "scissors"
+      computer_wins if @computer == "paper"
+    when "scissors"
+      you_win if @computer == "paper"
+      computer_wins if @computer == "rock"
+    when "paper"
+      you_win if @computer == "rock"
+      computer_wins if @computer == "scissors"
+    else
       "Computer chose #{@computer} – it's a draw!"
-    elsif @player == 'scissors' && @computer == 'rock'
-      "Computer chose #{@computer} – you lose!"
     end
+  end
+
+  def computer_wins
+    "Computer chose #{@computer} – you lose!"
+  end
+
+  def you_win
+    "Computer chose #{@computer} – you win!"
   end
 
 
