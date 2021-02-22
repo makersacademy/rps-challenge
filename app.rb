d7e1b01e42b741b452dc44125ca701de118ca0ff
Maximilian Clarke <maximilian-jc@live.com>
2021-02-22 10:24:10 +0000

    ani comments, created seperate game.rb file

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
+require_relative './lib/game.rb'
 
 class RockPaperScissors < Sinatra::Base
+  game = Game.new
   enable :sessions
 
   get '/' do
@@ -39,49 +41,8 @@ class RockPaperScissors < Sinatra::Base
      if @player2_move == ""
        @player2_move = ["rock", "paper", "scissors", "spock", "lizard"].sample
      end
-     if @player2_move == "rock" && @player_move == "paper"
-       @result = "You Win"
-     elsif @player2_move == "rock" && @player_move == "scissors"
-       @result = "You Lose"
-     elsif @player2_move == "rock" && @player_move == "lizard"
-       @result = "You Lose"
-     elsif @player2_move == "rock" && @player_move == "spock"
-       @result = "You Lose"
-     elsif @player2_move == "paper" && @player_move == "rock"
-       @result = "You Lose"
-     elsif @player2_move == "paper" && @player_move == "scissors"
-       @result = "You Win"
-     elsif @player2_move == "paper" && @player_move == "lizard"
-       @result = "You Win"
-     elsif @player2_move == "paper" && @player_move == "spock"
-       @result = "You Lose"
-     elsif @player2_move == "scissors" && @player_move == "rock"
-       @result = "You Win"
-     elsif @player2_move == "scissors" && @player_move == "paper"
-       @result = "You Lose"
-     elsif @player2_move == "scissors" && @player_move == "spock"
-       @result = "You Win"
-     elsif @player2_move == "scissors" && @player_move == "lizard"
-       @result = "You Lose"
-     elsif @player2_move == "spock" && @player_move == "rock"
-       @result = "You Lose"
-     elsif @player2_move == "spock" && @player_move == "paper"
-       @result = "You Win"
-     elsif @player2_move == "spock" && @player_move == "scissors"
-       @result = "You Lose"
-     elsif @player2_move == "spock" && @player_move == "lizard"
-       @result = "You Win"
-     elsif @player2_move == "lizard" && @player_move == "rock"
-       @result = "You Win"
-     elsif @player2_move == "lizard" && @player_move == "paper"
-       @result = "You Lose"
-     elsif @player2_move == "lizard" && @player_move == "spock"
-       @result = "You Lose"
-     elsif @player2_move == "lizard" && @player_move == "scissors"
-       @result = "You Win"
-     else
-       @result = "Draw"
-     end
+     game.moves(@player_move, @player2_move)
+     @result = game.result
      erb :result
    end
 end
