9a4b97393b1b8ee27722a307e071379490c6540b
Nicolas Bermejo <nicbermejo@gmail.com>
2021-02-22 10:06:09 +0100

    modified result testing for Rps class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,9 @@ require './app.rb'
 
 describe Rps do
   it 'shows the game results' do
-  @results = results("rock","rock")
+    player1 = instance_double('Player', name: 'Nico', move: 'rock' )
+    bot = instance_double('Bot', name: 'Bot', move: 'rock')
+  @results = results(player1.move,bot.move)
   expect(@results).to eq("You tied!")
   end
 end
