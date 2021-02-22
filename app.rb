dab3badc6ba7f0d1dab0bcf0e6d0ba7ef438dfca
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-02-22 08:16:13 +0000

    game not finished

--- app.rb
+++ app.rb
@@ -9,13 +9,17 @@ class RockPaperScissors < Sinatra::Base
 
   post '/weapons' do 
     @player = Player.new(params[:player1])
+    Game.create(player)    
     erb(:weapons)
   end 
 
   post '/game_over' do 
-    @weapon = params[:weapon]
+    weapon = params[:weapon]
+    Game.choose_weapon(Game.instance.player1, weapon)
+    Game.choose_weapon(Game.instance.player2)
     erb(:game_over)
   end 
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,12 @@
+require 'player'
+
 class Game
-  attr_reader :player1
+  attr_reader :player1, :player2
   WEAPONS = ["rock", "paper", "scissors"]
 
-  def initialize(player1)
+  def initialize(player1, player2 = Player.new("Enemy"))
     @player1 = player1
+    @player2 = player2
   end
 
   def self.create(player1)
@@ -14,19 +17,18 @@ class Game
     @game
   end
 
-  def self.choose_weapon(weapon)
-    @game.player1.equip(weapon)
+  def self.choose_weapon(player, weapon = WEAPONS.sample )
+    player.equip(weapon)
   end 
 
-  def self.duel
-    pc_weapon = WEAPONS.sample    
-    if @game.player1.weapon == pc_weapon
+  def self.duel  
+    if @game.player1.weapon == @game.player2.weapon
       :draw
-    elsif @game.player1.weapon == "rock" && pc_weapon == "scissors"
+    elsif @game.player1.weapon == "rock" && @game.player2.weapon == "scissors"
       :win
-    elsif @game.player1.weapon == "paper" && pc_weapon == "rock"
+    elsif @game.player1.weapon == "paper" && @game.player2.weapon == "rock"
       :win
-    elsif @game.player1.weapon == "scissors" && pc_weapon == "paper"
+    elsif @game.player1.weapon == "scissors" && @game.player2.weapon == "paper"
       :win
     else 
       :lost
