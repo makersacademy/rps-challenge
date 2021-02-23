f6980f6998c4b092d073408ea8b02b8a38786721
charlierdm <charlie.davismiller@gmail.com>
2021-02-23 21:09:00 +0000

    all game logic extracted into seperate class, test set up for Game class, 100% coverage

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,13 @@
 require 'game'
 
 describe Game do
-  
+  # let(:choice) { double :choice }
+  # let(:rps_comp) { double :rock }
+
+  it 'gives the result of rock paper scissors' do
+    expect(subject.letsPlayRPS('paper','rock')).to eq('You win!')
+    expect(subject.letsPlayRPS('scissor','rock')).to eq('You lose!')
+    expect(subject.letsPlayRPS('rock','rock')).to eq("It's a draw")
+  end
+
 end
