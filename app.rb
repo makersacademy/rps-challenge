64b414d99b8d1464a5b85d82f5e2ceab9d6efc97
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-20 12:38:13 +0000

    redefined routes to be more streamlined

--- app.rb
+++ app.rb
@@ -1,19 +1,23 @@
-require 'sinatra'
 require 'sinatra/base'
+require './lib/game'
 
 class RPS < Sinatra::Base
+
+  before do
+    @game = Game.current_game
+  end
 
   get '/' do
     erb :index
   end
 
-  get '/vscomputer' do
-    erb :vscomputer  
-  end
-
-  get '/vshuman' do
-    erb :vshuman
+  post '/names' do
+    @game = Game.new_game(params[:player1], params[:player2], 3)
+    erb :play  
   end
 
   run! if app_file == $0
 end
+
+
+# need tests for all the shiz inside index and stuff now
