4550bb9073a480026d3b4b43c4b63a93ebea3c40
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-03-20 13:44:27 +0000

    players specific confirmation message shows dependant on their choice

--- app.rb
+++ app.rb
@@ -20,11 +20,23 @@ class RockPaperScissors < Sinatra::Base
     erb :play
   end
 
+  post '/nextgame' do
+    redirect '/play'
+  end
+
   post '/result' do
+    if params[:choice] == 'Rock'
+        $player_choice = "Rock"
+    elsif params[:choice] == 'Paper'
+        $player_choice = "Paper"
+    elsif params[:choice] == 'Scissors'
+      $player_choice = "Scissors"
+    end
     redirect '/result'
   end
 
   get '/result' do
+    @player_choice = $player_choice
     erb :result
   end
 end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -11,7 +11,7 @@ describe RockPaperScissors do
   feature 'player can enter a name' do
     scenario 'the player can see their name on the play page' do
       sign_in_and_play
-      expect(page).to have_content "Sneaky Racoon"
+      expect(page).to have_content "Welcome Sneaky Racoon! Let's play Rock, Paper, Scissors!"
     end
   end
 
@@ -19,18 +19,25 @@ describe RockPaperScissors do
     scenario 'the player can select a button called rock and receive a confirmation' do
       sign_in_and_play
       click_button 'Rock'
+      expect(page).not_to have_content "You played Paper!"
+      expect(page).not_to have_content "You played Scissors!"
       expect(page).to have_content "You played Rock!"
+
     end
 
     scenario 'the player can select a button called rock and receive a confirmation' do
       sign_in_and_play
       click_button 'Paper'
+      expect(page).not_to have_content "You played Rock!"
+      expect(page).not_to have_content "You played Scissors!"
       expect(page).to have_content "You played Paper!"
     end
 
     scenario 'the player can select a button called rock and receive a confirmation' do
       sign_in_and_play
       click_button 'Scissors'
+      expect(page).not_to have_content "You played Rock!"
+      expect(page).not_to have_content "You played Paper!"
       expect(page).to have_content "You played Scissors!"
     end
   end
@@ -52,6 +59,15 @@ describe RockPaperScissors do
     sign_in_and_play
     click_button 'Scissors'
     expect(page).to have_content "Computer played Scissors!"
+    end
+  end
+
+  feature 'play next game' do
+    scenario 'player can select to play the next game' do
+    sign_in_and_play
+    click_button 'Scissors'
+    click_button 'Next Game'
+    expect(page).to have_content "Welcome Sneaky Racoon! Let's play Rock, Paper, Scissors!"
     end
   end
 end
