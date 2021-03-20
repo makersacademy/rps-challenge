837082e3749b14edf3bc854abc1fc17254c7d5de
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-20 12:39:02 +0000

    created player class

---
+++ lib/player.rb
@@ -0,0 +1,16 @@
+class Player
+
+  attr_reader :name, :score
+
+  def initialize(name)
+    @name = name
+    @score = 0
+  end
+
+  def wins
+    @score += 1
+  end
+end
+
+
+# add computer user stuff, if computer then this etc
