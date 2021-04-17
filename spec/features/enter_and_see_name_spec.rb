52b4ff0ca6576ffd9b8d4d5a2bee7db4a4696f0b
ian-1 <iandavidwalton@mail.com>
2021-04-17 15:54:27 +0100

    add web helper: sign in and play

--- spec/features/enter_and_see_name_spec.rb
+++ spec/features/enter_and_see_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'enter and see names' do
   scenario 'shows name after entering' do
-    visit '/'
-    fill_in 'name', with: 'User01'
-    click_button 'enter name and start game!'
+    sign_in_and_play
     expect(page).to have_content('User01')
   end
 end
