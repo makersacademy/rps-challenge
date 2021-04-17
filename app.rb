c182535b0158fb8c759f5116c6c3af5de3697e25
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-04-17 19:36:38 +0100

    implementation of class instance variables to save and load game

--- app.rb
+++ app.rb
@@ -1,23 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require './lib/player'
+require './lib/game'
 
 class Rps < Sinatra::Application
+  before { @game = Game.load_game }
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player = Player.new(params[:player_name])
-    @game = Game.new(@player)
-    $game = @game
+    player = Player.new(params[:player_name])
+    new_game = Game.new(player)
+    Game.save_game(new_game)
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
     @player_name = @game.player.name
     erb :play
   end
 
+  run! if app_file == $PROGRAM_NAME
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,17 @@
 class Game
   attr_reader :player
-  
+  @game = nil
+
   def initialize(player = Player.new)
     @player = player   
+    @game = nil
+  end
+
+  def self.save_game(new_game)
+    @game = new_game
+  end
+
+  def self.load_game
+    @game
   end
 end
