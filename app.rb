ca879204ea4cd648a4ec8d0702be02e0f4448f15
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-17 10:30:00 +0100

    player is able to sign in

--- app.rb
+++ app.rb
@@ -2,12 +2,24 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class RPS < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    'Hello RPS'
+    erb :index
+  end
+
+  post '/name' do
+    # @player_name = params[:player_name]
+    session[:player_name] = params[:player_name]
+    redirect :play
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
+    erb :play
   end
 
   run if app_file == $0
---
+++ spec/features/enter_name_spec.rb
@@ -0,0 +1,8 @@
+feature 'Enter name' do
+  scenario 'submit a name' do
+    visit('/')
+    fill_in :player_name, with: 'Yohann'
+    click_button 'Submit'
+    expect(page).to have_content 'Yohann entered the game'
+  end
+end
