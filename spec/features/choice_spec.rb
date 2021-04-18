2db8843f2366c9f636b3e154ffaa1cf549496627
Abdur Rehman <wabdulrehman@yahoo.com>
2021-04-18 23:26:41 +0100

    DRY refactor

--- spec/features/choice_spec.rb
+++ spec/features/choice_spec.rb
@@ -1,32 +1,29 @@
 feature 'choices' do
+  before do
+    fillin_and_submit
+    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
+  end  
   scenario 'shows the choice selected' do
     fillin_and_submit
     click_button('Rock')
     expect(page).to have_content('you chose Rock')
   end
   xscenario 'shows the choice of coputer' do
-    fillin_and_submit
-    allow_any_instance_of(Player).to receive(:sample)
-    allow_any_instance_of(Player).to receive(:choices).and_return("Scissors")
     click_button('Rock')
     expect(page).to have_content('Computer chose Scissors')
   end
   scenario 'takes back to play for' do
-    fillin_and_submit
     click_button('Rock')
     click_button('Play again')
     expect(page).to have_content('Welcome abdur')
   end
   scenario 'displays the winner' do
-    fillin_and_submit
-    allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
-    click_button('Paper')
+    
+    click_button('Rock')
     expect(page).to have_content('abdur wins!')
   end
   scenario 'displays a tie' do
-    fillin_and_submit
-    allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
-    click_button('Rock')
+    click_button('Scissors')
     expect(page).to have_content('it\'s a tie')
   end  
 end
