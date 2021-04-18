cc5beba0a06b57e36a62727a9f034f88cac5df94
Jessica Brand <jeshbrand@gmail.com>
2021-04-18 17:50:29 +0100

    RPS radio selection tests work - need more

---
+++ lib/random_response.rb
@@ -0,0 +1,1 @@
+# this page should handle the computer's response.
--- spec/features/choose_spec.rb
+++ spec/features/choose_spec.rb
@@ -1,8 +1,21 @@
 
 feature 'choose RPS' do
-  scenario 'solo player clicks buttons' do
+  scenario 'solo player clicks paper' do
     sign_in_and_play
-
-    # # expect(page).to have_content('Sally')
+    page.choose('paper')
+    click_button ('play')
+    expect(page).to have_content('you chose paper')
+  end
+  scenario 'solo player clicks scissors' do
+    sign_in_and_play
+    page.choose('scissors')
+    click_button ('play')
+    expect(page).to have_content('you chose scissors')
+  end
+  scenario 'solo player clicks rock' do
+    sign_in_and_play
+    page.choose('rock')
+    click_button ('play')
+    expect(page).to have_content('you chose rock')
   end
 end
