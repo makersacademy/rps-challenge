be210bc4c51c2c4766614428581ce12368d9d49b
fizzy-fifs <fkinyemi@gmail.com>
2021-04-18 15:06:18 +0100

    Added three buttons to the '/play' route: 'Rock', 'Paper' and 'Scissors'

---
+++ spec/features/play_spec.rb
@@ -0,0 +1,11 @@
+feature 'Rock, Paper, Scissors' do
+  scenario 'The player is presented with three options: rock, paper and scissors' do
+    sign_in_and_play
+    expect(page).to have_button("Rock")
+    expect(page).to have_button("Paper")
+    expect(page).to have_button("Scissors")
+  end
+
+  # scenario 'It has a paper button' do
+  #   sign_in_and_play
+end
