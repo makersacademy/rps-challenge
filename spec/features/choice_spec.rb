760805a899271f848340c81aa2d71d0a58216d20
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-18 12:15:43 +0100

    Buttons added

---
+++ spec/features/choice_spec.rb
@@ -0,0 +1,8 @@
+feature 'The player can choose either rock, paper or scissors' do
+  scenario 'The player has submitted their name' do
+    sign_in_and_play
+    expect(page).to have_button('Rock')
+    expect(page).to have_button('Paper')
+    expect(page).to have_button('Scissors')
+  end 
+end
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Enter player name' do
   scenario 'Marketeer registers their name before the game starts' do
-    visit('/')
-    fill_in :player_name, with: 'Sarah'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Welcome to Rock, Paper, Scissors Sarah!'
   end
 end
