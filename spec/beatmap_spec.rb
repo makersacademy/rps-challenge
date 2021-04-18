73ebef071ec101b4b6522b0611b7bc4cffc90548
YohannTisserand <tisserand.yohann@gmail.com>
2021-04-18 18:04:27 +0100

    allows the computer to select a weapon

--- spec/beatmap_spec.rb
+++ spec/beatmap_spec.rb
@@ -1,18 +1,18 @@
 require 'beatmap'
 
 describe Beatmap do
-  subject(:rock) { described_class.new('rock') }
+  subject(:beatmap) { described_class.new('rock') }
   let(:scissors) { double 'scissor', weapon: :scissors }
 
   it 'should store a weapon' do
-    expect(rock.weapon).to eq :rock
+    expect(beatmap.weapon).to eq :rock
   end
   
   it 'should return a winner' do
-    expect(rock.win?(scissors)).to eq true
+    expect(beatmap.win?(scissors)).to eq true
   end
 
   it 'should return a draw' do
-    expect(rock.draw?(rock)).to eq true
+    expect(beatmap.draw?(beatmap)).to eq true
   end
 end
