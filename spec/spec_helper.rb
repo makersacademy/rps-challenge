6bef69ad6bc4b0b069f438301ef5f56ed851aae4
Jessica Brand <jeshbrand@gmail.com>
2021-04-18 21:23:51 +0100

    included simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,14 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
 RSpec.configure do |config|
 
   config.expect_with :rspec do |expectations|
