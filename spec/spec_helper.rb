ff8febb86a9e3f9a5f7061b11b93d34dd253a51e
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-15 11:23:34 +0100

    write the first failing test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
-# ENV['RACK_ENV'] = 'test'
+ENV['RACK_ENV'] = 'test'
 
-# require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
-# require_relative './features/web_helpers'
-# require 'capybara'
+require_relative './features/web_helpers'
+require 'capybara'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
@@ -17,7 +17,7 @@ SimpleCov.start
 
 # For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
 
-# Capybara.app = RPS
+Capybara.app = RPS
 
 RSpec.configure do |config|
   config.after(:suite) do
