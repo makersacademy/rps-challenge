5a06483689cdb7750c632552c02ed1e18c8a7ee6
“Jonny <jonny.kelly192@gmail.com>
2021-05-15 15:38:20 +0100

    adds choice of move - Scissors

--- spec/features/move_choices_spec.rb
+++ spec/features/move_choices_spec.rb
@@ -11,5 +11,11 @@ feature 'view move choices' do
     click_button "Submit"
     expect(page).to have_selector(:link_or_button, 'Paper') 
   end
+  scenario "see paper" do 
+    visit('/')
+    fill_in :player_name, with: 'Toby'
+    click_button "Submit"
+    expect(page).to have_selector(:link_or_button, 'Scissors')
+  end
 
 end
