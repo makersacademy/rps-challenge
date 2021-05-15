463e7caca8ce5c7176590a6a8f8cf8fd0391ed98
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-15 15:32:39 +0100

    test drive game class

---
+++ lib/game.rb
@@ -0,0 +1,7 @@
+class Game
+  attr_reader :player1, :opponent
+  def initialize(player1, opponent)
+    @player1 = player1
+    @opponent = opponent
+  end 
+end 
---
+++ spec/units/game_spec.rb
@@ -0,0 +1,16 @@
+require 'game'
+
+describe Game do
+  subject(:game) { described_class.new(player1, opponent) }
+  let(:player1) { double :player1 }
+  let(:opponent) { double :opponent }
+
+  it 'returns the first player' do
+    expect(game.player1).to eq player1
+  end
+
+  it 'returns the second player' do
+    expect(game.opponent).to eq opponent
+  end
+
+end 
