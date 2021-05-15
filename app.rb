2abcdb1d3ae995a74992c459ac86cd3cb05b47fe
Charlie Slater <charliemslater1701@gmail.com>
2021-05-15 14:17:23 +0100

    refactored to use player class

--- app.rb
+++ app.rb
@@ -14,24 +14,23 @@ class App < Sinatra::Base
   end
   
   get '/play' do
-    @name = session[:name]
+    @player = session[:player]
     erb(:play)
   end
 
   get '/result' do
     @option = session[:option]
-    @name = session[:name]
-    @random_option = Player.new('Game').select_random
+    @player = session[:player]
+    @random_option = @player.select_random
     erb(:result)
   end
 
   post '/name' do
-    session[:name] = params[:name]
+    session[:player] = Player.new(params[:name])
     redirect('/play')
   end
 
   post '/select' do
-    # session[:option] = ""
     params.each_value { |v| session[:option] = v } 
     redirect('/result') 
   end
