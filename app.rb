72230d22212f6fa0e4016fa1f7846e1bcdf2b041
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-15 12:44:59 +0100

    first error with sessios

--- app.rb
+++ app.rb
@@ -21,6 +21,11 @@ class RPS < Sinatra::Base
     erb :play
   end
 
+  post '/choices' do
+    session[:choice] = params[:choice]
+    redirect '/play'
+  end 
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/game_play_spec.rb
+++ spec/features/game_play_spec.rb
@@ -8,5 +8,9 @@
     expect(page).to have_content 'Paper'
     expect(page).to have_content 'Scissors'
   end
-    
+  
+  scenario 'choose an option' do
+    click_button('Paper')
+    expect(page).to have_content 'Your choice is paper!'
+  end
 end
