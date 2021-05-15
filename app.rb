00bb7f1e494042824d19cc147880872f8662d50b
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-15 12:08:32 +0100

    make the first feature test pass

--- app.rb
+++ app.rb
@@ -11,6 +11,15 @@ class RPS < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  post '/name' do
+    session[:name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    erb :play
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
