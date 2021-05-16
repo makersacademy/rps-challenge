0d19bec159101cbebe813009f5bc021be0130d64
Arif Ebrahim <>
2021-05-16 14:45:15 +0100

    refactor game

--- lib/game.rb
+++ lib/game.rb
@@ -8,11 +8,9 @@ class Game
   end
 
   def declare_winner
-    if (@player_choice == "Rock" && @computer_choice == "Scissors") || 
-      (@player_choice == "Scissors" && @computer_choice == "Paper") ||
-      (@player_choice == "Paper" && @computer_choice == "Rock")
+    if player_wins_with_rock? || player_wins_with_paper? || player_wins_with_paper?
       "You win!"
-    elsif @player_choice == @computer_choice
+    elsif draw?
       "Draw!"
     else
       "You lose!"
@@ -25,6 +23,24 @@ class Game
 
   def self.instance
     @game
+  end
+
+  private
+
+  def player_wins_with_rock?
+    (@player_choice == "Rock" && @computer_choice == "Scissors")
+  end
+
+  def player_wins_with_scissors?
+    (@player_choice == "Scissors" && @computer_choice == "Paper")
+  end
+
+  def player_wins_with_paper?
+    (@player_choice == "Paper" && @computer_choice == "Rock")
+  end
+
+  def draw?
+    @player_choice == @computer_choice
   end
 
 end
