52356c62d2c300309ad069c0569a5e87d46edc55
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-16 12:45:07 +0100

    added post /rps-config route which stores name in session

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra/base'
 
 class RPSApp < Sinatra::Base
+  enable: sessions
+
   get '/' do
     erb(:registration)
+  end
+
+  post '/rps-config' do
+    session[:name] = params[:name]
+    redirect '/rps-game'
   end
 end
