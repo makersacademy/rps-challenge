b4941bb7c58a9d5d97ca2d8aa7682b448fe93783
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-16 14:31:38 +0100

    redesigned play to return outcome and opponent choice

--- spec/model/rps_game_spec.rb
+++ spec/model/rps_game_spec.rb
@@ -4,45 +4,12 @@ describe RPSGame do
   let(:game) { RPSGame.new() }
 
   describe '#play' do
-    it 'returns the outcome of the game' do
+    it 'returns the outcome of the game and the opponent\'s choice' do
       srand(23523)
 
       outcome = game.play('rock')
 
-      expect(outcome).to eq('win')
-    end
-  end
-
-
-  describe '#outcome' do
-    context 'when the user wins' do
-      it 'returns "win"' do
-        srand(23523)
-
-        game.play('rock')
-
-        expect(game.outcome).to eq('win')
-      end
-    end
-    
-    context 'when the user loses' do
-      it 'returns "loss"' do
-        srand(23604)
-
-        game.play('rock')
-
-        expect(game.outcome).to eq('loss')
-      end
-    end
-
-    context 'when the user draws' do
-      it 'returns "draw"' do
-        srand(23423)
-
-        game.play('rock')
-
-        expect(game.outcome).to eq('draw')
-      end
+      expect(outcome).to eq({outcome: 'win', opponent: 'scissors'})
     end
   end
 end
