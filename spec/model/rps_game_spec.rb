59e1e33aef73e26a79b5a0b9c15cc9406ffebbf0
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-16 15:54:28 +0100

    added tests for all possible outcomes

--- spec/model/rps_game_spec.rb
+++ spec/model/rps_game_spec.rb
@@ -4,12 +4,100 @@ describe RPSGame do
   let(:game) { RPSGame.new() }
 
   describe '#play' do
-    it 'returns the outcome of the game and the opponent\'s choice' do
-      srand(23_523)
+    context 'when given "rock"' do
+      context 'when the player wins' do
+        it "returns { outcome: 'win', opponent: 'scissors' }" do
+          srand(23_523)
 
-      outcome = game.play('rock')
+          outcome = game.play('rock')
 
-      expect(outcome).to eq({ outcome: 'win', opponent: 'scissors' })
+          expect(outcome).to eq({ outcome: 'win', opponent: 'scissors' })
+        end
+      end
+      
+      context 'when the player loses' do
+        it "returns { outcome: 'loss', opponent: 'paper' }" do
+          srand(23_470)
+
+          outcome = game.play('rock')
+
+          expect(outcome).to eq({ outcome: 'loss', opponent: 'paper' })
+        end
+      end
+
+      context 'when the player draws' do
+        it "returns { outcome: 'draw', opponent: 'rock' }" do
+          srand(23_423)
+
+          outcome = game.play('rock')
+
+          expect(outcome).to eq({ outcome: 'draw', opponent: 'rock' })
+        end
+      end
+    end
+
+    context 'when given "paper"' do
+      context 'when the player wins' do
+        it "returns { outcome: 'win', opponent: 'rock' }" do
+          srand(23_523)
+
+          outcome = game.play('paper')
+
+          expect(outcome).to eq({ outcome: 'win', opponent: 'rock' })
+        end
+      end
+      
+      context 'when the player loses' do
+        it "returns { outcome: 'loss', opponent: 'scissors' }" do
+          srand(23_470)
+
+          outcome = game.play('paper')
+
+          expect(outcome).to eq({ outcome: 'loss', opponent: 'scissors' })
+        end
+      end
+
+      context 'when the player draws' do
+        it "returns { outcome: 'draw', opponent: 'rock' }" do
+          srand(23_423)
+
+          outcome = game.play('paper')
+
+          expect(outcome).to eq({ outcome: 'draw', opponent: 'paper' })
+        end
+      end
+    end
+    
+    context 'when given "scissors"' do
+      context 'when the player wins' do
+        it "returns { outcome: 'win', opponent: 'paper' }" do
+          srand(23_523)
+
+          outcome = game.play('scissors')
+
+          expect(outcome).to eq({ outcome: 'win', opponent: 'paper' })
+        end
+      end
+
+      context 'when the player loses' do
+        it "returns { outcome: 'loss', opponent: 'rock' }" do
+          srand(23_470)
+
+          outcome = game.play('scissors')
+
+          expect(outcome).to eq({ outcome: 'loss', opponent: 'rock' })
+        end
+      end
+
+      context 'when the player draws' do
+        it "returns { outcome: 'draw', opponent: 'scissors' }" do
+          srand(23_423)
+
+          outcome = game.play('scissors')
+
+          expect(outcome).to eq({ outcome: 'draw', opponent: 'scissors' })
+        end
+      end
     end
   end
 end
