460e8282ad91f976dd76e6af871cc6d99b6bd93e
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-16 14:35:50 +0100

    changed feature spec for playing rps

--- spec/features/play_rps_spec.rb
+++ spec/features/play_rps_spec.rb
@@ -5,17 +5,14 @@ Capybara.app = RPSApp
 
 feature 'play RPS' do
   scenario 'user plays rock-paper-scissors' do
-    pending
     visit '/'
     fill_in('name', with: 'Alice')
     click_button('submit')
+    srand(23525)
 
     expect(page).to have_selector(:link_or_button, 'rock')
     expect(page).to have_selector(:link_or_button, 'paper')
     expect(page).to have_selector(:link_or_button, 'scissors')
-
-    allow(game).to receive_message(:outcome).and_return('win')
-    allow(game).to receive_message(:opponent).and_return('scissors')
 
     click_button('rock')
 
