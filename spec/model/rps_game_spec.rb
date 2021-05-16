09cc223597d2ca6e7ce54f207a1c42b695df137b
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-16 14:00:31 +0100

    added test for play method

--- spec/model/rps_game_spec.rb
+++ spec/model/rps_game_spec.rb
@@ -3,6 +3,17 @@ require 'rps_game'
 describe RPSGame do
   let(:game) { RPSGame.new() }
 
+  describe '#play' do
+    it 'returns the outcome of the game' do
+      srand(23523)
+
+      outcome = game.play('rock')
+
+      expect(outcome).to eq('win')
+    end
+  end
+
+
   describe '#outcome' do
     context 'when the user wins' do
       it 'returns "win"' do
