f52fc9c9cd8f57888dea98e7b76cd388203777fc
xavierloos <xavier.loos04@gmail.com>
2021-05-17 12:37:31 +0100

    Order

--- app.rb
+++ app.rb
@@ -10,13 +10,41 @@ class RockPaperScissors < Sinatra::Base
   get "/singleplayer" do
     erb :singleplayer
   end
-  get "/multiplayer" do
-    erb :multiplayer
-  end
-
   post "/name" do
     session[:name] = params[:name]
     redirect "/singlegame"
+  end
+
+  get "/singlegame" do
+    @name = session[:name]
+    erb :singlegame
+  end
+
+  post "/checkresult" do
+    function_singlegame(Game.new(session[:choice] = params[:choice]))
+    redirect "/result"
+  end
+  
+  get "/result" do
+    p "RESULT"
+    p session[:choice]
+    p @choice = session[:choice]
+    p "RESULT"
+    p @choice
+    erb :result
+  end
+
+  def function_singlegame(selection)
+    session[:choice] = selection.selected
+    # p "Your choice: #{@choice}"
+    @computer = selection.random
+    p "Computer choice: #{@computer}"
+    @result = selection.playing
+    p @result
+  end
+
+  get "/multiplayer" do
+    erb :multiplayer
   end
 
   post "/names" do
@@ -25,19 +53,9 @@ class RockPaperScissors < Sinatra::Base
     redirect "/multigame"
   end
 
-  get "/singlegame" do
-    @name = session[:name]
-    erb :singlegame
-  end
-
   get "/multigame" do
     @name_1 = session[:name_1]
     erb :player_1
-  end
-
-  post "/checkresult" do
-    function_singlegame(Game.new(session[:choice] = params[:choice]))
-    redirect "/result"
   end
 
   post "/player_1_turn" do
@@ -55,31 +73,12 @@ class RockPaperScissors < Sinatra::Base
     session[:choice_2] = params[:choice_2]
     # update the game with the 2 choices
     $multigame = MultiGame.new(session[:choice_1], session[:choice_2])
-    
     redirect "/results"
   end
 
   get "/results" do
     function_multigame
     erb :results
-  end
-
-  def function_singlegame(selection)
-    session[:choice] = selection.selected
-    # p "Your choice: #{@choice}"
-    @computer = selection.random
-    p "Computer choice: #{@computer}"
-    @result = selection.playing
-    p @result
-  end
-
-  get "/result" do
-    p "RESULT"
-    p session[:choice]
-    p @choice = session[:choice]
-    p "RESULT"
-    p @choice
-    erb :result
   end
 
   def function_multigame
