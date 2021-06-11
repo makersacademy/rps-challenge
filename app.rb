c26c5fd9df47583e6fce60dfb3ead60b3225ae4a
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-06-11 14:09:53 +0100

    adding model route for play

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/player'
+require './lib/rps'
 
 
 class RPS < Sinatra::Base
@@ -10,7 +12,32 @@ class RPS < Sinatra::Base
 
 
   get '/' do
-  ':)'
+    erb :enter_names
   end
+
+  post '/names' do
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    $rps_game = Rps.new(player_1, player_2)
+    redirect '/play'
+  end
+
+  get '/play' do
+    @rps_game = $game
+    erb :play
+  end
+
+  post '/play' do
+    @rps_game = $game
+    redirect '/result'
+  end
+
+  get '/result' do
+
+
+  end
+
+
+
 
 end
