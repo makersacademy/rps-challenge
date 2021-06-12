7ec250c2bc2efc1db218327c18af0b1c3e6c8c9b
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-06-12 17:51:39 +0200

    refactor - Turn class so that play controller only passes on instance variable

--- app.rb
+++ app.rb
@@ -14,19 +14,17 @@ class RPS < Sinatra::Base
   end
 
   post '/name' do
-    session[:name] = params[:name]
+    session[:player_name] = params[:name]
     redirect '/play'
   end
 
   get '/play' do
-    @name = session[:name]
-    @shape = session[:shape]
-    @opponent_move = session[:opponent_move]
+    @turn = Turn.new(session)
     erb :play
   end
 
   post '/play' do
-    session[:shape] = params[:shape]
+    session[:player_move] = params[:shape]
     session[:opponent_move] = :rock
     redirect '/play'
   end
