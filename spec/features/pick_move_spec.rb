dfda3190d9d9eb4ba6065a7a7afc32028dd19979
Cynthia Fu <yinnycfu@gmail.com>
2021-06-12 20:28:51 +0100

    can pick scissors

--- spec/features/pick_move_spec.rb
+++ spec/features/pick_move_spec.rb
@@ -6,13 +6,18 @@ feature 'Pick a move' do
   scenario 'pick rock' do
     click_button 'Rock!'
 
-    expect(page).to have_content 'You picked Rock.'
+    expect(page).to have_content 'You picked rock.'
   end
 
   scenario 'pick paper' do
     click_button 'Paper!'
 
-    expect(page).to have_content 'You picked Paper.'
+    expect(page).to have_content 'You picked paper.'
   end
 
+  scenario 'pick scissors' do
+    click_button 'Scissors!'
+
+    expect(page).to have_content 'You picked scissors.'
+  end
 end
