9d4376057d589f8bb963993169c0c513ecb0e0fb
Cynthia Fu <yinnycfu@gmail.com>
2021-06-12 21:54:04 +0100

    P1 rock, P2 scissors win

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,22 @@
 class Game
   def initialize(player_1, player_2)
-
+    @player_1 = player_1
+    @player_2 = player_2
   end
 
   def results
+    determine_moves
 
+    if @p1_move == "rock" && @p2_move == "scissors"
+      return "Win"
+    end
   end
-  
+
+  private
+
+  def determine_moves
+    @p1_move = @player_1.move
+    @p2_move = @player_2.move
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,5 +6,17 @@ describe Game do
   subject(:game) { described_class.new(player_1, player_2) }
 
   it { is_expected.to respond_to(:results) }
+  
+  context 'P1 picks rock' do
+    before do
+      allow(player_1).to receive(:move).and_return('rock')
+    end
+
+    it 'can win' do
+      allow(player_2).to receive(:move).and_return('scissors')
+
+      expect(subject.results).to eq 'Win'
+    end
+  end
 
 end
