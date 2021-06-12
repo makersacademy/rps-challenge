c7040c76df614111dbe82f82b9dca79455ebbf82
Cynthia Fu <yinnycfu@gmail.com>
2021-06-12 22:53:36 +0100

    refactoring Game class, SRP

--- lib/game.rb
+++ lib/game.rb
@@ -6,23 +6,9 @@ class Game
 
   def results
     determine_moves
-
-    return "Draw" if draw?
-
-    if @p1_move == "rock" && @p2_move == "scissors"
-      return "Win"
-    elsif @p1_move == "rock" && @p2_move == "paper"
-      return "Lose"
-    elsif @p1_move == "paper" && @p2_move == "rock"
-      return "Win"
-    elsif @p1_move == "paper" && @p2_move == "scissors"
-      return "Lose"
-    elsif @p1_move == "scissors" && @p2_move == "paper"
-      return "Win"
-    elsif @p1_move == "scissors" && @p2_move == "rock"
-      return "Lose"
-    end
-
+    return "Draw" if same_moves?
+    
+    return win_scenario? ? "Win" : "Lose"
   end
 
   private
@@ -32,8 +18,24 @@ class Game
     @p2_move = @player_2.move
   end
 
-  def draw?
+  def same_moves?
     @p1_move == @p2_move
+  end
+
+  def rock_and_scissors?
+    @p1_move == "rock" && @p2_move == "scissors"
+  end
+
+  def paper_and_rock?
+    @p1_move == "paper" && @p2_move == "rock"
+  end
+  
+  def scissors_and_paper?
+    @p1_move == "scissors" && @p2_move == "paper"
+  end
+
+  def win_scenario?
+    rock_and_scissors? || paper_and_rock? || scissors_and_paper?
   end
 
 end
