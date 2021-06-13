a5caa4b1cace4a3a11b314c71ff445041264ee82
Craig Eaton <craigea92@gmail.com>
2021-06-13 21:29:27 +0100

    Added web helpers and 2nd test

---
+++ spec/features/play_spec.rb
@@ -0,0 +1,8 @@
+feature ' playing a game' do
+  scenario 'see the options' do
+    sign_in_and_play
+    expect(page).to have_content 'Rock'
+    expect(page).to have_content 'Paper'
+    expect(page).to have_content 'Scissors'
+  end
+end
