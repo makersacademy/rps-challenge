ee6e98073b110da9826bfeb47112e43cc0152df3
Masako Gray <masakogray@hotmail.co.uk>
2021-06-13 23:01:31 +0100

    passing the index_spec test for entering name

--- app.rb
+++ app.rb
@@ -3,16 +3,17 @@ require 'sinatra/reloader'
 
 class RPS < Sinatra::Base
 
-configure :development do
-  register Sinatra::Reloader
-end 
+  configure :development do
+    register Sinatra::Reloader
+  end 
 
-get '/' do
-  erb(:index)
-end
+  get '/' do
+    erb(:index)
+  end
 
-post '/names' do
-  @player1 = params[:player1_name]
-end
+  post '/names' do
+    @player1_name = params[:player1_name]
+    erb(:play)
+  end
 
 end
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -2,7 +2,7 @@ feature 'Enter names' do
   scenario 'to submit names' do
     visit('/')
     fill_in :player1_name, with: 'Emily'
-    click_button 'Enter'
+    click_button 'Enter Game'
 
     expect(page).to have_content "Emily"
   end
