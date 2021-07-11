72fe0b19e43eec066d0c094f4741d26bb1bdc9ed
JooMin Choi <joominchoi23@gmail.com>
2021-07-11 14:41:50 +0100

    player can choose move

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,8 +1,6 @@
 feature 'Getting player name' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1, with: 'Omni-Man'
-    click_button "Let's play!"
+    sign_in_and_play
     expect(page).to have_content "Let's go, Omni-Man!"
   end
 end 
