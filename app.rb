7902799144cb3622fff751b0777c3423183b3f2d
Brett Karlson <brettjkarlson@gmail.com>
2021-07-16 22:00:04 +0100

    basic infrastructure set up

---
+++ app.rb
@@ -0,0 +1,15 @@
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class RPS < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Testing infrastructure working!'
+  end
+
+  run! if app_file == $0
+end
