5b2b30bdd388e3880232cc1e15d3b9d53c820446
Brett Karlson <brettjkarlson@gmail.com>
2021-07-17 12:45:48 +0100

    post redirect loop

--- app.rb
+++ app.rb
@@ -6,12 +6,20 @@ class RPS < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_name = params[:player_name]
+    p params
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_name = session[:player_name]
     erb :play
   end
 
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -3,6 +3,6 @@ RSpec.feature 'Enter name' do
     visit '/'
     fill_in :player_name, with: 'Denzel'
     click_button "Submit"
-    expect(page).to have_content 'Once again, a huge welcome to RPS, Denzel! You won\'t regret this'
+    expect(page).to have_content 'Once again, a huge welcome to RPS, Denzel! You definitely won\'t regret this ...'
   end
 end
