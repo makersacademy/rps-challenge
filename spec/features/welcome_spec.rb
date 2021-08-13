198dffbffb9070a46f58ca15d383679653f039e6
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-08-13 20:09:00 +0100

    Adding view files

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,6 +1,11 @@
-feature 'welcome page' do
-  scenario 'user visits the site for the first time' do
-    skip 'TODO'
+require 'spec_helper'
+
+feature 'visiting the site for the first time' do
+  describe 'welcome page' do
+    it 'shows the correct heading' do
+      visit('/')
+      expect(page).to have_content('Rock, Paper, Scissors!')
+    end
   end
 
   scenario 'user submits player names' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,18 @@
+require 'capybara'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+ENV['RACK_ENV'] = 'test'
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
-# For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+Capybara.app = RPS
 
 RSpec.configure do |config|
   config.after(:suite) do
