76340dc3cb5d656c1575ee69f06c1dc3fbabcd9f
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-14 17:11:38 +0100

    Added more unit tests for game.rb

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,4 +11,33 @@ describe Game do
     expect(game.player_1).to eq(player_1)
   end
 
+  describe ' #calculate' do
+
+    it 'allows a winner' do
+      allow(game).to receive(:winner) { player_1 }
+      expect(game.winner).to eq(player_1)
+    end
+
+    it 'allows a loser' do
+      allow(game).to receive(:winner) { player_1 }
+      expect(game.winner).to eq(player_1)
+    end
+
+    it 'is a tie if winner and loser are nil' do
+      expect(game.winner).to eq(nil)
+      expect(game.loser).to eq(nil)
+    end
+
+  end
+
+  describe ' #reset_winner_loser' do
+
+    it 'is expected to reset the winner loser values' do
+      allow(game).to receive(:winner) { player_1 }
+      allow(game).to receive(:loser) { player_2 }
+      expect(game.reset_winner_loser).to eq(nil)
+    end
+
+  end
+
 end
