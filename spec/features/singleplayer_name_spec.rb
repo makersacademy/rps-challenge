f21c7927fd802f91c8de2477fe874ec791516251
Willinlondon <williamjday11@gmail.com>
2021-08-14 12:18:09 +0100

    Fixed singleplayer_move problems and got Capybara passing by changing button name

--- spec/features/singleplayer_name_spec.rb
+++ spec/features/singleplayer_name_spec.rb
@@ -1,9 +1,8 @@
 feature 'single player name' do
   scenario 'checks that the game accepts a player name' do
-    visit("/")
-    click_button("Single Player")
+    visit("/singleplayer")
     fill_in("Player1", with: "Super Hans")
-    click_button("Let's go!")
+    click_button("Submit")
     expect(page).to have_content("Welcome Super Hans! Please select your move!")
   end
 end
