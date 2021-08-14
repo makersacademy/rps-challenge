6ee44aa57c92d2c190ba5ffed96948314fdf6066
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-08-14 20:21:51 +0100

    Adding images to move buttons

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 feature 'playing the game' do
   describe 'play page' do
-    it 'prompts the player to make a move' do
+    it 'prompts the player to make a move when they play for the first time' do
       visit_root_and_enter_name
 
-      expect(page).to have_content('Make your move!')
+      expect(page).to have_content('Make your move')
+    end
+
+    it 'prompts the player to play again if they have played before' do
+      visit_root_and_enter_name
+      enter_move('paper')
+
+      expect(page).to have_content('Play again')
     end
 
     it 'allows the player to enter a move' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,19 +12,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   SimpleCov::Formatter::HTMLFormatter
 ])
+
 SimpleCov.start
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
 Capybara.app = RPS
 
 RPS.configure do |app|
   app.use RackSessionAccess::Middleware
-end
-
-RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
 end
