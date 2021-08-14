3a1714baf3f63bd623cc13ce9da64c6df6306833
nicola <nicolaccarroll@gmail.com>
2021-08-14 19:50:01 +0100

    making a selection

--- app.rb
+++ app.rb
@@ -16,14 +16,25 @@ class RockPaperScissors < Sinatra::Base
   end
 
   post '/names' do
-    player_1 = Player.new(params[:player_1])
-    session[:player_1] = player_1.name
+    session[:player_1] = Player.new(params[:player_1])
     redirect '/play'
   end
 
   get '/play' do
     @player_1 = session[:player_1]
     erb(:play)
+  end
+
+  post '/make_move' do
+    p params
+    session[:rps_move] = params[:rps_move]
+    redirect '/winner'
+  end
+
+  get '/winner' do
+    @selection = session[:rps_move]
+    p @selection
+    erb(:winner)
   end
 
   run! if app_file == $0
