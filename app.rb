378b1e3b0e25fd11e61f0d6c130e4b3c6f55ce0b
Corinne <corinne.boesch@gmail.com>
2021-08-14 10:35:12 +0100

    infrastructure set up

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Game < Sinatra::Base
+
+  configure:development do 
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Lets play Rock Paper Scissors!'
+    # erb :index
+  end
+  run! if app_file == $0
+end
