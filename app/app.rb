559356fbb283b545c9887848c12101e0df90246c
Radu Necsanu <necsanu_radu@yahoo.com>
2021-08-15 20:05:45 +0100

    all feature test done going for the unit ones

--- app/app.rb
+++ app/app.rb
@@ -13,8 +13,8 @@ class RPS < Sinatra::Base
   end
 
   post "/game" do
-    @user = params[:name].downcase.split(" ").map(&:capitalize).join(" ")
-    session[:name] = @user
+    @name = params[:name].downcase.split(" ").map(&:capitalize).join(" ")
+    session[:name] = @name
     session[:wins] = 0
     session[:total] = 0
     @wins = 0 # session[:wins]
@@ -24,24 +24,32 @@ class RPS < Sinatra::Base
 
   get "/game" do 
     session[:total] = session[:total].to_i + 1
-    @user = session[:name]
+    @name = session[:name]
     @wins = session[:wins]
     @total = session[:total]
     erb(:game)
   end
 
   post "/results" do
-    @winner = "Radu"
+    @winner = session[:name]
     @winner_act = "rock"
     @loser_act = "scissors"
-    redirect("/results?winner=" << @winner << "&wact=" << @winner_act << "&lact=" << @loser_act)
+    @name = session[:name]
+      redirect("/results_lost?wact=" << @winner_act << "&lact=" << @loser_act)
   end
 
-  get "/results" do
-    @winner = params[:winner]
-    @winner_act = params[:wact]
-    @loser_act = params[:lact]
-    erb(:results)
+  get "/results_won" do
+    params[:name].nil? ? (@name = session[:name]) : (@name = params[:name])
+    @first_act = params[:wact]
+    @second_act = params[:lact]
+    erb(:results_won)
+  end
+
+  get "/results_lost" do
+    params[:name].nil? ? (@name = session[:name]) : (@name = params[:name])
+    @first_act = params[:wact]
+    @second_act = params[:lact]
+    erb(:results_lost)
   end
 
   run! if app_file ==$0
--- spec/features/results_spec.rb
+++ spec/features/results_spec.rb
@@ -1,7 +1,38 @@
 
 feature "it should present a winner" do
-  scenario "on landing on the page it should present first aplyer as the winner" do
-    visit("/results?winner=Mike")
-    expect(page).to have_content("Well done\nMike!\nbrilliant action")
+
+  scenario "on landing on the page it should present Mike as the winner" do
+    visit("/results_won?name=Mike")
+    expect(page).to have_content("You won\nMike!\nbrilliant action")
   end
+
+  scenario "on landing on the page it should present the Mike as the loser" do 
+    visit("/results_lost?name=Mike")
+    expect(page).to have_content("You lost\nMike..\nsoft action")
+  end
+
+  scenario "you won" do
+   visit("results_won?wact=paper&lact=rock")
+   expect(page).to have_content("against poor\nrock")
+   expect(page).to have_content("you had:\npaper")
+  end
+
+  scenario "you lost" do
+    visit("results_lost?wact=paper&lact=rock")
+    expect(page).to have_content("against wicked\nrock")
+    expect(page).to have_content("you had:\npaper")
+  end
+
+  scenario "navigate back to game from results where you won" do
+    visit("/results_won")
+    click_link("Play again")
+    expect(page).to have_content("What is your weapon of choice?")
+  end
+
+  scenario "navigate back to game from results where you lost" do
+    visit("/results_lost")
+    click_link("Play again")
+    expect(page).to have_content("What is your weapon of choice?")
+  end
+
 end
