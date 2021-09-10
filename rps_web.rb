7119682e95df1b0e81816fb8d9d4e63195952cdf
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-10 10:42:00 +0100

    refactor - post/redirect/get

--- rps_web.rb
+++ rps_web.rb
@@ -5,13 +5,19 @@ class RockPaperScissors < Sinatra::Base
     configure :development do
       register Sinatra::Reloader
     end
+    enable :sessions
   
     get '/' do
       erb :index
     end
 
-    post '/play' do 
-      @player_name = params[:player_name]
+    post '/name' do 
+      session[:player_name] = params[:player_name]
+      redirect '/play'
+    end
+
+    get '/play' do
+      @player_name = session[:player_name]
       erb :play
     end
   
