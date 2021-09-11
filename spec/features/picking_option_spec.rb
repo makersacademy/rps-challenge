3a15df1073174487276ff641ea8c754610fcc813
Adrienne Galloway <agalloway@Adriennes-MacBook-Air.local>
2021-09-11 12:49:29 +0100

    Adding the picking option test

---
+++ spec/features/picking_option_spec.rb
@@ -0,0 +1,17 @@
+require 'capybara/rspec'
+require_relative '../../app'
+
+# As a marketer 
+# So that I can choose an option
+# I will be presented the choices (rock, paper and scissors)
+
+# feature "Picking an option" do 
+#     scenario "marketer can select either rock, paper or scissors" do 
+#       visit ('/')
+#       fill_in :marketer_name_1, with: 'Adrienne'
+#       click_button 'Submit'
+#       click_button 'Rock'
+#       expect(page).to have_content("Adrienne selected rock")
+#     end
+#   end 
+  
