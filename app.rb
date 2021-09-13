25124a6bc8c0765175e92b9076fa315991eb2e5d
Leah <leahpitter95@gmail.com>
2021-09-13 02:36:06 +0100

    refactored code to fit RPS model. added a play.erb file to show welcome message with player name

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+class Rock_paper_scissors <Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
 
-post '/' do
-  p params
-  @name = params[:name]
-  erb (:index)
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    'hello'
+  end
+
+  post '/names' do
+    @player_name = params[:player_name]
+    erb :play
+  end
+
+run! if app_file == $0
 end
+
