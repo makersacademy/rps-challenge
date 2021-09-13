a3bcb59853582965a2df4a3c3ee13b8fcdc2cf2c
Tom Lee <tomalee2002@gmail.com>
2021-09-13 09:50:02 +0100

    adds test for being able to win game

--- spec/features/main_game_spec.rb
+++ spec/features/main_game_spec.rb
@@ -1,9 +1,19 @@
 describe 'able to play RPS' do
-  let(:computer_choice) { 'rock' }
+  let(:computer_choice) { 'scissors' }
 
   scenario 'can make a choice' do
     get_to_game
     fill_in :choice, with: 'rock'
-    expect(page).to have_content "rock VS #{computer_choice}"
+    click_button 'Submit'
+    expect(page).to have_content "rock"
+  end
+
+  scenario 'can win game' do
+    get_to_game
+    fill_in :choice, with: 'rock'
+    game = Game.new('rock', 'Bob')
+    @cpu_choice = :computer_choice
+    expect(game.results).to eq 'Bob wins!'
+    save_and_open_page
   end
 end
