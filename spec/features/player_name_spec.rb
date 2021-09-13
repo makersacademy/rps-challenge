a7d96049f5edd0c4a5ca31e11b62d1ab8a3da70b
LauraMaddaluno <lauralondon.4@gmail.com>
2021-09-13 11:34:31 +0100

    correct misspelling and add the future tast for enteing the user name

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,6 +1,6 @@
-# feature 'Enter name' do
-#   scenario 'submitting name' do
-#     sign_in_and_play
-#     expect(page).to have_content "Please Laura make your choise"
-#   end
-# end 
+feature 'Enter name' do
+  scenario 'submitting name' do
+    sign_in_and_play
+    expect(page).to have_content "Please Laura make your choice"
+  end
+end 
