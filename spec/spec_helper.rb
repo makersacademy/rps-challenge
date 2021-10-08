e712a77a0340f59e0408c3f98f765825d1e4fdb1
Max Fry <maxcfry@outlook.com>
2021-10-08 19:02:09 +0100

    User can enter name before starting game

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,39 @@
-require 'capybara/rspec'
-require 'simplecov'
-require 'simplecov-console'
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
-
-# For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
-
 RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+=begin
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+  config.warnings = true
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+  config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
+=end
 end
+
+# Set the environment to "test"
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# Require all the testing gems
+require 'rspec'
+require 'capybara'
+require 'capybara/rspec'
+require 'rack'
+
+# Tell Capybara to talk to BookmarkManager
+Capybara.app = BookmarkManager
