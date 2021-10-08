1f29cf78fe5b21fc437d295ed422b6734a3f499b
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-10-08 20:39:21 +0100

    enable player to enter and register its name

--- app.rb
+++ app.rb
@@ -2,12 +2,23 @@ require "sinatra/base"
 require "sinatra/reloader"
 
 class RockPaperScissor < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
 
   get "/" do
     erb :index
+  end
+
+  post "/names" do
+    session[:player_name] = params[:player_name]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player_name = session[:player_name]
+    erb :play
   end
 
   run! if app_file == $0
