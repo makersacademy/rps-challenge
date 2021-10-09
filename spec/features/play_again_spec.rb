bc59bb8144f507b6b4a758ff53251134752ab7d8
Jacob Cho <jacobcho@live.com>
2021-10-10 00:21:58 +0100

    rps changed to 3 buttons, not a text form

--- spec/features/play_again_spec.rb
+++ spec/features/play_again_spec.rb
@@ -2,8 +2,7 @@ feature 'check play again option' do
 
   scenario 'Play again after 1 round' do
     sign_in_and_play
-    fill_in :player_1_choice, with: 'rock'
-    click_button 'Choice'
+    click_button 'rock'
     click_button 'Play again'
     expect(page).not_to have_content 'chose'
     expect(page).to have_content 'Choose rock, paper or scissors!'
--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -2,8 +2,7 @@ feature 'check results' do
 
   scenario 'P1 chooses rock' do
     sign_in_and_play
-    fill_in :player_1_choice, with: 'rock'
-    click_button 'Choice'
+    click_button 'rock'
     expect(page).not_to have_content 'You chose: scissors'
     expect(page).to have_content 'You chose: rock'
   end
