95301c549df9a08ec975fb23a42595db6d33227f
Lisa Mac <lisa@MacBook-Air-2.lan>
2021-10-09 16:03:09 +0100

    Continued setup

--- spec/smokescreen_spec.rb
+++ spec/smokescreen_spec.rb
@@ -1,6 +1,6 @@
 feature 'visiting the homepage' do
-  scenario 'the basic smoke test passes' do
+  scenario 'Creating the home page route' do
     visit '/'
-    expect(page).to have_content "Hello World"
+    expect(page).to have_content "The beginning of Rock, Papers and Scissors"
   end
 end
