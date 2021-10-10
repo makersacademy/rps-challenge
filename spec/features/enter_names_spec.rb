f7c66c39741649d3b9519883b53e67146101459b
Jacob Cho <jacobcho@live.com>
2021-10-10 02:45:34 +0100

    tests refactored for new structure in app.rb

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,7 +2,7 @@ feature 'Enter names' do
 
   scenario 'Submitting names' do
     sign_in_and_play
-    expect(page).to have_content 'Timmy vs. RPS Computer'
+    expect(page).to have_content 'Timmy'
   end
   
 end
