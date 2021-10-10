b3219002057c74b7640ec74af797767fcae5fc1a
ConorButler <cwdbutler@gmail.com>
2021-10-10 12:21:27 +0100

    First user story

--- app.rb
+++ app.rb
@@ -1,12 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require './lib/game'
+require './lib/player'
 
 class RPS < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
-  get '/' do
-    "Rock, paper, scissors"
+  before do
+    @game = Game.game
   end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    player1 = Player.new(params[:name1])
+    player2 = Player.new(params[:name2])
+    @game = Game.start(player1, player2)
+    redirect '/play'
+  end
+
+  get '/play' do
+    erb :play
+  end
+
+  run! if app_file == $0
 end
---
+++ lib/game.rb
@@ -0,0 +1,21 @@
+class Game
+  def initialize(player1, player2)
+    @players = [player1, player2]
+  end
+
+  def self.start(player1, player2)
+    @game = Game.new(player1, player2)
+  end
+
+  def self.game
+    @game
+  end
+
+  def player1
+    @players.first
+  end
+
+  def player2
+    @players.last
+  end
+end
