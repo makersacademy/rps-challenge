e910ada6caaf719b027de2abe19481f004127c1d
AJOsmaston <dajoxx@gmail.com>
2021-10-10 18:12:23 +0100

    README updated

--- spec/features/classic_spec.rb
+++ spec/features/classic_spec.rb
@@ -1,25 +1,27 @@
 feature "ClassicMode" do
-  scenario "selects Rock" do
-    start_classic_game
-    click_button "Rock"
-    expect(page).to have_content "You picked Rock"
-    expect(page).not_to have_content "You picked Paper"
-    expect(page).not_to have_content "You picked Scissors"
-  end
+  feature "Selections" do
+    scenario "selects Rock" do
+      start_classic_game
+      click_button "Rock"
+      expect(page).to have_content "You picked Rock"
+      expect(page).not_to have_content "You picked Paper"
+      expect(page).not_to have_content "You picked Scissors"
+    end
 
-  scenario "selects Paper" do
-    start_classic_game
-    click_button "Paper"
-    expect(page).not_to have_content "You picked R"
-    expect(page).to have_content "You picked P"
-    expect(page).not_to have_content "You picked S"
-  end
+    scenario "selects Paper" do
+      start_classic_game
+      click_button "Paper"
+      expect(page).not_to have_content "You picked Rock"
+      expect(page).to have_content "You picked Paper"
+      expect(page).not_to have_content "You picked Scissors"
+    end
 
-  scenario "selects Scissors" do
-    start_classic_game
-    click_button "Scissors"
-    expect(page).not_to have_content "You picked R"
-    expect(page).not_to have_content "You picked P"
-    expect(page).to have_content "You picked S"
+    scenario "selects Scissors" do
+      start_classic_game
+      click_button "Scissors"
+      expect(page).not_to have_content "You picked Rock"
+      expect(page).not_to have_content "You picked Paper"
+      expect(page).to have_content "You picked Scissors"
+    end
   end
 end
