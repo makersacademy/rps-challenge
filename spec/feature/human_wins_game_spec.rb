6b67b18849c06975496561dc8f6a453b7373c467
Tom <t.p.kellett@gmail.com>
2021-10-10 17:59:40 +0100

    adding comment to feature test

--- spec/feature/human_wins_game_spec.rb
+++ spec/feature/human_wins_game_spec.rb
@@ -3,7 +3,7 @@
     # create_game
     # fill_in :player_action, with: "rock"
     # get :game
-    # expect(assigns(:game).select_random).to eq("scissors")
+    # expect(assigns(:game).select_random).to eq("scissors") -- CANNOT GET THIS TO WORK, INVESTIGATE IN WEEK 4
     
     # click_button "Continue"
     # expect(page).to have_content "The computer chose scissors. You win!"
