d545e238fe6682ba759d7814c9bc33a7be2e0d78
Arthur Fincham <arthurfincham@me.com>
2021-10-10 17:55:44 +0100

    add spec for multiplayer features

--- spec/features/set_up_spec.rb
+++ spec/features/set_up_spec.rb
@@ -1,6 +1,6 @@
 feature "#GameSetUp" do
   scenario 'home page has input' do
     visit('/')
-    expect(page).to have_text("User", normalize_ws: true)
+    expect(page).to have_text("Rock Paper Scissors", normalize_ws: true)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,11 @@
 require 'game'
 
 describe Game do
-  let(:user){ double :user}
-  let(:computer){ double :computer }
+  let(:user){ double :user, :name => "User"}
+  let(:player2) { double :computer, :name => "Player 2"}
+  let(:computer){ double :computer, :name => "Player 2"}
   subject(:game) { described_class.new(user, computer) }
+  let(:multigame) { described_class.new(user, player2) }
 
   describe "#init" do
     it 'retrieves the user' do
@@ -22,39 +24,54 @@ describe Game do
   describe "#winner" do
     it 'user wins with rock' do
       allow(computer).to receive(:hand) { 'rock' }
-      expect(subject.who_won('paper')).to eq 'user'
+      expect(subject.who_won('paper')).to eq 'User wins!'
     end
 
     it 'user wins with paper' do
       allow(computer).to receive(:hand) { 'paper' }
-      expect(subject.who_won('scissors')).to eq 'user'
+      expect(subject.who_won('scissors')).to eq 'User wins!'
     end
 
     it 'user wins with scissors' do
       allow(computer).to receive(:hand) { 'scissors' }
-      expect(subject.who_won('rock')).to eq 'user'
+      expect(subject.who_won('rock')).to eq 'User wins!'
     end
 
     it 'computer wins with rock' do
       allow(computer).to receive(:hand) { 'rock' }
-      expect(subject.who_won('scissors')).to eq 'computer'
+      expect(subject.who_won('scissors')).to eq 'Computer wins!'
     end
 
     it 'computer wins with paper' do
       allow(computer).to receive(:hand) { 'paper' }
-      expect(subject.who_won('rock')).to eq 'computer'
+      expect(subject.who_won('rock')).to eq 'Computer wins!'
     end
 
     it 'computer wins with scissors' do
       allow(computer).to receive(:hand) { 'scissors' }
-      expect(subject.who_won('paper')).to eq 'computer'
+      expect(subject.who_won('paper')).to eq 'Computer wins!'
+    end
+
+    it 'player 2 wins with rock' do
+      allow(player2).to receive(:hand) { 'rock' }
+      expect(subject.multi_who_won('scissors', 'rock')).to eq 'Player 2 wins!'
+    end
+
+    it 'player 2 wins with paper' do
+      allow(player2).to receive(:hand) { 'paper' }
+      expect(subject.multi_who_won('rock', 'paper')).to eq 'Player 2 wins!'
+    end
+
+    it 'player2  wins with scissors' do
+      allow(player2).to receive(:hand) { 'scissors' }
+      expect(subject.multi_who_won('paper', 'scissors')).to eq 'Player 2 wins!'
     end
   end
 
   describe "#draw" do
     it 'same hands are shown' do
       allow(computer).to receive(:hand) { 'rock' }
-      expect(subject.who_won('rock')).to eq 'draw'
+      expect(subject.who_won('rock')).to eq 'we have a draw'
     end
   end
 end
