2ef75312a958fd73cd12966d8957213495b1ac59
bmargerison <bmargerison@outlook.com>
2021-10-30 17:02:46 +0100

    Game logic and unit tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,43 @@
 class Game
 
+  attr_reader :result
+
+  @@weapons = ['paper', 'scissors', 'rock']
+
+  def initialize(player)
+    @player = player
+  end
+
+  def player_weapon(weapon)
+    @player_weapon = weapon
+    puts "You selected #{@player_weapon}"
+  end
+
+  def run_game
+    @cpu_weapon = cpu_weapon
+    @result = determine_result
+    print_result
+  end
+
+  private
+
+  def cpu_weapon
+    @@weapons[rand(0..2)]
+    puts "CPU selected #{@cpu_weapon}"
+  end
+
+  def determine_result
+    return 'draw' if @player_weapon == @cpu_weapon
+    return 'loss' if @player_weapon == @@weapons[@@weapons.index(@cpu_weapon)-1]
+    'win'
+  end
+
+  def print_result
+    case @result
+      when 'win' then "Contratulations #{@player.name}! YOU WIN"
+      when 'loss' then "Sorry #{@player.name}! YOU LOST"
+      when 'draw' then "Its a DRAW, #{@player.name}!"
+    end
+  end
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,20 +4,78 @@ describe Game do
 
   let(:player_1) { double('player_1') }
   let(:game) { Game.new(player_1) }
-  before { allow(player_1).to reveive(:name) {'player_1'} }
 
-  describe 'player wins' do
+  before { allow(player_1).to receive(:name) {'player_1'} }
+  before { allow(game).to receive(:name) {'player_1'} }
 
-    it 'player wins with rock' do
-      allow(game).to receive(:cpu_selection) { 'scissors' }
-      game.player_selection('rock')
-      expect(game.result).to eq "Contratulations #{player_1.name}! YOU WIN"
+  describe 'player selects rock' do
+
+    before { game.player_weapon('rock') }
+
+    it 'cpu selects scissors' do
+      allow(game).to receive(:cpu_weapon) { 'scissors' }
+      game.run_game
+      expect(game.result).to eq 'win'
     end
       
-    it 'player wins with paper' do
+    it 'cpu selects rock' do
+      allow(game).to receive(:cpu_weapon) { 'rock' }
+      game.run_game
+      expect(game.result).to eq 'draw'
     end
 
-    it 'player wins with scissors' do
+    it 'cpu selects paper' do
+      allow(game).to receive(:cpu_weapon) { 'paper' }
+      game.run_game
+      expect(game.result).to eq 'loss'
+    end
+
+  end
+
+  describe 'player selects paper' do
+
+    before { game.player_weapon('paper') }
+
+    it 'cpu selects scissors' do
+      allow(game).to receive(:cpu_weapon) { 'scissors' }
+      game.run_game
+      expect(game.result).to eq 'loss'
+    end
+      
+    it 'cpu selects rock' do
+      allow(game).to receive(:cpu_weapon) { 'rock' }
+      game.run_game
+      expect(game.result).to eq 'win'
+    end
+
+    it 'cpu selects paper' do
+      allow(game).to receive(:cpu_weapon) { 'paper' }
+      game.run_game
+      expect(game.result).to eq 'draw'
+    end
+
+  end
+
+  describe 'player selects scissors' do
+
+    before { game.player_weapon('scissors') }
+
+    it 'cpu selects scissors' do
+      allow(game).to receive(:cpu_weapon) { 'scissors' }
+      game.run_game
+      expect(game.result).to eq 'draw'
+    end
+      
+    it 'cpu selects rock' do
+      allow(game).to receive(:cpu_weapon) { 'rock' }
+      game.run_game
+      expect(game.result).to eq 'loss'
+    end
+
+    it 'cpu selects paper' do
+      allow(game).to receive(:cpu_weapon) { 'paper' }
+      game.run_game
+      expect(game.result).to eq 'win'
     end
 
   end
