3eaee8d1485c5ba3d10d288d596ecdfda436e850
Sigita <sigita.zeb92@gmail.com>
2021-11-05 21:40:26 +0000

    Initialize method where player can choose rps

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,15 @@
+require './lib/game'
+
 class Player
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @choice = nil
+  end
+
+  def player_choice(choice)
+    capitalized_choice = choice.downcase.capitalize
+    @choice = capitalized_choice if Game::CHOICES.include?(capitalized_choice)
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,4 +6,11 @@ describe Player do
    it "shows the players name" do
       expect(player_1.name).to eq 'Joe'
    end
+
+   describe '#rplayer_choice' do
+   it 'should give a choice' do
+     allow(player_1).to receive(:player_choice) { "Rock"}
+     expect(player_1.player_choice).to eq("Rock").or eq("Paper").or eq("Scissors")
+   end
+  end
 end 
