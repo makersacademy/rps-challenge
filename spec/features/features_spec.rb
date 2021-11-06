5157d71da7d2ba5a41600894e1fa02b9d296662d
Matt Zimmer <zimmermja@gmail.com>
2021-11-06 16:12:04 +0000

    tidied up sign-in page

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -2,7 +2,7 @@ describe Game do
   feature '#Testing sign-in page' do
     scenario '#Asks users to enter a name' do
       visit('/')
-      expect(page).to have_content 'Please enter your name here'
+      expect(page).to have_content 'Please enter and submit your name here'
     end
   end
 end
