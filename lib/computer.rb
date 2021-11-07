a3d9d5332539197cad7a278fe361c9e8e271db50
Mark McLeod <markmcleod45@gmail.com>
2021-11-07 16:37:27 +0000

    Add a move attribute to computer class. Add a method that can generate either rock, paper or scissors for the computer

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,9 +1,14 @@
 class Computer
 
-  attr_reader :name
+  attr_reader :name, :move
 
   def initialize
     @name = "Computer"
+    @move = ["Rock", "Paper", "Scissors"]
+  end
+
+  def random
+    @move.sample
   end
 
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -9,4 +9,17 @@ describe Computer do
     end
   end
 
+  describe '#move' do
+    it 'returns the attribute move' do
+      expect(computer.move).to eq ["Rock", "Paper", "Scissors"]
+    end
+  end
+
+  describe '#random' do
+    it 'updates the move attribute to either rock, paper or scissors' do
+    computer.random
+    expect(computer.random).to eq("Rock").or(eq("Paper")).or(eq("Scissors"))
+    end
+  end
+
 end
