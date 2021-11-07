72228d6f786cbec11063161f999df41ecfdd7c60
Mark McLeod <markmcleod45@gmail.com>
2021-11-07 13:31:28 +0000

    Made computer a hardcoded string in preparation for creating a computer class

--- app.rb
+++ app.rb
@@ -15,19 +15,16 @@ class RockPaperScissors < Sinatra::Base
 
   post '/names' do
     $player_1 = Player.new(params["player_1_name"])
-    $player_2 = Player.new(params["player_2_name"])
     redirect '/play'
   end
 
   get '/play' do
     @player_1 = $player_1
-    @player_2 = $player_2
     erb(:play)
   end
 
   get '/choose' do
     @player_1 = $player_1
-    @player_2 = $player_2
     erb(:choose)
   end
 
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,5 @@
 def sign_in_and_enter_names
   visit('/')
   fill_in :player_1_name, with: 'Mark'
-  fill_in :player_2_name, with: 'Computer'
   click_button 'Play'
 end
