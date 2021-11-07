2857f7258f99e0dc9b22a16efc55158284fa3c1b
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-07 14:31:08 +0000

    create and test a Game instance is created

--- lib/game.rb
+++ lib/game.rb
@@ -4,10 +4,17 @@ require_relative 'player'
 class Game
   attr_reader :player_name, :computer
 
+  def self.create(player_name, computer)
+    @game = Game.new(player_name, computer)
+  end
+   
+  def self.instance
+    @game
+  end
+  
   def initialize(player_name, computer)
     @player_name = player_name
     @computer = computer
   end
-
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,17 +1,23 @@
 require 'game'
 
 describe Game do
+  
   subject(:game) { described_class.new(player_name, computer) }
   let(:player_name) { double 'Player'}
   let(:computer) { double 'Computer' }
-
+  
   describe '#initialize' do
-    it "creates a new game with a player" do
+    it 'creates a new game with a player' do
       expect(game.player_name).to eq player_name
+    end
+
+    it 'creates a new game with the computer' do
+      expect(game.computer).to eq computer
     end
   end
 
-
-
-
+  describe '.instance' do
+    it { is_expected.to be_instance_of(Game) }
+  end
+  
 end
