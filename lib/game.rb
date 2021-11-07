4b38cef4d3649714287033eed00160d52fa5b44d
Ginny Thomas <ginnythomas@Ginnys-MacBook-Air.local>
2021-11-07 15:43:33 +0100

    added 2 TODOs for next steps to complete

--- lib/game.rb
+++ lib/game.rb
@@ -11,3 +11,6 @@ class Game
     computer_move_options.sample
   end
 end
+
+# TODO: Still to complete: I need to create a method to compare player choice vs computer choice to decide a winner or a tie
+# TODO: will still need to impliment this on the controller side
