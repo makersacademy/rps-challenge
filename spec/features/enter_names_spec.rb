14c2c7a26293c06c9d26a76239caf18f46fe64d8
Mark McLeod <markmcleod45@gmail.com>
2021-11-07 11:32:22 +0000

    Add web helper for entering player names. Use it in enter names feature test

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Mark'
-    fill_in :player_2_name, with: 'Computer'
-    click_button 'Play'
+    sign_in_and_enter_names
     expect(page).to have_content 'Mark vs. Computer'
   end
 end
