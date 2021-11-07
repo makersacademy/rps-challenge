c1b59bc34b69630181c094569f65c59f91a9aea1
Mohammed Salah <mohammed_salah@icloud.com>
2021-11-07 19:39:39 +0000

    Implemented User Story 1

--- app.rb
+++ app.rb
@@ -1,13 +1,25 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader   
   end
 
   get '/' do
-    'Hello World'
+    erb :index
+  end
+
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @name = session[:name]
+    erb :play
   end
 
   run! if app_file == $0
