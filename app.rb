6a9f1e0222af4166b38a3a2daf8db9285f5aa717
Mark McLeod <markmcleod45@gmail.com>
2021-11-07 11:15:26 +0000

    Enable sessions. Redirect /names to /play. Display names at /play. Enter names test passing

--- app.rb
+++ app.rb
@@ -6,19 +6,23 @@ class RockPaperScissors < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params["player_1_name"]
-    @player_2_name = params["player_2_name"]
-    erb(:play)
+    session[:player_1_name] = params["player_1_name"]
+    session[:player_2_name] = params["player_2_name"]
+    redirect '/play'
   end
 
-  # get '/play' do
-  #   erb(:play)
-  # end
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
