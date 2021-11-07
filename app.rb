d4870f3e69d643788f0df33c9fcc9e5e7e1e0390
dean <d.lewis.rave@gmail.com>
2021-11-07 19:33:59 +0000

    two passing tests and a post

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+
   configure :development do
     register Sinatra::Reloader
   end
@@ -10,9 +12,10 @@ class BookmarkManager < Sinatra::Base
     erb(:index)
   end
 
-  post '/' do
-
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    erb(:play)
   end 
-
+  
   run! if app_file == $0
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,3 +4,12 @@ feature 'Getting players names' do
     expect(page).to have_content 'Please enter your name below'
   end
 end
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Charlotte'
+    click_button 'Play'
+    expect(page).to have_content 'vs. Invincible computer!'
+  end
+end
