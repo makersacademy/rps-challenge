203eba48a826c7c5729802b86bf2540915c82a4d
Lilly Daniell <daniell.lilly@gmail.com>
2021-11-30 17:14:22 +0000

    Add feature test to play game

--- spec/features/pick_rps_spec.rb
+++ spec/features/pick_rps_spec.rb
@@ -1,6 +1,8 @@
-# feature 'Play rock, paper, scissor' do
-#   describe 'click on rock, paper, scissor' do
-#     enter_name_to_play
-
-#   end
-# end
+feature 'Play rock, paper, scissor' do
+  scenario 'click on rock, paper, scissor' do
+    enter_name_to_play
+    check 'paper'
+    click_button 'did you win or lose?'
+    expect(page).to have_content('play again?')
+  end
+end
