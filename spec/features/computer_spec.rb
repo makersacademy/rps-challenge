6461747ab23499ca30ff64cc7d50ca6c4b202ff1
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-03 19:00:17 +0000

    Refactor unused code

--- spec/features/computer_spec.rb
+++
@@ -1,7 +0,0 @@
-feature "computer chooses a random option" do
-  scenario "User picks an option, and computer randomly chooses an option" do
-    allow(game).to receive(:rock)
-    sign_in_and_play
-    expect(page).to have_content "Computer chose Rock!"
-  end
-end
