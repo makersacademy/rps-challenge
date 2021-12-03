bcd36a906caf64cd2e0592c0d79273a5ffe26bd1
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-03 17:29:24 +0000

    Implement name registration form

--- app.rb
+++ app.rb
@@ -2,12 +2,22 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
+
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    'Hello World'
+    erb :index
+  end
+
+  get '/play' do
+    erb :play
+  end
+
+  post '/name' do
+    session[:name] = params[:name]
   end
 
   run! if app_file == $0
--- spec/features/register_name_spec.rb
+++ spec/features/register_name_spec.rb
@@ -2,6 +2,7 @@ feature "name registration" do
   scenario "user registers name and name is displayed" do
     visit('/')
     fill_in :name, with: "Win"
+    click_button "Go!"
     expect(page).to have_content("Win")
   end
 end
