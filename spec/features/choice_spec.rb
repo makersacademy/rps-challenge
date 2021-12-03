5660061f6bdab4a7dfe7a1d1b64a22fc23723dc8
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-03 17:35:23 +0000

    Write tests for game choices

---
+++ spec/features/choice_spec.rb
@@ -0,0 +1,25 @@
+feature 'user can choose between rock, paper, and scissors' do
+  scenario 'user clicks rock and gets a notification' do
+    visit('/')
+    fill_in :name, with: 'Win'
+    click_button "Go!"
+    click_button "Rock"
+    expect(page).to have_content "Win chose Rock!"
+  end
+
+  scenario 'user clicks paper and gets a notification' do
+    visit('/')
+    fill_in :name, with: 'Win'
+    click_button "Go!"
+    click_button "Paper"
+    expect(page).to have_content "Win chose Paper!"
+  end
+
+  scenario 'user clicks scissors and gets a notification' do
+    visit('/')
+    fill_in :name, with: 'Win'
+    click_button "Go!"
+    click_button "Scissors"
+    expect(page).to have_content "Win chose Scissors!"
+  end
+end
