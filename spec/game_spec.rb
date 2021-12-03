c1a1a2c3674bb04c4762ec51ef33e2a9d1f29d95
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-03 20:18:24 +0000

    Rubocop corrections

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,8 @@
 require 'game'
 
 describe Game do
-  let(:player1) { double("Player 1", :name => "Player 1")}
-  let(:player2) { double("Player 2", :name => "Player 2")}
+  let(:player1) { double("Player 1", :name => "Player 1") }
+  let(:player2) { double("Player 2", :name => "Player 2") }
   let(:subject) { described_class.new }
 
   describe "#get_outcome" do
