18fcccf050b277a3c899b09737ce53b9a5738f59
riky moroni <riky5.moroni@gmail.com>
2021-12-04 15:15:46 +0000

    add 2 more tests to player_spec.rb

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,7 +2,15 @@ require 'player'
 
 describe Player do
   let(:subject) { Player.new('Pikachu') }
-  it 'picks one option' do
+  it 'picks rock' do
     expect(subject.choose_rock).to eq 'rock'
+  end
+
+  it 'picks paper' do
+    expect(subject.choose_paper).to eq 'paper'
+  end
+
+  it 'picks scissors' do
+    expect(subject.choose_scissors).to eq 'scissors'
   end
 end
