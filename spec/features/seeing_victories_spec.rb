aece6c11a773b1bfb28d250230528686c218f0f1
Sarah <dousse.sarah@gmail.com>
2021-12-04 19:19:23 +0000

    Player has a name and a victory count && update controller to use sessions

--- spec/features/seeing_victories_spec.rb
+++ spec/features/seeing_victories_spec.rb
@@ -1,4 +1,4 @@
-feature "enter name" do
+feature "victory attribute" do
   scenario "1 player can enter his/her name and see it displayed" do
     sign_in_and_play
     expect(page).to have_content("Wednesday vs Thing")
