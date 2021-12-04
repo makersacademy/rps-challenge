e0308013b8b8f033c71b6189785d2d066b7c903f
Francesco <francescocollotto@gmail.com>
2021-12-04 12:25:47 +0000

    initialise  in post 'name'

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/game'
+require_relative './lib/player'
 
 class RpsGame < Sinatra::Base
   configure :development do
@@ -11,7 +13,12 @@ class RpsGame < Sinatra::Base
   end
 
   post '/name' do
+    $game = Game.new(Player.new(params[:name]))
+    redirect '/play'
+  end
 
+  get '/play' do
+    erb :play
   end
 
   run! if app_file == $0
