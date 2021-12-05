fdd86ae6a1d570befcd5b0fabe74d641814422ba
Titus Chang <titus@Tituss-MacBook-Air.local>
2021-12-05 12:45:14 +0000

    Updated game logic unit tests

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -2,17 +2,17 @@ require 'game_logic'
 
 describe GameLogic do
   let(:gamelogic) { described_class.new(player1,player2) }
-  let(:player1) { double(:name => "Titus", :move => "Rock")}
-  let(:player2) { double(:name => "Chang", :move => "Scissors")}
-  let(:gamelogic2) { described_class.new(player1_lose,player2_win)}
-  let(:player1_lose) { double(:name => "Titus", :move => "Rock")}
-  let(:player2_win) { double(:name => "Chang", :move => "Paper")}
-  let(:gamelogic3) { described_class.new(player_draw,player_draw)}
-  let(:player_draw) { double(:name => "n/a", :move => "Rock")}
+  let(:player1) { double(:name => "Titus", :move => "Rock") }
+  let(:player2) { double(:name => "Chang", :move => "Scissors") }
+  let(:gamelogic2) { described_class.new(player1_lose,player2_win) }
+  let(:player1_lose) { double(:name => "Titus", :move => "Rock") }
+  let(:player2_win) { double(:name => "Chang", :move => "Paper") }
+  let(:gamelogic3) { described_class.new(player_draw,player_draw) }
+  let(:player_draw) { double(:name => "n/a", :move => "Rock") }
 
   context '#change_turn' 
   it 'can increment turn number by 1' do
-    expect{gamelogic.change_turn}.to change{gamelogic.turn_number}.by(1)
+    expect { gamelogic.change_turn }.to change { gamelogic.turn_number }.by(1)
   end
 
   context '#current_turn' 
