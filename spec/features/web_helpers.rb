105f7a272ed3bb461ec8bceccadcf0447537ce6d
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2021-12-05 18:41:09 +0000

    fixed quotation marks and added one more feature test

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play
-  visit('/')
-  fill_in 'name', with: 'Alfonso'
-  select 'Rock'
-  click_button 'Play'
+  visit("/")
+  fill_in "name", with: "Alfonso"
+  select "Rock"
+  click_button "Play"
 end
