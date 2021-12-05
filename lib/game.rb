f9c956ca7ec54d68e4ae8561bf2cfe6a6b7174b4
Sarah <dousse.sarah@gmail.com>
2021-12-05 11:48:36 +0000

    reafactoring

--- lib/game.rb
+++ lib/game.rb
@@ -15,22 +15,20 @@ class Game
   end
 
   def get_player_move
-    @p1_move = @player1.p_move  # or can use @player1.p_move to get the move?
+    @p1_move = @player1.p_move
   end
 
   def fight_outcome
     get_player_move
     pick_random
     if @c_move == @p1_move
-      @outcome = "it's a tie #{@c_move} = #{@p1_move}"
+      @outcome = 1
     elsif (@p1_move == "ROCK" && @c_move == "SCISSORS") || (@p1_move == "PAPER" && @c_move == "ROCK") || (@p1_move == "SCISSORS" && @c_move == "PAPER")
       @player1.victory
-      @outcome = "player wins (victories: #{@player1.victories}): #{@p1_move} beats #{@c_move}"
-    elsif #(@c_move == "ROCK" && @p1_move == "SCISSORS") || (@c_move == "PAPER" && @p1_move == "ROCK") || (@c_move == "SCISSORS" && @p1_move == "PAPER")
+      @outcome = 2
+    else
       @player2.victory
-      @outcome = "computer wins (victories: #{@player2.victories}): #{@c_move} beats #{@p1_move}"
-    # else
-    #   @outcome = "error #{@c_move} beats #{@p1_move}"
+      @outcome = 3
     end
   end
 end
