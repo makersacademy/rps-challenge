261ce2fce420cb8d636440431bfe97b8a3297970
Josh <joshlim092@gmail.com>
2022-01-08 10:58:49 +0000

    first route page added

--- app.rb
+++ app.rb
@@ -4,10 +4,13 @@ require 'sinatra/reloader'
 class MyApp < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
+    also_reload ''
   end
 
   # our routes would go here
-
+  get '/' do 
+    'Hello World!'
+  end 
 
   # # Start the server if this file is executed directly (do not change the line below)
   run! if app_file == $0
