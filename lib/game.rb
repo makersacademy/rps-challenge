0724b13d0e884a9d85807f38e17463d86c908439
Josh <joshlim092@gmail.com>
2022-01-08 19:10:42 +0000

    add game ability to determine winner/loser

--- lib/game.rb
+++ lib/game.rb
@@ -2,9 +2,19 @@ class Game
 
   def initialize
     @options = ['Rock', 'Paper', 'Scissor']
+    @computer_move
   end 
 
   def computer_move
     @options.sample
   end 
+
+  def result(player, computer)
+    return "You Win" if player == 'Rock' && computer == 'Scissor'
+    return "You Win" if player == 'Scissor' && computer == 'Paper'
+    return "You Win" if player == 'Paper' && computer == 'Rock'
+    return "It's a DRAW!" if player == computer_move
+    "You Lose"
+  end
+    
 end 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,6 +8,22 @@ describe Game do
       expect(['Rock', 'Paper', 'Scissor']).to include(game.computer_move)
     end 
   end 
+
+  describe '#result' do
+    it 'recognises a win' do 
+      game = Game.new
+      player = 'Rock'
+      computer = 'Scissor'
+      expect(game.result(player, computer)).to eq("You Win")
+    end 
+
+    it 'recognises a draw' do 
+      game = Game.new
+      player = 'Rock'
+      computer = 'Rock'
+      expect(game.result(player, computer)).to eq("It's a DRAW!")
+    end 
+  end 
 end 
 
 
