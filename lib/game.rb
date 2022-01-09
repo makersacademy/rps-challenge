94dae6db1781d57adcf6933e2e5ece1aff0e50ef
icvotria <icvotria@gmail.com>
2022-01-09 09:49:09 +0000

    #outcome testing and passing

--- lib/game.rb
+++ lib/game.rb
@@ -9,4 +9,14 @@ class Game
   def computer_move
     @moves.sample
   end
+
+  def outcome(move_1, move_2)
+    if @moves.find_index(move_1) == @moves.find_index(move_2)
+      'Draw!!'
+    elsif (@moves.find_index(move_1) + 1) % 3 == @moves.find_index(move_2)
+      'You lose!!'
+    else
+      'You win!!'
+    end
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,4 +7,10 @@ describe 'Game' do
   it 'returns player names' do
     expect(game.player).to eq player
   end
+
+  it 'gives correct outcome' do
+    expect(game.outcome('Rock', 'Scissors')).to eq 'You win!!'
+    expect(game.outcome('Rock', 'Rock')).to eq 'Draw!!'
+    expect(game.outcome('Rock', 'Paper')).to eq 'You lose!!'
+  end
 end
