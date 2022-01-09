6df22f00d1b62225fe26352c5af1a051a15c492c
Raven Sears <ravenrosereynolds@gmail.com>
2022-01-09 20:51:28 +0000

    Added User Story Test and Code

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class RPS < Sinatra::Base
+    enable :sessions
+    configure :development do
+        register Sinatra::reloader
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/name' do
+        session[:name] = params[:name]
+        redirect '/play'
+    end
+
+    get '/play' do
+      @name = session[:name]
+      erb :play
+    end
+
+    run! if app_file == $0
+end
