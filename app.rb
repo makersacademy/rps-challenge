74db19d34d55d67c00a489de0c6bc3d3554f44ca
SophieGowen <sophie.j.gowen@gmail.com>
2022-01-09 03:12:11 +0000

    Set up framework and test infrastructure

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class RockPaperScissors < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do 
+    "hello!"
+  end
+end
