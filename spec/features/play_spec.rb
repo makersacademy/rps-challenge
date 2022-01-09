6846ab916f8a121c18ecfd223b35eeef7ecd1e6c
Josh <joshlim092@gmail.com>
2022-01-09 12:24:38 +0000

    add spec bug fix of click_on(Submit) to other required files

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -4,6 +4,7 @@ feature 'Player 1 move submission' do
   scenario 'Player 1 selects their move and sees on screen' do 
     sign_in_and_play
     select "Rock", :from => "first_move"
+    click_on('Submit')
     expect(page).to have_content('Rock')
   end 
 end
