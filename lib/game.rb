5d98a1685a2ce3f5ea6fdab31bf3f66b1d121ab4
icvotria <icvotria@gmail.com>
2022-01-10 00:41:24 +0000

    made moves array a constant

--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,22 @@
 class Game
   attr_reader :player, :result, :tally
 
+  MOVES = ['Rock', 'Paper', 'Scissors']
+
   def initialize(player)
-    @moves = ['Rock', 'Paper', 'Scissors']
     @player = player
     @tally = { player: 0, computer: 0, draws: 0 }
   end
 
   def computer_move
-    @moves.sample
+    MOVES.sample
   end
 
   def outcome(move1, move2)
-    if @moves.find_index(move1) == @moves.find_index(move2)
+    if MOVES.find_index(move1) == MOVES.find_index(move2)
       @tally[:draws] += 1
       :draw
-    elsif (@moves.find_index(move1) + 1) % 3 == @moves.find_index(move2)
+    elsif (MOVES.find_index(move1) + 1) % 3 == MOVES.find_index(move2)
       @tally[:computer] += 1
       :lose
     else
