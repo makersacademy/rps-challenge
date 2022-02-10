edf885c4b263a0c4d3b03406814d265bbda4330b
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-02-10 22:56:40 +0000

    User Story 1: Removed globals and introduced PStore for storing state

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
+require 'pstore'
 
 require File.join(File.dirname(__FILE__), 'lib', 'player.rb')
 
@@ -11,18 +12,39 @@ class RPS < Sinatra::Base
 
   enable :sessions
 
+  helpers do
+    # Using PStore to store state
+    # names need to be unique
+    def add_player(player)
+      data = PStore.new("#{player.name}.pstore")
+      data.transaction do  
+        data[player.name] = player.name 
+        data.commit
+      end
+    end
+
+    def load_player(name)
+      data = PStore.new("#{name}.pstore")
+      data.transaction do
+        Player.new(data[name])
+      end
+    end
+
+  end
+
   get '/' do
     erb(:index)
   end
 
   post '/register_name' do
-    # Using globals until I know more about databases with Sinatra
-    $player = Player.new(params[:player_name])
+    session[:player_name] = params[:player_name]
+    add_player(Player.new(session[:player_name]))
     redirect('/play')
   end
 
   get '/play' do
-    @player = $player
+    @player = load_player(session[:player_name])
+    p "Player Name is: #{@player.name}"
     erb(:play)
   end
 
