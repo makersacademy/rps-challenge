979ee4f16a57fc64aa48334c18426543483204e3
Eddie Arnold <eddiearnoldz@me.com>
2022-02-11 14:33:58 +0000

    Initial commit, set and first feature_infrastructure_test passed

---
+++ spec/features/feature_framework__spec.rb
@@ -0,0 +1,6 @@
+feature "The infrastructure", type: :feature do
+  scenario "It shows us helllo world" do
+    visit '/'
+    expect(page).to have_content "Hello World"
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
+require './app.rb'
+ENV['RACK_ENV'] = 'test'
+require 'capybara'
 require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+Capybara.app = Rps
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
+  SimpleCov::Formatter::Console
   # Want a nice code coverage website? Uncomment this next line!
   # SimpleCov::Formatter::HTMLFormatter
 ])
