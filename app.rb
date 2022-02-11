baaec2afa2e1bbff20c01ecfe9964cdb8df5e5d1
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-02-11 17:16:02 +0000

    Set up the app and have tested the infrastructure

---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+
+class Rps < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+get "/" do
+  "Welcome to Rps"
+end
+
+run! if app_file == $0
+end
