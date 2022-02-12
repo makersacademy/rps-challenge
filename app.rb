18219f9f8d306c27acf5da88680551de7dedb257
Haroon <haroon20@hotmail.co.uk>
2022-02-12 21:27:29 +0000

    move submitted

--- app.rb
+++ app.rb
@@ -6,14 +6,33 @@ class Rps < Sinatra::Base
       register Sinatra::Reloader
     end
 
+enable :sessions
+
 get '/' do
   erb :index
 end
 
 post '/names' do
-   @player1 = params[:player1]
-  erb :view_name
+    session[:player1] = params[:player1]
+    redirect ('/view_name')
 end
+
+get '/view_name' do
+    @player1 = session[:player1]
+    @shape = session[:shape]
+    erb :view_name
+end
+
+post '/view_option' do
+   # @rps = params[:Rock]
+   session[:shape] = params[:shape]
+   redirect '/submit_move'
+end
+
+get '/submit_move' do
+   erb :submit_move
+end
+
 
     run! if app_file == $0
 end
---
+++ spec/features/select_rps_spec.rb
@@ -0,0 +1,21 @@
+
+feature '' do
+  before do
+    visit ('/')
+    fill_in :player1, with: 'Larry'
+    click_button 'Submit'
+  end
+
+  scenario 'Check if page has the rps buttons' do
+    expect(page).to have_button 'Rock'
+    expect(page).to have_button 'Scissors'
+    expect(page).to have_button 'Paper'
+  end 
+
+  scenario 'choose a shape' do
+    click_button 'Paper'
+    expect(page).to have_content 'You picked Paper'
+
+  end
+
+end
