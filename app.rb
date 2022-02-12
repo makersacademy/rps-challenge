408625d0ff91a7909a1446f9ef3e54a21800da35
Ed <ed321@hotmail.co.uk>
2022-02-12 21:48:37 +0000

    Created Game and Player class

--- app.rb
+++ app.rb
@@ -1,26 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative './lib/player'
+require_relative './lib/game'
 
 class RPS < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
   
   get '/game' do
-    @player = $player
+    @player = session["player"]
     erb(:game)
   end
 
   get '/play' do
+    Game.new(session["player"])
     erb(:play)
   end
 
   post '/login' do
-    $player = params["player"]
+    session["player"] = Player.new(params["player"])
     redirect '/game'
   end
 
