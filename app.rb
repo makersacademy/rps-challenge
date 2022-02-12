7530639cd5113151505abb7f1b51c28602609d9b
Jane Parsons <jane.parsons@waitrose.co.uk>
2022-02-12 20:54:15 +0000

    Add functionality for marketeer to make a move

--- app.rb
+++ app.rb
@@ -13,6 +13,31 @@ class RPSgame < Sinatra::Base
     erb :index
   end
 
+  post '/marketeer' do
+    p params
+    $marketeer = params[:marketeer]
+    @marketeer = $marketeer
+    erb :play
+  end
+
+  post '/rock' do
+    @marketeer = $marketeer
+    @marketeer_move = "rock"
+    erb :move
+  end  
+
+  post '/paper' do
+    @marketeer = $marketeer
+    @marketeer_move = "paper"
+    erb :move
+  end
+
+  post '/scissors' do
+    @marketeer = $marketeer
+    @marketeer_move = "scissors"
+    erb :move
+  end  
+
   # # Start the server if this file is executed directly (do not change the line below)
   run! if app_file == $0
 end
--- spec/features/enter_marketeer_spec.rb
+++ spec/features/enter_marketeer_spec.rb
@@ -1,7 +1,25 @@
 
-feature 'Testing entry and display of players names' do
+feature 'Testing entry and display of marketeer' do
+
   scenario 'Test basic content when loading screen' do
     visit('/')
     expect(page).to have_content 'Enter Marketeer:'
   end
+
+  scenario 'Test entering a marketeer' do
+    visit('/')
+    fill_in 'marketeer', with: 'Jane Lucas'
+    click_on 'Submit'
+    expect(page).to have_content 'Welcome Jane Lucas to Rock, Paper and Scissors!'
+  end
+  
+  scenario 'Test marketeer move' do
+    visit('/')
+    fill_in 'marketeer', with: 'Jane Lucas'
+    click_on 'Submit'
+    expect(page).to have_content 'Welcome Jane Lucas to Rock, Paper and Scissors!'
+    click_on 'Rock'
+    expect(page).to have_content 'Jane Lucas you have chosen rock'
+  end
+
 end
