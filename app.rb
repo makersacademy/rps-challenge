c622a9e3d995e6a8aaf4feedf1d9a5020fb51dba
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2022-02-12 22:14:00 +0000

    Hashed out some lines from app.rb, trying to get heroku working

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require "sinatra/base"
-require 'sinatra/reloader' if development?
+# require 'sinatra/reloader' if development?
 require './lib/game'
 
 class RPS < Sinatra::Base
 
-  configure :development do
-    register Sinatra::Reloader
-  end
+  # configure :development do
+  #   register Sinatra::Reloader
+  # end
 
   get '/' do
     erb(:index)
