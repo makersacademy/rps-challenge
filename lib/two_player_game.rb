7936c1439dfecfb546339e1f807544ca6fce22b5
Eddie Arnold <eddiearnoldz@me.com>
2022-02-12 11:43:21 +0000

    created two_player class that works in irb but not implemented to app.rb

---
+++ lib/two_player_game.rb
@@ -0,0 +1,61 @@
+require './lib/player.rb'
+
+class TwoPlayer
+
+  attr_reader :player1_score, :player2_score, :player1, :player2
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @player1_score = 0
+    @player2_score = 0
+  end
+
+
+  def play
+    return unless @player1.move && @player2.move != nil
+    result = outcome(@move)
+    score(result)
+    result
+  end
+
+  private
+
+  def score(result)
+    if result.include?(@player1.name)
+      @player1_score += 1
+    elsif result.include?(@player2.name)
+      @player2_score += 1
+    else
+    end
+  end
+
+  def outcome(player1_move)
+    case @player1.move
+    when "ROCK"
+      if @player2.move == "SCISSORS"
+        "ROCK beats SCISSORS. #{@player1.name} wins!"
+      elsif @player2.move == "PAPER"
+        "SCISSORS beats ROCK. #{@player2.name} wins!"
+      else
+        "DRAW"
+      end
+    when "PAPER"
+      if @player2.move == "SCISSORS"
+        "SCISSORS beats PAPER.  #{@player2.name} wins!"
+      elsif @player2.move == "ROCK"
+        "PAPER beats ROCK. #{@player1.name} wins!"
+      else
+        "DRAW"
+      end
+    when "SCISSORS"
+      if @player2.move == "ROCK"
+        "ROCK beats SCISSORS. #{@player2.name} wins!"
+      elsif @player2.move == "PAPER"
+        "SCISSORS beats PAPER. #{@player1.name} wins!"
+      else
+        "DRAW"
+      end
+    end
+  end
+end
