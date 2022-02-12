a51a65fd60b542b9037559f3e2e36d55d79d4d92
Francesco <francesco@monzo.com>
2022-02-12 19:19:30 +0100

    First user story: entering and storing player name

--- app.rb
+++ app.rb
@@ -6,9 +6,18 @@ class Rps < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get '/' do
-      "Rock Paper Scissors \m/"
+  get "/" do
+    erb :index
   end
+
+  post "/name" do
+    @marketeer = params[:marketeer]
+    erb :play
+  end
+
+  # get "/play" do
+  #   erb :play
+  # end
 
   run! if app_file == $0
 end
