092f35dd6de4c0927a07563ec07a0b69b9085765
Ed <ed321@hotmail.co.uk>
2022-02-12 22:14:45 +0000

    Player is displayed options

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -2,11 +2,15 @@
 # As a marketeer
 # So that I can see my name in lights
 # I would like to register my name before playing an online game
+def register
+  visit('/')
+  fill_in "player", with: "Marketeer"
+  click_button "Register"
+end
+
 feature 'Registering a name' do
   scenario 'player submits their name' do
-    visit('/')
-    fill_in "player", with: "Marketeer"
-    click_button "Register"
+    register
     expect(page).to have_content("Marketeer")
   end
 end
@@ -14,13 +18,19 @@ end
 # As a marketeer
 # So that I can enjoy myself away from the daily grind
 # I would like to be able to play rock/paper/scissors
-feature 'Plays Rock Paper Scissors' do
+feature 'Plays Rock, Paper, Scissors' do
   scenario 'player starts a game' do
-    visit('/')
-    fill_in "player", with: "Marketeer"
-    click_button "Register"
+    register
     click_button "Play"
     expect(page).to have_content("Game Started")
+  end
+
+  scenario 'player is shown options' do
+    register
+    click_button "Play"
+    expect(page).to have_button("Rock")
+    expect(page).to have_button("Paper")
+    expect(page).to have_button("Scissors")
   end
 
 end
