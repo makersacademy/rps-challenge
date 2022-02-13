7a556d49f09a0c895cf71fb29193fafe5ff3e07e
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-02-13 14:58:45 +0000

    Completed game. Took out unnecessary register.erb

--- app.rb
+++ app.rb
@@ -8,20 +8,24 @@ class Rps < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
 get "/" do
   erb :index
 end
 
 post "/register" do
-  @player = params[:player]  
-  erb :register
+  session[:player_1_name] = params[:player_1_name]
+  redirect "/play"
 end
 
-post "/play" do
+get "/play" do
+  @player_1 = session[:player_1_name]  
   erb :play
 end
 
-post "/result" do
+get "/result" do
+  $game = Game.new(params[:button])
   erb :result
 end
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,23 @@
 class Game
 
-attr_reader :player
+attr_reader :player, :ai
+# attr_accessor :ai
 
   def initialize(player)
     @player = player
+    @ai = ["Rock", "Paper", "Scissors"].sample
   end
 
-  def ai
-    choice = ["Rock", "Paper", "Scissors"]
-    choice[rand(choice.size)]
-  end  
+#   def ai
+#     choice = ["Rock", "Paper", "Scissors"]
+#     choice[rand(choice.size)]
+#   end  
 
   def result
-    return "#{player} won!" if player == "Rock" && ai == "Scissors"
-    return "#{player} won!" if player == "Scissors" && ai == "Paper"
-    return "#{player} won!" if player == "Paper" && ai == "Rock"
-    return "Draw" if player == ai
+    return "You win!" if @player == "Rock" && @ai == "Scissors"
+    return "You win!" if @player == "Scissors" && @ai == "Paper"
+    return "You win!" if @player == "Paper" && @ai == "Rock"
+    return "Draw" if @player == @ai
     return "The Computer won!"
   end
 
