e720c43d4b26fb3b5e5473164eb6d070dedc0dfb
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-02-13 14:51:02 +0000

    Tests added to result class for all options

--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -8,5 +8,19 @@ describe Result do
       expect(result).to receive(:winner).with(variables)
       result.winner(variables)
     end
+
+    it 'should result in a draw' do
+      expect(result.winner("rock", "rock")).to eq("It's a draw!")
+    end
+
+    it 'should result in player one winning' do
+      expect(result.winner("paper", "rock")).to eq("You won!")
+    end
+
+    it 'should result in player two winning' do
+      expect(result.winner("scissors", "rock")).to eq("I won!")
+    end
+
   end
+
 end
