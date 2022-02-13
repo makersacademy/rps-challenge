383ecfa5e878c698a393dcd89f0f185761412bf6
Shirley Macfarlane <shirley.macfarlane@waitrose.co.uk>
2022-02-13 16:46:33 +0000

    Updated to use a session instead of global variable

--- app.rb
+++ app.rb
@@ -14,16 +14,19 @@ class Rps < Sinatra::Base
     erb :index
   end
 
-  post '/play' do
-    $player_1 = Player.new(params[:player_1])
-    @player_1 = $player_1
+  post '/play' do 
+    @player_1 = Player.new(params[:player_1])
+    # store player_1 in the session
+    session[:message] = @player_1
     erb :play 
   end
 
    post '/game' do
     @weapon = Weapon.new(params[:choice])
-    @player_1 = $player_1
-    $player_1.add_weapon(Weapon.new(params[:choice]))
+    # retrieve player_1 from the session
+    @player_1 = session[:message] 
+    @player_1.add_weapon(@weapon)
+    # rcreate a player_2 to simulate the computer 
     @player_2 = Player.new("Computer")
     @weapon_2 = Weapon.new(Computer.new.weapon)
     @player_2.add_weapon(@weapon_2)
@@ -34,9 +37,11 @@ class Rps < Sinatra::Base
   end
 
   post '/finish' do
-     $player = nil
-     redirect '/'
-
+    # rset the session to nil
+    @player_1 = session[:message] 
+    @player_1 = nil
+    session[:message] = @player_1
+    redirect '/'
   end
 
   # start the server if ruby file executed directly
