c016d4aa2c1aef6e8e034b22a872bcad4bbd1b91
PhilBligh <philbligh@hotmail.com>
2022-03-09 18:53:48 +0000

    added more get actions and used sessions

--- app.rb
+++ app.rb
@@ -3,22 +3,34 @@ require 'sinatra/reloader'
 require_relative "./lib/rps"
 
 class Rps < Sinatra::Base
-  
   configure :development do
     register Sinatra::Reloader
   end
 
+  enable :sessions
+  
   get '/' do
     erb :index
   end
 
   post '/play' do
-    @player = params[:player_name]
+    session[:player_name] = params[:player_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player = session[:player_name]
     erb :play
   end
 
   post '/outcome' do
-    @answer = params[:answer]
+    session[:answer] = params[:answer]
+    redirect '/outcome'
+  end
+
+  get '/outcome' do
+    @player = session[:player_name]
+    @answer = session[:answer]
     game1 = Game.new(@answer)
     @computer_input = game1.computer_input
     @victory = game1.win_lose_draw
