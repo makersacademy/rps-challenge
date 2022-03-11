767f1caef992438f9866b7c1d1a09dff5b95e2c2
Valentina <maggio.vale@gmail.com>
2022-03-11 19:38:50 +0000

    Create test helper to remove repetitions in tests

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,8 +1,6 @@
 feature 'Testing form' do
   scenario 'The user can enter their name' do
-    visit('/')
-    fill_in 'player', with: 'Valentina'
-    click_button 'Go!'
+    sign_in_and_play
     expect(page).to have_text('What is your move Valentina?')
     end
   end
