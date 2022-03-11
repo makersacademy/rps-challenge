b26144aaf8f622569984c88c0142e87248e05cf5
Samuel Raducan <samuel.thembp@gmail.com>
2022-03-11 17:53:55 +0000

    Update app.rb file with Marketeer class and configure the development

--- app.rb
+++ app.rb
@@ -1,3 +1,13 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+class Marketeer < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello, world! App for marketeer'
+  end
+
+end
