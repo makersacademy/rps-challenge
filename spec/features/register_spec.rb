abc13c1f1083341c86d4733dc92a372ac8e59212
Anusha Nesargi <anushanesargi@gmail.com>
2022-03-11 10:59:13 +0000

    Added styling to index.erb

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -1,10 +1,11 @@
 feature 'Enter names' do
-    scenario 'submitting names' do
-        visit('/')
-        fill_in :player_1, with: 'Tinda'
-        fill_in :player_2, with: 'Bhatinda'
-        click_button 'Register'
+  scenario 'Registering names' do
+    visit('/')
+    fill_in :player_1, with: 'Tinda'
+    fill_in :player_2, with: 'Bhatinda'
+    click_button 'Register'
 
-        expect(page).to have_content 'Tinda vs. Bhatinda'
-    end
+    expect(page).to have_content 'Tinda vs. Bhatinda'
+  end
+  
 end
