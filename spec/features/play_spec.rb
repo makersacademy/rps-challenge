281c1d8497d0790a319fcb6e254364f3e2bd8926
Jonny Abrams <jonnyabrams@hotmail.com>
2022-03-11 15:06:11 +0000

    Returns name and choice after selection

--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -1,8 +1,15 @@
 feature 'Playing a game of rock paper scissors' do
-  scenario 'view options' do 
+  scenario 'view choices' do 
     sign_in
     expect(page).to have_field 'rock'
     expect(page).to have_field 'paper'
     expect(page).to have_field 'scissors'
+  end
+
+  scenario 'make choice' do
+    sign_in
+    choose 'rock'
+    click_button 'Submit'
+    expect(page).to have_content 'Jonny chose rock'
   end
 end
