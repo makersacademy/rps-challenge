6773cce21ab05d97ba8e236bbfbdea378bc42ed3
Samuel Raducan <samuel.thembp@gmail.com>
2022-03-11 18:24:56 +0000

    Add installed gems in spec_helper.rb file for future test

--- app.rb
+++ app.rb
@@ -2,12 +2,23 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Marketeer < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
 
+
   get '/' do
-    'Hello, world! App for marketeer'
+    erb :register
   end
 
+  post '/name' do
+    session[:name] = params[:name]
+    redirect ('/play')
+  end
+
+  get '/play' do
+    @name = session[:name]
+    erb :play
+  end
 end
