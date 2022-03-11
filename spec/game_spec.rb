21b551d5f59ab1f4540253ee2f76d662f606c460
ruiined <ruiined@gmail.com>
2022-03-11 20:07:52 +0000

    worked on tests, added stubs

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,13 +3,24 @@ require 'game'
 describe Game do
   subject(:game) { described_class.new(player) }
   let(:player) { double :player }
+  let(:result_options) { [true, false, nil] }
 
   it "initializes the player" do
     expect(game.player).to eq player
   end
 
-  it "creates a random output for computer" do
-    expect { game.computer }.to_not raise_error
+  it "calculates the result if the rock was picked" do
+    50.times { game.rock }
+    expect(result_options).to include game.result
   end
 
+  it "calculates the result if the paper was picked" do
+    50.times { game.paper }
+    expect(result_options).to include game.result
+  end
+
+  it "calculates the result if the scissors were picked" do
+    50.times { game.scissors }
+    expect(result_options).to include game.result
+  end
 end
