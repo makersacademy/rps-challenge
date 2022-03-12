8a9ce6015f53d9718b33b4ed1277bb4c0e168379
Paul <pkilgarriff@gmail.com>
2022-03-12 13:55:56 +0000

    Update Player class
    - add set_choice method
    - add clear_choice method
    - add attr_reader for choice
    
    Unit test for the above methods

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,6 @@
 class Player
-  attr_reader :name
+  attr_reader :name, :choice
+  VALID_OPTIONS = [ 'rock', 'paper', 'scissors' ]
 
   def initialize(name, npc = false)
     @name = name
@@ -9,5 +10,16 @@ class Player
 
   def computer?
     @computer_controlled
+  end
+
+  def set_choice(choice)
+    choice.downcase!
+    raise 'Not a valid option' unless VALID_OPTIONS.include?(choice)
+
+    @choice = choice
+  end
+
+  def clear_choice
+    @choice = nil
   end
 end
