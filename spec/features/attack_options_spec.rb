c4397e989ffb5ce6f630320309d5e07cb1b2b8a7
Valentina <maggio.vale@gmail.com>
2022-03-12 12:27:34 +0000

    Add feature test for Computer selections

--- spec/features/attack_options_spec.rb
+++ spec/features/attack_options_spec.rb
@@ -5,19 +5,16 @@ feature 'Player has three attack options' do
   end
 
   scenario 'Player can click on Rock button' do
-    sign_in_and_play
     click_button 'Rock'
     expect(page).to have_content('Valentina chose Rock')
   end
 
   scenario 'Player can click on Paper button' do
-    sign_in_and_play
     click_button 'Paper'
     expect(page).to have_content('Valentina chose Paper')
   end
 
   scenario 'Player can click on Scissors button' do
-    sign_in_and_play
     click_button 'Scissors'
     expect(page).to have_content('Valentina chose Scissors')
   end
---
+++ spec/features/computer_attack_options_spec.rb
@@ -0,0 +1,11 @@
+feature 'Computer has three attack options' do
+
+  before do
+    sign_in_and_play
+  end
+
+  scenario 'Computer can choose a random option' do
+    click_button 'Rock'
+    expect(page).to have_content('Computer chose')
+  end
+end
