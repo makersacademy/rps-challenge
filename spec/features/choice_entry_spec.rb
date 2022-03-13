b5b71c47d67100b3cc4db149fb26d924d182da02
Paul <pkilgarriff@gmail.com>
2022-03-13 22:22:10 +0000

    Correct spelling of Marketeer

--- spec/features/choice_entry_spec.rb
+++ spec/features/choice_entry_spec.rb
@@ -1,6 +1,6 @@
 feature 'Choice Entry Pages' do 
   scenario 'player 1 has a page where they can make a choice' do
-    setup_1_player('Marketer 1')
+    setup_1_player('Marketeer 1')
     click_button('Continue to Game')
     expect(page).to have_button 'Player 1 Choice'
     click_button('Player 1 Choice')
@@ -9,9 +9,9 @@ feature 'Choice Entry Pages' do
     expect(page).to have_button 'Scissors'
     Game.instance.reset
   end
-  
+
   scenario 'a multiplayer game has a page for player 2 to make choices as well' do
-    setup_2_players('Marketer 1', 'Marketer 2')
+    setup_2_players('Marketeer 1', 'Marketeer 2')
     click_button('Continue to Game')
     expect(page).to have_button 'Player 1 Choice'
     expect(page).to have_button 'Player 2 Choice'
--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,12 +1,12 @@
 feature 'Welcome Page' do 
   scenario 'entering a player name displays it on a welcome page' do
-    setup_1_player('Marketer 1')
-    expect(page).to have_content 'Hello Marketer 1'
+    setup_1_player('Marketeer 1')
+    expect(page).to have_content 'Hello Marketeer 1'
     Game.instance.reset
   end
 
   scenario 'playing as a single player causes a computer controlled player to be generated' do
-    setup_1_player('Marketer 1')
+    setup_1_player('Marketeer 1')
     expect(page).to have_content 'You are playing against the Computer'
     Game.instance.reset
   end
