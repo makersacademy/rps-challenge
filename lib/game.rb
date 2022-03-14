57446267834bb068e03bb7ffbf72adacc5ceac1d
arhussain1 <a.r.hussain@hotmail.com>
2022-03-14 09:36:23 +0000

    added method #decide_winner

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,8 @@
 class Game
-  attr_reader :users_choice
+  attr_reader :user_choice
 
   def initialize(users_choice)
-    @users_choice = users_choice
+    @user_choice = users_choice
   end
 
   def generate_computer_choice
@@ -11,6 +11,24 @@ class Game
     random_index = rand(3)
     
     @comp_choice = options[random_index]
+  end
+
+  def decide_winner
+    generate_computer_choice
+    p @user_choice
+    p @comp_choice
+
+    if @user_choice == @comp_choice
+      "draw"
+    elsif @user_choice == "Rock" && @comp_choice == "Scissors"
+      "user" 
+    elsif @user_choice == "Paper" && @comp_choice == "Rock"
+      "user"
+    elsif @user_choice == "Scissors" && @comp_choice == "Paper"
+      "user"
+    else
+      "computer"
+    end
   end
 
 
--- spec/unit/game_spec.rb
+++ spec/unit/game_spec.rb
@@ -5,7 +5,7 @@ describe Game do
   game = Game.new("Rock")
 
   it "should take the users choice as a parameter and store it" do
-    expect(game.users_choice).to eq("Rock")
+    expect(game.user_choice).to eq("Rock")
   end
 
   describe "making a random choice for the computer" do
@@ -28,5 +28,17 @@ describe Game do
       expect(game.generate_computer_choice).to eq("Scissors")
     end 
 
+  end
+
+  describe "the #decide_winner method" do
+    describe "situations where the user wins" do
+      it "should return 'user' if user_choice = 'Rock' and comp_choice = 'Scissors'" do
+        game1 = Game.new("Rock")
+
+        allow(game1).to receive(:rand).and_return(2)
+
+        expect(game1.decide_winner).to eq("user")
+      end
+    end
   end
 end
