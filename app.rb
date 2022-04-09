7ad1c305db1f0f432694e2f7fb4c15d90fa435fb
Michael Su <mailmsuu@gmail.com>
2022-04-09 11:58:59 +0100

    Inputting name now works and passed tests

--- app.rb
+++ app.rb
@@ -1,13 +1,26 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/player.rb'
 
 class RPS < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do 
     erb(:index)
+  end
+
+  post '/name' do
+    $player = Player.new(params[:name])
+    redirect '/game' 
+  end
+
+  get '/game' do
+    @player = $player
+    erb(:game)
   end
 
   run! if app_file == $0
--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,9 +1,10 @@
 require './app.rb'
 
 feature 'Player name' do
-  scenario 'Can fill in name and see names on-screen' do
+  scenario 'Can fill in name and see name on-screen' do
     visit('/')
     fill_in 'Player name', with: "Michael"
+    click_button 'Submit'
     expect(page).to have_content 'Michael'
   end
 end
