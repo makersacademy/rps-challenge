5d706ac1845c2ba739dac31ae2dc0fdbfd0759bb
Conor Hope <c_hope@hotmail.co.uk>
2022-04-10 18:54:46 +0100

    Amended weapon array to a constant in computer_player.rb

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -3,12 +3,14 @@ class ComputerPlayer
   attr_reader :name
   attr_accessor :weapon
 
+  WEAPON = ['Rock', 'Paper', 'Scissors'].freeze
+
   def initialize(name = 'Thor')
     @name = name
   end
 
   def random_weapon
-    @weapon = ['Rock', 'Paper', 'Scissors'].sample
+    @weapon = WEAPON.sample
   end
 
 end
