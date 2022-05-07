73453d56503d618718993cffca8824d505f3502e
Anish <anishkakaiya@outlook.com>
2022-05-07 16:37:21 +0100

    add name to play webpage

--- app.rb
+++ app.rb
@@ -2,13 +2,25 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class RockPaperScissors < Sinatra::Base
+  
   configure :development do
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get "/" do
-    'Hello World'
+    erb :index
   end
 
+  post "/name" do
+    session[:player_1_name] = params[:player_1_name]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player_1_name = session[:player_1_name]
+    erb :play
+  end
   run! if app_file == $0
 end
