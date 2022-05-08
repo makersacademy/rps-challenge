d3ae2f66832e632b74079d699a84e4179f3b85f9
Anish <anishkakaiya@outlook.com>
2022-05-08 21:38:14 +0100

    rename file

--- spec/unit/rps_unit_logic_spec.rb
+++ spec/unit/rps_unit_logic_spec.rb
@@ -2,8 +2,7 @@ require "rps_logic"
 
 describe RpsLogic do
   xit "chooses winner between rock (computer) and paper (user)" do
-    rps = RpsLogic.new("Paper")
-    allow_any_instance_of(RpsComputerSelector).to receive(:random_index).and_return(1)
+    rps = RpsLogic.new("Paper", "Rock")
     rps.calculate
     expect(rps.result).to eq("User Wins")
   end
