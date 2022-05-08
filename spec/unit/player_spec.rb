2004c63a7da388147257d580668a633b02263d00
Farzan-I <farzan_15@hotmail.co.uk>
2022-05-08 18:37:16 +0100

    Made Unit and Feature tests

---
+++ spec/unit/player_spec.rb
@@ -0,0 +1,14 @@
+require 'player'
+
+describe Player do
+
+  let(:player) { Player.new("Farzan") }
+
+  context "name" do
+
+    it "returns name of the player" do
+      expect(player.name).to eq "Farzan"
+    end 
+
+  end 
+end
