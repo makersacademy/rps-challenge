33655686e5421cffe4e9d1f96f5d3f624c0e5ab8
sjtinsley <simonjtinsley@gmail.com>
2022-05-10 11:47:55 +0100

    One step further to passing tests, committing as I would like feedback from here - thanks to Gawain for finding the previous problem

--- spec/feature_tests/game_feature_spec.rb
+++ spec/feature_tests/game_feature_spec.rb
@@ -5,23 +5,23 @@ feature "Player can play RPS" do
   end
 
     scenario "I select rock and I'm told I've won" do
-      allow_any_instance_of(Game).to receive(:name).and_return(2)
+      allow_any_instance_of(Game).to receive(:move).and_return(2) # Can't get this to stub out randomness in the tests
       enter_name
-      click_button "Rock"
+      click_link "Rock"
       expect(page).to have_content("Congratulations, you won! You picked rock and the computer picked scissors.")
     end
 
   scenario "I select scissors and I'm told it's a draw" do
-    allow_any_instance_of(Game).to receive(:name).and_return(2)
+    allow_any_instance_of(Game).to receive(:move).and_return(2)
     enter_name
-    click_button "Paper"
+    click_link "Scissors"
     expect(page).to have_content("It's a draw! You picked scissors and so did the computer.")
   end
 
   scenario "I select paper and I'm told it's a loss" do
-    allow_any_instance_of(Game).to receive(:name).and_return(2)
+    allow_any_instance_of(Game).to receive(:move).and_return(2)
     enter_name
-    click_button "Scissors"
+    click_link "Paper"
     expect(page).to have_text("Sorry, you lose! You picked paper and the computer picked scissors.")
   end
   
