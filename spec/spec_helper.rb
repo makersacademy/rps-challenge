fd1cb36cf1bdb216d6e94d424542c9e7d40499e2
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-05-10 20:04:14 +0100

    required name added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,10 +21,10 @@ end
 
 ENV['RACK_ENV'] = 'test'
 
-  require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
-  require 'capybara'
-  require 'capybara/rspec'
-  require 'rspec'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
 
-  Capybara.app = Game
+Capybara.app = Game
