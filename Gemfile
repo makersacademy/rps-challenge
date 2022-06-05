7e84d4830b902f799e7270262ca57857347dfb8f
benjamin <b.james.neustadt@gmail.com>
2022-06-05 14:33:36 +0200

    started draft on Rock paper scissor challenge

--- Gemfile
+++ Gemfile
@@ -2,15 +2,20 @@ source 'https://rubygems.org'
 
 ruby '3.0.2'
 
-gem 'sinatra'
+group :production do
+  gem 'sinatra'
+end
 
 group :test do
   gem 'capybara'
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem "webrick", "~> 1.7"
 end
 
 group :development, :test do
+  gem 'sinatra'
   gem 'rubocop', '1.20'
 end
+
---
+++ controller.rb
@@ -0,0 +1,47 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+require_relative 'lib/model1'
+
+class RockPaperScissors < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Welcome to Rock Paper Scissors, get ready to play.'
+    erb :name
+  end
+
+  get '/names' do
+    'Hello #{@player}'
+    redirect :play
+  end
+
+  post '/assign_name' do
+    @player = Player.new(params[:name])
+    erb :player
+  end
+
+  run! if app_file == $0
+end
+
+
+
+
+
+# As a marketeer
+# So that I can see my name in lights
+# I would like to register my name before playing an online game
+
+# As a marketeer
+# So that I can enjoy myself away from the daily grind
+# I would like to be able to play rock/paper/scissors
+# ```
+
+# Hints on functionality
+
+# - the marketeer should be able to enter their name before the game
+# - the marketeer will be presented the choices (rock, paper and scissors)
+# - the marketeer can choose one option
+# - the game will choose a random option
+# - a winner will be declared
