afe056418ffe4aea681e7d426d17d7b181fe83bf
Kay <kard@hotmail.co.uk>
2022-06-06 10:20:30 +0100

    changed result spec tests so they test the whole process

--- spec/features/result_spec.rb
+++ spec/features/result_spec.rb
@@ -1,30 +1,27 @@
 RSpec.describe 'results' do
   feature 'player 1 picks rock and player 2 chooses scissors' do
     scenario 'shows player 1 as the winner' do
-      player1 = double :player, choice: 'rock', name: 'Sandra'
-      player2 = double :player, choice: 'scissors', name: 'Sandy'
-      $game = Game.new(player1, player2)
-      visit '/result'
-      expect(page).to have_content 'Sandra wins!'
+      sign_in_and_play
+      click_on 'Rock'
+      click_on 'Scissors'
+      expect(page).to have_content 'Annabelle wins!'
     end
   end
 
   feature 'player 1 picks rock and player 2 picks paper' do
     scenario 'shows player 1 as the winner' do
-      player1 = double :player, choice: 'rock', name: 'Sandra'
-      player2 = double :player, choice: 'paper', name: 'Sandy'
-      $game = Game.new(player1, player2)
-      visit '/result'
+      sign_in_and_play
+      click_on 'Rock'
+      click_on 'Paper'
       expect(page).to have_content 'Sandy wins!'
     end
   end
 
   feature 'both players pick paper' do
     scenario 'shows a draw' do
-      player1 = double :player, choice: 'paper', name: 'Sandra'
-      player2 = double :player, choice: 'paper', name: 'Sandy'
-      $game = Game.new(player1, player2)
-      visit '/result'
+      sign_in_and_play
+      click_on 'Paper'
+      click_on 'Paper'
       expect(page).to have_content "It's a draw!"
     end
   end
