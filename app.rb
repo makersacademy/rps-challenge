508234a36e0ae878a37180b4a574904acd9e3e4c
S-Spiegl <steven.spiegl@gmail.com>
2022-08-08 00:23:24 +0100

    testing if PG is the issue

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader'
 require './lib/player'
 require './lib/round'
 require './lib/game'
-require './database_connection_setup'
+# require './database_connection_setup'
 
 class RockPaperScissors < Sinatra::Base
   configure :development do
@@ -12,36 +12,36 @@ class RockPaperScissors < Sinatra::Base
 
   enable :sessions
 
-  # get '/' do
-  #   $game = Game.new
-  #   erb :form
-  # end
+  get '/' do
+    $game = Game.new
+    erb :form
+  end
 
-  # post '/play' do
-  #   session[:player] = params[:player]
-  #   $player = Player.new(params[:player])
-  #   @player_name = $player.name
-  #   @game = $game
-  #   erb :play
-  # end
+  post '/play' do
+    session[:player] = params[:player]
+    $player = Player.new(params[:player])
+    @player_name = $player.name
+    @game = $game
+    erb :play
+  end
 
-  # post '/play_again' do
-  #   # session[:player] = params[:player]
-  #   # $player = Player.new(params[:player])
-  #   # @player_name = $player.name
-  #   @game = $game
-  #   erb :play_again
+  post '/play_again' do
+    # session[:player] = params[:player]
+    # $player = Player.new(params[:player])
+    # @player_name = $player.name
+    @game = $game
+    erb :play_again
     
-  # end
+  end
 
-  # post '/battle' do
-  #   @player = $player
-  #   @player_weapon = @player.select_weapon(params[:player_choice])
-  #   round = Round.new(@player_weapon, $game)
-  #   @message = round.engine
-  #   @computer_weapon = round.computer_weapon
-  #   erb :battle
-  # end
+  post '/battle' do
+    @player = $player
+    @player_weapon = @player.select_weapon(params[:player_choice])
+    round = Round.new(@player_weapon, $game)
+    @message = round.engine
+    @computer_weapon = round.computer_weapon
+    erb :battle
+  end
 
 # Start the server if this file is executed directly (don't change the line below)
   run! if app_file == $0
