a74ace61aace9ef95fcee99fdb2fe7fe36869d5d
S-Spiegl <steven.spiegl@gmail.com>
2022-08-08 00:28:19 +0100

    fixing PG

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -3,7 +3,7 @@ require 'pg'
 class DatabaseConnection
 
   def self.setup(dbname)
-    @connection = PG.connect(dbname: dbname)
+    @connection = PG.connect('postgres://oilfaxhbqovhdj:f1a4c5e7a499b5476a202fd6d46bfa69c31d582bd5f6bd0a7154c35fea44f621@ec2-44-206-11-200.compute-1.amazonaws.com:5432/daovotsf76t904')
   end
 
   def self.query(sql, params = [])
