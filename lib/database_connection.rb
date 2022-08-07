1a95f63b2aedc63cbe102b7b10e4e72a7ee4f51d
S-Spiegl <steven.spiegl@gmail.com>
2022-08-08 00:44:24 +0100

    fixing PG still

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -3,21 +3,33 @@ require 'pg'
 class DatabaseConnection
 
   def self.setup(dbname)
-    connection = pg_connection
     @connection = PG.connect('postgres://oilfaxhbqovhdj:f1a4c5e7a499b5476a202fd6d46bfa69c31d582bd5f6bd0a7154c35fea44f621@ec2-44-206-11-200.compute-1.amazonaws.com:5432/daovotsf76t904')
   end
 
   def self.query(sql, params = [])
-    connection = pg_connection
     @connection.exec_params(sql, params)
   end
 
-  def self.pg_connection
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect("postgres://oilfaxhbqovhdj:f1a4c5e7a499b5476a202fd6d46bfa69c31d582bd5f6bd0a7154c35fea44f621@ec2-44-206-11-200.compute-1.amazonaws.com:5432/daovotsf76t904")
-    end
-  end
+  # def self.pg_connection
+  #   if ENV['ENVIRONMENT'] == 'test'
+  #     connection = PG.connect(dbname: 'chitter_test')
+  #   else
+  #     connection = PG.connect("postgres://oilfaxhbqovhdj:f1a4c5e7a499b5476a202fd6d46bfa69c31d582bd5f6bd0a7154c35fea44f621@ec2-44-206-11-200.compute-1.amazonaws.com:5432/daovotsf76t904")
+  #   end
+  # end
 
 end
+
+# require 'pg'
+
+# class DatabaseConnection
+
+#   def self.setup(dbname)
+#     @connection = PG.connect(dbname: dbname)
+#   end
+
+#   def self.query(sql, params = [])
+#     @connection.exec_params(sql, params)
+#   end
+
+# end
