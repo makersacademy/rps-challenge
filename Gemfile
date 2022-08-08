c252626e6983efb5258124ac5672a33588718e0c
S-Spiegl <steven.spiegl@gmail.com>
2022-08-08 01:16:20 +0100

    got PG working by transferring some of the logic into the round.rb, but would be better to get this back in its own class if possible

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '3.0.0'
+ruby '3.0.2'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
--- lib/round.rb
+++ lib/round.rb
@@ -17,35 +17,36 @@ class Round
     @computer = computer
     @computer_weapon = computer.weapon
     @game = game
+    @connection = PG.connect("postgres://oilfaxhbqovhdj:f1a4c5e7a499b5476a202fd6d46bfa69c31d582bd5f6bd0a7154c35fea44f621@ec2-44-206-11-200.compute-1.amazonaws.com:5432/daovotsf76t904")
   end
 
   def engine
     if @player_weapon == @computer_weapon
-      DatabaseConnection.query("UPDATE scores SET drew = drew + 1;")
+      @connection.query("UPDATE scores SET drew = drew + 1;")
       @game.increment_drew
       "draw"
     elsif @player_weapon == :rock && @computer_weapon == :scissors
-      DatabaseConnection.query("UPDATE scores SET won = won + 1;")
+      @connection.query("UPDATE scores SET won = won + 1;")
       @game.increment_won
       "You chose wisely."
     elsif @player_weapon == :rock && @computer_weapon == :paper
-      DatabaseConnection.query("UPDATE scores SET lost = lost + 1;")
+      @connection.query("UPDATE scores SET lost = lost + 1;")
       @game.increment_lost
       "You did not choose wisely."
     elsif @player_weapon == :scissors && @computer_weapon == :paper
-      DatabaseConnection.query("UPDATE scores SET won = won + 1;")
+      @connection.query("UPDATE scores SET won = won + 1;")
       @game.increment_won
       "You chose wisely."
     elsif @player_weapon == :scissors && @computer_weapon == :rock
-      DatabaseConnection.query("UPDATE scores SET lost = lost + 1;")
+      @connection.query("UPDATE scores SET lost = lost + 1;")
       @game.increment_lost
       "You did not choose wisely."
     elsif @player_weapon == :paper && @computer_weapon == :rock
-      DatabaseConnection.query("UPDATE scores SET won = won + 1;")
+      @connection.query("UPDATE scores SET won = won + 1;")
       @game.increment_won
       "You chose wisely."
     elsif @player_weapon == :paper && @computer_weapon == :scissors
-      DatabaseConnection.query("UPDATE scores SET lost = lost + 1;")
+      @connection.query("UPDATE scores SET lost = lost + 1;")
       @game.increment_lost
       "You did not choose wisely."
     end
