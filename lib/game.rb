28e172d8e17e4a29415f8501053932451c8ad5bf
Lilly Daniell <daniell.lilly@gmail.com>
2023-03-24 16:52:55 +0000

    Refactor result method, sanity check test descriptions

--- lib/game.rb
+++ lib/game.rb
@@ -6,29 +6,29 @@ class Game
     @inputs = [player_input]
   end
   
+  def result
+    store_opponent_input
+    player, opponent = @inputs  
+    return :draw if player == opponent
+    winning_moves[player] == opponent ? :win : :lose
+  end
+
+  private 
+
   def store_opponent_input
     @inputs << opponent_input
-    opponent_input
   end
 
   def opponent_input # encapsulate to enable stubbing while testing
     ["rock", "paper", "scissor"].sample
   end
 
-  def result
-    store_opponent_input
-    p @inputs
-    player, opponent = @inputs
-    case
-      when player == "rock" && opponent == "paper" then return :lose
-      when player == "scissor" && opponent == "paper" then return :win
-      when player == "scissor" && opponent == "rock" then return :lose
-      when player == "paper" && opponent == "rock" then return :win
-      when player == "paper"  && opponent == "scissor" then return :lose
-      when player == "rock" && opponent == "scissor" then return :win
-      else
-        :draw
-    end 
+  def winning_moves
+    winning_moves = {
+      'rock' => 'scissor',
+      'scissor' => 'paper',
+      'paper' => 'rock'
+    }  
   end
 end
 
